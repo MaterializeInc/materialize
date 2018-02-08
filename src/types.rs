ef930cc621a33239bc0259c394cc0ec14acf50ce
Flavien Raynaud <flavien.raynaud@gmail.com>
2018-02-08 21:40:16 +0000

    Remove comments

--- src/types.rs
+++ src/types.rs
@@ -31,95 +31,6 @@ pub trait ToAvro {
     fn avro(self) -> Value;
 }
 
-macro_rules! to_avro(
-    ($t:ty, $v:expr) => (
-        impl ToAvro for $t {
-            fn avro(self) -> Value {
-                $v(self)
-            }
-        }
-    );
-);
-
-/*
-to_avro!(bool, Value::Boolean, Schema::Boolean);
-to_avro!(i32, Value::Int, Schema::Int);  // TODO: int to long/float/double, etc...
-to_avro!(i64, Value::Long, Schema::Long);
-to_avro!(f32, Value::Float, Schema::Float);
-to_avro!(f64, Value::Double, Schema::Double);
-to_avro!(String, Value::String, Schema::String);
-
-impl ToAvro for () {
-    fn avro(self) -> Value {
-        Value::Null
-    }
-}
-
-impl ToAvro for usize {
-    fn avro(self) -> Value {
-        (self as i64).avro()
-    }
-}
-
-impl<'a> ToAvro for &'a str {
-    fn avro(self, schema: &Schema) -> Option<Value> {
-        match schema {
-            &Schema::String => Some(Value::String(self.to_owned())),
-            _ => None,
-        }
-    }
-}
-
-impl<'a> ToAvro for &'a [u8] {
-    fn avro(self, schema: &Schema) -> Option<Value> {
-        match schema {
-            &Schema::Bytes => Some(Value::Bytes(self.to_owned())),
-            &Schema::Fixed { size, .. } if size == self.len() => Some(Value::Fixed(self.len(), self.to_owned())),
-            _ => None,
-        }
-
-    }
-}
-
-impl<T> ToAvro for Option<T> where T: ToAvro {
-    fn avro(self, schema: &Schema) -> Option<Value> {
-        match schema {
-            &Schema::Union(ref inner) => Value::Union(self.map(|v| Box::new(v.avro(inner)))),
-        }
-    }
-}
-
-impl<T> ToAvro for HashMap<String, T> where T: ToAvro {
-    fn avro(self) -> Value {
-        Value::Map(self
-            .into_iter()
-            .map(|(key, value)| (key, value.avro()))
-            .collect::<_>())
-    }
-}
-
-impl<'a, T> ToAvro for HashMap<&'a str, T> where T: ToAvro {
-    fn avro(self) -> Value {
-        Value::Map(self
-            .into_iter()
-            .map(|(key, value)| (key.to_owned(), value.avro()))
-            .collect::<_>())
-    }
-}
-
-impl ToAvro for Value {
-    fn avro(self) -> Value {
-        self
-    }
-}
-
-impl<T> ToAvro for Box<T> where T: ToAvro {
-    fn avro(self) -> Value {
-        (*self).avro()
-    }
-}
-*/
-
 impl<S: Serialize> ToAvro for S {
     fn avro(self) -> Value {
         use ser::Serializer;
@@ -199,21 +110,6 @@ impl Record {
         }
     }
 
-    /*
-    pub fn from_value(value: &Value) -> Option<Record> {
-        match value {
-            &Value::Record(ref fields) => {
-                Some(Record {
-                    rschema: rschema.clone(),
-                    // lookup: rschema.lookup(),
-                    fields: fields.clone(),
-                })
-            },
-            _ => None,
-        }
-    }
-    */
-
     pub fn put<V>(&mut self, field: &str, value: V) where V: ToAvro {
         let lookup = self.rschema.lookup();  // TODO
         if lookup.get(field).is_none() {
@@ -233,29 +129,6 @@ impl ToAvro for Record {
         Value::Record(self.fields)
     }
 }
-
-/*
-impl ToAvro for JsonValue {
-    fn avro(self) -> Value {
-        match self {
-            JsonValue::Null => Value::Null,
-            JsonValue::Bool(b) => Value::Boolean(b),
-            JsonValue::Number(ref n) if n.is_i64() => Value::Long(n.as_i64().unwrap()),
-            JsonValue::Number(ref n) if n.is_f64() => Value::Double(n.as_f64().unwrap()),
-            JsonValue::Number(n) => Value::Long(n.as_u64().unwrap() as i64),  // TODO: Not so great
-            JsonValue::String(s) => Value::String(s),
-            JsonValue::Array(items) =>
-                Value::Array(items.into_iter()
-                    .map(|item| item.avro())
-                    .collect::<_>()),
-            JsonValue::Object(items) =>
-                Value::Map(items.into_iter()
-                    .map(|(key, value)| (key, value.avro()))
-                    .collect::<_>()),
-        }
-    }
-}
-*/
 
 impl Value {
     pub fn with_schema(self, schema: &Schema) -> Option<Value> {
