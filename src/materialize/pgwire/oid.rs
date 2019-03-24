9f6f9aba3cd9c3747ab75ab1ec2b139b64a76724
Nikhil Benesch <nikhil.benesch@gmail.com>
2019-03-24 14:35:33 -0400

    mtrlz/pgwire: add missing copyright header

--- src/materialize/pgwire/oid.rs
+++ src/materialize/pgwire/oid.rs
@@ -1,2 +1,7 @@
+// Copyright 2019 Timely Data, Inc. All rights reserved.
+//
+// This file is part of Materialize. Materialize may not be used or
+// distributed without the express permission of Timely Data, Inc.
+
 pub const INT: u32 = 8;
 pub const STRING: u32 = 25;
