443a093a7eb1e2074afb15d0981b43ed7143eca8
Nikhil Benesch <nikhil.benesch@gmail.com>
2019-03-25 18:33:26 -0400

    mtrlz/repr: use ordered-float crate
    The OrderedFloat crate is a bit more feature-complete than the
    implementation I hacked together, and notably supports hashing floats,
    which we'll need momentarily.

--- src/materialize/repr.rs
+++ src/materialize/repr.rs
@@ -20,6 +20,7 @@
 //! [`Datum`]: repr::Datum
 //! [`Datum::Tuple`]: repr::Datum::Tuple
 
+use ordered_float::OrderedFloat;
 use serde::{Serialize, Deserialize};
 use std::cmp::Ordering;
 
@@ -41,9 +42,9 @@ pub enum Datum {
     /// A 64-bit signed integer.
     Int64(i64),
     /// A 32-bit floating point number.
-    Float32(F32),
+    Float32(OrderedFloat<f32>),
     /// A 64-bit floating point number.
-    Float64(F64),
+    Float64(OrderedFloat<f64>),
     /// A sequence of untyped bytes.
     Bytes(Vec<u8>),
     /// A sequence of Unicode codepoints encoded as UTF-8.
@@ -93,101 +94,4 @@ pub enum Type {
     String,
     Tuple(Vec<Schema>),
     Array(Box<Schema>),
-}
-
-/// A 32-bit floating integer that implements [`Eq`] and [`Ord`].
-///
-/// The transitivity of the equivalence relation is provided by treating NaN
-/// (not-a-number) as equal to itself, in violation of the IEEE 754 floating
-/// point standard. A total ordering is provided by treating NaN as greater than
-/// all other values. This non-standard behavior is necessary to allow NaNs to
-/// be sorted and stored in indices, and is shared with (at least) PostgreSQL.
-#[derive(Clone, Copy, Debug, Serialize, Deserialize)]
-pub struct F32(pub f32);
-
-impl Eq for F32 {}
-
-impl PartialEq for F32 {
-    fn eq(&self, other: &F32) -> bool {
-        (self.0.is_nan() && other.0.is_nan()) || (self.0 == other.0)
-    }
-}
-
-impl Ord for F32 {
-    fn cmp(&self, other: &F32) -> Ordering {
-        match self.0.partial_cmp(&other.0) {
-            Some(ordering) => ordering,
-            None => match (self.0.is_nan(), other.0.is_nan()) {
-                (true, true) => Ordering::Equal,
-                (true, false) => Ordering::Greater,
-                (false, true) => Ordering::Less,
-                (false, false) => unreachable!(),
-            }
-        }
-    }
-}
-
-impl PartialOrd for F32 {
-    fn partial_cmp(&self, other: &F32) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-/// A 64-bit floating integer that implements [`Eq`] and [`Ord`].
-///
-/// See [`F32`] for details.
-#[derive(Clone, Copy, Debug, Serialize, Deserialize)]
-pub struct F64(pub f64);
-
-impl Eq for F64 {}
-
-impl PartialEq for F64 {
-    fn eq(&self, other: &F64) -> bool {
-        (self.0.is_nan() && other.0.is_nan()) || (self.0 == other.0)
-    }
-}
-
-impl Ord for F64 {
-    fn cmp(&self, other: &F64) -> Ordering {
-        match self.0.partial_cmp(&other.0) {
-            Some(ordering) => ordering,
-            None => match (self.0.is_nan(), other.0.is_nan()) {
-                (true, true) => Ordering::Equal,
-                (true, false) => Ordering::Greater,
-                (false, true) => Ordering::Less,
-                (false, false) => unreachable!(),
-            }
-        }
-    }
-}
-
-impl PartialOrd for F64 {
-    fn partial_cmp(&self, other: &F64) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_f32_ord() {
-        let nan = F32(std::f32::NAN);
-
-        assert!(nan == nan);
-        assert!(nan > F32(1.0));
-        assert!(F32(1.0) < nan);
-        assert!(F32(1.0) == F32(1.0));
-    }
-
-    #[test]
-    fn test_f64_ord() {
-        let nan = F64(std::f64::NAN);
-
-        assert!(nan == nan);
-        assert!(nan > F64(1.0));
-        assert!(F64(1.0) < nan);
-        assert!(F64(1.0) == F64(1.0));
-    }
 }
