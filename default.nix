1b5b7cf2567a12c9b4fc6584d4f9b06f09981093
Jamie Brandon <jamie@scattered-thoughts.net>
2019-04-15 16:11:04 -0400

    Pin nixpkgs version

--- default.nix
+++ default.nix
@@ -1,6 +1,12 @@
-{ pkgs ? import <nixpkgs> {} }:
+let
 
-let confluent = pkgs.callPackage ./confluent.nix { pkgs = pkgs; };
+pkgs = import (builtins.fetchTarball {
+  name = "nixos-19-03-pre";
+  url = https://github.com/nixos/nixpkgs/archive/5c52b25283a6cccca443ffb7a358de6fe14b4a81.tar.gz;
+  sha256 = "0fhbl6bgabhi1sw1lrs64i0hibmmppy1bh256lq8hxy3a2p1haip";
+}) {};
+
+confluent = pkgs.callPackage ./confluent.nix { pkgs = pkgs; };
 
 in pkgs.stdenv.mkDerivation rec {
   name = "materialize";
