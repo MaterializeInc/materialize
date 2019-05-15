48a04450ae865bdb34dceb854d70860ae79b010a
Frank McSherry <fmcsherry@me.com>
2019-05-15 14:59:06 -0400

    add copyright

--- src/materialize/sql/multiway_plan.rs
+++ src/materialize/sql/multiway_plan.rs
@@ -1,3 +1,8 @@
+// Copyright 2019 Materialize, Inc. All rights reserved.
+//
+// This file is part of Materialize. Materialize may not be used or
+// distributed without the express permission of Materialize, Inc.
+
 use failure::bail;
 use sqlparser::sqlast;
 use sqlparser::sqlast::ASTNode;
