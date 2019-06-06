a80a265867eccadf719e2e6c45773f3aeef84321
Jamie Brandon <jamie@scattered-thoughts.net>
2019-06-06 16:39:25 +0100

    The shame, the shame...
    Fixes https://github.com/MaterializeInc/materialize/issues/125

--- src/materialize/sql/mod.rs
+++ src/materialize/sql/mod.rs
@@ -968,7 +968,7 @@ impl Planner {
             exprs.push(ScalarExpr::CallBinary {
                 func: BinaryFunc::Eq,
                 expr1: Box::new(ScalarExpr::Column(l)),
-                expr2: Box::new(ScalarExpr::Column(2)),
+                expr2: Box::new(ScalarExpr::Column(r)),
             });
             dropped_columns.insert(r);
         }
