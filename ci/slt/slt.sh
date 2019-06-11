53dfe885f8cd829fe0658e8f6536d25d503bf110
Nikhil Benesch <nikhil.benesch@gmail.com>
2019-06-11 15:25:47 -0400

    ci/slt: stopgap for new SLT output format

--- ci/slt/slt.sh
+++ ci/slt/slt.sh
@@ -32,8 +32,10 @@ cargo run --release --bin=sqllogictest -- \
     "$verbosity" "$@" | tee target/slt.out || true
 
 if [[ "${BUILDKITE_BRANCH-}" = master && "${BUILDKITE_COMMIT-}" ]]; then
+    # TODO(benesch): handle the new fields in the output, rather than just
+    # ignoring them.
     template="INSERT INTO slt (commit, unsupported, parse_failure, plan_failure, inference_failure, output_failure, bail, success) VALUES ('$BUILDKITE_COMMIT', \\1, \\2, \\3, \\4, \\5, \\6, \\7);"
     sql=$(tail -n1 target/slt.out \
-        | sed -E "s/^.*unsupported=([0-9]+) parse-failure=([0-9]+) plan-failure=([0-9]+) inference-failure=([0-9]+) output-failure=([0-9]+) bail=([0-9]+) success=([0-9]+).*$/$template/")
+        | sed -E "s/^.*unsupported=([0-9]+) parse-failure=([0-9]+) plan-failure=([0-9]+).*inference-failure=([0-9]+).*output-failure=([0-9]+) bail=([0-9]+) success=([0-9]+).*$/$template/")
     ssh buildkite@mtrlz.dev psql <<< "$sql"
 fi
