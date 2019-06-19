1e168c9106c25d0448c1c31a9468a46c66503434
Frank McSherry <fmcsherry@me.com>
2019-06-19 11:56:42 -0400

    single kafka operator

--- src/materialize/dataflow/source/kafka.rs
+++ src/materialize/dataflow/source/kafka.rs
@@ -20,11 +20,14 @@ pub fn kafka<G>(
     scope: &G,
     name: &str,
     connector: &KafkaSourceConnector,
-) -> (Stream<G, (Vec<Datum>, Timestamp, Diff)>, SharedCapability)
+    read_kafka: bool,
+) -> (Stream<G, (Vec<Datum>, Timestamp, Diff)>, Option<SharedCapability>)
 where
     G: Scope<Timestamp = Timestamp>,
 {
+    let (stream, capability) =
     source(scope, name, move |info| {
+
         let name = name.to_owned();
         let activator = scope.activator_for(&info.address[..]);
 
@@ -42,66 +45,83 @@ where
             .set("session.timeout.ms", "6000")
             .set("bootstrap.servers", &connector.addr.to_string());
 
-        let consumer: BaseConsumer<DefaultConsumerContext> = config.create().unwrap();
-        consumer.subscribe(&[&connector.topic]).unwrap();
+        let mut consumer: Option<BaseConsumer<DefaultConsumerContext>> = None;
+        if read_kafka {
+            consumer = Some(config.create().expect("Failed to create Kafka Consumer"));
+        }
+
+        if let Some(consumer) = consumer.as_mut() {
+            consumer.subscribe(&[&connector.topic]).unwrap();
+        }
 
         move |cap, output| {
-            // Indicate that we should run again.
-            activator.activate();
 
-            // Repeatedly interrogate Kafka for messages. Cease only when
-            // Kafka stops returning new data. We could cease earlier, if we
-            // had a better policy.
-            while let Some(result) = consumer.poll(Duration::from_millis(0)) {
-                match result {
-                    Ok(message) => {
-                        let payload = match message.payload() {
-                            Some(p) => p,
-                            None => {
-                                // TODO(benesch): what does it mean to have a
-                                // Kafka message with no payload?
-                                error!("dropped kafka message with no payload");
-                                continue;
-                            }
-                        };
+            if let Some(consumer) = consumer.as_mut() {
 
-                        let ms = match message.timestamp() {
-                            KafkaTimestamp::NotAvailable => {
-                                // TODO(benesch): do we need to do something
-                                // else?
-                                error!("dropped kafka message with no timestamp");
-                                continue;
-                            }
-                            KafkaTimestamp::CreateTime(ms) | KafkaTimestamp::LogAppendTime(ms) => {
-                                ms as u64
-                            }
-                        };
-                        if ms >= *cap.time() {
-                            cap.downgrade(&ms)
-                        } else {
-                            error!(
-                                "{}: fast-forwarding out-of-order Kafka timestamp from {} to {}",
-                                name,
-                                ms,
-                                *cap.time()
-                            );
-                        };
+                // Indicate that we should run again.
+                activator.activate();
 
-                        match decoder.decode(payload) {
-                            Ok(diff_pair) => {
-                                if let Some(before) = diff_pair.before {
-                                    output.session(&cap).give((before, *cap.time(), -1));
+                // Repeatedly interrogate Kafka for messages. Cease only when
+                // Kafka stops returning new data. We could cease earlier, if we
+                // had a better policy.
+                while let Some(result) = consumer.poll(Duration::from_millis(0)) {
+                    match result {
+                        Ok(message) => {
+                            let payload = match message.payload() {
+                                Some(p) => p,
+                                None => {
+                                    // TODO(benesch): what does it mean to have a
+                                    // Kafka message with no payload?
+                                    error!("dropped kafka message with no payload");
+                                    continue;
                                 }
-                                if let Some(after) = diff_pair.after {
-                                    output.session(&cap).give((after, *cap.time(), 1));
+                            };
+
+                            let ms = match message.timestamp() {
+                                KafkaTimestamp::NotAvailable => {
+                                    // TODO(benesch): do we need to do something
+                                    // else?
+                                    error!("dropped kafka message with no timestamp");
+                                    continue;
                                 }
+                                KafkaTimestamp::CreateTime(ms) | KafkaTimestamp::LogAppendTime(ms) => {
+                                    ms as u64
+                                }
+                            };
+                            if ms >= *cap.time() {
+                                cap.downgrade(&ms)
+                            } else {
+                                error!(
+                                    "{}: fast-forwarding out-of-order Kafka timestamp from {} to {}",
+                                    name,
+                                    ms,
+                                    *cap.time()
+                                );
+                            };
+
+                            match decoder.decode(payload) {
+                                Ok(diff_pair) => {
+                                    if let Some(before) = diff_pair.before {
+                                        output.session(&cap).give((before, *cap.time(), -1));
+                                    }
+                                    if let Some(after) = diff_pair.after {
+                                        output.session(&cap).give((after, *cap.time(), 1));
+                                    }
+                                }
+                                Err(err) => error!("avro deserialization error: {}", err),
                             }
-                            Err(err) => error!("avro deserialization error: {}", err),
                         }
+                        Err(err) => error!("kafka error: {}: {}", name, err),
                     }
-                    Err(err) => error!("kafka error: {}: {}", name, err),
                 }
             }
         }
-    })
+    });
+
+    if read_kafka {
+        (stream, Some(capability))
+    }
+    else {
+        (stream, None)
+    }
 }
