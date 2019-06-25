9f1509f45f65c844889fab5f7a0901b649c5cacc
Nikhil Benesch <nikhil.benesch@gmail.com>
2019-06-24 23:05:09 -0400

    mtrlz/repr: add notes about weird regex design

--- src/materialize/repr/regex.rs
+++ src/materialize/repr/regex.rs
@@ -9,8 +9,26 @@ use std::hash::{Hash, Hasher};
 
 /// A regular expression object suitable for use in `crate::repr::Datum`. The
 /// object exhibits a number of strange properties as a result: it claims to
-/// implement [`PartialEq`], [`Eq`], [`PartialOrd`], and [`Ord`], though it
-/// will panic if any of those traits' methods are invoked.
+/// implement [`PartialEq`], [`Eq`], [`PartialOrd`], and [`Ord`], though it will
+/// panic if any of those traits' methods are invoked. Efficiently implementing
+/// these traits with reasonable semantics is challenging, as doing so requires
+/// either peeking inside the regex object's internal state, or serializing the
+/// regex to a string, both of which are expensive. Since it is not currently
+/// possible to produce a plan that compares regexes, for now it seems best to
+/// panic.
+///
+/// We could avoid introducing this weirdness by store regexes as strings in the
+/// dataflow graph, compiling them into regex objects at the last possible
+/// moment (e.g., in the LIKE operator). But this would require special casing
+/// all regex optimizations to recover acceptable performance; we'd need
+/// regex-specific constant propagation to avoid recompiling constant regexes on
+/// every expression evaluation, and we wouldn't be able to pass compiled
+/// regexes between workers without forcing a recompilation.*
+///
+/// *Actually, I think the serde_regex serialization currently serializes
+/// regexes to a string and therefore recompiles them during deserialization.
+/// There is interest in the official regex crate about a more efficient
+/// binary serialization, though: https://github.com/rust-lang/regex/issues/258.
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct Regex(#[serde(with = "serde_regex")] pub regex::Regex);
 
