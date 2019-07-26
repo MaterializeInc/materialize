24553bbe1e5c865c0840d2dd7ceb271a89b299a2
Frank McSherry <fmcsherry@me.com>
2019-07-26 09:48:03 -0400

    extract worker zero address

--- src/materialize/server/mod.rs
+++ src/materialize/server/mod.rs
@@ -29,14 +29,16 @@ pub enum QueueConfig {
     Transient,
 }
 
+/// Options for how to return dataflow results.
 pub enum DataflowResultsConfig {
+    /// A process local exchange fabric.
     Local,
-    Remote,
+    /// An address for an HTTP listener.
+    Remote(String),
 }
 
 pub struct Config {
     queue: QueueConfig,
-    num_timely_workers: usize,
     dataflow_results: DataflowResultsConfig,
     timely_configuration: timely::Configuration,
 }
@@ -44,31 +46,44 @@ pub struct Config {
 impl Config {
     /// Constructs a materialize configuration from a timely dataflow configuration.
     pub fn from_timely(timely_configuration: timely::Configuration) -> Self {
-        let num_timely_workers = match &timely_configuration {
+        let post_address = match &timely_configuration {
+            timely::Configuration::Thread => {
+                "http://localhost:6875/api/dataflow-results".to_owned()
+            }
+            timely::Configuration::Process(_) => {
+                "http://localhost:6875/api/dataflow-results".to_owned()
+            }
+            timely::Configuration::Cluster { addresses, .. } => {
+                let address = addresses[0]
+                    .split(':')
+                    .next()
+                    .expect("Failed to find port in timely address");
+                format!("http://{}:6875/api/dataflow-results", address)
+            }
+        };
+
+        Self {
+            queue: QueueConfig::Transient,
+            dataflow_results: DataflowResultsConfig::Remote(post_address),
+            timely_configuration,
+        }
+    }
+
+    /// The number of timely workers described the by the configuration.
+    pub fn num_timely_workers(&self) -> usize {
+        match &self.timely_configuration {
             timely::Configuration::Thread => 1,
             timely::Configuration::Process(n) => *n,
             timely::Configuration::Cluster {
                 threads, addresses, ..
             } => threads * addresses.len(),
-        };
-
-        Self {
-            queue: QueueConfig::Transient,
-            num_timely_workers,
-            dataflow_results: DataflowResultsConfig::Remote,
-            timely_configuration,
         }
     }
 }
 
 impl Default for Config {
     fn default() -> Self {
-        Config {
-            queue: QueueConfig::Transient,
-            num_timely_workers: 4,
-            dataflow_results: DataflowResultsConfig::Remote,
-            timely_configuration: timely::Configuration::Process(4),
-        }
+        Self::from_timely(timely::Configuration::Thread)
     }
 }
 
@@ -123,6 +138,7 @@ pub fn serve(config: Config) -> Result<(), Box<dyn StdError>> {
     let dataflow_results_mux = DataflowResultsMux::default();
 
     // Initialize timely dataflow computation.
+    let num_timely_workers = config.num_timely_workers();
     let is_primary = match &config.timely_configuration {
         timely::Configuration::Thread => true,
         timely::Configuration::Process(_) => true,
@@ -132,7 +148,7 @@ pub fn serve(config: Config) -> Result<(), Box<dyn StdError>> {
         DataflowResultsConfig::Local => {
             dataflow::DataflowResultsHandler::Local(dataflow_results_mux.clone())
         }
-        DataflowResultsConfig::Remote => dataflow::DataflowResultsHandler::Remote,
+        DataflowResultsConfig::Remote(address) => dataflow::DataflowResultsHandler::Remote(address),
     };
     let dd_workers = dataflow::serve(
         dataflow_command_receiver,
@@ -157,7 +173,6 @@ pub fn serve(config: Config) -> Result<(), Box<dyn StdError>> {
     if is_primary {
         let listen_addr: SocketAddr = "127.0.0.1:6875".parse()?;
         let listener = TcpListener::bind(&listen_addr)?;
-        let num_timely_workers = config.num_timely_workers;
         let start = future::lazy(move || {
             let server = listener
                 .incoming()
