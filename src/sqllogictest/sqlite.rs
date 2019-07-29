e6e6f3c5e5c4e7b23d7c4166b0a062b03dc89ba4
Jamie Brandon <jamie@scattered-thoughts.net>
2019-07-29 15:44:19 -0700

    Handle conversions from sqlite to datum

--- src/sqllogictest/sqlite.rs
+++ src/sqllogictest/sqlite.rs
@@ -1,12 +1,15 @@
 use std::collections::HashMap;
 
-use failure::bail;
+use failure::{bail, ensure, format_err};
 use rusqlite::{types::ValueRef, Connection, NO_PARAMS};
-use sqlparser::ast::Statement;
-use sqlparser::dialect::AnsiDialect;
-use sqlparser::parser::{Parser as SqlParser, ParserError as SqlParserError};
+use sqlparser::ast::{DataType, Statement};
 
-use materialize::repr::Datum;
+use materialize::repr::{Datum, ScalarType};
+
+pub struct Sqlite {
+    connection: Connection,
+    table_types: HashMap<String, Vec<DataType>>,
+}
 
 pub type Row = Vec<Datum>;
 pub type Diff = Vec<(Row, isize)>;
@@ -17,66 +20,94 @@ pub enum Outcome {
     Changed(String, Diff),
 }
 
-pub fn open() -> Result<Connection, failure::Error> {
-    Ok(Connection::open_in_memory()?)
-}
-
-pub fn run_statement(
-    connection: &mut Connection,
-    statement: &Statement,
-) -> Result<Vec<Outcome>, failure::Error> {
-    Ok(match statement {
-        Statement::CreateTable { name, .. } | Statement::CreateView { name, .. } => {
-            connection.execute(&statement.to_string(), NO_PARAMS)?;
-            vec![Outcome::Created(name.to_string())]
-        }
-        Statement::Drop { names, .. } => {
-            connection.execute(&statement.to_string(), NO_PARAMS)?;
-            names
-                .iter()
-                .map(|name| Outcome::Dropped(name.to_string()))
-                .collect()
-        }
-        Statement::Delete { table_name, .. }
-        | Statement::Insert { table_name, .. }
-        | Statement::Update { table_name, .. } => {
-            let table_name = table_name.to_string();
-            let before = select_all(connection, &table_name)?;
-            connection.execute(&statement.to_string(), NO_PARAMS)?;
-            let after = select_all(connection, &table_name)?;
-            let mut diff = HashMap::new();
-            for row in before {
-                *diff.entry(row).or_insert(0) -= 1;
-            }
-            for row in after {
-                *diff.entry(row).or_insert(0) += 1;
-            }
-            diff.retain(|_, count| *count != 0);
-            vec![Outcome::Changed(table_name, diff.into_iter().collect())]
-        }
-        _ => bail!("Unsupported statement sent to sqlite: {:?}", statement),
-    })
-}
-
-fn select_all(connection: &mut Connection, table_name: &str) -> Result<Vec<Row>, failure::Error> {
-    let sqlite_rows = connection
-        .prepare(&format!("select * from {}", table_name))?
-        .query(NO_PARAMS)?;
-    let mut rows = vec![];
-    // TODO gonna need sql types in here
-    while let Some(sqlite_row) = sqlite_rows.next()? {
-        let row = (0..sqlite_row.column_count())
-            .map(|c| {
-                Ok(match sqlite_row.get_raw(c) {
-                    ValueRef::Null => Datum::Null,
-                    ValueRef::Integer(i) => Datum::Int64(i),
-                    ValueRef::Real(r) => Datum::Float64(r),
-                    ValueRef::Text(t) => Datum::String(String::from_utf8(t.to_vec())?),
-                    other => bail!("Unsupported sqlite type: {:?}", other),
-                })
-            })
-            .collect::<Result<_, _>>()?;
-        rows.push(row);
+impl Sqlite {
+    pub fn open() -> Result<Self, failure::Error> {
+        let connection = Connection::open_in_memory()?;
+        Ok(Self {
+            connection,
+            table_types: HashMap::new(),
+        })
     }
-    Ok(rows)
+
+    pub fn run_statement(&mut self, statement: &Statement) -> Result<Vec<Outcome>, failure::Error> {
+        Ok(match statement {
+            Statement::CreateTable { name, columns, .. } => {
+                self.connection.execute(&statement.to_string(), NO_PARAMS)?;
+                self.table_types.insert(
+                    name.to_string(),
+                    columns
+                        .iter()
+                        .map(|column| column.data_type.clone())
+                        .collect(),
+                );
+                vec![Outcome::Created(name.to_string())]
+            }
+            Statement::Drop { names, .. } => {
+                self.connection.execute(&statement.to_string(), NO_PARAMS)?;
+                names
+                    .iter()
+                    .map(|name| Outcome::Dropped(name.to_string()))
+                    .collect()
+            }
+            Statement::Delete { table_name, .. }
+            | Statement::Insert { table_name, .. }
+            | Statement::Update { table_name, .. } => {
+                let table_name = table_name.to_string();
+                let before = self.select_all(&table_name)?;
+                self.connection.execute(&statement.to_string(), NO_PARAMS)?;
+                let after = self.select_all(&table_name)?;
+                let mut diff = HashMap::new();
+                for row in before {
+                    *diff.entry(row).or_insert(0) -= 1;
+                }
+                for row in after {
+                    *diff.entry(row).or_insert(0) += 1;
+                }
+                diff.retain(|_, count| *count != 0);
+                vec![Outcome::Changed(table_name, diff.into_iter().collect())]
+            }
+            _ => bail!("Unsupported statement sent to sqlite: {:?}", statement),
+        })
+    }
+
+    fn select_all(&mut self, table_name: &str) -> Result<Vec<Row>, failure::Error> {
+        let mut prepared = self
+            .connection
+            .prepare(&format!("select * from {}", table_name))?;
+        let types = self
+            .table_types
+            .get(table_name)
+            .ok_or_else(|| format_err!("Unknown table: {:?}", table_name))?;
+        ensure!(
+            prepared.column_count() == types.len(),
+            "In table {:?} expected {} columns, received {}",
+            table_name,
+            types.len(),
+            prepared.column_count()
+        );
+        let mut sqlite_rows = prepared.query(NO_PARAMS)?;
+        let mut rows = vec![];
+        while let Some(sqlite_row) = sqlite_rows.next()? {
+            let row = (0..sqlite_row.column_count())
+                .map(|c| {
+                    Ok(
+                        match (sqlite_row.get_raw(c), ScalarType::from_sql(&types[c])?) {
+                            (ValueRef::Null, _) => Datum::Null,
+                            (ValueRef::Integer(i), ScalarType::Int64) => Datum::Int64(i),
+                            (ValueRef::Real(r), ScalarType::Float64) => {
+                                Datum::Float64(ordered_float::OrderedFloat(r))
+                            }
+                            (ValueRef::Text(t), ScalarType::String) => {
+                                Datum::String(String::from_utf8(t.to_vec())?)
+                            }
+                            // TODO(jamii) handle dates, decimals etc
+                            other => bail!("Unsupported sqlite type: {:?}", other),
+                        },
+                    )
+                })
+                .collect::<Result<_, _>>()?;
+            rows.push(row);
+        }
+        Ok(rows)
+    }
 }
