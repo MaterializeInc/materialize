8fac36046851dfc4c8c5ea9a0ab33739185908f9
Nikhil Benesch <nikhil.benesch@gmail.com>
2019-07-29 18:03:16 -0400

    ci: reorganize
    Modify layout of ci dir and Docker files to be more consistent.

--- ci/slt/slt.sh
+++ ci/slt/slt.sh
@@ -12,31 +12,16 @@ set -euo pipefail
 verbosity=-vv
 if [[ "${BUILDKITE-}" ]]; then
     verbosity=-v
-
-    # TODO(benesch): share with the deploy pipeline.
-    mkdir ~/.ssh
-    cat >> ~/.ssh/known_hosts <<EOF
-    mtrlz.dev ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCsJdMnBVoCu1StAcoSH9iMIdM94V3if6U87gdrucDV/NHeWSTZa3lxHV4mGAFfSBV0oF3y82Sx4gspTjBxAeqwLcm70DrtSzl4cEPiJo4Hgj9U54kuZcIpDP8He/rNYzncrzKoeKzOwtVH5+oaJPcfeZ2CI1BVGj/Xt2A44d8Bixtzc6r+Q9czzYIwTCSxN4+rr7wPnNT3QXF+EvmmIsChXfsjVJpFHQKygVd9+9hk+b/bnqjsMG0O2gHmXSh4FQgbjeJvdTLEqC6SN8VEkaLLosbn79gxjei+U8pHcnossNgJQiaj4N5MDQwzWwGV9dxFBvvxcpdUknIBFlML5Joj
-    mtrlz.dev ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBMl4Vm8V+nUA677qNBKcIjUACY1tZuUa74MVMScGUp+szeuPbkqHpYqSkGnU+ONYVT0nRmXFhIOU2cO6hi2lGs4=
-    mtrlz.dev ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPbUN6RkJgvRv3Y7pmnwth5nribjue21mf8BeD12qr2C
-EOF
+else
+    sqllogictest() {
+        cargo run --release --bin sqllogictest -- "$@"
+    }
 fi
-
-set -x
 
 mkdir -p target
 
-RUST_BACKTRACE=full cargo run --release --bin=sqllogictest -- \
+RUST_BACKTRACE=full sqllogictest \
     sqllogictest/test/ \
     test/*.slt \
     test/cockroach/*.slt \
     "$verbosity" "$@" | tee target/slt.out
-
-if [[ "${BUILDKITE_BRANCH-}" = master && "${BUILDKITE_COMMIT-}" ]]; then
-    # TODO(benesch): handle the new fields in the output, rather than just
-    # ignoring them.
-    template="INSERT INTO slt (commit, unsupported, parse_failure, plan_failure, inference_failure, output_failure, bail, success) VALUES ('$BUILDKITE_COMMIT', \\1, \\2, \\3, \\4, \\5, \\6, \\7);"
-    sql=$(tail -n1 target/slt.out \
-        | sed -E "s/^.*unsupported=([0-9]+) parse-failure=([0-9]+) plan-failure=([0-9]+).*inference-failure=([0-9]+).*output-failure=([0-9]+) bail=([0-9]+) success=([0-9]+).*$/$template/")
-    ssh buildkite@mtrlz.dev psql <<< "$sql"
-fi
