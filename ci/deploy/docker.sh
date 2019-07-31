d46de37d62da6d44ca901a19c538142c232c05ec
Nikhil Benesch <nikhil.benesch@gmail.com>
2019-07-31 11:06:45 -0400

    ci/deploy: learn to deploy official materialized binary

---
+++ ci/deploy/docker.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Copyright 2019 Materialize, Inc. All rights reserved.
+#
+# This file is part of Materialize. Materialize may not be used or
+# distributed without the express permission of Materialize, Inc.
+#
+# docker.sh — deploys official Docker images in CI.
+
+set -euo pipefail
+
+docker pull "materialize/ci-materialized:$MATERIALIZED_IMAGE_ID"
+
+for tag in "unstable-$BUILDKITE_COMMIT" latest; do
+    docker tag "materialize/ci-materialized:$MATERIALIZED_IMAGE_ID" "materialize/materialized:$tag"
+    docker push "$tag"
+done
