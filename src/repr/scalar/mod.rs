cfade4d65f2c5f2b7c4cc60f749f2a62562b4302
Nikhil Benesch <nikhil.benesch@gmail.com>
2019-08-19 11:25:43 -0400

    repr: reorganize into proper modules
    repr/lib.rs was getting a bit large. Split into proper modules.

---
+++ src/repr/scalar/mod.rs
@@ -0,0 +1,313 @@
+// Copyright 2019 Materialize, Inc. All rights reserved.
+//
+// This file is part of Materialize. Materialize may not be used or
+// distributed without the express permission of Materialize, Inc.
+
+pub mod decimal;
+pub mod regex;
+
+use ordered_float::OrderedFloat;
+use serde::{Deserialize, Serialize};
+use std::fmt;
+
+use self::decimal::Significand;
+use self::regex::Regex;
+use crate::ColumnType;
+
+/// A literal value.
+///
+/// Note that datums may be scalar, like [`Datum::Int32`], or composite, like
+/// [`Datum::Tuple`], but they are always constant.
+#[serde(rename_all = "snake_case")]
+#[derive(Clone, Debug, Eq, PartialEq, Hash, Ord, PartialOrd, Serialize, Deserialize)]
+pub enum Datum {
+    /// An unknown value.
+    Null,
+    /// The `true` boolean value.
+    True,
+    /// The `false` boolean value.
+    False,
+    /// A 32-bit signed integer.
+    Int32(i32),
+    /// A 64-bit signed integer.
+    Int64(i64),
+    /// A 32-bit floating point number.
+    Float32(OrderedFloat<f32>),
+    /// A 64-bit floating point number.
+    Float64(OrderedFloat<f64>),
+    /// An exact decimal number, possibly with a fractional component, with up
+    /// to 38 digits of precision.
+    Decimal(Significand),
+    /// A sequence of untyped bytes.
+    Bytes(Vec<u8>),
+    /// A sequence of Unicode codepoints encoded as UTF-8.
+    String(String),
+    /// A compiled regular expression.
+    Regex(Regex),
+}
+
+impl Datum {
+    pub fn is_null(&self) -> bool {
+        match self {
+            Datum::Null => true,
+            _ => false,
+        }
+    }
+
+    pub fn unwrap_bool(&self) -> bool {
+        match self {
+            Datum::False => false,
+            Datum::True => true,
+            _ => panic!("Datum::unwrap_bool called on {:?}", self),
+        }
+    }
+
+    pub fn unwrap_int32(&self) -> i32 {
+        match self {
+            Datum::Int32(i) => *i,
+            _ => panic!("Datum::unwrap_int32 called on {:?}", self),
+        }
+    }
+
+    pub fn unwrap_int64(&self) -> i64 {
+        match self {
+            Datum::Int64(i) => *i,
+            _ => panic!("Datum::unwrap_int64 called on {:?}", self),
+        }
+    }
+
+    pub fn unwrap_ordered_float32(&self) -> OrderedFloat<f32> {
+        match self {
+            Datum::Float32(f) => *f,
+            _ => panic!("Datum::unwrap_ordered_float32 called on {:?}", self),
+        }
+    }
+
+    pub fn unwrap_ordered_float64(&self) -> OrderedFloat<f64> {
+        match self {
+            Datum::Float64(f) => *f,
+            _ => panic!("Datum::unwrap_ordered_float64 called on {:?}", self),
+        }
+    }
+
+    pub fn unwrap_float32(&self) -> f32 {
+        match self {
+            Datum::Float32(f) => f.into_inner(),
+            _ => panic!("Datum::unwrap_float32 called on {:?}", self),
+        }
+    }
+
+    pub fn unwrap_float64(&self) -> f64 {
+        match self {
+            Datum::Float64(f) => f.into_inner(),
+            _ => panic!("Datum::unwrap_float64 called on {:?}", self),
+        }
+    }
+
+    pub fn unwrap_decimal(&self) -> Significand {
+        match self {
+            Datum::Decimal(d) => *d,
+            _ => panic!("Datum::unwrap_decimal called on {:?}", self),
+        }
+    }
+
+    pub fn unwrap_str(&self) -> &str {
+        match self {
+            Datum::String(s) => s,
+            _ => panic!("Datum::unwrap_string called on {:?}", self),
+        }
+    }
+
+    pub fn unwrap_string(self) -> String {
+        match self {
+            Datum::String(s) => s,
+            _ => panic!("Datum::unwrap_string called on {:?}", self),
+        }
+    }
+
+    pub fn unwrap_regex(self) -> Regex {
+        match self {
+            Datum::Regex(r) => r,
+            _ => panic!("Datum::unwrap_regex calloed on {:?}", self),
+        }
+    }
+
+    pub fn is_instance_of(&self, column_type: &ColumnType) -> bool {
+        match (self, &column_type.scalar_type) {
+            (Datum::Null, _) if column_type.nullable => true,
+            (Datum::False, ScalarType::Bool) => true,
+            (Datum::True, ScalarType::Bool) => true,
+            (Datum::Int32(_), ScalarType::Int32) => true,
+            (Datum::Int64(_), ScalarType::Int64) => true,
+            (Datum::Float32(_), ScalarType::Float32) => true,
+            (Datum::Float64(_), ScalarType::Float64) => true,
+            (Datum::Decimal(_), ScalarType::Decimal(_, _)) => true,
+            (Datum::Bytes(_), ScalarType::Bytes) => true,
+            (Datum::String(_), ScalarType::String) => true,
+            (Datum::Regex(_), ScalarType::Regex) => true,
+            _ => false,
+        }
+    }
+}
+
+impl From<bool> for Datum {
+    fn from(b: bool) -> Datum {
+        if b {
+            Datum::True
+        } else {
+            Datum::False
+        }
+    }
+}
+
+impl From<i32> for Datum {
+    fn from(i: i32) -> Datum {
+        Datum::Int32(i)
+    }
+}
+
+impl From<i64> for Datum {
+    fn from(i: i64) -> Datum {
+        Datum::Int64(i)
+    }
+}
+
+impl From<OrderedFloat<f32>> for Datum {
+    fn from(f: OrderedFloat<f32>) -> Datum {
+        Datum::Float32(f)
+    }
+}
+
+impl From<OrderedFloat<f64>> for Datum {
+    fn from(f: OrderedFloat<f64>) -> Datum {
+        Datum::Float64(f)
+    }
+}
+
+impl From<f32> for Datum {
+    fn from(f: f32) -> Datum {
+        Datum::Float32(OrderedFloat(f))
+    }
+}
+
+impl From<f64> for Datum {
+    fn from(f: f64) -> Datum {
+        Datum::Float64(OrderedFloat(f))
+    }
+}
+
+impl From<i128> for Datum {
+    fn from(d: i128) -> Datum {
+        Datum::Decimal(Significand::new(d))
+    }
+}
+
+impl From<Significand> for Datum {
+    fn from(d: Significand) -> Datum {
+        Datum::Decimal(d)
+    }
+}
+
+impl From<String> for Datum {
+    fn from(s: String) -> Datum {
+        Datum::String(s)
+    }
+}
+
+impl From<&str> for Datum {
+    fn from(s: &str) -> Datum {
+        Datum::String(s.to_owned())
+    }
+}
+
+impl From<::regex::Regex> for Datum {
+    fn from(r: ::regex::Regex) -> Datum {
+        Datum::Regex(Regex(r))
+    }
+}
+
+impl From<Vec<u8>> for Datum {
+    fn from(b: Vec<u8>) -> Datum {
+        Datum::Bytes(b)
+    }
+}
+
+impl<T> From<Option<T>> for Datum
+where
+    Datum: From<T>,
+{
+    fn from(o: Option<T>) -> Datum {
+        if let Some(d) = o {
+            d.into()
+        } else {
+            Datum::Null
+        }
+    }
+}
+
+/// The fundamental type of a [`Datum`].
+///
+/// A fundamental type is what is typically thought of as a type, like "Int32"
+/// or "String." The full [`ColumnType`] struct bundles additional information, like
+/// an optional default value and nullability, that must also be considered part
+/// of a datum's type.
+#[serde(rename_all = "snake_case")]
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize, Hash)]
+pub enum ScalarType {
+    /// The type of a datum that can only be null.
+    ///
+    /// This is uncommon. Most [`Datum:Null`]s appear with a different type.
+    Null,
+    Bool,
+    Int32,
+    Int64,
+    Float32,
+    Float64,
+    /// An exact decimal number with a specified precision and scale. The
+    /// precision constrains the total number of digits in the number, while the
+    /// scale specifies the number of digits after the decimal point. The
+    /// maximum precision is [`decimal::MAX_DECIMAL_PRECISION`]. The scale must
+    /// be less than or equal to the precision.
+    Decimal(u8, u8),
+    Date,
+    Time,
+    Timestamp,
+    Bytes,
+    String,
+    Regex,
+}
+
+impl ScalarType {
+    pub fn unwrap_decimal_parts(&self) -> (u8, u8) {
+        match self {
+            ScalarType::Decimal(p, s) => (*p, *s),
+            _ => panic!("ScalarType::unwrap_decimal_parts called on {:?}", self),
+        }
+    }
+}
+
+impl fmt::Display for ScalarType {
+    /// Arbitrary display name for scalars
+    ///
+    /// Right now the names correspond most closely to rust names (e.g. i32
+    /// instead of int4), but we will want to make them more like some other
+    /// system at some point.
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use ScalarType::*;
+        match self {
+            Null => f.write_str("null"),
+            Bool => f.write_str("bool"),
+            Int32 => f.write_str("i32"),
+            Int64 => f.write_str("i64"),
+            Float32 => f.write_str("f32"),
+            Float64 => f.write_str("f64"),
+            Decimal(scale, precision) => write!(f, "decimal({}, {})", scale, precision),
+            Date => f.write_str("date"),
+            Time => f.write_str("time"),
+            Timestamp => f.write_str("timestamp"),
+            Bytes => f.write_str("bytes"),
+            String => f.write_str("string"),
+            Regex => f.write_str("regex"),
+        }
+    }
+}
