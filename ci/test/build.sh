3754e52ba9df1d1463d82dce5ac77c9054a7b863
Brandon W Maister <bwm@materialize.io>
2019-09-04 13:10:26 -0400

    Change the unstable-COMMIT docker image to not strip symbols
    This should allow us to get backtraces if materialized crashes.

--- ci/test/build.sh
+++ ci/test/build.sh
@@ -51,6 +51,8 @@ docker_run "cargo test --no-run && cargo test --no-run --message-format=json > t
 
 ci_collapsed_heading "Preparing Docker context"
 {
+    cp target/release/materialized misc/docker/ci-raw-materialized
+
     # NOTE(benesch): the debug information is large enough that it slows down CI,
     # since we're packaging these binaries up into Docker images and shipping them
     # around. A bit unfortunate, since it'd be nice to have useful backtraces if
@@ -97,12 +99,23 @@ ci_collapsed_heading "Preparing Docker context"
     mv target/release/sqllogictest misc/docker/ci-sqllogictest
 }
 
-for image in materialized testdrive sqllogictest cargo-test; do
+images=(
+    materialized
+    testdrive
+    sqllogictest
+    cargo-test
+)
+
+if [[ "$BUILDKITE_BRANCH" = master ]]; then
+    images+=(raw-materialized)
+fi
+
+for image in "${images[@]}"; do
     ci_collapsed_heading "Building Docker image $image"
     tag=materialize/ci-$image:${BUILDKITE_BUILD_NUMBER}
     docker build \
         --tag "$tag" \
         --pull \
-        misc/docker/ci-$image
+        "misc/docker/ci-$image"
     docker push "$tag"
 done
