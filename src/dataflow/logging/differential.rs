aa79419ca6b9be97bf1c68314f7ed8d89729bf2e
Frank McSherry <fmcsherry@me.com>
2019-09-06 18:22:36 -0400

    surface arrangement sharing

--- src/dataflow/logging/differential.rs
+++ src/dataflow/logging/differential.rs
@@ -67,6 +67,7 @@ pub fn construct<A: Allocate>(
                         Some(((event.operator, worker), time_ms, difference))
                     }
                     DifferentialEvent::MergeShortfall(_) => None,
+                    DifferentialEvent::TraceShare(_) => None,
                 }
             })
             .as_collection()
@@ -81,10 +82,38 @@ pub fn construct<A: Allocate>(
             })
             .arrange_by_self();
 
-        vec![(
-            LogVariant::Differential(DifferentialLog::Arrangement),
-            arrangements.trace,
-        )]
+        // Duration statistics derive from the non-rounded event times.
+        let sharing = logs
+            .flat_map(move |(ts, worker, event)| {
+                let time_ms = ((ts.as_millis() as Timestamp / granularity_ms) + 1) * granularity_ms;
+                if let DifferentialEvent::TraceShare(event) = event {
+                    Some(((event.operator, worker), time_ms, event.diff))
+                }
+                else {
+                    None
+                }
+            })
+            .as_collection()
+            .count()
+            .map(|((op, worker), count)| {
+                vec![
+                    Datum::Int64(op as i64),
+                    Datum::Int64(worker as i64),
+                    Datum::Int64(count as i64),
+                ]
+            })
+            .arrange_by_self();
+
+        vec![
+            (
+                LogVariant::Differential(DifferentialLog::Arrangement),
+                arrangements.trace,
+            ),
+            (
+                LogVariant::Differential(DifferentialLog::Sharing),
+                sharing.trace,
+            )
+        ]
         .into_iter()
         .filter(|(name, _trace)| config.active_logs().contains(name))
         .collect()
