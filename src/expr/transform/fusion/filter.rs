f086cc079d311223b257d943eadef2c2087639e2
JLDLaughlin <jessica@materialize.io>
2019-09-18 17:23:23 -0400

    deduplicate correct answer in filter.rs doc test

--- src/expr/transform/fusion/filter.rs
+++ src/expr/transform/fusion/filter.rs
@@ -29,9 +29,10 @@
 //!     ColumnType::new(ScalarType::Bool),
 //! ]);
 //!
+//! // .transform() will deduplicate any predicates
 //! Filter.transform(&mut expr, &typ);
 //!
-//! let correct = input.filter(vec![predicate0, predicate1, predicate2]);
+//! let correct = input.filter(vec![predicate0]);
 //!
 //! assert_eq!(expr, correct);
 //! ```
