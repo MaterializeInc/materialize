c332165ad32bd3ee9d343c48a15d5697ce4b325b
Nikhil Benesch <nikhil.benesch@gmail.com>
2019-09-24 11:34:09 -0400

    sqllogictest: improve description

--- src/sqllogictest/lib.rs
+++ src/sqllogictest/lib.rs
@@ -3,7 +3,14 @@
 // This file is part of Materialize. Materialize may not be used or
 // distributed without the express permission of Materialize, Inc.
 
-//! https://www.sqlite.org/sqllogictest/doc/trunk/about.wiki
+//! A driver for sqllogictest, a SQL correctness testing framework.
+//!
+//! sqllogictest is developed as part of the SQLite project.
+//! Details can be found on the SQLite website:
+//! <https://www.sqlite.org/sqllogictest/doc/trunk/about.wiki>
+//!
+//! This crate implements a parser and runner for sqllogictest files.
+//! The parser is generic, but the runner is specific to Materialize.
 
 pub mod ast;
 pub mod fuzz;
