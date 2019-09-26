e2b76ff24bd499e64094766dd9a7bd850df7b601
Jamie Brandon <jamie@scattered-thoughts.net>
2019-09-26 23:34:11 +0100

    Unicode spaces?

--- src/sql/query.rs
+++ src/sql/query.rs
@@ -1067,10 +1067,7 @@ impl Planner {
 
                 "ascii" => {
                     if sql_func.args.len() != 1 {
-                        bail!(
-                            "ascii expects one argument, got {}",
-                            sql_func.args.len()
-                        );
+                        bail!("ascii expects one argument, got {}", sql_func.args.len());
                     }
                     let (expr, typ) = self.plan_expr(ctx, &sql_func.args[0])?;
                     if typ.scalar_type != ScalarType::String && typ.scalar_type != ScalarType::Null
