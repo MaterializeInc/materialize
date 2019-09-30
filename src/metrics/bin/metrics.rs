b39a0dfd36ead682e0c2503ca8c5e5fa59e208e6
JLDLaughlin <jessica@materialize.io>
2019-09-30 11:36:01 -0400

    delete lib.rs, add comment

--- src/metrics/bin/metrics.rs
+++ src/metrics/bin/metrics.rs
@@ -2,6 +2,12 @@
 //
 // This file is part of Materialize. Materialize may not be used or
 // distributed without the express permission of Materialize, Inc..
+
+// the prometheus macros (e.g. `register*`) all depend on each other, including on
+// internal `__register*` macros, instead of doing the right thing and I assume using
+// something like `$crate::__register_*`. That means that without using a macro_use here,
+// we would end up needing to import several internal macros everywhere we want to use
+// any of the prometheus macros.
 #[macro_use]
 extern crate prometheus;
 
