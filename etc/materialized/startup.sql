8ab776d123ae031c84c96c1d890da55749130ddb
Robert Grimm <grimmr@materialize.io>
2019-10-08 16:40:00 -0400

    Add missing copyright, yo!

--- etc/materialized/startup.sql
+++ etc/materialized/startup.sql
@@ -1,3 +1,8 @@
+-- Copyright 2019 Materialize, Inc. All rights reserved.
+--
+-- This file is part of Materialize. Materialize may not be used or
+-- distributed without the express permission of Materialize, Inc.
+
 CREATE VIEW logs_records_per_dataflow AS
 SELECT address_value AS dataflow, SUM(records) AS records
 FROM logs_operates AS lo
