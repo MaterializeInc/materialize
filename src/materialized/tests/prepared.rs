37c4c51813fa9f9cd68408b482c61e840ba66bd3
JLDLaughlin <jessica@materialize.io>
2019-11-11 13:25:55 -0500

    connect to random port to test

--- src/materialized/tests/prepared.rs
+++ src/materialized/tests/prepared.rs
@@ -6,24 +6,31 @@
 use std::error::Error;
 
 use postgres::{Connection, TlsMode};
+use rand;
+use rand::Rng;
 
 #[test]
 fn test_prepared_statements() -> Result<(), Box<dyn Error>> {
     ore::log::init();
     ore::panic::set_abort_on_panic();
 
+    // connect to a random, ephemeral port
+    let rand_port = rand::thread_rng().gen_range(20_000, 40_000).to_string();
     let _server = materialized::serve(materialized::Config {
         logging_granularity: None,
         version: "TEST".into(),
         threads: 1,
         process: 0,
-        addresses: vec!["127.0.0.1:6875".into()],
+        addresses: vec![format!("127.0.0.1:{}", rand_port).into()],
         sql: "".into(),
         symbiosis_url: None,
         gather_metrics: false,
     })?;
 
-    let conn = Connection::connect("postgresql://root@127.0.0.1:6875", TlsMode::None)?;
+    let conn = Connection::connect(
+        format!("postgresql://root@127.0.0.1:{}", rand_port),
+        TlsMode::None,
+    )?;
 
     for row in &conn.query("SELECT $1", &[&String::from("42")])? {
         let val: String = row.get(0);
