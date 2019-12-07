db20a38f08be85b967e0b25ea7320b69eefd2fef
Brandon W Maister <bwm@materialize.io>
2019-12-07 17:12:26 -0500

    Change bootstrap views from logs_ -> mz_
    I also renmaed `unit_length_addresses` -> `addresses_with_unit_length` to match the
    new hierarchy.

--- misc/dist/etc/materialized/bootstrap.sql
+++ misc/dist/etc/materialized/bootstrap.sql
@@ -5,7 +5,7 @@
 
 -- Stores all addresses that only have one slot (0) in mz_dataflow_operator_addresses
 -- The resulting addresses are either channels or dataflows
-CREATE VIEW logs_unit_length_addresses as
+CREATE VIEW mz_addresses_with_unit_length as
 SELECT
     mz_dataflow_operator_addresses.id,
     mz_dataflow_operator_addresses.worker
@@ -16,8 +16,9 @@ GROUP BY
     mz_dataflow_operator_addresses.worker
 HAVING count(*) = 1;
 
--- Maintains a list of the current dataflow operator ids, and their corresponding operator names and local ids (per worker)
-CREATE VIEW logs_dataflow_names AS
+-- Maintains a list of the current dataflow operator ids, and their corresponding
+-- operator names and local ids (per worker)
+CREATE VIEW mz_dataflow_names AS
 SELECT
     mz_dataflow_operator_addresses.id,
     mz_dataflow_operator_addresses.worker,
@@ -26,76 +27,79 @@ SELECT
 FROM
     mz_dataflow_operator_addresses,
     mz_dataflow_operators,
-    logs_unit_length_addresses
+    mz_addresses_with_unit_length
 WHERE
     mz_dataflow_operator_addresses.id = mz_dataflow_operators.id AND
     mz_dataflow_operator_addresses.worker = mz_dataflow_operators.worker AND
-    mz_dataflow_operator_addresses.id = logs_unit_length_addresses.id AND
-    mz_dataflow_operator_addresses.worker = logs_unit_length_addresses.worker AND
+    mz_dataflow_operator_addresses.id = mz_addresses_with_unit_length.id AND
+    mz_dataflow_operator_addresses.worker = mz_addresses_with_unit_length.worker AND
     mz_dataflow_operator_addresses.slot = 0;
 
--- Maintains a list of all operators bound to a dataflow and their corresponding names and dataflow names and ids (per worker)
--- Keeping this as a separate view instead of rolling it into logs_records_per_dataflow_operator to simplify logic
-CREATE VIEW logs_dataflow_operators AS
+-- Maintains a list of all operators bound to a dataflow and their corresponding names
+-- and dataflow names and ids (per worker)
+--
+-- Keeping this as a separate view instead of rolling it into
+-- mz_records_per_dataflow_operator to simplify logic
+CREATE VIEW mz_dataflow_operators AS
 SELECT
     mz_dataflow_operators.id,
     mz_dataflow_operators.name,
     mz_dataflow_operators.worker,
-    logs_dataflow_names.id as dataflow_id,
-    logs_dataflow_names.name as dataflow_name
+    mz_dataflow_names.id as dataflow_id,
+    mz_dataflow_names.name as dataflow_name
 FROM
     mz_dataflow_operators,
     mz_dataflow_operator_addresses,
-    logs_dataflow_names
+    mz_dataflow_names
 WHERE
     mz_dataflow_operators.id = mz_dataflow_operator_addresses.id AND
     mz_dataflow_operators.worker = mz_dataflow_operator_addresses.worker AND
     mz_dataflow_operator_addresses.slot = 0 AND
-    logs_dataflow_names.local_id = mz_dataflow_operator_addresses.value AND
-    logs_dataflow_names.worker = mz_dataflow_operator_addresses.worker;
+    mz_dataflow_names.local_id = mz_dataflow_operator_addresses.value AND
+    mz_dataflow_names.worker = mz_dataflow_operator_addresses.worker;
 
 -- Maintains the number of records used by each operator in a dataflow (per worker)
 -- Operators not using any records are not shown
-CREATE VIEW logs_records_per_dataflow_operator AS
+CREATE VIEW mz_records_per_dataflow_operator AS
 SELECT
-    logs_dataflow_operators.id,
-    logs_dataflow_operators.name,
-    logs_dataflow_operators.worker,
-    logs_dataflow_operators.dataflow_id,
+    mz_dataflow_operators.id,
+    mz_dataflow_operators.name,
+    mz_dataflow_operators.worker,
+    mz_dataflow_operators.dataflow_id,
     mz_arrangement_sizes.records
 FROM
     mz_arrangement_sizes,
-    logs_dataflow_operators
+    mz_dataflow_operators
 WHERE
-    logs_dataflow_operators.id = mz_arrangement_sizes.operator AND
-    logs_dataflow_operators.worker = mz_arrangement_sizes.worker;
+    mz_dataflow_operators.id = mz_arrangement_sizes.operator AND
+    mz_dataflow_operators.worker = mz_arrangement_sizes.worker;
 
 -- Maintains the number of records used by each dataflow (per worker)
-CREATE VIEW logs_records_per_dataflow AS
+CREATE VIEW mz_records_per_dataflow AS
 SELECT
-    logs_records_per_dataflow_operator.dataflow_id as id,
-    logs_dataflow_names.name,
-    logs_records_per_dataflow_operator.worker,
-    SUM(logs_records_per_dataflow_operator.records) as records
+    mz_records_per_dataflow_operator.dataflow_id as id,
+    mz_dataflow_names.name,
+    mz_records_per_dataflow_operator.worker,
+    SUM(mz_records_per_dataflow_operator.records) as records
 FROM
-    logs_records_per_dataflow_operator,
-    logs_dataflow_names
+    mz_records_per_dataflow_operator,
+    mz_dataflow_names
 WHERE
-    logs_records_per_dataflow_operator.dataflow_id = logs_dataflow_names.id AND
-    logs_records_per_dataflow_operator.worker = logs_dataflow_names.worker
+    mz_records_per_dataflow_operator.dataflow_id = mz_dataflow_names.id AND
+    mz_records_per_dataflow_operator.worker = mz_dataflow_names.worker
 GROUP BY
-    logs_records_per_dataflow_operator.dataflow_id,
-    logs_dataflow_names.name,
-    logs_records_per_dataflow_operator.worker;
+    mz_records_per_dataflow_operator.dataflow_id,
+    mz_dataflow_names.name,
+    mz_records_per_dataflow_operator.worker;
 
 -- Maintains the number of records used by each dataflow (across all workers)
-CREATE VIEW logs_records_per_dataflow_global AS
+CREATE VIEW mz_records_per_dataflow_global AS
 SELECT
-    logs_records_per_dataflow.id,
-    logs_records_per_dataflow.name,
-    SUM(logs_records_per_dataflow.records) as records
+    mz_records_per_dataflow.id,
+    mz_records_per_dataflow.name,
+    SUM(mz_records_per_dataflow.records) as records
 FROM
-    logs_records_per_dataflow
+    mz_records_per_dataflow
 GROUP BY
-    logs_records_per_dataflow.id,
-    logs_records_per_dataflow.name;
+    mz_records_per_dataflow.id,
+    mz_records_per_dataflow.name;
