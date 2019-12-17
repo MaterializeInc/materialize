7d9a0639bdcee6e270d4796186dda06d9eb0aeb5
Brandon W Maister <bwm@materialize.io>
2019-12-17 18:04:58 -0500

    Move setup-metric-tables.sh commands into bootstrap.sql
    This allows us to get rid of another docker container, and should be more reliable in
    general.

--- misc/dist/etc/materialized/bootstrap.sql
+++ misc/dist/etc/materialized/bootstrap.sql
@@ -103,3 +103,55 @@ FROM
 GROUP BY
     mz_records_per_dataflow.id,
     mz_records_per_dataflow.name;
+
+--- Performance-rlated tables, used by prometheus
+
+--
+CREATE VIEW mz_perf_dependency_frontiers AS
+SELECT DISTINCT
+    coalesce(mcn.name, ldd.dataflow) as dataflow,
+    coalesce(mcn_source.name, lf_source.name) as source,
+    lf_source.time - lf_df.time as lag_ms
+FROM
+    mz_view_dependencies ldd
+JOIN mz_view_frontiers lf_source ON ldd.source = lf_source.name
+JOIN mz_view_frontiers lf_df ON ldd.dataflow = lf_df.name
+LEFT JOIN mz_catalog_names mcn ON mcn.global_id = ldd.dataflow
+LEFT JOIN mz_catalog_names mcn_source ON mcn_source.global_id = lf_source.name;
+
+-- operator operator is due to issue #1217
+CREATE VIEW mz_perf_arrangement_records AS
+ SELECT mas.worker, name, records, operator operator
+ FROM mz_arrangement_sizes mas
+ JOIN mz_dataflow_operators mdo ON mdo.id = mas.operator;
+
+-- There are three steps required for a prometheus histogram from the mz_peek_durations
+-- logs:
+--
+--   1. Create some values that all represent everything in them and below (_core)
+--   2. Find the max value and alias that with +Inf (_bucket)
+--   3. calculate a couple aggregates (_aggregates)
+CREATE VIEW mz_perf_peek_durations_core AS
+SELECT
+    d_upper.worker,
+    CAST(d_upper.duration_ns AS TEXT) AS le,
+    sum(d_summed.count) AS count
+FROM
+    mz_peek_durations AS d_upper,
+    mz_peek_durations AS d_summed
+WHERE
+    d_upper.worker = d_summed.worker AND
+    d_upper.duration_ns >= d_summed.duration_ns
+GROUP BY d_upper.worker, d_upper.duration_ns;
+
+CREATE VIEW mz_perf_peek_durations_bucket AS (
+    SELECT * FROM mz_perf_peek_durations_core
+) UNION (
+    SELECT worker, '+Inf', max(count) AS count FROM mz_perf_peek_durations_core
+    GROUP BY worker
+);
+
+CREATE VIEW mz_perf_peek_durations_aggregates AS
+SELECT worker, sum(duration_ns * count) AS sum, sum(count) AS count
+FROM mz_peek_durations lpd
+GROUP BY worker;
