798849b7bb7d42253739078d12a18a88a8e4eb9d
Nikhil Benesch <nikhil.benesch@gmail.com>
2019-12-17 18:01:26 -0500

    materialized/tests: add test for [tailing] file sources

---
+++ src/materialized/tests/source.rs
@@ -0,0 +1,107 @@
+// Copyright 2019 Materialize, Inc. All rights reserved.
+//
+// This file is part of Materialize. Materialize may not be used or
+// distributed without the express permission of Materialize, Inc.
+
+use std::error::Error;
+use std::fs;
+use std::io::Write;
+use std::path::Path;
+use std::process::{Command, Stdio};
+use std::thread;
+use std::time::Duration;
+
+pub mod util;
+
+#[test]
+fn test_file_sources() -> Result<(), Box<dyn Error>> {
+    ore::log::init();
+
+    let temp_dir = tempfile::tempdir()?;
+    let config = util::Config::default();
+
+    let (_server, mut conn) = util::start_server(config.clone())?;
+
+    let fetch_rows = |conn: &mut postgres::Client, source| -> Result<_, Box<dyn Error>> {
+        // TODO(benesch): use a blocking SELECT when that exists.
+        thread::sleep(Duration::from_secs(1));
+        Ok(conn
+            .query(&*format!("SELECT * FROM {} ORDER BY 1", source), &[])?
+            .into_iter()
+            .map(|row| (row.get(0), row.get(1), row.get(2)))
+            .collect::<Vec<(String, String, String)>>())
+    };
+
+    // macOS doesn't send filesystem events to the process that caused them, so
+    // this function appends data to a file via a `tee` child process so that
+    // the dataflow layer actually gets notified of the changes.
+    let append = |path, data| -> Result<_, Box<dyn Error>> {
+        let mut child = Command::new("tee")
+            .arg("-a")
+            .arg(path)
+            .stdin(Stdio::piped())
+            .stdout(Stdio::null())
+            .spawn()?;
+        child.stdin.as_mut().unwrap().write_all(data)?;
+        child.wait()?;
+        Ok(())
+    };
+
+    let static_path = Path::join(temp_dir.path(), "static.csv");
+    let dynamic_path = Path::join(temp_dir.path(), "dynamic.csv");
+
+    fs::write(
+        &static_path,
+        "Rochester,NY,14618
+New York,NY,10004
+\"Rancho Santa Margarita\",CA,92679
+",
+    )?;
+
+    let line1 = ("New York".into(), "NY".into(), "10004".into());
+    let line2 = ("Rancho Santa Margarita".into(), "CA".into(), "92679".into());
+    let line3 = ("Rochester".into(), "NY".into(), "14618".into());
+
+    conn.execute(
+        &*format!(
+            "CREATE SOURCE static_csv FROM 'file://{}' WITH (format = 'csv', columns = 3)",
+            static_path.display(),
+        ),
+        &[],
+    )?;
+
+    assert_eq!(
+        fetch_rows(&mut conn, "static_csv")?,
+        &[line1.clone(), line2.clone(), line3.clone()],
+    );
+
+    append(&dynamic_path, b"")?;
+
+    conn.execute(&*format!(
+        "CREATE SOURCE dynamic_csv FROM 'file://{}' WITH (format = 'csv', columns = 3, tail = true)",
+        dynamic_path.display()
+    ), &[])?;
+
+    append(&dynamic_path, b"New York,NY,10004\n")?;
+
+    // There is a lag of one line, so we don't expect to see anything yet.
+    assert_eq!(fetch_rows(&mut conn, "dynamic_csv")?, &[]);
+
+    append(&dynamic_path, b"Rochester,NY,14618\n")?;
+    assert_eq!(fetch_rows(&mut conn, "dynamic_csv")?, &[line1.clone()]);
+
+    append(&dynamic_path, b"\"Rancho Santa Margarita\",CA,92679\n")?;
+    assert_eq!(
+        fetch_rows(&mut conn, "dynamic_csv")?,
+        &[line1.clone(), line3.clone()]
+    );
+
+    // A blank line will flush all existing data.
+    append(&dynamic_path, b"\n")?;
+    assert_eq!(
+        fetch_rows(&mut conn, "dynamic_csv")?,
+        &[line1.clone(), line2.clone(), line3.clone()]
+    );
+
+    Ok(())
+}
