74502915b835aa852a5d71f02190257c226d49ff
Ruchir Khaitan <ruchir0001@gmail.com>
2019-12-19 15:50:40 -0500

    flag proto generation with an envvar

--- src/interchange/build.rs
+++ src/interchange/build.rs
@@ -1,8 +1,18 @@
 extern crate protoc_rust;
 
+use std::env;
+
 use protoc_rust::Customize;
 
 fn main() {
+    let generate_protos = env::var("MATERIALIZE_GENERATE_PROTO")
+        .map(|v| v == "1")
+        .unwrap_or(false);
+
+    if !generate_protos {
+        return;
+    }
+
     protoc_rust::run(protoc_rust::Args {
         out_dir: "protobuf/test/",
         input: &["testdata/test-proto-schemas.proto"],
