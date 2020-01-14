c35e52d3ea4921e961a0e6bbafb93bc253bfe0ce
Brennan Vincent <brennan@materialize.io>
2020-01-14 13:50:38 -0500

    copyright header

--- src/coord/tests/noblock.rs
+++ src/coord/tests/noblock.rs
@@ -1,3 +1,8 @@
+// Copyright 2020 Materialize, Inc. All rights reserved.
+//
+// This file is part of Materialize. Materialize may not be used or
+// distributed without the express permission of Materialize, Inc.
+//
 use coord::{Command, ExecuteResponse, Response};
 use dataflow_types::PeekResponse;
 use futures::channel::oneshot;
