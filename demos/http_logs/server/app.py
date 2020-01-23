afd24bc83724f0ab6bce191d1024c20ede3bec61
Brennan Vincent <brennan@materialize.io>
2020-01-23 12:40:12 -0500

    Basic demo of logs from an HTTP server (#1644)

---
+++ demos/http_logs/server/app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# Copyright 2020 Materialize, Inc. All rights reserved.
+#
+# This file is part of Materialize. Materialize may not be used or
+# distributed without the express permission of Materialize, Inc.
+
+from flask import Flask
+
+from werkzeug.middleware.proxy_fix import ProxyFix
+app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app, num_proxies=1)
+
+
+@app.route('/')
+def gateway():
+    return 'Gateway page!'
+
+@app.route('/detail/<id>')
+def detail(id):
+    return 'Product detail page for {}'.format(id)
+
+@app.route('/search/')
+def search():
+    return 'fixme'
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
