00e3bbf9fb9004556704e2fbefe81b9fd233498d
JLDLaughlin <jessica@materialize.io>
2020-01-28 17:59:12 -0500

    Download metabase-materialize-driver and mount it

--- demo/chbench/dc.sh
+++ demo/chbench/dc.sh
@@ -126,6 +126,9 @@ bring_up() {
     bring_up_source_data
     echo "materialize and ingstion should be running fine, bringing up introspection and metabase"
     bring_up_introspection
+
+    rm metabase/*
+    curl -L "https://github.com/MaterializeInc/metabase-materialize-driver/releases/download/0.0.1/materialize-driver-0.0.1-SNAPSHOT-standalone.jar" -o metabase/materialize-driver-0.0.1-SNAPSHOT-standalone.jar
     dc_up metabase
 }
 
