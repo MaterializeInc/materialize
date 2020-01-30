5c27ba2ade2c544629b88c6c568d9db8c65aad47
Brennan Vincent <brennan@materialize.io>
2020-01-30 15:51:17 -0500

    adjust http_logs demo setup script to new view creating syntax (#1771)

--- demo/http_logs/cli/setup.sh
+++ demo/http_logs/cli/setup.sh
@@ -11,14 +11,14 @@ CREATE SOURCE requests FROM 'file:///log/requests' WITH ( regex='(?P<ip>\d{1,3}\
 
 -- Average number of product detail pages viewed per IP that has viewed the
 -- search page at least once
-CREATE VIEW avg_dps_for_searcher AS SELECT avg(dp_hits) FROM (SELECT ip, count(product_detail_id) AS dp_hits, count(search_kw) AS search_hits FROM requests GROUP BY ip) WHERE search_hits > 0;
+CREATE MATERIALIZED VIEW avg_dps_for_searcher AS SELECT avg(dp_hits) FROM (SELECT ip, count(product_detail_id) AS dp_hits, count(search_kw) AS search_hits FROM requests GROUP BY ip) WHERE search_hits > 0;
 
 -- Number of unique IP hits
 -- This could just be SELECT count(distinct(ip)) FROM requests
 -- once https://github.com/MaterializeInc/materialize/issues/676
 -- is addressed.
-CREATE VIEW unique_visitors AS SELECT count(*) FROM (SELECT ip FROM requests GROUP BY ip);
+CREATE MATERIALIZED VIEW unique_visitors AS SELECT count(*) FROM (SELECT ip FROM requests GROUP BY ip);
 
 -- 40 products with the most hits
-CREATE VIEW top_products AS SELECT count(product_detail_id) ct, product_detail_id from requests GROUP BY product_detail_id ORDER BY ct DESC LIMIT 40;
+CREATE MATERIALIZED VIEW top_products AS SELECT count(product_detail_id) ct, product_detail_id from requests GROUP BY product_detail_id ORDER BY ct DESC LIMIT 40;
 EOF
