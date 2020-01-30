35e70f89c66260a9144ea0bf1313821ecb58c944
Brennan Vincent <brennan@materialize.io>
2020-01-30 15:46:37 -0500

    fix psql invocation (#1770)

--- demo/http_logs/cli/setup.sh
+++ demo/http_logs/cli/setup.sh
@@ -5,7 +5,7 @@
 # This file is part of Materialize. Materialize may not be used or
 # distributed without the express permission of Materialize, Inc.
 
-psql -h materialized -p 6875 sslmode=disable << EOF
+psql -h materialized -p 6875 -d materialize << EOF
 -- Flask request logs format
 CREATE SOURCE requests FROM 'file:///log/requests' WITH ( regex='(?P<ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - - \[(?P<ts>[^]]+)\] "(?P<path>(?:GET /search/\?kw=(?P<search_kw>[^ ]*) HTTP/\d\.\d)|(?:GET /detail/(?P<product_detail_id>[a-zA-Z0-9]+) HTTP/\d\.\d)|(?:[^"]+))" (?P<code>\d{3}) -', tail=true);
 
