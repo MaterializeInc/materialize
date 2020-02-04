d83a23708da48cb6e9c85c9c7d0cd1d9a87ee75d
JLDLaughlin <jessica@materialize.io>
2020-02-04 16:01:50 -0500

    Update driver version

--- demo/chbench/dc.sh
+++ demo/chbench/dc.sh
@@ -155,7 +155,7 @@ bring_up_introspection() {
 bring_up_metabase() {
     if [ ! -f metabase/materialize-driver-0.0.1-SNAPSHOT-standalone.jar ]; then
         echo "Materialize Metabase driver not found, downloading..."
-        curl -L "https://github.com/MaterializeInc/metabase-materialize-driver/releases/download/0.0.1/materialize-driver-0.0.1-SNAPSHOT-standalone.jar" \
+        curl -L "https://github.com/MaterializeInc/metabase-materialize-driver/releases/download/0.0.2/materialize-driver-0.0.2-SNAPSHOT-standalone.jar" \
           -o metabase/materialize-driver-0.0.1-SNAPSHOT-standalone.jar
     fi
 
