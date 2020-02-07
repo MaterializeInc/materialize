841a2d2a161393e3e15cbaa480a39ee9414f6e21
Nikhil Benesch <nikhil.benesch@gmail.com>
2020-02-07 02:08:19 -0500

    sql: fix normalization of idents

--- src/sql-parser/src/parser.rs
+++ src/sql-parser/src/parser.rs
@@ -1661,12 +1661,17 @@ impl Parser {
         self.expect_keyword("ON")?;
         let on_name = self.parse_object_name()?;
         self.expect_token(&Token::LParen)?;
-        let key_parts = self
-            .parse_comma_separated(Parser::parse_order_by_expr)?
-            .into_iter()
-            .map(|x| x.expr)
-            .collect::<Vec<_>>();
-        self.expect_token(&Token::RParen)?;
+        let key_parts = if self.consume_token(&Token::RParen) {
+            vec![]
+        } else {
+            let key_parts = self
+                .parse_comma_separated(Parser::parse_order_by_expr)?
+                .into_iter()
+                .map(|x| x.expr)
+                .collect::<Vec<_>>();
+            self.expect_token(&Token::RParen)?;
+            key_parts
+        };
         Ok(Statement::CreateIndex {
             name,
             on_name,
