a354a709864cbd6c15107904ef1345c70fd0ebee
Brennan Vincent <brennan@materialize.io>
2020-02-10 17:12:44 -0500

    add semicolon

--- demo/http_logs/setup.sh
+++ demo/http_logs/setup.sh
@@ -13,7 +13,7 @@ psql -h materialized -p 6875 -d materialize << EOF
 -- Flask request logs format
 CREATE SOURCE requests
 FROM FILE '/log/requests' WITH (tail = true)
-FORMAT REGEX '(?P<ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - - \[(?P<ts>[^]]+)\] "(?P<path>(?:GET /search/\?kw=(?P<search_kw>[^ ]*) HTTP/\d\.\d)|(?:GET /detail/(?P<product_detail_id>[a-zA-Z0-9]+) HTTP/\d\.\d)|(?:[^"]+))" (?P<code>\d{3}) -'
+FORMAT REGEX '(?P<ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - - \[(?P<ts>[^]]+)\] "(?P<path>(?:GET /search/\?kw=(?P<search_kw>[^ ]*) HTTP/\d\.\d)|(?:GET /detail/(?P<product_detail_id>[a-zA-Z0-9]+) HTTP/\d\.\d)|(?:[^"]+))" (?P<code>\d{3}) -';
 
 -- Average number of product detail pages viewed per IP that has viewed the
 -- search page at least once
