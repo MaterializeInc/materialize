5526ee44f68ea94c04ebfcc06c3d86c5c00d08dd
Nikhil Benesch <nikhil.benesch@gmail.com>
2020-03-16 21:25:14 -0400

    testdrive: split kafka actions into modules
    The main Kafka module was getting rather unwieldy.

---
+++ src/testdrive/action/kafka/add_partitions.rs
@@ -0,0 +1,120 @@
+// Copyright Materialize, Inc. All rights reserved.
+//
+// Use of this software is governed by the Business Source License
+// included in the LICENSE file.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0.
+
+use std::thread;
+use std::time::Duration;
+
+use futures::executor::block_on;
+use futures::future::TryFutureExt;
+use rdkafka::admin::NewPartitions;
+
+use ore::cast::CastFrom;
+use ore::collections::CollectionExt;
+
+use crate::action::{Action, State};
+use crate::parser::BuiltinCommand;
+
+pub struct AddPartitionsAction {
+    topic_prefix: String,
+    partitions: i32,
+}
+
+pub fn build_add_partitions(mut cmd: BuiltinCommand) -> Result<AddPartitionsAction, String> {
+    let topic_prefix = format!("testdrive-{}", cmd.args.string("topic")?);
+    let partitions = cmd.args.opt_parse("total-partitions")?.unwrap_or(1);
+    cmd.args.done()?;
+
+    Ok(AddPartitionsAction {
+        topic_prefix,
+        partitions,
+    })
+}
+
+impl Action for AddPartitionsAction {
+    fn undo(&self, _: &mut State) -> Result<(), String> {
+        Ok(())
+    }
+
+    fn redo(&self, state: &mut State) -> Result<(), String> {
+        let topic_name = format!("{}-{}", self.topic_prefix, state.seed);
+        println!(
+            "Raising partition count of Kafka topic {} to {}",
+            topic_name, self.partitions
+        );
+
+        match state.kafka_topics.get(&topic_name) {
+            Some(partitions) => {
+                if self.partitions <= *partitions {
+                    return Err(format!(
+                        "new partition count {} is not greater than current partition count {}",
+                        self.partitions, partitions
+                    ));
+                }
+            }
+            None => {
+                return Err(format!(
+                    "topic {} not created by kafka-create-topic",
+                    topic_name
+                ))
+            }
+        }
+
+        let partitions = NewPartitions::new(&topic_name, usize::cast_from(self.partitions));
+        let res = block_on(
+            state
+                .kafka_admin
+                .create_partitions(&[partitions], &state.kafka_admin_opts)
+                .map_err(|e| e.to_string()),
+        )?;
+        if res.len() != 1 {
+            return Err(format!(
+                "kafka partition addition returned {} results, but exactly one result was expected",
+                res.len()
+            ));
+        }
+        if let Err((_topic_name, e)) = res.into_element() {
+            return Err(e.to_string());
+        }
+
+        let mut i = 0;
+        loop {
+            let res = (|| {
+                let metadata = state
+                    .kafka_producer
+                    .client()
+                    .fetch_metadata(Some(&topic_name), Some(Duration::from_secs(1)))
+                    .map_err(|e| e.to_string())?;
+                if metadata.topics().len() != 1 {
+                    return Err("metadata fetch returned no topics".to_string());
+                }
+                let topic = metadata.topics().into_element();
+                if topic.partitions().len() as i32 != self.partitions {
+                    return Err(format!(
+                        "topic {} has {} partitions when exactly {} was expected",
+                        topic_name,
+                        topic.partitions().len(),
+                        self.partitions,
+                    ));
+                }
+                Ok(())
+            })();
+            match res {
+                Ok(()) => break,
+                Err(e) if i == 6 => return Err(e),
+                _ => {
+                    thread::sleep(Duration::from_millis(100 * 2_u64.pow(i)));
+                    i += 1;
+                }
+            }
+        }
+
+        state.kafka_topics.insert(topic_name, self.partitions);
+        Ok(())
+    }
+}
---
+++ src/testdrive/action/kafka/verify.rs
@@ -0,0 +1,153 @@
+// Copyright Materialize, Inc. All rights reserved.
+//
+// Use of this software is governed by the Business Source License
+// included in the LICENSE file.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0.
+
+use avro::types::Value as AvroValue;
+use byteorder::{BigEndian, ByteOrder};
+use futures::executor::block_on;
+use futures::stream::StreamExt;
+use rdkafka::config::ClientConfig;
+use rdkafka::consumer::{Consumer, StreamConsumer};
+use rdkafka::message::Message;
+
+use crate::action::{Action, State};
+use crate::parser::BuiltinCommand;
+
+pub struct VerifyAction {
+    topic_prefix: String,
+    schema: String,
+    expected_messages: Vec<String>,
+}
+
+pub fn build_verify(mut cmd: BuiltinCommand) -> Result<VerifyAction, String> {
+    let _format = cmd.args.string("format")?;
+    let topic_prefix = cmd.args.string("topic")?;
+    let schema = cmd.args.string("schema")?;
+    let expected_messages = cmd.input;
+
+    cmd.args.done()?;
+
+    Ok(VerifyAction {
+        topic_prefix,
+        schema,
+        expected_messages,
+    })
+}
+
+impl Action for VerifyAction {
+    fn undo(&self, _state: &mut State) -> Result<(), String> {
+        Ok(())
+    }
+
+    fn redo(&self, state: &mut State) -> Result<(), String> {
+        let mut config = ClientConfig::new();
+        config.set("bootstrap.servers", &state.kafka_addr);
+        config.set("auto.offset.reset", "earliest");
+        config.set("group.id", "materialize-testdrive");
+
+        let schema = interchange::avro::parse_schema(&self.schema)
+            .map_err(|e| format!("parsing avro schema: {}", e))?;
+        let mut converted_expected_messages = Vec::new();
+        for expected in &self.expected_messages {
+            converted_expected_messages.push(
+                crate::avro::json_to_avro(
+                    &serde_json::from_str(expected)
+                        .map_err(|e| format!("parsing avro datum: {}", e.to_string()))?,
+                    &schema,
+                )
+                .unwrap(),
+            );
+        }
+        let consumer: StreamConsumer = config
+            .create()
+            .map_err(|e| format!("creating kafka consumer: {}", e))?;
+        consumer
+            .subscribe(&[&self.topic_prefix])
+            .map_err(|e| e.to_string())?;
+        let mut message_stream = consumer.start();
+        let mut actual_messages = Vec::new();
+        for _i in 0..converted_expected_messages.len() {
+            let output = block_on(message_stream.next());
+            match output {
+                Some(result) => match result {
+                    Ok(m) => match m.payload() {
+                        Some(mut bytes) => {
+                            if bytes.len() < 5 {
+                                return Err(format!(
+                                        "avro datum is too few bytes: expected at least 5 bytes, got {}",
+                                        bytes.len()
+                                    ));
+                            }
+                            let magic = bytes[0];
+                            let _schema_id = BigEndian::read_i32(&bytes[1..5]);
+                            bytes = &bytes[5..];
+
+                            if magic != 0 {
+                                return Err(format!(
+                                    "wrong avro serialization magic: expected 0, got {}",
+                                    bytes[0]
+                                ));
+                            }
+                            actual_messages.push(
+                                block_on(avro::from_avro_datum(&schema, &mut bytes, None))
+                                    .map_err(|e| format!("from_avro_datum: {}", e.to_string()))?,
+                            );
+                        }
+                        None => {
+                            return Err(String::from("No bytes found in Kafka message payload."))
+                        }
+                    },
+                    Err(e) => return Err(e.to_string()),
+                },
+                None => {
+                    return Err(format!(
+                        "No Kafka messages found for topic {}",
+                        &self.topic_prefix
+                    ))
+                }
+            }
+        }
+        // NB: We can't compare messages as they come in because
+        // Kafka sinks do not currently support ordering.
+        // Additionally, we do this bummer of a comparison because
+        // avro::types::Value does not implement Eq or Ord.
+        // TODO@jldlaughlin: update this once we have Kafka ordering guarantees
+        let missing_values =
+            get_values_in_first_list_not_in_second(&converted_expected_messages, &actual_messages);
+        let additional_values =
+            get_values_in_first_list_not_in_second(&actual_messages, &converted_expected_messages);
+
+        if !missing_values.is_empty() || !additional_values.is_empty() {
+            return Err(format!(
+                "Mismatched Kafka sink rows. Missing: {:#?}, Unexpected: {:#?}",
+                missing_values, additional_values
+            ));
+        }
+
+        Ok(())
+    }
+}
+
+fn get_values_in_first_list_not_in_second(
+    first_list: &[AvroValue],
+    second_list: &[AvroValue],
+) -> Vec<AvroValue> {
+    let mut first_list_clone: Vec<AvroValue> = first_list.to_vec();
+    let mut missing_values = Vec::new();
+    for s in second_list {
+        let pos = first_list_clone.iter().position(|x| *x == *s);
+        match pos {
+            Some(index) => {
+                first_list_clone.remove(index);
+                continue;
+            }
+            None => missing_values.push(s.clone()),
+        }
+    }
+    missing_values
+}
