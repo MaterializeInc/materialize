6ed88355c39908ffee9dafa5ea612b72c95345f9
JLDLaughlin <jessica@materialize.io>
2020-04-03 14:52:07 -0400

    remove bail

--- src/dataflow/source/kinesis.rs
+++ src/dataflow/source/kinesis.rs
@@ -73,7 +73,8 @@ where
             let (client, shard_iterator) = match &mut state {
                 Ok(state) => state,
                 Err(e) => {
-                    bail!("failed to create Kinesis state: {}", e);
+                    error!("failed to create Kinesis state: {}", e);
+                    println!("failed to create Kinesis state: {}", e);
                     return SourceStatus::Done;
                 }
             };
