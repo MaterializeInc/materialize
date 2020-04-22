d2630c49e28843d047ba3076738a9185707e6bf2
Nikhil Benesch <nikhil.benesch@gmail.com>
2020-04-22 19:25:56 -0400

    ci-builder: adjust env configuration for uploading within container

--- ci/test/mkpipeline.py
+++ ci/test/mkpipeline.py
@@ -7,6 +7,18 @@
 # the Business Source License, use of this software will be governed
 # by the Apache License, Version 2.0.
 
+"""Generator for the test CI pipeline.
+
+This script takes pipeline.template.yml as input, possibly trims out jobs
+whose inputs have not changed relative to the code on master, and uploads the
+resulting pipeline to the Buildkite job that triggers this script.
+
+On master and tags, all jobs are always run.
+
+For details about how steps are trimmed, see the comment at the top of
+pipeline.template.yml and the docstring on `trim_pipeline` below.
+"""
+
 from collections import OrderedDict
 from materialize import mzbuild
 from materialize import spawn
@@ -26,6 +38,12 @@ def main() -> int:
     if os.environ["BUILDKITE_BRANCH"] != "master" and not os.environ["BUILDKITE_TAG"]:
         print("--- Trimming unchanged steps from pipeline")
         trim_pipeline(pipeline)
+
+    # Remove the Materialize-specific keys from the configuration that are
+    # only used to inform how to trim the pipeline.
+    for step in pipeline["steps"]:
+        if "inputs" in step:
+            del step["inputs"]
 
     f = TemporaryFile()
     yaml.dump(pipeline, f, encoding="utf-8")  # type: ignore
