1cf1b525719cdaa2316cc959dff4a95098f0cadc
Brennan Vincent <brennan@materialize.io>
2020-04-24 11:59:44 -0400

    re-buffer message

--- src/dataflow/source/kafka.rs
+++ src/dataflow/source/kafka.rs
@@ -256,6 +256,7 @@ where
                             &activator,
                         ) {
                             needs_refresh = true;
+                            buffer = Some(message);
                             return SourceStatus::Alive;
                         }
                     }
