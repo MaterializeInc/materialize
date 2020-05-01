7a84a6f8884480bc66eda9ae0a41f9d84ed771d1
Nikhil Benesch <nikhil.benesch@gmail.com>
2020-05-01 14:07:02 -0400

    ccsr: document all TLS methods
    Also avoid encoding URLs directly in the rustdoc. This will happen
    automatically when using bin/doc.

--- src/ccsr/tls.rs
+++ src/ccsr/tls.rs
@@ -7,7 +7,7 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
 
-//! TLS support for CCSR clients.
+//! TLS certificates and identities.
 
 use serde::{Deserialize, Serialize};
 
@@ -19,16 +19,14 @@ pub(crate) enum CertDetails {
     DER(Vec<u8>, String),
 }
 
-/// Provides a serde wrapper around
-/// [`reqwest::Identity`](https://docs.rs/reqwest/latest/reqwest/struct.Identity.html).
+/// A [Serde][serde]-enabled wrapper around [`reqwest::Identity`].
 #[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
 pub struct Identity {
     pub(crate) cert: CertDetails,
 }
 
 impl Identity {
-    /// Wraps
-    /// [`reqwest::Identity::from_pem`](https://docs.rs/reqwest/latest/reqwest/struct.Identity.html#method.from_pem).
+    /// Wraps [`reqwest::Identity::from_pem`].
     pub fn from_pem(pem: &[u8]) -> Result<Self, reqwest::Error> {
         let _ = reqwest::Identity::from_pem(&pem)?;
         Ok(Identity {
@@ -36,8 +34,7 @@ impl Identity {
         })
     }
 
-    /// Wraps
-    /// [`reqwest::Identity::from_pem`](https://docs.rs/reqwest/latest/reqwest/struct.Identity.html#method.from_pkcs12_der).
+    /// Wraps [`reqwest::Identity::from_pkcs12_der`].
     pub fn from_pkcs12_der(der: &[u8], password: &str) -> Result<Self, reqwest::Error> {
         let _ = reqwest::Identity::from_pkcs12_der(&der, password)?;
         Ok(Identity {
@@ -49,32 +46,36 @@ impl Identity {
 impl Into<reqwest::Identity> for Identity {
     fn into(self) -> reqwest::Identity {
         match self.cert {
-            CertDetails::PEM(pem) => {
+            CertDetails::Pem(pem) => {
                 reqwest::Identity::from_pem(&pem).expect("known to be a valid identity")
             }
-            CertDetails::DER(der, pass) => reqwest::Identity::from_pkcs12_der(&der, &pass)
+            CertDetails::Der(der, pass) => reqwest::Identity::from_pkcs12_der(&der, &pass)
                 .expect("known to be a valid identity"),
         }
     }
 }
 
-/// Provides a serde wrapper around
-/// [`reqwest::Certificate`](https://docs.rs/reqwest/latest/reqwest/struct.Certificate.html).
+/// A [Serde][serde]-enabled wrapper around [`reqwest::Certificate`].
 #[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
 pub struct Certificate {
     der: Vec<u8>,
 }
+
 impl Certificate {
+    /// Wraps [`reqwest::Certificate::from_pem`].
     pub fn from_pem(pem: &[u8]) -> native_tls::Result<Certificate> {
         Ok(Certificate {
             der: native_tls::Certificate::from_pem(pem)?.to_der()?,
         })
     }
+
+    /// Wraps [`reqwest::Certificate::from_der`].
     pub fn from_der(der: &[u8]) -> native_tls::Result<Certificate> {
         let _ = native_tls::Certificate::from_der(der)?;
         Ok(Certificate { der: der.into() })
     }
 }
+
 impl Into<reqwest::Certificate> for Certificate {
     fn into(self) -> reqwest::Certificate {
         reqwest::Certificate::from_der(&self.der).expect("known to be a valid cert")
