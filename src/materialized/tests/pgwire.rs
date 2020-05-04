d726842bb9c1952b216b5ce6acd7a973e8d8c5d8
Brennan Vincent <brennan@materialize.io>
2020-05-04 16:30:24 -0400

    code review

--- src/materialized/tests/pgwire.rs
+++ src/materialized/tests/pgwire.rs
@@ -246,6 +246,8 @@ fn test_multiple_statements() -> Result<(), Box<dyn Error>> {
 
 #[test]
 fn test_simple_query_no_hang() -> Result<(), Box<dyn Error>> {
+    ore::test::init_logging();
+
     let (_server, mut client) = util::start_server(util::Config::default())?;
     assert!(client.simple_query("asdfjkl;").is_err());
     // This will hang if #2880 is not fixed.
