903cb8dbdfe5db3b553cfbc6746292fc0a52067d
Justin Jaffray <justin.jaffray@gmail.com>
2020-05-26 14:30:38 -0400

    Enable apply_join logic test

--- ci/test/slt-fast.sh
+++ ci/test/slt-fast.sh
@@ -58,7 +58,7 @@ tests=(
     test/sqllogictest/cockroach/alias_types.slt
     test/sqllogictest/cockroach/alter_column_type.slt
     test/sqllogictest/cockroach/alter_table.slt
-    # test/sqllogictest/cockroach/apply_join.slt
+    test/sqllogictest/cockroach/apply_join.slt
     test/sqllogictest/cockroach/array.slt
     test/sqllogictest/cockroach/as_of.slt
     test/sqllogictest/cockroach/bit.slt
