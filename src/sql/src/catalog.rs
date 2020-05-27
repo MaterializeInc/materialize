c6e7e81921681ea2fc43717a6053db4b188fe59f
Nikhil Benesch <nikhil.benesch@gmail.com>
2020-05-27 13:03:08 -0400

    sql: remove iter from the PlanCatalog trait
    Nothing in the SQL planner should be iterating the catalog in its
    entirety. Update the one usage of iter() to use a more efficient
    implementation that does not require iterating the whole catalog.

--- src/sql/src/catalog.rs
+++ src/sql/src/catalog.rs
@@ -33,8 +33,6 @@ pub trait PlanCatalog: fmt::Debug {
 
     fn get_by_id(&self, id: &GlobalId) -> &dyn PlanCatalogEntry;
 
-    fn iter<'a>(&'a self) -> Box<dyn Iterator<Item = &'a dyn PlanCatalogEntry> + 'a>;
-
     fn get_schemas(&self, database_spec: &DatabaseSpecifier) -> Option<&dyn SchemaMap>;
 
     fn database_resolver<'a>(
@@ -129,10 +127,6 @@ impl PlanCatalog for DummyCatalog {
     }
 
     fn get_by_id(&self, _: &GlobalId) -> &dyn PlanCatalogEntry {
-        unimplemented!();
-    }
-
-    fn iter<'a>(&'a self) -> Box<dyn Iterator<Item = &'a dyn PlanCatalogEntry> + 'a> {
         unimplemented!();
     }
 
