f78ee7b65d69fb9bd0c2ce2e15b506b15f2b3c2d
Nikhil Benesch <nikhil.benesch@gmail.com>
2020-06-02 11:46:24 -0400

    dataflow: don't block on Kafka metadata refreshes
    Synchronously fetching metadata in the Kafka source causes problems. At
    best it blocks the worker thread from performing computation for a few
    milliseconds. At worst it blocks the worker thread for the full 1s
    allotment and then times out, resulting in a second of lost CPU time and
    a wedged Kafka source. Our hope was that production Kafka clusters would
    be reliable enough that the synchronous fetch would be a rare problem,
    but empirical evidence has shown otherwise.
    
    It turns out there is no need to synchronously fetch metadata in the
    Kafka source. librdkafka is happy, almost too happy, to allow us to
    subscribe to a partition for which it does not have metadata. The only
    stipulation is that we will not receive any data for the partition until
    the partition's metadata is retrieved.
    
    There are two main ways that the metadata refresh occurs. The first is
    an explicit call to `fetch_metadata`. The second is the
    regularly-scheduled background metadata fetch, controlled by
    topic.metadata.refresh.interval.ms, which defaults to 5m. This patch
    uses a combination of both strategies for optimal performance
    characteristics.
    
    First, this patch removes the synchronous metadata fetch entirely. Upon
    arrival of a new partition ID, the Timely worker responsible for that
    partition blindly assigns the consumer to that partition. If the
    partition ID is invalid, the source will wedge, but a) the partition ID
    can't be wrong with real-time timestamping, and b) if you use BYO
    consistency, you've put us in an impossible situation, so wedging is
    possibly the most correct outcome.
    
    Second, this patch turns sets the background metadata fetch interval to
    30s, down from its default of 5m. I suspect this is entirely sufficient
    for production deployments, where adding a partition is a rare operation
    on a human timescale, but it means our tests would take 30s to detect
    new partitions.
    
    So, finally, this patch triggers an asynchronous metadata fetch whenever
    it assigns a partition. This means tests detect new partitions almost
    instantly. We don't actually need to wait for the metadata fetch to
    complete, and it's not a problem if it fails, since the background
    metadata fetches will retry indefinitely, and we just need one to
    succeed eventually.
    
    Note that there is a bug in librdkafka that generates spurious error
    messages with this approach, but I've opened a patch to fix that. [0]
    
    [0]: https://github.com/edenhill/librdkafka/pull/2916

--- src/dataflow/src/source/kafka.rs
+++ src/dataflow/src/source/kafka.rs
@@ -169,6 +169,7 @@ fn create_kafka_config(
     kafka_config
         .set("enable.auto.commit", "false")
         .set("auto.offset.reset", "earliest")
+        .set("topic.metadata.refresh.interval.ms", "30000") // 30 seconds
         .set("fetch.message.max.bytes", "134217728")
         .set("bootstrap.servers", &url.to_string());
 
@@ -268,38 +269,6 @@ struct DataPlaneInfo {
     worker_id: i32,
     /// Worker Count
     worker_count: i32,
-}
-
-/// Refreshes metadata for a single consumer
-fn refresh_metadata(
-    consumer: &BaseConsumer<GlueConsumerContext>,
-    topic: &str,
-    expected_partitions: i32,
-) -> bool {
-    match consumer.fetch_metadata(Some(topic), Duration::from_secs(1)) {
-        Ok(md) => match md.topics().iter().find(|mdt| mdt.name() == topic) {
-            None => {
-                warn!("Topic {} not found in Kafka metadata", topic);
-                false
-            }
-            Some(mdt) => {
-                let partitions = mdt.partitions().len();
-                if partitions < expected_partitions.try_into().unwrap() {
-                    warn!(
-                        "Topic {} does not have as many partitions as expected ({} < {})",
-                        topic, partitions, expected_partitions
-                    );
-                    false
-                } else {
-                    true
-                }
-            }
-        },
-        Err(e) => {
-            warn!("Error refreshing Kafka metadata: {}", e);
-            false
-        }
-    }
 }
 
 impl DataPlaneInfo {
@@ -432,48 +401,63 @@ impl DataPlaneInfo {
 
     /// Create a new partition queue and adds it to the list of existing consumers
     fn create_partition_queue(&mut self, partition_id: i32) -> bool {
-        if refresh_metadata(self.consumer.as_ref(), &self.topic_name, partition_id + 1) {
-            // Collect old partition assignments
-            let tpl = self.consumer.assignment().unwrap();
-            // Create list from assignments
-            let mut partition_list = TopicPartitionList::new();
-            for partition in tpl.elements_for_topic(&self.topic_name) {
-                partition_list.add_partition_offset(
-                    partition.topic(),
-                    partition.partition(),
-                    partition.offset(),
-                );
-            }
-            // Add new partition
-            partition_list.add_partition_offset(&self.topic_name, partition_id, Offset::Beginning);
-            self.consumer.assign(&partition_list).unwrap();
-            let partition_queue = self
-                .consumer
-                .split_partition_queue(&self.topic_name, partition_id);
-            if let Some(partition_queue) = partition_queue {
-                self.partition_consumers
-                    .push_front(PartitionConsumer::new(partition_id, partition_queue));
-                assert_eq!(
-                    self.consumer
-                        .assignment()
-                        .unwrap()
-                        .elements_for_topic(&self.topic_name)
-                        .len(),
-                    self.partition_consumers.len()
-                );
-                self.partition_metrics.insert(
-                    partition_id,
-                    PartitionMetrics::new(
-                        &self.topic_name,
-                        &self.source_id,
-                        &partition_id.to_string(),
-                    ),
-                );
-                self.consumer.poll(Duration::from_secs(0));
-                true
-            } else {
-                false
-            }
+        // Collect old partition assignments
+        let tpl = self.consumer.assignment().unwrap();
+        // Create list from assignments
+        let mut partition_list = TopicPartitionList::new();
+        for partition in tpl.elements_for_topic(&self.topic_name) {
+            partition_list.add_partition_offset(
+                partition.topic(),
+                partition.partition(),
+                partition.offset(),
+            );
+        }
+        // Add new partition
+        partition_list.add_partition_offset(&self.topic_name, partition_id, Offset::Beginning);
+        self.consumer.assign(&partition_list).unwrap();
+
+        // Trick librdkafka into updating its metadata for the topic so that we
+        // start seeing data for the partition immediately. Otherwise we might
+        // need to wait the full `topic.metadata.refresh.interval.ms` interval.
+        //
+        // We don't actually care about the results of the metadata refresh, and
+        // would prefer not to wait for it to complete, so we execute this
+        // request with a timeout of 0s and ignore the result. This relies on an
+        // implementation detail, which is that librdkafka does not proactively
+        // cancel metadata fetch operations when they reach their timeout.
+        // Unfortunately there is no asynchronous metadata fetch API.
+        //
+        // It is not a problem if the metadata request fails, because the
+        // background metadata refresh will retry indefinitely. As long as a
+        // background request succeeds eventually, we'll start receiving data
+        // for the new partitions.
+        //
+        // TODO(benesch): remove this if upstream makes this metadata refresh
+        // happen automatically [0].
+        //
+        // [0]: https://github.com/edenhill/librdkafka/issues/2917
+        let _ = self.consumer.fetch_metadata(Some(&self.topic_name), Duration::from_secs(0));
+
+        let partition_queue = self
+            .consumer
+            .split_partition_queue(&self.topic_name, partition_id);
+        if let Some(partition_queue) = partition_queue {
+            self.partition_consumers
+                .push_front(PartitionConsumer::new(partition_id, partition_queue));
+            assert_eq!(
+                self.consumer
+                    .assignment()
+                    .unwrap()
+                    .elements_for_topic(&self.topic_name)
+                    .len(),
+                self.partition_consumers.len()
+            );
+            self.partition_metrics.insert(
+                partition_id,
+                PartitionMetrics::new(&self.topic_name, &self.source_id, &partition_id.to_string()),
+            );
+            self.consumer.poll(Duration::from_secs(0));
+            true
         } else {
             false
         }
