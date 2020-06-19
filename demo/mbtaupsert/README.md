81d564acf5fad4b8616a19ecbdf0a2c676479e80
Andi Wang <andi@materialize.io>
2020-06-19 10:53:51 -0400

    Add MBTA demo to main.

---
+++ demo/mbtaupsert/README.md
@@ -0,0 +1,71 @@
+# Real time application demo using the MBTA API
+
+## What is the MBTA API?
+
+Massachusetts Bay Transportation Authority manages public transit in the Boston
+area. It has a bunch of live JSON server-sent event source streams whose format
+are roughly like this:
+
+```
+event:reset
+[{id: ..., type: ..., other_fields: ...},
+ {id: ..., type: ..., other_fields: ...},
+ {id: ..., type: ..., other_fields: ...}, ...]
+
+event: update
+{id: ..., type: ..., other_fields: ...}
+
+event: remove
+{id: ..., type: ...}
+```
+
+## What will I be able to do in this demo?
+
+You will be able to issue SQL queries and maintain views on the live state of
+the Boston-area public transit system. 
+[doc/mbta-predictions.md](/doc/mbta-predictions.md) contains some inspiration to
+get you started. It tells you:
+* How to reproduce the MBTA countdown clock.
+* How to find the quickest way from stop A to stop B.
+
+In addition, you can run SQL queries and maintain views on the history of the
+state of the Boston-area public transit system from the time you started running
+the demo onwards by omitting the `ENVELOPE UPSERT` when creating a source. 
+
+## What does this code do?
+
+This code is a workaround for the fact that we don't yet support:
+* directly connecting server-sent event sources yet. 
+  (Follow progress on this at #2237)
+* converting a kafka topic not in key-value format to key-value format.
+  (Follow progress on this at #1576)
+It takes a file where the MBTA live stream is being written to and converts the
+data into a Kafka stream of the key-value format:
+* ```
+   event: remove
+   {id: some_id, type: ...}
+   ```
+   gets converted to a Kafka message with key `some_id` and null payload
+* ```
+    event: update
+    {id: some_id, type: ..., other_field1: ..., other_field2: ..., etc}
+    ```
+    gets converted to a Kafka message with key `some_id` and payload
+    ```
+    {other_field1: ..., other_field2: ..., etc}
+    ```
+* ```
+   event:reset
+   [{id: ..., type: ..., other_field1: ..., other_field2: ..., etc},
+    {id: ..., type: ..., other_field1: ..., other_field2: ..., etc},
+    {id: ..., type: ..., other_field1: ..., other_field2: ..., etc}, ...]
+   ```
+   gets converted into one Kafka per message per object in the array. Each
+   message has key `whatever_the_id_was` and payload
+   ```
+   {other_field1: whatever_is_in_this_field, other_field2: ..., etc}
+   ```
+
+Technically, this code is not MBTA stream-specific. With a few lines of changes, it should be able to take any stream of json objects, parse out the desired key, and then produce a key-value Kafka topic out of it. 
+
+Look in [doc/mbtaupsert-doc.md](doc/mbtaupsert-doc.md) for more information.
---
+++ demo/mbtaupsert/demo/all-frequent.sql
@@ -0,0 +1,78 @@
+DROP SOURCE IF EXISTS mbta_directions CASCADE;
+DROP SOURCE IF EXISTS mbta_stops CASCADE;
+DROP SOURCE IF EXISTS mbta_routes CASCADE;
+DROP SOURCE IF EXISTS all_pred CASCADE;
+DROP SOURCE IF EXISTS all_trips CASCADE;
+DROP SOURCE IF EXISTS all_schd CASCADE;
+DROP SOURCE IF EXISTS vehicles CASCADE;
+
+CREATE MATERIALIZED SOURCE mbta_directions
+  FROM FILE '/workdir/workspace/MBTA_GTFS/directions.txt' FORMAT CSV WITH HEADER;
+
+CREATE MATERIALIZED SOURCE mbta_stops
+  FROM FILE '/workdir/workspace/MBTA_GTFS/stops.txt' FORMAT CSV WITH HEADER;
+
+CREATE MATERIALIZED SOURCE mbta_routes
+  FROM FILE '/workdir/workspace/MBTA_GTFS/routes.txt' FORMAT CSV WITH HEADER;
+
+CREATE SOURCE all_pred
+  FROM KAFKA BROKER 'kafka:9092' TOPIC 'all-pred'
+  FORMAT TEXT ENVELOPE UPSERT FORMAT TEXT;
+
+CREATE SOURCE all_trip
+  FROM KAFKA BROKER 'kafka:9092' TOPIC 'all-trip'
+  FORMAT TEXT ENVELOPE UPSERT FORMAT TEXT;
+
+CREATE SOURCE all_schd
+  FROM KAFKA BROKER 'kafka:9092' TOPIC 'all-schd'
+  FORMAT TEXT ENVELOPE UPSERT FORMAT TEXT;
+
+CREATE SOURCE vehicles
+  FROM KAFKA BROKER 'kafka:9092' TOPIC 'all-vehicles'
+  FORMAT TEXT ENVELOPE UPSERT FORMAT TEXT;
+
+CREATE MATERIALIZED VIEW parsed_all_pred as 
+SELECT id,
+    CAST(payload->'attributes'->>'arrival_time' AS timestamptz) arrival_time,
+    CAST(payload->'attributes'->>'departure_time'  AS timestamptz) departure_time,
+    CAST(CAST(payload->'attributes'->>'direction_id' AS DECIMAL(5,1)) AS INT) direction_id,
+    payload->'attributes'->>'schedule_relationship' schedule_relationship,
+    payload->'attributes'->>'status' status,
+    CAST(CAST(payload->'attributes'->>'stop_sequence' AS DECIMAL(5,1)) AS INT) stop_sequence,
+    payload->'relationships'->'route'->'data'->>'id' route_id,
+    payload->'relationships'->'stop'->'data'->>'id' stop_id,
+    payload->'relationships'->'trip'->'data'->>'id' trip_id,
+    payload->'relationships'->'vehicle'->'data'->>'id' vehicle_id
+FROM (SELECT key0 as id, cast (text as jsonb) AS payload FROM all_pred);
+
+CREATE MATERIALIZED VIEW parsed_all_schd as 
+SELECT id,
+CAST(payload->'attributes'->>'arrival_time' AS timestamptz) arrival_time,
+CAST(payload->'attributes'->>'departure_time'  AS timestamptz) departure_time,
+CAST(CAST(payload->'attributes'->>'direction_id' AS DECIMAL(5,1)) AS INT) direction_id,
+CAST(CAST(payload->'attributes'->>'stop_sequence' AS DECIMAL(5,1)) AS INT) stop_sequence,
+payload->'relationships'->'route'->'data'->>'id' route_id,
+payload->'relationships'->'stop'->'data'->>'id' stop_id,
+payload->'relationships'->'trip'->'data'->>'id' trip_id
+FROM (SELECT key0 as id, cast ("text" as jsonb) AS payload FROM all_schd);
+
+CREATE MATERIALIZED VIEW parsed_all_trip as 
+SELECT id,
+payload->'attributes'->>'bikes_allowed' bikes_allowed,
+CAST(CAST(payload->'attributes'->>'direction_id' AS DECIMAL(5,1)) AS INT) direction_id,
+payload->'attributes'->>'headsign' headsign,
+payload->'attributes'->>'wheelchair_accessible' wheelchair_accessible,
+payload->'relationships'->'route'->'data'->>'id' route_id,
+payload->'relationships'->'route_pattern'->'data'->>'id' route_pattern_id,
+payload->'relationships'->'service'->'data'->>'id' service_id,
+payload->'relationships'->'shape'->'data'->>'id' shape_id
+FROM (SELECT key0 as id, cast ("text" as jsonb) AS payload FROM all_trip);
+
+CREATE MATERIALIZED VIEW parsed_vehicles as 
+SELECT id,
+payload->'attributes'->>'current_status' status,
+CAST(CAST(payload->'attributes'->>'direction_id' AS DECIMAL(5,1)) AS INT) direction_id,
+payload->'relationships'->'route'->'data'->>'id' route_id,
+payload->'relationships'->'stop'->'data'->>'id' stop_id,
+payload->'relationships'->'trip'->'data'->>'id' trip_id
+FROM (SELECT key0 as id, cast ("text" as jsonb) AS payload FROM vehicles);
