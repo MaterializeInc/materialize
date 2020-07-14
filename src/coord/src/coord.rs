9588d929f52519c4841fc6a16e1254b29af37917
Jessica Laughlin <JLDLaughlin@users.noreply.github.com>
2020-07-14 16:46:38 -0400

    Remove errant commented line (#3501)

--- src/coord/src/coord.rs
+++ src/coord/src/coord.rs
@@ -1858,7 +1858,6 @@ where
             _ => (),
         }
         let mut dataflow = DataflowDesc::new(name);
-        // let as_of = ;
         dataflow.set_as_of(connector.get_frontier());
         self.import_source_or_view(&id, &from, &mut dataflow);
         let from_type = self.catalog.get_by_id(&from).desc().unwrap().clone();
