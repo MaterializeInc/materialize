a85f2951b2b490e7a27cfdfc981abde47431a683
Nikhil Benesch <nikhil.benesch@gmail.com>
2020-07-22 00:53:50 -0400

    transform: fix bug in literal lifting
    The LiteralLifting transform was incorrectly hardcoding the output type
    of AggregateFunc::{Any,All} as non-nullable, when in fact their output
    type is nullable.
    
    This caused incorrect output on the two correlated subquery tests
    enabled in this commit.
    
    The fix is to use `AggregateFunc::output_type` to compute the correct
    output type; the optimizer should not be duplicating this information.
    (There *is* a larger problem of "it is too easy to get nullability
    information wrong", but even if that were solved, we still wouldn't want
    to be hardcoding information about function output types in the
    optimizer.)

--- src/transform/src/map_lifting.rs
+++ src/transform/src/map_lifting.rs
@@ -379,7 +379,7 @@ impl LiteralLifting {
                         .eval(Some(aggr.expr.eval(&[], &temp).unwrap()), &temp);
                     result.push(ScalarExpr::literal_ok(
                         eval,
-                        repr::ColumnType::new(repr::ScalarType::Bool),
+                        aggr.func.output_type(aggr.expr.typ(&input.typ())),
                     ));
                 }
                 result.reverse();
