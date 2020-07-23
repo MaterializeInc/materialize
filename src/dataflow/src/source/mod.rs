e069f6041b0413bfaf08ef6686cb66d8617f8173
Natacha Crooks <nacrooks@gmail.com>
2020-07-23 11:08:07 -0400

    Adding comments

--- src/dataflow/src/source/mod.rs
+++ src/dataflow/src/source/mod.rs
@@ -771,7 +771,9 @@ where
                             let partition = message.partition.clone();
                             let offset = message.offset;
 
-                            // Update ingestion metrics
+                            // Update ingestion metrics. Guaranteed to exist as the appropriate
+                            // entry gets created in SourceConstructor or when a new partition
+                            // is discovered
                             consistency_info
                                 .partition_metrics
                                 .get_mut(&partition)
