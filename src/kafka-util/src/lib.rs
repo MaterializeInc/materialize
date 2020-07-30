de293f6b67ed32dbd4882a11e3a6b60c46f39c51
Nikhil Benesch <nikhil.benesch@gmail.com>
2020-07-30 17:08:24 -0400

    kafka-util: add docs

--- src/kafka-util/src/lib.rs
+++ src/kafka-util/src/lib.rs
@@ -7,6 +7,10 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
 
+#![deny(missing_docs)]
+
+//! Utilities for working with Kafka.
+
 mod addr;
 
 pub mod admin;
