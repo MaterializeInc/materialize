6ad3c39c76966753b1f5634498feb3bd8b7b8975
Nikhil Benesch <nikhil.benesch@gmail.com>
2020-08-03 16:24:46 -0400

    ci: fix slt upload
    We need to pass through the "BUILDKITE_BUILD_ID" environment variable
    too.

--- ci/slt/upload.sh
+++ ci/slt/upload.sh
@@ -15,7 +15,7 @@ set -euo pipefail
 
 set -x  # TODO(benesch): remove when this script works reliably.
 
-if [[ "${BUILDKITE_BRANCH-}" =~ master|main && "${BUILDKITE_COMMIT-}" ]]; then
+if [[ "${BUILDKITE_BRANCH-}" = main && "${BUILDKITE_COMMIT-}" ]]; then
     mkdir -p target
     buildkite-agent artifact download target/slt-summary.json .
     jq --arg commit "$BUILDKITE_COMMIT" -rf ci/slt/parse-summary.jq target/slt-summary.json \
