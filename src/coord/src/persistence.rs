ee7b0d8b76e4f1331bde79ade259c6b08fb1c2bb
Ruchir Khaitan <ruchir0001@gmail.com>
2020-08-05 00:42:40 -0400

    do more actual work in the persister

--- src/coord/src/persistence.rs
+++ src/coord/src/persistence.rs
@@ -7,7 +7,7 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
 
-use std::collections::HashMap;
+use std::collections::{BTreeMap, HashMap};
 use std::path::PathBuf;
 use std::thread;
 use std::time::Duration;
@@ -16,13 +16,95 @@ use futures::executor::block_on;
 use futures::stream::StreamExt;
 use log::{error, info};
 
-use dataflow_types::{Persistence, WorkerPersistenceData};
+use dataflow_types::{Persistence, Timestamp, WorkerPersistenceData};
 use expr::GlobalId;
+
+#[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
+pub struct PersistedRecord {
+    offset: i64,
+    timestamp: Timestamp,
+    data: Vec<u8>,
+}
+
+#[derive(Debug)]
+pub struct Partition {
+    last_persisted_offset: Option<i64>,
+    pending: Vec<PersistedRecord>,
+}
 
 #[derive(Debug)]
 pub struct Source {
     id: GlobalId,
     path: PathBuf,
+    // TODO have this use partition id
+    partitions: BTreeMap<i32, Partition>,
+}
+
+impl Source {
+    pub fn new(id: GlobalId, path: PathBuf) -> Self {
+        Source {
+            id,
+            path,
+            partitions: BTreeMap::new(),
+        }
+    }
+
+    pub fn insert_record(
+        &mut self,
+        partition_id: i32,
+        offset: i64,
+        timestamp: Timestamp,
+        data: Vec<u8>,
+    ) {
+        // Start tracking this partition id if we are not already
+        self.partitions.entry(partition_id).or_insert(Partition {
+            last_persisted_offset: None,
+            pending: Vec::new(),
+        });
+
+        if let Some(partition) = self.partitions.get_mut(&partition_id) {
+            partition.pending.push(PersistedRecord {
+                offset,
+                timestamp,
+                data,
+            });
+        }
+    }
+
+    pub fn persist(&mut self) {
+        for (partition_id, partition) in self.partitions.iter_mut() {
+            if partition.pending.is_empty() {
+                // No data to persist here
+                continue;
+            }
+
+            partition.pending.sort();
+
+            let mut prev = partition.last_persisted_offset;
+            let mut prefix_length = 0;
+
+            for p in partition.pending.iter() {
+                match prev {
+                    None => {
+                        prefix_length += 1;
+                        prev = Some(p.offset);
+                    }
+                    Some(offset) => {
+                        if p.offset == offset + 1 {
+                            prefix_length += 1;
+                            prev = Some(p.offset);
+                        } else {
+                            break;
+                        }
+                    }
+                }
+            }
+            info!(
+                "partition {} found a prefix of {:?}",
+                partition_id, prefix_length
+            );
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -75,11 +157,7 @@ impl Persister {
                         continue;
                     }
                     // Allocate a new source
-                    let source = Source {
-                        id,
-                        path: persistence.path.clone(),
-                    };
-
+                    let source = Source::new(id, persistence.path.clone());
                     self.sources.insert(id, source);
                 }
                 PersistenceMetadata::DropSource(id) => {
@@ -101,11 +179,33 @@ impl Persister {
         loop {
             match tokio::time::timeout(Duration::from_secs(1), self.data_rx.next()).await {
                 Ok(None) => break,
-                Ok(Some(_)) => {
+                Ok(Some(Ok(data))) => {
                     info!("received some data to be persisted");
+                    if !self.sources.contains_key(&data.source_id) {
+                        error!(
+                            "received some data for source {} that we are not currently tracking.",
+                            data.source_id
+                        );
+                        continue;
+                    }
+
+                    let source = self
+                        .sources
+                        .get_mut(&data.source_id)
+                        .expect("source known to exist");
+
+                    source.insert_record(data.partition, data.offset, data.timestamp, data.data);
+                }
+                Ok(Some(Err(e))) => {
+                    error!("received error from persistence rx: {}", e);
+                    break;
                 }
                 Err(tokio::time::Elapsed { .. }) => break,
             }
+        }
+
+        for (_, s) in self.sources.iter_mut() {
+            s.persist();
         }
 
         false
