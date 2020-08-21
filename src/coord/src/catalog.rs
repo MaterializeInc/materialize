836db1b9657551fed5ee5eeea8fa23b8ffeed3e6
JLDLaughlin <jessica@materialize.io>
2020-08-21 08:03:13 -0400

    Return unknown database error
    If we try to resolve an item in an unknown databse, surface the error.
    This may happen if a user deletes the current database.

--- src/coord/src/catalog.rs
+++ src/coord/src/catalog.rs
@@ -696,16 +696,7 @@ impl Catalog {
                     Some(schema) => Ok(schema),
                     None => Err(SqlCatalogError::UnknownSchema(schema_name.into())),
                 },
-                None => {
-                    // Materialize allows you to drop the current database, meaning
-                    // that the current database may not exist when trying to get its
-                    // schema. If the current database isn't found, try to get the schema
-                    // from the set of ambient schemas, instead.
-                    match self.ambient_schemas.get(schema_name) {
-                        Some(schema) => Ok(schema),
-                        None => Err(SqlCatalogError::UnknownDatabase(name.to_owned())),
-                    }
-                }
+                None => Err(SqlCatalogError::UnknownDatabase(name.to_owned())),
             },
         }
     }
@@ -731,16 +722,7 @@ impl Catalog {
                     Some(schema) => Ok(schema),
                     None => Err(SqlCatalogError::UnknownSchema(schema_name.into())),
                 },
-                None => {
-                    // Materialize allows you to drop the current database, meaning
-                    // that the current database may not exist when trying to get its
-                    // schema. If the current database isn't found, try to get the schema
-                    // from the set of ambient schemas, instead.
-                    match self.ambient_schemas.get_mut(schema_name) {
-                        Some(schema) => Ok(schema),
-                        None => Err(SqlCatalogError::UnknownDatabase(name.to_owned())),
-                    }
-                }
+                None => Err(SqlCatalogError::UnknownDatabase(name.to_owned())),
             },
         }
     }
