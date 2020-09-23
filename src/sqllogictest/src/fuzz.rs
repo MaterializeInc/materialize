b391b28193dadbd1325c512fc73a9b29968f0697
Nikhil Benesch <nikhil.benesch@gmail.com>
2020-09-23 16:58:44 -0400

    sqllogictest: properly asyncify
    Plumb enough `async fn`s that all calls to `block_on` can be replaced
    with `.await`.
    
    This is part of a commit series to improve our async/sync glue code.

--- src/sqllogictest/src/fuzz.rs
+++ src/sqllogictest/src/fuzz.rs
@@ -9,8 +9,6 @@
 
 //! Fuzz testing via sqllogictest.
 
-use futures::executor::block_on;
-
 use coord::ExecuteResponse;
 
 use crate::runner::State;
@@ -18,8 +16,8 @@ use crate::runner::State;
 pub async fn fuzz(sqls: &str) {
     let mut state = State::start().await.unwrap();
     for sql in sqls.split(';') {
-        if let Ok((Some(desc), ExecuteResponse::SendingRows(rx))) = state.run_sql(sql) {
-            for row in block_on(rx).unwrap().unwrap_rows() {
+        if let Ok((Some(desc), ExecuteResponse::SendingRows(rx))) = state.run_sql(sql).await {
+            for row in rx.await.unwrap().unwrap_rows() {
                 for (typ, datum) in desc.iter_types().zip(row.iter()) {
                     assert!(datum.is_instance_of(typ));
                 }
--- src/sqllogictest/src/runner.rs
+++ src/sqllogictest/src/runner.rs
@@ -38,7 +38,6 @@ use std::time::Duration;
 
 use anyhow::{anyhow, bail};
 use bytes::BytesMut;
-use futures::executor::block_on;
 use itertools::izip;
 use lazy_static::lazy_static;
 use md5::{Digest, Md5};
@@ -412,7 +411,10 @@ impl State {
         })
     }
 
-    fn run_record<'a>(&mut self, record: &'a Record) -> Result<Outcome<'a>, anyhow::Error> {
+    async fn run_record<'a>(
+        &mut self,
+        record: &'a Record<'a>,
+    ) -> Result<Outcome<'a>, anyhow::Error> {
         match &record {
             Record::Statement {
                 expected_error,
@@ -420,7 +422,10 @@ impl State {
                 sql,
                 location,
             } => {
-                match self.run_statement(*expected_error, *rows_affected, sql, location.clone())? {
+                match self
+                    .run_statement(*expected_error, *rows_affected, sql, location.clone())
+                    .await?
+                {
                     Outcome::Success => Ok(Outcome::Success),
                     other => {
                         if expected_error.is_some() {
@@ -441,12 +446,12 @@ impl State {
                 sql,
                 output,
                 location,
-            } => self.run_query(sql, output, location.clone()),
+            } => self.run_query(sql, output, location.clone()).await,
             _ => Ok(Outcome::Success),
         }
     }
 
-    fn run_statement<'a>(
+    async fn run_statement<'a>(
         &mut self,
         expected_error: Option<&'a str>,
         expected_rows_affected: Option<usize>,
@@ -462,7 +467,7 @@ impl State {
             return Ok(Outcome::Success);
         }
 
-        match self.run_sql(sql) {
+        match self.run_sql(sql).await {
             Ok((_desc, resp)) => {
                 if let Some(expected_error) = expected_error {
                     return Ok(Outcome::UnexpectedPlanSuccess {
@@ -505,10 +510,10 @@ impl State {
         }
     }
 
-    fn run_query<'a>(
+    async fn run_query<'a>(
         &mut self,
         sql: &'a str,
-        output: &'a Result<QueryOutput, &'a str>,
+        output: &'a Result<QueryOutput<'_>, &'a str>,
         location: Location,
     ) -> Result<Outcome<'a>, anyhow::Error> {
         // get statement
@@ -540,10 +545,10 @@ impl State {
         }
 
         // send plan, read response
-        let (desc, rows) = match self.run_sql(sql) {
+        let (desc, rows) = match self.run_sql(sql).await {
             Ok((desc, ExecuteResponse::SendingRows(rx))) => {
                 let desc = desc.expect("RelationDesc missing for query that returns rows");
-                let rows = match block_on(rx)? {
+                let rows = match rx.await? {
                     PeekResponse::Rows(rows) => Ok(rows),
                     PeekResponse::Error(e) => Err(anyhow!("{}", e)),
                     PeekResponse::Canceled => {
@@ -722,7 +727,7 @@ impl State {
         Ok(Outcome::Success)
     }
 
-    pub(crate) fn run_sql(
+    pub(crate) async fn run_sql(
         &mut self,
         sql: &str,
     ) -> Result<(Option<RelationDesc>, ExecuteResponse), anyhow::Error> {
@@ -747,7 +752,7 @@ impl State {
                     tx,
                 })
                 .expect("futures channel should not fail");
-            let resp = block_on(rx).expect("futures channel should not fail");
+            let resp = rx.await.expect("futures channel should not fail");
             resp.result?;
             self.session = resp.session;
         }
@@ -772,7 +777,7 @@ impl State {
                     tx,
                 })
                 .expect("futures channel should not fail");
-            let resp = block_on(rx).expect("futures channel should not fail");
+            let resp = rx.await.expect("futures channel should not fail");
             self.session = resp.session;
             Ok((desc, resp.result?))
         }
@@ -807,6 +812,7 @@ pub async fn run_string(
 
         let outcome = state
             .run_record(&record)
+            .await
             .map_err(|err| format!("In {}:\n{}", source, err))
             .unwrap();
 
@@ -859,7 +865,7 @@ pub async fn rewrite_file(filename: &Path, _verbosity: usize) -> Result<(), anyh
     println!("==> {}", filename.display());
     for record in parser.parse_records()? {
         let record = record;
-        let outcome = state.run_record(&record)?;
+        let outcome = state.run_record(&record).await?;
 
         // If we see an output failure for a query, rewrite the expected output
         // to match the observed output.
