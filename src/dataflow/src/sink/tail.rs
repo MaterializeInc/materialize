8d4a3085d1ec2b72f1f7071b10fb041216e9f30f
Brennan Vincent <brennan@materialize.io>
2020-10-05 17:52:45 -0400

    wip

--- src/dataflow/src/sink/tail.rs
+++ src/dataflow/src/sink/tail.rs
@@ -7,7 +7,11 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
 
-use timely::dataflow::channels::pact::Pipeline;
+use std::collections::BinaryHeap;
+
+use differential_dataflow::AsCollection;
+use differential_dataflow::{operators::consolidate::Consolidate, Hashable};
+use timely::dataflow::channels::pact::{Exchange, Pipeline};
 use timely::dataflow::operators::Operator;
 use timely::dataflow::{Scope, Stream};
 
@@ -18,6 +22,21 @@ use dataflow_types::{TailSinkConnector, Update};
 use expr::GlobalId;
 use repr::{Diff, Row, Timestamp};
 
+#[derive(PartialEq, Eq, Debug)]
+struct TsOrder(Row, Timestamp, Diff);
+
+impl Ord for TsOrder {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        other.1.cmp(&self.1)
+    }
+}
+
+impl PartialOrd for TsOrder {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
 pub fn tail<G>(
     stream: &Stream<G, (Row, Timestamp, Diff)>,
     id: GlobalId,
@@ -26,29 +45,46 @@ pub fn tail<G>(
     G: Scope<Timestamp = Timestamp>,
 {
     let mut tx = block_on(connector.tx.connect()).expect("tail transmitter failed");
-    stream.sink(Pipeline, &format!("tail-{}", id), move |input| {
-        input.for_each(|_, rows| {
-            let mut results: Vec<Update> = Vec::new();
-            for (row, time, diff) in rows.iter() {
-                let should_emit = if connector.strict {
-                    connector.frontier.less_than(time)
-                } else {
-                    connector.frontier.less_equal(time)
-                };
-                if should_emit {
-                    results.push(Update {
-                        row: row.clone(),
-                        timestamp: *time,
-                        diff: *diff,
-                    });
-                }
-            }
+    let hash = id.hashed();
+    let consolidated = stream.as_collection().consolidate();
 
-            // TODO(benesch): this blocks the Timely thread until the send
-            // completes. Hopefully it's just a quick write to a kernel buffer,
-            // but perhaps not if the batch gets too large? We may need to do
-            // something smarter, like offloading to a networking thread.
-            block_on(tx.send(results)).expect("tail send failed");
-        });
-    })
+    let mut stash = BinaryHeap::new();
+    consolidated.inner.sink(
+        Exchange::new(move |_| hash),
+        &format!("tail-{}", id),
+        move |input| {
+            input.for_each(|_, rows| {
+                for (row, time, diff) in rows.replace(vec![]) {
+                    let should_emit = if connector.strict {
+                        connector.frontier.less_than(&time)
+                    } else {
+                        connector.frontier.less_equal(&time)
+                    };
+                    if should_emit {
+                        stash.push(TsOrder(row, time, diff));
+                    }
+                }
+            });
+            let mut results = Vec::new();
+            while stash
+                .peek()
+                .map(|elt| !input.frontier().less_equal(&elt.1))
+                .unwrap_or(false)
+            {
+                let TsOrder(row, timestamp, diff) = stash.pop().unwrap();
+                results.push(Update {
+                    row,
+                    timestamp,
+                    diff,
+                })
+            }
+            if !results.is_empty() {
+                // TODO(benesch): this blocks the Timely thread until the send
+                // completes. Hopefully it's just a quick write to a kernel buffer,
+                // but perhaps not if the batch gets too large? We may need to do
+                // something smarter, like offloading to a networking thread.
+                block_on(tx.send(results)).expect("tail send failed");
+            }
+        },
+    )
 }
