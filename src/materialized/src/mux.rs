MaterializeInc/materialize

commit 47d13475cc4c14cd9366b0e55754d6de384f4fdb

Provide context for mux error messages
Currently some users are seeing error logs like this:

    materialized::mux: error handling connection: connection closed before message completed

Which doesn't provide enough context for us to know where the issue is, or what it is.

src/materialized/src/mux.rs
@@ -9,6 +9,7 @@
 
 use std::sync::Arc;
 
+use anyhow::Context;
 use async_trait::async_trait;
 use futures::future::TryFutureExt;
 use futures::stream::{Stream, StreamExt};
@@ -126,7 +127,9 @@ impl ConnectionHandler for pgwire::Server {
     }
 
     async fn handle_connection(&self, conn: SniffedStream<TcpStream>) -> Result<(), anyhow::Error> {
-        self.handle_connection(conn).await
+        self.handle_connection(conn)
+            .await
+            .context("in pgwire server")
     }
 }
 
@@ -137,7 +140,7 @@ impl ConnectionHandler for http::Server {
     }
 
     async fn handle_connection(&self, conn: SniffedStream<TcpStream>) -> Result<(), anyhow::Error> {
-        self.handle_connection(conn).await
+        self.handle_connection(conn).await.context("in http server")
     }
 }
 

commit 5de39e68c4fe9463b4e99b649429397ff60c955d

materialized: improve warn/error logs in the mux
These were broken by 47d1347, resulting in error messages like:

    materialized::mux: error handling connection: in pgwire server

with no information about the actual error. This patch fixes the issue
by ensuring that the underlying error is printed along with all of its
causes.

It also makes adding the name of the handler to the error message the
responsibility of the mux, rather than the handler, so that handlers
don't forget to add the appropriate context.

Fix #4441.

src/materialized/Cargo.toml
@@ -22,6 +22,7 @@ dataflow = { path = "../dataflow" }
 dataflow-types = { path = "../dataflow-types" }
 futures = "0.3"
 getopts = "0.2"
+hex = "0.4"
 hyper = "0.13.8"
 include_dir = "0.6.0"
 krb5-src = { version = "0.2.3", features = ["binaries"] }

src/materialized/src/mux.rs
@@ -9,7 +9,6 @@
 
 use std::sync::Arc;
 
-use anyhow::Context;
 use async_trait::async_trait;
 use futures::future::TryFutureExt;
 use futures::stream::{Stream, StreamExt};
@@ -99,19 +98,25 @@ async fn handle_connection(handlers: Arc<Handlers>, conn: TcpStream) {
     for handler in &*handlers {
         if handler.match_handshake(buf) {
             if let Err(e) = handler.handle_connection(ss.into_sniffed()).await {
-                error!("error handling connection: {}", e);
+                error!("error handling connection in {}: {:#}", handler.name(), e);
             }
             return;
         }
     }
 
-    log::warn!("unknown protocol connection!");
+    log::warn!(
+        "dropped connection using unknown protocol (sniffed: 0x{})",
+        hex::encode(buf)
+    );
     let _ = ss.into_sniffed().write_all(b"unknown protocol\n").await;
 }
 
 /// A connection handler manages an incoming network connection.
 #[async_trait]
 pub trait ConnectionHandler {
+    /// Returns the name of the connection handler for use in e.g. log messages.
+    fn name(&self) -> &str;
+
     /// Determines whether this handler can accept the connection based on the
     /// first several bytes in the stream.
     fn match_handshake(&self, buf: &[u8]) -> bool;
@@ -122,30 +127,40 @@ pub trait ConnectionHandler {
 
 #[async_trait]
 impl ConnectionHandler for pgwire::Server {
+    fn name(&self) -> &str {
+        "pgwire server"
+    }
+
     fn match_handshake(&self, buf: &[u8]) -> bool {
         pgwire::match_handshake(buf)
     }
 
     async fn handle_connection(&self, conn: SniffedStream<TcpStream>) -> Result<(), anyhow::Error> {
-        self.handle_connection(conn)
-            .await
-            .context("in pgwire server")
+        self.handle_connection(conn).await
     }
 }
 
 #[async_trait]
 impl ConnectionHandler for http::Server {
+    fn name(&self) -> &str {
+        "http server"
+    }
+
     fn match_handshake(&self, buf: &[u8]) -> bool {
         self.match_handshake(buf)
     }
 
     async fn handle_connection(&self, conn: SniffedStream<TcpStream>) -> Result<(), anyhow::Error> {
-        self.handle_connection(conn).await.context("in http server")
+        self.handle_connection(conn).await
     }
 }
 
 #[async_trait]
 impl ConnectionHandler for comm::Switchboard<SniffedStream<TcpStream>> {
+    fn name(&self) -> &str {
+        "switchboard"
+    }
+
     fn match_handshake(&self, buf: &[u8]) -> bool {
         comm::protocol::match_handshake(buf)
     }