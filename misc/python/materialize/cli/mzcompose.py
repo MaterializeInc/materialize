ea036eed70284e5f9ec1a88cecebdd6197acea6f
Chris Golden <551285+cirego@users.noreply.github.com>
2020-10-12 21:47:37 -0700

    Relax minimum docker compose version to 1.25 (from 1.27) (#4489)
    Our CI tests use Docker Compose 1.25.4, so let's relax the version
    check.

--- misc/python/materialize/cli/mzcompose.py
+++ misc/python/materialize/cli/mzcompose.py
@@ -28,7 +28,7 @@ from materialize import ui
 announce = ui.speaker("==>")
 say = ui.speaker("")
 
-MIN_COMPOSE_VERSION = (1, 27, 0)
+MIN_COMPOSE_VERSION = (1, 25, 0)
 
 
 def assert_docker_compose_version() -> None:
