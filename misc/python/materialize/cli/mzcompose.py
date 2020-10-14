MaterializeInc/materialize

commit f0b9476b856f39f02175e6b723026d38be004d20

mzbuild: use ui.speaker and verbosity more regularly

misc/python/materialize/ui.py
@@ -10,14 +10,35 @@
 """Utilities for interacting with humans
 """
 
+import os
 import shlex
 import sys
 import time
 from typing import Any, Callable, Generator, Iterable, Optional
 
 
+class Verbosity:
+    """How noisy logs should be"""
+
+    quiet: bool = False
+
+    @classmethod
+    def init_from_env(cls, explicit: Optional[bool]) -> None:
+        """Set to quiet based on MZ_QUIET being set to almost any value
+
+        The only values that this gets set to false for are the empty string, 0, or no
+        """
+        env = os.getenv("MZ_QUIET")
+        if env is not None:
+            cls.quiet = env not in ("", "0", "no")
+        if explicit is not None:
+            cls.quiet = explicit
+
+
 def speaker(prefix: str, for_progress: bool = False) -> Callable[..., None]:
     """Create a function that will log with a prefix to stderr
+
+    Obeys `Verbosity.quiet`_
 
     Example::
 
@@ -27,7 +48,8 @@ def speaker(prefix: str, for_progress: bool = False) -> Callable[..., None]:
     """
 
     def say(msg: str, *fmt: str) -> None:
-        print("{}> {}".format(prefix, msg.format(*fmt)), file=sys.stderr)
+        if not Verbosity.quiet:
+            print("{} {}".format(prefix, msg.format(*fmt)), file=sys.stderr)
 
     return say
 

misc/python/bin/mzcompose.py
@@ -9,7 +9,6 @@
 #
 # mzcompose.py — runs Docker Compose with Materialize customizations.
 
-from materialize import mzbuild
 from pathlib import Path
 from tempfile import TemporaryFile
 from typing import List, Tuple, Text, Optional, Sequence
@@ -19,6 +18,13 @@ import os
 import subprocess
 import sys
 import yaml
+
+from materialize import ui
+from materialize import mzbuild
+
+
+announce = ui.speaker("==>")
+say = ui.speaker("")
 
 
 def main(argv: List[str]) -> int:
@@ -35,9 +41,10 @@ def main(argv: List[str]) -> int:
     else:
         config_file = args.file[0]
 
-    def say(s: str) -> None:
-        if not args.mz_quiet:
-            print(s)
+    ui.Verbosity.init_from_env(args.mz_quiet)
+    # TODO: we should propagate this down to subprocesses by explicit command-line flags, probably
+    if ui.Verbosity.quiet:
+        os.environ["MZ_QUIET"] = "yes"
 
     root = Path(os.environ["MZ_ROOT"])
     repo = mzbuild.Repository(root)
@@ -46,7 +53,7 @@ def main(argv: List[str]) -> int:
         return gen_shortcuts(repo)
 
     # Determine what images this particular compose file depends upon.
-    say("==> Collecting mzbuild dependencies")
+    announce("Collecting mzbuild dependencies")
     images = []
     with open(config_file) as f:
         compose = yaml.safe_load(f)
@@ -104,7 +111,7 @@ def main(argv: List[str]) -> int:
     tempfile.seek(0)
 
     # Hand over control to Docker Compose.
-    say("==> Delegating to Docker Compose")
+    announce("Delegating to Docker Compose")
     dc_args = [
         "docker-compose",
         "-f",
@@ -148,7 +155,7 @@ exec "$(dirname "$0")/{}/bin/mzcompose" "$@"
 class ArgumentParser(argparse.ArgumentParser):
     def __init__(self) -> None:
         super().__init__(add_help=False)
-        self.add_argument("--mz-quiet", action="store_true")
+        self.add_argument("--mz-quiet", action="store_true", default=None)
         self.add_argument("-f", "--file", action="append")
         self.add_argument("--project-directory")
         self.add_argument("command", nargs="?")

misc/python/bin/mzconduct.py
@@ -46,6 +46,7 @@ from materialize import ui
 
 
 T = TypeVar("T")
+say = ui.speaker("C>")
 
 
 @click.group(context_settings=dict(help_option_names=["-h", "--help"]))
@@ -600,9 +601,6 @@ def mzcompose_down(destroy_volumes: bool = False) -> subprocess.CompletedProcess
 
 
 # Helpers
-
-
-say = ui.speaker("C")
 
 
 def print_docker_logs(pattern: str, tail: int = 0) -> None:

commit 82713a2c633c5c3aa85b943a554898f1a4329254

misc/python: don't require spacing in ui.speaker
f0b9476 introduced a bug in which messages in mzcompose would be output
with a leading space. Fix that.

misc/python/materialize/cli/mzcompose.py
@@ -25,7 +25,7 @@ from materialize import spawn
 from materialize import ui
 
 
-announce = ui.speaker("==>")
+announce = ui.speaker("==> ")
 say = ui.speaker("")
 
 MIN_COMPOSE_VERSION = (1, 25, 0)

misc/python/materialize/cli/mzconduct.py
@@ -55,7 +55,7 @@ from materialize.errors import (
 
 
 T = TypeVar("T")
-say = ui.speaker("C>")
+say = ui.speaker("C> ")
 
 _BASHLIKE_ENV_VAR_PATTERN = re.compile(
     r"""\$\{

misc/python/materialize/ui.py
@@ -36,21 +36,22 @@ class Verbosity:
             cls.quiet = explicit
 
 
-def speaker(prefix: str, for_progress: bool = False) -> Callable[..., None]:
-    """Create a function that will log with a prefix to stderr
+def speaker(prefix: str) -> Callable[..., None]:
+    """Create a function that will log with a prefix to stderr.
 
-    Obeys `Verbosity.quiet`_
+    Obeys `Verbosity.quiet`. Note that you must include any necessary
+    spacing after the prefix.
 
     Example::
 
-        >>> say = speaker("mz")
+        >>> say = speaker("mz> ")
         >>> say("hello")  # doctest: +SKIP
         mz> hello
     """
 
     def say(msg: str) -> None:
         if not Verbosity.quiet:
-            print("{} {}".format(prefix, msg), file=sys.stderr)
+            print(f"{prefix}{msg}", file=sys.stderr)
 
     return say
 