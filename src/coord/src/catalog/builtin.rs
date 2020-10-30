8e01b0c3db6611f51938963637e9ef8ae699852f
JLDLaughlin <jessica@materialize.io>
2020-10-30 10:42:23 -0400

    catalog: add mz_builtin_types system tables
    Adds a new mz_builtin_types system table to track builtin types. Updates
    the CatalogItem::Type::Map variant to hold the GlobalIds for its key and
    value, which can be obtained with a join.

--- src/coord/src/catalog/builtin.rs
+++ src/coord/src/catalog/builtin.rs
@@ -383,16 +383,27 @@ lazy_static! {
         id: GlobalId::System(2025),
         index_id: GlobalId::System(2026),
     };
+    pub static ref MZ_BUILTIN_TYPES: BuiltinTable = BuiltinTable {
+        name: "mz_builtin_types",
+        schema: MZ_CATALOG_SCHEMA,
+        desc: RelationDesc::empty()
+            .with_column("id", ScalarType::String.nullable(false))
+            .with_column("oid", ScalarType::Oid.nullable(false))
+            .with_column("name", ScalarType::String.nullable(false)),
+        id: GlobalId::System(2027),
+        index_id: GlobalId::System(2028),
+    };
     pub static ref MZ_MAP_TYPES: BuiltinTable = BuiltinTable {
         name: "mz_map_types",
         schema: MZ_CATALOG_SCHEMA,
         desc: RelationDesc::empty()
+            .with_column("id", ScalarType::String.nullable(false))
             .with_column("oid", ScalarType::Oid.nullable(false))
             .with_column("name", ScalarType::String.nullable(false))
-            .with_column("key_oid", ScalarType::Oid.nullable(false))
-            .with_column("value_oid", ScalarType::Oid.nullable(false)),
-        id: GlobalId::System(2027),
-        index_id: GlobalId::System(2028),
+            .with_column("key_id", ScalarType::String.nullable(false))
+            .with_column("value_id", ScalarType::String.nullable(false)),
+        id: GlobalId::System(2029),
+        index_id: GlobalId::System(2030),
     };
 }
 
@@ -1060,6 +1071,7 @@ lazy_static! {
             Builtin::Table(&MZ_SOURCES),
             Builtin::Table(&MZ_SINKS),
             Builtin::Table(&MZ_VIEWS),
+            Builtin::Table(&MZ_BUILTIN_TYPES),
             Builtin::Table(&MZ_MAP_TYPES),
             Builtin::View(&MZ_RELATIONS),
             Builtin::View(&MZ_OBJECTS),
