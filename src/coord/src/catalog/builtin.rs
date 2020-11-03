f4b3153703973776e836892a2dc7c40f3a8fb969
Eli Lindsey <eli@siliconsprawl.com>
2020-11-03 17:44:26 -0500

    extend mz_perf_dependency_frontiers to include sources

--- src/coord/src/catalog/builtin.rs
+++ src/coord/src/catalog/builtin.rs
@@ -645,12 +645,22 @@ pub const MZ_PERF_DEPENDENCY_FRONTIERS: BuiltinView = BuiltinView {
     sql: "CREATE VIEW mz_perf_dependency_frontiers AS SELECT DISTINCT
     mcn.name AS dataflow,
     mcn_source.name AS source,
-    frontier_source.time - frontier_df.time as lag_ms
+    frontier_source.time - frontier_df.time AS lag_ms
 FROM mz_catalog.mz_materialization_dependencies index_deps
 JOIN mz_catalog.mz_materialization_frontiers frontier_source ON index_deps.source = frontier_source.global_id
 JOIN mz_catalog.mz_materialization_frontiers frontier_df ON index_deps.dataflow = frontier_df.global_id
 JOIN mz_catalog.mz_catalog_names mcn ON mcn.global_id = index_deps.dataflow
-JOIN mz_catalog.mz_catalog_names mcn_source ON mcn_source.global_id = frontier_source.global_id",
+JOIN mz_catalog.mz_catalog_names mcn_source ON mcn_source.global_id = frontier_source.global_id
+UNION
+SELECT DISTINCT
+    mcn.name AS dataflow,
+    mcn_source.name AS source,
+    CASE WHEN source_info.time < frontier_df.time THEN 0 ELSE source_info.time - frontier_df.time END AS lag_ms
+FROM mz_catalog.mz_materialization_dependencies index_deps
+JOIN (SELECT source_id, MAX(timestamp) time FROM mz_catalog.mz_source_info GROUP BY source_id) source_info ON index_deps.source = source_info.source_id
+JOIN mz_catalog.mz_materialization_frontiers frontier_df ON index_deps.dataflow = frontier_df.global_id
+JOIN mz_catalog.mz_catalog_names mcn ON mcn.global_id = index_deps.dataflow
+JOIN mz_catalog.mz_catalog_names mcn_source ON mcn_source.global_id = source_info.source_id",
     id: GlobalId::System(3014),
     needs_logs: true,
 };
