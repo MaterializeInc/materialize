aa7944fce19ce0a48922a687de61a9a00a966731
Nikhil Benesch <nikhil.benesch@gmail.com>
2020-12-09 15:37:34 -0500

    testdrive: learn to decode arbitrary byte escapes
    Teach testdrive to accept arbitrary byte escapes using the standard C
    hexadecimal escape syntax
    
        \xNN
    
    where each N is a valid hexadecimal digit (0-9, a-f, A-F).
    
    This makes it possible to use testdrive to test invalid UTF-8 input.

---
+++ src/testdrive/src/format/bytes.rs
@@ -0,0 +1,43 @@
+// Copyright Materialize, Inc. All rights reserved.
+//
+// Use of this software is governed by the Business Source License
+// included in the LICENSE file.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0.
+
+/// Unescapes a testdrive byte string.
+///
+/// The escape character is `\` and the only interesting escape sequence is
+/// `\xNN`, where each `N` is a valid hexadecimal digit. All other characters
+/// following a backslash are taken literally.
+pub fn unescape(s: &[u8]) -> Result<Vec<u8>, String> {
+    let mut out = vec![];
+    let mut s = s.iter().copied().fuse();
+    while let Some(b) = s.next() {
+        match b {
+            b'\\' if s.next() == Some(b'x') => match (next_hex(&mut s), next_hex(&mut s)) {
+                (Some(c1), Some(c0)) => out.push((c1 << 4) + c0),
+                _ => return Err("invalid hexadecimal escape".into()),
+            },
+            b'\\' => continue,
+            _ => out.push(b),
+        }
+    }
+    Ok(out)
+}
+
+/// Retrieves the value of the next hexadecimal digit in `iter`, if the next
+/// byte is a valid hexadecimal digit.
+fn next_hex<I>(iter: &mut I) -> Option<u8>
+where
+    I: Iterator<Item = u8>,
+{
+    iter.next().and_then(|c| match c {
+        b'A'..=b'F' => Some(c - b'A' + 10),
+        b'a'..=b'f' => Some(c - b'a' + 10),
+        b'0'..=b'9' => Some(c - b'0'),
+        _ => None,
+    })
+}
