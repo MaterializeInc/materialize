a84f4f9d27e2d1f459b5d22d7e0c9e413d690ac9
Sean Loiselle <sean@materialize.io>
2020-12-10 20:37:13 -0500

    catalog: reserialize and rewrite catalog items for type resolution
    Previous commits resolve type names to catalog items, which we now
    want to persist in the catalog. This commit achieves that by
    reserializing items all items in the catalog, and then overwriting
    the old version without qualified type names with the new version
    with them.

--- src/coord/src/catalog.rs
+++ src/coord/src/catalog.rs
@@ -385,7 +385,46 @@ impl CatalogEntry {
 lazy_static! {
     /// Content migrations for the catalog's on-disk state.
     static ref CATALOG_CONTENT_MIGRATIONS: Vec<Box<dyn Fn(&mut Catalog) -> Result<(), Error> + Send + Sync>> =
-        vec![];
+        vec![
+            // Reparses and rewrites all catalog items, which was necessary
+            // because we began qualifying type names.
+            //
+            // Introduced for v0.6.1
+            Box::new(|catalog: &mut Catalog| {
+                let mut storage = catalog.storage();
+                let items = storage.load_items()?;
+                let tx = storage.transaction()?;
+                for (id, name, def) in items {
+                    let item = match catalog.deserialize_item(def) {
+                        Ok(item) => item,
+                        Err(e) => {
+                            return Err(Error::new(ErrorKind::Corruption {
+                                detail: format!("failed to deserialize item {} ({}): {}", id, name, e),
+                            }))
+                        }
+                    };
+                    let serialized_item = catalog.serialize_item(&item);
+                    tx.update_item(id, &name.item, &serialized_item)?;
+                }
+                tx.commit()?;
+                Ok(())
+            })
+            // Add new migrations here.
+            //
+            // Migrations should be preceded with a comment of the following form:
+            //
+            //     > Short summary of migration's purpose.
+            //     >
+            //     > Introduced in <VERSION>.
+            //     >
+            //     > Optional additional commentary about safety or approach.
+            //
+            // Please include @benesch on any code reviews that add or edit migrations.
+            // Migrations must preserve backwards compatibility with all past releases
+            // of materialized. Migrations can be edited up until they ship in a
+            // release, after which they must never be removed, only patched by future
+            // migrations.
+        ];
 }
 
 impl Catalog {
