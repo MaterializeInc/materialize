47f9558a8154e2f0390bad150cb97f3e893b1b9a
Sean Loiselle <sean@materialize.io>
2020-12-13 14:21:09 -0500

    sql: reslove type names to catalog

--- src/sql/src/plan.rs
+++ src/sql/src/plan.rs
@@ -55,7 +55,7 @@ pub(crate) mod typeconv;
 pub use self::expr::RelationExpr;
 pub use error::PlanError;
 // This is used by sqllogictest to turn SQL values into `Datum`s.
-pub use query::scalar_type_from_sql;
+pub use query::{scalar_type_from_sql, unwrap_numeric_typ_mod};
 pub use statement::{describe_statement, FetchOptions, StatementContext, StatementDesc};
 
 /// Instructions for executing a SQL query.
