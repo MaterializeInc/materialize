21f8951bcc8c21f034188f3e577f1ed09ceb5014
Chris Golden <551285+cirego@users.noreply.github.com>
2020-12-14 17:12:45 -0800

    Fixup logic for waiting on Materialize to catch up to a Kafka topic (#4997)
    Wait for sources to reach a specific offset in the Kafka topic before we
    read the view contents. Otherwise, for sources that includes deletions
    and updates, we can hit the desired view long before we have read all
    contents of the Kafka topic.
    
    Fixes #4994

--- misc/mzutil/scripts/wait_for_view_states.py
+++ misc/mzutil/scripts/wait_for_view_states.py
@@ -19,6 +19,7 @@ Prints timing information to indicate how long each view took to reach the desir
 import argparse
 import glob
 import io
+import json
 import os
 import sys
 import pathlib
@@ -40,15 +41,41 @@ def view_names(
             yield row[0]
 
 
-def view_matches(cursor: psycopg2.extensions.cursor, view: str, expected: str) -> bool:
+def view_matches(
+    cursor: psycopg2.extensions.cursor, view: str, expected: str, timestamp: int
+) -> bool:
     """Return True if a SELECT from the VIEW matches the expected string."""
     stream = io.StringIO()
+    query = f"COPY (SELECT * FROM {view} WHERE mz_logical_timestamp() > {timestamp}) TO STDOUT"
     try:
-        cursor.copy_expert(f"COPY (SELECT * FROM {view}) TO STDOUT", stream)
+        cursor.copy_expert(query, stream)
     except psycopg2.errors.InternalError_:
         # The view is not yet ready to be queried
         return False
     return stream.getvalue() == expected
+
+
+def source_at_offset(
+    cursor: psycopg2.extensions.cursor, source_name: str, desired_offset: int
+) -> typing.Union[None, int]:
+    """Return the mz timestamp from a source if it has reached the desired offset."""
+    query = (
+        "SELECT timestamp FROM mz_source_info WHERE source_name = %s and offset = %s"
+    )
+    try:
+        cursor.execute(query, (source_name, desired_offset))
+        if cursor.rowcount > 1:
+            print("ERROR: More than one row returned when querying source offsets:")
+            for row in cursor:
+                print(f"\t{row}")
+            sys.exit(1)
+        if not cursor.rowcount:
+            return None
+
+        return int(cursor.fetchone()[0])
+    except psycopg2.errors.InternalError_:
+        # The view is not yet ready to be queried
+        return None
 
 
 def wait_for_materialize_views(args: argparse.Namespace) -> None:
@@ -61,6 +88,10 @@ def wait_for_materialize_views(args: argparse.Namespace) -> None:
         p.stem: p.read_text() for p in pathlib.Path(args.snapshot_dir).glob("*.sql")
     }
 
+    # Create a dictionary mapping view names to source name and offset
+    with open(os.path.join(args.snapshot_dir, "offsets.json")) as fd:
+        source_offsets = json.load(fd)
+
     with psycopg2.connect(f"postgresql://{args.host}:{args.port}/materialize") as conn:
         installed_views = set(view_names(conn))
 
@@ -69,6 +100,7 @@ def wait_for_materialize_views(args: argparse.Namespace) -> None:
     if not captured_views.issuperset(installed_views):
         missing_views = installed_views.difference(captured_views)
         print(f"ERROR: Missing final state for views: {missing_views}")
+        print(f"       Have: {captured_views}")
         sys.exit(1)
 
     print("Recording time required until each view matches its snapshot")
@@ -79,7 +111,14 @@ def wait_for_materialize_views(args: argparse.Namespace) -> None:
             views_to_remove = []
             for view in pending_views:
                 with conn.cursor() as cursor:
-                    if view_matches(cursor, view, view_snapshots[view]):
+
+                    desired_offset = source_offsets[view]["offset"]
+                    source_name = source_offsets[view]["topic"]
+                    timestamp = source_at_offset(cursor, source_name, desired_offset)
+                    if not timestamp:
+                        continue
+
+                    if view_matches(cursor, view, view_snapshots[view], timestamp):
                         time_taken = time.time() - start_time
                         print(f"{time_taken:>6.1f}s: {view}")
                         views_to_remove.append(view)
