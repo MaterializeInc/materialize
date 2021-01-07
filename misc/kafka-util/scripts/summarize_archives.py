6edddf886ee78d677275420e538a8eb155a87da5
Chris Golden <551285+cirego@users.noreply.github.com>
2021-01-07 10:48:28 -0800

    Create script to print a CSV of statistics about topic archives (#5206)
    Kafka topic archives are the binary data read from Kafka, encoded using
    Apache arrow on disk. This script reads the binary data, decodes the
    Avro encoded messages within the binary messages and prints them to
    stdout.

--- misc/kafka-util/scripts/summarize_archives.py
+++ misc/kafka-util/scripts/summarize_archives.py
@@ -9,15 +9,18 @@
 # the Business Source License, use of this software will be governed
 # by the Apache License, Version 2.0.
 
-# Script to read all messages in an arrow archive of a kafka topic and print the python objects to
-# stdout
+# Script to read all topic archives and produce a basic statistical summary of the messages
+# contained within
 
 import argparse
+import collections
+import functools
 import io
 import json
 import logging
-import pathlib
-import pprint
+import multiprocessing
+import glob
+import os
 import sys
 import typing
 
@@ -30,7 +33,7 @@ import pyarrow.fs  # type: ignore
 logging.basicConfig(
     format="%(asctime)s %(levelname)s %(name)s %(message)s", level=logging.INFO
 )
-log = logging.getLogger("print.records")
+log = logging.getLogger("summarize.archive")
 
 
 def decode(reader: avro.io.DatumReader, msg: bytes) -> typing.Any:
@@ -39,49 +42,42 @@ def decode(reader: avro.io.DatumReader, msg: bytes) -> typing.Any:
     return reader.read(decoder)
 
 
-def print_archive(args: argparse.Namespace) -> None:
-    """Print the decoded representation of messages in an archive file."""
-
-    # Why not 'printer = pprint.pprint if args.pretty_print else print'?
-    # Because mypy exits with 'error: Cannot call function of unknown type'
-    def printer(obj: typing.Any) -> None:
-        if args.pretty_print:
-            pprint.pprint(obj)
-        else:
-            print(obj)
-
-    topic = args.topic
+def summarize_archive(archive: str) -> None:
+    """Print interesting statistics about an archived Kafka topic."""
 
     with open("schemas.json") as fd:
         schemas = json.load(fd)
 
+    topic = os.path.splitext(archive)[0]
     try:
         key_schema = schemas[f"{topic}-key"]["schema"]
         value_schema = schemas[f"{topic}-value"]["schema"]
-        if args.print_schemas:
-            printer(json.loads(key_schema))
-            printer(json.loads(value_schema))
     except KeyError as e:
-        log.error("Failed to locate schema for topic: %s", e)
+        log.error("Failed to locate schema for topic %s", e)
         raise
 
     key_reader = avro.io.DatumReader(avro.schema.parse(key_schema))
     value_reader = avro.io.DatumReader(avro.schema.parse(value_schema))
 
-    arrow_file = pathlib.Path(f"{topic}.arrow")
-    if not arrow_file.exists():
-        log.error(f"No arrow file for topic {topic}")
-        sys.exit(1)
-
     local = pyarrow.fs.LocalFileSystem()
-    with local.open_input_file(f"{topic}.arrow") as f:
+    with local.open_input_file(archive) as f:
         with pyarrow.RecordBatchFileReader(f) as reader:
             table = reader.read_all()
 
-    for i in range(args.offset, min(table.num_rows, args.offset + args.count)):
+    num_messages = table.num_rows
+    key_bytes = 0
+    value_bytes = 0
+    num_ops: typing.DefaultDict[str, int] = collections.defaultdict(int)
+
+    for i in range(0, table.num_rows):
         key = table["key"][i].as_py()
         value = table["value"][i].as_py()
         timestamp = table["timestamp"][i].value
+
+        if key:
+            key_bytes += len(key)
+        if value:
+            value_bytes += len(value)
 
         # Strip the first 5 bytes, as they are added by Confluent Platform and are not part of the
         # actual serialized data
@@ -91,46 +87,48 @@ def print_archive(args: argparse.Namespace) -> None:
             "value": decode(value_reader, value[5:]),
         }
 
-        printer(decoded)
+        op = decoded["value"]["op"]
+        num_ops[op] += 1
+
+    print(
+        f"{topic},{num_messages},{key_bytes},{value_bytes},{num_ops['c']},{num_ops['u']},{num_ops['d']}"
+    )
+
+
+def summarize_archives(args: argparse.Namespace) -> None:
+    """Print interesting statistics about archived Kafka topics."""
+
+    topic_archives = glob.glob(f"{args.topic_filter}.arrow")
+    if not topic_archives:
+        log.error("No archives matching filter %s", args.topic_filter)
+        sys.exit(1)
+
+    print("Topic,NumMessages,KeyBytes,ValueBytes,NumCreates,NumUpdates,NumDeletes")
+    with multiprocessing.Pool(args.num_procs) as pool:
+        pool.map(summarize_archive, topic_archives)
 
 
 def main() -> None:
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        "-c",
-        "--count",
-        help="Number of messages to print",
+        "-n",
+        "--num-procs",
+        help="Number of parallel processes to spawn",
         type=int,
-        default=float("inf"),
+        default=multiprocessing.cpu_count(),
     )
 
     parser.add_argument(
-        "-o",
-        "--offset",
-        help="Starting offset to print messages from",
-        type=int,
-        default=0,
+        "-t",
+        "--topic-filter",
+        help="Only restore messagges from topics that match filter string",
+        type=str,
+        default="debezium.tpcch.*",
     )
-
-    parser.add_argument(
-        "-p",
-        "--pretty-print",
-        help="Whether or not to print messages over multiple-lines using a pretty printer",
-        action="store_true",
-    )
-
-    parser.add_argument(
-        "-s",
-        "--print-schemas",
-        help="Whether or not to print schema definitions",
-        action="store_true",
-    )
-
-    parser.add_argument("topic", help="Name of the topic", type=str)
 
     args = parser.parse_args()
-    print_archive(args)
+    summarize_archives(args)
 
 
 if __name__ == "__main__":
