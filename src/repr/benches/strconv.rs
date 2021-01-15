fade91dc46b5c86b689cf94def61bf117c04c2b6
Nikhil Benesch <nikhil.benesch@gmail.com>
2021-01-15 17:40:27 -0500

    repr: don't allocate while parsing floats (#5341)
    parse_float32/-3.0      time:   [41.721 ns 41.940 ns 42.170 ns]
                            change: [-55.287% -55.053% -54.764%] (p = 0.00 < 0.05)
                            Performance has improved.
    
    parse_float32/9.7       time:   [41.411 ns 41.671 ns 41.918 ns]
                            change: [-49.983% -49.783% -49.552%] (p = 0.00 < 0.05)
                            Performance has improved.
    
    parse_float32/NaN       time:   [25.068 ns 25.253 ns 25.447 ns]
                            change: [-60.892% -60.692% -60.434%] (p = 0.00 < 0.05)
                            Performance has improved.
    
    parse_float32/inFiNiTy  time:   [16.827 ns 16.840 ns 16.852 ns]
                            change: [-86.343% -86.325% -86.307%] (p = 0.00 < 0.05)
                            Performance has improved.

--- src/repr/benches/strconv.rs
+++ src/repr/benches/strconv.rs
@@ -7,12 +7,25 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
 
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 use rand::rngs::StdRng;
 use rand::seq::SliceRandom;
 use rand::{Rng, SeedableRng};
 
 use repr::strconv;
+
+fn bench_parse_float32(c: &mut Criterion) {
+    for s in &["-3.0", "9.7", "NaN", "inFiNiTy"] {
+        c.bench_with_input(BenchmarkId::new("parse_float32", s), s, |b, s| {
+            b.iter(|| strconv::parse_float32(s).unwrap())
+        });
+    }
+
+    let input = include_str!("testdata/twitter.json");
+    c.bench_function("parse_jsonb", |b| {
+        b.iter(|| black_box(strconv::parse_jsonb(input).unwrap()))
+    });
+}
 
 fn bench_parse_jsonb(c: &mut Criterion) {
     let input = include_str!("testdata/twitter.json");
@@ -73,6 +86,7 @@ criterion_group!(
     benches,
     bench_format_list_simple,
     bench_format_list_nested,
+    bench_parse_float32,
     bench_parse_jsonb
 );
 criterion_main!(benches);
