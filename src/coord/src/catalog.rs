d4c85d3e998117e67af26aaedfdae3f6160fc939
Sean Loiselle <sean@materialize.io>
2021-01-18 21:56:14 -0500

    coord: add mz_procs table for functions

--- src/coord/src/catalog.rs
+++ src/coord/src/catalog.rs
@@ -634,7 +634,7 @@ impl Catalog {
 
                 Builtin::Func(func) => {
                     let oid = catalog.allocate_oid()?;
-                    let _ = catalog.insert_item(
+                    events.push(catalog.insert_item(
                         func.id,
                         oid,
                         name.clone(),
@@ -642,7 +642,7 @@ impl Catalog {
                             plan_cx: PlanContext::default(),
                             inner: func.inner,
                         }),
-                    );
+                    ));
                 }
 
                 _ => (),
@@ -840,6 +840,11 @@ impl Catalog {
     }
 
     pub fn get_by_id(&self, id: &GlobalId) -> &CatalogEntry {
+        &self.by_id[id]
+    }
+
+    pub fn get_by_oid(&self, oid: &u32) -> &CatalogEntry {
+        let id = &self.by_oid[oid];
         &self.by_id[id]
     }
 
