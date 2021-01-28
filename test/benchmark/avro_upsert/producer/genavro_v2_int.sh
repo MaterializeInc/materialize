7c2e499764cc1a6cee6d85ca51f88432330941dd
Chris Golden <chris@materialize.io>
2021-01-28 10:09:21 -0800

    Use larger dataset and fixup compaction window size
    Also, disable default binding to host ports for services and write a
    Python version of the data generator script.

--- test/benchmark/avro_upsert/producer/genavro_v2_int.sh
+++ test/benchmark/avro_upsert/producer/genavro_v2_int.sh
@@ -10,23 +10,25 @@
 
 # Generates about 3M unique values with 100 + 32000 (for bookId plus SecurityId)
 
-for _ in {1..1000000000} ## 1B rows
-do
-bookID=$((1+RANDOM % 100))
-SecurityID=$((1+RANDOM % 32000))
-Exposure1=$((SecurityID+bookID+100000+RANDOM))
-Exposure2=$((Exposure1+RANDOM))
-Exposure3=$((Exposure1+RANDOM))
-Exposure4=$((Exposure1+RANDOM))
-#bookID=`shuf -i 1-9 -n 1 --random-source=/dev/urandom`
-#SecurityID=$((bookID+1000))
-#SecurityID=`shuf -i 1-1000 -n 1 --random-source=/dev/urandom`
-#Exposure1=`shuf -i 1-100000 -n 1 --random-source=/dev/urandom`
-#Exposure2=`shuf -i 1-100000 -n 1 --random-source=/dev/urandom`
-#Exposure3=`shuf -i 1-100000 -n 1 --random-source=/dev/urandom`
-#Exposure4=`shuf -i 1-100000 -n 1 --random-source=/dev/urandom`
+set -euo pipefail
 
-#echo $bookID
-#echo $SecurityID
-echo "$bookID"'-'"$SecurityID"':'"{\"BookId\": $bookID, \"SecurityId\": $SecurityID, \"Exposure\": {\"Current\": {\"Long2\": {\"Exposure\": $Exposure1 }, \"Short2\": {\"Exposure\": $Exposure2}}, \"Target\": {\"Long\": {\"Exposure\": $Exposure3}, \"Short\": {\"Exposure\": $Exposure4}}}}"
+for _ in {1..400000000} ## 400M rows
+do
+    bookID=$((1+RANDOM % 100))
+    SecurityID=$((1+RANDOM % 32000))
+    Exposure1=$((SecurityID+bookID+100000+RANDOM))
+    Exposure2=$((Exposure1+RANDOM))
+    Exposure3=$((Exposure1+RANDOM))
+    Exposure4=$((Exposure1+RANDOM))
+    #bookID=`shuf -i 1-9 -n 1 --random-source=/dev/urandom`
+    #SecurityID=$((bookID+1000))
+    #SecurityID=`shuf -i 1-1000 -n 1 --random-source=/dev/urandom`
+    #Exposure1=`shuf -i 1-100000 -n 1 --random-source=/dev/urandom`
+    #Exposure2=`shuf -i 1-100000 -n 1 --random-source=/dev/urandom`
+    #Exposure3=`shuf -i 1-100000 -n 1 --random-source=/dev/urandom`
+    #Exposure4=`shuf -i 1-100000 -n 1 --random-source=/dev/urandom`
+
+    #echo $bookID
+    #echo $SecurityID
+    echo "$bookID"'-'"$SecurityID"':'"{\"BookId\": $bookID, \"SecurityId\": $SecurityID, \"Exposure\": {\"Current\": {\"Long2\": {\"Exposure\": $Exposure1 }, \"Short2\": {\"Exposure\": $Exposure2}}, \"Target\": {\"Long\": {\"Exposure\": $Exposure3}, \"Short\": {\"Exposure\": $Exposure4}}}}"
 done
