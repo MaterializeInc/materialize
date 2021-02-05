5bb919f8981ace904e5383368d07989c1a697200
Brennan Vincent <brennan@materialize.io>
2021-02-05 14:46:33 -0500

    blahblah

--- src/sql/src/plan/query.rs
+++ src/sql/src/plan/query.rs
@@ -3156,6 +3156,10 @@ impl<'a> QueryContext<'a> {
 
                 Ok((expr, scope))
             }
+            Id::BareSource(_) | Id::LocalBareSource => {
+                // These are never introduced except when planning source transformations.
+                unreachable!()
+            }
         }
     }
 
