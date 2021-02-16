57e920dc9ce5b4173a058e44c8302c99d1f941eb
Andi Wang <andi@materialize.io>
2021-02-16 14:47:30 -0500

    Switch to using arrangementless upsert exclusively.

--- src/dataflow/src/render/upsert.rs
+++ src/dataflow/src/render/upsert.rs
@@ -13,85 +13,21 @@ use differential_dataflow::hashable::Hashable;
 use differential_dataflow::lattice::Lattice;
 
 use timely::dataflow::channels::pact::{Exchange, Pipeline};
-use timely::dataflow::operators::generic::{operator, Operator};
-use timely::dataflow::operators::map::Map;
+use timely::dataflow::operators::generic::Operator;
 use timely::dataflow::{Scope, Stream};
 use timely::progress::Antichain;
 
-use dataflow_types::{DataEncoding, DataflowError, LinearOperator};
 use log::error;
-use repr::{Datum, Diff, RelationType, Row, RowArena, Timestamp};
+use repr::{Diff, Row, Timestamp};
 
-use crate::decode::{decode_upsert, DecoderState};
-use crate::operator::StreamExt;
+use crate::decode::DecoderState;
 use crate::source::{SourceData, SourceOutput};
 
-/// Entrypoint to the upsert-specific transformations involved
-/// in rendering a stream that came from an upsert source.
-/// Upsert-specific operators are different from the rest of
-/// the rendering pipeline in that their input is a stream
-/// with two components instead of one, and the second component
-/// can be null or empty.
-#[allow(clippy::too_many_arguments)]
-pub fn pre_arrange_from_upsert_transforms<G>(
-    stream: &Stream<G, SourceOutput<Vec<u8>, Vec<u8>>>,
-    encoding: DataEncoding,
-    key_encoding: DataEncoding,
-    debug_name: &str,
-    worker_index: usize,
-    as_of_frontier: Antichain<Timestamp>,
-    linear_operator: &mut Option<LinearOperator>,
-    src_type: &RelationType,
-) -> (
-    Stream<G, (Row, Option<Row>, Timestamp)>,
-    Stream<G, DataflowError>,
-)
-where
-    G: Scope<Timestamp = Timestamp>,
-{
-    // Currently, the upsert-specific transformations run in the
-    // following order:
-    // 1. as_of
-    // 2. deduplicating records by key
-    // 3. decoding records
-    // 4. applying linear operator, which currently consist of
-    //     a. filter
-    //     b. project
-    //     c. prepending the key to the value so that the stream becomes
-    //        of the format (key, <entire record>)
-    //
-    // We may want to consider switching the order of the transformations to
-    // optimize performance trade-offs. In the current order, by running
-    // deduplicating before decoding/linear operators, we're reducing compute at the
-    // cost of requiring more memory.
-    //
-    // In the future, we may want to have optimization hints that enable people
-    // to specify that they believe that they have a large number of unique
-    // keys, at which point materialize may be more performant if it runs
-    // decoding/linear operators before deduplicating.
-
-    // This operator changes the timestamp from capability to message payload,
-    // and applies `as_of` frontier compaction. The compaction is important as
-    // downstream upsert preparation can compact away updates for the same keys
-    // at the same times, and by advancing times we make more of them the same.
-    let stream = apply_as_of_frontier(&stream, as_of_frontier);
-
-    // Deduplicate records by key
-    let deduplicated = prepare_upsert_by_max_offset(&stream);
-
-    // Decode
-    let decoded = decode_upsert(
-        &deduplicated,
-        encoding,
-        key_encoding,
-        debug_name,
-        worker_index,
-    );
-
-    apply_linear_operators(&decoded, linear_operator, src_type)
-}
-
-pub fn apply_as_of_frontier<G>(
+/// This operator changes the timestamp from capability to message payload,
+/// and applies `as_of` frontier compaction. The compaction is important as
+/// downstream upsert preparation can compact away updates for the same keys
+/// at the same times, and by advancing times we make more of them the same.
+fn apply_as_of_frontier<G>(
     stream: &Stream<G, SourceOutput<Vec<u8>, Vec<u8>>>,
     as_of_frontier: Antichain<Timestamp>,
 ) -> Stream<G, (SourceOutput<Vec<u8>, Vec<u8>>, Timestamp)>
@@ -113,134 +49,51 @@ where
     })
 }
 
-/// Produces at most one entry for each `(key, time)` pair.
-///
-/// The incoming stream of `(key, (val, off), time)` records may have many
-/// entries with the same `key` and `time`. We are able to reduce this to
-/// at most one record for each pair, by retaining only the record with the
-/// greatest offset: its action summarizes the sequence of many actions that
-/// occur at the same moment and so are not distinguishable.
-fn prepare_upsert_by_max_offset<G>(
-    stream: &Stream<G, (SourceOutput<Vec<u8>, Vec<u8>>, Timestamp)>,
-) -> Stream<G, ((Vec<u8>, SourceData), Timestamp)>
-where
-    G: Scope<Timestamp = Timestamp>,
-{
-    stream.unary_frontier(
-        Exchange::new(move |x: &(SourceOutput<Vec<u8>, Vec<u8>>, Timestamp)| x.0.key.hashed()),
-        "UpsertCompaction",
-        |_cap, _info| {
-            // this is a map of (time) -> ((key) -> (value with max offset))
-            let mut values = HashMap::<_, HashMap<_, SourceData>>::new();
-            let mut vector = Vec::new();
-
-            move |input, output| {
-                // Digest each input, reduce by presented timestamp.
-                input.for_each(|cap, data| {
-                    data.swap(&mut vector);
-                    for (
-                        SourceOutput {
-                            key,
-                            value: new_value,
-                            position: new_position,
-                            upstream_time_millis: new_upstream_time_millis,
-                        },
-                        time,
-                    ) in vector.drain(..)
-                    {
-                        let entry = values
-                            .entry(cap.delayed(&time))
-                            .or_insert_with(HashMap::new)
-                            .entry(key)
-                            .or_insert_with(Default::default);
-
-                        if let Some(new_offset) = new_position {
-                            if let Some(offset) = entry.position {
-                                if offset < new_offset {
-                                    *entry = SourceData {
-                                        value: new_value,
-                                        position: new_position,
-                                        upstream_time_millis: new_upstream_time_millis,
-                                    };
-                                }
-                            } else {
-                                *entry = SourceData {
-                                    value: new_value,
-                                    position: new_position,
-                                    upstream_time_millis: new_upstream_time_millis,
-                                };
-                            }
-                        }
-                    }
-                });
-
-                // Produce (key, val) pairs at any complete times.
-                for (cap, map) in values.iter_mut() {
-                    if !input.frontier.less_equal(cap.time()) {
-                        let mut session = output.session(cap);
-                        for (key, val) in map.drain() {
-                            session.give(((key, val), cap.time().clone()))
-                        }
-                    }
-                }
-                // Discard entries, capabilities for complete times.
-                values.retain(|_cap, map| !map.is_empty());
-            }
-        },
-    )
-}
-
-/// Apply a filter followed by a project to an upsert stream.
-/// Also, prepend key columns to the beginning of the value so
-/// the return stream is `Stream<G, (key, Option<entire record>, Timestamp)>
-/// whereas the input stream is `Stream<G, (key, Option<value>, Timestamp)>
-fn apply_linear_operators<G>(
-    stream: &Stream<G, (Row, Option<Row>, Timestamp)>,
-    _linear_operator: &mut Option<LinearOperator>,
-    _src_type: &RelationType,
-) -> (
-    Stream<G, (Row, Option<Row>, Timestamp)>,
-    Stream<G, DataflowError>,
-)
-where
-    G: Scope<Timestamp = Timestamp>,
-{
-    let mut row_packer = repr::RowPacker::new();
-
-    // just prepend the key to the value without unpacking rows
-    let prepended = stream.map({
-        move |(key, value, timestamp)| {
-            if let Some(value) = value {
-                row_packer.extend_by_row(&key);
-                row_packer.extend_by_row(&value);
-                (key, Some(row_packer.finish_and_reuse()), timestamp)
-            } else {
-                (key, None, timestamp)
-            }
-        }
-    });
-    let scope = &prepended.scope();
-    (prepended, operator::empty(scope))
-}
-
-pub fn no_arrangement_upsert<G, K, V>(
-    stream: &Stream<G, (SourceOutput<Vec<u8>, Vec<u8>>, Timestamp)>,
-    mut key_decoder_state: K,
-    mut value_decoder_state: V,
+/// Entrypoint to the upsert-specific transformations involved
+/// in rendering a stream that came from an upsert source.
+/// Upsert-specific operators are different from the rest of
+/// the rendering pipeline in that their input is a stream
+/// with two components instead of one, and the second component
+/// can be null or empty.
+pub fn decode_stream<G>(
+    stream: &Stream<G, SourceOutput<Vec<u8>, Vec<u8>>>,
+    as_of_frontier: Antichain<Timestamp>,
+    mut key_decoder_state: Box<dyn DecoderState>,
+    mut value_decoder_state: Box<dyn DecoderState>,
 ) -> Stream<G, (Row, Timestamp, Diff)>
 where
     G: Scope<Timestamp = Timestamp>,
-    K: DecoderState + 'static,
-    V: DecoderState + 'static,
 {
-    stream.unary_frontier(
+    // Currently, the upsert-specific transformations run in the
+    // following order:
+    // 1. as_of
+    // 2. deduplicating records by key
+    // 3. decoding records
+    // 4. prepending the key to the value so that the stream becomes
+    //        of the format (key, <entire record>)
+    // We may want to consider switching the order of the transformations to
+    // optimize performance trade-offs. In the current order, by running
+    // deduplicating before decoding/linear operators, we're reducing compute at the
+    // cost of requiring more memory.
+    //
+    // In the future, we may want to have optimization hints that enable people
+    // to specify that they believe that they have a large number of unique
+    // keys, at which point materialize may be more performant if it runs
+    // decoding/linear operators before deduplicating.
+    (&apply_as_of_frontier(stream, as_of_frontier)).unary_frontier(
         Exchange::new(move |x: &(SourceOutput<Vec<u8>, Vec<u8>>, Timestamp)| x.0.key.hashed()),
-        "LightUpsert",
+        "Upsert",
         |_cap, _info| {
-            // this is a map of (time) -> (capability, ((key) -> (value with max offset)))
+            // this is a map of (time) -> (capability, ((key) -> (value with max
+            // offset))) This is a BTreeMap because we want to ensure that if we
+            // receive (key1, value1, time 5) and (key1, value2, time 7) that we
+            // send (key1, value1, time 5) before (key1, value2, time 7)
             let mut to_send = BTreeMap::<_, (_, HashMap<_, SourceData>)>::new();
-            // this is a map of (decoded key) -> (decoded value)
+            // this is a map of (decoded key) -> (decoded_value). We store the
+            // latest value for a given key that way we know what to retract if
+            // a new value with the same key comes along
             let mut current_values = HashMap::new();
+
             let mut vector = Vec::new();
             let mut row_packer = repr::RowPacker::new();
 
@@ -319,16 +172,19 @@ where
                                         Err(())
                                     };
                                     if let Ok(decoded_value) = decoded_value {
-                                        // TODO: apply linear operator
+                                        // TODO: add linear operators such as
+                                        // filters and projects?
                                         let old_value = if let Some(new_value) = &decoded_value {
                                             current_values.insert(decoded_key, new_value.clone())
                                         } else {
                                             current_values.remove(&decoded_key)
                                         };
                                         if let Some(old_value) = old_value {
+                                            // retract old value
                                             session.give((old_value, cap.time().clone(), -1));
                                         }
                                         if let Some(new_value) = decoded_value {
+                                            // give new value
                                             session.give((new_value, cap.time().clone(), 1));
                                         }
                                     }
@@ -339,6 +195,10 @@ where
                             }
                         }
                     } else {
+                        // because this is a BTreeMap, the rest of the times in
+                        // the map will be greater than this time. So if the
+                        // input_frontier is less than or equal to this time,
+                        // it will be less than the times in the rest of the map
                         break;
                     }
                 }
