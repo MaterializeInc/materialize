954417e48239874da292bd7b614ad1978c9e200a
Andi Wang <5522362+wangandi@users.noreply.github.com>
2021-04-02 11:12:58 -0400

    Optimization: Reduce the set of predicates by replacing subexpressions (#6196)
    Reduce away repeats of the same predicate.
    
    Co-authored-by: Philip Stoev <pstoev@materialize.io>

--- src/expr/src/relation/canonicalize.rs
+++ src/expr/src/relation/canonicalize.rs
@@ -10,8 +10,8 @@
 //! Utility functions to transform parts of a single `MirRelationExpr`
 //! into canonical form.
 
-use crate::{BinaryFunc, MirScalarExpr};
-use repr::RelationType;
+use crate::{BinaryFunc, MirScalarExpr, UnaryFunc};
+use repr::{Datum, RelationType, ScalarType};
 
 /// Canonicalize equivalence classes of a join.
 ///
@@ -61,8 +61,10 @@ pub fn canonicalize_equivalences(equivalences: &mut Vec<Vec<MirScalarExpr>>) {
 ///
 /// This function reduces and canonicalizes the structure of each individual
 /// predicate. Then, it transforms predicates of the form "A and B" into two: "A"
-/// and "B". Afterwards, it sorts and deduplicates the predicates.
+/// and "B". Aftewards, it reduces predicates based on information from other
+/// predicates in the set. Finally, it sorts and deduplicates the predicates.
 pub fn canonicalize_predicates(predicates: &mut Vec<MirScalarExpr>, input_type: &RelationType) {
+    // 1) Reduce each individual predicate.
     let mut pending_predicates = predicates
         .drain(..)
         .map(|mut p| {
@@ -70,6 +72,8 @@ pub fn canonicalize_predicates(predicates: &mut Vec<MirScalarExpr>, input_type: 
             return p;
         })
         .collect::<Vec<MirScalarExpr>>();
+
+    // 2) Split "A and B" into two predicates: "A" and "B"
     while let Some(expr) = pending_predicates.pop() {
         if let MirScalarExpr::CallBinary {
             func: BinaryFunc::And,
@@ -83,6 +87,148 @@ pub fn canonicalize_predicates(predicates: &mut Vec<MirScalarExpr>, input_type: 
             predicates.push(expr);
         }
     }
+
+    // 3) Reduce across `predicates`.
+    // If a predicate `p` cannot be null, and `f(p)` is a nullable bool
+    // then the predicate `p & f(p)` is equal to `p & f(true)`, and
+    // `!p & f(p)` is equal to `!p & f(false)`. For any index i, the `Vec` of
+    // predicates `[p1, ... pi, ... pn]` is equivalent to the single predicate
+    // `pi & (p1 & ... & p(i-1) & p(i+1) ... & pn)`. Thus, if `pi`
+    // (resp. `!pi`) cannot be null, it is valid to replace with `true` (resp.
+    // `false`) every subexpression in `(p1 & ... & p(i-1) & p(i+1) ... & pn)`
+    // that is equal to `pi`.
+
+    // If `p` is null and `q` is a nullable bool, then `p & q` can be either
+    // `null` or `false` depending on what `q`. Our rendering pipeline treats
+    // both as "remove this row." Thus, in the specific context of filter
+    // predicates, it is acceptable to make the aforementioned substitution
+    // even if `pi` can be null.
+
+    // Note that this does some dedupping of predicates since if `p1 = p2`
+    // then this reduction process will replace `p1` with true.
+
+    // Maintain respectively:
+    // 1) A list of predicates for which we have checked for matching
+    // subexpressions
+    // 2) A list of predicates for which we have yet to do so.
+    let mut completed = Vec::new();
+    let mut todo = Vec::new();
+    // Seed `todo` with all predicates.
+    std::mem::swap(&mut todo, predicates);
+
+    while let Some(predicate_to_apply) = todo.pop() {
+        // Helper method: for each predicate `p`, see if all other predicates
+        // (a.k.a. the union of todo & completed) contains `p` as a
+        // subexpression, and replace the subexpression accordingly.
+        // This method lives inside the loop because in order to comply with
+        // Rust rules that only one mutable reference to `todo` can be held at a
+        // time.
+        let mut replace_subexpr_other_predicates =
+            |expr: &MirScalarExpr, constant_bool: &MirScalarExpr| {
+                // Do not replace subexpressions equal to `expr` if `expr` is a
+                // literal to avoid infinite looping.
+                if !expr.is_literal() {
+                    for other_predicate in todo.iter_mut() {
+                        replace_subexpr_and_reduce(
+                            other_predicate,
+                            expr,
+                            constant_bool,
+                            input_type,
+                        );
+                    }
+                    for other_idx in (0..completed.len()).rev() {
+                        if replace_subexpr_and_reduce(
+                            &mut completed[other_idx],
+                            expr,
+                            constant_bool,
+                            input_type,
+                        ) {
+                            // If a predicate in the `completed` list has
+                            // been simplified, stick it back into the `todo` list.
+                            todo.push(completed.remove(other_idx));
+                        }
+                    }
+                }
+            };
+        // Meat of loop starts here. If a predicate p is of the form `!q`, replace
+        // every instance of `q` in every other predicate with `false.`
+        // Otherwise, replace every instance of `p` in every other predicate
+        // with `true`.
+        if let MirScalarExpr::CallUnary {
+            func: UnaryFunc::Not,
+            expr,
+        } = &predicate_to_apply
+        {
+            replace_subexpr_other_predicates(
+                expr,
+                &MirScalarExpr::literal_ok(Datum::False, ScalarType::Bool),
+            )
+        } else {
+            replace_subexpr_other_predicates(
+                &predicate_to_apply,
+                &MirScalarExpr::literal_ok(Datum::True, ScalarType::Bool),
+            );
+        }
+        completed.push(predicate_to_apply);
+    }
+    // Remove any predicates that have been reduced to "true"
+    completed.retain(|p| !p.is_literal_true());
+    *predicates = completed;
+
+    // 4) Sort and dedup predicates.
     predicates.sort();
     predicates.dedup();
 }
+
+/* #region helper functions for substituting a bool literal for a matching subexpression */
+
+/// Replaces a subexpression if it matches `replace_if_equal_to`, and record that
+/// the expression has changed.
+fn replace(
+    subexpr: &mut MirScalarExpr,
+    replace_if_equal_to: &MirScalarExpr,
+    replace_with: &MirScalarExpr,
+    changed: &mut bool,
+) {
+    if subexpr == replace_if_equal_to {
+        *subexpr = replace_with.clone();
+        *changed = true;
+    }
+}
+
+// TODO: Have a common method in `MirScalarExpr` for this and similar logic.
+/// Similar to what `MirScalarExpr::visit_mut_pre` would be like, but the
+/// `cond` of an if statement is not visited to avoid `then` or `els` to be
+/// evaluated before `cond`, resulting in a correctness error.
+fn visit_mut_pre_skip_cond<F>(e: &mut MirScalarExpr, f: &mut F)
+where
+    F: FnMut(&mut MirScalarExpr),
+{
+    f(e);
+    if let MirScalarExpr::If { cond: _, then, els } = e {
+        visit_mut_pre_skip_cond(then, f);
+        visit_mut_pre_skip_cond(els, f);
+    } else {
+        e.visit1_mut(|e2| visit_mut_pre_skip_cond(e2, f));
+    }
+}
+
+/// Replace any matching subexpressions in a predicate, and if the predicate has
+/// changed, reduce it.
+fn replace_subexpr_and_reduce(
+    predicate: &mut MirScalarExpr,
+    replace_if_equal_to: &MirScalarExpr,
+    replace_with: &MirScalarExpr,
+    input_type: &RelationType,
+) -> bool {
+    let mut changed = false;
+    visit_mut_pre_skip_cond(predicate, &mut |e| {
+        replace(e, replace_if_equal_to, replace_with, &mut changed)
+    });
+    if changed {
+        predicate.reduce(input_type);
+    }
+    changed
+}
+
+/* #endregion */
