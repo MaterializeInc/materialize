69b1ea163d02ea5cfadfb29741e83dad4ad18355
Frank McSherry <fmcsherry@me.com>
2021-04-05 16:23:20 -0400

    reformat

--- src/dataflow/src/render/flat_map.rs
+++ src/dataflow/src/render/flat_map.rs
@@ -49,89 +49,88 @@ where
                 .collect::<Vec<_>>();
 
             let (ok_collection, err_collection) = self.collection(input).unwrap();
-            let (oks, errs) =
-                ok_collection.inner.flat_map_fallible({
-                    let mut datums = DatumVec::new();
-                    let mut row_packer = repr::Row::default();
-                    move |(input_row, time, diff)| {
-                        let temp_storage = RowArena::new();
-                        // Unpack datums and capture its length (to rewind MFP eval).
-                        let mut datums_local = datums.borrow_with(&input_row);
-                        let datums_len = datums_local.len();
-                        let exprs = exprs
-                            .iter()
-                            .map(|e| e.eval(&datums_local, &temp_storage))
-                            .collect::<Result<Vec<_>, _>>();
-                        let exprs = match exprs {
-                            Ok(exprs) => exprs,
-                            Err(e) => return vec![(Err((e.into(), time, diff)))],
-                        };
-                        let output_rows = func.eval(exprs, &temp_storage);
-                        // Blank out entries in `datum` here, for simplicity later on.
-                        for index in 0..datums_len {
-                            if replace[index] {
-                                datums_local[index] = Datum::Dummy;
-                            }
+            let (oks, errs) = ok_collection.inner.flat_map_fallible({
+                let mut datums = DatumVec::new();
+                let mut row_packer = repr::Row::default();
+                move |(input_row, time, diff)| {
+                    let temp_storage = RowArena::new();
+                    // Unpack datums and capture its length (to rewind MFP eval).
+                    let mut datums_local = datums.borrow_with(&input_row);
+                    let datums_len = datums_local.len();
+                    let exprs = exprs
+                        .iter()
+                        .map(|e| e.eval(&datums_local, &temp_storage))
+                        .collect::<Result<Vec<_>, _>>();
+                    let exprs = match exprs {
+                        Ok(exprs) => exprs,
+                        Err(e) => return vec![(Err((e.into(), time, diff)))],
+                    };
+                    let output_rows = func.eval(exprs, &temp_storage);
+                    // Blank out entries in `datum` here, for simplicity later on.
+                    for index in 0..datums_len {
+                        if replace[index] {
+                            datums_local[index] = Datum::Dummy;
                         }
-                        // Declare borrows outside the closure so that appropriately lifetimed
-                        // borrows are moved in and used by `mfp.evaluate`.
-                        let map_filter_project = &map_filter_project;
-                        let row_packer = &mut row_packer;
-                        let temp_storage = &temp_storage;
-                        let replace = &replace;
-                        if let Some(mfp) = map_filter_project {
-                            output_rows
-                                .iter()
-                                .flat_map(move |(output_row, r)| {
-                                    // Remove any additional columns added in prior evaluation.
-                                    datums_local.truncate(datums_len);
-                                    // Extend datums with additional columns, replace some with dummy values.
-                                    datums_local.extend(output_row.iter());
-                                    for index in datums_len..datums_local.len() {
-                                        if replace[index] {
-                                            datums_local[index] = Datum::Dummy;
-                                        }
+                    }
+                    // Declare borrows outside the closure so that appropriately lifetimed
+                    // borrows are moved in and used by `mfp.evaluate`.
+                    let map_filter_project = &map_filter_project;
+                    let row_packer = &mut row_packer;
+                    let temp_storage = &temp_storage;
+                    let replace = &replace;
+                    if let Some(mfp) = map_filter_project {
+                        output_rows
+                            .iter()
+                            .flat_map(move |(output_row, r)| {
+                                // Remove any additional columns added in prior evaluation.
+                                datums_local.truncate(datums_len);
+                                // Extend datums with additional columns, replace some with dummy values.
+                                datums_local.extend(output_row.iter());
+                                for index in datums_len..datums_local.len() {
+                                    if replace[index] {
+                                        datums_local[index] = Datum::Dummy;
                                     }
-                                    mfp.evaluate(&mut datums_local, temp_storage, time, diff * *r)
-                                        .map(|x| {
-                                            (x.map_err(|(e, t, r)| (DataflowError::from(e), t, r)))
-                                        })
-                                })
-                                .collect::<Vec<_>>()
-                        } else {
-                            output_rows
-                                .iter()
-                                .map(move |(output_row, r)| {
-                                    Ok((
-                                        {
-                                            row_packer.extend(
-                                                datums_local
-                                                    .iter()
-                                                    .cloned()
-                                                    .chain(output_row.iter())
-                                                    .zip(replace.iter())
-                                                    .map(|(datum, replace)| {
+                                }
+                                mfp.evaluate(&mut datums_local, temp_storage, time, diff * *r)
+                                    .map(|x| x.map_err(|(e, t, r)| (DataflowError::from(e), t, r)))
+                            })
+                            .collect::<Vec<_>>()
+                    } else {
+                        output_rows
+                            .iter()
+                            .map(move |(output_row, r)| {
+                                Ok((
+                                    {
+                                        row_packer.extend(
+                                            datums_local
+                                                .iter()
+                                                .cloned()
+                                                .chain(output_row.iter())
+                                                .zip(replace.iter())
+                                                .map(
+                                                    |(datum, replace)| {
                                                         if *replace {
                                                             Datum::Dummy
                                                         } else {
                                                             datum
                                                         }
-                                                    }),
-                                            );
-                                            row_packer.finish_and_reuse()
-                                        },
-                                        time,
-                                        diff * *r,
-                                    ))
-                                })
-                                .collect::<Vec<_>>()
-                        }
-                        // The collection avoids the lifetime issues of the `datums` borrow,
-                        // which allows us to avoid multiple unpackings of `input_row`. We
-                        // could avoid this allocation with a custom iterator that understands
-                        // the borrowing, but it probably isn't the leading order issue here.
+                                                    },
+                                                ),
+                                        );
+                                        row_packer.finish_and_reuse()
+                                    },
+                                    time,
+                                    diff * *r,
+                                ))
+                            })
+                            .collect::<Vec<_>>()
                     }
-                });
+                    // The collection avoids the lifetime issues of the `datums` borrow,
+                    // which allows us to avoid multiple unpackings of `input_row`. We
+                    // could avoid this allocation with a custom iterator that understands
+                    // the borrowing, but it probably isn't the leading order issue here.
+                }
+            });
 
             use differential_dataflow::AsCollection;
             let ok_collection = oks.as_collection();
