a4baa3a640a9add0186f4ad72a0edcbe04ce3435
Nikhil Benesch <nikhil.benesch@gmail.com>
2021-04-13 18:28:36 -0400

    ore: add OptionExt::display_or[_else]
    This combinator makes it easy to efficiently display the contents of an
    option or a default if the option is `None`. Supersedes #6002.
    
    Closes #6002.

--- src/ore/src/option.rs
+++ src/ore/src/option.rs
@@ -14,7 +14,10 @@
 
 //! Option utilities.
 
+use std::fmt;
 use std::ops::Deref;
+
+use either::Either;
 
 /// Extension methods for [`std::option::Option`].
 pub trait OptionExt<T> {
@@ -29,6 +32,50 @@ pub trait OptionExt<T> {
     where
         T: Deref,
         T::Target: ToOwned;
+
+    /// Returns a type that displays the option's value if it is present, or
+    /// the provided default otherwise.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use ore::option::OptionExt;
+    ///
+    /// fn render(number: Option<i32>) -> String {
+    ///     format!("Your lucky number is {}.", number.display_or("unknown"))
+    /// }
+    ///
+    /// assert_eq!(render(Some(42)), "Your lucky number is 42.");
+    /// assert_eq!(render(None), "Your lucky number is unknown.");
+    /// ```
+    fn display_or<D>(self, default: D) -> Either<T, D>
+    where
+        T: fmt::Display,
+        D: fmt::Display;
+
+    /// Like [`OptionExt::display_or`], but the default value is computed
+    /// only if the option is `None`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use ore::option::OptionExt;
+    ///
+    /// fn render(number: Option<i32>, guess: i32) -> String {
+    ///     format!(
+    ///         "Your lucky number is {}.",
+    ///         number.display_or_else(|| format!("unknown (best guess: {})", guess)),
+    ///     )
+    /// }
+    ///
+    /// assert_eq!(render(Some(42), 7), "Your lucky number is 42.");
+    /// assert_eq!(render(None, 7), "Your lucky number is unknown (best guess: 7).");
+    /// ```
+    fn display_or_else<D, R>(self, default: D) -> Either<T, R>
+    where
+        T: fmt::Display,
+        D: FnOnce() -> R,
+        R: fmt::Display;
 }
 
 impl<T> OptionExt<T> for Option<T> {
@@ -38,5 +85,28 @@ impl<T> OptionExt<T> for Option<T> {
         T::Target: ToOwned,
     {
         self.as_ref().map(|x| x.deref().to_owned())
+    }
+
+    fn display_or<D>(self, default: D) -> Either<T, D>
+    where
+        T: fmt::Display,
+        D: fmt::Display,
+    {
+        match self {
+            Some(t) => Either::Left(t),
+            None => Either::Right(default),
+        }
+    }
+
+    fn display_or_else<D, R>(self, default: D) -> Either<T, R>
+    where
+        T: fmt::Display,
+        D: FnOnce() -> R,
+        R: fmt::Display,
+    {
+        match self {
+            Some(t) => Either::Left(t),
+            None => Either::Right(default()),
+        }
     }
 }
