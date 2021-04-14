dbd9c344e6e4898f38a605f50af671fc3a93bd74
Petros Angelatos <petrosagg@gmail.com>
2021-04-14 12:41:50 +0200

    pubnub: add reconnection logic
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>

--- src/dataflow/src/source/pubnub.rs
+++ src/dataflow/src/source/pubnub.rs
@@ -7,9 +7,11 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
 
+use std::time::Duration;
+
 use async_trait::async_trait;
 use futures::StreamExt;
-use pubnub_hyper::core::data::message::Type;
+use pubnub_hyper::core::data::{channel, message::Type};
 use pubnub_hyper::{Builder, DefaultRuntime, DefaultTransport};
 
 use crate::source::{SimpleSource, SourceError, Timestamper};
@@ -43,24 +45,36 @@ impl SimpleSource for PubNubSourceReader {
             .runtime(DefaultRuntime)
             .build();
 
-        let channel = self.connector.channel.parse().unwrap();
+        let channel = self.connector.channel;
+        let channel: channel::Name = channel.parse().or_else(|_| {
+            Err(SourceError::FileIO(format!(
+                "invalid pubnub channel: {}",
+                channel
+            )))
+        })?;
 
-        let stream = pubnub.subscribe(channel).await;
-        tokio::pin!(stream);
+        loop {
+            let stream = pubnub.subscribe(channel.clone()).await;
+            tokio::pin!(stream);
 
-        while let Some(msg) = stream.next().await {
-            if msg.message_type == Type::Publish {
-                let s = msg.json.dump();
+            while let Some(msg) = stream.next().await {
+                if msg.message_type == Type::Publish {
+                    let s = msg.json.dump();
 
-                let row = Row::pack_slice(&[Datum::String(&s)]);
+                    let row = Row::pack_slice(&[Datum::String(&s)]);
 
-                timestamper
-                    .insert(row)
-                    .await
-                    .map_err(|e| SourceError::FileIO(e.to_string()))?;
+                    timestamper
+                        .insert(row)
+                        .await
+                        .map_err(|e| SourceError::FileIO(e.to_string()))?;
+                }
             }
-        }
 
-        Ok(())
+            log::info!(
+                "pubnub channel {:?} disconnected. reconnecting",
+                channel.to_string()
+            );
+            tokio::time::sleep(Duration::from_secs(5)).await;
+        }
     }
 }
