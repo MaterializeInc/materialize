dd2325c0dd3be0204317cd9482cf2ecca321fa14
Andrés Senac <andres@senac.es>
2021-04-15 16:42:01 +0200

    Extend union fusion transformation (#6474)
    When a union branch contains a negated union, push the negate to each
    branch of the nested union and merge them into the parent union.

--- src/transform/src/fusion/union.rs
+++ src/transform/src/fusion/union.rs
@@ -8,6 +8,9 @@
 // by the Apache License, Version 2.0.
 
 //! Fuses multiple `Union` operators into one.
+//!
+//! Nested negated unions are merged into the parent one by pushing
+//! the Negate to all their branches.
 
 use std::iter;
 
@@ -33,21 +36,44 @@ impl crate::Transform for Union {
 
 impl Union {
     /// Fuses multiple `Union` operators into one.
+    /// Nested negated unions are merged into the parent one by pushing
+    /// the Negate to all their branches.
     pub fn action(&self, relation: &mut MirRelationExpr) {
         let relation_type = relation.typ();
         if let MirRelationExpr::Union { base, inputs } = relation {
-            let can_fuse = iter::once(&**base)
-                .chain(&*inputs)
-                .any(|input| matches!(input, MirRelationExpr::Union { .. }));
+            let can_fuse = iter::once(&**base).chain(&*inputs).any(|input| -> bool {
+                match input {
+                    MirRelationExpr::Union { .. } => true,
+                    MirRelationExpr::Negate { input: inner_input } => {
+                        if let MirRelationExpr::Union { .. } = **inner_input {
+                            true
+                        } else {
+                            false
+                        }
+                    }
+                    _ => false,
+                }
+            });
             if can_fuse {
                 let mut new_inputs: Vec<MirRelationExpr> = vec![];
                 for input in iter::once(&mut **base).chain(inputs) {
-                    match input.take_dangerous() {
+                    let outer_input = input.take_dangerous();
+                    match outer_input {
                         MirRelationExpr::Union { base, inputs } => {
                             new_inputs.push(*base);
                             new_inputs.extend(inputs);
                         }
-                        input => new_inputs.push(input),
+                        MirRelationExpr::Negate {
+                            input: ref inner_input,
+                        } => {
+                            if let MirRelationExpr::Union { base, inputs } = &**inner_input {
+                                new_inputs.push(base.to_owned().negate());
+                                new_inputs.extend(inputs.into_iter().map(|x| x.clone().negate()));
+                            } else {
+                                new_inputs.push(outer_input);
+                            }
+                        }
+                        _ => new_inputs.push(outer_input),
                     }
                 }
                 *relation = MirRelationExpr::union_many(new_inputs, relation_type);
