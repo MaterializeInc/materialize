48ea156b0f2b05e6e362abdce969026965b72d0b
Nikhil Benesch <nikhil.benesch@gmail.com>
2021-05-04 14:58:29 -0400

    mzcompose: hardcode help message
    As mentioned in my review of #6515, I think improving the mzcompose help
    message is a great idea, but I think we can provide better help if we
    just hardcode our optimal message as a string.
    
    The upside to this approach is that we don't need to fight argparse to
    get our desired usage string. (E.g., we don't really want to document
    the --file option just to say you can't use it.) This commit also adds
    docs for the mzcompose-specific commands like "list-compositions" and
    "web".

--- misc/python/materialize/cli/mzcompose.py
+++ misc/python/materialize/cli/mzcompose.py
@@ -38,13 +38,6 @@ def main(argv: List[str]) -> int:
     args, unknown_args = parser.parse_known_args(argv)
     if args.help:
         parser.print_help()
-        print()
-        print(
-            "Additionally, mzcompose is a thin wrapper around docker-compose,\n"
-            "All of the following arguments will be forwarded:"
-        )
-        print()
-        subprocess.run(["docker-compose", "--help"])
         return 0
 
     if args.file:
@@ -235,22 +228,17 @@ def web(composition: mzcompose.Composition, service: Optional[str]) -> int:
 class ArgumentParser(argparse.ArgumentParser):
     def __init__(self) -> None:
         super().__init__(add_help=False)
+        # If you add an option here, add it to the help text in `print_help`.
         self.add_argument("--mz-quiet", action="store_true", default=None)
-        self.add_argument("--mz-find", help="Find a composition by directory name")
+        self.add_argument("--mz-find")
         self.add_argument(
             "--mz-build-mode", default="release", choices=["dev", "release"]
         )
-        self.add_argument("-f", "--file", help="Explicitly disallowed, use --mz-find")
+        self.add_argument("-f", "--file")
         self.add_argument("--project-directory")
-        self.add_argument(
-            "command",
-            nargs="?",
-            help="All docker-compose commands, plus: web, lint, list-compositions",
-        )
-        # arguments are required to be distinguished to make spawning work well, but are not
-        # particularly useful in the help text, so provide them in a more generic way
-        self.add_argument("first_command_arg", nargs="?", metavar="ARG...")
-        self.add_argument("remainder", nargs=argparse.REMAINDER, metavar="ARG...")
+        self.add_argument("command", nargs="?")
+        self.add_argument("first_command_arg", nargs="?")
+        self.add_argument("remainder", nargs="*")
         self.add_argument("--help", action="store_true", default=False)
 
     def parse_known_args(
@@ -272,6 +260,46 @@ class ArgumentParser(argparse.ArgumentParser):
             return (pargs, unknown_args)
         except ValueError:
             return (ns, [])
+
+    def print_help(self, file: Optional[IO[str]] = None) -> None:
+        print(
+            """mzcompose orchestrates services defined in mzcompose.yml.
+It wraps Docker Compose to add some Materialize-specific features.
+
+Usage:
+  mzcompose [options] COMMAND [ARGS...]
+
+Options:
+  --mz-build-mode <dev|release>
+                     Specify the Cargo profile to use when compiling Rust
+                     crates (default: release)
+  --mz-find DIR      Use the mzcompose.yml file from DIR, rather than the
+                     current directory
+  --mz-quiet         Suppress Materialize-specific informational messages
+  --verbose          Show more output
+
+Commands:
+  build              Build or rebuild services
+  down               Stop and remove containers, networks, images, and volumes
+  gen-shortcuts      Generate shortcut `mzcompose` shell scripts in mzcompose directories
+  lint               Surface common errors in compositions
+  list-compositions  List the directories that contain compositions
+  list-ports SERVICE List any ports exposed by SERVICE
+  list-workflows     List the workflows in the composition
+  ps                 List containers
+  run                Run a one-off command or workflow
+  start              Start services
+  stop               Stop services
+  up                 Create and start containers
+  web SERVICE        Open SERVICE's URL in a web browser
+
+These are only the most common commands. There are additional Docker Compose
+commands that are also supported. Consult `docker-compose help` for the full
+set.
+
+For additional details on mzcompose, consult doc/developer/mzbuild.md.""",
+            file=file,
+        )
 
     def error(self, message: str) -> NoReturn:
         raise ValueError(message)
