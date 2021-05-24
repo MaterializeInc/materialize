41a1306d8f490118bdddcbfda025a05f9fb2ddc9
Brennan Vincent <brennan@materialize.io>
2021-05-24 15:26:36 -0400

    remove some dead code

--- src/interchange/src/avro.rs
+++ src/interchange/src/avro.rs
@@ -17,7 +17,7 @@ mod schema;
 
 pub use envelope_cdc_v2 as cdc_v2;
 
-pub use self::decode::{extract_row, Decoder, DiffPair};
+pub use self::decode::{Decoder, DiffPair};
 pub use self::encode::{
     column_names_and_types, encode_datums_as_avro, encode_debezium_transaction_unchecked,
     get_debezium_transaction_schema, Encoder,
