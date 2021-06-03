6b6b217c889591117a91bdbebee6eaea008afcfa
Andres Senac <asenac@materialize.com>
2021-06-03 14:33:56 +0200

    Use a builder struct

--- src/transform/src/fusion/join.rs
+++ src/transform/src/fusion/join.rs
@@ -50,133 +50,152 @@ impl Join {
         if let MirRelationExpr::Join {
             inputs,
             equivalences,
-            demand,
-            implementation,
+            ..
         } = relation
         {
-            let mut new_inputs = Vec::new();
-            let mut new_equivalences = Vec::new();
-            let mut new_columns = 0;
-            let mut new_filters = Vec::new();
+            let mut join_builder = JoinBuilder::new(equivalences);
 
             // We scan through each input, digesting any joins that we find and updating their equivalence classes.
             // We retain any existing equivalence classes, as they are already with respect to the cross product.
             for input in inputs.drain(..) {
-                if let MirRelationExpr::Join {
-                    mut inputs,
-                    mut equivalences,
-                    ..
-                } = input
-                {
-                    // Update and push all of the variables.
-                    for mut equivalence in equivalences.drain(..) {
-                        for expr in equivalence.iter_mut() {
-                            expr.visit_mut(&mut |e| {
-                                if let MirScalarExpr::Column(c) = e {
-                                    *c += new_columns;
-                                }
-                            });
-                        }
-                        new_equivalences.push(equivalence);
-                    }
-                    // Add all of the inputs.
-                    for input in inputs.drain(..) {
-                        new_columns += input.arity();
-                        new_inputs.push(input);
-                    }
-                } else if let MirRelationExpr::Filter {
-                    input,
-                    mut predicates,
-                } = input
-                {
-                    if let MirRelationExpr::Join {
-                        mut inputs,
-                        mut equivalences,
+                match input {
+                    MirRelationExpr::Join {
+                        inputs,
+                        equivalences,
                         ..
-                    } = *input
-                    {
-                        // Update and push all of the variables.
-                        for mut equivalence in equivalences.drain(..) {
-                            for expr in equivalence.iter_mut() {
-                                expr.visit_mut(&mut |e| {
-                                    if let MirScalarExpr::Column(c) = e {
-                                        *c += new_columns;
-                                    }
-                                });
-                            }
-                            new_equivalences.push(equivalence);
-                        }
-
-                        // Update and push all the filters
-                        for mut expr in predicates.drain(..) {
-                            expr.visit_mut(&mut |e| {
-                                if let MirScalarExpr::Column(c) = e {
-                                    *c += new_columns;
-                                }
-                            });
-                            new_filters.push(expr);
-                        }
-
-                        // Add all of the inputs.
-                        for input in inputs.drain(..) {
-                            new_columns += input.arity();
-                            new_inputs.push(input);
-                        }
-                    } else {
-                        let input = input.filter(predicates);
-                        new_columns += input.arity();
-                        new_inputs.push(input);
+                    } => {
+                        // Merge the inputs into the new join being built.
+                        join_builder.add_inputs(inputs, equivalences, None);
                     }
-                } else {
-                    // Retain the input.
-                    new_columns += input.arity();
-                    new_inputs.push(input);
+                    MirRelationExpr::Filter { input, predicates } => {
+                        if let MirRelationExpr::Join {
+                            inputs,
+                            equivalences,
+                            ..
+                        } = *input
+                        {
+                            // Merge the inputs and the predicates into the new join being built.
+                            join_builder.add_inputs(inputs, equivalences, Some(predicates));
+                        } else {
+                            // Retain the input.
+                            let input = input.filter(predicates);
+                            join_builder.add_input(input);
+                        }
+                    }
+                    _ => {
+                        // Retain the input.
+                        join_builder.add_input(input);
+                    }
                 }
             }
 
-            // Filter join identities out of the inputs.
-            // The join identity is a single 0-ary row constant expression.
-            new_inputs.retain(|input| {
-                if let MirRelationExpr::Constant {
-                    rows: Ok(rows),
-                    typ,
-                } = &input
-                {
-                    !(rows.len() == 1 && typ.column_types.len() == 0 && rows[0].1 == 1)
-                } else {
-                    true
-                }
-            });
+            *relation = join_builder.build();
+        }
+    }
+}
 
-            new_equivalences.extend(equivalences.drain(..));
-            expr::canonicalize::canonicalize_equivalences(&mut new_equivalences);
+/// Helper builder for fusing the inputs of nested joins into a single Join expression.
+struct JoinBuilder {
+    inputs: Vec<MirRelationExpr>,
+    equivalences: Vec<Vec<MirScalarExpr>>,
+    num_columns: usize,
+    /// Predicates that will be evaluated on top of the join, if any.
+    predicates: Vec<MirScalarExpr>,
+}
 
-            *inputs = new_inputs;
-            *equivalences = new_equivalences;
-            *demand = None;
-            *implementation = expr::JoinImplementation::Unimplemented;
+impl JoinBuilder {
+    fn new(equivalences: &mut Vec<Vec<MirScalarExpr>>) -> Self {
+        Self {
+            inputs: Vec::new(),
+            equivalences: equivalences.drain(..).collect(),
+            num_columns: 0,
+            predicates: Vec::new(),
+        }
+    }
 
-            // If `inputs` is now empty or a singleton (without constraints),
-            // we can remove the join.
-            match inputs.len() {
-                0 => {
-                    // The identity for join is the collection containing a single 0-ary row.
-                    *relation = MirRelationExpr::constant(vec![vec![]], RelationType::empty());
-                }
-                1 => {
-                    // if there are constraints, they probably should have
-                    // been pushed down by predicate pushdown, but .. let's
-                    // not re-write that code here.
-                    if equivalences.is_empty() {
-                        *relation = inputs.pop().unwrap();
+    fn add_input(&mut self, input: MirRelationExpr) {
+        self.num_columns += input.arity();
+        self.inputs.push(input);
+    }
+
+    fn add_inputs<I>(
+        &mut self,
+        inputs: I,
+        mut equivalences: Vec<Vec<MirScalarExpr>>,
+        predicates: Option<Vec<MirScalarExpr>>,
+    ) where
+        I: IntoIterator<Item = MirRelationExpr>,
+    {
+        // Update and push all of the variables.
+        for mut equivalence in equivalences.drain(..) {
+            for expr in equivalence.iter_mut() {
+                expr.visit_mut(&mut |e| {
+                    if let MirScalarExpr::Column(c) = e {
+                        *c += self.num_columns;
                     }
-                }
-                _ => {}
+                });
             }
+            self.equivalences.push(equivalence);
+        }
 
-            if !new_filters.is_empty() {
-                *relation = relation.take_dangerous().filter(new_filters);
+        if let Some(mut predicates) = predicates {
+            for mut expr in predicates.drain(..) {
+                expr.visit_mut(&mut |e| {
+                    if let MirScalarExpr::Column(c) = e {
+                        *c += self.num_columns;
+                    }
+                });
+                self.predicates.push(expr);
             }
         }
+
+        // Add all of the inputs.
+        for input in inputs {
+            self.add_input(input);
+        }
+    }
+
+    fn build(mut self) -> MirRelationExpr {
+        // Filter join identities out of the inputs.
+        // The join identity is a single 0-ary row constant expression.
+        self.inputs.retain(|input| {
+            if let MirRelationExpr::Constant {
+                rows: Ok(rows),
+                typ,
+            } = &input
+            {
+                !(rows.len() == 1 && typ.column_types.len() == 0 && rows[0].1 == 1)
+            } else {
+                true
+            }
+        });
+
+        expr::canonicalize::canonicalize_equivalences(&mut self.equivalences);
+
+        // If `inputs` is now empty or a singleton (without constraints),
+        // we can remove the join.
+        let mut join = match self.inputs.len() {
+            0 => {
+                // The identity for join is the collection containing a single 0-ary row.
+                MirRelationExpr::constant(vec![vec![]], RelationType::empty())
+            }
+            1 if self.equivalences.is_empty() => {
+                // if there are constraints, they probably should have
+                // been pushed down by predicate pushdown, but .. let's
+                // not re-write that code here.
+                self.inputs.pop().unwrap()
+            }
+            _ => MirRelationExpr::Join {
+                inputs: self.inputs,
+                equivalences: self.equivalences,
+                demand: None,
+                implementation: expr::JoinImplementation::Unimplemented,
+            },
+        };
+
+        if !self.predicates.is_empty() {
+            join = join.filter(self.predicates);
+        }
+        join
     }
 }
