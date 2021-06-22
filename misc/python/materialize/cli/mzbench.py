57a8b0b5879b7d4d39540a2e765bb58e5c23de1b
Brennan Vincent <brennan@materialize.io>
2021-06-22 15:25:50 -0400

    fix typo in comment (#7123)

--- misc/python/materialize/cli/mzbench.py
+++ misc/python/materialize/cli/mzbench.py
@@ -7,7 +7,7 @@
 # the Business Source License, use of this software will be governed
 # by the Apache License, Version 2.0.
 #
-# mzbuild.py -- script to run materialized benchmarks
+# mzbench.py -- script to run materialized benchmarks
 
 import argparse
 import csv
