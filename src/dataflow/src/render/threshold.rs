c6f4bcf83220d67c95915547c1be8b067ef7347e
Moritz Hoffmann <mh@materialize.com>
2021-07-06 10:04:16 +0200

    dataflow/threshold: De-duplicate reduction
    Extract the reduction code for all arrangement flavors into a common
    function.
    
    Signed-off-by: Moritz Hoffmann <mh@materialize.com>

--- src/dataflow/src/render/threshold.rs
+++ src/dataflow/src/render/threshold.rs
@@ -8,13 +8,14 @@
 // by the Apache License, Version 2.0.
 
 use differential_dataflow::lattice::Lattice;
+use differential_dataflow::operators::arrange::{Arranged, TraceAgent};
 use differential_dataflow::operators::reduce::ReduceCore;
 use differential_dataflow::operators::Consolidate;
 use differential_dataflow::trace::implementations::ord::OrdValSpine;
 use timely::dataflow::Scope;
 use timely::progress::{timestamp::Refines, Timestamp};
 
-use repr::Row;
+use repr::{Diff, Row};
 
 use crate::render::context::CollectionBundle;
 use crate::render::context::{ArrangementFlavor, Context};
@@ -30,6 +31,24 @@ where
         mut input: CollectionBundle<G, Row, T>,
         arity: usize,
     ) -> CollectionBundle<G, Row, T> {
+        fn threshold_arrangement<G, T, R>(
+            arrangement: &R,
+        ) -> Arranged<G, TraceAgent<OrdValSpine<Row, Row, G::Timestamp, Diff>>>
+        where
+            G: Scope,
+            G::Timestamp: Lattice + Refines<T>,
+            T: Timestamp + Lattice,
+            R: ReduceCore<G, Row, Row, Diff>,
+        {
+            arrangement.reduce_abelian("Threshold", move |_k, s, t| {
+                for (record, count) in s.iter() {
+                    if *count < 0 {
+                        t.push(((*record).clone(), *count));
+                    }
+                }
+            })
+        }
+
         // Arrange the input by all columns in order.
         // Different trace variants require different implementations because their
         // types are different, but the logic is identical.
@@ -38,33 +57,14 @@ where
             all_columns.push(expr::MirScalarExpr::Column(column));
         }
         input = input.ensure_arrangements(Some(all_columns.clone()));
-        let (oks, negatives, errs) = match input
+        let arrangement = input
             .arrangement(&all_columns)
-            .expect("Arrangement ensured to exist")
-        {
-            ArrangementFlavor::Local(oks, errs) => (
-                oks.as_collection(|k, _| k.clone()),
-                oks.reduce_abelian::<_, OrdValSpine<_, _, _, _>>("Threshold", move |_k, s, t| {
-                    for (record, count) in s.iter() {
-                        if *count < 0 {
-                            t.push(((*record).clone(), *count));
-                        }
-                    }
-                }),
-                errs.as_collection(|k, _| k.clone()),
-            ),
-            ArrangementFlavor::Trace(_, oks, errs) => (
-                oks.as_collection(|k, _| k.clone()),
-                oks.reduce_abelian::<_, OrdValSpine<_, _, _, _>>("Threshold", move |_k, s, t| {
-                    for (record, count) in s.iter() {
-                        if *count < 0 {
-                            t.push(((*record).clone(), *count));
-                        }
-                    }
-                }),
-                errs.as_collection(|k, _| k.clone()),
-            ),
+            .expect("Arrangement ensured to exist");
+        let negatives = match &arrangement {
+            ArrangementFlavor::Local(oks, _) => threshold_arrangement(oks),
+            ArrangementFlavor::Trace(_, oks, _) => threshold_arrangement(oks),
         };
+        let (oks, errs) = arrangement.as_collection();
         let oks = negatives
             .as_collection(|k, _| k.clone())
             .negate()
