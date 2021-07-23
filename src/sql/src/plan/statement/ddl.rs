28edf4af4dc6a2efb25776bb1ab8d7e4ad9edb37
Aljoscha Krettek <aljoscha.krettek@gmail.com>
2021-07-23 16:49:10 +0200

    sinks,kafka: make consistency_topic optional, pick default when reuse_topic is set
    This removes the config from most tests but introduces one test that
    verifies that we can set a custom topic.
    
    We only remove the config from tests where reuse_topic=true, because
    only in that case do we pick a default consistency topic. Other tests,
    that only had consistency_topic='some-topic' still need the explicit
    config.

--- src/sql/src/plan/statement/ddl.rs
+++ src/sql/src/plan/statement/ddl.rs
@@ -22,7 +22,7 @@ use anyhow::{anyhow, bail};
 use aws_arn::ARN;
 use globset::GlobBuilder;
 use itertools::Itertools;
-use log::error;
+use log::{debug, error};
 use regex::Regex;
 use reqwest::Url;
 
@@ -1261,6 +1261,23 @@ fn kafka_sink_builder(
         Some(_) => bail!("consistency_topic must be a string"),
     };
 
+    let reuse_topic = match with_options.remove("reuse_topic") {
+        Some(Value::Boolean(b)) => b,
+        None => false,
+        Some(_) => bail!("reuse_topic must be a boolean"),
+    };
+
+    let consistency_topic = if reuse_topic && consistency_topic.is_none() {
+        let default_consistency_topic = format!("{}-consistency", topic_prefix);
+        debug!(
+            "Using default consistency topic '{}' for topic '{}'",
+            default_consistency_topic, topic_prefix
+        );
+        Some(default_consistency_topic)
+    } else {
+        consistency_topic
+    };
+
     let config_options = kafka_util::extract_config(with_options)?;
 
     let format = match format {
@@ -1307,16 +1324,6 @@ fn kafka_sink_builder(
     };
 
     let broker_addrs = broker.parse()?;
-
-    let reuse_topic = match with_options.remove("reuse_topic") {
-        Some(Value::Boolean(b)) => b,
-        None => false,
-        Some(_) => bail!("reuse_topic must be a boolean"),
-    };
-
-    if reuse_topic && consistency_topic.is_none() {
-        bail!("reuse_topic requires a consistency topic");
-    }
 
     let transitive_source_dependencies: Vec<_> = if reuse_topic {
         for item in root_dependencies.iter() {
