e1bb7d9331b6c853b177377bd10319a20a8d5954
Brandon W Maister <bwm@materialize.io>
2021-08-09 18:01:35 -0400

    upgrade: Remove the shared volumes between different version workflows
    With this change "upgrade from v0.7.3" will not have the catalog that was leftover
    after the "test loading catalog from materialized-current" workflow ran at the
    end of the "upgrade from v0.6.1" tests.
    
    `docker-compose rm --volumes <container>*` only removes unnamed volumes, so it
    was not doing anything about the existing `mzdata` directory.
    
    The upgrade test framework was implicitly depending on every `check-from*` test
    dropping items that cannot be parsed in any earlier version of materialized --
    which are specifically the things that upgrade is most likely to test. Based on
    the fact that the upgrade documentation doesn't state that you absolutely must
    drop backwards-incompatible statements, I believe that this commit gets us
    closer to the intended implementation. We could just update the docs, but a
    genuine clean slate for each `upgrade-from` workflow seems easier to reason
    about than a possibly populated one.

--- misc/python/materialize/mzcompose.py
+++ misc/python/materialize/mzcompose.py
@@ -33,6 +33,7 @@ from typing import (
     Any,
     Callable,
     Collection,
+    Container,
     Dict,
     Iterable,
     List,
@@ -359,6 +360,30 @@ class Composition:
             stdout=subprocess.PIPE if capture else 1,
             stderr=subprocess.PIPE if capture else 2,
         )
+
+    def inspect_volumes(self, volumes: Container[str]) -> Iterable[Dict[str, Any]]:
+        """
+        Return the JSON from `docker inspect` for each volume with the given compose name
+
+        There is no explicit documentation of the structure of the returned
+        fields, but you can see them in the docker core repo:
+        https://github.com/moby/moby/blob/91dc595e9648318/api/types/types.go#L345-L379
+        """
+        actual_volumes = spawn.capture(
+            ["docker", "volume", "ls", "--format", "{{ .Name }}"], unicode=True
+        ).splitlines()
+        metadata = spawn.capture(
+            ["docker", "inspect", "-f", "{{json .}}", *actual_volumes]
+        )
+        for line in metadata.splitlines():
+            info = json.loads(line)
+            # Labels may be present and null or not present
+            labels = info.get("Labels")
+            if (
+                labels is not None
+                and labels.get("com.docker.compose.volume") in volumes
+            ):
+                yield info
 
     def find_host_ports(self, service: str) -> List[str]:
         """Find all ports open on the host for a given service"""
@@ -703,6 +728,33 @@ class RemoveServicesStep(WorkflowStep):
         except subprocess.CalledProcessError:
             services = ", ".join(self._services)
             raise errors.Failed(f"ERROR: services didn't restart cleanly: {services}")
+
+
+@Steps.register("remove-volumes")
+class RemoveVolumesStep(WorkflowStep):
+    """
+    Params:
+      volumes: List of volume names
+    """
+
+    def __init__(self, *, volumes: List[str]) -> None:
+        self._volumes = volumes
+        if not isinstance(self._volumes, list):
+            raise errors.BadSpec(f"volumes should be a list, got: {self._volumes}")
+
+    def run(self, workflow: Workflow) -> None:
+        volumes = [
+            v["Name"] for v in workflow.composition.inspect_volumes(self._volumes)
+        ]
+        if len(volumes) != len(self._volumes):
+            raise errors.Failed(
+                f"Unable to find correct number of volumes, found: {volumes}"
+            )
+        try:
+            spawn.runv(["docker", "volume", "rm", *volumes])
+        except subprocess.CalledProcessError:
+            msg = ", ".join(self._volumes)
+            raise errors.Failed(f"ERROR: unable to remove volumes: {msg}")
 
 
 @Steps.register("wait-for-postgres")
