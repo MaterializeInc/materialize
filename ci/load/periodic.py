c059289f6698e51e53f3328241a9b977c41cea8a
Brennan Vincent <brennan@materialize.com>
2021-08-09 09:10:03 -0400

    copyright header

--- ci/load/periodic.py
+++ ci/load/periodic.py
@@ -1,3 +1,12 @@
+# Copyright Materialize, Inc. and contributors. All rights reserved.
+#
+# Use of this software is governed by the Business Source License
+# included in the LICENSE file at the root of this repository.
+#
+# As of the Change Date specified in that file, in accordance with
+# the Business Source License, use of this software will be governed
+# by the Apache License, Version 2.0.
+
 import datetime
 
 from materialize.cli.scratch import DEFAULT_INSTPROF_NAME, DEFAULT_SG_ID, DEFAULT_SUBNET_ID
