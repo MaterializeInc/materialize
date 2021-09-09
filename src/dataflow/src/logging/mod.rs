d84c03a135d5d47967ee3709cd2c273577a79a28
Moritz Hoffmann <mh@materialize.com>
2021-09-09 14:46:34 +0200

    dataflow/logging: Implement a consolidating output buffer
    The ConsolidateBuffer implements a wrapper around OutputHandles
    consolidating the elements pushed to it. It tries to amortize work
    by only consolidating while half the buffer is cleared.
    
    Signed-off-by: Moritz Hoffmann <mh@materialize.com>

--- src/dataflow/src/logging/mod.rs
+++ src/dataflow/src/logging/mod.rs
@@ -14,10 +14,22 @@ pub mod materialized;
 pub mod reachability;
 pub mod timely;
 
+use std::time::Duration;
+
+use ::timely::communication::Push;
+use ::timely::dataflow::channels::{Bundle, Message};
 use ::timely::dataflow::operators::capture::{Event, EventPusher};
+use ::timely::dataflow::operators::generic::OutputHandle;
+use ::timely::dataflow::operators::Capability;
+use ::timely::dataflow::operators::CapabilityRef;
+use ::timely::progress::Timestamp as TimelyTimestamp;
+use differential_dataflow::consolidation::consolidate_updates;
+use differential_dataflow::difference::Semigroup;
+use differential_dataflow::lattice::Lattice;
+use differential_dataflow::ExchangeData;
+
 use dataflow_types::logging::{DifferentialLog, LogVariant, MaterializedLog, TimelyLog};
 use repr::Timestamp;
-use std::time::Duration;
 
 /// Logs events as a timely stream, with progress statements.
 pub struct BatchLogger<T, E, P>
@@ -107,5 +119,91 @@ where
     fn drop(&mut self) {
         self.event_pusher
             .push(Event::Progress(vec![(self.time_ms as Timestamp, -1)]));
+    }
+}
+
+/// A buffer that consolidates updates
+///
+/// The buffer is filled with updates until it reaches its capacity. At this point, the updates are
+/// consolidated to free up space. This process repeats until the consolidation recovered less than
+/// half of the buffer's capacity, at which point the buffer will be filled, once more consolidated
+/// and shipped.
+///
+/// The buffer retains a capability to send data on flush. It will flush all data once dropped, if
+/// time changes, or if the buffer capacity is reached.
+pub struct ConsolidateBuffer<'a, T, D: ExchangeData, R: Semigroup, P>
+where
+    P: Push<Bundle<T, (D, T, R)>> + 'a,
+    T: Clone + Lattice + Ord + TimelyTimestamp + 'a,
+    D: 'a,
+{
+    // a buffer for records, to send at self.cap
+    // Invariant: Buffer only contains data if cap is Some.
+    buffer: Vec<(D, T, R)>,
+    output_handle: OutputHandle<'a, T, (D, T, R), P>,
+    cap: Option<Capability<T>>,
+    clean: usize,
+    port: usize,
+}
+
+impl<'a, T, D: ExchangeData, R: Semigroup, P> ConsolidateBuffer<'a, T, D, R, P>
+where
+    T: Clone + Lattice + Ord + TimelyTimestamp + 'a,
+    P: Push<Bundle<T, (D, T, R)>> + 'a,
+{
+    /// Create a new [ConsolidateBuffer], wrapping the provided session.
+    ///
+    /// * `output_handle`: The output to send data to.
+    /// * 'port': The output port to retain capabilities for.
+    pub fn new(output_handle: OutputHandle<'a, T, (D, T, R), P>, port: usize) -> Self {
+        Self {
+            output_handle,
+            port,
+            cap: None,
+            buffer: Vec::with_capacity(Message::<T, (D, T, R)>::default_length()),
+            clean: 0,
+        }
+    }
+
+    /// Give an element to the buffer
+    pub fn give(&mut self, cap: &CapabilityRef<T>, data: (D, T, R)) {
+        // Retain a cap for the current time, which will be used on flush.
+        if self.cap.as_ref().map_or(true, |t| t.time() != cap.time()) {
+            // Flush on capability change
+            self.flush();
+            // Retain capability for the specified output port.
+            self.cap = Some(cap.delayed_for_output(cap.time(), self.port));
+        }
+        self.buffer.push(data);
+        if self.buffer.len() == self.buffer.capacity() {
+            // Consolidate while the consolidation frees at least half the buffer
+            if self.clean < self.buffer.capacity() / 2 {
+                consolidate_updates(&mut self.buffer);
+                self.clean = self.buffer.len();
+            } else {
+                // Consolidate once more and flush
+                consolidate_updates(&mut self.buffer);
+                self.flush();
+            }
+        }
+    }
+
+    /// Flush the internal buffer to the underlying session
+    pub fn flush(&mut self) {
+        if let Some(cap) = &self.cap {
+            self.output_handle.session(cap).give_vec(&mut self.buffer);
+        }
+        self.clean = 0;
+    }
+}
+
+impl<'a, T, D: ExchangeData, R: Semigroup, P> Drop for ConsolidateBuffer<'a, T, D, R, P>
+where
+    P: Push<Bundle<T, (D, T, R)>> + 'a,
+    T: Clone + Lattice + Ord + TimelyTimestamp + 'a,
+    D: 'a,
+{
+    fn drop(&mut self) {
+        self.flush();
     }
 }
