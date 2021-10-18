53c42a6fe041ad934cd02a5a4c37449f615b0d5f
Karsten Jeschkies <k@jeschkies.xyz>
2021-10-18 17:48:27 +0200

    Stream Loki entries.

--- src/dataflow/src/source/loki.rs
+++ src/dataflow/src/source/loki.rs
@@ -1,29 +1,34 @@
+use futures::future;
+use futures::stream::StreamExt;
 use serde::Deserialize;
-use std::time::{Duration, UNIX_EPOCH, SystemTime};
+use std::{collections::HashMap, time::{Duration, UNIX_EPOCH, SystemTime}};
+use tokio_stream::wrappers::IntervalStream;
 
 pub struct LokiSourceReader {
 
 }
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 struct QueryResult {
     status: String,
     data: Data,
 }
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 #[serde(tag = "resultType", content = "result")]
 enum Data {
     #[serde(rename = "streams")]
     Streams(Vec<Stream>),
 }
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 struct Stream {
+    #[serde(rename = "stream")]
+    labels: HashMap<String, String>,
     values: Vec<LogEntry>,
 }
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 struct LogEntry {
     ts: String,
     line: String,
@@ -37,22 +42,48 @@ mod test {
     #[tokio::test]
     async fn connect() {
 
-        let start = SystemTime::now() - Duration::new(60 * 60 * 6, 0);
+        let start = SystemTime::now() - Duration::from_secs(60 * 60);
 
-        let client = reqwest::Client::new();
-        let resp = client.get(format!("{}/loki/api/v1/query_range", endpoint))
-            .basic_auth(user, Some(pw))
-            .query(&[("query", "{job=\"systemd-journal\"}")])
-            .query(&[("start", format!("{}", start.duration_since(UNIX_EPOCH).unwrap().as_nanos()))])
-            .query(&[("direction", "forward")])
-            .send()
-            .await.unwrap();
+        async fn query(user: &str, pw: &str, endpoint: &str, start: u128, end: u128) -> reqwest::Response {
+            let client = reqwest::Client::new();
+            client.get(format!("{}/loki/api/v1/query_range", endpoint))
+                .basic_auth(user, Some(pw))
+                .query(&[("query", "{job=\"systemd-journal\"}")])
+                .query(&[("start", format!("{}", start))])
+                .query(&[("end", format!("{}", end))])
+                .query(&[("direction", "forward")])
+                .send()
+                .await.unwrap()
+        }
 
+        let resp = query(
+            user, pw, endpoint,
+            start.duration_since(UNIX_EPOCH).unwrap().as_nanos(),
+            (start.duration_since(UNIX_EPOCH).unwrap() + Duration::from_secs(60 * 5)).as_nanos()).await;
         println!("Status: {}", resp.status());
         //println!("Body: {}", resp.text().await.unwrap());
 
-        let parsed = resp.json::<QueryResult>().await.unwrap();
-        let Data::Streams(s) = parsed.data;
-        println!("{}: {}", s[0].values[0].ts, s[0].values[0].line);
+        //let parsed = resp.json::<QueryResult>().await.unwrap();
+        //let Data::Streams(s) = parsed.data;
+        //println!("{}: {}", s[0].values[0].ts, s[0].values[0].line);
+
+        let window = Duration::from_secs(60);
+        let polls = IntervalStream::new(tokio::time::interval(window));
+        let fut = polls.take(5).then(|_tick| async move {
+            let start = SystemTime::now() - window;
+            let end = SystemTime::now();
+
+            let resp = query(
+                user, pw, endpoint,
+                start.duration_since(UNIX_EPOCH).unwrap().as_nanos(),
+                end.duration_since(UNIX_EPOCH).unwrap().as_nanos()).await;
+
+            let parsed = resp.json::<QueryResult>().await.unwrap();
+            parsed.data
+        });
+
+        fut.for_each(|data| async move {
+            println!("{:?}", data);
+        }).await;
     }
 }
