753f8c5f12083f8878a148c49c636e41d2fae01c
Petros Angelatos <petrosagg@gmail.com>
2021-11-05 13:36:24 +0100

    ore: remove unused functions
    We don't use the Lottery or MaybeFuture abstractions anywhere so remove
    them
    
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>

--- src/ore/src/iter.rs
+++ src/ore/src/iter.rs
@@ -15,7 +15,6 @@
 
 //! Iterator utilities.
 
-use std::cmp::Ordering;
 use std::iter::{self, Chain, Once};
 
 /// Extension methods for iterators.
@@ -23,80 +22,6 @@ pub trait IteratorExt
 where
     Self: Iterator + Sized,
 {
-    /// Checks if the elements of this iterator are sorted.
-    ///
-    /// That is, for each element `a` and its following element `b`, `a <= b`
-    /// must hold. If the iterator yields exactly zero or one element, `true` is
-    /// returned.
-    ///
-    /// Note that if `Self::Item` is only `PartialOrd`, but not `Ord`, the above
-    /// definition implies that this function returns `false` if any two
-    /// consecutive items are not comparable.
-    ///
-    /// **Note:** this is a Materialize forward-port of a forthcoming feature
-    /// to the standard library.
-    /// See [rust-lang/rust#53485](https://github.com/rust-lang/rust/issues/53485).
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use ore::iter::IteratorExt;
-    /// assert!([1, 2, 2, 9].iter().mz_is_sorted());
-    /// assert!(![1, 3, 2, 4].iter().mz_is_sorted());
-    /// assert!([0].iter().mz_is_sorted());
-    /// assert!(std::iter::empty::<i32>().mz_is_sorted());
-    /// assert!(![0.0, 1.0, f32::NAN].iter().mz_is_sorted());
-    /// ```
-    fn mz_is_sorted(self) -> bool
-    where
-        Self: Sized,
-        Self::Item: PartialOrd,
-    {
-        self.mz_is_sorted_by(PartialOrd::partial_cmp)
-    }
-
-    /// Checks if the elements of this iterator are sorted using the given comparator function.
-    ///
-    /// Instead of using `PartialOrd::partial_cmp`, this function uses the given `compare`
-    /// function to determine the ordering of two elements. Apart from that, it's equivalent to
-    /// [`Iterator::is_sorted`]; see its documentation for more information.
-    ///
-    /// **Note:** this is a Materialize forward-port of a forthcoming feature
-    /// to the standard library.
-    /// See [rust-lang/rust#53485](https://github.com/rust-lang/rust/issues/53485).
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use ore::iter::IteratorExt;
-    /// assert!([1, 2, 2, 9].iter().mz_is_sorted_by(|a, b| a.partial_cmp(b)));
-    /// assert!(![1, 3, 2, 4].iter().mz_is_sorted_by(|a, b| a.partial_cmp(b)));
-    /// assert!([0].iter().mz_is_sorted_by(|a, b| a.partial_cmp(b)));
-    /// assert!(std::iter::empty::<i32>().mz_is_sorted_by(|a, b| a.partial_cmp(b)));
-    /// assert!(![0.0, 1.0, f32::NAN].iter().mz_is_sorted_by(|a, b| a.partial_cmp(b)));
-    /// ```
-    ///
-    /// [`std::iter::Iterator::is_sorted`]: IteratorExt::is_sorted
-    fn mz_is_sorted_by<F>(mut self, mut compare: F) -> bool
-    where
-        Self: Sized,
-        F: FnMut(&Self::Item, &Self::Item) -> Option<Ordering>,
-    {
-        let mut last = match self.next() {
-            Some(e) => e,
-            None => return true,
-        };
-
-        for curr in self {
-            if let Some(Ordering::Greater) | None = compare(&last, &curr) {
-                return false;
-            }
-            last = curr;
-        }
-
-        true
-    }
-
     /// Chains a single `item` onto the end of this iterator.
     ///
     /// Equivalent to `self.chain(iter::once(item))`.
