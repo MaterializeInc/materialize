655c62ee47a6142bcd2c85b1d7fd1ed17f7bd308
Eli Lindsey <eli@siliconsprawl.com>
2021-11-08 17:08:32 -0500

    log errors and early termination from the stream consumer in kafka-verify

--- src/testdrive/src/action/kafka/verify.rs
+++ src/testdrive/src/action/kafka/verify.rs
@@ -148,15 +148,27 @@ impl Action for VerifyAction {
         // instead get an error message about the expected messages that
         // were missing.
         let mut actual_bytes = vec![];
-        while let Some(Ok(message)) = message_stream.next().await {
-            let message = message.map_err_to_string()?;
-            consumer
-                .store_offset_from_message(&message)
-                .map_err(|e| format!("storing message offset: {:#}", e))?;
-            actual_bytes.push((
-                message.key().and_then(|bytes| Some(bytes.to_owned())),
-                message.payload().and_then(|bytes| Some(bytes.to_owned())),
-            ));
+        loop {
+            match message_stream.next().await {
+                Some(Ok(message)) => {
+                    let message = message.map_err_to_string()?;
+                    consumer
+                        .store_offset_from_message(&message)
+                        .map_err(|e| format!("storing message offset: {:#}", e))?;
+                    actual_bytes.push((
+                        message.key().and_then(|bytes| Some(bytes.to_owned())),
+                        message.payload().and_then(|bytes| Some(bytes.to_owned())),
+                    ));
+                }
+                Some(Err(e)) => {
+                    println!("Received error from Kafka stream consumer: {}", e);
+                    break;
+                }
+                None => {
+                    println!("Received None from Kafka stream consumer");
+                    break;
+                }
+            }
         }
 
         match &self.format {
