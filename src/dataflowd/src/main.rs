8eed9121f878127b9819f4c510ca4f4827541c92
Moritz Hoffmann <mh@materialize.com>
2021-11-09 10:44:28 +0100

    dataflowd: accept String as network addresses
    This enables hostname resolution on network addresses and avoids confusion
    when `localhost` resolves to `[::1]`, but the other end specified an IPv4
    address.
    
    Signed-off-by: Moritz Hoffmann <mh@materialize.com>

--- src/dataflowd/src/main.rs
+++ src/dataflowd/src/main.rs
@@ -7,7 +7,6 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
 
-use std::net::SocketAddr;
 use std::process;
 
 use anyhow::bail;
@@ -33,7 +32,7 @@ struct Args {
         value_name = "HOST:PORT",
         default_value = "0.0.0.0:6876"
     )]
-    listen_addr: SocketAddr,
+    listen_addr: String,
     /// Number of dataflow worker threads.
     #[structopt(
         short,
