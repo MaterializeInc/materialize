2ce21129db5920f0bffc87ff65ccde2df9edc039
Andi Wang <andi@materialize.io>
2022-01-24 18:48:01 -0500

    Address feedback. Use a non-recursive method for DFT.

--- src/sql/src/query_model/rewrites/mod.rs
+++ src/sql/src/query_model/rewrites/mod.rs
@@ -7,7 +7,7 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
 
-//! Transform a Query Graph Model into a normalized, decorrelated form.
+//! Apply rewrites to the Query Graph Model.
 
 use crate::query_model::{BoxId, Model};
 use std::collections::HashSet;
@@ -16,7 +16,7 @@ use std::collections::HashSet;
 pub(crate) trait Rule {
     fn name(&self) -> &'static str;
 
-    fn rule_type() -> RuleType
+    fn rule_type() -> ApplyStrategy
     where
         Self: Sized;
 
@@ -24,8 +24,8 @@ pub(crate) trait Rule {
     ///
     /// This method is not allowed to modify the model in any way.
     ///
-    /// If this method should return true, the rule instance should modified to
-    /// store information about how the box should be rewritten to avoid
+    /// If this method should return true, the rule instance should be modified
+    /// to store information about how the box model should be rewritten to avoid
     /// duplicating work.
     ///
     /// If this method returns false, the rewrite rule instance will be
@@ -41,27 +41,36 @@ pub(crate) trait Rule {
     fn rewrite(&mut self, model: &mut Model, box_id: BoxId);
 }
 
-/// Classifies the rule by which boxes it should be applied to and in what
-/// order.
+/// Where and how a rule should be applied to boxes in the model.
 #[allow(dead_code)]
 #[derive(Debug)]
-pub(crate) enum RuleType {
-    /// The rule should be applied to all boxes.
-    /// Order of the boxes does not matter.
-    AllBoxes,
-    /// The rule should be applied to all boxes in pre-order.
-    PreOrder,
-    /// The rule should be applied to all boxes in post-order.
-    PostOrder,
-    /// The rule should only be applied to the top box.
-    TopBoxOnly,
+pub(crate) enum ApplyStrategy {
+    // Apply only to the top box.
+    TopBox,
+    // Apply to all boxes. Customize with the order in which the boxes should be
+    // visited.
+    AllBoxes(VisitOrder),
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub(crate) enum VisitOrder {
+    Pre,
+    Post,
+}
+
+impl Default for VisitOrder {
+    /// Call if a rule does not care about the order in which it is applied.
+    fn default() -> Self {
+        VisitOrder::Post
+    }
 }
 
 /// Generates blank instances of a rule.
 #[allow(missing_debug_implementations)]
 struct RuleGenerator {
     /// The type of the rule.
-    pub typ: RuleType,
+    pub typ: ApplyStrategy,
     /// Function that creates a blank instance of the rule.
     pub generate: Box<dyn Fn() -> Box<dyn Rule>>,
 }
@@ -75,11 +84,10 @@ fn make_generator<R: 'static + Rule + Default>() -> RuleGenerator {
     }
 }
 
-/// Entry-point of the normalization stage.
+/// Apply all available rewrite rules to the model.
 pub fn rewrite_model(model: &mut Model) {
-    // TODO: populate this list.
-    let mut rules: Vec<RuleGenerator> = vec![];
-    apply_rules_to_model(model, &mut rules);
+    let rules: Vec<RuleGenerator> = vec![];
+    apply_rules_to_model(model, rules);
     model.garbage_collect();
 
     // At the end of the process, update box and quantifier ids to make it
@@ -88,25 +96,25 @@ pub fn rewrite_model(model: &mut Model) {
 }
 
 /// Transform the model by applying a list of rewrite rules.
-fn apply_rules_to_model(model: &mut Model, rules: &mut Vec<RuleGenerator>) {
-    let mut rewritten = true;
+fn apply_rules_to_model(model: &mut Model, rules: Vec<RuleGenerator>) {
+    let (top_box, other): (Vec<RuleGenerator>, Vec<RuleGenerator>) = rules
+        .into_iter()
+        .partition(|r| matches!(r.typ, ApplyStrategy::TopBox));
+    let (pre, post): (Vec<RuleGenerator>, Vec<RuleGenerator>) = other
+        .into_iter()
+        .partition(|r| matches!(r.typ, ApplyStrategy::AllBoxes(VisitOrder::Pre)));
 
+    for rule in &top_box {
+        apply_rule(&rule, model, model.top_box);
+    }
+
+    let mut rewritten = true;
     while rewritten {
         rewritten = false;
 
-        for rule in rules
-            .iter_mut()
-            .filter(|r| matches!(r.typ, RuleType::TopBoxOnly))
-        {
-            rewritten |= apply_rule(&rule, model, model.top_box);
-        }
+        rewritten |= apply_dft_rules(&pre, &post, model);
 
-        rewritten |= recursively_apply_rules(rules, model, model.top_box, &mut HashSet::new());
-
-        for rule in rules
-            .iter_mut()
-            .filter(|r| matches!(r.typ, RuleType::TopBoxOnly))
-        {
+        for rule in &top_box {
             rewritten |= apply_rule(&rule, model, model.top_box);
         }
     }
@@ -125,41 +133,63 @@ fn apply_rule(rule_generator: &RuleGenerator, model: &mut Model, box_id: BoxId) 
     rewrite
 }
 
-/// Apply the given list of rewrite rules to the given box and
-/// all descendent boxes.
+/// Traverse the model depth-first, applying rules to each box.
 ///
-/// `visited_boxes` keeps track of all the visited boxes so far
-/// to avoid visiting them again.
+/// To avoid the possibility of stack overflow, the implementation does not use
+/// recursion. Instead, it keeps track of boxes we have entered using a `Vec`.
+/// It also keeps track of boxes we have exited so that if multiple boxes are
+/// parents of the same child box, the child will only be visited once.
+///
+/// Rules in `pre` are applied at enter-time, and rules in `post` are applied at
+/// exit-time.
 ///
 /// Returns whether any box was rewritten.
-fn recursively_apply_rules(
-    rules: &mut Vec<RuleGenerator>,
-    model: &mut Model,
-    box_id: BoxId,
-    visited_boxes: &mut HashSet<BoxId>,
-) -> bool {
+fn apply_dft_rules(pre: &Vec<RuleGenerator>, post: &Vec<RuleGenerator>, model: &mut Model) -> bool {
     let mut rewritten = false;
 
-    if visited_boxes.insert(box_id) {
-        for rule in rules
-            .iter_mut()
-            .filter(|r| matches!(r.typ, RuleType::PreOrder))
-        {
-            rewritten |= apply_rule(&rule, model, box_id);
-        }
+    // All nodes that have been entered but not exited. Last node in the vec is
+    // the node that we most recently entered.
+    let mut entered = Vec::new();
+    // All nodes that have been exited.
+    let mut exited = HashSet::new();
 
-        let quantifiers = model.get_box(box_id).quantifiers.clone();
-        for q_id in quantifiers {
-            let input_box = model.get_quantifier(q_id).input_box;
-            rewritten |= recursively_apply_rules(rules, model, input_box, visited_boxes);
+    fn find_next_child_to_enter(
+        model: &mut Model,
+        entered: &mut Vec<(BoxId, usize)>,
+        exited: &mut HashSet<BoxId>,
+    ) -> Option<BoxId> {
+        let (box_id, traversed_quantifiers) = entered.last_mut().unwrap();
+        let b = model.get_box(*box_id);
+        for q_id in b.quantifiers.iter().skip(*traversed_quantifiers) {
+            let q = model.get_quantifier(*q_id);
+            *traversed_quantifiers += 1;
+            if !exited.contains(&q.input_box) {
+                return Some(q.input_box);
+            }
         }
+        return None;
+    }
 
-        for rule in rules
-            .iter_mut()
-            .filter(|r| matches!(r.typ, RuleType::PostOrder | RuleType::AllBoxes))
-        {
-            rewritten |= apply_rule(&rule, model, box_id);
+    // Enter the top box.
+    for rule in pre {
+        rewritten |= apply_rule(&rule, model, model.top_box);
+    }
+    entered.push((model.top_box, 0));
+    while !entered.is_empty() {
+        if let Some(to_enter) = find_next_child_to_enter(model, &mut entered, &mut exited) {
+            for rule in pre {
+                rewritten |= apply_rule(&rule, model, to_enter);
+            }
+            entered.push((to_enter, 0));
+        } else {
+            // If we can't descend any more, exit the current node we are in.
+            let (box_id, _) = entered.pop().unwrap();
+            for rule in post {
+                rewritten |= apply_rule(&rule, model, box_id);
+            }
+            exited.insert(box_id);
         }
     }
+
     rewritten
 }
