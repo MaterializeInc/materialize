e052a1dcca80d759a35791ac59c7d0570acc5856
Nick Harring <furiouseyebrows@gmail.com>
2022-01-27 12:49:10 -0800

    lint cleanup

--- src/postgres-util/src/lib.rs
+++ src/postgres-util/src/lib.rs
@@ -22,7 +22,7 @@ use tokio_postgres::{Client, Config};
 use ore::task;
 use repr::adt;
 use sql_parser::ast::display::{AstDisplay, AstFormatter};
-use sql_parser::ast::{ColumnDef, DataType, Ident, PgTable, Raw, RawName, UnresolvedObjectName};
+use sql_parser::ast::{ColumnDef, Ident, PgTable, Raw, RawName, UnresolvedObjectName};
 use sql_parser::impl_display;
 
 pub struct PgNumericMod {
