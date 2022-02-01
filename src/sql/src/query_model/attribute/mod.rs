7380a4cdced23e0d2724d355e01f3413c700325b
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2022-02-01 15:18:49 +0100

    qgm: introduce `attributes::core` module

--- src/sql/src/query_model/attribute/mod.rs
+++ src/sql/src/query_model/attribute/mod.rs
@@ -7,15 +7,6 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
 
-mod attribute;
-mod dot;
-mod hir;
-mod mir;
-mod model;
-mod rewrite;
-#[cfg(test)]
-mod test;
-mod validator;
+//! Derived attributes framework and definitions.
 
-pub use model::{BoxId, DistinctOperation, Model, QuantifierId};
-pub use validator::{ValidationError, ValidationResult};
+pub mod core;
