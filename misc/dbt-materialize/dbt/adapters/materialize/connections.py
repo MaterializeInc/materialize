5658067f008dd707ec9fb13c5920a69afa8ecdf6
Nikhil Benesch <nikhil.benesch@gmail.com>
2022-02-02 01:43:46 -0500

    dbt-materialize: avoid copy/pasting a dbt-postgres method

--- misc/dbt-materialize/dbt/adapters/materialize/connections.py
+++ misc/dbt-materialize/dbt/adapters/materialize/connections.py
@@ -15,21 +15,12 @@
 # limitations under the License.
 
 from dataclasses import dataclass
-from typing import Optional
 
-import dbt.exceptions
-import psycopg2
-from dbt import flags
 from dbt.adapters.postgres import PostgresConnectionManager, PostgresCredentials
-from dbt.logger import GLOBAL_LOGGER as logger
 
 
 @dataclass
 class MaterializeCredentials(PostgresCredentials):
-    sslcert: Optional[str] = None
-    sslkey: Optional[str] = None
-    sslrootcert: Optional[str] = None
-
     @property
     def type(self):
         return "materialize"
@@ -40,66 +31,8 @@ class MaterializeConnectionManager(PostgresConnectionManager):
 
     @classmethod
     def open(cls, connection):
-        if connection.state == "open":
-            logger.debug("Connection is already open, skipping open.")
-            return connection
-
-        credentials = cls.get_credentials(connection.credentials)
-        kwargs = {}
-        # we don't want to pass 0 along to connect() as postgres will try to
-        # call an invalid setsockopt() call (contrary to the docs).
-        if credentials.keepalives_idle:
-            kwargs["keepalives_idle"] = credentials.keepalives_idle
-
-        # psycopg2 doesn't support search_path officially,
-        # see https://github.com/psycopg/psycopg2/issues/465
-        search_path = credentials.search_path
-        if search_path is not None and search_path != "":
-            # see https://postgresql.org/docs/9.5/libpq-connect.html
-            kwargs["options"] = "-c search_path={}".format(
-                search_path.replace(" ", "\\ ")
-            )
-
-        if credentials.sslmode:
-            kwargs["sslmode"] = credentials.sslmode
-
-        if credentials.sslcert is not None:
-            kwargs["sslcert"] = credentials.sslcert
-
-        if credentials.sslkey is not None:
-            kwargs["sslkey"] = credentials.sslkey
-
-        if credentials.sslrootcert is not None:
-            kwargs["sslrootcert"] = credentials.sslrootcert
-
-        try:
-            handle = psycopg2.connect(
-                dbname=credentials.database,
-                user=credentials.user,
-                host=credentials.host,
-                password=credentials.password,
-                port=credentials.port,
-                connect_timeout=10,
-                **kwargs,
-            )
-
-            if credentials.role:
-                handle.cursor().execute("set role {}".format(credentials.role))
-
-            connection.handle = handle
-            connection.state = "open"
-        except psycopg2.Error as e:
-            logger.debug(
-                "Got an error when attempting to open a postgres "
-                "connection: '{}'".format(e)
-            )
-
-            connection.handle = None
-            connection.state = "fail"
-
-            raise dbt.exceptions.FailedToConnectException(str(e))
-
-        # Prevents psycopg connection from automatically opening transactions
+        connection = super().open(connection)
+        # Prevents psycopg connection from automatically opening transactions.
         # More info: https://www.psycopg.org/docs/usage.html#transactions-control
         connection.handle.autocommit = True
         return connection
