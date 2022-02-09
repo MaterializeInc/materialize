f42e875cabba1a945bc5f9562d7b97d49fc8b6f2
Andi Wang <andi@materialize.io>
2022-02-08 22:13:13 -0500

    Redo the rule traversing up from an outer join box.
    Previously, the rule traversed down starting from a box that rejected
    nulls.

--- src/sql/src/query_model/rewrite/rule/simplify_outer_joins.rs
+++ src/sql/src/query_model/rewrite/rule/simplify_outer_joins.rs
@@ -7,13 +7,10 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
 
-//! Implements outerjoin simplification as a variant of Algorithm A in the seminal
-//! paper by Rosenthal and Galindo-Legaria[^1].
+//! Implements outerjoin simplification.
 //!
-//! [^1]: [Galindo-Legaria, Cesar, and Arnon Rosenthal.
-//! "Outerjoin simplification and reordering for query optimization."
-//! ACM Transactions on Database Systems (TODS) 22.1 (1997): 43-74.
-//! ](https://www.academia.edu/26160408/Outerjoin_simplification_and_reordering_for_query_optimization)
+//! For each outer join box, check to see if any ancestor rejects nulls
+//! on its output.
 
 use std::cell::RefCell;
 use std::collections::HashSet;
@@ -21,7 +18,9 @@ use std::collections::HashSet;
 use crate::query_model::attribute::core::Attribute;
 use crate::query_model::attribute::propagated_nulls::PropagatedNulls;
 use crate::query_model::attribute::rejected_nulls::RejectedNulls;
-use crate::query_model::model::{BoxType, QuantifierType, Select};
+use crate::query_model::model::{
+    BoundRef, BoxType, ColumnReference, QuantifierType, QueryBox, Select,
+};
 use crate::query_model::rewrite::ApplyStrategy;
 use crate::query_model::rewrite::Rule;
 use crate::query_model::rewrite::VisitOrder;
@@ -30,9 +29,10 @@ use crate::query_model::{BoxId, Model, QuantifierId};
 pub(crate) struct SimplifyOuterJoins;
 
 impl Rule for SimplifyOuterJoins {
-    /// A (non-empty) sequence of boxes to be modified, together with
-    /// specifically which quantifiers are to be changed to type Foreach
-    type Match = Vec<(BoxId, Vec<QuantifierId>)>;
+    /// A (non-empty) sequence of ids corresponding to [Quantifier]s whose type
+    /// should be changed from [QuantifierType::PreservedForeach] to
+    /// [QuantifierType::Foreach], and the box that the quantifiers belong to.
+    type Match = (BoxId, Vec<QuantifierId>);
 
     fn name(&self) -> &'static str {
         "SimplifyOuterJoins"
@@ -52,151 +52,177 @@ impl Rule for SimplifyOuterJoins {
     fn check(
         &self,
         model: &crate::query_model::Model,
-        box_id: crate::query_model::BoxId,
+        box_id_to_check: crate::query_model::BoxId,
     ) -> Option<Self::Match> {
-        // get the rejected nulls at the root
-        let r#box = model.get_box(box_id);
-        let rej_nulls = r#box.attributes.get::<RejectedNulls>();
+        let mut quantifiers_to_change = vec![];
 
-        // we can return immediately if there are no rejected nulls at this node
-        if rej_nulls.is_empty() {
-            return None;
+        let box_to_check = model.get_box(box_id_to_check);
+        if let BoxType::OuterJoin(..) = box_to_check.box_type {
+            assert_eq!(box_to_check.input_quantifiers().count(), 2);
+
+            // We currently only apply the rule to an outer join if a unique
+            // ancestor chain can be found going from the top box to the box to
+            // check. We might remove this constraint in the future.
+            // See [this comment](https://github.com/MaterializeInc/materialize/issues/10239#issuecomment-1030123237)
+            // and the preceding discussion for more details.
+
+            let unique_ancestor_chain = unique_ancestor_chain(model, box_id_to_check);
+
+            if let Some(ancestor_chain) = unique_ancestor_chain {
+                // Collect all the rejected nulls from all the ancestors in the chain.
+                let mut ancestral_rejected_nulls = HashSet::new();
+
+                // As we move down the chain, going from parent to child,
+                // We want to translate the rejected nulls for each parent box
+                // to be in terms of the child box. Then we want to add the
+                // nulls that the child box rejects.
+                for ancestor_id in ancestor_chain {
+                    let ancestor_box = model.get_box(ancestor_id);
+                    map_nulls_to_inputs(&mut ancestral_rejected_nulls, &ancestor_box);
+                    ancestral_rejected_nulls.extend(
+                        ancestor_box
+                            .attributes
+                            .get::<RejectedNulls>()
+                            .into_iter()
+                            .cloned(),
+                    );
+                }
+                map_nulls_to_inputs(&mut ancestral_rejected_nulls, &box_to_check);
+
+                let mut quantifiers = box_to_check.input_quantifiers();
+                let lhs = quantifiers.next().unwrap();
+                let rhs = quantifiers.next().unwrap();
+
+                let rej_lhs = ancestral_rejected_nulls
+                    .iter()
+                    .any(|c| c.quantifier_id == lhs.id);
+                let rej_rhs = ancestral_rejected_nulls
+                    .iter()
+                    .any(|c| c.quantifier_id == rhs.id);
+
+                // If null rows are rejected from LHS, and RHS is a
+                // PreservedForeach quantifier, change the RHS to a Foreach
+                // quantifier.
+                if rej_lhs && rhs.quantifier_type == QuantifierType::PreservedForeach {
+                    quantifiers_to_change.push(rhs.id);
+                }
+                // And vice versa.
+                if rej_rhs && lhs.quantifier_type == QuantifierType::PreservedForeach {
+                    quantifiers_to_change.push(lhs.id);
+                }
+            }
         }
 
-        // An optional match found at the current box.
-        let mut mat = vec![] as Self::Match;
-
-        // A stack that keeps track of the number of ranging quantifiers
-        // associated with each box along the recursion trace.
-        let quantifier_counts = RefCell::new(vec![] as Vec<usize>);
-
-        // A stack of the rejected nulls for the predicates associated
-        // with the current box (identified by box_id) expressed in
-        // terms of the input quantifiers of the current box
-        // initialize with the nulls at the root.
-        let rej_nulls = RefCell::new(vec![rej_nulls.clone()]);
-
-        let _: Result<(), ()> = model.try_visit_pre_post_descendants(
-            &mut |model, box_id| {
-                // mutably borrow stack variables for the duration of this closure
-                let mut rej_nulls = rej_nulls.borrow_mut();
-                let mut quantifier_counts = quantifier_counts.borrow_mut();
-
-                let r#box = model.get_box(*box_id);
-
-                // Push the ranging quantifiers count the current node.
-                quantifier_counts.push(r#box.ranging_quantifiers().count());
-
-                // At the moment, we don't want to do any work:
-                // (1) if a match is already found, or
-                // (2) if we are in a in subtree with a non-unique ancestor chain.
-                // In the future, we might remove constraint (2).
-                // See [this comment](https://github.com/MaterializeInc/materialize/issues/10239#issuecomment-1030123237)
-                // and the preceding discussion for more details.
-                if quantifier_counts.iter().all(|count| count == &1) {
-                    // Get the ID of the quantifer that connects this box with its parent.
-                    let quantifier_id = r#box.ranging_quantifiers().nth(0).unwrap().id;
-                    // Get the propagated nulls for the output attributes of this box.
-                    let propagated_nulls = r#box.attributes.get::<PropagatedNulls>();
-
-                    // Take the rej_nulls expressed in terms of the parent's quantifiers.
-                    let rej_nulls_parent = rej_nulls.last().unwrap();
-
-                    // And use it to compute the rej_nulls expressed in terms of this child's quantifiers.
-                    let rej_nulls_child = rej_nulls_parent
-                        .iter()
-                        // Filter out columns that are not from this child
-                        .filter(|c| c.quantifier_id != quantifier_id)
-                        // and replace the rest with the set of nulls that
-                        // the corresponding column propagates from its input.
-                        .flat_map(|c| match propagated_nulls.get(c.position) {
-                            Some(set) => set.clone(),
-                            None => HashSet::new(),
-                        })
-                        .collect::<HashSet<_>>();
-
-                    // If this is an outer join box, we might have a potential match
-                    if let BoxType::OuterJoin(..) = r#box.box_type {
-                        assert_eq!(r#box.input_quantifiers().count(), 2);
-
-                        let lhs = r#box.input_quantifiers().nth(0).unwrap();
-                        let rhs = r#box.input_quantifiers().nth(0).unwrap();
-
-                        let rej_lhs = rej_nulls_child.iter().any(|c| c.quantifier_id == lhs.id);
-                        let rej_rhs = rej_nulls_child.iter().any(|c| c.quantifier_id == rhs.id);
-
-                        let mut quantifiers_to_change = Vec::new();
-
-                        // If null rows are rejected from LHS, and RHS is a
-                        // PreservedForeach quantifier, change the RHS to a Foreach
-                        // quantifier.
-                        if rej_lhs && rhs.quantifier_type == QuantifierType::PreservedForeach {
-                            quantifiers_to_change.push(rhs.id);
-                        }
-                        // And vice versa.
-                        if rej_rhs && lhs.quantifier_type == QuantifierType::PreservedForeach {
-                            quantifiers_to_change.push(lhs.id);
-                        }
-                        if !quantifiers_to_change.is_empty() {
-                            mat.push((*box_id, quantifiers_to_change));
-                        }
-                    };
-
-                    // Push rej_nulls_child to the stack.
-                    rej_nulls.push(rej_nulls_child);
-                }
-
-                Ok(())
-            },
-            &mut |_model, _box_id| {
-                // mutably borrow stack variables for the duration of this closure
-                let mut rej_nulls = rej_nulls.borrow_mut();
-                let mut quantifier_counts = quantifier_counts.borrow_mut();
-
-                if quantifier_counts.iter().all(|count| count == &1) {
-                    // Pop the rejected nulls for the current node.
-                    rej_nulls.pop();
-                }
-
-                // Pop the quantifier count of the current node.
-                quantifier_counts.pop();
-
-                Ok(())
-            },
-            box_id,
-        );
-
-        if mat.len() > 0 {
-            Some(mat)
+        if quantifiers_to_change.len() > 0 {
+            Some((box_id_to_check, quantifiers_to_change))
         } else {
+            // If there are no quantifiers to change, return None.
             None
         }
     }
 
     fn rewrite(&self, model: &mut Model, mat: Self::Match) {
-        for (box_id, q_ids) in mat {
-            // Change the specified quantifiers to type Foreach.
-            for q_id in q_ids {
-                let mut q = model.get_mut_quantifier(q_id);
-                q.quantifier_type = QuantifierType::Foreach;
-            }
-
-            // If all the quantifiers in the box are type Foreach,
-            // convert the box to type Select.
-            let mut r#box = model.get_mut_box(box_id);
-            if r#box
-                .input_quantifiers()
-                .all(|q| q.quantifier_type == QuantifierType::Foreach)
-            {
-                let predicates = if let BoxType::OuterJoin(outer_join) = &r#box.box_type {
-                    Some(outer_join.predicates.clone())
-                } else {
-                    None
-                };
-                let mut select = Select::default();
-                select.predicates = predicates.unwrap();
-                r#box.box_type = BoxType::from(select);
-            }
+        // Change the specified quantifiers to type Foreach.
+        let q_ids = mat.1;
+        for q_id in q_ids {
+            let mut q = model.get_mut_quantifier(q_id);
+            q.quantifier_type = QuantifierType::Foreach;
         }
+
+        // If all the quantifiers in the box are type Foreach,
+        // convert the box to type Select.
+        let box_id = mat.0;
+        let mut r#box = model.get_mut_box(box_id);
+        if r#box
+            .input_quantifiers()
+            .all(|q| q.quantifier_type == QuantifierType::Foreach)
+        {
+            let predicates = if let BoxType::OuterJoin(outer_join) = &r#box.box_type {
+                Some(outer_join.predicates.clone())
+            } else {
+                None
+            };
+            let mut select = Select::default();
+            select.predicates = predicates.unwrap();
+            r#box.box_type = BoxType::from(select);
+        }
+    }
+}
+
+/// Find the unique ancestor chain from the top box of `model` to `target` box.
+/// The ancestor chain does not contain `target`.
+fn unique_ancestor_chain(model: &Model, target: BoxId) -> Option<Vec<BoxId>> {
+    // Until target is reached, this is the stack of boxes we have entered.
+    // This does not change after target is reached.
+    let ancestor_chain = RefCell::new(vec![]);
+
+    let unique_ancestor_chain_found = RefCell::new(false);
+
+    // Traverse the graph starting from the top until `target` is reached.
+    // If a box with more than one parent is found, do not go deeper.
+    let _: Result<(), ()> = ore::graph::try_nonrecursive_dft(
+        model,
+        model.top_box,
+        &mut |model, box_id| {
+            let r#box = model.get_box(*box_id);
+            // If target is reached, register this and stop further traversal.
+            if *box_id == target {
+                *unique_ancestor_chain_found.borrow_mut() = true;
+                return Ok(vec![]);
+            }
+            if *unique_ancestor_chain_found.borrow() {
+                return Ok(vec![]);
+            }
+            // Otherwise, register that we have visited this node.
+            let mut ancestor_chain = ancestor_chain.borrow_mut();
+            ancestor_chain.push(*box_id);
+            // Only go deeper if the box has more than one parent.
+            if r#box.ranging_quantifiers().count() > 1 {
+                Ok(vec![])
+            } else {
+                Ok(model
+                    .get_box(*box_id)
+                    .input_quantifiers()
+                    .map(|q| q.input_box)
+                    .collect())
+            }
+        },
+        &mut |_, _| {
+            if !*unique_ancestor_chain_found.borrow() {
+                let mut ancestor_chain = ancestor_chain.borrow_mut();
+                ancestor_chain.pop();
+            }
+            Ok(())
+        },
+    );
+    if *unique_ancestor_chain_found.borrow() {
+        Some(ancestor_chain.take())
+    } else {
+        None
+    }
+}
+
+/// Map (rejected nulls from any ancestor of `box`, expressed as column
+/// references from an input of the parent of `box`) ->
+/// (column references from an input of `box`)
+fn map_nulls_to_inputs(
+    ancestral_rejected_nulls: &mut HashSet<ColumnReference>,
+    r#box: &BoundRef<'_, QueryBox>,
+) {
+    if !ancestral_rejected_nulls.is_empty() {
+        // Get the ID of the quantifer that connects this box with its parent.
+        let quantifier_id = r#box.ranging_quantifiers().nth(0).unwrap().id;
+        // Retain only columns from the child box.
+        ancestral_rejected_nulls.retain(|c| c.quantifier_id == quantifier_id);
+        // Replace each column with the set of nulls that
+        // the column propagates from its input.
+        let propagated_nulls = r#box.attributes.get::<PropagatedNulls>();
+        *ancestral_rejected_nulls = ancestral_rejected_nulls
+            .iter()
+            .flat_map(|c| match propagated_nulls.get(c.position) {
+                Some(set) => set.clone(),
+                None => HashSet::new(),
+            })
+            .collect();
     }
 }
