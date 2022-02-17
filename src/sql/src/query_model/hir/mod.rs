8a459c8d7811cb3f0bb5ad40f9c3f1b4f23e2e97
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2022-02-17 14:34:56 +0200

    qgm: move `hir.rs` to `hir/qgm_from_hir.rs`

---
+++ src/sql/src/query_model/hir/mod.rs
@@ -0,0 +1,16 @@
+// Copyright Materialize, Inc. and contributors. All rights reserved.
+//
+// Use of this software is governed by the Business Source License
+// included in the LICENSE file.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0.
+
+//! Conversion methods a `Model` to a `HirRelationExpr` and back.
+//!
+//! The public interface consists of:
+//! * `From<HirRelationExpr>` implemenation for `Result<Model, QGMError>`.
+//! * `From<Model>` implemenation for `HirRelationExpr`.
+
+mod qgm_from_hir;
