cbd56664bb9ea973953442e9607b7b2fc49cb3cc
Frank McSherry <fmcsherry@me.com>
2022-02-20 16:38:33 -0500

    Slice controller states by GlobalId (#10776)

--- src/dataflow-types/src/client/controller/storage.rs
+++ src/dataflow-types/src/client/controller/storage.rs
@@ -10,24 +10,24 @@
 use std::collections::BTreeMap;
 
 use differential_dataflow::lattice::Lattice;
+use timely::progress::frontier::MutableAntichain;
 use timely::progress::{Antichain, Timestamp};
 use tracing::error;
 
-use super::SinceUpperMap;
 use crate::client::SourceConnector;
 use crate::client::{Client, Command, StorageCommand};
+use crate::sources::SourceDesc;
 use crate::Update;
 use mz_expr::GlobalId;
 use mz_expr::PartitionId;
 
 /// Controller state maintained for each storage instance.
 pub struct StorageControllerState<T> {
-    /// Sources that have been created.
+    /// Collections maintained by the storage controller.
     ///
-    /// A `None` variant means that the source was dropped before it was first created.
-    source_descriptions: BTreeMap<GlobalId, Option<(crate::sources::SourceDesc, Antichain<T>)>>,
-    /// Tracks expressed `since` and received `upper` frontiers for sources and tables.
-    pub(super) since_uppers: SinceUpperMap<T>,
+    /// This collection only grows, although individual collections may be rendered unusable.
+    /// This is to prevent the re-binding of identifiers to other descriptions.
+    pub(super) collections: BTreeMap<GlobalId, CollectionState<T>>,
 }
 
 /// A controller for a storage instance.
@@ -41,18 +41,27 @@ pub enum StorageError {
     /// The source identifier was re-created after having been dropped,
     /// or installed with a different description.
     SourceIdReused(GlobalId),
+    /// The source identifier is not present.
+    IdentifierMissing(GlobalId),
 }
 
 impl<T> StorageControllerState<T> {
     pub(super) fn new() -> Self {
         Self {
-            source_descriptions: BTreeMap::default(),
-            since_uppers: SinceUpperMap::default(),
+            collections: BTreeMap::default(),
         }
     }
 }
 
+// Public interface
 impl<'a, C: Client<T>, T: Timestamp + Lattice> StorageController<'a, C, T> {
+    pub fn collection(&self, id: GlobalId) -> Result<&CollectionState<T>, StorageError> {
+        self.storage
+            .collections
+            .get(&id)
+            .ok_or(StorageError::IdentifierMissing(id))
+    }
+
     pub async fn create_sources(
         &mut self,
         mut bindings: Vec<(GlobalId, (crate::sources::SourceDesc, Antichain<T>))>,
@@ -69,27 +78,16 @@ impl<'a, C: Client<T>, T: Timestamp + Lattice> StorageController<'a, C, T> {
             }
         }
         for (id, description_since) in bindings.iter() {
-            match self.storage.source_descriptions.get(&id) {
-                Some(None) => Err(StorageError::SourceIdReused(*id))?,
-                Some(Some(prior_description)) => {
-                    if prior_description != description_since {
-                        Err(StorageError::SourceIdReused(*id))?
-                    }
-                }
-                None => {
-                    // All is well; no reason to panic.
+            if let Ok(collection) = self.collection(*id) {
+                if &collection.description != description_since {
+                    Err(StorageError::SourceIdReused(*id))?
                 }
             }
         }
-
+        // Install collection state for each bound source.
         for (id, (description, since)) in bindings.iter() {
-            self.storage
-                .source_descriptions
-                .insert(*id, Some((description.clone(), since.clone())));
-            self.storage.since_uppers.insert(
-                *id,
-                (since.clone(), Antichain::from_elem(Timestamp::minimum())),
-            );
+            let collection = CollectionState::new(description.clone(), since.clone());
+            self.storage.collections.insert(*id, collection);
         }
 
         self.client
@@ -99,12 +97,19 @@ impl<'a, C: Client<T>, T: Timestamp + Lattice> StorageController<'a, C, T> {
         Ok(())
     }
     pub async fn drop_sources(&mut self, identifiers: Vec<GlobalId>) {
+        // Dropping a source is equivalent to releasing any implicit capability.
+        // TODO(mcsherry): hold back this announcement until all capabilities are released.
         for id in identifiers.iter() {
-            if !self.storage.source_descriptions.contains_key(id) {
-                // This isn't an unrecoverable error, just .. probably wrong.
-                error!("Source id {} dropped without first being created", id);
-            } else {
-                self.storage.source_descriptions.insert(*id, None);
+            match self.collection_mut(*id) {
+                Ok(collection) => {
+                    // Apply the updates but ignore the results for now.
+                    // TODO(mcsherry): observe the results and allow compaction.
+                    let _ = collection.capability_downgrade(Antichain::new());
+                }
+                Err(_identifier_missing) => {
+                    // This isn't an unrecoverable error, just .. probably wrong.
+                    error!("Source id {} dropped without first being created", id);
+                }
             }
         }
 
@@ -138,16 +143,29 @@ impl<'a, C: Client<T>, T: Timestamp + Lattice> StorageController<'a, C, T> {
             }))
             .await
     }
-    pub async fn allow_source_compaction(&mut self, frontiers: Vec<(GlobalId, Antichain<T>)>) {
-        for (id, frontier) in frontiers.iter() {
-            self.storage.since_uppers.advance_since_for(*id, frontier);
-        }
+    pub async fn allow_source_compaction(
+        &mut self,
+        frontiers: Vec<(GlobalId, Antichain<T>)>,
+    ) -> Result<(), StorageError> {
+        // Validate that the ids exist.
+        self.validate_ids(frontiers.iter().map(|(id, _)| *id))?;
 
+        // Downgrade the implicit capability for each referenced id.
+        for (id, frontier) in frontiers.iter() {
+            // Apply the updates but ignore the results for now.
+            // TODO(mcsherry): observe the results and allow compaction.
+            let _ = self
+                .collection_mut(*id)?
+                .capability_downgrade(frontier.clone());
+        }
+        // TODO(mcsherry): Delay compation subject to read capability constraints.
         self.client
             .send(Command::Storage(StorageCommand::AllowSourceCompaction(
                 frontiers,
             )))
-            .await
+            .await;
+
+        Ok(())
     }
     pub async fn drop_source_timestamping(&mut self, id: GlobalId) {
         self.client
@@ -162,5 +180,58 @@ impl<'a, C: Client<T>, T: Timestamp + Lattice> StorageController<'a, C, T> {
                 advance_to,
             }))
             .await
+    }
+}
+
+// Internal interface
+impl<'a, C: Client<T>, T: Timestamp + Lattice> StorageController<'a, C, T> {
+    pub fn collection_mut(
+        &mut self,
+        id: GlobalId,
+    ) -> Result<&mut CollectionState<T>, StorageError> {
+        self.storage
+            .collections
+            .get_mut(&id)
+            .ok_or(StorageError::IdentifierMissing(id))
+    }
+
+    pub fn validate_ids(&self, ids: impl Iterator<Item = GlobalId>) -> Result<(), StorageError> {
+        for id in ids {
+            self.collection(id)?;
+        }
+        Ok(())
+    }
+}
+
+/// State maintained about individual collections.
+pub struct CollectionState<T> {
+    /// Description with which the source was created, and its initial `since`.
+    pub(super) description: (crate::sources::SourceDesc, Antichain<T>),
+    /// Accumulation of read capabilities for the collection.
+    pub(super) since: MutableAntichain<T>,
+    /// The implicit capability associated with source creation.
+    // TODO(mcsherry): make these capabilities explicit.
+    pub(super) capability: Antichain<T>,
+}
+
+impl<T: Timestamp> CollectionState<T> {
+    pub fn new(description: SourceDesc, since: Antichain<T>) -> Self {
+        Self {
+            description: (description, since.clone()),
+            since: since.borrow().into(),
+            capability: since,
+        }
+    }
+
+    pub fn capability_downgrade(
+        &mut self,
+        mut frontier: Antichain<T>,
+    ) -> impl Iterator<Item = (T, i64)> + '_ {
+        std::mem::swap(&mut self.capability, &mut frontier);
+        let changes = frontier
+            .into_iter()
+            .map(|time| (time, -1))
+            .chain(self.capability.iter().map(|time| (time.clone(), 1)));
+        self.since.update_iter(changes)
     }
 }
