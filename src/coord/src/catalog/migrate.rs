e72c41b3ce21a2a28cb6c6fb91ea5cc904b45d9e
Nick Harring <furiouseyebrows@gmail.com>
2022-02-22 15:05:49 -0800

    put migration in correct spot

--- src/coord/src/catalog/migrate.rs
+++ src/coord/src/catalog/migrate.rs
@@ -74,6 +74,7 @@ lazy_static! {
     static ref VER_0_9_2: Version = Version::new(0, 9, 2);
     static ref VER_0_9_13: Version = Version::new(0, 9, 13);
     static ref VER_0_20_0: Version = Version::new(0, 20, 0);
+    static ref VER_0_21_0: Version = Version::new(0, 21, 0);
 }
 
 pub(crate) fn migrate(catalog: &mut Catalog) -> Result<(), anyhow::Error> {
@@ -92,7 +93,6 @@ pub(crate) fn migrate(catalog: &mut Catalog) -> Result<(), anyhow::Error> {
         ast_use_pg_catalog_0_7_1(stmt)?;
         ast_insert_default_confluent_wire_format_0_7_1(stmt)?;
         ast_remove_csr_confluent_wire_format_0_19_0(stmt)?;
-        ast_rewrite_pgcdc_with_details_0_21_0(stmt)?;
         if catalog_version < *VER_0_9_1 {
             ast_rewrite_pg_catalog_char_to_text_0_9_1(stmt)?;
         }
@@ -104,6 +104,9 @@ pub(crate) fn migrate(catalog: &mut Catalog) -> Result<(), anyhow::Error> {
         }
         if catalog_version < *VER_0_20_0 {
             ast_rewrite_ccsr_with_options_to_compiled_0_20_0(stmt)?;
+        }
+        if catalog_version < *VER_0_21_0 {
+            ast_rewrite_pgcdc_with_details_0_21_0(stmt)?;
         }
         Ok(())
     })?;
