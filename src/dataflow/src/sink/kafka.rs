e7a8da2ac6cae5496a89b34b6e1a0582d082dfc7
Chae Jubb <chae@materialize.com>
2022-02-24 15:51:32 -0500

    unified retrymanager struct; comments

--- src/dataflow/src/sink/kafka.rs
+++ src/dataflow/src/sink/kafka.rs
@@ -13,7 +13,7 @@ use std::cmp;
 use std::collections::{HashMap, VecDeque};
 use std::future::Future;
 use std::rc::Rc;
-use std::sync::atomic::{AtomicBool, AtomicU64, Ordering};
+use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
 
@@ -185,22 +185,57 @@ impl SinkMetrics {
     }
 }
 
+#[derive(Clone)]
+pub struct KafkaSinkSendRetryManager {
+    // Because we flush this fully before moving onto the next timestamp, this queue cannot contain
+    // more messages than are contained in a single timely timestamp.  It's also unlikely to get
+    // beyond the size of the rdkafka internal message queue because that seems to stop accepting
+    // new messages when erorrs are being returned by the delivery callback -- though we should not
+    // rely on that fact if/when we eventually (tm) get around to making our queues bounded.
+    //
+    // If perf becomes an issue, we can do something slightly more complex with a crossbeam channel
+    q: VecDeque<OwnedMessage>,
+    outstanding_send_count: u64,
+}
+
+impl KafkaSinkSendRetryManager {
+    fn new() -> Self {
+        Self {
+            q: VecDeque::new(),
+            outstanding_send_count: 0,
+        }
+    }
+    fn record_send(&mut self) {
+        self.outstanding_send_count += 1;
+    }
+    fn record_error(&mut self, msg: OwnedMessage) {
+        self.q.push_back(msg);
+        self.outstanding_send_count -= 1;
+    }
+    fn record_success(&mut self) {
+        self.outstanding_send_count -= 1;
+    }
+    fn sends_flushed(&mut self) -> bool {
+        self.outstanding_send_count == 0 && self.q.len() == 0
+    }
+    fn pop_retry(&mut self) -> Option<OwnedMessage> {
+        self.q.pop_front()
+    }
+}
+
 pub struct SinkProducerContext {
     metrics: Arc<SinkMetrics>,
-    retry_queue: Arc<Mutex<VecDeque<OwnedMessage>>>,
-    outstanding_send_count: Arc<AtomicU64>,
+    retry_manager: Arc<Mutex<KafkaSinkSendRetryManager>>,
 }
 
 impl SinkProducerContext {
     pub fn new(
         metrics: Arc<SinkMetrics>,
-        retry_queue: Arc<Mutex<VecDeque<OwnedMessage>>>,
-        outstanding_send_count: Arc<AtomicU64>,
+        retry_manager: Arc<Mutex<KafkaSinkSendRetryManager>>,
     ) -> Self {
         SinkProducerContext {
             metrics,
-            retry_queue,
-            outstanding_send_count,
+            retry_manager,
         }
     }
 }
@@ -220,14 +255,16 @@ impl ProducerContext for SinkProducerContext {
 
     fn delivery(&self, result: &DeliveryResult, _: Self::DeliveryOpaque) {
         match result {
-            Ok(_) => (),
+            Ok(_) => self.retry_manager.lock().unwrap().record_success(),
             Err((_e, msg)) => {
                 self.metrics.message_delivery_errors_counter.inc();
                 // Should be bounded by the size of the kafka buffer
-                self.retry_queue.lock().unwrap().push_back(msg.detach());
+                self.retry_manager
+                    .lock()
+                    .unwrap()
+                    .record_error(msg.detach());
             }
         }
-        self.outstanding_send_count.fetch_sub(1, Ordering::SeqCst);
     }
 }
 
@@ -371,8 +408,7 @@ struct KafkaSinkState {
     transactional: bool,
     pending_rows: HashMap<Timestamp, Vec<EncodedRow>>,
     ready_rows: VecDeque<(Timestamp, Vec<EncodedRow>)>,
-    retry_queue: Arc<Mutex<VecDeque<OwnedMessage>>>,
-    outstanding_send_count: Arc<AtomicU64>,
+    retry_manager: Arc<Mutex<KafkaSinkSendRetryManager>>,
     sink_state: KafkaSinkStateEnum,
 
     /// Timestamp of the latest `END` record that was written out to Kafka.
@@ -410,8 +446,7 @@ impl KafkaSinkState {
             &worker_id,
         ));
 
-        let outstanding_send_count = Arc::new(AtomicU64::new(0));
-        let retry_queue = Arc::new(Mutex::new(VecDeque::new()));
+        let retry_manager = Arc::new(Mutex::new(KafkaSinkSendRetryManager::new()));
 
         let producer = KafkaTxProducer {
             name: sink_name.clone(),
@@ -419,8 +454,7 @@ impl KafkaSinkState {
                 config
                     .create_with_context::<_, ThreadedProducer<_>>(SinkProducerContext::new(
                         Arc::clone(&metrics),
-                        Arc::clone(&retry_queue),
-                        Arc::clone(&outstanding_send_count),
+                        Arc::clone(&retry_manager),
                     ))
                     .expect("creating kafka producer for Kafka sink failed"),
             ),
@@ -446,8 +480,7 @@ impl KafkaSinkState {
             transactional: connector.exactly_once,
             pending_rows: HashMap::new(),
             ready_rows: VecDeque::new(),
-            outstanding_send_count,
-            retry_queue,
+            retry_manager,
             sink_state,
             latest_progress_ts: Timestamp::minimum(),
             write_frontier,
@@ -614,7 +647,7 @@ impl KafkaSinkState {
             match self.producer.send(record) {
                 Ok(_) => {
                     self.metrics.messages_sent_counter.inc();
-                    self.outstanding_send_count.fetch_add(1, Ordering::SeqCst);
+                    self.retry_manager.lock().unwrap().record_send();
                     return Ok(());
                 }
                 Err((e, rec)) => {
@@ -646,12 +679,16 @@ impl KafkaSinkState {
     async fn flush(&self) -> KafkaResult<()> {
         loop {
             self.flush_inner().await?;
-            if self.outstanding_send_count.load(Ordering::SeqCst) == 0
-                && self.retry_queue.lock().unwrap().len() == 0
-            {
+            if {
+                let mut guard = self.retry_manager.lock().unwrap();
+                guard.sends_flushed()
+            } {
                 break;
             }
-            while let Some(msg) = { self.retry_queue.lock().unwrap().pop_front() } {
+            while let Some(msg) = {
+                let mut guard = self.retry_manager.lock().unwrap();
+                guard.pop_retry()
+            } {
                 let mut transformed_msg = BaseRecord::to(msg.topic());
                 transformed_msg = match msg.key() {
                     Some(k) => transformed_msg.key(k),
@@ -1321,7 +1358,7 @@ where
             }
 
             debug_assert_eq!(s.producer.inner.in_flight_count(), 0);
-            debug_assert_eq!(s.retry_queue.lock().unwrap().len(), 0);
+            debug_assert!(s.retry_manager.lock().unwrap().sends_flushed());
 
             if !s.pending_rows.is_empty() {
                 // We have some more rows that we need to wait for frontiers to advance before we
