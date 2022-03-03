a82325ca9719f6db7c4976965ce681b6ded16927
Paul Hemberger <paul@materialize.com>
2022-03-03 14:34:00 -0500

    quick fix to handle arrays in composite types (#10998)

--- src/sql/src/plan/statement/ddl.rs
+++ src/sql/src/plan/statement/ddl.rs
@@ -2106,7 +2106,7 @@ pub fn plan_create_type(
     let CreateTypeStatement { name, as_type, .. } = stmt;
     fn ensure_valid_data_type(
         scx: &StatementContext,
-        data_type: ResolvedDataType,
+        data_type: &ResolvedDataType,
         as_type: &CreateTypeAs<Raw>,
         key: &str,
     ) -> Result<(), anyhow::Error> {
@@ -2156,8 +2156,8 @@ pub fn plan_create_type(
         Ok(())
     }
 
-    let mut ids = vec![];
-    let mut record_field_names = vec![];
+    let mut depends_on = vec![];
+    let mut record_fields = vec![];
     match &as_type {
         CreateTypeAs::List { with_options } | CreateTypeAs::Map { with_options } => {
             let mut with_options = normalize::option_objects(&with_options);
@@ -2171,8 +2171,8 @@ pub fn plan_create_type(
                 match with_options.remove(&key.to_string()) {
                     Some(SqlOption::DataType { data_type, .. }) => {
                         let (data_type, dt_ids) = resolve_names_data_type(scx, data_type)?;
-                        ensure_valid_data_type(scx, data_type, &as_type, key)?;
-                        ids.extend(dt_ids);
+                        ensure_valid_data_type(scx, &data_type, &as_type, key)?;
+                        depends_on.extend(dt_ids);
                     }
                     Some(_) => bail!("{} must be a data type", key),
                     None => bail!("{} parameter required", key),
@@ -2186,9 +2186,13 @@ pub fn plan_create_type(
                 let key = ident(column_def.name.clone());
                 let (data_type, dt_ids) =
                     resolve_names_data_type(scx, column_def.data_type.clone())?;
-                ensure_valid_data_type(scx, data_type, &as_type, &key)?;
-                ids.extend(dt_ids);
-                record_field_names.push(ColumnName::from(key.clone()));
+                ensure_valid_data_type(scx, &data_type, &as_type, &key)?;
+                depends_on.extend(dt_ids);
+                if let ResolvedDataType::Named { id, .. } = data_type {
+                    record_fields.push((ColumnName::from(key.clone()), id));
+                } else {
+                    bail!("field {} must be a named type", key)
+                }
             }
         }
     };
@@ -2200,10 +2204,10 @@ pub fn plan_create_type(
 
     let inner = match as_type {
         CreateTypeAs::List { .. } => CatalogType::List {
-            element_id: *ids.get(0).expect("custom type to have element id"),
+            element_id: *depends_on.get(0).expect("custom type to have element id"),
         },
         CreateTypeAs::Map { .. } => {
-            let key_id = *ids.get(0).expect("key");
+            let key_id = *depends_on.get(0).expect("key");
             let entry = scx.catalog.get_item_by_id(&key_id);
             match entry.type_details() {
                 Some(CatalogTypeDetails {
@@ -2216,14 +2220,11 @@ pub fn plan_create_type(
 
             CatalogType::Map {
                 key_id,
-                value_id: *ids.get(1).expect("value"),
+                value_id: *depends_on.get(1).expect("value"),
             }
         }
         CreateTypeAs::Record { .. } => CatalogType::Record {
-            fields: record_field_names
-                .into_iter()
-                .zip_eq(ids.iter().cloned())
-                .collect_vec(),
+            fields: record_fields,
         },
     };
 
@@ -2232,7 +2233,7 @@ pub fn plan_create_type(
         typ: Type {
             create_sql,
             inner,
-            depends_on: ids,
+            depends_on,
         },
     }))
 }
