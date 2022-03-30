2ae4cebd01b0e46e80e20ef70025bb05a7cf48f4
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2022-03-30 11:30:26 +0300

    expr: align protobuf layout with `repr`
    - convert `gen.rs` to `proto/{mod.rs + id.rs}`
    - move `id.proto` to `proto/id.proto`
    - move `ProtoPartitionId` codecs to `proto/id.rs`

--- src/expr/src/id.rs
+++ src/expr/src/id.rs
@@ -10,10 +10,7 @@
 use std::fmt;
 use std::str::FromStr;
 
-use crate::gen::id::{proto_partition_id, ProtoPartitionId};
 use anyhow::{anyhow, Error};
-use bytes::BufMut;
-use prost::Message;
 use serde::{Deserialize, Serialize};
 
 /// An opaque identifier for a dataflow component. In other words, identifies
@@ -134,47 +131,6 @@ impl fmt::Display for SourceInstanceId {
 pub enum PartitionId {
     Kafka(i32),
     None,
-}
-
-impl From<&PartitionId> for ProtoPartitionId {
-    fn from(x: &PartitionId) -> Self {
-        ProtoPartitionId {
-            kind: Some(match x {
-                PartitionId::Kafka(x) => proto_partition_id::Kind::Kafka(*x),
-                PartitionId::None => proto_partition_id::Kind::None(()),
-            }),
-        }
-    }
-}
-
-impl TryFrom<ProtoPartitionId> for PartitionId {
-    type Error = String;
-
-    fn try_from(x: ProtoPartitionId) -> Result<Self, Self::Error> {
-        match x.kind {
-            Some(proto_partition_id::Kind::Kafka(x)) => Ok(PartitionId::Kafka(x)),
-            Some(proto_partition_id::Kind::None(_)) => Ok(PartitionId::None),
-            None => return Err("unknown partition_id".into()),
-        }
-    }
-}
-
-impl mz_persist_types::Codec for PartitionId {
-    fn codec_name() -> String {
-        "protobuf[PartitionId]".into()
-    }
-
-    fn encode<B: BufMut>(&self, buf: &mut B) {
-        ProtoPartitionId::from(self)
-            .encode(buf)
-            .expect("provided buffer had sufficient capacity")
-    }
-
-    fn decode<'a>(buf: &'a [u8]) -> Result<Self, String> {
-        ProtoPartitionId::decode(buf)
-            .map_err(|err| err.to_string())?
-            .try_into()
-    }
 }
 
 impl fmt::Display for PartitionId {
