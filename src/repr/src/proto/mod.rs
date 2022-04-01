c8c78bbe9e9175be8239aab9b2e3a381a9f99427
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2022-04-01 15:32:03 +0300

    repr: add `TryFromProtoError` to `gen/mod.rs`

--- src/repr/src/proto/mod.rs
+++ src/repr/src/proto/mod.rs
@@ -10,3 +10,47 @@
 //! Generated protobuf code and companion impls.
 
 pub mod row;
+
+use std::num::TryFromIntError;
+
+/// An error thrown when trying to convert from a `*.proto`-generated type
+/// `Proto$T` to `$T`.
+#[derive(Debug)]
+pub enum TryFromProtoError {
+    TryFromIntError(TryFromIntError),
+    MissingField(String),
+    InvalidChar(String),
+}
+
+impl TryFromProtoError {
+    pub fn missing_field<T: ToString>(s: T) -> TryFromProtoError {
+        TryFromProtoError::MissingField(s.to_string())
+    }
+}
+
+impl From<TryFromIntError> for TryFromProtoError {
+    fn from(error: TryFromIntError) -> Self {
+        TryFromProtoError::TryFromIntError(error)
+    }
+}
+
+impl std::fmt::Display for TryFromProtoError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        use TryFromProtoError::*;
+        match self {
+            TryFromIntError(error) => error.fmt(f),
+            MissingField(field) => write!(f, "Missing value for `{}`", field),
+            InvalidChar(str) => write!(f, "String '{}' does not encode a single UTF8 char", str),
+        }
+    }
+}
+
+impl std::error::Error for TryFromProtoError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        use TryFromProtoError::*;
+        match self {
+            TryFromIntError(error) => Some(error),
+            MissingField(_) | InvalidChar(_) => None,
+        }
+    }
+}
