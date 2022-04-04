3ae8b0cdaea211510476d2a6dd0281c489fb65bd
Lukas Humbel <lukas@materialize.com>
2022-04-04 17:32:31 +0200

    expr: add protobuf for global and local id

--- src/expr/src/proto/id.rs
+++ src/expr/src/proto/id.rs
@@ -11,9 +11,51 @@ use bytes::BufMut;
 use prost::Message;
 
 use crate::id::PartitionId;
+use crate::{GlobalId, LocalId};
 use mz_repr::proto::TryFromProtoError;
 
 include!(concat!(env!("OUT_DIR"), "/id.rs"));
+
+impl From<&GlobalId> for ProtoGlobalId {
+    fn from(x: &GlobalId) -> Self {
+        ProtoGlobalId {
+            kind: Some(match x {
+                GlobalId::System(x) => proto_global_id::Kind::System(*x),
+                GlobalId::User(x) => proto_global_id::Kind::User(*x),
+                GlobalId::Transient(x) => proto_global_id::Kind::Transient(*x),
+                GlobalId::Explain => proto_global_id::Kind::Explain(()),
+            }),
+        }
+    }
+}
+
+impl TryFrom<ProtoGlobalId> for GlobalId {
+    type Error = TryFromProtoError;
+
+    fn try_from(x: ProtoGlobalId) -> Result<Self, Self::Error> {
+        match x.kind {
+            Some(proto_global_id::Kind::System(x)) => Ok(GlobalId::System(x)),
+            Some(proto_global_id::Kind::User(x)) => Ok(GlobalId::User(x)),
+            Some(proto_global_id::Kind::Transient(x)) => Ok(GlobalId::Transient(x)),
+            Some(proto_global_id::Kind::Explain(_)) => Ok(GlobalId::Explain),
+            None => Err(TryFromProtoError::missing_field("ProtoGlobalId::kind")),
+        }
+    }
+}
+
+impl From<&LocalId> for ProtoLocalId {
+    fn from(x: &LocalId) -> Self {
+        ProtoLocalId { value: x.0 }
+    }
+}
+
+impl TryFrom<ProtoLocalId> for LocalId {
+    type Error = TryFromProtoError;
+
+    fn try_from(x: ProtoLocalId) -> Result<Self, Self::Error> {
+        Ok(LocalId::new(x.value))
+    }
+}
 
 impl From<&PartitionId> for ProtoPartitionId {
     fn from(x: &PartitionId) -> Self {
