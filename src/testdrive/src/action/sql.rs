ecd11f0e6bb337dc53d855cd79ae9a09068f3340
Joseph Koshakow <koshy44@gmail.com>
2022-04-04 22:26:01 -0400

    Add storage DB to copy

--- src/testdrive/src/action/sql.rs
+++ src/testdrive/src/action/sql.rs
@@ -35,7 +35,7 @@ use mz_sql_parser::ast::{
 
 use crate::action::{Action, ControlFlow, State};
 use crate::parser::{FailSqlCommand, SqlCommand, SqlErrorMatchType, SqlOutput};
-use crate::util::catalog::catalog_copy;
+use crate::util::mz_data::mzdata_copy;
 
 pub struct SqlAction {
     cmd: SqlCommand,
@@ -191,8 +191,8 @@ impl Action for SqlAction {
                 | Statement::CreateView { .. }
                 | Statement::DropDatabase { .. }
                 | Statement::DropObjects { .. } => {
-                    let temp_catalog = catalog_copy(path)?;
-                    let path = temp_catalog.path();
+                    let temp_mzdata = mzdata_copy(path)?;
+                    let path = temp_mzdata.path();
                     let disk_state = Catalog::open_debug(&path, NOW_ZERO.clone()).await?.dump();
                     let mem_state = reqwest::get(&format!(
                         "http://{}/internal/catalog",
--- src/testdrive/src/util/mz_data.rs
+++ src/testdrive/src/util/mz_data.rs
@@ -11,15 +11,18 @@ use std::fs;
 use std::path::PathBuf;
 use tempfile::TempDir;
 
-/// Creates a temporary copy of Materialize's catalog
+const CATALOG_DB_NAME: &str = "catalog";
+const STORAGE_DB_NAME: &str = "storage";
+
+/// Creates a temporary copy of Materialize's mzdata databases
 ///
 /// This is useful because running validations against the catalog can conflict with the running
 /// Materialize instance. Therefore it's better to run validations on a copy of the catalog.
-pub fn catalog_copy(catalog_path: &PathBuf) -> Result<TempDir, anyhow::Error> {
+pub fn mzdata_copy(catalog_path: &PathBuf) -> Result<TempDir, anyhow::Error> {
     let temp_dir = tempfile::tempdir()?;
     fs::copy(
-        catalog_path.join("catalog"),
-        temp_dir.path().join("catalog"),
+        catalog_path.join(CATALOG_DB_NAME),
+        temp_dir.path().join(CATALOG_DB_NAME),
     )?;
     Ok(temp_dir)
 }
