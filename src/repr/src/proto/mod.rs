3e1961354331a80baf79d3454fa60ae495468ff6
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2022-04-07 18:41:45 +0300

    repr: add `TryIntoIfSome` trait

--- src/repr/src/proto/mod.rs
+++ src/repr/src/proto/mod.rs
@@ -22,7 +22,7 @@ use std::{char::CharTryFromError, num::TryFromIntError};
 pub enum TryFromProtoError {
     /// A wrapped [`TryFromIntError`] due to failed integer downcast.
     TryFromIntError(TryFromIntError),
-    /// A wrapped [`TryFromIntError`] due to failed [`char`] conversion.
+    /// A wrapped [`CharTryFromError`] due to failed [`char`] conversion.
     CharTryFromError(CharTryFromError),
     /// Indicates an `Option<U>` field in the `Proto$T` that should be set,
     /// but for some reason it is not. In practice this should never occur.
@@ -30,7 +30,7 @@ pub enum TryFromProtoError {
 }
 
 impl TryFromProtoError {
-    // Construct a new [`TryFromProtoError::MissingField`] instance.
+    /// Construct a new [`TryFromProtoError::MissingField`] instance.
     pub fn missing_field<T: ToString>(s: T) -> TryFromProtoError {
         TryFromProtoError::MissingField(s.to_string())
     }
@@ -140,5 +140,24 @@ impl<T: ProtoRepr> ProtoRepr for Option<T> {
 
     fn from_proto(repr: Self::Repr) -> Result<Self, TryFromProtoError> {
         repr.map(T::from_proto).transpose()
+    }
+}
+
+/// Convenience syntax for trying to convert a `Self` value of type
+/// `Option<U>` to `T` if the value is `Some(value)`, or returning
+/// [`TryFromProtoError::MissingField`] if the value is `None`.
+pub trait TryIntoIfSome<T> {
+    fn try_into_if_some<S: ToString>(self, field: S) -> Result<T, TryFromProtoError>;
+}
+
+/// A blanket implementation for `Option<U>` where `U` is the
+/// `Proto$T` type for `T`.
+impl<T, U> TryIntoIfSome<T> for Option<U>
+where
+    T: TryFrom<U, Error = TryFromProtoError>,
+{
+    fn try_into_if_some<S: ToString>(self, field: S) -> Result<T, TryFromProtoError> {
+        self.ok_or_else(|| TryFromProtoError::missing_field(field))?
+            .try_into()
     }
 }
