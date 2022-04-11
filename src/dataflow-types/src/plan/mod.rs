411b7a3b54f012a47d126d17b0e6a0452fdfc592
Brennan Vincent <brennan@umanwizard.com>
2022-04-11 14:45:35 -0400

    assert -> panic

--- src/dataflow-types/src/plan/mod.rs
+++ src/dataflow-types/src/plan/mod.rs
@@ -15,7 +15,7 @@ use std::collections::BTreeMap;
 use std::collections::BTreeSet;
 use std::collections::HashMap;
 
-use mz_ore::soft_assert_or_log;
+use mz_ore::soft_panic_or_log;
 use serde::{Deserialize, Serialize, Serializer};
 
 use mz_expr::{
@@ -598,7 +598,7 @@ impl<T: timely::progress::Timestamp> Plan<T> {
                             // we shouldn't plan delta joins at all if not all of the required arrangements
                             // are available. Print an error message, to increase the chances that
                             // the user will tell us about this.
-                            soft_assert_or_log!(false, "Arrangements depended on by delta join alarmingly absent: {:?}
+                            soft_panic_or_log!("Arrangements depended on by delta join alarmingly absent: {:?}
 This is not expected to cause incorrect results, but could indicate a performance issue in Materialize.", missing);
                         } else {
                             // It's fine and expected that linear joins don't have all their arrangements available up front,
