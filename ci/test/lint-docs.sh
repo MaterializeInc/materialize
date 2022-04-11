1a0c5976e47ecd48175ba7e998bd77737e016050
ruf-io <andy@materialize.com>
2022-04-11 15:28:48 -0400

    switch to pure git

--- ci/test/lint-docs.sh
+++ ci/test/lint-docs.sh
@@ -12,13 +12,18 @@
 # lint.sh — checks for broken links and other HTML errors.
 
 set -euo pipefail
-
+# Save current branch so we can temp checkout main and switch back
+export CURRENT_BRANCH=(git rev-parse --abbrev-ref HEAD)
 git clean -ffdX ci/www/public
 hugo --gc --baseURL https://ci.materialize.com/docs --source doc/user --destination ../../ci/www/public/docs
 # To avoid breaking URLs that already exist, pull the production sitemap
 # and convert it to HTML. htmltest will scan the current prod paths and
 # complain if any have been broken in this build.
-curl https://materialize.com/docs/sitemap.xml -o ci/www/sitemap.xml
+git checkout main
+git pull
+hugo --gc --source doc/user --destination tmp
 echo "<!doctype html>" > ci/www/public/index.html
+sed ... tmp/sitemap.xml >> ci/www/public/index.html
 sed 's/<loc>\(.*\)<\/loc>/<a href="\1"><\/a>/g' ci/www/sitemap.xml >> ci/www/public/index.html
 htmltest -s ci/www/public -c doc/user/.htmltest.yml
+git checkout $(CURRENT_BRANCH)
