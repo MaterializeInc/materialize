06b88a4b11fe2d3fdd578030b990652627329f6f
Nikhil Benesch <nikhil.benesch@gmail.com>
2022-04-15 02:06:02 -0400

    materialized: add basic WebSocket API
    Add a WebSocket API to `materialized` via the /api/sql endpoint The
    protocol uses JSON to stream back the results of TAIL queries, and looks
    like this:
    
      client: {"sql": "TAIL t"}
      server: [time, diff, data]
      server: [time, diff, data]
      server: ...

--- src/materialized/src/http/sql.rs
+++ src/materialized/src/http/sql.rs
@@ -7,10 +7,16 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
 
+use axum::extract::ws::{Message, WebSocket};
+use axum::extract::WebSocketUpgrade;
 use axum::response::IntoResponse;
 use axum::Json;
+use futures::TryStreamExt;
 use http::StatusCode;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
+
+use mz_coord::SessionClient;
+use tokio::pin;
 
 use crate::http::AuthedClient;
 
@@ -27,4 +33,47 @@ pub async fn handle_sql(
         Ok(res) => Ok(Json(res)),
         Err(e) => Err((StatusCode::BAD_REQUEST, e.to_string())),
     }
+}
+
+#[derive(Serialize)]
+struct ErrorResponse {
+    error: String,
+}
+
+pub async fn handle_sql_ws(
+    AuthedClient(mut client): AuthedClient,
+    ws: WebSocketUpgrade,
+) -> impl IntoResponse {
+    ws.on_upgrade(|mut ws| async move {
+        if let Err(e) = run_ws(&mut ws, &mut client).await {
+            let _ = send_ws_response(
+                &mut ws,
+                ErrorResponse {
+                    error: e.to_string(),
+                },
+            );
+        }
+    })
+}
+
+async fn run_ws(ws: &mut WebSocket, client: &mut SessionClient) -> Result<(), anyhow::Error> {
+    let request = match ws.recv().await.transpose()? {
+        None => return Ok(()),
+        Some(request) => request.into_text()?,
+    };
+    let SqlRequest { sql } = serde_json::from_str(&request)?;
+    let rx = client.simple_tail(&sql).await?;
+    pin!(rx);
+    while let Some(row) = rx.try_next().await? {
+        send_ws_response(ws, row).await?;
+    }
+    Ok(())
+}
+
+async fn send_ws_response<T>(ws: &mut WebSocket, response: T) -> Result<(), axum::Error>
+where
+    T: Serialize,
+{
+    ws.send(Message::Text(serde_json::to_string(&response).unwrap()))
+        .await
 }
