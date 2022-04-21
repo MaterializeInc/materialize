03c6bb9ef6467e72119e909e9e2322153c928b07
Brennan Vincent <brennan@materialize.com>
2022-04-20 21:48:58 -0400

    derp

--- src/prof/build.rs
+++ src/prof/build.rs
@@ -7,8 +7,6 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
 
-use std::env;
-
 fn main() -> Result<(), anyhow::Error> {
     mz_npm::ensure()
 }
