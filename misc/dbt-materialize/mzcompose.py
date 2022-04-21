c109e511eb51c8c532a752b636552bdeddda7fb9
Nikhil Benesch <nikhil.benesch@gmail.com>
2022-04-21 15:17:25 -0400

    dbt-materialize: stop using Avro OCF sources/sinks
    Avro OCF sources and sinks are being removed since they aren't usable in
    Materialize Cloud (#11875). This commit changes the dbt test suite to
    use Kafka sources and sinks instead.
    
    We lose a little test coverage here because we can no longer verify that
    the source actually *works*, but that's okay; we've got plenty of other
    tests in Materialize for that. We'll get the test coverage back when
    Platform fully supports EOS sinks, and then we can test that a sink that
    emits static data can be reingested with the same data, all in dbt.
    
    Touches #11875.

--- misc/dbt-materialize/mzcompose.py
+++ misc/dbt-materialize/mzcompose.py
@@ -11,11 +11,21 @@ from dataclasses import dataclass
 from typing import Dict, List, Optional
 
 from materialize.mzcompose import Composition, WorkflowArgumentParser
-from materialize.mzcompose.services import Materialized, Service, TestCerts
+from materialize.mzcompose.services import (
+    Kafka,
+    Materialized,
+    SchemaRegistry,
+    Service,
+    TestCerts,
+    Zookeeper,
+)
 
 SERVICES = [
     TestCerts(),
     Materialized(),
+    Zookeeper(),
+    Kafka(),
+    SchemaRegistry(),
     Service(
         "dbt-test",
         {
@@ -45,16 +55,12 @@ test_cases = [
     TestCase(
         name="no-tls-head",
         materialized_options=[],
-        dbt_env={
-            "DBT_HOST": "materialized",
-        },
+        dbt_env={},
     ),
     TestCase(
         name="no-tls-min-supported-version",
         materialized_options=[],
-        dbt_env={
-            "DBT_HOST": "materialized",
-        },
+        dbt_env={},
         materialized_image="materialize/materialized:v0.20.0",
     ),
     TestCase(
@@ -66,7 +72,6 @@ test_cases = [
             "--tls-ca=/secrets/ca.crt",
         ],
         dbt_env={
-            "DBT_HOST": "materialized",
             "DBT_SSLCERT": "/secrets/materialized.crt",
             "DBT_SSLKEY": "/secrets/materialized.key",
             "DBT_SSLROOTCERT": "/secrets/ca.crt",
@@ -95,11 +100,19 @@ def workflow_default(c: Composition, parser: WorkflowArgumentParser) -> None:
             with c.test_case(test_case.name):
                 with c.override(materialized):
                     c.down()
+                    c.start_and_wait_for_tcp(
+                        services=["zookeeper", "kafka", "schema-registry"]
+                    )
                     c.up("materialized")
                     c.wait_for_tcp(host="materialized", port=6875)
                     c.run(
                         "dbt-test",
                         "pytest",
                         "dbt-materialize/test",
-                        env_extra=test_case.dbt_env,
+                        env_extra={
+                            "DBT_HOST": "materialized",
+                            "KAFKA_ADDR": "kafka:9092",
+                            "SCHEMA_REGISTRY_URL": "http://schema-registry:8081",
+                            **test_case.dbt_env,
+                        },
                     )
