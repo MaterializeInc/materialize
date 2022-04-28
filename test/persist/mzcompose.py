2d36154e328a37c8c5fad6ccb1740074b02e0888
Daniel Harrison <52528+danhhz@users.noreply.github.com>
2022-04-27 19:21:20 -0700

    persist: hook maelstrom up to ci

---
+++ test/persist/mzcompose.py
@@ -0,0 +1,23 @@
+# Copyright Materialize, Inc. and contributors. All rights reserved.
+#
+# Use of this software is governed by the Business Source License
+# included in the LICENSE file at the root of this repository.
+#
+# As of the Change Date specified in that file, in accordance with
+# the Business Source License, use of this software will be governed
+# by the Apache License, Version 2.0.
+
+from materialize.mzcompose import Composition
+from materialize.mzcompose.services import Service
+
+SERVICES = [
+    Service(
+        "maelstrom-persist",
+        {"mzbuild": "maelstrom-persist", "volumes": ["./maelstrom:/store"]},
+    ),
+]
+
+
+def workflow_default(c: Composition) -> None:
+    """Run the nemesis for 5 seconds as a smoke test."""
+    c.run("maelstrom-persist", "--time-limit=5", "--", "maelstrom")
