3724127d7c6893b7d8a85e7d294817d6dc68a8d6
Joseph Koshakow <koshy44@gmail.com>
2022-05-18 18:58:18 -0400

    test

--- src/materialized/tests/sql.rs
+++ src/materialized/tests/sql.rs
@@ -84,13 +84,15 @@ fn test_linearizability() -> Result<(), Box<dyn Error>> {
     println!("Finished PG operations");
 
     let _ = mz_client.batch_execute(&"SELECT 1;")?;
+    // :'(
+    thread::sleep(Duration::from_secs(5));
     // Create postgres source in Materialize
-    // mz_client.batch_execute(
-    //     &"CREATE SOURCE mz_source
-    //         FROM POSTGRES
-    //         CONNECTION 'host=localhost port=5432 user=postgres dbname=postgres'
-    //         PUBLICATION 'mz_source';",
-    // )?;
+    mz_client.batch_execute(
+        &"CREATE SOURCE mz_source
+            FROM POSTGRES
+            CONNECTION 'host=localhost port=5432 user=postgres dbname=postgres'
+            PUBLICATION 'mz_source';",
+    )?;
     /*mz_client.batch_execute(&"CREATE MATERIALIZED VIEWS FROM SOURCE mz_source (v);")?;
 
     // Create user table in Materialize
