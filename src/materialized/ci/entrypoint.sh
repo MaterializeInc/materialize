cf30eac6b37e007f4e9a429cbb990b140d05cc51
Brennan Vincent <brennan@umanwizard.com>
2022-06-01 01:50:09 -0400

    Use persist/catalog URLs that are already set in entrypoint.sh

--- src/materialized/ci/entrypoint.sh
+++ src/materialized/ci/entrypoint.sh
@@ -16,8 +16,9 @@ pg_ctlcluster 14 materialize start
 psql -Atc "CREATE SCHEMA IF NOT EXISTS consensus"
 psql -Atc "CREATE SCHEMA IF NOT EXISTS catalog"
 
+export MZ_PERSIST_CONSENSUS_URL="${MZ_PERSIST_CONSENSUS_URL:-postgresql://materialize@%2Ftmp?options=--search_path=consensus}"
+export MZ_CATALOG_POSTGRES_STASH="${MZ_CATALOG_POSTGRES_STASH:-postgresql://materialize@%2Ftmp?options=--search_path=catalog}"
+
 exec materialized \
     --listen-addr=0.0.0.0:6875 \
-    --persist-consensus-url=postgresql://materialize@%2Ftmp?options=--search_path=consensus \
-    --catalog-postgres-stash=postgresql://materialize@%2Ftmp?options=--search_path=catalog \
     "$@"
