5443753e4c07a9851ab606e573bac40947872c28
Daniel Harrison <52528+danhhz@users.noreply.github.com>
2022-06-03 13:32:05 -0700

    persist: copy in differential_dataflow's Spine unchanged
    We're about to fork it. This is Spine exactly as of [5a013e02c] with the
    following modifications:
    - added our copyright header
    - rustfmt
    - fixup imports
    - remove the Trace and TraceReader impls (in service of the following)
    - reorder some methods
    
    [f5a013e02coo]: https://github.com/TimelyDataflow/differential-dataflow/blob/5a013e02cc21b325d81520c5b8a8a2301f4ba5ab/src/trace/implementations/spine_fueled.rs

--- src/persist-client/src/lib.rs
+++ src/persist-client/src/lib.rs
@@ -50,6 +50,7 @@ pub(crate) mod r#impl {
     pub mod machine;
     pub mod metrics;
     pub mod state;
+    pub mod trace;
 }
 
 // TODO: Remove this in favor of making it possible for all PersistClients to be
