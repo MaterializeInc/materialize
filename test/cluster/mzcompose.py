7b5ff35afd7ffd3b4b0b04af7fff901dd43cb04f
Lukas Humbel <lukas@materialize.com>
2022-06-07 17:18:51 +0200

    add network interrupt to cluster test

--- test/cluster/mzcompose.py
+++ test/cluster/mzcompose.py
@@ -9,6 +9,7 @@
 
 import time
 from pathlib import Path
+from textwrap import dedent
 
 from pg8000.dbapi import ProgrammingError
 
@@ -21,8 +22,16 @@ from materialize.mzcompose.services import (
     Materialized,
     SchemaRegistry,
     Testdrive,
+    Toxiproxy,
     Zookeeper,
 )
+
+PROXIES = {
+    "computed_1:2100": "toxiproxy:3001",
+    "computed_2:2100": "toxiproxy:3002",
+    "computed_3:2100": "toxiproxy:3003",
+    "computed_4:2100": "toxiproxy:3004",
+}
 
 SERVICES = [
     Zookeeper(),
@@ -50,6 +59,7 @@ SERVICES = [
         ports=[2100, 2102],
     ),
     Materialized(),
+    Toxiproxy(),
     Testdrive(
         volumes=[
             "mzdata:/mzdata",
@@ -60,6 +70,50 @@ SERVICES = [
         materialized_params={"cluster": "cluster1"},
     ),
 ]
+
+
+def toxiproxy_setup(c: Composition) -> None:
+    for dest, listen in PROXIES.items():
+        dest_host, _ = dest.split(":")
+        listen_host, listen_port = listen.split(":")
+        assert listen_host == "toxiproxy"
+        x = dedent(
+            f"""
+        $ http-request method=POST url=http://toxiproxy:8474/proxies content-type=application/json
+        {{
+            "name": "{dest_host}",
+            "listen": "0.0.0.0:{listen_port}",
+            "upstream": "{dest}"
+        }}"""
+        )
+
+        # Override to disable the cluster argument
+        with c.override(
+            Testdrive(
+                validate_data_dir=False,
+                no_reset=True,
+            )
+        ):
+            c.run("testdrive", stdin=x)
+
+
+def toxiproxy_set_enabled(c: Composition, name: str, enabled: bool) -> None:
+    with c.override(
+        Testdrive(
+            validate_data_dir=False,
+            no_reset=True,
+        )
+    ):
+        enable_json = "true" if enabled else "false"
+        td = dedent(
+            f"""
+            $ http-request method=POST url=http://toxiproxy:8474/proxies/{name} content-type=application/json
+            {{
+              "enabled": {enable_json}
+            }}
+            """
+        )
+        c.run("testdrive", stdin=td)
 
 
 def workflow_default(c: Composition) -> None:
@@ -85,15 +139,16 @@ def workflow_nightly(c: Composition) -> None:
 
 
 def test_cluster(c: Composition, *glob: str) -> None:
-    c.up("materialized")
-    c.wait_for_materialized()
+    c.start_and_wait_for_tcp(services=["materialized", "toxiproxy"])
+    c.up("testdrive", persistent=True)
+    toxiproxy_setup(c)
 
     # Create a remote cluster and verify that tests pass.
     c.up("computed_1")
     c.up("computed_2")
     c.sql("DROP CLUSTER IF EXISTS cluster1 CASCADE;")
     c.sql(
-        "CREATE CLUSTER cluster1 REPLICAS (replica1 (REMOTE ('computed_1:2100', 'computed_2:2100')));"
+        f"CREATE CLUSTER cluster1 REPLICAS (replica1 (REMOTE ('{PROXIES['computed_1:2100']}', '{PROXIES['computed_2:2100']}')));"
     )
     c.run("testdrive", *glob)
 
@@ -101,7 +156,7 @@ def test_cluster(c: Composition, *glob: str) -> None:
     c.up("computed_3")
     c.up("computed_4")
     c.sql(
-        "CREATE CLUSTER REPLICA cluster1.replica2 REMOTE ('computed_3:2100', 'computed_4:2100')"
+        f"CREATE CLUSTER REPLICA cluster1.replica2 REMOTE ('{PROXIES['computed_3:2100']}', '{PROXIES['computed_4:2100']}')"
     )
     c.run("testdrive", *glob)
 
@@ -112,6 +167,11 @@ def test_cluster(c: Composition, *glob: str) -> None:
 
     # Leave only replica 2 up and verify that tests still pass.
     c.sql("DROP CLUSTER REPLICA cluster1.replica1")
+    c.run("testdrive", *glob)
+
+    # Test re-connection to replica2
+    toxiproxy_set_enabled(c, "computed_3", False)
+    toxiproxy_set_enabled(c, "computed_3", True)
     c.run("testdrive", *glob)
 
 
