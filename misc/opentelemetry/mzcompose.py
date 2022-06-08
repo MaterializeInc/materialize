9c59372215aea38eab0611d53063fae8243dc364
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2022-06-08 17:00:55 +0300

    misc/opentelemetry: remove unneeded imports from `mzcompose.py`

--- misc/opentelemetry/mzcompose.py
+++ misc/opentelemetry/mzcompose.py
@@ -7,10 +7,6 @@
 # the Business Source License, use of this software will be governed
 # by the Apache License, Version 2.0.
 
-from dataclasses import dataclass
-from typing import Dict, List, Optional
-
-from materialize.mzcompose import Composition, WorkflowArgumentParser
 from materialize.mzcompose.services import Service
 
 SERVICES = [
