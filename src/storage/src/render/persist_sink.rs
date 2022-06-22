54eac0b60b1ce3726aa97dd4506bf72ca626f84b
Gus Wynn <gus@materialize.com>
2022-06-22 08:52:49 -0700

    storage: minimize the the number of created batches in persist_sink

--- src/storage/src/render/persist_sink.rs
+++ src/storage/src/render/persist_sink.rs
@@ -8,11 +8,10 @@
 // by the Apache License, Version 2.0.
 
 use std::any::Any;
-use std::collections::HashMap;
 use std::rc::Rc;
 use std::sync::Arc;
 
-use differential_dataflow::{Collection, Hashable};
+use differential_dataflow::{lattice::Lattice, Collection, Hashable};
 use timely::dataflow::channels::pact::Exchange;
 use timely::dataflow::operators::generic::builder_rc::OperatorBuilder;
 use timely::dataflow::Scope;
@@ -23,10 +22,64 @@ use timely::PartialOrder;
 use mz_dataflow_types::client::controller::storage::CollectionMetadata;
 use mz_dataflow_types::sources::SourceData;
 use mz_dataflow_types::DataflowError;
+use mz_persist_client::batch::BatchBuilder;
 use mz_repr::{Diff, GlobalId, Row, Timestamp};
+
 use mz_timely_util::operators_async_ext::OperatorBuilderExt;
 
 use crate::storage_state::StorageState;
+
+enum SinkState<K, V, T: mz_persist_types::Codec64 + timely::progress::Timestamp + Lattice, D> {
+    Empty,
+    NewBatch {
+        batch: BatchBuilder<K, V, T, D>,
+        batch_upper: Antichain<T>,
+        fresh: bool,
+    },
+    ClosingBatchAndNewBatch {
+        closing_batch: BatchBuilder<K, V, T, D>,
+        final_batch_upper: Antichain<T>,
+        batch: BatchBuilder<K, V, T, D>,
+        batch_upper: Antichain<T>,
+        fresh: bool,
+    },
+}
+
+impl<K, V, T: mz_persist_types::Codec64 + timely::progress::Timestamp + Lattice, D>
+    SinkState<K, V, T, D>
+{
+    fn batch(self) -> (BatchBuilder<K, V, T, D>, Antichain<T>) {
+        match self {
+            SinkState::NewBatch {
+                batch, batch_upper, ..
+            } => (batch, batch_upper),
+            SinkState::ClosingBatchAndNewBatch {
+                batch, batch_upper, ..
+            } => (batch, batch_upper),
+            SinkState::Empty => panic!(),
+        }
+    }
+
+    fn closed_batch(
+        self,
+    ) -> (
+        BatchBuilder<K, V, T, D>,
+        Antichain<T>,
+        BatchBuilder<K, V, T, D>,
+        Antichain<T>,
+    ) {
+        match self {
+            SinkState::ClosingBatchAndNewBatch {
+                batch,
+                batch_upper,
+                closing_batch,
+                final_batch_upper,
+                ..
+            } => (batch, batch_upper, closing_batch, final_batch_upper),
+            _ => panic!(),
+        }
+    }
+}
 
 pub fn render<G>(
     scope: &mut G,
@@ -64,7 +117,8 @@ pub fn render<G>(
         move |mut capabilities, frontiers, scheduler| async move {
             capabilities.clear();
             let mut buffer = Vec::new();
-            let mut stashed_batches = HashMap::new();
+
+            let mut sink_state = SinkState::Empty;
 
             let mut write = persist_clients
                 .lock()
@@ -93,89 +147,227 @@ pub fn render<G>(
                     // (100 was chosen arbitrarily), and avoid having to make a batch
                     // per-timestamp.
                     for (row, ts, diff) in buffer.drain(..) {
-                        stashed_batches
-                            .entry(ts)
-                            .or_insert_with(|| {
-                                // TODO: the lower has to be the min because we don't know
-                                // what the minimum ts of data we will see is. In the future,
-                                // this lower should be declared in `finish` instead.
-                                write.builder(100, Antichain::from_elem(Timestamp::minimum()))
-                            })
+                        let (batch, upper) = match &mut sink_state {
+                            SinkState::Empty => {
+                                sink_state = SinkState::NewBatch {
+                                    // TODO: the lower has to be the min because we don't know
+                                    // what the minimum ts of data we will see is. In the future,
+                                    // this lower should be declared in `finish` instead.
+                                    batch: write
+                                        .builder(100, Antichain::from_elem(Timestamp::minimum())),
+                                    batch_upper: Antichain::from_elem(Timestamp::minimum()),
+                                    fresh: true,
+                                };
+
+                                if let SinkState::NewBatch {
+                                    batch,
+                                    batch_upper,
+                                    fresh,
+                                } = &mut sink_state
+                                {
+                                    *fresh = false;
+                                    (batch, batch_upper)
+                                } else {
+                                    unreachable!()
+                                }
+                            }
+                            SinkState::NewBatch {
+                                batch,
+                                batch_upper,
+                                fresh,
+                            } => {
+                                *fresh = false;
+                                (batch, batch_upper)
+                            }
+                            SinkState::ClosingBatchAndNewBatch {
+                                closing_batch,
+                                final_batch_upper,
+                                batch,
+                                batch_upper,
+                                fresh,
+                            } => {
+                                if PartialOrder::less_than(
+                                    &Antichain::from_elem(ts),
+                                    final_batch_upper,
+                                ) {
+                                    (closing_batch, final_batch_upper)
+                                } else {
+                                    *fresh = false;
+                                    (batch, batch_upper)
+                                }
+                            }
+                        };
+
+                        batch
                             .add(&SourceData(row), &(), &ts, &diff)
                             .await
                             .expect("invalid usage");
+                        // ANTICHAIN
+                        *upper = Lattice::join(&Antichain::from_elem(ts + 1), upper);
                     }
                 }
 
                 // See if any timestamps are done!
-                // TODO(guswynn/petrosagg): remove this additional allocation
-                let mut finalized_timestamps: Vec<_> = stashed_batches
-                    .keys()
-                    .filter(|ts| !input_frontier.less_equal(ts))
-                    .copied()
-                    .collect();
-                finalized_timestamps.sort_unstable();
-
                 // If the frontier has advanced, we need to finalize data being written to persist
                 if PartialOrder::less_than(&*shared_frontier.borrow(), &input_frontier) {
-                    // We always append, even in case we don't have any updates, because appending
-                    // also advances the frontier.
-                    if finalized_timestamps.is_empty() {
-                        let expected_upper = shared_frontier.borrow().clone();
-                        write
-                            .compare_and_append(
-                                Vec::<((SourceData, ()), Timestamp, Diff)>::new(),
-                                expected_upper,
-                                input_frontier.clone(),
-                            )
-                            .await
-                            .expect("cannot append updates")
-                            .expect("cannot append updates")
-                            .expect("invalid/outdated upper");
+                    let possible_batch = match &mut sink_state {
+                        SinkState::Empty | SinkState::NewBatch { fresh: true, .. } => {
+                            // TODO ADVANCE EMPTY
+                            (None, None)
+                        }
+                        SinkState::NewBatch { .. } => {
+                            // if our batch is already past the frontier, then we change the
+                            // sink state only, otherwise we return the current batch to be
+                            // finalized and closed.
+                            let current_batch =
+                                std::mem::replace(&mut sink_state, SinkState::Empty);
+                            let (batch, batch_upper) = current_batch.batch();
 
-                        // advance our stashed frontier
-                        *shared_frontier.borrow_mut() = input_frontier.clone();
-                        // wait for more data or a new input frontier
-                        continue;
+                            if PartialOrder::less_than(&input_frontier, &batch_upper) {
+                                sink_state = SinkState::ClosingBatchAndNewBatch {
+                                    closing_batch: batch,
+                                    final_batch_upper: batch_upper,
+                                    batch: write
+                                        .builder(100, Antichain::from_elem(Timestamp::minimum())),
+                                    batch_upper: Antichain::from_elem(Timestamp::minimum()),
+                                    fresh: true,
+                                };
+                                (None, None)
+                            } else {
+                                (Some((batch, batch_upper)), None)
+                            }
+                        }
+                        SinkState::ClosingBatchAndNewBatch { fresh, .. } => {
+                            // if our batch is already past the frontier, then we change the
+                            // sink state only, otherwise we return the current batch to be
+                            // finalized and closed.
+
+                            let fresh = *fresh;
+                            let current_batch =
+                                std::mem::replace(&mut sink_state, SinkState::Empty);
+
+                            let (batch, batch_upper, closing_batch, final_batch_upper) =
+                                current_batch.closed_batch();
+
+                            if !fresh && PartialOrder::less_equal(&batch_upper, &input_frontier) {
+                                // If the frontier is pass the batch_upper, then its pass the
+                                // previous batch as well
+                                sink_state = SinkState::Empty;
+                                (
+                                    Some((batch, batch_upper)),
+                                    Some((closing_batch, final_batch_upper)),
+                                )
+                            } else if PartialOrder::less_equal(&final_batch_upper, &input_frontier)
+                            {
+                                sink_state = SinkState::NewBatch {
+                                    batch,
+                                    batch_upper,
+                                    fresh,
+                                };
+                                (None, Some((closing_batch, final_batch_upper)))
+                            } else {
+                                // NEITHER is ahead, we just continue ahead
+
+                                sink_state = SinkState::ClosingBatchAndNewBatch {
+                                    closing_batch,
+                                    final_batch_upper,
+                                    batch,
+                                    batch_upper,
+                                    fresh,
+                                };
+                                (None, None)
+                            }
+                        }
+                    };
+
+                    match possible_batch {
+                        (None, None) => {
+                            // We always append, even in case we don't have any updates, because appending
+                            // also advances the frontier.
+                            let expected_upper = shared_frontier.borrow().clone();
+                            write
+                                .compare_and_append(
+                                    Vec::<((SourceData, ()), Timestamp, Diff)>::new(),
+                                    expected_upper,
+                                    input_frontier.clone(),
+                                )
+                                .await
+                                .expect("cannot append updates")
+                                .expect("cannot append updates")
+                                .expect("invalid/outdated upper");
+
+                            // advance our stashed frontier
+                            *shared_frontier.borrow_mut() = input_frontier.clone();
+                        }
+                        (None, Some((batch, _batch_upper))) => {
+                            let mut batch = batch
+                                .finish(input_frontier.clone())
+                                .await
+                                .expect("invalid usage");
+                            let expected_upper = shared_frontier.borrow().clone();
+                            write
+                                .compare_and_append_batch(
+                                    &mut batch,
+                                    expected_upper,
+                                    input_frontier.clone(),
+                                )
+                                .await
+                                .expect("cannot append updates")
+                                .expect("cannot append updates")
+                                .expect("invalid/outdated upper");
+
+                            // advance our stashed frontier
+                            *shared_frontier.borrow_mut() = input_frontier.clone();
+                        }
+                        (Some((batch, _batch_upper)), closing_batch) => {
+                            // TODO(aljoscha): Figure out how errors from this should be reported.
+                            //
+                            let expected_upper =
+                                if let Some((closing_batch, final_batch_upper)) = closing_batch {
+                                    let mut batch = closing_batch
+                                        .finish(final_batch_upper.clone())
+                                        .await
+                                        .expect("invalid usage");
+                                    let expected_upper = shared_frontier.borrow().clone();
+                                    write
+                                        .compare_and_append_batch(
+                                            &mut batch,
+                                            expected_upper,
+                                            final_batch_upper.clone(),
+                                        )
+                                        .await
+                                        .expect("cannot append updates")
+                                        .expect("cannot append updates")
+                                        .expect("invalid/outdated upper");
+
+                                    final_batch_upper
+                                } else {
+                                    shared_frontier.borrow().clone()
+                                };
+
+                            let mut batch = batch
+                                .finish(input_frontier.clone())
+                                .await
+                                .expect("invalid usage");
+                            write
+                                .compare_and_append_batch(
+                                    &mut batch,
+                                    expected_upper,
+                                    input_frontier.clone(),
+                                )
+                                .await
+                                .expect("cannot append updates")
+                                .expect("cannot append updates")
+                                .expect("invalid/outdated upper");
+
+                            // advance our stashed frontier
+                            *shared_frontier.borrow_mut() = input_frontier.clone();
+                        }
                     }
-
-                    // `shared_frontier` tracks the last known upper
-                    let mut expected_upper = shared_frontier.borrow().clone();
-                    let finalized_batch_count = finalized_timestamps.len();
-
-                    for (i, ts) in finalized_timestamps.into_iter().enumerate() {
-                        // TODO(aljoscha): Figure out how errors from this should be reported.
-
-                        // Set the upper to the upper of the batch (which is 1 past the ts it
-                        // manages) OR the new frontier if we are appending the final batch
-                        let new_upper = if i == finalized_batch_count - 1 {
-                            input_frontier.clone()
-                        } else {
-                            Antichain::from_elem(ts + 1)
-                        };
-
-                        let mut batch = stashed_batches
-                            .remove(&ts)
-                            .expect("batch for timestamp to still be there")
-                            .finish(new_upper.clone())
-                            .await
-                            .expect("invalid usage");
-
-                        write
-                            .compare_and_append_batch(&mut batch, expected_upper, new_upper.clone())
-                            .await
-                            .expect("cannot append updates")
-                            .expect("cannot append updates")
-                            .expect("invalid/outdated upper");
-
-                        // next `expected_upper` is the one we just successfully appended
-                        expected_upper = new_upper;
-                    }
-                    // advance our stashed frontier
-                    *shared_frontier.borrow_mut() = input_frontier.clone();
                 } else {
                     // We cannot have updates without the frontier advancing
-                    assert!(finalized_timestamps.is_empty());
+                    // TODO HOW TO DO THIS CHECK
+                    //assert!(finalized_timestamps.is_empty());
                 }
             }
         },
