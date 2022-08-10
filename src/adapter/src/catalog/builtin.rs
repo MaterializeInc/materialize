36020cbdf677cfad249532842f07ea0fa1de8283
Joseph Koshakow <koshy44@gmail.com>
2022-08-10 17:28:34 -0400

    Fix new role name

--- src/adapter/src/catalog/builtin.rs
+++ src/adapter/src/catalog/builtin.rs
@@ -2107,8 +2107,8 @@ FROM mz_catalog.mz_roles r",
 
 pub const MZ_SYSTEM: BuiltinRole = BuiltinRole { name: "mz_system" };
 
-pub const HTTP_DEFAULT_USER: BuiltinRole = BuiltinRole {
-    name: "http_default_user",
+pub const MZ_HTTP_DEFAULT_USER: BuiltinRole = BuiltinRole {
+    name: "mz_http_default_user",
 };
 
 pub static BUILTINS_STATIC: Lazy<Vec<Builtin<NameReference>>> = Lazy::new(|| {
@@ -2293,7 +2293,8 @@ pub static BUILTINS_STATIC: Lazy<Vec<Builtin<NameReference>>> = Lazy::new(|| {
 
     builtins
 });
-pub static BUILTIN_ROLES: Lazy<Vec<BuiltinRole>> = Lazy::new(|| vec![MZ_SYSTEM, HTTP_DEFAULT_USER]);
+pub static BUILTIN_ROLES: Lazy<Vec<BuiltinRole>> =
+    Lazy::new(|| vec![MZ_SYSTEM, MZ_HTTP_DEFAULT_USER]);
 
 #[allow(non_snake_case)]
 pub mod BUILTINS {
