721b4677f978d02b458879a5cad6112991fdce8e
Philip Stoev <pstoev@materialize.io>
2022-08-10 16:45:56 +0200

    cloudtest: Tests for antiaffinity and availability zones
    - expand the KinD cluster to 3 worker nodes
    - tag each worker with its name as the availability zone
    - add various tests that create clusters and replicas and
      confirm that they are properly spread out across availability
      zones
    
    Relates to #13580

--- misc/python/materialize/cloudtest/k8s/environmentd.py
+++ misc/python/materialize/cloudtest/k8s/environmentd.py
@@ -76,6 +76,9 @@ class EnvironmentdStatefulSet(K8sStatefulSet):
             args=[
                 "--storaged-image=" + self.image("storaged"),
                 "--computed-image=" + self.image("computed"),
+                "--availability-zone=kind-worker",
+                "--availability-zone=kind-worker2",
+                "--availability-zone=kind-worker3",
                 f"--persist-blob-url=s3://minio:minio123@persist/persist?endpoint={s3_endpoint}&region=minio",
                 "--orchestrator=kubernetes",
                 "--orchestrator-kubernetes-image-pull-policy=never",
