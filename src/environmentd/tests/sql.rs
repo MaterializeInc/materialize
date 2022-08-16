MaterializeInc/materialize

commit 041f326fdea44508fb80b22246a9815f2d3548a6

adapter: Prevent login to mz_system externally (#14093)
mz_system is the system user that is responsible for executing commands
that only an administrator should be doing. This commit prevents
logging into the mz_system user from an external port. You can only
login to the mz_system user via internal ports.

Works towards resolving: MaterializeInc/cloud#3292

src/adapter/src/catalog.rs
@@ -93,7 +93,8 @@ pub mod builtin;
 pub mod storage;
 
 pub const SYSTEM_CONN_ID: ConnectionId = 0;
-const SYSTEM_USER: &str = "mz_system";
+pub const SYSTEM_USER: &str = "mz_system";
+pub const HTTP_DEFAULT_USER: &str = "anonymous_http_user";
 const CREATE_SQL_TODO: &str = "TODO";
 
 /// A `Catalog` keeps track of the SQL objects known to the planner.

src/adapter/src/client.rs
@@ -20,6 +20,7 @@ use mz_ore::thread::JoinOnDropHandle;
 use mz_repr::{Datum, GlobalId, Row, ScalarType};
 use mz_sql::ast::{Raw, Statement};
 
+use crate::catalog::SYSTEM_USER;
 use crate::command::{
     Canceled, Command, ExecuteResponse, Response, SimpleExecuteResponse, SimpleResult,
     StartupResponse,
@@ -67,6 +68,15 @@ impl Handle {
     }
 }
 
+/// Distinguishes between external and internal clients.
+#[derive(Debug, Clone)]
+pub enum ClientType {
+    /// Client that has connected over an external port.
+    External,
+    /// Client that has connected over an internal port.
+    Internal,
+}
+
 /// A coordinator client.
 ///
 /// A coordinator client is a simple handle to a communication channel with the
@@ -78,13 +88,15 @@ impl Handle {
 pub struct Client {
     cmd_tx: mpsc::UnboundedSender<Command>,
     id_alloc: Arc<IdAllocator<ConnectionId>>,
+    client_type: ClientType,
 }
 
 impl Client {
-    pub(crate) fn new(cmd_tx: mpsc::UnboundedSender<Command>) -> Client {
+    pub(crate) fn new(cmd_tx: mpsc::UnboundedSender<Command>, client_type: ClientType) -> Client {
         Client {
             cmd_tx,
             id_alloc: Arc::new(IdAllocator::new(1, 1 << 16)),
+            client_type,
         }
     }
 
@@ -103,7 +115,7 @@ impl Client {
     /// a system user.
     pub async fn system_execute(&self, stmts: &str) -> Result<SimpleExecuteResponse, AdapterError> {
         let conn_client = self.new_conn()?;
-        let session = Session::new(conn_client.conn_id(), "mz_system".into());
+        let session = Session::new(conn_client.conn_id(), SYSTEM_USER.into());
         let (mut session_client, _) = conn_client.startup(session, false).await?;
         session_client.simple_execute(stmts).await
     }
@@ -117,6 +129,11 @@ impl Client {
     pub async fn system_execute_one(&self, stmt: &str) -> Result<SimpleResult, AdapterError> {
         let response = self.system_execute(stmt).await?;
         Ok(response.results.into_element())
+    }
+
+    /// Returns the `ClientType` associated with this client.
+    pub fn client_type(&self) -> &ClientType {
+        &self.client_type
     }
 }
 
@@ -136,6 +153,11 @@ impl ConnClient {
     /// Returns the ID of the connection associated with this client.
     pub fn conn_id(&self) -> ConnectionId {
         self.conn_id
+    }
+
+    /// Returns the `ClientType` associated with this client.
+    pub fn client_type(&self) -> &ClientType {
+        self.inner.client_type()
     }
 
     /// Upgrades this connection client to a session client.

src/adapter/src/coord.rs
@@ -78,7 +78,6 @@ use chrono::{DateTime, Utc};
 use derivative::Derivative;
 use futures::StreamExt;
 use itertools::Itertools;
-use mz_ore::tracing::OpenTelemetryContext;
 use rand::seq::SliceRandom;
 use timely::progress::Timestamp as _;
 use tokio::runtime::Handle as TokioHandle;
@@ -95,6 +94,7 @@ use mz_ore::metrics::MetricsRegistry;
 use mz_ore::now::NowFn;
 use mz_ore::stack;
 use mz_ore::thread::JoinHandleExt;
+use mz_ore::tracing::OpenTelemetryContext;
 use mz_persist_client::usage::StorageUsageClient;
 use mz_persist_client::ShardId;
 use mz_repr::{Datum, Diff, GlobalId, Row, Timestamp};
@@ -114,7 +114,7 @@ use crate::catalog::{
     self, storage, BuiltinMigrationMetadata, BuiltinTableUpdate, Catalog, CatalogItem,
     ClusterReplicaSizeMap, StorageHostSizeMap, StorageSinkConnectionState,
 };
-use crate::client::{Client, ConnectionId, Handle};
+use crate::client::{Client, ClientType, ConnectionId, Handle};
 use crate::command::{Canceled, Command, ExecuteResponse};
 use crate::coord::appends::{BuiltinTableUpdateSource, Deferred, PendingWriteTxn};
 use crate::coord::id_bundle::CollectionIdBundle;
@@ -815,7 +815,7 @@ pub async fn serve<S: Append + 'static>(
         connection_context,
         storage_usage_client,
     }: Config<S>,
-) -> Result<(Handle, Client), AdapterError> {
+) -> Result<(Handle, Client, Client), AdapterError> {
     let (cmd_tx, cmd_rx) = mpsc::unbounded_channel();
     let (internal_cmd_tx, internal_cmd_rx) = mpsc::unbounded_channel();
     let (strict_serializable_reads_tx, strict_serializable_reads_rx) = mpsc::unbounded_channel();
@@ -933,8 +933,9 @@ pub async fn serve<S: Append + 'static>(
                 start_instant,
                 _thread: thread.join_on_drop(),
             };
-            let client = Client::new(cmd_tx);
-            Ok((handle, client))
+            let external_client = Client::new(cmd_tx.clone(), ClientType::External);
+            let internal_client = Client::new(cmd_tx, ClientType::Internal);
+            Ok((handle, external_client, internal_client))
         }
         Err(e) => Err(e),
     }

src/environmentd/src/http.rs
@@ -34,9 +34,6 @@ use http::header::{AUTHORIZATION, CONTENT_TYPE};
 use http::{Request, StatusCode};
 use hyper::server::conn::AddrIncoming;
 use hyper_openssl::MaybeHttpsStream;
-use mz_adapter::SessionClient;
-use mz_ore::metrics::MetricsRegistry;
-use mz_ore::tracing::OpenTelemetryEnableCallback;
 use openssl::nid::Nid;
 use openssl::ssl::{Ssl, SslContext};
 use openssl::x509::X509;
@@ -47,8 +44,12 @@ use tokio_openssl::SslStream;
 use tower_http::cors::{AllowOrigin, Any, CorsLayer};
 use tracing::{error, warn};
 
+use mz_adapter::catalog::HTTP_DEFAULT_USER;
 use mz_adapter::session::Session;
+use mz_adapter::SessionClient;
 use mz_frontegg_auth::{FronteggAuthentication, FronteggError};
+use mz_ore::metrics::MetricsRegistry;
+use mz_ore::tracing::OpenTelemetryEnableCallback;
 
 use crate::BUILD_INFO;
 
@@ -56,8 +57,6 @@ mod catalog;
 mod memory;
 mod root;
 mod sql;
-
-const SYSTEM_USER: &str = "mz_system";
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -113,11 +112,11 @@ impl Server {
             )
             .nest("/prof/", mz_prof::http::router(&BUILD_INFO))
             .route("/static/*path", routing::get(root::handle_static))
-            .layer(Extension(adapter_client))
             .layer(middleware::from_fn(move |req, next| {
                 let frontegg = Arc::clone(&frontegg);
                 async move { auth(req, next, tls_mode, &frontegg).await }
             }))
+            .layer(Extension(adapter_client))
             .layer(
                 CorsLayer::new()
                     .allow_credentials(false)
@@ -221,6 +220,8 @@ enum AuthError {
     HttpsRequired,
     #[error("invalid username in client certificate")]
     InvalidCertUserName,
+    #[error("unauthorized login to user '{0}'")]
+    InvalidLogin(String),
     #[error("{0}")]
     Frontegg(#[from] FronteggError),
     #[error("missing authorization header")]
@@ -277,8 +278,8 @@ async fn auth<B>(
     // Then, handle Frontegg authentication if required.
     let user = match frontegg {
         // If no Frontegg authentication, we can use the cert's username if
-        // present, otherwise the system user.
-        None => user.unwrap_or_else(|| SYSTEM_USER.to_string()),
+        // present, otherwise the default HTTP user.
+        None => user.unwrap_or_else(|| HTTP_DEFAULT_USER.to_string()),
         // If we require Frontegg auth, fetch credentials from the HTTP auth
         // header. Basic auth comes with a username/password, where the password
         // is the client+secret pair. Bearer auth is an existing JWT that must
@@ -308,11 +309,21 @@ async fn auth<B>(
         }
     };
 
+    // Validate that mz_system only logs in via an internal port.
+    let adapter_client = req.extensions().get::<mz_adapter::Client>().unwrap();
+    match (adapter_client.client_type(), user.as_str()) {
+        (mz_adapter::client::ClientType::External, mz_adapter::catalog::SYSTEM_USER) => {
+            return Err(AuthError::InvalidLogin(user))
+        }
+        (mz_adapter::client::ClientType::Internal, _)
+        | (mz_adapter::client::ClientType::External, _) => {}
+    };
+
     // Add the authenticated user as an extension so downstream handlers can
     // inspect it if necessary.
     req.extensions_mut().insert(AuthedUser {
         user,
-        create_if_not_exists: frontegg.is_some(),
+        create_if_not_exists: frontegg.is_some() || !matches!(tls_mode, Some(TlsMode::AssumeUser)),
     });
 
     // Run the request.

src/environmentd/src/lib.rs
@@ -26,6 +26,7 @@ use tokio::net::TcpListener;
 use tokio::sync::oneshot;
 use tokio_stream::wrappers::TcpListenerStream;
 use tower_http::cors::AllowOrigin;
+use tracing::error;
 
 use mz_adapter::catalog::storage::BootstrapArgs;
 use mz_adapter::catalog::{ClusterReplicaSizeMap, StorageHostSizeMap};
@@ -39,7 +40,6 @@ use mz_ore::tracing::OpenTelemetryEnableCallback;
 use mz_persist_client::usage::StorageUsageClient;
 use mz_secrets::SecretsController;
 use mz_storage::types::connections::ConnectionContext;
-use tracing::error;
 
 use crate::tcp_connection::ConnectionHandler;
 
@@ -250,22 +250,23 @@ pub async fn serve(config: Config) -> Result<Server, anyhow::Error> {
     let controller = mz_controller::Controller::new(config.controller).await;
 
     // Initialize adapter.
-    let (adapter_handle, adapter_client) = mz_adapter::serve(mz_adapter::Config {
-        dataflow_client: controller,
-        storage: adapter_storage,
-        unsafe_mode: config.unsafe_mode,
-        build_info: &BUILD_INFO,
-        metrics_registry: config.metrics_registry.clone(),
-        now: config.now,
-        secrets_controller: config.secrets_controller,
-        cluster_replica_sizes: config.cluster_replica_sizes,
-        storage_host_sizes: config.storage_host_sizes,
-        default_storage_host_size: config.default_storage_host_size,
-        availability_zones: config.availability_zones,
-        connection_context: config.connection_context,
-        storage_usage_client,
-    })
-    .await?;
+    let (adapter_handle, external_adapter_client, internal_adapter_client) =
+        mz_adapter::serve(mz_adapter::Config {
+            dataflow_client: controller,
+            storage: adapter_storage,
+            unsafe_mode: config.unsafe_mode,
+            build_info: &BUILD_INFO,
+            metrics_registry: config.metrics_registry.clone(),
+            now: config.now,
+            secrets_controller: config.secrets_controller,
+            cluster_replica_sizes: config.cluster_replica_sizes,
+            storage_host_sizes: config.storage_host_sizes,
+            default_storage_host_size: config.default_storage_host_size,
+            availability_zones: config.availability_zones,
+            connection_context: config.connection_context,
+            storage_usage_client,
+        })
+        .await?;
 
     // TODO(benesch): replace both `TCPListenerStream`s below with
     // `<type>_listener.incoming()` if that is
@@ -282,7 +283,7 @@ pub async fn serve(config: Config) -> Result<Server, anyhow::Error> {
     task::spawn(|| "pgwire_server", {
         let pgwire_server = mz_pgwire::Server::new(mz_pgwire::Config {
             tls: pgwire_tls,
-            adapter_client: adapter_client.clone(),
+            adapter_client: external_adapter_client.clone(),
             frontegg: config.frontegg.clone(),
         });
 
@@ -302,7 +303,7 @@ pub async fn serve(config: Config) -> Result<Server, anyhow::Error> {
         task::spawn(|| "internal_pgwire_server", {
             let internal_pgwire_server = mz_pgwire::Server::new(mz_pgwire::Config {
                 tls: None,
-                adapter_client: adapter_client.clone(),
+                adapter_client: internal_adapter_client,
                 frontegg: None,
             });
             let mut incoming = TcpListenerStream::new(internal_sql_listener);
@@ -321,7 +322,7 @@ pub async fn serve(config: Config) -> Result<Server, anyhow::Error> {
             let http_server = http::Server::new(http::Config {
                 tls: http_tls,
                 frontegg: config.frontegg,
-                adapter_client,
+                adapter_client: external_adapter_client,
                 allowed_origin: config.cors_allowed_origin,
             });
             let mut incoming = TcpListenerStream::new(http_listener);

src/pgwire/src/protocol.rs
@@ -115,6 +115,18 @@ where
 
     let user = params.remove("user").unwrap_or_else(String::new);
 
+    // Validate that mz_system only logs in via an internal port.
+    match (adapter_client.client_type(), user.as_str()) {
+        (mz_adapter::client::ClientType::External, mz_adapter::catalog::SYSTEM_USER) => {
+            let msg = format!("unauthorized login to user '{user}'");
+            return conn
+                .send(ErrorResponse::fatal(SqlState::INSUFFICIENT_PRIVILEGE, msg))
+                .await;
+        }
+        (mz_adapter::client::ClientType::Internal, _)
+        | (mz_adapter::client::ClientType::External, _) => {}
+    };
+
     // Validate that the connection is compatible with the TLS mode.
     //
     // The match here explicitly spells out all cases to be resilient to

src/environmentd/tests/sql.rs
@@ -13,7 +13,6 @@
 //! scripts. The tests here are simply too complicated to be easily expressed
 //! in testdrive, e.g., because they depend on the current time.
 
-use anyhow::anyhow;
 use std::error::Error;
 use std::net::{Ipv4Addr, SocketAddr};
 use std::sync::Arc;
@@ -21,12 +20,12 @@ use std::sync::Mutex;
 use std::time::{Duration, Instant};
 use std::{env, thread};
 
+use anyhow::anyhow;
 use axum::response::IntoResponse;
 use axum::response::Response;
 use axum::{routing, Json, Router};
 use chrono::{DateTime, Utc};
 use http::StatusCode;
-use mz_ore::retry::Retry;
 use postgres::Row;
 use regex::Regex;
 use serde_json::json;
@@ -36,8 +35,10 @@ use tokio_postgres::config::Host;
 use tokio_postgres::Client;
 use tracing::info;
 
+use mz_adapter::catalog::SYSTEM_USER;
 use mz_ore::assert_contains;
 use mz_ore::now::{EpochMillis, NowFn, NOW_ZERO, SYSTEM_TIME};
+use mz_ore::retry::Retry;
 use mz_ore::task::{self, AbortOnDropHandle, JoinHandleExt};
 
 use crate::util::{MzTimestamp, PostgresErrorExt, KAFKA_ADDRS};
@@ -1381,6 +1382,27 @@ fn test_linearizability() -> Result<(), Box<dyn Error>> {
     assert!(join_ts >= view_ts);
 
     cleanup_fn(&mut mz_client, &mut pg_client, &server.runtime)?;
+
+    Ok(())
+}
+
+#[test]
+fn test_system_user() -> Result<(), Box<dyn Error>> {
+    mz_ore::test::init_logging();
+
+    let config = util::Config::default();
+    let server = util::start_server(config)?;
+
+    assert!(server
+        .pg_config()
+        .user(SYSTEM_USER)
+        .connect(postgres::NoTls)
+        .is_err());
+    assert!(server
+        .pg_config_internal()
+        .user(SYSTEM_USER)
+        .connect(postgres::NoTls)
+        .is_ok());
 
     Ok(())
 }

src/adapter/src/lib.rs
@@ -32,7 +32,6 @@ macro_rules! coord_bail {
     }
 }
 
-mod client;
 mod command;
 mod coord;
 mod error;
@@ -42,6 +41,7 @@ mod tail;
 mod util;
 
 pub mod catalog;
+pub mod client;
 pub mod session;
 
 pub use crate::client::{Client, ConnClient, Handle, SessionClient};

commit bf6cdbd6996996304ce803d0ef5f7e334b1da9db

environmentd: Fix connection ID allocation
Previously, internal ports and external ports used separate
IDAllocators to allocate connection IDs. This caused connection IDs
on internal and external ports to collide. This commit fixes this issue
by having internal and external ports share an IDAllocator. This fixes
a regression caused by 041f326fdea44508fb80b22246a9815f2d3548a6.

src/adapter/src/catalog.rs
@@ -4229,7 +4229,7 @@ impl<S: Append> Catalog<S> {
     }
 }
 
-fn is_reserved_name(name: &str) -> bool {
+pub fn is_reserved_name(name: &str) -> bool {
     BUILTIN_ROLE_PREFIXES
         .iter()
         .any(|prefix| name.starts_with(prefix))

src/adapter/src/client.rs
@@ -68,15 +68,6 @@ impl Handle {
     }
 }
 
-/// Distinguishes between external and internal clients.
-#[derive(Debug, Clone)]
-pub enum ClientType {
-    /// Client that has connected over an external port.
-    External,
-    /// Client that has connected over an internal port.
-    Internal,
-}
-
 /// A coordinator client.
 ///
 /// A coordinator client is a simple handle to a communication channel with the
@@ -88,15 +79,13 @@ pub enum ClientType {
 pub struct Client {
     cmd_tx: mpsc::UnboundedSender<Command>,
     id_alloc: Arc<IdAllocator<ConnectionId>>,
-    client_type: ClientType,
 }
 
 impl Client {
-    pub(crate) fn new(cmd_tx: mpsc::UnboundedSender<Command>, client_type: ClientType) -> Client {
+    pub(crate) fn new(cmd_tx: mpsc::UnboundedSender<Command>) -> Client {
         Client {
             cmd_tx,
             id_alloc: Arc::new(IdAllocator::new(1, 1 << 16)),
-            client_type,
         }
     }
 
@@ -130,11 +119,6 @@ impl Client {
         let response = self.system_execute(stmt).await?;
         Ok(response.results.into_element())
     }
-
-    /// Returns the `ClientType` associated with this client.
-    pub fn client_type(&self) -> &ClientType {
-        &self.client_type
-    }
 }
 
 /// A coordinator client that is bound to a connection.
@@ -153,11 +137,6 @@ impl ConnClient {
     /// Returns the ID of the connection associated with this client.
     pub fn conn_id(&self) -> ConnectionId {
         self.conn_id
-    }
-
-    /// Returns the `ClientType` associated with this client.
-    pub fn client_type(&self) -> &ClientType {
-        self.inner.client_type()
     }
 
     /// Upgrades this connection client to a session client.

src/adapter/src/coord.rs
@@ -114,7 +114,7 @@ use crate::catalog::{
     self, storage, BuiltinMigrationMetadata, BuiltinTableUpdate, Catalog, CatalogItem,
     ClusterReplicaSizeMap, StorageHostSizeMap, StorageSinkConnectionState,
 };
-use crate::client::{Client, ClientType, ConnectionId, Handle};
+use crate::client::{Client, ConnectionId, Handle};
 use crate::command::{Canceled, Command, ExecuteResponse};
 use crate::coord::appends::{BuiltinTableUpdateSource, Deferred, PendingWriteTxn};
 use crate::coord::id_bundle::CollectionIdBundle;
@@ -815,7 +815,7 @@ pub async fn serve<S: Append + 'static>(
         connection_context,
         storage_usage_client,
     }: Config<S>,
-) -> Result<(Handle, Client, Client), AdapterError> {
+) -> Result<(Handle, Client), AdapterError> {
     let (cmd_tx, cmd_rx) = mpsc::unbounded_channel();
     let (internal_cmd_tx, internal_cmd_rx) = mpsc::unbounded_channel();
     let (strict_serializable_reads_tx, strict_serializable_reads_rx) = mpsc::unbounded_channel();
@@ -933,9 +933,8 @@ pub async fn serve<S: Append + 'static>(
                 start_instant,
                 _thread: thread.join_on_drop(),
             };
-            let external_client = Client::new(cmd_tx.clone(), ClientType::External);
-            let internal_client = Client::new(cmd_tx, ClientType::Internal);
-            Ok((handle, external_client, internal_client))
+            let client = Client::new(cmd_tx.clone());
+            Ok((handle, client))
         }
         Err(e) => Err(e),
     }

src/environmentd/src/http.rs
@@ -309,15 +309,9 @@ async fn auth<B>(
         }
     };
 
-    // Validate that mz_system only logs in via an internal port.
-    let adapter_client = req.extensions().get::<mz_adapter::Client>().unwrap();
-    match (adapter_client.client_type(), user.as_str()) {
-        (mz_adapter::client::ClientType::External, mz_adapter::catalog::SYSTEM_USER) => {
-            return Err(AuthError::InvalidLogin(user))
-        }
-        (mz_adapter::client::ClientType::Internal, _)
-        | (mz_adapter::client::ClientType::External, _) => {}
-    };
+    if mz_adapter::catalog::is_reserved_name(user.as_str()) {
+        return Err(AuthError::InvalidLogin(user));
+    }
 
     // Add the authenticated user as an extension so downstream handlers can
     // inspect it if necessary.

src/environmentd/src/lib.rs
@@ -250,23 +250,22 @@ pub async fn serve(config: Config) -> Result<Server, anyhow::Error> {
     let controller = mz_controller::Controller::new(config.controller).await;
 
     // Initialize adapter.
-    let (adapter_handle, external_adapter_client, internal_adapter_client) =
-        mz_adapter::serve(mz_adapter::Config {
-            dataflow_client: controller,
-            storage: adapter_storage,
-            unsafe_mode: config.unsafe_mode,
-            build_info: &BUILD_INFO,
-            metrics_registry: config.metrics_registry.clone(),
-            now: config.now,
-            secrets_controller: config.secrets_controller,
-            cluster_replica_sizes: config.cluster_replica_sizes,
-            storage_host_sizes: config.storage_host_sizes,
-            default_storage_host_size: config.default_storage_host_size,
-            availability_zones: config.availability_zones,
-            connection_context: config.connection_context,
-            storage_usage_client,
-        })
-        .await?;
+    let (adapter_handle, adapter_client) = mz_adapter::serve(mz_adapter::Config {
+        dataflow_client: controller,
+        storage: adapter_storage,
+        unsafe_mode: config.unsafe_mode,
+        build_info: &BUILD_INFO,
+        metrics_registry: config.metrics_registry.clone(),
+        now: config.now,
+        secrets_controller: config.secrets_controller,
+        cluster_replica_sizes: config.cluster_replica_sizes,
+        storage_host_sizes: config.storage_host_sizes,
+        default_storage_host_size: config.default_storage_host_size,
+        availability_zones: config.availability_zones,
+        connection_context: config.connection_context,
+        storage_usage_client,
+    })
+    .await?;
 
     // TODO(benesch): replace both `TCPListenerStream`s below with
     // `<type>_listener.incoming()` if that is
@@ -283,8 +282,9 @@ pub async fn serve(config: Config) -> Result<Server, anyhow::Error> {
     task::spawn(|| "pgwire_server", {
         let pgwire_server = mz_pgwire::Server::new(mz_pgwire::Config {
             tls: pgwire_tls,
-            adapter_client: external_adapter_client.clone(),
+            adapter_client: adapter_client.clone(),
             frontegg: config.frontegg.clone(),
+            internal: false,
         });
 
         async move {
@@ -303,8 +303,9 @@ pub async fn serve(config: Config) -> Result<Server, anyhow::Error> {
         task::spawn(|| "internal_pgwire_server", {
             let internal_pgwire_server = mz_pgwire::Server::new(mz_pgwire::Config {
                 tls: None,
-                adapter_client: internal_adapter_client,
+                adapter_client: adapter_client.clone(),
                 frontegg: None,
+                internal: true,
             });
             let mut incoming = TcpListenerStream::new(internal_sql_listener);
             async move {
@@ -322,7 +323,7 @@ pub async fn serve(config: Config) -> Result<Server, anyhow::Error> {
             let http_server = http::Server::new(http::Config {
                 tls: http_tls,
                 frontegg: config.frontegg,
-                adapter_client: external_adapter_client,
+                adapter_client,
                 allowed_origin: config.cors_allowed_origin,
             });
             let mut incoming = TcpListenerStream::new(http_listener);

src/pgwire/src/protocol.rs
@@ -78,7 +78,11 @@ pub struct RunParams<'a, A> {
     pub version: i32,
     /// The parameters that the client provided in the startup message.
     pub params: HashMap<String, String>,
+    /// Frontegg authentication.
     pub frontegg: Option<&'a FronteggAuthentication>,
+    /// Whether this is an internal server that permits access to restricted
+    /// system resources.
+    pub internal: bool,
 }
 
 /// Runs a pgwire connection to completion.
@@ -99,6 +103,7 @@ pub async fn run<'a, A>(
         version,
         mut params,
         frontegg,
+        internal,
     }: RunParams<'a, A>,
 ) -> Result<(), io::Error>
 where
@@ -115,17 +120,13 @@ where
 
     let user = params.remove("user").unwrap_or_else(String::new);
 
-    // Validate that mz_system only logs in via an internal port.
-    match (adapter_client.client_type(), user.as_str()) {
-        (mz_adapter::client::ClientType::External, mz_adapter::catalog::SYSTEM_USER) => {
-            let msg = format!("unauthorized login to user '{user}'");
-            return conn
-                .send(ErrorResponse::fatal(SqlState::INSUFFICIENT_PRIVILEGE, msg))
-                .await;
-        }
-        (mz_adapter::client::ClientType::Internal, _)
-        | (mz_adapter::client::ClientType::External, _) => {}
-    };
+    // Validate that builtin roles only log in via an internal port.
+    if !internal && mz_adapter::catalog::is_reserved_name(user.as_str()) {
+        let msg = format!("unauthorized login to user '{user}'");
+        return conn
+            .send(ErrorResponse::fatal(SqlState::INSUFFICIENT_PRIVILEGE, msg))
+            .await;
+    }
 
     // Validate that the connection is compatible with the TLS mode.
     //

src/pgwire/src/server.rs
@@ -40,6 +40,9 @@ pub struct Config {
     /// a valid Frontegg API token as a password to authenticate. Otherwise,
     /// password authentication is disabled.
     pub frontegg: Option<FronteggAuthentication>,
+    /// Whether this is an internal server that permits access to restricted
+    /// system resources.
+    pub internal: bool,
 }
 
 /// Configures a server's TLS encryption and authentication.
@@ -66,6 +69,7 @@ pub struct Server {
     tls: Option<TlsConfig>,
     adapter_client: mz_adapter::Client,
     frontegg: Option<FronteggAuthentication>,
+    internal: bool,
 }
 
 impl Server {
@@ -75,6 +79,7 @@ impl Server {
             tls: config.tls,
             adapter_client: config.adapter_client,
             frontegg: config.frontegg,
+            internal: config.internal,
         }
     }
 
@@ -109,6 +114,7 @@ impl Server {
                         version,
                         params,
                         frontegg: self.frontegg.as_ref(),
+                        internal: self.internal,
                     })
                     .await?;
                     conn.flush().await?;

src/environmentd/tests/sql.rs
@@ -1407,6 +1407,60 @@ fn test_system_user() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+#[test]
+// Tests that you can have simultaneous connections on the internal and external ports without
+// crashing
+fn test_internal_ports() -> Result<(), Box<dyn Error>> {
+    mz_ore::test::init_logging();
+
+    let config = util::Config::default();
+    let server = util::start_server(config)?;
+
+    {
+        let mut external_client = server.connect(postgres::NoTls)?;
+        let mut internal_client = server
+            .pg_config_internal()
+            .user(SYSTEM_USER)
+            .connect(postgres::NoTls)?;
+
+        assert_eq!(
+            1,
+            external_client
+                .query_one("SELECT 1;", &[])?
+                .get::<_, i32>(0)
+        );
+        assert_eq!(
+            1,
+            internal_client
+                .query_one("SELECT 1;", &[])?
+                .get::<_, i32>(0)
+        );
+    }
+
+    {
+        let mut external_client = server.connect(postgres::NoTls)?;
+        let mut internal_client = server
+            .pg_config_internal()
+            .user(SYSTEM_USER)
+            .connect(postgres::NoTls)?;
+
+        assert_eq!(
+            1,
+            external_client
+                .query_one("SELECT 1;", &[])?
+                .get::<_, i32>(0)
+        );
+        assert_eq!(
+            1,
+            internal_client
+                .query_one("SELECT 1;", &[])?
+                .get::<_, i32>(0)
+        );
+    }
+
+    Ok(())
+}
+
 /// Group commit will block writes until the current time has advanced. This can make
 /// performing inserts while using deterministic time difficult. This is a helper
 /// method to perform writes and advance the current time.