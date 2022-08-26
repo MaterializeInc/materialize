809b36b31fb296eeb08b009094ef82882f028eb8
Ben Kirwin <bkirwi@materialize.com>
2022-08-26 15:22:39 -0400

    Push the alter-source implementation into the catalog

--- src/sql/src/plan.rs
+++ src/sql/src/plan.rs
@@ -456,7 +456,7 @@ pub struct AlterIndexResetOptionsPlan {
     pub options: HashSet<IndexOptionName>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum AlterSourceItem {
     Set(String),
     Reset,
