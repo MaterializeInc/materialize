17ed0f31b3d20c5c0b88bc0d89279d547411a082
Cara Haas <cara@materialize.com>
2022-08-30 16:22:08 -0400

    lint

--- src/testdrive/src/action/kafka/verify.rs
+++ src/testdrive/src/action/kafka/verify.rs
@@ -109,9 +109,11 @@ async fn get_topic(
     topic_field: &str,
     state: &mut State,
 ) -> Result<String, anyhow::Error> {
-    let query = format!("SELECT {} FROM mz_sinks JOIN mz_kafka_sinks \
+    let query = format!(
+        "SELECT {} FROM mz_sinks JOIN mz_kafka_sinks \
         ON mz_sinks.id = mz_kafka_sinks.sink_id WHERE name = $1",
-        topic_field);
+        topic_field
+    );
     let result = state
         .pgclient
         .query_one(query.as_str(), &[&sink])
