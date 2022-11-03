19d16de18701ac5d3011870950080c6eed8794de
Gus Wynn <gus@materialize.com>
2022-11-03 16:09:05 -0700

    otel: simplify and fix local jaeger setup

--- misc/opentelemetry/mzcompose.py
+++ misc/opentelemetry/mzcompose.py
@@ -14,26 +14,10 @@ SERVICES = [
         "jaeger",
         {
             "image": "jaegertracing/all-in-one:1.36",
-            "ports": ["16686:16686", 14268, 14250],
+            "ports": ["16686:16686", "4317:4317", "4318:4318", 14268, 14250],
             "command": "--collector.grpc-server.max-message-size=16777216",
+            "environment": ["COLLECTOR_OTLP_ENABLED=true"],
             "allow_host_ports": True,
-        },
-    ),
-    Service(
-        "otel-collector",
-        {
-            "image": "otel/opentelemetry-collector:0.56.0",
-            "command": "--config=/etc/otel-collector-config.yaml",
-            "ports": [
-                1888,  # pprof
-                13133,  # health_check
-                "4317:4317",  # otlp grpc
-                "4318:4318",  # otlp http
-                55670,  # zpages
-            ],
-            "allow_host_ports": True,
-            "volumes": ["./otel-collector-config.yaml:/etc/otel-collector-config.yaml"],
-            "depends_on": ["jaeger"],
         },
     ),
 ]
