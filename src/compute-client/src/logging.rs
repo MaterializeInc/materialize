ca1cf7b883f7c737c1c5e41b23a1b362c5127524
Frank McSherry <fmcsherry@me.com>
2022-11-04 13:52:21 -0400

    Correct timestamp type and rename to time

--- src/compute-client/src/logging.rs
+++ src/compute-client/src/logging.rs
@@ -688,7 +688,7 @@ impl LogVariant {
                 .with_column("port", ScalarType::UInt64.nullable(false))
                 .with_column("worker_id", ScalarType::UInt64.nullable(false))
                 .with_column("update_type", ScalarType::String.nullable(false))
-                .with_column("timestamp", ScalarType::UInt64.nullable(true)),
+                .with_column("time", ScalarType::MzTimestamp.nullable(true)),
 
             LogVariant::Differential(DifferentialLog::ArrangementBatches)
             | LogVariant::Differential(DifferentialLog::ArrangementRecords)
