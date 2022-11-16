49ef397a6be0e11d4e093a8b0a9d4a92fb811454
Chae Jubb <chae@materialize.com>
2022-11-16 13:50:09 -0800

    [storage] [healthcheck] Partially unify implementations (#16095)
    Use the same loop for both healthcheckers. I believe they should stay embedded differently (sources as a timely operator but sinks remaining integrated into the sink itself) so I haven't completely aligned the implementations -- but I think this is a good way to ensure the core of the implementations stay aligned

---
+++ src/storage/src/healthcheck.rs
@@ -0,0 +1,89 @@
+// Copyright Materialize, Inc. and contributors. All rights reserved.
+//
+// Use of this software is governed by the Business Source License
+// included in the LICENSE file.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0.
+
+//! Healthcheck common
+
+use chrono::{DateTime, NaiveDateTime, Utc};
+use differential_dataflow::lattice::Lattice;
+use mz_ore::now::NowFn;
+use timely::progress::Antichain;
+
+use mz_persist_client::{PersistClient, ShardId, Upper};
+use mz_repr::{Datum, GlobalId, Row, Timestamp};
+use mz_storage_client::types::sources::SourceData;
+use tracing::trace;
+
+pub async fn write_to_persist(
+    collection_id: GlobalId,
+    new_status: &str,
+    new_error: Option<&str>,
+    now: NowFn,
+    client: &PersistClient,
+    status_shard: ShardId,
+) {
+    let now_ms = now();
+    let row = {
+        let timestamp = NaiveDateTime::from_timestamp(
+            (now_ms / 1000)
+                .try_into()
+                .expect("timestamp seconds does not fit into i64"),
+            (now_ms % 1000 * 1_000_000)
+                .try_into()
+                .expect("timestamp millis does not fit into a u32"),
+        );
+        let timestamp = Datum::TimestampTz(
+            DateTime::from_utc(timestamp, Utc)
+                .try_into()
+                .expect("must fit"),
+        );
+        let collection_id = collection_id.to_string();
+        let collection_id = Datum::String(&collection_id);
+        let status = Datum::String(new_status);
+        let error = new_error.into();
+        let metadata = Datum::Null;
+        Row::pack_slice(&[timestamp, collection_id, status, error, metadata])
+    };
+
+    let mut handle = client.open_writer(status_shard).await.expect(
+        "Invalid usage of the persist client for collection {collection_id} status history shard",
+    );
+
+    let mut recent_upper = handle.upper().clone();
+    let mut append_ts = Timestamp::from(now_ms);
+    'retry_loop: loop {
+        // We don't actually care so much about the timestamp we append at; it's best-effort.
+        // Ensure that the append timestamp is not less than the current upper, and the new upper
+        // is past that timestamp. (Unless we've advanced to the empty antichain, but in
+        // that case we're already in trouble.)
+        for t in recent_upper.elements() {
+            append_ts.join_assign(t);
+        }
+        let mut new_upper = Antichain::from_elem(append_ts.step_forward());
+        new_upper.join_assign(&recent_upper);
+
+        let updates = vec![((SourceData(Ok(row.clone())), ()), append_ts, 1i64)];
+        let cas_result = handle
+            .compare_and_append(updates, recent_upper.clone(), new_upper)
+            .await;
+        match cas_result {
+            Ok(Ok(Ok(()))) => break 'retry_loop,
+            Ok(Ok(Err(Upper(upper)))) => {
+                recent_upper = upper;
+            }
+            Ok(Err(e)) => {
+                panic!("Invalid usage of the persist client for collection {collection_id} status history shard: {e:?}");
+            }
+            Err(e) => {
+                trace!("compare_and_append in update_status failed: {e}");
+            }
+        }
+    }
+
+    handle.expire().await
+}
--- src/storage/src/source/source_reader_pipeline.rs
+++ src/storage/src/source/source_reader_pipeline.rs
@@ -30,7 +30,6 @@ use std::rc::Rc;
 use std::sync::Arc;
 use std::time::{Duration, Instant};
 
-use differential_dataflow::lattice::Lattice;
 use differential_dataflow::Hashable;
 use futures::future::Either;
 use itertools::Itertools;
@@ -54,20 +53,19 @@ use mz_ore::cast::CastFrom;
 use mz_ore::now::NowFn;
 use mz_ore::vec::VecExt;
 use mz_persist_client::cache::PersistClientCache;
-use mz_persist_client::Upper;
-use mz_repr::{GlobalId, Row, Timestamp};
+use mz_repr::{GlobalId, Timestamp};
 use mz_storage_client::controller::{CollectionMetadata, ResumptionFrontierCalculator};
 use mz_storage_client::source::util::async_source;
 use mz_storage_client::types::connections::ConnectionContext;
 use mz_storage_client::types::errors::SourceError;
 use mz_storage_client::types::sources::encoding::SourceDataEncoding;
-use mz_storage_client::types::sources::{AsyncSourceToken, MzOffset, SourceData, SourceToken};
+use mz_storage_client::types::sources::{AsyncSourceToken, MzOffset, SourceToken};
 use mz_timely_util::builder_async::{Event, OperatorBuilder as AsyncOperatorBuilder};
 use mz_timely_util::operator::StreamExt as _;
 use mz_timely_util::operators_async_ext::OperatorBuilderExt;
 
+use crate::healthcheck::write_to_persist;
 use crate::source::antichain::{MutableOffsetAntichain, OffsetAntichain};
-use crate::source::healthcheck;
 use crate::source::healthcheck::{SourceStatus, SourceStatusUpdate};
 
 use crate::source::metrics::SourceBaseMetrics;
@@ -902,70 +900,22 @@ where
         move |mut _capabilities, _frontiers, scheduler| async move {
             let mut buffer = Vec::new();
 
-            let mut persist_clients = persist_clients.lock().await;
-            let persist_client = persist_clients
-                .open(storage_metadata.persist_location.clone())
-                .await
-                .expect("error creating persist client for Healthchecker");
-            drop(persist_clients);
-
-            let client_ref = &persist_client;
-
-            let write_to_persist = |row: Row, timestamp: Timestamp| async move {
-                let status_shard = match storage_metadata.status_shard {
-                    Some(s) => s,
-                    None => return,
-                };
-                let handle = client_ref
-                    .open_writer(status_shard)
-                    .await;
-
-                let mut handle = match handle {
-                    Ok(stuff) => stuff,
-                    Err(e) => {
-                        panic!("Invalid usage of the persist client for source {source_id} status history shard: {e:?}");
-                    }
-                };
-
-                let mut recent_upper = handle.upper().clone();
-                let mut append_ts = timestamp;
-                'retry_loop: loop {
-                    // We don't actually care so much about the timestamp we append at; it's best-effort.
-                    // Ensure that the append timestamp is not less than the current upper, and the new upper
-                    // is past that timestamp. (Unless we've advanced to the empty antichain, but in
-                    // that case we're already in trouble.)
-                    for t in recent_upper.elements() {
-                        append_ts.join_assign(t);
-                    }
-                    let mut new_upper = Antichain::from_elem(append_ts.step_forward());
-                    new_upper.join_assign(&recent_upper);
-
-                    let updates = vec![
-                        ((SourceData(Ok(row.clone())), ()), append_ts, 1i64)
-                    ];
-                    let cas_result = handle.compare_and_append(updates, recent_upper.clone(), new_upper).await;
-                    match cas_result {
-                        Ok(Ok(Ok(()))) => break 'retry_loop,
-                        Ok(Ok(Err(Upper(upper)))) => {
-                            recent_upper = upper;
-                        }
-                        Ok(Err(e)) => {
-                            panic!("Invalid usage of the persist client for source {source_id} status history shard: {e:?}");
-                        }
-                        Err(e) => {
-                            trace!("compare_and_append in update_status failed: {e}");
-                        }
-                    }
-                }
-
-                handle.expire().await
+            let persist_client = {
+                let mut persist_clients = persist_clients.lock().await;
+                persist_clients
+                    .open(storage_metadata.persist_location.clone())
+                    .await
+                    .expect("error creating persist client for Healthchecker")
             };
 
             if is_active_worker {
+                if let Some(status_shard) = storage_metadata.status_shard {
+                    info!("Health for source {source_id} being written to {status_shard}");
+                    write_to_persist(source_id, last_reported_status.name(), last_reported_status.error(), now.clone(), &persist_client, status_shard).await;
+                } else {
+                    info!("Health for source {source_id} not being written to status shard");
+                }
                 info!("Health for source {source_id} initialized to: {last_reported_status:?}");
-                let now_ms = now();
-                let row = healthcheck::pack_status_row(source_id, last_reported_status.name(), last_reported_status.error(), now_ms);
-                write_to_persist(row, Timestamp::from(now_ms)).await;
             }
 
             while scheduler.notified().await {
@@ -986,9 +936,9 @@ where
                 let new_status = overall_status(&healths);
                 if &last_reported_status != new_status {
                     info!("Health transition for source {source_id}: {last_reported_status:?} -> {new_status:?}");
-                    let now_ms = now();
-                    let row = healthcheck::pack_status_row(source_id, new_status.name(), new_status.error(), now_ms);
-                    write_to_persist(row, Timestamp::from(now_ms)).await;
+                    if let Some(status_shard) = storage_metadata.status_shard {
+                        write_to_persist(source_id, new_status.name(), new_status.error(), now.clone(), &persist_client, status_shard).await;
+                    }
 
                     last_reported_status = new_status.clone();
                 }
