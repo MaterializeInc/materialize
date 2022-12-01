d339d9abc983e88d0cfd067a2caaaa891856f34b
Gabor Gevay <gabor@materialize.com>
2022-12-01 17:00:17 +0100

    Remove the `skip(1)` from JoinImplementation
    And add the characteristics of the starting input to EXPLAIN.

--- src/expr/src/explain/text.rs
+++ src/expr/src/explain/text.rs
@@ -439,6 +439,7 @@ impl MirRelationExpr {
                     };
                     let join_order = |start_idx: usize,
                                       start_key: &Option<Vec<MirScalarExpr>>,
+                                      start_characteristics: &Option<JoinInputCharacteristics>,
                                       tail: &Vec<(
                         usize,
                         Vec<MirScalarExpr>,
@@ -446,12 +447,16 @@ impl MirRelationExpr {
                     )>|
                      -> String {
                         format!(
-                            "{}{} » {}",
+                            "{}{}{} » {}",
                             input_name(start_idx),
                             match start_key {
                                 None => "".to_owned(),
                                 Some(key) => format!("[{}]", join_key_to_string(key)),
                             },
+                            start_characteristics
+                                .as_ref()
+                                .map(|c| c.explain())
+                                .unwrap_or_else(|| "".to_string()),
                             separated(
                                 " » ",
                                 tail.iter().map(|(pos, key, characteristics)| {
@@ -470,7 +475,10 @@ impl MirRelationExpr {
                     };
                     ctx.indented(|ctx| {
                         match implementation {
-                            JoinImplementation::Differential((start_idx, start_key), tail) => {
+                            JoinImplementation::Differential(
+                                (start_idx, start_key, start_characteristics),
+                                tail,
+                            ) => {
                                 soft_assert!(inputs.len() == tail.len() + 1);
 
                                 writeln!(f, "{}implementation", ctx.indent)?;
@@ -479,7 +487,12 @@ impl MirRelationExpr {
                                         f,
                                         "{}{}",
                                         ctx.indent,
-                                        join_order(*start_idx, start_key, tail)
+                                        join_order(
+                                            *start_idx,
+                                            start_key,
+                                            start_characteristics,
+                                            tail
+                                        )
                                     )
                                 })?;
                             }
@@ -493,7 +506,7 @@ impl MirRelationExpr {
                                             f,
                                             "{}{}",
                                             ctx.indent,
-                                            join_order(pos, &None, chain)
+                                            join_order(pos, &None, &None, chain)
                                         )?;
                                     }
                                     Ok(())
