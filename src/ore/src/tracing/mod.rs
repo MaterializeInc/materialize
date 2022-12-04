MaterializeInc/materialize

commit 6c09ac7eb117224668abed158222dfa1bf84ca63

Add a commandline flag --stderr-log-format for optional json logging
This adds the ability to configure our tracing-subscriber setup to log
either pretty text, or structured json - one is great for humans, the
other is good in infrastructure with a structured log aggregation
setup.

src/ore/src/tracing/mod.rs
@@ -83,11 +83,26 @@ pub struct SentryConfig {
 /// Configures the stderr log.
 #[derive(Debug, Clone)]
 pub struct StderrLogConfig {
-    /// Whether to prefix each log line with the service name.
-    /// An optional prefix for each stderr log line.
-    pub prefix: Option<String>,
+    /// The format in which to emit messages.
+    pub format: StderrLogFormat,
     /// A filter which determines which events are emitted to the log.
     pub filter: Targets,
+}
+
+/// Specifies the format of a stderr log message.
+#[derive(Debug, Clone)]
+pub enum StderrLogFormat {
+    /// Format as human readable, optionally colored text.
+    ///
+    /// Best suited for direct human consumption in a terminal.
+    Text {
+        /// An optional prefix for each log message.
+        prefix: Option<String>,
+    },
+    /// Format as JSON (in reality, JSONL).
+    ///
+    /// Best suited for ingestion in structured logging aggregators.
+    Json,
 }
 
 /// Callbacks used to dynamically modify tracing-related filters
@@ -181,6 +196,28 @@ pub struct TokioConsoleConfig {
     pub retention: Duration,
 }
 
+fn stderr_log_layer<S>(stderr_config: StderrLogConfig) -> Box<dyn Layer<S> + Send + Sync>
+where
+    S: Subscriber + for<'a> LookupSpan<'a>,
+{
+    match stderr_config.format {
+        StderrLogFormat::Text { prefix } => {
+            // See: https://no-color.org/
+            let no_color = std::env::var_os("NO_COLOR").unwrap_or_else(|| "".into()) != "";
+            Box::new(
+                fmt::layer()
+                    .with_writer(io::stderr)
+                    .event_format(PrefixFormat {
+                        inner: format(),
+                        prefix,
+                    })
+                    .with_ansi(!no_color && atty::is(atty::Stream::Stderr)),
+            )
+        }
+        StderrLogFormat::Json => Box::new(fmt::layer().with_writer(io::stderr).json()),
+    }
+}
+
 /// Enables application tracing via the [`tracing`] and [`opentelemetry`]
 /// libraries.
 ///
@@ -218,24 +255,14 @@ where
     let service_name = service_name.to_string();
 
     let config = config.into();
-    // See: https://no-color.org/
-    let no_color = std::env::var_os("NO_COLOR").unwrap_or_else(|| "".into()) != "";
-    let stderr_log_layer = fmt::layer()
-        .event_format(PrefixFormat {
-            inner: format(),
-            prefix: config.stderr_log.prefix,
-        })
-        .with_writer(io::stderr)
-        .with_ansi(!no_color && atty::is(atty::Stream::Stderr))
-        .with_filter(config.stderr_log.filter);
-
-    let (stderr_log_layer, stderr_reloader) = reload::Layer::new(stderr_log_layer);
+    let (stderr_log_filter, stderr_reloader) = reload::Layer::new(config.stderr_log.filter.clone());
     let stderr_callback = DynamicTargetsCallback {
         callback: Arc::new(move |targets| {
-            stderr_reloader.modify(|layer| *layer.filter_mut() = targets)?;
+            stderr_reloader.reload(targets)?;
             Ok(())
         }),
     };
+    let stderr_log_layer = stderr_log_layer(config.stderr_log).with_filter(stderr_log_filter);
 
     let (otel_layer, otel_reloader) = if let Some(otel_config) = config.opentelemetry {
         // TODO(guswynn): figure out where/how to call
@@ -292,10 +319,7 @@ where
             .with_filter(filter);
         let reloader = DynamicTargetsCallback {
             callback: Arc::new(move |targets| {
-                // This code should be kept panic-free to
-                // avoid weird poisoning issues in our subscriber
-                // stack.
-                filter_handle.modify(|filter| *filter = targets)?;
+                filter_handle.reload(targets)?;
                 Ok(())
             }),
         };

src/orchestrator-tracing/src/lib.rs
@@ -35,7 +35,9 @@ use mz_orchestrator::{
 use mz_ore::cli::{DefaultTrue, KeyValueArg};
 #[cfg(feature = "tokio-console")]
 use mz_ore::tracing::TokioConsoleConfig;
-use mz_ore::tracing::{OpenTelemetryConfig, SentryConfig, StderrLogConfig, TracingConfig};
+use mz_ore::tracing::{
+    OpenTelemetryConfig, SentryConfig, StderrLogConfig, StderrLogFormat, TracingConfig,
+};
 
 /// Command line arguments for application tracing.
 ///
@@ -80,8 +82,13 @@ pub struct TracingCliArgs {
         default_value = "info"
     )]
     pub log_filter: SerializableTargets,
+    /// The format to use for stderr log messages.
+    #[clap(long, env = "LOG_FORMAT", default_value_t, value_enum)]
+    pub log_format: LogFormat,
     /// An optional prefix for each stderr log line.
-    #[clap(long, env = "LOG_INCLUDE_SERVICE_NAME")]
+    ///
+    /// Only respected when `--log-format` is `text`.
+    #[clap(long, env = "LOG_PREFIX")]
     pub log_prefix: Option<String>,
     /// Whether OpenTelemetry tracing is enabled by default.
     ///
@@ -192,7 +199,12 @@ impl From<&TracingCliArgs> for TracingConfig {
     fn from(args: &TracingCliArgs) -> TracingConfig {
         TracingConfig {
             stderr_log: StderrLogConfig {
-                prefix: args.log_prefix.clone(),
+                format: match args.log_format {
+                    LogFormat::Text => StderrLogFormat::Text {
+                        prefix: args.log_prefix.clone(),
+                    },
+                    LogFormat::Json => StderrLogFormat::Json,
+                },
                 filter: args.log_filter.inner.clone(),
             },
             opentelemetry: args.opentelemetry_endpoint.clone().map(|endpoint| {
@@ -296,6 +308,7 @@ impl NamespacedOrchestrator for NamespacedTracingOrchestrator {
             let TracingCliArgs {
                 log_filter,
                 log_prefix,
+                log_format,
                 opentelemetry_endpoint,
                 opentelemetry_header,
                 opentelemetry_filter,
@@ -310,6 +323,7 @@ impl NamespacedOrchestrator for NamespacedTracingOrchestrator {
                 sentry_dsn,
             } = &self.tracing_args;
             args.push(format!("--log-filter={log_filter}"));
+            args.push(format!("--log-format={log_format}"));
             if log_prefix.is_some() {
                 args.push(format!("--log-prefix={}-{}", self.namespace, id));
             }
@@ -397,5 +411,28 @@ impl FromStr for SerializableTargets {
 impl fmt::Display for SerializableTargets {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         f.write_str(&self.raw)
+    }
+}
+
+/// Specifies the format of a stderr log message.
+#[derive(Debug, Clone, Default, clap::ValueEnum)]
+pub enum LogFormat {
+    /// Format as human readable, optionally colored text.
+    ///
+    /// Best suited for direct human consumption in a terminal.
+    #[default]
+    Text,
+    /// Format as JSON (in reality, JSONL).
+    ///
+    /// Best suited for ingestion in structured logging aggregators.
+    Json,
+}
+
+impl fmt::Display for LogFormat {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            LogFormat::Text => f.write_str("text"),
+            LogFormat::Json => f.write_str("json"),
+        }
     }
 }

src/ore/Cargo.toml
@@ -40,7 +40,7 @@ tokio-openssl = { version = "0.6.3", optional = true }
 # log lines we care about.
 # Note that this feature is distinct from `tracing`'s `log` feature, which has `tracing` macros emit `log` records if
 # there is no global `tracing` subscriber.
-tracing-subscriber = { version = "0.3.16", default-features = false, features = ["env-filter", "fmt", "tracing-log"], optional = true }
+tracing-subscriber = { version = "0.3.16", default-features = false, features = ["env-filter", "fmt", "json", "tracing-log"], optional = true }
 
 
 # For the `tracing` feature

commit 8e71adf37057c067ed055fe362eb9b5e804b5f31

ore: fix performance regression in tracing
6c09ac7 introduced a major performance regression, likely by perturbing
the tracing reload layer. This commit restores the old approach to
reloading filters, in the hopes of restoring the old performance. ( The
tracing library is inscrutable enough that I didn't bother tracking down
the root cause.)

src/ore/src/tracing/mod.rs
@@ -47,8 +47,8 @@ use tracing_subscriber::fmt::format::{format, Writer};
 use tracing_subscriber::fmt::{self, FmtContext, FormatEvent, FormatFields};
 use tracing_subscriber::layer::{Layer, SubscriberExt};
 use tracing_subscriber::registry::LookupSpan;
-use tracing_subscriber::reload;
 use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::{reload, Registry};
 
 /// Application tracing configuration.
 ///
@@ -196,28 +196,6 @@ pub struct TokioConsoleConfig {
     pub retention: Duration,
 }
 
-fn stderr_log_layer<S>(stderr_config: StderrLogConfig) -> Box<dyn Layer<S> + Send + Sync>
-where
-    S: Subscriber + for<'a> LookupSpan<'a>,
-{
-    match stderr_config.format {
-        StderrLogFormat::Text { prefix } => {
-            // See: https://no-color.org/
-            let no_color = std::env::var_os("NO_COLOR").unwrap_or_else(|| "".into()) != "";
-            Box::new(
-                fmt::layer()
-                    .with_writer(io::stderr)
-                    .event_format(PrefixFormat {
-                        inner: format(),
-                        prefix,
-                    })
-                    .with_ansi(!no_color && atty::is(atty::Stream::Stderr)),
-            )
-        }
-        StderrLogFormat::Json => Box::new(fmt::layer().with_writer(io::stderr).json()),
-    }
-}
-
 /// Enables application tracing via the [`tracing`] and [`opentelemetry`]
 /// libraries.
 ///
@@ -255,14 +233,31 @@ where
     let service_name = service_name.to_string();
 
     let config = config.into();
-    let (stderr_log_filter, stderr_reloader) = reload::Layer::new(config.stderr_log.filter.clone());
+
+    let stderr_log_layer: Box<dyn Layer<Registry> + Send + Sync> = match config.stderr_log.format {
+        StderrLogFormat::Text { prefix } => {
+            // See: https://no-color.org/
+            let no_color = std::env::var_os("NO_COLOR").unwrap_or_else(|| "".into()) != "";
+            Box::new(
+                fmt::layer()
+                    .with_writer(io::stderr)
+                    .event_format(PrefixFormat {
+                        inner: format(),
+                        prefix,
+                    })
+                    .with_ansi(!no_color && atty::is(atty::Stream::Stderr)),
+            )
+        }
+        StderrLogFormat::Json => Box::new(fmt::layer().with_writer(io::stderr).json()),
+    };
+    let (stderr_log_layer, stderr_reloader) =
+        reload::Layer::new(stderr_log_layer.with_filter(config.stderr_log.filter));
     let stderr_callback = DynamicTargetsCallback {
         callback: Arc::new(move |targets| {
-            stderr_reloader.reload(targets)?;
+            stderr_reloader.modify(|layer| *layer.filter_mut() = targets)?;
             Ok(())
         }),
     };
-    let stderr_log_layer = stderr_log_layer(config.stderr_log).with_filter(stderr_log_filter);
 
     let (otel_layer, otel_reloader) = if let Some(otel_config) = config.opentelemetry {
         // TODO(guswynn): figure out where/how to call
@@ -319,7 +314,7 @@ where
             .with_filter(filter);
         let reloader = DynamicTargetsCallback {
             callback: Arc::new(move |targets| {
-                filter_handle.reload(targets)?;
+                filter_handle.modify(|filter| *filter = targets)?;
                 Ok(())
             }),
         };