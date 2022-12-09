MaterializeInc/materialize

commit ac1028d87a2c7b44b80140007348b5c1a044ff54

orchestrator-process: rewrite
Rewrite the process orchestrator to be more reliable:

  * Use Unix domain sockets rather than TCP, to avoid competing with
    other `environmentd` processes (e.g., in tests) for a very limited
    range of TCP ports.

  * Restructure process supervision so that existing processes that are
    adopted at startup are monitored for failures and restarted if
    necessary.

  * Enable process status updates.

  * Enable process metrics collection.

Fix #15725.
Fix #15336.

src/orchestrator-tracing/src/lib.rs
@@ -9,6 +9,7 @@
 
 //! Service orchestration for tracing-aware services.
 
+use std::collections::HashMap;
 use std::ffi::OsString;
 use std::fmt;
 #[cfg(feature = "tokio-console")]
@@ -29,7 +30,7 @@ use tracing_subscriber::filter::Targets;
 #[cfg(feature = "tokio-console")]
 use mz_orchestrator::ServicePort;
 use mz_orchestrator::{
-    NamespacedOrchestrator, Orchestrator, Service, ServiceAssignments, ServiceConfig, ServiceEvent,
+    NamespacedOrchestrator, Orchestrator, Service, ServiceConfig, ServiceEvent,
     ServiceProcessMetrics,
 };
 use mz_ore::cli::{DefaultTrue, KeyValueArg};
@@ -301,10 +302,10 @@ impl NamespacedOrchestrator for NamespacedTracingOrchestrator {
         id: &str,
         mut service_config: ServiceConfig<'_>,
     ) -> Result<Box<dyn Service>, anyhow::Error> {
-        let args_fn = |assigned: &ServiceAssignments| {
+        let args_fn = |listen_addrs: &HashMap<String, String>| {
             #[cfg(feature = "tokio-console")]
-            let tokio_console_port = assigned.ports.get("tokio-console");
-            let mut args = (service_config.args)(assigned);
+            let tokio_console_listen_addr = listen_addrs.get("tokio-console");
+            let mut args = (service_config.args)(listen_addrs);
             let TracingCliArgs {
                 log_filter,
                 log_prefix,
@@ -345,10 +346,10 @@ impl NamespacedOrchestrator for NamespacedTracingOrchestrator {
                 args.push(format!("--opentelemetry-enabled={}", opentelemetry_enabled));
             }
             #[cfg(feature = "tokio-console")]
-            if let Some(port) = tokio_console_port {
+            if let Some(tokio_console_listen_addr) = tokio_console_listen_addr {
                 args.push(format!(
-                    "--tokio-console-listen-addr={}:{}",
-                    assigned.listen_host, port,
+                    "--tokio-console-listen-addr={}",
+                    tokio_console_listen_addr,
                 ));
                 args.push(format!(
                     "--tokio-console-publish-interval={} us",

src/orchestrator-kubernetes/src/lib.rs
@@ -9,7 +9,6 @@
 
 use std::collections::{BTreeMap, HashMap};
 use std::fmt;
-use std::net::{IpAddr, Ipv4Addr};
 use std::num::NonZeroUsize;
 use std::sync::{Arc, Mutex};
 
@@ -39,8 +38,8 @@ use tracing::warn;
 use mz_cloud_resources::crd::vpc_endpoint::v1::VpcEndpoint;
 use mz_cloud_resources::AwsExternalIdPrefix;
 use mz_orchestrator::{
-    LabelSelectionLogic, NamespacedOrchestrator, Orchestrator, Service, ServiceAssignments,
-    ServiceConfig, ServiceEvent, ServiceStatus,
+    LabelSelectionLogic, NamespacedOrchestrator, Orchestrator, Service, ServiceConfig,
+    ServiceEvent, ServiceStatus,
 };
 use mz_orchestrator::{LabelSelector as MzLabelSelector, ServiceProcessMetrics};
 
@@ -508,29 +507,11 @@ impl NamespacedOrchestrator for NamespacedKubernetesOrchestrator {
             .iter()
             .map(|p| (p.name.clone(), p.port_hint))
             .collect::<HashMap<_, _>>();
-        let peers = hosts
+        let listen_addrs = ports_in
             .iter()
-            .map(|host| (host.clone(), ports.clone()))
-            .collect::<Vec<_>>();
-
-        let mut node_selector: BTreeMap<String, String> = self
-            .config
-            .service_node_selector
-            .clone()
-            .into_iter()
-            .collect();
-        if let Some(availability_zone) = availability_zone {
-            node_selector.insert(
-                "materialize.cloud/availability-zone".to_string(),
-                availability_zone,
-            );
-        }
-        let mut args = args(&ServiceAssignments {
-            listen_host: IpAddr::V4(Ipv4Addr::UNSPECIFIED),
-            ports: &ports,
-            index: None,
-            peers: &peers,
-        });
+            .map(|p| (p.name.clone(), format!("0.0.0.0:{}", p.port_hint)))
+            .collect::<HashMap<_, _>>();
+        let mut args = args(&listen_addrs);
         args.push("--secrets-reader=kubernetes".into());
         args.push(format!(
             "--secrets-reader-kubernetes-context={}",
@@ -566,6 +547,19 @@ impl NamespacedOrchestrator for NamespacedKubernetesOrchestrator {
             // cluster-autoscaler. Notably, eviction of pods for resource overuse is still enabled.
             "cluster-autoscaler.kubernetes.io/safe-to-evict".to_owned() => "false".to_string(),
         };
+
+        let mut node_selector: BTreeMap<String, String> = self
+            .config
+            .service_node_selector
+            .clone()
+            .into_iter()
+            .collect();
+        if let Some(availability_zone) = availability_zone {
+            node_selector.insert(
+                "materialize.cloud/availability-zone".to_string(),
+                availability_zone,
+            );
+        }
 
         let container_name = image
             .splitn(2, '/')

src/orchestrator-process/Cargo.toml
@@ -12,6 +12,7 @@ async-stream = "0.3.3"
 async-trait = "0.1.58"
 chrono = { version = "0.4.23", default_features = false, features = ["clock"] }
 futures = "0.3.25"
+hex = "0.4.3"
 itertools = "0.10.5"
 mz-orchestrator = { path = "../orchestrator" }
 mz-ore = { path = "../ore", features = ["async"] }
@@ -20,6 +21,7 @@ mz-repr = { path = "../repr" }
 mz-secrets = { path = "../secrets" }
 serde_json = "1.0.88"
 scopeguard = "1.1.0"
+sha1 = "0.10.5"
 sysinfo = "0.26.7"
 tokio = { version = "1.22.0", features = [ "fs", "process", "time" ] }
 tracing = "0.1.37"

src/orchestrator/src/lib.rs
@@ -9,7 +9,6 @@
 
 use std::collections::HashMap;
 use std::fmt;
-use std::net::IpAddr;
 use std::num::NonZeroUsize;
 use std::str::FromStr;
 use std::sync::Arc;
@@ -171,9 +170,9 @@ pub struct ServiceConfig<'a> {
     /// configure for the pod running the service.
     pub init_container_image: Option<String>,
     /// A function that generates the arguments for each process of the service
-    /// given the assignments that the orchestrator has made.
+    /// given the assigned listen addresses for each named port.
     #[derivative(Debug = "ignore")]
-    pub args: &'a (dyn Fn(&ServiceAssignments) -> Vec<String> + Send + Sync),
+    pub args: &'a (dyn Fn(&HashMap<String, String>) -> Vec<String> + Send + Sync),
     /// Ports to expose.
     pub ports: Vec<ServicePort>,
     /// An optional limit on the memory that the service can use.
@@ -209,25 +208,6 @@ pub struct ServicePort {
     ///
     /// Not all orchestrator backends will make use of the hint.
     pub port_hint: u16,
-}
-
-/// Assignments that the orchestrator has made for a service.
-pub struct ServiceAssignments<'a> {
-    /// The host that the service should bind to.
-    pub listen_host: IpAddr,
-    /// The assigned port for each entry in [`ServiceConfig::ports`].
-    pub ports: &'a HashMap<String, u16>,
-    /// The index of this service in [`peers`](ServiceAssignments::peers), if
-    /// known.
-    ///
-    /// Not all orchestrators are capable of providing this information.
-    pub index: Option<usize>,
-    /// The hostname and port assignments for each peer in the service. The
-    /// order of peers is significant. Each peer is uniquely identified by its
-    /// position in the slice.
-    ///
-    /// The number of peers is determined by [`ServiceConfig::scale`].
-    pub peers: &'a [(String, HashMap<String, u16>)],
 }
 
 /// Describes a limit on memory.

src/compute-client/src/controller.rs
@@ -41,16 +41,17 @@ use chrono::{DateTime, Utc};
 use differential_dataflow::lattice::Lattice;
 use futures::stream::BoxStream;
 use futures::{future, FutureExt, StreamExt};
-use mz_ore::{halt, soft_assert};
 use serde::{Deserialize, Serialize};
 use timely::progress::frontier::{AntichainRef, MutableAntichain};
 use timely::progress::{Antichain, Timestamp};
+use tracing::warn;
 use uuid::Uuid;
 
 use mz_build_info::BuildInfo;
 use mz_expr::RowSetFinishing;
 use mz_orchestrator::{CpuLimit, MemoryLimit, NamespacedOrchestrator, ServiceProcessMetrics};
 use mz_ore::tracing::OpenTelemetryContext;
+use mz_ore::{halt, soft_assert};
 use mz_repr::{GlobalId, Row};
 use mz_storage_client::controller::{ReadPolicy, StorageController};
 
@@ -496,7 +497,7 @@ where
                 let metrics = match result {
                     Ok(metrics) => metrics,
                     Err(e) => {
-                        tracing::log::warn!("Failed to get metrics for replica {replica_id}: {e}");
+                        warn!("failed to get metrics for replica {replica_id}: {e}");
                         return;
                     }
                 };
@@ -761,7 +762,7 @@ where
             if let Ok(mut instance) = self.instance(instance_id) {
                 instance.handle_response(response, replica_id)
             } else {
-                tracing::warn!(
+                warn!(
                     ?instance_id,
                     ?response,
                     "processed response from unknown instance"

src/compute-client/src/controller/orchestrator.rs
@@ -105,23 +105,12 @@ impl ComputeOrchestrator {
                     image: self.computed_image.clone(),
                     init_container_image: self.init_container_image.clone(),
                     args: &|assigned| {
-                        let mut compute_opts = vec![
-                            format!(
-                                "--controller-listen-addr={}:{}",
-                                assigned.listen_host, assigned.ports["controller"]
-                            ),
-                            format!(
-                                "--internal-http-listen-addr={}:{}",
-                                assigned.listen_host, assigned.ports["internal-http"]
-                            ),
+                        vec![
+                            format!("--controller-listen-addr={}", assigned["controller"]),
+                            format!("--internal-http-listen-addr={}", assigned["internal-http"]),
                             format!("--opentelemetry-resource=instance_id={}", instance_id),
                             format!("--opentelemetry-resource=replica_id={}", replica_id),
-                        ];
-                        if let Some(index) = assigned.index {
-                            compute_opts
-                                .push(format!("--opentelemetry-resource=replica_index={}", index));
-                        }
-                        compute_opts
+                        ]
                     },
                     ports: vec![
                         ServicePort {

src/environmentd/src/bin/environmentd/main.rs
@@ -54,7 +54,6 @@ use mz_orchestrator_process::{ProcessOrchestrator, ProcessOrchestratorConfig};
 use mz_orchestrator_tracing::{TracingCliArgs, TracingOrchestrator};
 use mz_ore::cgroup::{detect_memory_limit, MemoryLimit};
 use mz_ore::cli::{self, CliConfig, KeyValueArg};
-use mz_ore::id_gen::PortAllocator;
 use mz_ore::metrics::MetricsRegistry;
 use mz_ore::now::SYSTEM_TIME;
 use mz_persist_client::cache::PersistClientCache;
@@ -292,13 +291,6 @@ pub struct Args {
     /// value.
     #[clap(long, env = "ORCHESTRATOR_PROCESS_WRAPPER")]
     orchestrator_process_wrapper: Option<String>,
-    /// Base port for services spawned by the process orchestrator.
-    #[structopt(
-        long,
-        env = "ORCHESTRATOR_PROCESS_BASE_SERVICE_PORT",
-        default_value = "2100"
-    )]
-    orchestrator_process_base_service_port: u16,
     /// Where the process orchestrator should store secrets.
     #[clap(
         long,
@@ -600,12 +592,6 @@ fn run(mut args: Args) -> Result<(), anyhow::Error> {
                         // binaries and release binaries look for other release
                         // binaries.
                         image_dir: env::current_exe()?.parent().unwrap().to_path_buf(),
-                        port_allocator: Arc::new(PortAllocator::new(
-                            args.orchestrator_process_base_service_port,
-                            args.orchestrator_process_base_service_port
-                                .checked_add(1000)
-                                .expect("Port number overflow, base-service-port too large."),
-                        )),
                         suppress_output: false,
                         environment_id: args.environment_id.clone(),
                         secrets_dir: args

commit 597ae1d22c41be28286a7d8c5748f265722f79f8

Support UDS listen address for tokio-console
This commit fixes local tokio-console support, which was broken in
ac1028d because tokio-console does not support Unix domain sockets
(yet).

The fix involves adding that support by switching to Nikhil's fork of
concole-subscriber and using the `mz_ore::netio::SocketAddr` type for
the tokio-console listen address.

src/orchestrator-tracing/src/lib.rs
@@ -12,8 +12,6 @@
 use std::collections::HashMap;
 use std::ffi::OsString;
 use std::fmt;
-#[cfg(feature = "tokio-console")]
-use std::net::SocketAddr;
 use std::str::FromStr;
 use std::sync::Arc;
 #[cfg(feature = "tokio-console")]
@@ -34,6 +32,8 @@ use mz_orchestrator::{
     ServiceProcessMetrics,
 };
 use mz_ore::cli::{DefaultTrue, KeyValueArg};
+#[cfg(feature = "tokio-console")]
+use mz_ore::netio::SocketAddr;
 #[cfg(feature = "tokio-console")]
 use mz_ore::tracing::TokioConsoleConfig;
 use mz_ore::tracing::{
@@ -227,13 +227,13 @@ impl From<&TracingCliArgs> for TracingConfig {
                 }
             }),
             #[cfg(feature = "tokio-console")]
-            tokio_console: args
-                .tokio_console_listen_addr
-                .map(|listen_addr| TokioConsoleConfig {
+            tokio_console: args.tokio_console_listen_addr.clone().map(|listen_addr| {
+                TokioConsoleConfig {
                     listen_addr,
                     publish_interval: args.tokio_console_publish_interval,
                     retention: args.tokio_console_retention,
-                }),
+                }
+            }),
             sentry: args.sentry_dsn.clone().map(|dsn| SentryConfig {
                 dsn,
                 tags: args

src/ore/Cargo.toml
@@ -56,7 +56,7 @@ hyper = { version = "0.14.23", features = ["http1", "server"], optional = true }
 hyper-tls = { version = "0.5.0", optional = true }
 opentelemetry = { git = "https://github.com/MaterializeInc/opentelemetry-rust.git", features = ["rt-tokio", "trace"], optional = true }
 opentelemetry-otlp = { git = "https://github.com/MaterializeInc/opentelemetry-rust.git", optional = true }
-console-subscriber = { version = "0.1.8", optional = true }
+console-subscriber = { git = "https://github.com/benesch/tokio-console.git", branch = "uds", optional = true }
 sentry-tracing = { version = "0.29.0", optional = true }
 
 [dev-dependencies]

src/ore/src/tracing/mod.rs
@@ -22,8 +22,6 @@
 use std::borrow::Cow;
 use std::collections::HashMap;
 use std::io;
-#[cfg(feature = "tokio-console")]
-use std::net::SocketAddr;
 use std::sync::Arc;
 use std::time::Duration;
 
@@ -49,6 +47,9 @@ use tracing_subscriber::layer::{Layer, SubscriberExt};
 use tracing_subscriber::registry::LookupSpan;
 use tracing_subscriber::util::SubscriberInitExt;
 use tracing_subscriber::{reload, Registry};
+
+#[cfg(feature = "tokio-console")]
+use crate::netio::SocketAddr;
 
 /// Application tracing configuration.
 ///
@@ -330,12 +331,17 @@ where
 
     #[cfg(feature = "tokio-console")]
     let tokio_console_layer = if let Some(console_config) = config.tokio_console.clone() {
-        let layer = ConsoleLayer::builder()
-            .server_addr(console_config.listen_addr)
+        let builder = ConsoleLayer::builder()
             .publish_interval(console_config.publish_interval)
-            .retention(console_config.retention)
-            .spawn();
-        Some(layer)
+            .retention(console_config.retention);
+        let builder = match console_config.listen_addr {
+            SocketAddr::Inet(addr) => builder.server_addr(addr),
+            SocketAddr::Unix(addr) => {
+                let path = addr.as_pathname().unwrap().as_ref();
+                builder.server_addr(path)
+            }
+        };
+        Some(builder.spawn())
     } else {
         None
     };
@@ -373,10 +379,11 @@ where
 
     #[cfg(feature = "tokio-console")]
     if let Some(console_config) = config.tokio_console {
-        tracing::info!(
-            "starting tokio console on http://{}",
-            console_config.listen_addr
-        );
+        let endpoint = match console_config.listen_addr {
+            SocketAddr::Inet(addr) => format!("http://{addr}"),
+            SocketAddr::Unix(addr) => addr.to_string(),
+        };
+        tracing::info!("starting tokio console on {endpoint}");
     }
 
     Ok((