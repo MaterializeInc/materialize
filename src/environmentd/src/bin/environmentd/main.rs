f0315dea233281ecf36f9999a6a2104f5ee8dc9a
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2022-12-11 21:53:24 +0200

    environmentd: remove env vars from `invoked as:`

--- src/environmentd/src/bin/environmentd/main.rs
+++ src/environmentd/src/bin/environmentd/main.rs
@@ -693,16 +693,9 @@ max log level: {max_log_level}",
         dep_versions = build_info().join("\n"),
         invocation = {
             use shell_words::quote as escape;
-            env::vars_os()
-                .map(|(name, value)| {
-                    (
-                        name.to_string_lossy().into_owned(),
-                        value.to_string_lossy().into_owned(),
-                    )
-                })
-                .filter(|(name, _value)| name.starts_with("MZ_") || name == "FAILPOINTS")
-                .map(|(name, value)| format!("{}={}", escape(&name), escape(&value)))
-                .chain(env::args().into_iter().map(|arg| escape(&arg).into_owned()))
+            env::args()
+                .into_iter()
+                .map(|arg| escape(&arg).into_owned())
                 .join(" ")
         },
         os = os_info::get(),
