f913a252de9a7b5edbdb58784ccbc2d33f2b56fb
umanwizard <brennan@materialize.com>
2022-12-15 09:55:21 -0500

    Update src/adapter/src/catalog/builtin.rs

--- src/adapter/src/catalog/builtin.rs
+++ src/adapter/src/catalog/builtin.rs
@@ -2265,8 +2265,8 @@ pub const MZ_CLUSTER_REPLICA_UTILIZATION: BuiltinView = BuiltinView {
 SELECT
     r.id AS replica_id,
     m.process_id,
-    100 * m.cpu_nano_cores / s.cpu_nano_cores AS cpu_percent,
-    100 * m.memory_bytes / s.memory_bytes AS memory_percent
+    m.cpu_nano_cores::float8 / s.cpu_nano_cores AS cpu_percent,
+    m.memory_bytes::float8 / s.memory_bytes AS memory_percent
 FROM
     mz_cluster_replicas AS r
         JOIN mz_internal.mz_cluster_replica_sizes AS s ON r.size = s.size
