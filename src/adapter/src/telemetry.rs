eed75532ecbaa226be3432d02b8750294723d5f6
Nikhil Benesch <nikhil.benesch@gmail.com>
2022-12-24 18:18:21 -0500

    adapter,environmentd: move segment context fields to properties
    Many Segment destinations only understand properties and canonical
    context fields, and can't understand non-canonical context fields. So
    promote the non-cononical context fields to properties.
    
    Fix MaterializeInc/analytics#97.

--- src/adapter/src/telemetry.rs
+++ src/adapter/src/telemetry.rs
@@ -10,21 +10,63 @@
 //! Telemetry utilities.
 
 use serde_json::json;
+use uuid::Uuid;
 
 use mz_sql::catalog::EnvironmentId;
 
-/// Extension trait for [`EnvironmentId`].
-pub trait EnvironmentIdExt {
-    /// Returns the Segment context fields associated with this environment ID.
-    fn as_segment_context(&self) -> serde_json::Value;
+/// Extension trait for [`mz_segment::Client`].
+pub trait SegmentClientExt {
+    /// Tracks an event associated with an environment.
+    fn environment_track<S>(
+        &self,
+        environment_id: &EnvironmentId,
+        user_id: Uuid,
+        event: S,
+        properties: serde_json::Value,
+    ) where
+        S: Into<String>;
 }
 
-impl EnvironmentIdExt for EnvironmentId {
-    fn as_segment_context(&self) -> serde_json::Value {
-        json!({
-            "group_id": self.organization_id(),
-            "cloud_provider": self.cloud_provider().to_string(),
-            "cloud_provider_region": self.cloud_provider_region(),
-        })
+impl SegmentClientExt for mz_segment::Client {
+    /// Tracks an event associated with an environment.
+    ///
+    /// Various metadata about the environment is automatically attached
+    /// using canonical field names.
+    ///
+    /// # Panics
+    ///
+    /// Panics if `properties` is not a [`serde_json::Value::Object`].
+    fn environment_track<S>(
+        &self,
+        environment_id: &EnvironmentId,
+        user_id: Uuid,
+        event: S,
+        mut properties: serde_json::Value,
+    ) where
+        S: Into<String>,
+    {
+        {
+            let properties = match &mut properties {
+                serde_json::Value::Object(map) => map,
+                _ => {
+                    panic!("SegmentClientExt::environment_track called with non-object properties")
+                }
+            };
+            properties.insert("event_source".into(), json!("environment"));
+            properties.insert(
+                "cloud_provider".into(),
+                json!(environment_id.cloud_provider().to_string()),
+            );
+            properties.insert(
+                "cloud_provider_region".into(),
+                json!(environment_id.cloud_provider_region()),
+            );
+        }
+        self.track(
+            user_id,
+            event,
+            properties,
+            Some(json!({ "group_id": environment_id.organization_id() })),
+        );
     }
 }
