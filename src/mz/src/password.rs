c94c12801d3b4fa50db9066cb8f2221422d3bfde
Seth Wiesman <sjwiesman@gmail.com>
2022-12-27 10:22:27 -0600

    mz: cleanup header management

--- src/mz/src/password.rs
+++ src/mz/src/password.rs
@@ -9,10 +9,9 @@
 
 use std::collections::HashMap;
 
-use crate::configuration::ValidProfile;
 use crate::FronteggAppPassword;
+use crate::{configuration::ValidProfile, utils::RequestBuilderExt};
 use anyhow::{Context, Result};
-use reqwest::header::{HeaderMap, HeaderValue, AUTHORIZATION, CONTENT_TYPE, USER_AGENT};
 use reqwest::Client;
 
 /// Get Frontegg API tokens using an access token
@@ -20,21 +19,12 @@ pub(crate) async fn list_passwords(
     client: &Client,
     valid_profile: &ValidProfile<'_>,
 ) -> Result<Vec<FronteggAppPassword>> {
-    let authorization: String = format!("Bearer {}", valid_profile.frontegg_auth.access_token);
-
-    let mut headers = HeaderMap::new();
-    headers.insert(USER_AGENT, HeaderValue::from_static("reqwest"));
-    headers.insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
-    headers.insert(
-        AUTHORIZATION,
-        HeaderValue::from_str(authorization.as_str()).unwrap(),
-    );
     let mut body = HashMap::new();
     body.insert("description", &"App password for the CLI");
 
     client
         .get(valid_profile.profile.endpoint().api_token_url())
-        .headers(headers)
+        .authenticate(&valid_profile.frontegg_auth)
         .json(&body)
         .send()
         .await
