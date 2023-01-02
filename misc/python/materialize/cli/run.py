MaterializeInc/materialize

commit f0c63fa5affb12d8f93701a015d95280e006438f

orchestrator-process: export a prometheus service discovery configuration
This makes it possible to run Prometheus locally, using our setup in
misc/prometheus, with auto-discovery of all created clusters.

misc/python/materialize/cli/run.py
@@ -111,7 +111,7 @@ def main() -> int:
             path = ROOT / "target" / "release" / args.program
         else:
             path = ROOT / "target" / "debug" / args.program
-        command = [str(path), *args.args]
+        command = [str(path)]
         if args.tokio_console:
             command += ["--tokio-console-listen-addr=127.0.0.1:6669"]
         if args.program == "environmentd":
@@ -128,8 +128,16 @@ def main() -> int:
             # files that have been deleted. There's no race if we reset in the
             # opposite order.
             if args.reset:
-                print("Removing mzdata directory...")
-                shutil.rmtree(mzdata, ignore_errors=True)
+                # Remove everything in the `mzdata`` directory *except* for
+                # the `prometheus` directory.
+                paths = list(mzdata.glob("prometheus/*"))
+                paths.extend(p for p in mzdata.glob("*") if p.name != "prometheus")
+                for path in paths:
+                    print(f"Removing {path}...")
+                    if path.is_dir():
+                        shutil.rmtree(path, ignore_errors=True)
+                    else:
+                        path.unlink()
 
             mzdata.mkdir(exist_ok=True)
             environment_file = mzdata / "environment-id"
@@ -140,14 +148,20 @@ def main() -> int:
                 environment_file.write_text(environment_id)
 
             command += [
+                # Setting the listen addresses below to 0.0.0.0 is required
+                # to allow Prometheus running in Docker (misc/prometheus)
+                # access these services to scrape metrics.
+                f"--internal-http-listen-addr=0.0.0.0:6878",
                 f"--orchestrator=process",
                 f"--orchestrator-process-secrets-directory={mzdata}/secrets",
-                f"--orchestrator-process-tcp-proxy-listen-addr=127.0.0.1",
+                f"--orchestrator-process-tcp-proxy-listen-addr=0.0.0.0",
+                f"--orchestrator-process-prometheus-service-discovery-directory={mzdata}/prometheus",
                 f"--persist-consensus-url={args.postgres}?options=--search_path=consensus",
                 f"--persist-blob-url=file://{mzdata}/persist/blob",
                 f"--adapter-stash-url={args.postgres}?options=--search_path=adapter",
                 f"--storage-stash-url={args.postgres}?options=--search_path=storage",
                 f"--environment-id={environment_id}",
+                *args.args,
             ]
         elif args.program == "sqllogictest":
             _handle_lingering_services(kill=True)

misc/monitoring/grafana/datasources/prometheus.yml
@@ -1,0 +1,14 @@
+# Copyright Materialize, Inc. and contributors. All rights reserved.
+#
+# Use of this software is governed by the Business Source License
+# included in the LICENSE file at the root of this repository.
+#
+# As of the Change Date specified in that file, in accordance with
+# the Business Source License, use of this software will be governed
+# by the Apache License, Version 2.0.
+
+apiVersion: 1
+datasources:
+  - name: Prometheus
+    type: prometheus
+    url: http://prometheus:9090

misc/monitoring/mzcompose
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # Copyright Materialize, Inc. and contributors. All rights reserved.
 #
 # Use of this software is governed by the Business Source License
@@ -6,13 +8,7 @@
 # As of the Change Date specified in that file, in accordance with
 # the Business Source License, use of this software will be governed
 # by the Apache License, Version 2.0.
+#
+# mzcompose — runs Docker Compose with Materialize customizations.
 
-global:
-  scrape_interval: 15s
-scrape_configs:
-  - job_name: environmentd
-    static_configs:
-    - targets: ["host.docker.internal:6878"]
-      labels:
-        namespace: localhost
-        pod: environmentd
+exec "$(dirname "$0")"/../../bin/pyactivate -m materialize.cli.mzcompose "$@"

misc/monitoring/mzcompose.py
@@ -1,0 +1,54 @@
+# Copyright Materialize, Inc. and contributors. All rights reserved.
+#
+# Use of this software is governed by the Business Source License
+# included in the LICENSE file at the root of this repository.
+#
+# As of the Change Date specified in that file, in accordance with
+# the Business Source License, use of this software will be governed
+# by the Apache License, Version 2.0.
+
+from materialize import ROOT
+from materialize.mzcompose import Composition
+from materialize.mzcompose.services import Service
+
+SERVICES = [
+    Service(
+        "prometheus",
+        {
+            "image": "prom/prometheus:v2.41.0",
+            "ports": ["9090:9090"],
+            "volumes": [
+                "./prometheus.yml:/etc/prometheus/prometheus.yml",
+                "../../mzdata/prometheus:/mnt/services",
+            ],
+            "extra_hosts": ["host.docker.internal:host-gateway"],
+            "allow_host_ports": True,
+        },
+    ),
+    Service(
+        "grafana",
+        {
+            "image": "grafana/grafana:9.3.2",
+            "ports": ["3000:3000"],
+            "environment": [
+                "GF_AUTH_ANONYMOUS_ENABLED=true",
+                "GF_AUTH_ANONYMOUS_ORG_ROLE=Admin",
+            ],
+            "volumes": [
+                "./grafana/datasources:/etc/grafana/provisioning/datasources",
+            ],
+            "allow_host_ports": True,
+        },
+    ),
+]
+
+
+def workflow_default(c: Composition) -> None:
+    # Create the `mzdata/prometheus` directory that will be bind mounted into
+    # the container before invoking Docker Compose, since otherwise the Docker
+    # daemon will create the directory as root, and `environmentd` won't be
+    # able to write to it.
+    (ROOT / "mzdata" / "prometheus").mkdir(parents=True, exist_ok=True)
+    c.up()
+    print(f"Prometheus running at http://localhost:{c.default_port('prometheus')}")
+    print(f"Grafana running at http://localhost:{c.default_port('grafana')}")

misc/monitoring/prometheus.yml
@@ -1,0 +1,46 @@
+# Copyright Materialize, Inc. and contributors. All rights reserved.
+#
+# Use of this software is governed by the Business Source License
+# included in the LICENSE file at the root of this repository.
+#
+# As of the Change Date specified in that file, in accordance with
+# the Business Source License, use of this software will be governed
+# by the Apache License, Version 2.0.
+
+global:
+  scrape_interval: 15s
+scrape_configs:
+  - job_name: environmentd
+    static_configs:
+    - targets: [host.docker.internal:6878]
+      labels:
+        namespace: local
+        pod: environmentd-0
+  - job_name: services
+    file_sd_configs:
+    - files:
+      - /mnt/services/*.json
+      refresh_interval: 30s
+    relabel_configs:
+      # Rewrite references to 127.0.0.1 or 0.0.0.0 to host.docker.internal,
+      # since the services are running on the host.
+      - source_labels: [__address__]
+        target_label: __address__
+        regex: (127\.0\.0\.1|0\.0\.0\.0)(.*)
+        replacement: host.docker.internal$2
+        action: replace
+      # The process orchestrator emits static configurations for all ports, but
+      # only the "internal-http" port serves metrics. Filter out other configs,
+      # to avoid scrape failures in the Prometheus UI.
+      - source_labels: [mz_orchestrator_port]
+        regex: internal-http
+        action: keep
+      # Construct namespace and pod labels that are similar to the label that
+      # Kubernetes installs, so that production dashboards work without changes
+      # when running locally.
+      - target_label: namespace
+        replacement: local
+      - source_labels: [mz_orchestrator_namespace, mz_orchestrator_service_id]
+        separator: "-"
+        target_label: pod
+        replacement: $1-0

misc/python/materialize/mzcompose/__init__.py
@@ -839,6 +839,9 @@ class ServiceConfig(TypedDict, total=False):
     By default, the name of the service is used as the hostname.
     """
 
+    extra_hosts: List[str]
+    """Additional hostname mappings."""
+
     entrypoint: List[str]
     """Override the entrypoint specified in the image."""
 

src/environmentd/src/bin/environmentd/main.rs
@@ -115,7 +115,9 @@ use mz_orchestrator::Orchestrator;
 use mz_orchestrator_kubernetes::{
     KubernetesImagePullPolicy, KubernetesOrchestrator, KubernetesOrchestratorConfig,
 };
-use mz_orchestrator_process::{ProcessOrchestrator, ProcessOrchestratorConfig};
+use mz_orchestrator_process::{
+    ProcessOrchestrator, ProcessOrchestratorConfig, ProcessOrchestratorTcpProxyConfig,
+};
 use mz_orchestrator_tracing::{StaticTracingConfig, TracingCliArgs, TracingOrchestrator};
 use mz_ore::cli::{self, CliConfig, KeyValueArg};
 use mz_ore::metric;
@@ -386,6 +388,23 @@ pub struct Args {
     /// browsers).
     #[clap(long, env = "ORCHESTRATOR_PROCESS_TCP_PROXY_LISTEN_ADDR")]
     orchestrator_process_tcp_proxy_listen_addr: Option<IpAddr>,
+    /// A directory in which the process orchestrator should write Prometheus
+    /// scrape targets, for use with Prometheus's file-based service discovery.
+    ///
+    /// Each namespaced orchestrator will maintain a single JSON file into the
+    /// directory named `NAMESPACE.json` containing the scrape targets for all
+    /// extant services. The scrape targets will use the TCP proxy address, as
+    /// Prometheus does not support scraping over Unix domain sockets.
+    ///
+    /// This option is ignored unless
+    /// `--orchestrator-process-tcp-proxy-listen-addr` is set.
+    ///
+    /// See also: <https://prometheus.io/docs/guides/file-sd/>
+    #[clap(
+        long,
+        env = "ORCHESTRATOR_PROCESS_PROMETHEUS_SERVICE_DISCOVERY_DIRECTORY"
+    )]
+    orchestrator_process_prometheus_service_discovery_directory: Option<PathBuf>,
     /// The clusterd image reference to use.
     #[structopt(
         long,
@@ -713,7 +732,13 @@ fn run(mut args: Args) -> Result<(), anyhow::Error> {
                             .orchestrator_process_wrapper
                             .map_or(Ok(vec![]), |s| shell_words::split(&s))?,
                         propagate_crashes: args.orchestrator_process_propagate_crashes,
-                        tcp_proxy_listen_addr: args.orchestrator_process_tcp_proxy_listen_addr,
+                        tcp_proxy: args.orchestrator_process_tcp_proxy_listen_addr.map(
+                            |listen_addr| ProcessOrchestratorTcpProxyConfig {
+                                listen_addr,
+                                prometheus_service_discovery_dir: args
+                                    .orchestrator_process_prometheus_service_discovery_directory,
+                            },
+                        ),
                     }))
                     .context("creating process orchestrator")?,
             );

src/environmentd/tests/util.rs
@@ -261,7 +261,7 @@ pub fn start_server(config: Config) -> Result<Server, anyhow::Error> {
             secrets_dir: data_directory.join("secrets"),
             command_wrapper: vec![],
             propagate_crashes: config.propagate_crashes,
-            tcp_proxy_listen_addr: None,
+            tcp_proxy: None,
         }))?,
     );
     // Messing with the clock causes persist to expire leases, causing hangs and

commit 3aa55da877399daf6c87acd3e75ab75aa9680175

Fix run's invocation of sqllogictest
Commit f0c63fa5aff broke supplying additional arguments to
sqllogictest.

misc/python/materialize/cli/run.py
@@ -164,7 +164,7 @@ def main() -> int:
                 *args.args,
             ]
         elif args.program == "sqllogictest":
-            command += [f"--postgres-url={args.postgres}"]
+            command += [f"--postgres-url={args.postgres}", *args.args]
     elif args.program == "test":
         build_retcode = _build(args)
         if args.build_only: