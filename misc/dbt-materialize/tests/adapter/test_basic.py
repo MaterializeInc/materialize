a5d616a547e257d010ae1f6a29c5336317435701
Dennis Hume <dennis@materialize.com>
2023-01-23 13:13:28 -0600

    Replace Deprecated Exceptions

--- misc/dbt-materialize/tests/adapter/test_basic.py
+++ misc/dbt-materialize/tests/adapter/test_basic.py
@@ -147,7 +147,7 @@ class TestBaseAdapterMethodMaterialize(BaseAdapterMethod):
     {%- do adapter.drop_schema(upstream) -%}
     {% set existing = adapter.get_relation(upstream.database, upstream.schema, upstream.identifier) %}
     {% if existing is not none %}
-        {% do exceptions.raise_compiler_error('expected ' ~ ' to not exist, but it did') %}
+        {% do exceptions.CompilationError('expected ' ~ ' to not exist, but it did') %}
     {% endif %}
     {%- do adapter.create_schema(upstream) -%}
     {% set sql = materialize__create_view_as(upstream, 'select 2 as id') %}
