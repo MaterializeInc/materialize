6cf8acf65762da83971598e38cfd65e36aabbb00
Philip Stoev <pstoev@materialize.com>
2023-01-30 12:43:30 +0100

    test: Use CRDB v22.2.3 across the board when testing
    - Have the CRDB tag to use only in a single location in the code
    - Upgrade to v22.2.3 in order to get the fix for cockroachdb/cockroach#93892
    - remove the `SET CLUSTER SETTING` workaround that was previously
      used, which was not active across all test frameworks and workflows anyway.
    
    Relates to #11431

--- misc/cockroach/setup_materialize.sql
+++ misc/cockroach/setup_materialize.sql
@@ -9,12 +9,6 @@
 
 -- Sets up a CockroachDB cluster for use by Materialize.
 
--- See: https://github.com/cockroachdb/cockroach/issues/93892
--- See: https://github.com/MaterializeInc/materialize/issues/16726
--- TODO: remove this workaround before upgrading to CockroachDB 22.2 in
--- production.
-SET CLUSTER SETTING sql.stats.forecasts.enabled = false;
-
 CREATE SCHEMA IF NOT EXISTS consensus;
 CREATE SCHEMA IF NOT EXISTS adapter;
 CREATE SCHEMA IF NOT EXISTS storage;
