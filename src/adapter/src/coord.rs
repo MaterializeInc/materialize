61fa48f25de58e51f9e637483cb9b4dd6b991cbd
Ben Kirwin <bkirwi@materialize.com>
2023-02-20 13:22:52 -0500

    Revert "Merge pull request #17555 from danhhz/persist_usage"
    This reverts commit 212ff8f4a0369a3285dce500e399cc0a332850c5, reversing
    changes made to b3a625d3075daea58bb10f25d113875eea1d0c17.

--- src/adapter/src/coord.rs
+++ src/adapter/src/coord.rs
@@ -99,7 +99,8 @@ use mz_ore::task::spawn;
 use mz_ore::thread::JoinHandleExt;
 use mz_ore::tracing::OpenTelemetryContext;
 use mz_ore::{stack, task};
-use mz_persist_client::usage::{ShardsUsage, StorageUsageClient};
+use mz_persist_client::usage::StorageUsageClient;
+use mz_persist_client::ShardId;
 use mz_repr::explain::ExplainFormat;
 use mz_repr::{Datum, Diff, GlobalId, Row, Timestamp};
 use mz_secrets::SecretsController;
@@ -201,7 +202,7 @@ pub enum Message<T = mz_repr::Timestamp> {
     },
     LinearizeReads(Vec<PendingReadTxn>),
     StorageUsageFetch,
-    StorageUsageUpdate(ShardsUsage),
+    StorageUsageUpdate(BTreeMap<Option<ShardId>, u64>),
     RealTimeRecencyTimestamp {
         conn_id: ConnectionId,
         transient_revision: u64,
