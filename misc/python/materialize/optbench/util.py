8ee802acba6896b38bd1845d7940f0d2bb224820
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2023-02-22 20:24:29 +0200

    optbench: support targeting Postgres
    Rework `optbench` commands so one can get reference nubmers from a
    Postgres datbase. This is useful for behing honest how much slower we
    actually are compared to the database we are pretending to be.

--- misc/python/materialize/optbench/util.py
+++ misc/python/materialize/optbench/util.py
@@ -26,7 +26,7 @@ def duration_to_timedelta(duration: str) -> Optional[np.timedelta64]:
         "ns": lambda frac: "0",  # ns units should not have frac
     }
 
-    p = r"(?P<time>[0-9]+)(\.(?P<frac>[0-9]+))?(?P<unit>s|ms|µs|ns)"
+    p = r"(?P<time>[0-9]+)(\.(?P<frac>[0-9]+))?\s?(?P<unit>s|ms|µs|ns)"
     m = match(p, duration)
 
     if m is None:
@@ -38,9 +38,19 @@ def duration_to_timedelta(duration: str) -> Optional[np.timedelta64]:
         return time + frac
 
 
-def results_path(repository: Path, scenario: Scenario, mz_version: str) -> Path:
-    mz_version = mz_version.replace(" ", "-")
-    mz_version = mz_version.replace("(", "")
-    mz_version = mz_version.replace(")", "")
-    file = f"optbench-{scenario}-{mz_version}.csv"
+def results_path(repository: Path, scenario: Scenario, version: str) -> Path:
+    # default suffix
+    suffix = "unknown"
+
+    # try to match Postgres version syntax
+    m = match(r"PostgreSQL (?P<version>[0-9\.]+)", version)
+    if m:
+        suffix = f"pg-v{m['version']}"
+
+    # try to match Materialize version syntax
+    m = match(r"v(?P<version>[0-9\.]+(-dev)?) \((?P<commit>[0-9a-f]+)\)", version)
+    if m:
+        suffix = f"mz-v{m['version']}-{m['commit']}"
+
+    file = f"optbench-{scenario}-{suffix}.csv"
     return repository / file
