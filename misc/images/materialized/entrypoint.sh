441e07ef185e0347f9fb2d94dbc3b9bab6172a6c
Philip Stoev <pstoev@materialize.com>
2023-02-24 12:59:58 +0200

    DO NOT MERGE. Disable fsync in CRDB

--- misc/images/materialized/entrypoint.sh
+++ misc/images/materialized/entrypoint.sh
@@ -37,6 +37,8 @@ COCKROACH_SKIP_ENABLING_DIAGNOSTIC_REPORTING=true cockroach start-single-node \
 # See: https://github.com/MaterializeInc/materialize/issues/16726
 cockroach sql --insecure -e "SET CLUSTER SETTING sql.stats.forecasts.enabled = false"
 
+cockroach sql --insecure -e "SET CLUSTER SETTING kv.raft_log.disable_synchronization_unsafe = true"
+
 cockroach sql --insecure -e "CREATE SCHEMA IF NOT EXISTS consensus"
 cockroach sql --insecure -e "CREATE SCHEMA IF NOT EXISTS storage"
 cockroach sql --insecure -e "CREATE SCHEMA IF NOT EXISTS adapter"
