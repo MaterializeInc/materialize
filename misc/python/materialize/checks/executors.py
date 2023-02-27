0b91db799c24ac47c7a8a14e4f3fed5cb68ca933
Dennis Felsing <dennis@felsin9.de>
2023-02-27 09:53:22 +0100

    Remove duplicate copyright notices
    $ git grep "Copyright Materialize, Inc"|sort|uniq -c|grep -v "      1"

--- misc/python/materialize/checks/executors.py
+++ misc/python/materialize/checks/executors.py
@@ -6,14 +6,6 @@
 # As of the Change Date specified in that file, in accordance with
 # the Business Source License, use of this software will be governed
 # by the Apache License, Version 2.0.
-# Copyright Materialize, Inc. and contributors. All rights reserved.
-#
-# Use of this software is governed by the Business Source License
-# included in the LICENSE file at the root of this repository.
-#
-# As of the Change Date specified in that file, in accordance with
-# the Business Source License, use of this software will be governed
-# by the Apache License, Version 2.0.
 
 import random
 import threading
