42ab8777af07a4185d4a63f0e92189614823a477
Ben Kirwin <bkirwi@materialize.com>
2023-03-08 12:21:54 -0500

    A couple utilities for managing keys and data

--- src/storage-client/src/source/util.rs
+++ src/storage-client/src/source/util.rs
@@ -24,6 +24,18 @@ use mz_repr::Timestamp;
 
 use crate::types::sources::SourceToken;
 
+use arrayvec::ArrayVec;
+use bytes::BufMut;
+use differential_dataflow::lattice::{Lattice, Maximum};
+use mz_persist_types::columnar::{DataType, PartDecoder, PartEncoder, Schema};
+use mz_persist_types::part::{ColumnsMut, ColumnsRef};
+use mz_persist_types::Codec;
+use serde::{Deserialize, Serialize};
+use std::sync::Arc;
+use timely::order::TotalOrder;
+use timely::progress::{PathSummary, Timestamp as TimelyTimestamp};
+use timely::PartialOrder;
+
 /// Constructs a source named `name` in `scope` whose lifetime is controlled
 /// both internally and externally.
 ///
@@ -122,4 +134,118 @@ where
     // `build()` promises to call the provided closure before returning,
     // so we are guaranteed that `token` is non-None.
     (data_stream, token.unwrap())
+}
+
+/// A key along with its serialized representation. Used to both deserialize the data and keep
+/// around the raw bytes for comparison.
+#[derive(Debug)]
+struct WithPrefix<K>(K, ArrayVec<u8, 16>);
+
+#[derive(Debug)]
+struct Prefixed<S>(S);
+
+impl<K: Codec> Codec for WithPrefix<K> {
+    type Schema = Prefixed<Arc<K::Schema>>;
+
+    fn codec_name() -> String {
+        K::codec_name()
+    }
+
+    fn encode<B>(&self, buf: &mut B)
+    where
+        B: BufMut,
+    {
+        self.0.encode(buf);
+    }
+
+    fn decode<'a>(buf: &'a [u8]) -> Result<Self, String> {
+        let len = buf.len().min(16);
+        let mut vec = ArrayVec::new();
+        vec.try_extend_from_slice(&buf[..len]).unwrap();
+        Ok(WithPrefix(K::decode(buf)?, vec))
+    }
+}
+
+impl<'a, T, D: PartDecoder<'a, T>> PartDecoder<'a, WithPrefix<T>> for Prefixed<D> {
+    fn decode(&self, idx: usize, val: &mut WithPrefix<T>) {
+        // Since this interface doesn't provide a meaningful sort key, default to the empty key.
+        // TODO: revisit the sort key type when we switch interfaces.
+        val.1.clear();
+        self.0.decode(idx, &mut val.0)
+    }
+}
+
+impl<'a, T, D: PartEncoder<'a, T>> PartEncoder<'a, WithPrefix<T>> for Prefixed<D> {
+    fn encode(&mut self, val: &WithPrefix<T>) {
+        self.0.encode(&val.0)
+    }
+}
+
+impl<K: Codec> Schema<WithPrefix<K>> for Prefixed<Arc<K::Schema>> {
+    type Encoder<'a> = Prefixed<<K::Schema as Schema<K>>::Encoder<'a>>;
+    type Decoder<'a> = Prefixed<<K::Schema as Schema<K>>::Decoder<'a>>;
+
+    fn columns(&self) -> Vec<(String, DataType)> {
+        self.0.columns()
+    }
+
+    fn decoder<'a>(&self, cols: ColumnsRef<'a>) -> Result<Self::Decoder<'a>, String> {
+        self.0.decoder(cols).map(Prefixed)
+    }
+
+    fn encoder<'a>(&self, cols: ColumnsMut<'a>) -> Result<Self::Encoder<'a>, String> {
+        self.0.encoder(cols).map(Prefixed)
+    }
+}
+
+/// A timestamp based on the sort order of our data in Persist, intended for use as the
+/// inner timestamp of a Hybrid timestamp type.
+#[derive(PartialOrd, PartialEq, Ord, Eq, Debug, Clone, Serialize, Deserialize, Hash)]
+pub enum Sort {
+    /// An actual key. Meaningless aside from the sort.
+    Data(ArrayVec<u8, 16>),
+    /// A key that compares larger than any other sort key.
+    Max, // Here for the lattice implementation; otherwise unused.
+}
+
+impl PartialOrder for Sort {
+    fn less_equal(&self, other: &Self) -> bool {
+        self <= other
+    }
+}
+
+impl TotalOrder for Sort {}
+
+impl Lattice for Sort {
+    fn join(&self, other: &Self) -> Self {
+        self.max(other).clone()
+    }
+
+    fn meet(&self, other: &Self) -> Self {
+        self.min(other).clone()
+    }
+}
+
+impl TimelyTimestamp for Sort {
+    type Summary = ();
+
+    fn minimum() -> Self {
+        Sort::Data(ArrayVec::new())
+    }
+}
+
+impl Maximum for Sort {
+    fn maximum() -> Self {
+        Sort::Max
+    }
+}
+
+impl PathSummary<Sort> for () {
+    fn results_in(&self, src: &Sort) -> Option<Sort> {
+        Some(src.clone())
+    }
+
+    fn followed_by(&self, (): &Self) -> Option<Self> {
+        Some(())
+    }
 }
