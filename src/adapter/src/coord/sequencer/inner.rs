1b6214aa218d53dabd2979788d8d90d534131d38
Joseph Koshakow <koshy44@gmail.com>
2023-03-16 13:14:28 -0400

    Fix merge skew

--- src/adapter/src/coord/sequencer/inner.rs
+++ src/adapter/src/coord/sequencer/inner.rs
@@ -78,7 +78,7 @@ use crate::coord::peek::{FastPathPlan, PlannedPeek};
 use crate::coord::timeline::TimelineContext;
 use crate::coord::timestamp_selection::{TimestampContext, TimestampSource};
 use crate::coord::{
-    peek, Coordinator, Message, PendingReadTxn, PendingTxn, RealTimeRecencyContext, SendDiffs,
+    peek, Coordinator, Message, PendingReadTxn, PendingTxn, RealTimeRecencyContext,
     SinkConnectionReady, DEFAULT_LOGICAL_COMPACTION_WINDOW_TS,
 };
 use crate::error::AdapterError;
@@ -2733,8 +2733,7 @@ impl Coordinator {
     }
 
     #[tracing::instrument(level = "debug", skip_all)]
-    pub(super) fn sequence_send_diffs(
-        &mut self,
+    fn sequence_send_diffs(
         session: &mut Session,
         mut plan: SendDiffsPlan,
     ) -> Result<ExecuteResponse, AdapterError> {
@@ -2790,10 +2789,7 @@ impl Coordinator {
                 offset: 0,
                 project: (0..plan.returning[0].0.iter().count()).collect(),
             };
-            return match finishing.finish(
-                plan.returning,
-                self.catalog.system_config().max_result_size(),
-            ) {
+            return match finishing.finish(plan.returning, plan.max_result_size) {
                 Ok(rows) => Ok(send_immediate_rows(rows)),
                 Err(e) => Err(AdapterError::ResultSize(e)),
             };
@@ -2911,8 +2907,9 @@ impl Coordinator {
                     updates: rows,
                     kind: MutationKind::Insert,
                     returning: Vec::new(),
+                    max_result_size: self.catalog.system_config().max_result_size(),
                 };
-                self.sequence_send_diffs(session, diffs_plan)
+                Self::sequence_send_diffs(session, diffs_plan)
             }
             None => panic!(
                 "tried using sequence_insert_constant on non-constant MirRelationExpr {:?}",
@@ -3035,6 +3032,7 @@ impl Coordinator {
 
         let internal_cmd_tx = self.internal_cmd_tx.clone();
         let strict_serializable_reads_tx = self.strict_serializable_reads_tx.clone();
+        let max_result_size = self.catalog.system_config().max_result_size();
         task::spawn(|| format!("sequence_read_then_write:{id}"), async move {
             let (peek_response, mut session) = match peek_rx.await {
                 Ok(Response {
@@ -3220,17 +3218,25 @@ impl Coordinator {
                 }
             }
 
-            // It is not an error for these results to be ready after `internal_cmd_rx` has been dropped.
-            let result = internal_cmd_tx.send(Message::SendDiffs(SendDiffs {
-                session,
-                tx,
-                id,
-                diffs,
-                kind,
-                returning: returning_rows,
-            }));
-            if let Err(e) = result {
-                warn!("internal_cmd_rx dropped before we could send: {:?}", e);
+            match diffs {
+                Ok(diffs) => {
+                    tx.send(
+                        Self::sequence_send_diffs(
+                            &mut session,
+                            SendDiffsPlan {
+                                id,
+                                updates: diffs,
+                                kind,
+                                returning: returning_rows,
+                                max_result_size,
+                            },
+                        ),
+                        session,
+                    );
+                }
+                Err(e) => {
+                    tx.send(Err(e), session);
+                }
             }
         });
     }
