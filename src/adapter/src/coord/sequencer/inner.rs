ba56f5715e32800b106f4103dbff2a53817e3539
Joseph Koshakow <koshy44@gmail.com>
2023-03-17 12:07:52 -0400

    adapter: Fix bugs with role membership sys table (#18217)
    This commit fixes the following two bugs related to role membership:
    
      1. When a role is dropped, it's membership in other roles is not
    removed from the system table mz_role_members.
    
      2. When Materialize is reset, mz_role_members is not re-populated.
    
    Part of #11579
    Fixes #18203
    Fixes #18199

--- src/adapter/src/coord/sequencer/inner.rs
+++ src/adapter/src/coord/sequencer/inner.rs
@@ -1461,6 +1461,16 @@ impl Coordinator {
                 })
             }
         }
+        // We must also revoke all role memberships that the dropped roles belongs to.
+        for dropped_id in dropped_ids {
+            let role = self.catalog.get_role(dropped_id);
+            for group_id in role.membership.map.keys() {
+                ops.push(catalog::Op::RevokeRole {
+                    role_id: *group_id,
+                    member_id: *dropped_id,
+                });
+            }
+        }
 
         ops.extend(
             plan.ids
