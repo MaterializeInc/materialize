27b8dad7261aa83d8cb38cfa5b85267082b8cb43
Joseph Koshakow <koshy44@gmail.com>
2023-03-20 12:47:02 -0400

    ore: Fix error message for into_element (#18275)

--- src/ore/src/collections.rs
+++ src/ore/src/collections.rs
@@ -43,7 +43,7 @@ where
     ///
     /// This method panics if the collection does not have exactly one element.
     fn into_element(self) -> T::Item {
-        self.expect_element(|| "into_element called on collection with more than one element")
+        self.expect_element(|| "into_element called on collection without exactly one element")
     }
 
     /// Consumes the collection and returns its only element.
