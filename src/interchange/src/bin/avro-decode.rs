9fdda1711eeade475ef51f8c16721349b8d41a40
Nikhil Benesch <nikhil.benesch@gmail.com>
2023-03-28 21:13:50 -0400

    interchange: add an avro-decode helper program
    This commit adds a new avro-decode program, which is useful for
    debugging issues with Materialize's custom Avro decoding logic.  The
    program takes a file containing the raw Avro data and a file containing
    the Avro schema, decodes the Avro data, and prints the decoded row to
    stdout.

--- src/interchange/src/bin/avro-decode.rs
+++ src/interchange/src/bin/avro-decode.rs
@@ -75,13 +75,56 @@
 #![warn(clippy::from_over_into)]
 // END LINT CONFIG
 
-//! Translations for various data serialization formats.
+use std::path::PathBuf;
+use std::process;
 
-#![warn(missing_debug_implementations)]
+use anyhow::Context;
+use tokio::fs;
 
-pub mod avro;
-pub mod confluent;
-pub mod encode;
-pub mod envelopes;
-pub mod json;
-pub mod protobuf;
+use mz_interchange::avro::Decoder;
+use mz_interchange::confluent;
+use mz_ore::cli;
+use mz_ore::cli::CliConfig;
+
+/// Decode a single Avro row using Materialize's Avro decoder.
+#[derive(clap::Parser)]
+struct Args {
+    /// The path to a file containing the raw bytes of a single Avro datum.
+    data_file: PathBuf,
+    /// The path to a file containing the Avro schema.
+    schema_file: PathBuf,
+    /// Whether the data file uses the Confluent wire format.
+    #[clap(long)]
+    confluent_wire_format: bool,
+}
+
+#[tokio::main]
+async fn main() {
+    let args: Args = cli::parse_args(CliConfig::default());
+    if let Err(e) = run(args).await {
+        println!("{e:#}");
+        process::exit(1);
+    }
+}
+
+async fn run(args: Args) -> Result<(), anyhow::Error> {
+    let mut data = &*fs::read(&args.data_file)
+        .await
+        .context("reading data file")?;
+    if args.confluent_wire_format {
+        let (schema_id, adjusted_data) = confluent::extract_avro_header(data)?;
+        data = adjusted_data;
+        println!("schema id: {schema_id}");
+    }
+    let schema = fs::read_to_string(&args.schema_file)
+        .await
+        .context("reading schema file")?;
+    let ccsr_client: Option<Box<mz_ccsr::Client>> = None;
+    let debug_name = String::from("avro-decode");
+    let confluent_wire_format = false;
+    let mut decoder = Decoder::new(&schema, ccsr_client, debug_name, confluent_wire_format)
+        .context("creating decoder")?;
+    let row = decoder.decode(&mut data).await.context("decoding data")?;
+    println!("row: {row:?}");
+    Ok(())
+}
