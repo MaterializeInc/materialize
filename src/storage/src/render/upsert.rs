4569d5ee7fa8bacfeed4762cd633e4c2b3db0ce3
Petros Angelatos <petrosagg@gmail.com>
2023-03-30 15:09:33 +0200

    upsert: dedup correctly, operate on (hash(key), value) pairs
    This PR fixes a logic but in the upsert operator where it was previously
    sorting updates based on `(ts, order)` and then deduplicating based on
    `(ts, key)`. Of course this breaks completely if the topic contains
    updates like:
    
    ```
    (ts1, key1, order1)
    (ts1, key2, order2)
    (ts1, key1, order3)
    (ts1, key2, order4)
    ...
    ```
    
    because we never dedup anything.
    
    This PR fixes that by sorting based on `(ts, key)`. During this work I
    also took the opportunity to build the upsert based on `(hash(key),
    value)` pairs instead of relying on trickly implementations of `Hash`
    traits and ab-using a `HashSet`. This new implementation is going to be
    compatible with the upcoming work of storing the hashmap out of core
    where we can't rely on rust trait tricks.
    
    Fixes #18466
    
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>

--- src/storage/src/render/upsert.rs
+++ src/storage/src/render/upsert.rs
@@ -8,117 +8,105 @@
 // by the Apache License, Version 2.0.
 
 use std::any::Any;
-use std::borrow::Borrow;
+use std::cell::RefCell;
+use std::cmp::Reverse;
 use std::hash::{Hash, Hasher};
 use std::rc::Rc;
 
 use differential_dataflow::consolidation;
 use differential_dataflow::hashable::Hashable;
 use differential_dataflow::{AsCollection, Collection};
-use itertools::Either;
 use itertools::Itertools;
 use serde::{Deserialize, Serialize};
+use sha2::{Digest, Sha256};
 use timely::dataflow::channels::pact::Exchange;
 use timely::dataflow::Scope;
 use timely::order::{PartialOrder, TotalOrder};
 use timely::progress::{Antichain, Timestamp};
 
-use mz_ore::collections::{CollectionExt, HashSet};
-use mz_repr::{Datum, Diff, Row};
+use mz_ore::collections::{CollectionExt, HashMap};
+use mz_repr::{Datum, DatumVec, Diff, Row};
 use mz_storage_client::types::errors::{DataflowError, EnvelopeError, UpsertError};
 use mz_timely_util::builder_async::{Event as AsyncEvent, OperatorBuilder as AsyncOperatorBuilder};
 
-#[derive(Debug, Clone, PartialOrd, Ord, PartialEq, Eq, Serialize, Deserialize)]
-pub(crate) enum UpsertCommand {
-    Insert(Result<Row, UpsertError>),
-    Remove(Result<Row, UpsertError>),
+#[derive(Clone, Debug, Hash, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize)]
+pub struct UpsertKey([u8; 32]);
+
+thread_local! {
+    /// A thread-local datum cache used to calculate hashes
+    pub static KEY_DATUMS: RefCell<DatumVec> = RefCell::new(DatumVec::new());
 }
 
-impl UpsertCommand {
-    fn keyed<'a>(&'a self, key_indices: &'a [usize]) -> KeyedCommand<'a, &Self> {
-        KeyedCommand {
-            command: self,
-            key_indices,
-        }
+/// The hash function used to map upsert keys. It is important that this hash is a cryptographic
+/// hash so that there is no risk of collisions. Collisions on SHA256 have a probability of 2^128
+/// which is many orders of magnitude smaller than many other events that we don't even think about
+/// (e.g bit flips). In short, we can safely assume that sha256(a) == sha256(b) iff a == b.
+type KeyHash = Sha256;
+
+impl UpsertKey {
+    pub fn from_key(key: Result<&Row, &UpsertError>) -> Self {
+        Self::from_iter(key.map(|r| r.iter()))
     }
 
-    fn into_keyed(self, key_indices: &[usize]) -> KeyedCommand<'_, Self> {
-        KeyedCommand {
-            command: self,
-            key_indices,
-        }
-    }
-}
-
-/// An UpsertCommand wrapper whose implementations of Hash,PartialEq, and Eq are such that any
-/// variant with the same key is considered the same.
-struct KeyedCommand<'a, C: Borrow<UpsertCommand>> {
-    command: C,
-    key_indices: &'a [usize],
-}
-
-impl<'a, C: Borrow<UpsertCommand>> KeyedCommand<'a, C> {
-    /// Produces the key of this upsert command. If the command has a well-formed key then an
-    /// `Ok(_)` variant is returned with an iterator over the datum keys. If the command is about a
-    /// key error then an `Err(_)` variant is returned.
-    fn key(&self) -> Result<impl Iterator<Item = Datum<'_>> + '_, Datum<'_>> {
-        match self.command.borrow() {
-            UpsertCommand::Insert(Ok(row)) => {
-                let mut key_indices = &*self.key_indices;
-                let iter = row.iter().enumerate().flat_map(move |(idx, datum)| {
-                    let key_idx = key_indices.get(0)?;
-                    if idx == *key_idx {
-                        key_indices = &key_indices[1..];
-                        Some(datum)
-                    } else {
-                        None
-                    }
-                });
-                Ok(Either::Left(Either::Left(iter)))
-            }
-            UpsertCommand::Remove(Ok(key)) => Ok(Either::Left(Either::Right(key.iter()))),
-            UpsertCommand::Insert(Err(err)) | UpsertCommand::Remove(Err(err)) => match err {
-                UpsertError::Value(err) => Ok(Either::Right(err.for_key.iter())),
-                UpsertError::KeyDecode(err) => Err(Datum::Bytes(&err.raw)),
-                UpsertError::NullKey(_) => Err(Datum::Null),
-            },
-        }
-    }
-}
-
-impl<C: Borrow<UpsertCommand>> Hash for KeyedCommand<'_, C> {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        match self.key() {
-            Ok(iter) => {
-                for datum in iter {
-                    datum.hash(state);
+    pub fn from_value(value: Result<&Row, &UpsertError>, mut key_indices: &[usize]) -> Self {
+        Self::from_iter(value.map(|value| {
+            value.iter().enumerate().flat_map(move |(idx, datum)| {
+                let key_idx = key_indices.get(0)?;
+                if idx == *key_idx {
+                    key_indices = &key_indices[1..];
+                    Some(datum)
+                } else {
+                    None
                 }
-                state.write_u8(0x01);
-            }
-            Err(datum) => {
-                datum.hash(state);
-                state.write_u8(0x02);
-            }
-        }
+            })
+        }))
+    }
+
+    pub fn from_iter<'a, 'b>(
+        key: Result<impl Iterator<Item = Datum<'a>> + 'b, &UpsertError>,
+    ) -> Self {
+        KEY_DATUMS.with(|key_datums| {
+            let mut key_datums = key_datums.borrow_mut();
+            // Borrowing the DatumVec gives us a temporary buffer to store datums in that will be
+            // automatically cleared on Drop. See the DatumVec docs for more details.
+            let mut key_datums = key_datums.borrow();
+            let key: Result<&[Datum], Datum> = match key {
+                Ok(key) => {
+                    for datum in key {
+                        key_datums.push(datum);
+                    }
+                    Ok(&*key_datums)
+                }
+                Err(UpsertError::Value(err)) => {
+                    key_datums.extend(err.for_key.iter());
+                    Ok(&*key_datums)
+                }
+                Err(UpsertError::KeyDecode(err)) => Err(Datum::Bytes(&err.raw)),
+                Err(UpsertError::NullKey(_)) => Err(Datum::Null),
+            };
+            let mut hasher = DigestHasher(KeyHash::new());
+            key.hash(&mut hasher);
+            Self(hasher.0.finalize().into())
+        })
     }
 }
 
-impl<C: Borrow<UpsertCommand>> PartialEq for KeyedCommand<'_, C> {
-    fn eq(&self, other: &Self) -> bool {
-        match (self.key(), other.key()) {
-            (Ok(this), Ok(other)) => this.eq(other),
-            (Err(this), Err(other)) => this.eq(&other),
-            _ => false,
-        }
+struct DigestHasher<H: Digest>(H);
+
+impl<H: Digest> Hasher for DigestHasher<H> {
+    fn write(&mut self, bytes: &[u8]) {
+        self.0.update(bytes);
+    }
+
+    fn finish(&self) -> u64 {
+        panic!("digest wrapper used to produce a hash");
     }
 }
-
-impl<C: Borrow<UpsertCommand>> Eq for KeyedCommand<'_, C> {}
 
 /// Resumes an upsert computation at `resume_upper` given as inputs a collection of upsert commands
 /// and the collection of the previous output of this operator.
 pub(crate) fn upsert<G: Scope, O: timely::ExchangeData + Ord>(
-    input: &Collection<G, (UpsertCommand, O), Diff>,
+    input: &Collection<G, (UpsertKey, Option<Result<Row, UpsertError>>, O), Diff>,
     mut key_indices: Vec<usize>,
     resume_upper: Antichain<G::Timestamp>,
     previous: Collection<G, Result<Row, DataflowError>, Diff>,
@@ -132,30 +120,27 @@ where
 
     let mut builder = AsyncOperatorBuilder::new("Upsert".to_string(), input.scope());
 
-    let mut input = {
-        let key_indices = key_indices.clone();
-        builder.new_input(
-            &input.inner,
-            Exchange::new(move |((cmd, _), _, _)| UpsertCommand::keyed(cmd, &key_indices).hashed()),
-        )
-    };
+    let mut input = builder.new_input(
+        &input.inner,
+        Exchange::new(move |((key, _, _), _, _)| UpsertKey::hashed(key)),
+    );
 
     // We only care about UpsertValueError since this is the only error that we can retract
-    let previous = previous.flat_map(|result| match result {
-        Ok(ok) => Some(UpsertCommand::Insert(Ok(ok))),
-        Err(DataflowError::EnvelopeError(err)) => match *err {
-            EnvelopeError::Upsert(err) => Some(UpsertCommand::Insert(Err(err))),
-            _ => None,
-        },
-        Err(_) => None,
+    let previous = previous.flat_map(move |result| {
+        let value = match result {
+            Ok(ok) => Ok(ok),
+            Err(DataflowError::EnvelopeError(err)) => match *err {
+                EnvelopeError::Upsert(err) => Err(err),
+                _ => return None,
+            },
+            Err(_) => return None,
+        };
+        Some((UpsertKey::from_value(value.as_ref(), &key_indices), value))
     });
-    let mut previous = {
-        let key_indices = key_indices.clone();
-        builder.new_input(
-            &previous.inner,
-            Exchange::new(move |(cmd, _, _)| UpsertCommand::keyed(cmd, &key_indices).hashed()),
-        )
-    };
+    let mut previous = builder.new_input(
+        &previous.inner,
+        Exchange::new(|((key, _), _, _)| UpsertKey::hashed(key)),
+    );
     let (mut output_handle, output) = builder.new_output();
 
     builder.build(move |caps| async move {
@@ -189,15 +174,15 @@ where
 
         consolidation::consolidate(&mut snapshot);
 
-        let mut state = HashSet::new();
+        let mut state = HashMap::new();
 
-        for (cmd, diff) in snapshot {
+        for ((key, value), diff) in snapshot {
             assert_eq!(diff, 1, "invalid upsert state");
-            state.insert(cmd.into_keyed(&key_indices));
+            state.insert(key, value);
         }
 
         // Now can can resume consuming the collection
-        let mut pending_batches = vec![];
+        let mut stash = vec![];
         let mut output_updates = vec![];
         let mut input_upper = Antichain::from_elem(Timestamp::minimum());
         while let Some(event) = input.next_mut().await {
@@ -207,64 +192,40 @@ where
                         data.retain(|(_, ts, _)| resume_upper.less_equal(ts));
                     }
 
-                    // This could be done with a BTree but since we don't need indexed access merge
-                    // sorting sorted batches is simpler.
-                    data.sort_unstable_by(|((_, a_order), a_ts, _), ((_, b_order), b_ts, _)| {
-                        a_ts.cmp(b_ts).then_with(|| a_order.cmp(b_order))
-                    });
-
-                    pending_batches.push(std::mem::take(data).into_iter().peekable());
+                    stash.extend(data.drain(..).map(|((key, value, order), time, diff)| {
+                        assert!(diff > 0, "invalid upsert input");
+                        (time, key, Reverse(order), value)
+                    }));
                 }
                 AsyncEvent::Progress(upper) => {
-                    // From all the pending batches take the prefix that is not beyond upper and
-                    // produce the overall sorted iterator by merge sorting them.
-                    let mut commands = pending_batches
-                        .iter_mut()
-                        .map(|batch| batch.peeking_take_while(|(_, ts, _)| !upper.less_equal(ts)))
-                        .kmerge_by(|((_, a_order), a_ts, _), ((_, b_order), b_ts, _)| {
-                            a_ts.cmp(b_ts).then_with(|| a_order.cmp(b_order)).is_lt()
-                        })
-                        .peekable();
+                    stash.sort_unstable();
 
-                    while let Some(((cmd, _order), ts, diff)) = commands.next() {
-                        assert!(diff > 0, "invalid upsert input");
+                    // Find the prefix that we can emit
+                    let idx = stash.partition_point(|(ts, _, _, _)| !upper.less_equal(ts));
 
-                        if let Some(((p, _), p_ts, _)) = commands.peek() {
-                            // Skip this command if the next one is for the same (time, key) pair.
-                            // This skips to the command with the latest offset, as sorted above.
-                            if p_ts == &ts && p.keyed(&key_indices) == cmd.keyed(&key_indices) {
-                                continue;
-                            }
-                        }
+                    // From the prefix that can be emitted we can deduplicate based on (ts, key) in
+                    // order to only process the command with the maximum order within the (ts,
+                    // key) group. This is achieved by wrapping order in `Reverse(order)` above.
+                    let mut commands = stash.drain(..idx).dedup_by(|a, b| {
+                        let ((a_ts, a_key, _, _), (b_ts, b_key, _, _)) = (a, b);
+                        a_ts == b_ts && a_key == b_key
+                    });
 
-                        match cmd {
-                            UpsertCommand::Insert(result) => {
-                                let new_cmd =
-                                    UpsertCommand::Insert(result.clone()).into_keyed(&key_indices);
-                                if let Some(old_cmd) = state.replace(new_cmd) {
-                                    let UpsertCommand::Insert(old_result) = old_cmd.command else {
-                                        // We only store insertions in the state set
-                                        unreachable!()
-                                    };
-                                    output_updates.push((old_result, ts.clone(), -1));
+                    while let Some((ts, key, _, value)) = commands.next() {
+                        match value {
+                            Some(value) => {
+                                if let Some(old_value) = state.insert(key, value.clone()) {
+                                    output_updates.push((old_value, ts.clone(), -1));
                                 }
-                                output_updates.push((result, ts, 1));
+                                output_updates.push((value, ts, 1));
                             }
-                            UpsertCommand::Remove(result) => {
-                                let cmd = UpsertCommand::Remove(result).into_keyed(&key_indices);
-                                if let Some(old_cmd) = state.take(&cmd) {
-                                    let UpsertCommand::Insert(old_result) = old_cmd.command else {
-                                        // We only store insertions in the state set
-                                        unreachable!()
-                                    };
-                                    output_updates.push((old_result, ts, -1));
+                            None => {
+                                if let Some(old_value) = state.remove(&key) {
+                                    output_updates.push((old_value, ts, -1));
                                 }
                             }
                         }
                     }
-
-                    // Retain batches that still have data
-                    pending_batches.retain_mut(|batch| batch.peek().is_some());
 
                     output_handle
                         .give_container(&output_cap, &mut output_updates)
