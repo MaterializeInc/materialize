8dd6e203588a5aaf95b4784cab32fcecae2de5f6
Jan Teske <jteske@posteo.net>
2023-04-14 14:38:27 +0200

    compute: introduce `ErrorLogger` for rendering
    This commit introduces a new `ErrorLogger` type that will be used for
    logging errors inside dataflows. This type wraps a dataflow shutdown
    token and encapsules both shutdown-checked logging and our
    `warn!`/`error!` approach to logging dataflow errors.

--- src/compute/src/render/errors.rs
+++ src/compute/src/render/errors.rs
@@ -70,3 +70,63 @@ where
         Some(Err)
     }
 }
+
+/// Error logger to be used by rendering code.
+///
+/// Holds onto a token to ensure that no false-positive errors are logged while the dataflow is in
+/// the process of shutting down.
+#[derive(Clone)]
+pub(super) struct ErrorLogger {
+    token: Option<Weak<()>>,
+}
+
+impl ErrorLogger {
+    pub fn new(token: Option<Weak<()>>) -> Self {
+        Self { token }
+    }
+
+    fn token_alive(&self) -> bool {
+        match &self.token {
+            Some(t) => t.upgrade().is_some(),
+            None => true,
+        }
+    }
+
+    /// Log the given error, unless the dataflow is shutting down.
+    ///
+    /// The logging format is optimized for surfacing errors with Sentry:
+    ///  * `error` is logged at ERROR level and will appear as the error title in Sentry.
+    ///    We require it to be a static string, to ensure that Sentry always merges instances of
+    ///    the same error together.
+    ///  * `details` is logged at WARN level and will appear in the breadcrumbs.
+    ///    Put relevant dynamic information here.
+    ///
+    /// The message that's logged at WARN level has the format
+    ///   "[customer-data] {message} ({details})"
+    /// We include the [customer-data] tag out of the expectation that `details` will always
+    /// contain some sensitive customer data. We include the `message` to make it possible to match
+    /// the breadcrumbs to their associated error in Sentry.
+    ///
+    // TODO(#18214): Rethink or justify our error logging strategy.
+    pub fn log(&self, message: &'static str, details: &str) {
+        if self.token_alive() {
+            self.log_always(message, details);
+        }
+    }
+
+    /// Like [`Self::log`], but also logs errors when the dataflow is shutting down.
+    ///
+    /// Use this method to notify about errors that cannot be caused by dataflow shutdown.
+    pub fn log_always(&self, message: &'static str, details: &str) {
+        tracing::warn!("[customer-data] {message} ({details})");
+        tracing::error!(message);
+    }
+
+    /// Like [`Self::log_always`], but panics in debug mode.
+    ///
+    /// Use this method to notify about errors that are certainly caused by bugs in Materialize.
+    pub fn soft_panic_or_log(&self, message: &'static str, details: &str) {
+        tracing::warn!("[customer-data] {message} ({details})");
+        mz_ore::soft_panic_or_log!("{}", message);
+    }
+}
