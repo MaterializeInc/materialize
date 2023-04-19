fc9267e86167ec89d7d3e9e38a874d825484ea3d
Joseph Koshakow <koshy44@gmail.com>
2023-04-19 18:18:30 -0400

    Update src/adapter/src/catalog/builtin.rs
    Co-authored-by: Nikhil Benesch <nikhil.benesch@gmail.com>

--- src/adapter/src/catalog/builtin.rs
+++ src/adapter/src/catalog/builtin.rs
@@ -1742,7 +1742,7 @@ pub static MZ_CLUSTER_REPLICA_SIZES: Lazy<BuiltinTable> = Lazy::new(|| BuiltinTa
         .with_column("memory_bytes", ScalarType::UInt64.nullable(false))
         .with_column("disk_bytes", ScalarType::UInt64.nullable(true))
         .with_column(
-            "compute_credits_per_hour",
+            "credits_per_hour",
             ScalarType::Numeric { max_scale: None }.nullable(false),
         ),
     is_retained_metrics_object: true,
