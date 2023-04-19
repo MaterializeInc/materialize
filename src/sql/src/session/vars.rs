907360e5676b2ac9e7dae3cbba6622936c0f16ce
Joseph Koshakow <koshy44@gmail.com>
2023-04-19 18:20:44 -0400

    Update src/sql/src/session/vars.rs
    Co-authored-by: Nikhil Benesch <nikhil.benesch@gmail.com>

--- src/sql/src/session/vars.rs
+++ src/sql/src/session/vars.rs
@@ -377,7 +377,7 @@ pub const MAX_REPLICAS_PER_CLUSTER: ServerVar<u32> = ServerVar {
 
 static DEFAULT_MAX_COMPUTE_CREDITS_PER_HOUR: Lazy<Numeric> = Lazy::new(|| 64.into());
 pub static MAX_COMPUTE_CREDITS_PER_HOUR: Lazy<ServerVar<Numeric>> = Lazy::new(|| ServerVar {
-    name: UncasedStr::new("max_compute_credits_per_hour"),
+    name: UncasedStr::new("max_credits_per_hour"),
     value: &DEFAULT_MAX_COMPUTE_CREDITS_PER_HOUR,
     description: "The maximum number of compute credits per hour in the region (Materialize).",
     internal: false,
