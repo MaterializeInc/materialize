79ccc881dc395a8a4eb68321d415f2a995ee205f
nrainer-materialize <129728240+nrainer-materialize@users.noreply.github.com>
2023-04-24 15:53:33 +0200

    ci: add multiple_partitions platform check (#18875)
    This fixes https://github.com/MaterializeInc/materialize/issues/18297.

--- misc/python/materialize/checks/all_checks.py
+++ misc/python/materialize/checks/all_checks.py
@@ -34,6 +34,7 @@ from materialize.checks.kafka_formats import *  # noqa: F401 F403
 from materialize.checks.large_tables import *  # noqa: F401 F403
 from materialize.checks.like import *  # noqa: F401 F403
 from materialize.checks.materialized_views import *  # noqa: F401 F403
+from materialize.checks.multiple_partitions import *  # noqa: F401 F403
 from materialize.checks.nested_types import *  # noqa: F401 F403
 from materialize.checks.null_value import *  # noqa: F401 F403
 from materialize.checks.numeric_types import *  # noqa: F401 F403
