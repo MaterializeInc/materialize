7f8a4adce0b075bb23f6967d4bee9514b5c147fa
Rainer Niedermayr <rainer@materialize.com>
2023-04-24 13:36:41 +0200

    Use default schema

--- misc/python/materialize/checks/multiple_partitions.py
+++ misc/python/materialize/checks/multiple_partitions.py
@@ -11,28 +11,11 @@ from typing import List
 
 from materialize.checks.actions import Testdrive
 from materialize.checks.checks import Check
+from materialize.checks.common import KAFKA_SCHEMA_WITH_SINGLE_STRING_FIELD
 
 
 def schemas() -> str:
-    return dedent(
-        """
-       $ set keyschema={
-           "type": "record",
-           "name": "Key",
-           "fields": [
-               {"name": "key1", "type": "string"}
-           ]
-         }
-
-       $ set schema={
-           "type" : "record",
-           "name" : "test",
-           "fields" : [
-               {"name":"f1", "type":"string"}
-           ]
-         }
-    """
-    )
+    return dedent(KAFKA_SCHEMA_WITH_SINGLE_STRING_FIELD)
 
 
 class MultiplePartitions(Check):
