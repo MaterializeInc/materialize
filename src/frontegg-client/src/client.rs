751887613a846eb65e520115724aec29cf670e4b
Joaquin Colacci <joaquincolacci@gmail.com>
2023-05-04 14:23:17 -0400

    frontegg-client: turn pub 'fn auth()'

--- src/frontegg-client/src/client.rs
+++ src/frontegg-client/src/client.rs
@@ -163,7 +163,7 @@ impl Client {
 
     /// Authenticates with the server, if not already authenticated,
     /// and returns the authentication token.
-    async fn auth(&self) -> Result<String, Error> {
+    async pub fn auth(&self) -> Result<String, Error> {
         let mut auth = self.auth.lock().await;
         let mut req;
 
