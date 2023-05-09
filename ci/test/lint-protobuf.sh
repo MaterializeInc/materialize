d3d230265fcf7cfe34fe5064d367c4461f99563f
Rainer Niedermayr <rainer@materialize.com>
2023-05-09 17:46:33 +0200

    ci: protobuf breaking change linting

---
+++ ci/test/lint-protobuf.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Copyright Materialize, Inc. and contributors. All rights reserved.
+#
+# Use of this software is governed by the Business Source License
+# included in the LICENSE file at the root of this repository.
+#
+# As of the Change Date specified in that file, in accordance with
+# the Business Source License, use of this software will be governed
+# by the Apache License, Version 2.0.
+#
+# lint-protobuf.sh - lint the protobuf compatibility.
+
+set -euo pipefail
+
+. misc/shlib/shlib.bash
+
+BUILDKITE_REPO_REF="origin"
+./ci/test/utils/fetch-merge-target.sh
+
+ci_collapsed_heading "Lint protobuf"
+ci_try buf breaking src --against ".git#branch=$BUILDKITE_PULL_REQUEST_BASE_BRANCH,subdir=src"
+
+ci_status_report
