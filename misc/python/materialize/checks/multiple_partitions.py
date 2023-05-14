3193684bdfcb53b3d0651a04604403da01efcce7
Sean Loiselle <sean@materialize.io>
2023-05-14 13:09:20 -0400

    test: remove unsafe mode test changes

--- misc/python/materialize/checks/multiple_partitions.py
+++ misc/python/materialize/checks/multiple_partitions.py
@@ -26,6 +26,11 @@ class MultiplePartitions(Check):
             schemas()
             + dedent(
                 """
+                $ postgres-execute connection=postgres://mz_system:materialize@${testdrive.materialize-internal-sql-addr}
+                ALTER SYSTEM SET enable_create_source_denylist_with_options = true
+                ALTER SYSTEM SET enable_kafka_config_denylist_options = true
+
+
                 $ kafka-create-topic topic=multiple-partitions-topic
 
                 # ingest A-key entries
