e8a579963b4d5b8281c58880bd4ce0e22e1d35ba
Rainer Niedermayr <rainer@materialize.com>
2023-05-16 15:48:47 +0200

    Add AnyOperationParam

---
+++ misc/python/materialize/output_consistency/input_data/params/any_operation_param.py
@@ -0,0 +1,33 @@
+# Copyright Materialize, Inc. and contributors. All rights reserved.
+#
+# Use of this software is governed by the Business Source License
+# included in the LICENSE file at the root of this repository.
+#
+# As of the Change Date specified in that file, in accordance with
+# the Business Source License, use of this software will be governed
+# by the Apache License, Version 2.0.
+
+from typing import Optional
+
+from materialize.output_consistency.data_type.data_type_category import DataTypeCategory
+from materialize.output_consistency.expression.expression_characteristics import (
+    ExpressionCharacteristics,
+)
+from materialize.output_consistency.operation.operation_param import OperationParam
+
+
+class AnyOperationParam(OperationParam):
+    def __init__(
+        self,
+        optional: bool = False,
+        incompatibilities: Optional[set[ExpressionCharacteristics]] = None,
+        incompatibility_combinations: Optional[
+            list[set[ExpressionCharacteristics]]
+        ] = None,
+    ):
+        super().__init__(
+            DataTypeCategory.ANY,
+            optional,
+            incompatibilities,
+            incompatibility_combinations,
+        )
