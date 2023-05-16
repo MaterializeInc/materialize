ac7723dc423e8e2b4c906409b0e4dcdb659eb8ff
umanwizard <brennan@materialize.com>
2023-05-16 00:35:18 -0400

    Update mz_internal.mz_cluster_replica_history (#19300)
    Fixes two issues with this view.
    
    First, we were surfacing an "old-style" replica ID, which is an
    implementation detail of `mz_audit_events`. Change its name to `internal_replica_id` to clarify this.
    
    Second, we need to include `replica_name` and `cluster_name` in this
    view, since in general users won't be able to get those by joining
    against `mz_clusters` and `mz_cluster_replicas`, in the case where the
    cluster or replica has already been dropped.
    
    See full discussion
    [here](https://materializeinc.slack.com/archives/C02PPB50ZHS/p1684175174531589).
    
    ### Checklist
    
    - [x] This PR has adequate test coverage / QA involvement has been duly
    considered.
    - [ ] This PR has an associated up-to-date [design
    doc](https://github.com/MaterializeInc/materialize/blob/main/doc/developer/design/README.md),
    is a design doc
    ([template](https://github.com/MaterializeInc/materialize/blob/main/doc/developer/design/00000000_template.md)),
    or is sufficiently small to not require a design.
      <!-- Reference the design in the description. -->
    - [ ] This PR evolves [an existing `$T ⇔ Proto$T`
    mapping](https://github.com/MaterializeInc/materialize/blob/main/doc/developer/command-and-response-binary-encoding.md)
    (possibly in a backwards-incompatible way) and therefore is tagged with
    a `T-proto` label.
    - [ ] If this PR will require changes to cloud orchestration, there is a
    companion cloud PR to account for those changes that is tagged with the
    release-blocker label
    ([example](https://github.com/MaterializeInc/cloud/pull/5021)).
    <!-- Ask in #team-cloud on Slack if you need help preparing the cloud
    PR. -->
    - [x] This PR includes the following [user-facing behavior
    changes](https://github.com/MaterializeInc/materialize/blob/main/doc/developer/guide-changes.md#what-changes-require-a-release-note):
    - Improve the relevance of the information in
    `mz_cluster_replica_history`.

--- src/adapter/src/catalog/builtin.rs
+++ src/adapter/src/catalog/builtin.rs
@@ -3242,6 +3242,8 @@ pub const MZ_CLUSTER_REPLICA_HISTORY: BuiltinView = BuiltinView {
                 SELECT
                     details ->> 'logical_size' AS size,
                     details ->> 'replica_id' AS replica_id,
+                    details ->> 'replica_name' AS replica_name,
+                    details ->> 'cluster_name' AS cluster_name,
                     occurred_at
                 FROM mz_catalog.mz_audit_events
                 WHERE
@@ -3258,8 +3260,10 @@ pub const MZ_CLUSTER_REPLICA_HISTORY: BuiltinView = BuiltinView {
                 WHERE object_type = 'cluster-replica' AND event_type = 'drop'
             )
         SELECT
-            creates.replica_id,
+            creates.replica_id AS internal_replica_id,
             creates.size,
+            creates.cluster_name,
+            creates.replica_name,
             creates.occurred_at AS created_at,
             drops.occurred_at AS dropped_at,
             mz_internal.mz_error_if_null(
