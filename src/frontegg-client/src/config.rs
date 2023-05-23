d61cb1edb1cd2a4cc6f52f017e7dde36c7f6fa0c
Joaquin Colacci <joaquincolacci@gmail.com>
2023-05-23 11:29:53 +0200

    frontegg-client: enables authentication using email and password (#19421)
    The motivation for this PR is to enable authentication using email and
    password. It is a feature needed by `mz` to login a user without using
    the web browser.
    
    Currently the `frontegg-client` supports to authenticate using
    AppPasswords, so an enum has been added: `Authentication` that could be an
    `AppPassword` or `Credentials`, and support for each type of request.

--- src/frontegg-client/src/config.rs
+++ src/frontegg-client/src/config.rs
@@ -22,11 +22,10 @@
 
 use std::time::Duration;
 
-use mz_frontegg_auth::AppPassword;
 use once_cell::sync::Lazy;
 use reqwest::Url;
 
-use crate::client::Client;
+use crate::client::{Authentication, Client};
 
 /// The default endpoint the client will use to issue the requests. Currently
 /// points to Materialize admin endpoint.
@@ -40,7 +39,7 @@ pub static DEFAULT_ENDPOINT: Lazy<Url> = Lazy::new(|| {
 pub struct ClientConfig {
     /// A singular, legitimate app password that will remain in use to identify
     /// the user throughout the client's existence.
-    pub app_password: AppPassword,
+    pub authentication: Authentication,
 }
 
 /// A builder for a [`Client`].
@@ -75,7 +74,7 @@ impl ClientBuilder {
             .unwrap();
         Client {
             inner,
-            app_password: config.app_password,
+            authentication: config.authentication,
             endpoint: self.endpoint,
             auth: Default::default(),
         }
