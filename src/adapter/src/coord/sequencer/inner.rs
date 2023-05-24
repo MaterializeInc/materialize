92c874657969bfbead648ce661b9f2371f218ef8
Sean Loiselle <sean@materialize.io>
2023-05-24 19:02:56 -0400

    adapter: make creating sources modular

--- src/adapter/src/coord/sequencer/inner.rs
+++ src/adapter/src/coord/sequencer/inner.rs
@@ -44,8 +44,8 @@ use mz_sql::catalog::{
     ErrorMessageObjectDescription, ObjectType, SessionCatalog,
 };
 use mz_sql::names::{
-    ObjectId, ResolvedDatabaseSpecifier, ResolvedIds, ResolvedItemName, SchemaSpecifier,
-    SystemObjectId,
+    ObjectId, QualifiedItemName, ResolvedDatabaseSpecifier, ResolvedIds, ResolvedItemName,
+    SchemaSpecifier, SystemObjectId,
 };
 use mz_sql::plan::{
     AlterDefaultPrivilegesPlan, AlterIndexResetOptionsPlan, AlterIndexSetOptionsPlan,
@@ -53,7 +53,7 @@ use mz_sql::plan::{
     AlterSinkPlan, AlterSourceAction, AlterSourcePlan, AlterSystemResetAllPlan,
     AlterSystemResetPlan, AlterSystemSetPlan, CreateConnectionPlan, CreateDatabasePlan,
     CreateIndexPlan, CreateMaterializedViewPlan, CreateRolePlan, CreateSchemaPlan,
-    CreateSecretPlan, CreateSinkPlan, CreateSourcePlan, CreateTablePlan, CreateTypePlan,
+    CreateSecretPlan, CreateSinkPlan, CreateSourcePlans, CreateTablePlan, CreateTypePlan,
     CreateViewPlan, CreateWebhookSourcePlan, DropObjectsPlan, DropOwnedPlan, ExecutePlan,
     ExplainPlan, GrantPrivilegesPlan, GrantRolePlan, IndexOption, InsertPlan, InspectShardPlan,
     MaterializedView, MutationKind, OptimizerConfig, Params, Plan, QueryWhen, ReadThenWritePlan,
@@ -130,28 +130,45 @@ struct DropOps {
     dropped_active_cluster: bool,
 }
 
+// A bundle of values returned from create_source_inner
+struct CreateSourceInner {
+    ops: Vec<Op>,
+    sources: Vec<(GlobalId, catalog::Source)>,
+    if_not_exists_ids: BTreeMap<GlobalId, QualifiedItemName>,
+}
+
 impl Coordinator {
-    #[tracing::instrument(level = "debug", skip(self))]
-    pub(super) async fn sequence_create_source(
+    async fn create_source_inner(
         &mut self,
         session: &mut Session,
-        plans: Vec<(GlobalId, CreateSourcePlan, ResolvedIds)>,
-    ) -> Result<ExecuteResponse, AdapterError> {
+        plans: Vec<CreateSourcePlans>,
+    ) -> Result<CreateSourceInner, AdapterError> {
         let mut ops = vec![];
         let mut sources = vec![];
 
         let if_not_exists_ids = plans
             .iter()
-            .filter_map(|(id, plan, _)| {
-                if plan.if_not_exists {
-                    Some((*id, plan.name.clone()))
-                } else {
-                    None
-                }
-            })
+            .filter_map(
+                |CreateSourcePlans {
+                     source_id,
+                     plan,
+                     resolved_ids: _,
+                 }| {
+                    if plan.if_not_exists {
+                        Some((*source_id, plan.name.clone()))
+                    } else {
+                        None
+                    }
+                },
+            )
             .collect::<BTreeMap<_, _>>();
 
-        for (source_id, plan, depends_on) in plans {
+        for CreateSourcePlans {
+            source_id,
+            plan,
+            resolved_ids,
+        } in plans
+        {
             let name = plan.name.clone();
             let source_oid = self.catalog_mut().allocate_oid()?;
             let cluster_id = match plan.source.data_source {
@@ -167,7 +184,8 @@ impl Coordinator {
                 ),
                 _ => None,
             };
-            let source = catalog::Source::new(source_id, plan, cluster_id, depends_on, None, false);
+            let source =
+                catalog::Source::new(source_id, plan, cluster_id, resolved_ids, None, false);
             ops.push(catalog::Op::CreateItem {
                 id: source_id,
                 oid: source_oid,
@@ -177,6 +195,26 @@ impl Coordinator {
             });
             sources.push((source_id, source));
         }
+
+        Ok(CreateSourceInner {
+            ops,
+            sources,
+            if_not_exists_ids,
+        })
+    }
+
+    #[tracing::instrument(level = "debug", skip(self))]
+    pub(super) async fn sequence_create_source(
+        &mut self,
+        session: &mut Session,
+        plans: Vec<CreateSourcePlans>,
+    ) -> Result<ExecuteResponse, AdapterError> {
+        let CreateSourceInner {
+            ops,
+            sources,
+            if_not_exists_ids,
+        } = self.create_source_inner(session, plans).await?;
+
         match self.catalog_transact(Some(session), ops).await {
             Ok(()) => {
                 let mut source_ids = Vec::with_capacity(sources.len());
