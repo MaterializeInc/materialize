4d6f7e1755d5f5022945f9df5069a57d572f6297
Parker Timmerman <parker.timmerman@materialize.com>
2023-05-25 11:51:25 -0400

    add previously failing test

--- src/adapter/src/coord/command_handler.rs
+++ src/adapter/src/coord/command_handler.rs
@@ -11,6 +11,7 @@
 //! client via some external Materialize API (ex: HTTP and psql).
 
 use std::sync::Arc;
+use std::time::Duration;
 
 use mz_compute_client::protocol::response::PeekResponse;
 use mz_ore::task;
@@ -561,6 +562,18 @@ impl Coordinator {
     ) {
         let catalog = self.owned_catalog();
         mz_ore::task::spawn(|| "coord::handle_prepare", async move {
+            // Note: This failpoint is used to simulate a request outliving the external connection
+            // that made it.
+            let mut async_pause = false;
+            (|| {
+                fail::fail_point!("async_prepare", |val| {
+                    async_pause = val.map_or(false, |val| val.parse().unwrap_or(false))
+                });
+            })();
+            if async_pause {
+                tokio::time::sleep(Duration::from_secs(1)).await;
+            };
+
             let res = match Self::describe(&catalog, &session, stmt.clone(), param_types) {
                 Ok(desc) => {
                     session.set_prepared_statement(
