d9e93d80236dc7896a6a79187f60df6ef2d533f6
Rainer Niedermayr <rainer@materialize.com>
2023-05-26 10:32:48 +0200

    Add known deviations

--- misc/python/materialize/output_consistency/known_inconsistencies/known_deviation_filter.py
+++ misc/python/materialize/output_consistency/known_inconsistencies/known_deviation_filter.py
@@ -7,10 +7,15 @@
 # the Business Source License, use of this software will be governed
 # by the Apache License, Version 2.0.
 
-from materialize.output_consistency.execution.value_storage_layout import (
-    ValueStorageLayout,
-)
 from materialize.output_consistency.expression.expression import Expression
+from materialize.output_consistency.expression.expression_characteristics import (
+    ExpressionCharacteristics,
+)
+from materialize.output_consistency.expression.expression_with_args import (
+    ExpressionWithArgs,
+)
+from materialize.output_consistency.expression.leaf_expression import LeafExpression
+from materialize.output_consistency.operation.operation import DbFunction
 from materialize.output_consistency.selection.selection import DataRowSelection
 
 
@@ -18,8 +23,49 @@ class KnownOutputInconsistenciesFilter:
     """Allows specifying and excluding expressions with known output inconsistencies"""
 
     def matches(self, expression: Expression, row_selection: DataRowSelection) -> bool:
-        if expression.storage_layout == ValueStorageLayout.HORIZONTAL:
-            # Optimization because currently no issues are known
+        if isinstance(expression, LeafExpression):
             return False
+        elif isinstance(expression, ExpressionWithArgs):
+            if not expression.operation.is_aggregation:
+                # Optimization: currently no issues without aggregation are known
+                return False
+
+            return self._matches_expression_with_args(expression, row_selection)
+        else:
+            raise RuntimeError(f"Unsupported expression type: {type(expression)}")
+
+    def _matches_expression_with_args(
+        self,
+        expression: ExpressionWithArgs,
+        row_selection: DataRowSelection,
+    ) -> bool:
+        # check if any argument is already problematic (relevant for nested expressions)
+        for arg in expression.args:
+            if isinstance(arg, ExpressionWithArgs):
+                if self.matches(arg, row_selection):
+                    return True
+
+        if isinstance(expression.operation, DbFunction):
+            return self._matches_db_function(
+                expression, expression.operation, row_selection
+            )
+
+        return False
+
+    def _matches_db_function(
+        self,
+        expression: ExpressionWithArgs,
+        db_function: DbFunction,
+        row_selection: DataRowSelection,
+    ) -> bool:
+        for arg in expression.args:
+            expression_characteristics = arg.collect_involved_characteristics(
+                row_selection
+            )
+
+            # Note that function names will always be provided in lower case.
+            if db_function.function_name in {"sum", "avg", "stddev_samp", "stddev_pop"}:
+                if ExpressionCharacteristics.MAX_VALUE in expression_characteristics:
+                    return True
 
         return False
