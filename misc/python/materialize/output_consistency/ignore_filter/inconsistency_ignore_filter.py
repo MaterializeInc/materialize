85d3ac86db5530943dfb137b503c1c2f3d6dad80
Rainer Niedermayr <rainer@materialize.com>
2023-05-31 12:05:34 +0200

    Ignore aggregation on numeric non-leaf expression

--- misc/python/materialize/output_consistency/ignore_filter/inconsistency_ignore_filter.py
+++ misc/python/materialize/output_consistency/ignore_filter/inconsistency_ignore_filter.py
@@ -8,6 +8,7 @@
 # by the Apache License, Version 2.0.
 from typing import Set
 
+from materialize.output_consistency.data_type.data_type_category import DataTypeCategory
 from materialize.output_consistency.expression.expression import Expression
 from materialize.output_consistency.expression.expression_characteristics import (
     ExpressionCharacteristics,
@@ -16,7 +17,10 @@ from materialize.output_consistency.expression.expression_with_args import (
     ExpressionWithArgs,
 )
 from materialize.output_consistency.expression.leaf_expression import LeafExpression
-from materialize.output_consistency.operation.operation import DbFunction
+from materialize.output_consistency.operation.operation import (
+    DbFunction,
+    DbOperationOrFunction,
+)
 from materialize.output_consistency.selection.selection import DataRowSelection
 
 
@@ -59,17 +63,40 @@ class InconsistencyIgnoreFilter:
             # currently no issues without aggregation are known
             return False
 
+        expression_characteristics = (
+            expression.recursively_collect_involved_characteristics(row_selection)
+        )
+
+        if self._matches_problematic_operation_or_function_invocation(
+            expression, expression.operation, expression_characteristics
+        ):
+            return True
+
         if isinstance(expression.operation, DbFunction):
             # currently only issues with functions are known, therefore ignore operations
             db_function = expression.operation
-            expression_characteristics = (
-                expression.recursively_collect_involved_characteristics(row_selection)
-            )
 
             if self._matches_problematic_function_invocation(
                 db_function, expression_characteristics
             ):
                 return True
+
+        return False
+
+    def _matches_problematic_operation_or_function_invocation(
+        self,
+        expression: ExpressionWithArgs,
+        operation: DbOperationOrFunction,
+        _all_involved_characteristics: Set[ExpressionCharacteristics],
+    ) -> bool:
+        if operation.is_aggregation:
+            for arg in expression.args:
+                if (
+                    not isinstance(arg, LeafExpression)
+                    and arg.resolve_return_type_category() == DataTypeCategory.NUMERIC
+                ):
+                    # tracked with https://github.com/MaterializeInc/materialize/issues/19592
+                    return True
 
         return False
 
