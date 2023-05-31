1b33eeb67e553230526e450613c30984d1464086
Moritz Hoffmann <mh@materialize.com>
2023-05-30 21:04:33 -0400

    Remove unused columnation implementation
    Signed-off-by: Moritz Hoffmann <mh@materialize.com>

--- src/expr/src/relation/mod.rs
+++ src/expr/src/relation/mod.rs
@@ -2184,17 +2184,6 @@ pub struct ColumnOrder {
     pub nulls_last: bool,
 }
 
-mod columnation {
-    use columnation::{CloneRegion, Columnation};
-
-    use crate::ColumnOrder;
-
-    impl Columnation for ColumnOrder {
-        // ColumnOrder does not contain any pointers.
-        type InnerRegion = CloneRegion<ColumnOrder>;
-    }
-}
-
 impl RustType<ProtoColumnOrder> for ColumnOrder {
     fn into_proto(&self) -> ProtoColumnOrder {
         ProtoColumnOrder {
