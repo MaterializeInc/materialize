8c4ad53172361b15e73c3431c6703c103d35bfd0
Sean Loiselle <sean@materialize.io>
2023-06-02 22:24:05 -0400

    expr: adjust lower bounds when concatenating arrays

--- src/expr/src/scalar/func.rs
+++ src/expr/src/scalar/func.rs
@@ -6760,7 +6760,7 @@ fn array_array_concat<'a>(
                 return Err(EvalError::IncompatibleArrayDimensions { dims: None });
             }
             dims = vec![ArrayDimension {
-                lower_bound: 1,
+                lower_bound: a_dims[0].lower_bound,
                 length: a_dims[0].length + b_dims[0].length,
             }];
             dims.extend(&a_dims[1..]);
@@ -6773,7 +6773,7 @@ fn array_array_concat<'a>(
                 return Err(EvalError::IncompatibleArrayDimensions { dims: None });
             }
             dims = vec![ArrayDimension {
-                lower_bound: 1,
+                lower_bound: b_dims[0].lower_bound,
                 // Since `a` is treated as an element of `b`, the length of
                 // the first dimension of `b` is incremented by one, as `a` is
                 // non-empty.
@@ -6789,7 +6789,7 @@ fn array_array_concat<'a>(
                 return Err(EvalError::IncompatibleArrayDimensions { dims: None });
             }
             dims = vec![ArrayDimension {
-                lower_bound: 1,
+                lower_bound: a_dims[0].lower_bound,
                 // Since `b` is treated as an element of `a`, the length of
                 // the first dimension of `a` is incremented by one, as `b`
                 // is non-empty.
