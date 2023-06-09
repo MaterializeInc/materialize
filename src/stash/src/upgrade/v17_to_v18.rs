a7a94be10abeee890b928bf174b1e805e523d7af
Joseph Koshakow <koshy44@gmail.com>
2023-06-09 10:44:01 -0400

    adapter: Fix default privilege stash migrations (#19820)
    7e91d54e342a9cae3756b2a55db71202ea8678b5 Added a new stash collection
    for default privileges, and initialized it with a single USAGE privilege
    for all types. However, the initialization logic is only run on
    uninitialized stashes. As a result, stashes that were upgraded never got
    the single row with the USAGE privilege.
    
    This commit updates the upgrade logic so that the USAGE privilege is
    added during stash upgrades.
    
    Fixes #19811

--- src/stash/src/upgrade/v17_to_v18.rs
+++ src/stash/src/upgrade/v17_to_v18.rs
@@ -7,5 +7,42 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
 
-/// No-op migration. Just need to bump the version number after adding default privileges.
-pub fn upgrade() {}
+use crate::{StashError, Transaction, TypedCollection};
+
+pub mod objects_v18 {
+    include!(concat!(env!("OUT_DIR"), "/objects_v18.rs"));
+}
+
+/// Migration that adds USAGE privileges on all types to the PUBLIC role in the default privileges
+/// cluster.
+///
+/// Author - jkosh44
+pub async fn upgrade(tx: &'_ mut Transaction<'_>) -> Result<(), StashError> {
+    const DEFAULT_PRIVILEGES_COLLECTION: TypedCollection<
+        objects_v18::DefaultPrivilegesKey,
+        objects_v18::DefaultPrivilegesValue,
+    > = TypedCollection::new("default_privileges");
+    const ACL_MODE_USAGE: objects_v18::AclMode = objects_v18::AclMode { bitflags: 1 << 8 };
+
+    DEFAULT_PRIVILEGES_COLLECTION
+        .initialize(
+            tx,
+            vec![(
+                objects_v18::DefaultPrivilegesKey {
+                    role_id: Some(objects_v18::RoleId {
+                        value: Some(objects_v18::role_id::Value::Public(Default::default())),
+                    }),
+                    database_id: None,
+                    schema_id: None,
+                    object_type: objects_v18::ObjectType::Type.into(),
+                    grantee: Some(objects_v18::RoleId {
+                        value: Some(objects_v18::role_id::Value::Public(Default::default())),
+                    }),
+                },
+                objects_v18::DefaultPrivilegesValue {
+                    privileges: Some(ACL_MODE_USAGE),
+                },
+            )],
+        )
+        .await
+}
