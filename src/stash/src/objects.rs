a495fed01e2e6e38a2a3e6b3d10618bd24143ba7
Parker Timmerman <parker.timmerman@materialize.com>
2023-06-14 11:08:37 -0400

    stash: Add more asserts around snapshots (#19921)
    ### Motivation
    
    This PR adds two tests, which assert the following properties of our
    Stash snapshots:
    
    1. Snapshots exist for all versions `min..=current`, and that no other
    snapshots exist.
    * This guards against folks making new snapshots, but not bumping the
    `STASH_VERSION` and thus their migration never running.
    2. That `objects.proto` and `objects_v<STASH_VERSION>.proto` are exactly
    the same.
    * This ensures that the next time we bump the Stash version, the
    snapshot we're migrating from is an accurate representation of how the
    Stash objects currently exist.
    
    This PR also updates the most recent snapshot to be the same as
    `objects.proto`, because the test discovered that the two already came
    out of sync :)
    
    ### Checklist
    
    - [ ] This PR has adequate test coverage / QA involvement has been duly
    considered.
    - [ ] This PR has an associated up-to-date [design
    doc](https://github.com/MaterializeInc/materialize/blob/main/doc/developer/design/README.md),
    is a design doc
    ([template](https://github.com/MaterializeInc/materialize/blob/main/doc/developer/design/00000000_template.md)),
    or is sufficiently small to not require a design.
      <!-- Reference the design in the description. -->
    - [ ] If this PR evolves [an existing `$T ⇔ Proto$T`
    mapping](https://github.com/MaterializeInc/materialize/blob/main/doc/developer/command-and-response-binary-encoding.md)
    (possibly in a backwards-incompatible way), then it is tagged with a
    `T-proto` label.
    - [ ] If this PR will require changes to cloud orchestration, there is a
    companion cloud PR to account for those changes that is tagged with the
    release-blocker label
    ([example](https://github.com/MaterializeInc/cloud/pull/5021)).
    <!-- Ask in #team-cloud on Slack if you need help preparing the cloud
    PR. -->
    - [x] This PR includes the following [user-facing behavior
    changes](https://github.com/MaterializeInc/materialize/blob/main/doc/developer/guide-changes.md#what-changes-require-a-release-note):
      - N/a

--- src/stash/src/objects.rs
+++ src/stash/src/objects.rs
@@ -175,3 +175,88 @@ impl From<String> for proto::StringWrapper {
         proto::StringWrapper { inner: value }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use std::collections::BTreeSet;
+    use std::fs;
+    use std::io::{BufRead, BufReader};
+
+    use crate::STASH_VERSION;
+
+    // Note: Feel free to update this path if the protos move.
+    const PROTO_DIRECTORY: &str = "protos";
+
+    #[mz_ore::test]
+    fn test_assert_snapshots_exist() {
+        // Get all of the files in the snapshot directory, with the `.proto` extension.
+        let mut filenames: BTreeSet<_> = fs::read_dir(PROTO_DIRECTORY)
+            .expect("failed to read protos dir")
+            .map(|entry| entry.expect("failed to read dir entry").file_name())
+            .map(|filename| filename.to_str().expect("utf8").to_string())
+            .filter(|filename| filename.ends_with("proto"))
+            .collect();
+
+        // Assert objects.proto exists.
+        assert!(filenames.remove("objects.proto"));
+
+        // Assert snapshots exist for all of the versions we support.
+        //
+        // TODO(parkmycar): Change `15` to be MIN_STASH_VERSION, once we delete all of the JSON
+        // migration code.
+        for version in 15..=STASH_VERSION {
+            let filename = format!("objects_v{version}.proto");
+            assert!(
+                filenames.remove(&filename),
+                "Missing snapshot for v{version}."
+            );
+        }
+
+        // Common case. Check to make sure the user bumped the STASH_VERSION.
+        if !filenames.is_empty()
+            && filenames.remove(&format!("objects_v{}.proto", STASH_VERSION + 1))
+        {
+            panic!(
+                "Found snapshot for v{}, please also bump `STASH_VERSION`.",
+                STASH_VERSION + 1
+            )
+        }
+
+        // Assert there aren't any extra snapshots.
+        assert!(
+            filenames.is_empty(),
+            "Found snapshots for unsupported Stash versions {filenames:?}.\nIf you just increased `MIN_STASH_VERSION`, then please delete the old snapshots. If you created a new snapshot, please bump `STASH_VERSION`."
+        );
+    }
+
+    #[mz_ore::test]
+    fn test_assert_current_snapshot() {
+        // Read the content from both files.
+        let current = fs::File::open(format!("{PROTO_DIRECTORY}/objects.proto"))
+            .map(BufReader::new)
+            .expect("read current");
+        let snapshot = fs::File::open(format!("{PROTO_DIRECTORY}/objects_v{STASH_VERSION}.proto"))
+            .map(BufReader::new)
+            .expect("read snapshot");
+
+        // Read in all of the lines so we can compare the content of †he files.
+        let current: Vec<_> = current
+            .lines()
+            .map(|r| r.expect("failed to read line from current"))
+            // Filter out the package name, since we expect that to be different.
+            .filter(|line| line != "package objects;")
+            .collect();
+        let snapshot: Vec<_> = snapshot
+            .lines()
+            .map(|r| r.expect("failed to read line from current"))
+            // Filter out the package name, since we expect that to be different.
+            .filter(|line| line != &format!("package objects_v{STASH_VERSION};"))
+            .collect();
+
+        // Note: objects.proto and objects_v<STASH_VERSION>.proto should be exactly the same. The
+        // reason being, when bumping the Stash to the next version, STASH_VERSION + 1, we need a
+        // snapshot to migrate _from_, which should be a snapshot of how the protos are today.
+        // Hence why the two files should be exactly the same.
+        similar_asserts::assert_eq!(current, snapshot);
+    }
+}
