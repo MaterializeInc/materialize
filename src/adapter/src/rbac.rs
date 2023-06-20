6b296d7a4843b7728aa657cbd62f9796929fca67
Brennan Vincent <brennan@materialize.com>
2023-06-20 10:50:21 -0400

    oops

--- src/adapter/src/rbac.rs
+++ src/adapter/src/rbac.rs
@@ -133,7 +133,6 @@ pub fn check_command(catalog: &Catalog, cmd: &Command) -> Result<(), Unauthorize
         | Command::Prepare { .. }
         | Command::VerifyPreparedStatement { .. }
         | Command::Execute { .. }
-        // | Command::ExecuteInner { .. }
         | Command::Commit { .. }
         | Command::CancelRequest { .. }
         | Command::PrivilegedCancelRequest { .. }
