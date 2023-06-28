59c0da8b3ab19f80c947e14ebaa3d615134f0ed3
Ben Kirwin <bkirwi@materialize.com>
2023-06-28 12:57:01 -0400

    Don't return the always-ignored writer state

--- src/persist-client/src/lib.rs
+++ src/persist-client/src/lib.rs
@@ -632,7 +632,7 @@ impl PersistClient {
             )
         });
         let heartbeat_ts = (self.cfg.now)();
-        let (shard_upper, _, maintenance) = machine
+        let (shard_upper, maintenance) = machine
             .register_writer(
                 &writer_id,
                 &diagnostics.handle_purpose,
