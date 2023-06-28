4152a51a37e5d6024f83e8a4d2c2d05f634d29f3
Michael Greenberg <michael.greenberg@materialize.com>
2023-06-28 15:03:23 -0400

    late materialization of Message view

--- test/ldbc-bi/views.sql
+++ test/ldbc-bi/views.sql
@@ -41,99 +41,57 @@ CREATE OR REPLACE MATERIALIZED VIEW Person_likes_Message AS
   (SELECT creationDate, PersonId, PostId as MessageId FROM Person_likes_Post);
 
 -- A recursive materialized view containing the root Post of each Message (for Posts, themselves, for Comments, traversing up the Message thread to the root Post of the tree)
-CREATE OR REPLACE MATERIALIZED VIEW Message AS
-    WITH MUTUALLY RECURSIVE
-      Message_CTE(creationDate timestamp with time zone,
-                  MessageId bigint,
-                  RootPostId bigint,
-                  RootPostLanguage text,
-                  content text,
-                  imageFile text,
-                  locationIP text,
-                  browserUsed text,
-                  length int,
-                  CreatorPersonId bigint,
-                  ContainerForumId bigint,
-                  LocationCountryId bigint,
-                  ParentMessageId bigint) AS (
-        -- base of the union: every post is a message
-        SELECT
-            creationDate,
-            id AS MessageId,
-            id AS RootPostId,
-            language AS RootPostLanguage,
-            content,
-            imageFile,
-            locationIP,
-            browserUsed,
-            length,
-            CreatorPersonId,
-            ContainerForumId,
-            LocationCountryId,
-            NULL::bigint AS ParentMessageId
-	FROM Post
-	UNION
-        SELECT
-            Comment.creationDate AS creationDate,
-            Comment.id AS MessageId,
-            Message_CTE.RootPostId AS RootPostId,
-            Message_CTE.RootPostLanguage AS RootPostLanguage,
-            Comment.content AS content,
-            NULL::text AS imageFile,
-            Comment.locationIP AS locationIP,
-            Comment.browserUsed AS browserUsed,
-            Comment.length AS length,
-            Comment.CreatorPersonId AS CreatorPersonId,
-            Message_CTE.ContainerForumId AS ContainerForumId,
-            Comment.LocationCountryId AS LocationCityId,
-            coalesce(Comment.ParentPostId, Comment.ParentCommentId) AS ParentMessageId
-        FROM Comment
-        JOIN Message_CTE
-          ON Message_CTE.MessageId = coalesce(Comment.ParentPostId, Comment.ParentCommentId)
-        UNION
-        -- second half of the union: transitive closure, following Comments
-        SELECT
-            Comment.creationDate AS creationDate,
-            Comment.id AS MessageId,
-            Message_CTE.RootPostId AS RootPostId,
-            Message_CTE.RootPostLanguage AS RootPostLanguage,
-            Comment.content AS content,
-            NULL::text AS imageFile,
-            Comment.locationIP AS locationIP,
-            Comment.browserUsed AS browserUsed,
-            Comment.length AS length,
-            Comment.CreatorPersonId AS CreatorPersonId,
-            Message_CTE.ContainerForumId AS ContainerForumId,
-            Comment.LocationCountryId AS LocationCityId,
-            coalesce(Comment.ParentPostId, Comment.ParentCommentId) AS ParentMessageId
-        FROM Comment
-        JOIN Message_CTE
-          ON Comment.ParentCommentId = Message_CTE.MessageId -- FORCEORDER?
-      )
-    -- returning...
-    SELECT
-    *
-    FROM Message_CTE
-    WHERE creationDate IS NOT NULL
-      AND MessageId IS NOT NULL
-      AND RootPostId IS NOT NULL
-      AND locationIP IS NOT NULL
-      AND browserUsed IS NOT NULL
-      AND length IS NOT NULL
-      AND CreatorPersonId IS NOT NULL
-      AND LocationCountryId IS NOT NULL;
-;
-
 CREATE OR REPLACE MATERIALIZED VIEW AllMessages AS
 WITH MUTUALLY RECURSIVE
-  ms (MessageId bigint, RootId bigint) AS
-    (SELECT id AS MessageId, id AS RootId FROM Post
-    UNION
-     SELECT id AS MessageId, ParentPostId AS RootId FROM Comment WHERE ParentPostId IS NOT NULL AND ParentCommentId IS NULL
-    UNION
-     SELECT id AS MessageId, ms.RootId FROM Comment JOIN ms ON ParentCommentId = ms.MessageId
-    )
-  SELECT * FROM ms;
+  -- compute the transitive closure (with root information) using minimnal info
+  roots (MessageId bigint, RootId bigint, RootPostLanguage text, ContainerForumId bigint, ParentMessageId bigint) AS
+    (      SELECT id AS MessageId, id AS RootId, language AS RootPostLanguage, ContainerForumId, NULL::bigint AS ParentMessageId FROM Post
+     UNION SELECT
+     	     id AS MessageId,
+	     ParentPostId AS RootId,
+	     language AS RootPostLanguage,
+	     ContainerForumId,
+	     ParentPostId as ParentMessageId,
+           FROM Comment
+	   JOIN Post
+	   ON Comment.ParentPostId = Post.id)
+  ms (MessageId bigint, RootId bigint, RootPostLanguage text, ContainerForumId bigint, ParentMessageId bigint) AS
+    (      SELECT * from roots
+     UNION SELECT id AS MessageId, ms.RootId, ms.RootPostLanguage, ms.ContainerForumId, ParentCommentId as ParentMessageId FROM Comment JOIN ms ON ParentCommentId = ms.MessageId)
+  -- now do the late materialization
+        SELECT
+          creationDate,
+          id AS MessageId,
+          id AS RootPostId,
+          language AS RootPostLanguage,
+          content,
+          imageFile,
+          locationIP,
+          browserUsed,
+          length,
+          CreatorPersonId,
+          ContainerForumId,
+          LocationCountryId,
+          NULL::bigint AS ParentMessageId
+        FROM Post
+  UNION SELECT
+          Comment.creationDate AS creationDate,
+          Comment.id AS MessageId,
+          ms.RootPostId AS RootPostId,
+          ms.RootPostLanguage AS RootPostLanguage,
+          Comment.content AS content,
+          NULL::text AS imageFile,
+          Comment.locationIP AS locationIP,
+          Comment.browserUsed AS browserUsed,
+          Comment.length AS length,
+          Comment.CreatorPersonId AS CreatorPersonId,
+          ms.ContainerForumId AS ContainerForumId,
+          Comment.LocationCountryId AS LocationCityId,
+          ms.ParentMessageId AS ParentMessageId
+	FROM Comment
+	JOIN ms
+	ON Comment.id = ms.MessageId;
+
 
 CREATE INDEX Message_MessageId ON Message (MessageId);
 
