54bb79cdd6897c0be9a2d64c977a025e6cdf5df6
Marcos Antonio Vaz Salles <vmarcos@materialize.com>
2023-07-04 17:10:30 +0100

    Enable monotonic selects in mzcompose
    This commit turns on the feature flag enable_monotonic_oneshot_selects
    in all mzcompose workflows. This change ensures that we will be testing
    one-shot SELECTs with physical plans corresponding to the ones we
    expect to use in production when the feature flag is enabled and
    subsequently eventually retired.

--- misc/python/materialize/mzcompose/services.py
+++ misc/python/materialize/mzcompose/services.py
@@ -112,6 +112,7 @@ class Materialized(Service):
                 "persist_stats_audit_percent": "100",
                 "enable_ld_rbac_checks": "true",
                 "enable_rbac_checks": "true",
+                "enable_monotonic_oneshot_selects": "true",
                 # Following values are set based on Load Test environment to
                 # reduce CRDB load as we are struggling with it in CI:
                 "persist_next_listen_batch_retryer_clamp": "100ms",
