MaterializeInc/materialize

commit 80aa52b39edf1457792fba88301ccd805e067a93

adapter: Fix panic when creating a Mat View (#19648)
### Motivation

Fixes #18908

This PR changes the `depends_on` set that is provided when sequencing a
"create materialized view". Previously when picking an `as_of` timestamp
when sequencing, we were using the `depends_on` set from planning, which
only does name resolution. When creating a materialized view with a
`SHOW` command, we wouldn't yet have determined that our materialized
view depends on a system table, in fact our dependencies list was empty.
This resulted in us picking an `Antichain { elements: [0] }`, which
later on resulted in a panic.

### Tips for reviewer

<!--
Leave some tips for your reviewer, like:

    * The diff is much smaller if viewed with whitespace hidden.
    * [Some function/module/file] deserves extra attention.
* [Some function/module/file] is pure code movement and only needs a
skim.

Delete this section if no tips.
-->

### Checklist

- [ ] This PR has adequate test coverage / QA involvement has been duly
considered.
- [ ] This PR has an associated up-to-date [design
doc](https://github.com/MaterializeInc/materialize/blob/main/doc/developer/design/README.md),
is a design doc
([template](https://github.com/MaterializeInc/materialize/blob/main/doc/developer/design/00000000_template.md)),
or is sufficiently small to not require a design.
  <!-- Reference the design in the description. -->
- [ ] If this PR evolves [an existing `$T ⇔ Proto$T`
mapping](https://github.com/MaterializeInc/materialize/blob/main/doc/developer/command-and-response-binary-encoding.md)
(possibly in a backwards-incompatible way), then it is tagged with a
`T-proto` label.
- [ ] If this PR will require changes to cloud orchestration, there is a
companion cloud PR to account for those changes that is tagged with the
release-blocker label
([example](https://github.com/MaterializeInc/cloud/pull/5021)).
<!-- Ask in #team-cloud on Slack if you need help preparing the cloud

src/adapter/src/coord/sequencer.rs
@@ -173,7 +173,7 @@ impl Coordinator {
             }
             Plan::CreateMaterializedView(plan) => {
                 tx.send(
-                    self.sequence_create_materialized_view(&mut session, plan, depends_on)
+                    self.sequence_create_materialized_view(&mut session, plan)
                         .await,
                     session,
                 );

src/adapter/src/coord/sequencer/inner.rs
@@ -1167,7 +1167,6 @@ impl Coordinator {
         &mut self,
         session: &mut Session,
         plan: CreateMaterializedViewPlan,
-        depends_on: Vec<GlobalId>,
     ) -> Result<ExecuteResponse, AdapterError> {
         let CreateMaterializedViewPlan {
             name,
@@ -1194,8 +1193,12 @@ impl Coordinator {
             return Err(AdapterError::BadItemInStorageCluster { cluster_name });
         }
 
-        self.validate_timeline_context(depends_on.clone())?;
+        // Generate the optimized expression, which allows us to know exacly what we depend on.
+        let optimized_expr = self.view_optimizer.optimize(view_expr)?;
+        let depends_on: Vec<_> = optimized_expr.depends_on().into_iter().collect();
 
+        // Validate after generating the optimized expression.
+        self.validate_timeline_context(depends_on.clone())?;
         self.validate_system_column_references(ambiguous_columns, &depends_on)?;
 
         // Materialized views are not allowed to depend on log sources, as replicas
@@ -1221,7 +1224,6 @@ impl Coordinator {
         // connect the view dataflow to the storage sink.
         let internal_view_id = self.allocate_transient_id()?;
 
-        let optimized_expr = self.view_optimizer.optimize(view_expr)?;
         let desc = RelationDesc::new(optimized_expr.typ(), column_names);
 
         // Pick the least valid read timestamp as the as-of for the view

test/sqllogictest/materialized_views.slt
@@ -370,6 +370,53 @@ ALTER VIEW mv RENAME TO mv2
 statement error materialize\.public\.mv is not a view\nHINT: Use EXPLAIN \[\.\.\.\] MATERIALIZED VIEW to explain a materialized view\.
 EXPLAIN WITH(arity, join_impls) VIEW mv
 
+# We should be able to create materialized views on top of 'SHOW' commands.
+
+statement ok
+CREATE MATERIALIZED VIEW mat_clusters AS SELECT name FROM (SHOW CLUSTERS);
+
+# Depends on an mz_internal table.
+statement error cannot create materialized view with unstable dependencies
+CREATE MATERIALIZED VIEW mat_cluster_replicas AS SELECT cluster, replica, size, ready FROM (SHOW CLUSTER REPLICAS);
+
+# Note: the only reason we use mat_clusters here, is because we know it should exist.
+statement ok
+CREATE MATERIALIZED VIEW mat_columns AS SELECT name, nullable, type FROM (SHOW COLUMNS FROM mat_clusters);
+
+statement ok
+CREATE MATERIALIZED VIEW mat_connections AS SELECT name, type FROM (SHOW CONNECTIONS);
+
+statement ok
+CREATE MATERIALIZED VIEW mat_databases AS SELECT name FROM (SHOW DATABASES);
+
+# Depends on an mz_internal table.
+statement error cannot create materialized view with unstable dependencies
+CREATE MATERIALIZED VIEW mat_indexes AS SELECT name, on, cluster, key FROM (SHOW INDEXES);
+
+# Depends on an mz_internal table.
+statement error cannot create materialized view with unstable dependencies
+CREATE MATERIALIZED VIEW mat_mat_views AS SELECT name, cluster FROM (SHOW MATERIALIZED VIEWS);
+
+statement ok
+CREATE MATERIALIZED VIEW mat_objects AS SELECT name FROM (SHOW OBJECTS);
+
+statement ok
+CREATE MATERIALIZED VIEW mat_schemas AS SELECT name FROM (SHOW SCHEMAS);
+
+statement ok
+CREATE MATERIALIZED VIEW mat_secrets AS SELECT name FROM (SHOW SECRETS);
+
+statement ok
+CREATE MATERIALIZED VIEW mat_sinks AS SELECT name, type, size FROM (SHOW SINKS);
+
+statement ok
+CREATE MATERIALIZED VIEW mat_sources AS SELECT name, type, size FROM (SHOW SOURCES);
+
+statement ok
+CREATE MATERIALIZED VIEW mat_tables AS SELECT name FROM (SHOW TABLES);
+
+statement ok
+CREATE MATERIALIZED VIEW mat_views AS SELECT name FROM (SHOW VIEWS);
 
 # Cleanup
 

src/sql/src/plan/statement/show.rs
@@ -257,7 +257,7 @@ pub fn show_databases<'a>(
     filter: Option<ShowStatementFilter<Aug>>,
 ) -> Result<ShowSelect<'a>, PlanError> {
     let query = "SELECT name FROM mz_catalog.mz_databases".to_string();
-    ShowSelect::new(scx, query, filter, None, None)
+    ShowSelect::new(scx, query, filter, None, Some(&["name"]))
 }
 
 pub fn show_schemas<'a>(
@@ -280,7 +280,7 @@ pub fn show_schemas<'a>(
         FROM mz_catalog.mz_schemas
         WHERE database_id IS NULL OR database_id = '{database_id}'",
     );
-    ShowSelect::new(scx, query, filter, None, None)
+    ShowSelect::new(scx, query, filter, None, Some(&["name"]))
 }
 
 pub fn show_objects<'a>(
@@ -338,7 +338,7 @@ fn show_connections<'a>(
         FROM mz_catalog.mz_connections
         WHERE schema_id = '{schema_spec}'",
     );
-    ShowSelect::new(scx, query, filter, None, None)
+    ShowSelect::new(scx, query, filter, None, Some(&["name", "type"]))
 }
 
 fn show_tables<'a>(
@@ -352,7 +352,7 @@ fn show_tables<'a>(
         FROM mz_catalog.mz_tables
         WHERE schema_id = '{schema_spec}'",
     );
-    ShowSelect::new(scx, query, filter, None, None)
+    ShowSelect::new(scx, query, filter, None, Some(&["name"]))
 }
 
 fn show_sources<'a>(
@@ -366,7 +366,7 @@ fn show_sources<'a>(
         FROM mz_catalog.mz_sources
         WHERE schema_id = '{schema_spec}'"
     );
-    ShowSelect::new(scx, query, filter, None, None)
+    ShowSelect::new(scx, query, filter, None, Some(&["name", "type", "size"]))
 }
 
 fn show_views<'a>(
@@ -380,7 +380,7 @@ fn show_views<'a>(
         FROM mz_catalog.mz_views
         WHERE schema_id = '{schema_spec}'"
     );
-    ShowSelect::new(scx, query, filter, None, None)
+    ShowSelect::new(scx, query, filter, None, Some(&["name"]))
 }
 
 fn show_materialized_views<'a>(
@@ -403,7 +403,7 @@ fn show_materialized_views<'a>(
          WHERE {where_clause}"
     );
 
-    ShowSelect::new(scx, query, filter, None, None)
+    ShowSelect::new(scx, query, filter, None, Some(&["name", "cluster"]))
 }
 
 fn show_sinks<'a>(
@@ -421,7 +421,7 @@ fn show_sinks<'a>(
          FROM mz_catalog.mz_sinks AS sinks
          WHERE schema_id = '{schema_spec}'",
     );
-    ShowSelect::new(scx, query, filter, None, None)
+    ShowSelect::new(scx, query, filter, None, Some(&["name", "type", "size"]))
 }
 
 fn show_types<'a>(
@@ -435,7 +435,7 @@ fn show_types<'a>(
         FROM mz_catalog.mz_types
         WHERE schema_id = '{schema_spec}'",
     );
-    ShowSelect::new(scx, query, filter, None, None)
+    ShowSelect::new(scx, query, filter, None, Some(&["name"]))
 }
 
 fn show_all_objects<'a>(
@@ -449,7 +449,7 @@ fn show_all_objects<'a>(
         FROM mz_catalog.mz_objects
         WHERE schema_id = '{schema_spec}'",
     );
-    ShowSelect::new(scx, query, filter, None, None)
+    ShowSelect::new(scx, query, filter, None, Some(&["name", "type"]))
 }
 
 pub fn show_indexes<'a>(
@@ -499,7 +499,13 @@ pub fn show_indexes<'a>(
         itertools::join(query_filter.iter(), " AND ")
     );
 
-    ShowSelect::new(scx, query, filter, None, None)
+    ShowSelect::new(
+        scx,
+        query,
+        filter,
+        None,
+        Some(&["name", "on", "cluster", "key"]),
+    )
 }
 
 pub fn show_columns<'a>(
@@ -549,7 +555,7 @@ pub fn show_clusters<'a>(
 ) -> Result<ShowSelect<'a>, PlanError> {
     let query = "SELECT mz_clusters.name FROM mz_catalog.mz_clusters".to_string();
 
-    ShowSelect::new(scx, query, filter, None, None)
+    ShowSelect::new(scx, query, filter, None, Some(&["name"]))
 }
 
 pub fn show_cluster_replicas<'a>(
@@ -559,7 +565,13 @@ pub fn show_cluster_replicas<'a>(
     let query = "SELECT cluster, replica, size, ready FROM mz_internal.mz_show_cluster_replicas"
         .to_string();
 
-    ShowSelect::new(scx, query, filter, None, None)
+    ShowSelect::new(
+        scx,
+        query,
+        filter,
+        None,
+        Some(&["cluster", "replica", "size", "ready"]),
+    )
 }
 
 pub fn show_secrets<'a>(
@@ -575,7 +587,7 @@ pub fn show_secrets<'a>(
         WHERE schema_id = '{schema_spec}'",
     );
 
-    ShowSelect::new(scx, query, filter, None, None)
+    ShowSelect::new(scx, query, filter, None, Some(&["name"]))
 }
 
 /// An intermediate result when planning a `SHOW` query.

commit 264c1bf63f10a09808a6c1ac620d218216aaa12b

adapter: correctly compute dependencies for materialized views
This commit fixes a bug introduced by #19648, in which we stopped
tracking the dependencies of a materialized view correctly.

The root cause was that there are two notions of dependencies tracked by
the system:

  1. An object required while planning an `mz_sql_parser::ast::Statement`.
  2. An object required to determine the timestamp of a `MirRelationExpr` in
     a plan.

Pre PR #19648, the code used (1) in a place where (2) was expected.
Post PR #19648, the code used (2) in a place where (1) was expected.
This commit uses (1) where (1) is expected and (2) where (2) is
expected.

A future commit will make this easier to understand.

Fix #20315.

src/adapter/src/coord/sequencer.rs
@@ -165,7 +165,7 @@ impl Coordinator {
             }
             Plan::CreateMaterializedView(plan) => {
                 let result = self
-                    .sequence_create_materialized_view(ctx.session_mut(), plan)
+                    .sequence_create_materialized_view(ctx.session_mut(), plan, depends_on)
                     .await;
                 ctx.retire(result);
             }

src/adapter/src/coord/sequencer/inner.rs
@@ -798,6 +798,7 @@ impl Coordinator {
         &mut self,
         session: &mut Session,
         plan: CreateMaterializedViewPlan,
+        planning_depends_on: Vec<GlobalId>,
     ) -> Result<ExecuteResponse, AdapterError> {
         let CreateMaterializedViewPlan {
             name,
@@ -824,19 +825,18 @@ impl Coordinator {
             return Err(AdapterError::BadItemInStorageCluster { cluster_name });
         }
 
-        // Generate the optimized expression, which allows us to know exacly what we depend on.
-        let optimized_expr = self.view_optimizer.optimize(view_expr)?;
-        let depends_on: Vec<_> = optimized_expr.depends_on().into_iter().collect();
-
-        // Validate after generating the optimized expression.
-        self.validate_timeline_context(depends_on.clone())?;
-        self.validate_system_column_references(ambiguous_columns, &depends_on)?;
-
+        // Validate any references in the materialized view's expression. We do
+        // this on the unoptimized plan to better reflect what the user typed.
+        // We want to reject queries that depend on log sources, for example,
+        // even if we can *technically* optimize that reference away.
+        let expr_depends_on: Vec<_> = view_expr.depends_on().into_iter().collect();
+        self.validate_timeline_context(expr_depends_on.iter().cloned())?;
+        self.validate_system_column_references(ambiguous_columns, &expr_depends_on)?;
         // Materialized views are not allowed to depend on log sources, as replicas
         // are not producing the same definite collection for these.
         // TODO(teskje): Remove this check once arrangement-based log sources
         // are replaced with persist-based ones.
-        let log_names = depends_on
+        let log_names = expr_depends_on
             .iter()
             .flat_map(|id| self.catalog().introspection_dependencies(*id))
             .map(|id| self.catalog().get_entry(&id).name().item.clone())
@@ -855,6 +855,7 @@ impl Coordinator {
         // connect the view dataflow to the storage sink.
         let internal_view_id = self.allocate_transient_id()?;
 
+        let optimized_expr = self.view_optimizer.optimize(view_expr)?;
         let desc = RelationDesc::new(optimized_expr.typ(), column_names);
 
         // Pick the least valid read timestamp as the as-of for the view
@@ -862,7 +863,7 @@ impl Coordinator {
         // amount of historical detail.
         let id_bundle = self
             .index_oracle(cluster_id)
-            .sufficient_collections(&depends_on);
+            .sufficient_collections(&expr_depends_on);
         let as_of = self.least_valid_read(&id_bundle);
 
         let mut ops = Vec::new();
@@ -879,7 +880,7 @@ impl Coordinator {
                 create_sql,
                 optimized_expr,
                 desc: desc.clone(),
-                depends_on,
+                depends_on: planning_depends_on,
                 cluster_id,
             }),
             owner_id: *session.current_role_id(),

test/sqllogictest/materialized_views.slt
@@ -224,6 +224,28 @@ query error unknown catalog item 'v'
 SELECT * FROM v
 
 
+# Test: a view on a materialized view that optimizes to the empty set
+# still prevents the underlying view from being dropped.
+# See: https://github.com/MaterializeInc/materialize/issues/20315
+
+statement ok
+CREATE VIEW v AS SELECT 1 AS c
+
+statement ok
+CREATE MATERIALIZED VIEW mv AS SELECT * FROM v WHERE c IS NULL
+
+statement error cannot drop view v: still depended upon by materialized view mv
+DROP VIEW v
+
+statement ok
+DROP VIEW v CASCADE
+
+query I
+SELECT count(*) FROM mz_materialized_views WHERE name = 'mv'
+----
+0
+
+
 # Test: Materialized view prevents dropping its cluster.
 
 statement ok