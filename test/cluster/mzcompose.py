16ae1196a15d6402e868ae282dab2ad45141ef33
andrewrodriguez-m <andrew.rodriguez@materialize.com>
2023-07-06 15:31:56 -0700

    fix tests

--- test/cluster/mzcompose.py
+++ test/cluster/mzcompose.py
@@ -1796,4 +1796,4 @@ def workflow_test_query_without_default_cluster(c: Composition) -> None:
     ):
         c.up("materialized", "postgres")
 
-        c.run("testdrive", "query-without-default-cluster.td")
+        c.run("testdrive", "query-without-default-cluster/query-without-default-cluster.td")
