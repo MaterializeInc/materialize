342354facb6d49fe8f434a32612f4f528ca7e5d6
Matt Jibson <matt.jibson@gmail.com>
2023-07-06 22:55:49 -0600

    stash-debug: add delete and JSON support (#20384)
    Fixes #20306
    
    ### Motivation
    
      * This PR adds a known-desirable feature.
    
    ### Tips for reviewer
    
    - Best viewed commit-by-commit.
    
    ### Checklist
    
    - [ ] This PR has adequate test coverage / QA involvement has been duly
    considered.
    - [ ] This PR has an associated up-to-date [design
    doc](https://github.com/MaterializeInc/materialize/blob/main/doc/developer/design/README.md),
    is a design doc
    ([template](https://github.com/MaterializeInc/materialize/blob/main/doc/developer/design/00000000_template.md)),
    or is sufficiently small to not require a design.
      <!-- Reference the design in the description. -->
    - [ ] If this PR evolves [an existing `$T ⇔ Proto$T`
    mapping](https://github.com/MaterializeInc/materialize/blob/main/doc/developer/command-and-response-binary-encoding.md)
    (possibly in a backwards-incompatible way), then it is tagged with a
    `T-proto` label.
    - [ ] If this PR will require changes to cloud orchestration, there is a
    companion cloud PR to account for those changes that is tagged with the
    release-blocker label
    ([example](https://github.com/MaterializeInc/cloud/pull/5021)).
    <!-- Ask in #team-cloud on Slack if you need help preparing the cloud
    PR. -->
    - [ ] This PR includes the following [user-facing behavior
    changes](https://github.com/MaterializeInc/materialize/blob/main/doc/developer/guide-changes.md#what-changes-require-a-release-note):
      - n/a
    
    ---------
    
    Co-authored-by: Nikhil Benesch <nikhil.benesch@gmail.com>

--- src/stash/src/lib.rs
+++ src/stash/src/lib.rs
@@ -84,6 +84,7 @@ use std::sync::Arc;
 
 use mz_ore::soft_assert;
 use mz_proto::{RustType, TryFromProtoError};
+use serde::{Deserialize, Serialize};
 use timely::progress::Antichain;
 
 pub mod objects;
@@ -122,8 +123,14 @@ pub type Id = i64;
 
 // A common trait for uses of K and V to express in a single place all of the
 // traits required by async_trait and StashCollection.
-pub trait Data: prost::Message + Default + Ord + Send + Sync {}
-impl<T: prost::Message + Default + Ord + Send + Sync> Data for T {}
+pub trait Data:
+    prost::Message + Default + Ord + Send + Sync + Serialize + for<'de> Deserialize<'de>
+{
+}
+impl<T: prost::Message + Default + Ord + Send + Sync + Serialize + for<'de> Deserialize<'de>> Data
+    for T
+{
+}
 
 /// `StashCollection` is like a differential dataflow [`Collection`], but the
 /// state of the collection is durable.
