a27932f6ace0fab192c064290c0e325113c6f3d7
Joseph Koshakow <koshy44@gmail.com>
2023-07-13 09:18:59 -0400

    Add explicit usage privilege to catalog schemas

--- src/adapter/src/catalog/storage/stash.rs
+++ src/adapter/src/catalog/storage/stash.rs
@@ -382,6 +382,11 @@ pub async fn initialize(
 
     let schema_privileges = vec![
         rbac::default_builtin_object_privilege(mz_sql::catalog::ObjectType::Schema).into_proto(),
+        proto::MzAclItem {
+            grantee: Some(MZ_INTROSPECTION_ROLE_ID.into_proto()),
+            grantor: Some(MZ_SYSTEM_ROLE_ID.into_proto()),
+            acl_mode: Some(AclMode::USAGE.into_proto()),
+        },
         rbac::owner_privilege(mz_sql::catalog::ObjectType::Schema, MZ_SYSTEM_ROLE_ID).into_proto(),
     ];
 
