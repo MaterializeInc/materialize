5d7efdf56f1598850d4d2058b0e675f69282db64
Marta Paes <marta.paes.moreira@gmail.com>
2023-07-13 10:51:39 +0200

    dbt-materialize: exclude subsources from introspection (#20501)
    Fixes #20483.

--- misc/dbt-materialize/dbt/include/materialize/macros/adapters.sql
+++ misc/dbt-materialize/dbt/include/materialize/macros/adapters.sql
@@ -96,11 +96,17 @@
         d.name as database,
         s.name as schema,
         o.name,
-        case when o.type = 'materialized-view' then 'materializedview' else o.type end as type
+        case when o.type = 'materialized-view' then 'materializedview'
+             else o.type
+        end as type
     from mz_objects o
+    left join mz_sources so on o.id = so.id
     join mz_schemas s on o.schema_id = s.id and s.name = '{{ schema_relation.schema }}'
     join mz_databases d on s.database_id = d.id and d.name = '{{ schema_relation.database }}'
-    where type in ('table', 'source', 'view', 'materialized-view', 'index', 'sink')
+    where o.type in ('table', 'source', 'view', 'materialized-view', 'index', 'sink')
+      --Exclude subsources and progress subsources, which aren't relevant in this
+      --context and can bork the adapter (see #20483)
+      and coalesce(so.type, '') not in ('subsource', 'progress')
   {% endcall %}
   {{ return(load_result('list_relations_without_caching').table) }}
 {% endmacro %}
--- misc/dbt-materialize/tests/adapter/test_custom_materializations.py
+++ misc/dbt-materialize/tests/adapter/test_custom_materializations.py
@@ -24,6 +24,7 @@ from fixtures import (
     test_sink,
     test_source,
     test_source_index,
+    test_subsources,
     test_view_index,
 )
 
@@ -48,6 +49,7 @@ class TestCustomMaterializations:
             "test_relation_name_loooooooooooooooooonger_than_postgres_63_limit.sql": test_relation_name_length,
             "test_source.sql": test_source,
             "test_source_index.sql": test_source_index,
+            "test_subsources.sql": test_subsources,
             "test_sink.sql": test_sink,
             "actual_indexes.sql": actual_indexes,
         }
@@ -60,7 +62,12 @@ class TestCustomMaterializations:
         # run models
         results = run_dbt(["run"])
         # run result length
-        assert len(results) == 8
+        assert len(results) == 9
+        # re-run models to ensure there are no lingering errors in recreating
+        # the materializations
+        results = run_dbt(["run"])
+        # re-run result length
+        assert len(results) == 9
         # relations_equal
         check_relations_equal(
             project.adapter, ["test_materialized_view", "test_view_index"]
