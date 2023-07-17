f561006e2cc723deccd7ad9465b70a11285e479e
Marcos Antonio Vaz Salles <vmarcos@materialize.com>
2023-07-17 15:49:36 +0100

    Enable selected SQLite SLTs for auto_index_selects
    This commit chooses two SQLite SLTs to be run in CI in the slow SLT
    pipeline with --auto-index-selects. The two SLTs are selected based on
    including min/max query patterns and being sufficiently small to
    represent only a small execution time impact on the total test running
    time. Thus, these tests can be used to increase the corpus used for
    daily testing of monotonic one-shot SELECTs. Other tests in the SQLite
    corpus that we run were considered. However, we observed that:
    (a) There seem to be none with top-k patterns; (b) The other tests
    with min/max patterns were about 10x larger in number of statements,
    being a poor fit in terms of performance to be run without
    --auto-transactions.

--- ci/slt/slt.sh
+++ ci/slt/slt.sh
@@ -67,8 +67,34 @@ tests=("${temp[@]}")
 sqllogictest -v --auto-index-selects "$@" "${tests[@]}" | tee -a target/slt.log
 sqllogictest -v "$@" "${tests_without_views[@]}" | tee -a target/slt.log
 
+# Due to performance issues (see below), we pick two selected SLTs from
+# the SQLite corpus that we can reasonably run with --auto-index-selects
+# and that include min/max query patterns. Note that none of the SLTs in
+# the corpus we presently use from SQLite contain top-k patterns.
+tests_with_views=(
+     test/sqllogictest/sqlite/test/index/random/1000/slt_good_0.test  \
+     test/sqllogictest/sqlite/test/random/aggregates/slt_good_129.test \
+)
+
+readarray -d '' tests < <(find test/sqllogictest/sqlite/test -type f -print0)
+# Exclude tests_with_views from tests
+for f in "${tests_with_views[@]}"; do
+    tests=("${tests[@]/$f}")
+done
+# Remove empty entries from tests, since
+# sqllogictests emits failures on them.
+temp=()
+for f in "${tests[@]}"; do
+    if [ -n "$f" ]; then
+        temp+=( "$f" )
+    fi
+done
+tests=("${temp[@]}")
+
+# Run selected tests with --auto-index-selects
+sqllogictest -v --auto-index-selects --enable-table-keys "$@" "${tests_with_views[@]}" | tee -a target/slt.log
 # Too slow to run with --auto-index-selects, can't run together with
 # --auto-transactions, no differences seen in previous run. We might want to
 # revisit and see if we can periodically test them, even if it takes 2-3 days
 # for the run to finish.
-find test/sqllogictest/sqlite/test -type f -print0 | xargs -0 sqllogictest -v --auto-transactions --auto-index-tables --enable-table-keys "$@" | tee -a target/slt.log
+sqllogictest -v --auto-transactions --auto-index-tables --enable-table-keys "$@" "${tests[@]}" | tee -a target/slt.log
