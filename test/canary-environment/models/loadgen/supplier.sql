a25fd9576a07f29087ad15f1ecae8244337cc84b
Philip Stoev <pstoev@materialize.com>
2023-07-17 09:15:30 +0200

    Add indexes to the loadgen sources

--- test/canary-environment/models/loadgen/supplier.sql
+++ test/canary-environment/models/loadgen/supplier.sql
@@ -7,6 +7,6 @@
 -- the Business Source License, use of this software will be governed
 -- by the Apache License, Version 2.0.
 
-{{ config(materialized='source') }}
+{{ config(materialized='source', indexes=[{'default': True}]) }}
 
 {{ create_loadgen_source(this) }}
