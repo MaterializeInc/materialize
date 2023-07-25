d453990595a4649f30ada55f4ea5da90b1c47a83
Philip Stoev <pstoev@materialize.com>
2023-07-25 15:08:17 +0200

    sqlsmith: Silence error due to new ACL-related functions (Part #2)
    Some of those functions return OIDs which in turn confuses COALESCE.

--- test/sqlsmith/mzcompose.py
+++ test/sqlsmith/mzcompose.py
@@ -151,6 +151,7 @@ known_errors = [
     '" does not exist',  # role does not exist
     "csv_extract number of columns too large",
     "coalesce types text and oid cannot be matched",  # with ACL-related functions
+    "coalesce types oid and text cannot be matched",  # with ACL-related functions
 ]
 
 
