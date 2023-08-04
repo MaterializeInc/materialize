7e198a5641d236801ce7dc34b4217a0e9d0576b5
Rainer Niedermayr <rainer@materialize.com>
2023-08-04 19:35:08 +0200

    postgres consistency: comparison: lenient decimal value comparison

--- misc/python/materialize/postgres_consistency/validation/pg_result_comparator.py
+++ misc/python/materialize/postgres_consistency/validation/pg_result_comparator.py
@@ -6,6 +6,7 @@
 # As of the Change Date specified in that file, in accordance with
 # the Business Source License, use of this software will be governed
 # by the Apache License, Version 2.0.
+
 import math
 from decimal import Decimal
 from typing import Any
@@ -22,24 +23,37 @@ class PostgresResultComparator(ResultComparator):
 
     def __init__(self, ignore_filter: InconsistencyIgnoreFilter):
         super().__init__(ignore_filter)
+        self.floating_precision = 1e-03
 
     def shall_validate_error_message(self, query_execution: QueryExecution) -> bool:
         # do not compare error messages at all
         return False
 
     def is_value_equal(self, value1: Any, value2: Any) -> bool:
-        if value1 == value2:
+        if super().is_value_equal(value1, value2):
             return True
 
-        value1 = self.round_value_if_decimal(value1)
-        value2 = self.round_value_if_decimal(value2)
-        return super().is_value_equal(value1, value2)
+        if isinstance(value1, Decimal):
+            if isinstance(value2, Decimal):
+                return self.is_decimal_equal(value1, value2)
+            if isinstance(value2, float):
+                return self.is_decimal_equal(value1, Decimal(value2))
+        if isinstance(value1, float):
+            if isinstance(value2, float):
+                return self.is_float_equal(value1, value2)
+            if isinstance(value2, Decimal):
+                return self.is_decimal_equal(Decimal(value1), value2)
 
-    def round_value_if_decimal(self, value: Any, digits: int = 3) -> Any:
-        if isinstance(value, Decimal) and not value.is_nan():
-            return round(value, digits)
+        return False
 
-        if isinstance(value, float) and not math.isnan(value):
-            return round(value, digits)
+    def is_decimal_equal(self, value1: Decimal, value2: Decimal) -> bool:
+        if value1.is_nan():
+            return value2.is_nan()
 
-        return value
+        return math.isclose(value1, value2, rel_tol=self.floating_precision)
+
+    def is_float_equal(self, value1: float, value2: float) -> bool:
+        if math.isnan(value1):
+            return math.isnan(value2)
+
+        return math.isclose(value1, value2, rel_tol=self.floating_precision)
