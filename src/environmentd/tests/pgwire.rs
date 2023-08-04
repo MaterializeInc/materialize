988ad98b7bc9ac8998d4b32c40f9c52ccf4e5cc8
Joseph Koshakow <koshy44@gmail.com>
2023-08-04 15:11:36 -0400

    Fix test

--- src/environmentd/tests/pgwire.rs
+++ src/environmentd/tests/pgwire.rs
@@ -579,6 +579,7 @@ fn test_pgtest_mz() {
         &[
             "enable_raise_statement",
             "enable_unmanaged_cluster_replicas",
+            "enable_dangerous_functions",
         ],
     );
 }
