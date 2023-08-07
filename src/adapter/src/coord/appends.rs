b26072d2cc8964a8d208fb410966706f75209957
jkoshakow44 <jkoshakow@wesleyan.edu>
2023-08-07 14:24:32 -0400

    Update appends.rs

--- src/adapter/src/coord/appends.rs
+++ src/adapter/src/coord/appends.rs
@@ -426,6 +426,16 @@ impl Coordinator {
         // Avoid excessive group commits by resetting the periodic table advancement timer. The
         // group commit triggered by above will already advance all tables.
         self.advance_timelines_interval.reset();
+
+
+
+
+
+
+
+
+
+        
     }
 
     /// Defers executing `deferred` until the write lock becomes available; waiting
