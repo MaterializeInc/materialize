79b04462763a6db6398f759da7a332c912acdc94
Mouli Mukherjee <mouli@materialize.com>
2023-08-08 15:58:31 -0700

    updating test

--- test/upsert/mzcompose.py
+++ test/upsert/mzcompose.py
@@ -156,7 +156,7 @@ def workflow_rehydration(c: Composition) -> None:
         "clusterd1",
     ]
 
-    for (style, mz, clusterd) in [
+    for style, mz, clusterd in [
         (
             "with DISK",
             Materialized(
@@ -168,7 +168,7 @@ def workflow_rehydration(c: Composition) -> None:
                     "enable_disk_cluster_replicas": "true",
                     # Force backpressure to be enabled.
                     "storage_dataflow_max_inflight_bytes": "1",
-                    "storage_dataflow_max_inflight_bytes_to_cluster_size_percent": "1",
+                    "storage_dataflow_max_inflight_bytes_to_cluster_size_fraction": "0.01",
                     "storage_dataflow_max_inflight_bytes_disk_only": "false",
                 },
                 environment_extra=materialized_environment_extra,
@@ -190,7 +190,7 @@ def workflow_rehydration(c: Composition) -> None:
                 additional_system_parameter_defaults={
                     # Force backpressure to be enabled.
                     "storage_dataflow_max_inflight_bytes": "1",
-                    "storage_dataflow_max_inflight_bytes_to_cluster_size_percent": "1",
+                    "storage_dataflow_max_inflight_bytes_to_cluster_size_fraction": "0.01",
                     "storage_dataflow_max_inflight_bytes_disk_only": "false",
                 },
                 environment_extra=materialized_environment_extra,
@@ -200,7 +200,6 @@ def workflow_rehydration(c: Composition) -> None:
             ),
         ),
     ]:
-
         with c.override(
             mz,
             clusterd,
@@ -254,7 +253,6 @@ def run_one_failpoint(c: Composition, failpoint: str, error_message: str) -> Non
     with c.override(
         Testdrive(no_reset=True, consistent_seed=True),
     ):
-
         dependencies = ["zookeeper", "kafka", "clusterd1", "materialized"]
         c.up(*dependencies)
         c.run("testdrive", "failpoint/02-source.td")
@@ -293,7 +291,7 @@ def workflow_incident_49(c: Composition) -> None:
         "schema-registry",
     ]
 
-    for (style, mz) in [
+    for style, mz in [
         (
             "with DISK",
             Materialized(
@@ -313,7 +311,6 @@ def workflow_incident_49(c: Composition) -> None:
             ),
         ),
     ]:
-
         with c.override(
             mz,
             Testdrive(no_reset=True, consistent_seed=True),
@@ -369,7 +366,7 @@ def workflow_rocksdb_cleanup(c: Composition) -> None:
         ("drop-source-in-cluster.td", "DROP SOURCE dropped_upsert", False),
     ]
 
-    for (testdrive_file, drop_stmt, cluster_dropped) in scenarios:
+    for testdrive_file, drop_stmt, cluster_dropped in scenarios:
         with c.override(
             Testdrive(no_reset=True),
         ):
