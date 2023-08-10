19505537bf340a004bd731d6a4e66943bf422526
Sean Loiselle <sean@materialize.io>
2023-08-10 12:33:21 -0400

    adapter: sequence ALTER CONNECTION

--- src/adapter/src/error.rs
+++ src/adapter/src/error.rs
@@ -217,6 +217,9 @@ pub enum AdapterError {
     ///
     /// The map keys are role names and values are detailed error messages.
     DependentObject(BTreeMap<String, Vec<String>>),
+    /// When performing an `ALTER` of some variety, re-planning the statement
+    /// errored.
+    InvalidAlter(&'static str, PlanError),
 }
 
 impl AdapterError {
@@ -301,6 +304,7 @@ impl AdapterError {
                 storage_error.source().map(|source_error| source_error.to_string_with_causes())
             }
             AdapterError::ReadOnlyTransaction => Some("SELECT queries cannot be combined with other query types, including SUBSCRIBE.".into()),
+            AdapterError::InvalidAlter(_, e) => e.detail(),
             _ => None,
         }
     }
@@ -361,6 +365,7 @@ impl AdapterError {
                 "Use `SET CLUSTER = <cluster-name>` to change your cluster and re-run the query."
                     .into(),
             ),
+            AdapterError::InvalidAlter(_, e) => e.hint(),
             _ => None,
         }
     }
@@ -462,6 +467,7 @@ impl AdapterError {
                 VarError::RequiresUnsafeMode { .. } => SqlState::CANT_CHANGE_RUNTIME_PARAM,
                 VarError::RequiresFeatureFlag { .. } => SqlState::CANT_CHANGE_RUNTIME_PARAM,
             },
+            AdapterError::InvalidAlter(_, _) => SqlState::FEATURE_NOT_SUPPORTED,
         }
     }
 
@@ -676,6 +682,9 @@ impl fmt::Display for AdapterError {
                     "{role_str} \"{}\" cannot be dropped because some objects depend on it",
                     dependent_objects.keys().join(", ")
                 )
+            }
+            AdapterError::InvalidAlter(t, e) => {
+                write!(f, "invalid ALTER {t}: resulting plan is invalid: {e}")
             }
         }
     }
