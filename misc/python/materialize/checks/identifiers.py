c0fda07d86329942ef5625fb564f4b7664077e27
calum <calum.mj.russell@gmail.com>
2023-08-10 17:48:22 +0100

    try identifiers.py fix, system-cluster.slt output fix

--- misc/python/materialize/checks/identifiers.py
+++ misc/python/materialize/checks/identifiers.py
@@ -272,9 +272,9 @@ class Identifiers(Check):
         {dq_print(self.ident["schema"])}
 
         > SHOW SINKS FROM {dq(self.ident["schema"])};
-        {dq_print(self.ident["sink0"])} kafka ${{arg.default-storage-size}} {dq_print(self.ident["db"]) + "_"}{dq_print(self.ident["schema"])}_{dq_print(self.ident["sink0"])}
-        {dq_print(self.ident["sink1"])} kafka ${{arg.default-storage-size}} {dq_print(self.ident["db"]) + "_"}{dq_print(self.ident["schema"])}_{dq_print(self.ident["sink1"])}
-        {dq_print(self.ident["sink2"])} kafka ${{arg.default-storage-size}} {dq_print(self.ident["db"]) + "_"}{dq_print(self.ident["schema"])}_{dq_print(self.ident["sink2"])}
+        {dq_print(self.ident["sink0"])} kafka ${{arg.default-storage-size}} {dq_print(self.ident["db"]) + "_"}{dq_print(self.ident["schema"]) + "_"}{dq_print(self.ident["sink0"]) + "_"}
+        {dq_print(self.ident["sink1"])} kafka ${{arg.default-storage-size}} {dq_print(self.ident["db"]) + "_"}{dq_print(self.ident["schema"]) + "_"}{dq_print(self.ident["sink1"]) + "_"}
+        {dq_print(self.ident["sink2"])} kafka ${{arg.default-storage-size}} {dq_print(self.ident["db"]) + "_"}{dq_print(self.ident["schema"]) + "_"}{dq_print(self.ident["sink2"]) + "_"}
 
         > SELECT * FROM {dq(self.ident["schema"])}.{dq(self.ident["mv0"])};
         3
