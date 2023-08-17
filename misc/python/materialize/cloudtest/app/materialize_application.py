f4601b5a0dd261041d6533bda2e24eddef3b2b4f
Rainer Niedermayr <rainer@materialize.com>
2023-08-17 19:31:03 +0200

    mz: rename root var

--- misc/python/materialize/cloudtest/app/materialize_application.py
+++ misc/python/materialize/cloudtest/app/materialize_application.py
@@ -49,7 +49,7 @@ class MaterializeApplication(Application):
         self.testdrive = Testdrive(release_mode=release_mode, aws_region=aws_region)
         self.release_mode = release_mode
         self.aws_region = aws_region
-        self.root = MZ_ROOT
+        self.mz_root = MZ_ROOT
 
         # Register the VpcEndpoint CRD.
         self.register_vpc_endpoint()
@@ -94,7 +94,7 @@ class MaterializeApplication(Application):
             "apply",
             "-f",
             os.path.join(
-                os.path.abspath(self.root),
+                os.path.abspath(self.mz_root),
                 "src/cloud-resources/src/crd/gen/vpcendpoints.json",
             ),
         )
@@ -126,7 +126,7 @@ class MaterializeApplication(Application):
 
     def acquire_images(self) -> None:
         repo = mzbuild.Repository(
-            self.root, release_mode=self.release_mode, coverage=self.coverage_mode()
+            self.mz_root, release_mode=self.release_mode, coverage=self.coverage_mode()
         )
         for image in self.images:
             self._acquire_image(repo, image)
