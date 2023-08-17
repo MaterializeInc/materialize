112809e6aa07924b50463f3eea498cd5134128b3
Dennis Hume <dennis@materialize.com>
2023-08-17 09:18:02 -0500

    Profile Template

---
+++ misc/dbt-materialize/dbt/include/starter_project/models/example/avg_bids.sql
@@ -0,0 +1,33 @@
+-- Copyright 2020 Josh Wills. All rights reserved.
+-- Copyright Materialize, Inc. and contributors. All rights reserved.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License in the LICENSE file at the
+-- root of this repository, or online at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+-- Most of these macros are direct copies of their PostgreSQL counterparts.
+-- See: https://github.com/dbt-labs/dbt-core/blob/13b18654f/plugins/postgres/dbt/include/postgres/macros/adapters.sql
+
+{{ config(materialized='view', indexes=[{'columns': ['item']}]) }}
+
+-- Dependent on auction source but cannot reference subsource directly
+dep as (
+    SELECT * FROM {{ ref('auction') }}
+),
+
+SELECT
+  auctions.item,
+  avg(bids.amount) AS average_bid
+FROM bids
+JOIN auctions ON bids.auction_id = auctions.id
+WHERE bids.bid_time < auctions.end_time
+GROUP BY auctions.item
