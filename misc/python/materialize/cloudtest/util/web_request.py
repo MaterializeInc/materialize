82740657d3a148952a5268cb0a7db6e1dcf2e264
Rainer Niedermayr <rainer@materialize.com>
2023-08-23 20:50:23 +0200

    cloudtests: convert web-request utils to class

--- misc/python/materialize/cloudtest/util/web_request.py
+++ misc/python/materialize/cloudtest/util/web_request.py
@@ -31,153 +31,153 @@ def verbose_http_errors() -> Generator[None, None, None]:
         raise
 
 
-def get(
-    auth: Optional[AuthConfig],
-    base_url: str,
-    path: str,
-    timeout: int = 15,
-) -> requests.Response:
-    eprint(f"GET {base_url}{path}")
+class WebRequest:
+    def get(
+        self,
+        auth: Optional[AuthConfig],
+        base_url: str,
+        path: str,
+        timeout: int = 15,
+    ) -> requests.Response:
+        eprint(f"GET {base_url}{path}")
 
-    def try_get() -> requests.Response:
-        with verbose_http_errors():
-            headers = _create_headers(auth)
-            response = requests.get(
-                f"{base_url}{path}",
-                headers=headers,
-                timeout=timeout,
-                cert=_get_cert(auth),
-            )
-            response.raise_for_status()
-            return response
+        def try_get() -> requests.Response:
+            with verbose_http_errors():
+                headers = self._create_headers(auth)
+                response = requests.get(
+                    f"{base_url}{path}",
+                    headers=headers,
+                    timeout=timeout,
+                    cert=self._get_cert(auth),
+                )
+                response.raise_for_status()
+                return response
 
-    try:
-        response = try_get()
-    except requests.exceptions.HTTPError as e:
-        if auth and e.response.status_code == 401:
-            auth.refresh()
+        try:
             response = try_get()
-        else:
-            raise
+        except requests.exceptions.HTTPError as e:
+            if auth and e.response.status_code == 401:
+                auth.refresh()
+                response = try_get()
+            else:
+                raise
 
-    return response
+        return response
 
+    def post(
+        self,
+        auth: Optional[AuthConfig],
+        base_url: str,
+        path: str,
+        json: Any,
+        timeout: int = 15,
+    ) -> requests.Response:
+        eprint(f"POST {base_url}{path}")
 
-def post(
-    auth: Optional[AuthConfig],
-    base_url: str,
-    path: str,
-    json: Any,
-    timeout: int = 15,
-) -> requests.Response:
-    eprint(f"POST {base_url}{path}")
+        def try_post() -> requests.Response:
+            with verbose_http_errors():
+                headers = self._create_headers(auth)
+                response = requests.post(
+                    f"{base_url}{path}",
+                    headers=headers,
+                    json=json,
+                    timeout=timeout,
+                    cert=self._get_cert(auth),
+                )
+                response.raise_for_status()
+                return response
 
-    def try_post() -> requests.Response:
-        with verbose_http_errors():
-            headers = _create_headers(auth)
-            response = requests.post(
-                f"{base_url}{path}",
-                headers=headers,
-                json=json,
-                timeout=timeout,
-                cert=_get_cert(auth),
-            )
-            response.raise_for_status()
-            return response
-
-    try:
-        response = try_post()
-    except requests.exceptions.HTTPError as e:
-        if auth and e.response.status_code == 401:
-            auth.refresh()
+        try:
             response = try_post()
-        else:
-            raise
+        except requests.exceptions.HTTPError as e:
+            if auth and e.response.status_code == 401:
+                auth.refresh()
+                response = try_post()
+            else:
+                raise
 
-    return response
+        return response
 
+    def patch(
+        self,
+        auth: Optional[AuthConfig],
+        base_url: str,
+        path: str,
+        json: Any,
+        timeout: int = 15,
+    ) -> requests.Response:
+        eprint(f"PATCH {base_url}{path}")
 
-def patch(
-    auth: Optional[AuthConfig],
-    base_url: str,
-    path: str,
-    json: Any,
-    timeout: int = 15,
-) -> requests.Response:
-    eprint(f"PATCH {base_url}{path}")
+        def try_patch() -> requests.Response:
+            with verbose_http_errors():
+                headers = self._create_headers(auth)
+                response = requests.patch(
+                    f"{base_url}{path}",
+                    headers=headers,
+                    json=json,
+                    timeout=timeout,
+                    cert=self._get_cert(auth),
+                )
+                response.raise_for_status()
+                return response
 
-    def try_patch() -> requests.Response:
-        with verbose_http_errors():
-            headers = _create_headers(auth)
-            response = requests.patch(
-                f"{base_url}{path}",
-                headers=headers,
-                json=json,
-                timeout=timeout,
-                cert=_get_cert(auth),
-            )
-            response.raise_for_status()
-            return response
-
-    try:
-        response = try_patch()
-    except requests.exceptions.HTTPError as e:
-        if auth and e.response.status_code == 401:
-            auth.refresh()
+        try:
             response = try_patch()
-        else:
-            raise
+        except requests.exceptions.HTTPError as e:
+            if auth and e.response.status_code == 401:
+                auth.refresh()
+                response = try_patch()
+            else:
+                raise
 
-    return response
+        return response
 
+    def delete(
+        self,
+        auth: Optional[AuthConfig],
+        base_url: str,
+        path: str,
+        params: Any = None,
+        timeout: int = 15,
+    ) -> requests.Response:
+        eprint(f"DELETE {base_url}{path}")
 
-def delete(
-    auth: Optional[AuthConfig],
-    base_url: str,
-    path: str,
-    params: Any = None,
-    timeout: int = 15,
-) -> requests.Response:
-    eprint(f"DELETE {base_url}{path}")
+        def try_delete() -> requests.Response:
+            with verbose_http_errors():
+                headers = self._create_headers(auth)
+                response = requests.delete(
+                    f"{base_url}{path}",
+                    headers=headers,
+                    timeout=timeout,
+                    cert=self._get_cert(auth),
+                    **(
+                        {
+                            "params": params,
+                        }
+                        if params is not None
+                        else {}
+                    ),
+                )
+                response.raise_for_status()
+                return response
 
-    def try_delete() -> requests.Response:
-        with verbose_http_errors():
-            headers = _create_headers(auth)
-            response = requests.delete(
-                f"{base_url}{path}",
-                headers=headers,
-                timeout=timeout,
-                cert=_get_cert(auth),
-                **(
-                    {
-                        "params": params,
-                    }
-                    if params is not None
-                    else {}
-                ),
-            )
-            response.raise_for_status()
-            return response
-
-    try:
-        response = try_delete()
-    except requests.exceptions.HTTPError as e:
-        if auth and e.response.status_code == 401:
-            auth.refresh()
+        try:
             response = try_delete()
-        else:
-            raise
+        except requests.exceptions.HTTPError as e:
+            if auth and e.response.status_code == 401:
+                auth.refresh()
+                response = try_delete()
+            else:
+                raise
 
-    return response
+        return response
 
+    def _create_headers(self, auth: Optional[AuthConfig]) -> dict[str, Any]:
+        headers = {}
+        if auth:
+            headers["Authorization"] = f"Bearer {auth.token}"
 
-def _create_headers(auth: Optional[AuthConfig]) -> dict[str, Any]:
-    headers = {}
-    if auth:
-        headers["Authorization"] = f"Bearer {auth.token}"
+        return headers
 
-    return headers
-
-
-def _get_cert(auth: Optional[AuthConfig]) -> Optional[tuple[str, str]]:
-    return auth.client_cert if auth is not None else None
+    def _get_cert(self, auth: Optional[AuthConfig]) -> Optional[tuple[str, str]]:
+        return auth.client_cert if auth is not None else None
