4e2c858ee42546c25923b07c674de499ffd396d9
Rainer Niedermayr <rainer@materialize.com>
2023-08-23 13:30:31 +0200

    cloudtests: add client_cert to AuthConfig

--- misc/python/materialize/cloudtest/util/authentication.py
+++ misc/python/materialize/cloudtest/util/authentication.py
@@ -27,6 +27,8 @@ class AuthConfig:
 
     refresh_fn: Callable[[AuthConfig], None]
 
+    client_cert: Optional[tuple[str, str]] = None
+
     def refresh(self) -> None:
         self.refresh_fn(self)
 
