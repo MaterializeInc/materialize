cd9e141eac65498e720d8b7a15242eb6f3412e61
Parker Timmerman <parker.timmerman@materialize.com>
2023-08-28 09:54:10 -0400

    frontegg: Use reqwest_retry to retry on 502 errors (#21370)
    This PR uses `reqwest_retry` in the `mz_frontegg_auth::Client` so we
    retry on more types of errors than we do now.
    
    Note: In this PR we rely on a fork of `reqwest_retry` because otherwise
    we end up adding `parking_lot v0.11.2` to our `Cargo.lock`, which is a
    duplicated dependency. Ideally in the fork we can just upgrade
    `parking_lot`, but that doesn't work because
    [`wasm-timer`](https://github.com/tomaka/wasm-timer) also depends on
    `parking_lot v0.11` and that crate appears to be unmaintained. So if we
    wanted to retain WASM support for `reqwest_retry` we'd need to fork both
    `reqwest_retry` and `wasm-timer`. But since we don't need WASM support,
    in the fork I just comment out the WASM related deps with a note as to
    why.
    
    
    ### Motivation
    
    * This PR fixes a recognized bug.
    
    Fixes https://github.com/MaterializeInc/materialize/issues/21356
    
    ### Tips for reviewer
    
    <!--
    Leave some tips for your reviewer, like:
    
        * The diff is much smaller if viewed with whitespace hidden.
        * [Some function/module/file] deserves extra attention.
    * [Some function/module/file] is pure code movement and only needs a
    skim.
    
    Delete this section if no tips.
    -->
    
    ### Checklist
    
    - [ ] This PR has adequate test coverage / QA involvement has been duly
    considered.
    - [ ] This PR has an associated up-to-date [design
    doc](https://github.com/MaterializeInc/materialize/blob/main/doc/developer/design/README.md),
    is a design doc
    ([template](https://github.com/MaterializeInc/materialize/blob/main/doc/developer/design/00000000_template.md)),
    or is sufficiently small to not require a design.
      <!-- Reference the design in the description. -->
    - [ ] If this PR evolves [an existing `$T ⇔ Proto$T`
    mapping](https://github.com/MaterializeInc/materialize/blob/main/doc/developer/command-and-response-binary-encoding.md)
    (possibly in a backwards-incompatible way), then it is tagged with a
    `T-proto` label.
    - [ ] If this PR will require changes to cloud orchestration or tests,
    there is a companion cloud PR to account for those changes that is
    tagged with the release-blocker label
    ([example](https://github.com/MaterializeInc/cloud/pull/5021)).
    <!-- Ask in #team-cloud on Slack if you need help preparing the cloud
    PR. -->
    - [x] This PR includes the following [user-facing behavior
    changes](https://github.com/MaterializeInc/materialize/blob/main/doc/developer/guide-changes.md#what-changes-require-a-release-note):
      - Retry more often on authentication errors.

--- src/frontegg-auth/src/client/tokens.rs
+++ src/frontegg-auth/src/client/tokens.rs
@@ -19,18 +19,16 @@ impl Client {
         secret: Uuid,
         admin_api_token_url: &str,
     ) -> Result<ApiTokenResponse, Error> {
-        self.request(|client| async move {
-            let res: ApiTokenResponse = client
-                .post(admin_api_token_url)
-                .json(&ApiTokenArgs { client_id, secret })
-                .send()
-                .await?
-                .error_for_status()?
-                .json::<ApiTokenResponse>()
-                .await?;
-            Ok(res)
-        })
-        .await
+        let res = self
+            .client
+            .post(admin_api_token_url)
+            .json(&ApiTokenArgs { client_id, secret })
+            .send()
+            .await?
+            .error_for_status()?
+            .json::<ApiTokenResponse>()
+            .await?;
+        Ok(res)
     }
 
     /// Exchanges a client id and secret for a jwt token.
@@ -39,22 +37,20 @@ impl Client {
         refresh_url: &str,
         refresh_token: &str,
     ) -> Result<ApiTokenResponse, Error> {
-        self.request(|client| async move {
-            let res: ApiTokenResponse = client
-                .post(refresh_url)
-                .json(&RefreshToken { refresh_token })
-                .send()
-                .await?
-                .error_for_status()?
-                .json::<ApiTokenResponse>()
-                .await?;
-            Ok(res)
-        })
-        .await
+        let res = self
+            .client
+            .post(refresh_url)
+            .json(&RefreshToken { refresh_token })
+            .send()
+            .await?
+            .error_for_status()?
+            .json::<ApiTokenResponse>()
+            .await?;
+        Ok(res)
     }
 }
 
-#[derive(Debug, serde::Serialize, serde::Deserialize)]
+#[derive(Debug, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct ApiTokenResponse {
     pub expires: String,
@@ -74,4 +70,105 @@ pub struct ApiTokenArgs {
 #[serde(rename_all = "camelCase")]
 pub struct RefreshToken<'a> {
     pub refresh_token: &'a str,
+}
+
+#[cfg(test)]
+mod tests {
+    use axum::{routing::post, Router};
+    use reqwest::StatusCode;
+    use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+    use std::sync::atomic::{AtomicUsize, Ordering};
+    use std::sync::Arc;
+    use uuid::Uuid;
+
+    use super::ApiTokenResponse;
+    use crate::Client;
+
+    #[mz_ore::test(tokio::test)]
+    #[cfg_attr(miri, ignore)] // unsupported operation: can't call foreign function `TLS_method` on OS `linux`
+    async fn response_retries() {
+        let count = Arc::new(AtomicUsize::new(0));
+        let count_ = Arc::clone(&count);
+
+        // Fake server that returns the provided status code a few times before returning success.
+        let app = Router::new().route(
+            "/:status_code",
+            post(
+                |axum::extract::Path(code): axum::extract::Path<u16>| async move {
+                    let cnt = count_.fetch_add(1, Ordering::Relaxed);
+                    println!("cnt: {cnt}");
+
+                    let resp = ApiTokenResponse {
+                        expires: "test".to_string(),
+                        expires_in: 0,
+                        access_token: "test".to_string(),
+                        refresh_token: "test".to_string(),
+                    };
+                    let resp = serde_json::to_string(&resp).unwrap();
+
+                    if cnt >= 2 {
+                        Ok(resp.clone())
+                    } else {
+                        Err(StatusCode::from_u16(code).unwrap())
+                    }
+                },
+            ),
+        );
+
+        // Use port 0 to get a dynamically assigned port.
+        let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 0);
+        let tcp = std::net::TcpListener::bind(addr).expect("able to bind");
+        let addr = tcp.local_addr().expect("valid addr");
+        mz_ore::task::spawn(|| "test-server", async move {
+            axum::Server::from_tcp(tcp)
+                .expect("able to start")
+                .serve(app.into_make_service())
+                .await
+                .unwrap();
+        });
+
+        let client = Client::default();
+        async fn test_case(
+            client: &Client,
+            addr: &SocketAddr,
+            count: &Arc<AtomicUsize>,
+            code: u16,
+            should_retry: bool,
+        ) -> Result<(), String> {
+            let exchange_result = client
+                .exchange_client_secret_for_token(
+                    Uuid::new_v4(),
+                    Uuid::new_v4(),
+                    &format!("http://{addr}/{code}"),
+                )
+                .await
+                .map(|_| ())
+                .map_err(|e| e.to_string());
+            let prev_count = count.swap(0, Ordering::Relaxed);
+            let expected_count = should_retry.then_some(3).unwrap_or(1);
+            assert_eq!(prev_count, expected_count);
+
+            let refresh_result = client
+                .refresh_token(&format!("http://{addr}/{code}"), "test")
+                .await
+                .map(|_| ())
+                .map_err(|e| e.to_string());
+            let prev_count = count.swap(0, Ordering::Relaxed);
+            let expected_count = should_retry.then_some(3).unwrap_or(1);
+            assert_eq!(prev_count, expected_count);
+
+            assert_eq!(exchange_result, refresh_result);
+            exchange_result
+        }
+
+        // Should get retried which results in eventual success.
+        assert!(test_case(&client, &addr, &count, 500, true).await.is_ok());
+        assert!(test_case(&client, &addr, &count, 502, true).await.is_ok());
+        assert!(test_case(&client, &addr, &count, 429, true).await.is_ok());
+        assert!(test_case(&client, &addr, &count, 408, true).await.is_ok());
+
+        // Should not get retried, and thus return an error.
+        assert!(test_case(&client, &addr, &count, 404, false).await.is_err());
+        assert!(test_case(&client, &addr, &count, 400, false).await.is_err());
+    }
 }
