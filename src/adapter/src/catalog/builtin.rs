3afb7d87629cfdee12a35b16afd60f2043bb84bf
Joseph Koshakow <koshy44@gmail.com>
2023-08-30 10:25:14 -0400

    Fix lint

--- src/adapter/src/catalog/builtin.rs
+++ src/adapter/src/catalog/builtin.rs
@@ -4227,7 +4227,7 @@ JOIN mz_catalog.mz_clusters AS clusters ON clusters.id = sources.cluster_id",
 pub const MZ_SHOW_SINKS: BuiltinView = BuiltinView {
     name: "mz_show_sinks",
     schema: MZ_INTERNAL_SCHEMA,
-    sql:"CREATE VIEW mz_internal.mz_show_sinks
+    sql: "CREATE VIEW mz_internal.mz_show_sinks
 AS SELECT sinks.name, sinks.type, sinks.size, clusters.name as cluster, schema_id, cluster_id
 FROM mz_catalog.mz_sinks AS sinks
 JOIN mz_catalog.mz_clusters AS clusters ON clusters.id = sinks.cluster_id",
