a8c5a2c98f3f552a27adaee97266145f41758195
Calum <calum.mj.russell@gmail.com>
2023-08-31 17:48:43 +0000

    switched join for show sources back to left join, without this certain source types are missed

--- src/adapter/src/catalog/builtin.rs
+++ src/adapter/src/catalog/builtin.rs
@@ -4221,7 +4221,7 @@ pub const MZ_SHOW_SOURCES: BuiltinView = BuiltinView {
     sql: "CREATE VIEW mz_internal.mz_show_sources
 AS SELECT sources.name, sources.type, sources.size, clusters.name as cluster, schema_id, cluster_id
 FROM mz_catalog.mz_sources AS sources
-JOIN mz_catalog.mz_clusters AS clusters ON clusters.id = sources.cluster_id",
+LEFT JOIN mz_catalog.mz_clusters AS clusters ON clusters.id = sources.cluster_id",
 };
 
 pub const MZ_SHOW_SINKS: BuiltinView = BuiltinView {
