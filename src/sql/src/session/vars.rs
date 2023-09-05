b8ceb960cdd0ca04d31121c8ba8618ae981bf460
Gabor Gevay <gabor@materialize.com>
2023-09-05 11:24:28 +0200

    Notice for indexes with empty key
    Closes #21329

--- src/sql/src/session/vars.rs
+++ src/sql/src/session/vars.rs
@@ -1600,6 +1600,11 @@ feature_flags!(
         "emitting notices for IndexTooWideForLiteralConstraints (doesn't affect EXPLAIN)",
         false
     ),
+    (
+        enable_notices_for_index_empty_key,
+        "emitting notices for indexes with an empty key (doesn't affect EXPLAIN)",
+        true
+    ),
 );
 
 /// Represents the input to a variable.
