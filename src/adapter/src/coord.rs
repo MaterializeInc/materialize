463fc3bc3513aed9fd39633578a01f72f01d7f0d
Joseph Koshakow <koshy44@gmail.com>
2023-09-08 16:33:15 -0400

    Update error variant

--- src/adapter/src/coord.rs
+++ src/adapter/src/coord.rs
@@ -106,6 +106,7 @@ use mz_sql::ast::{CreateSubsourceStatement, Raw, Statement};
 use mz_sql::catalog::EnvironmentId;
 use mz_sql::names::{Aug, ResolvedIds};
 use mz_sql::plan::{CopyFormat, CreateConnectionPlan, Params, QueryWhen};
+use mz_sql::rbac::UnauthorizedError;
 use mz_sql::session::user::{RoleMetadata, User};
 use mz_sql::session::vars::ConnectionCounter;
 use mz_storage_client::controller::{
@@ -477,11 +478,18 @@ impl PlanValidity {
         if catalog
             .try_get_role(&self.role_metadata.current_role)
             .is_none()
-            || catalog
-                .try_get_role(&self.role_metadata.session_role)
-                .is_none()
         {
-            return Err(AdapterError::ChangedPlan);
+            return Err(AdapterError::Unauthorized(
+                UnauthorizedError::ConcurrentRoleDrop(self.role_metadata.current_role.clone()),
+            ));
+        }
+        if catalog
+            .try_get_role(&self.role_metadata.session_role)
+            .is_none()
+        {
+            return Err(AdapterError::Unauthorized(
+                UnauthorizedError::ConcurrentRoleDrop(self.role_metadata.session_role.clone()),
+            ));
         }
         self.transient_revision = catalog.transient_revision();
         Ok(())
