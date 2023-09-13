a7eaf0b03f631e4dd6be6d791433cd644ba4b667
Parker Timmerman <parker.timmerman@materialize.com>
2023-09-13 14:37:51 -0400

    cargo fmt

--- src/environmentd/src/http.rs
+++ src/environmentd/src/http.rs
@@ -33,7 +33,7 @@ use futures::future::{FutureExt, Shared, TryFutureExt};
 use headers::authorization::{Authorization, Basic, Bearer};
 use headers::{HeaderMapExt, HeaderName};
 use http::header::{AUTHORIZATION, CONTENT_TYPE};
-use http::{Request, StatusCode, Method};
+use http::{Method, Request, StatusCode};
 use hyper_openssl::MaybeHttpsStream;
 use mz_adapter::{AdapterError, AdapterNotice, Client, SessionClient};
 use mz_frontegg_auth::{Authentication as FronteggAuthentication, Error as FronteggError};
@@ -168,7 +168,7 @@ impl HttpServer {
                 CorsLayer::new()
                     .allow_methods(Method::POST)
                     .allow_origin(AllowOrigin::mirror_request())
-                    .allow_headers(Any)
+                    .allow_headers(Any),
             )
             .layer(
                 ServiceBuilder::new()
