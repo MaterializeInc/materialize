ddb8691b00f051c78e683190ac6777b89e9c47c0
Parker Timmerman <parker.timmerman@materialize.com>
2023-09-19 16:07:37 -0400

    start, add an in-memory map of inflight requests to de-dupe them

--- src/frontegg-auth/src/error.rs
+++ src/frontegg-auth/src/error.rs
@@ -7,20 +7,21 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
 
+use std::sync::Arc;
 use thiserror::Error;
 
 use crate::AppPasswordParseError;
 
-#[derive(Error, Debug)]
+#[derive(Clone, Error, Debug)]
 pub enum Error {
     #[error(transparent)]
     InvalidPasswordFormat(#[from] AppPasswordParseError),
     #[error("invalid token format: {0}")]
     InvalidTokenFormat(#[from] jsonwebtoken::errors::Error),
     #[error("authentication token exchange failed: {0}")]
-    ReqwestError(#[from] reqwest::Error),
+    ReqwestError(Arc<reqwest::Error>),
     #[error("middleware programming error: {0}")]
-    MiddlewareError(anyhow::Error),
+    MiddlewareError(Arc<anyhow::Error>),
     #[error("authentication token expired")]
     TokenExpired,
     #[error("unauthorized organization")]
@@ -28,14 +29,34 @@ pub enum Error {
     #[error("email in access token did not match the expected email")]
     WrongEmail,
     #[error("request timeout")]
-    Timeout(#[from] tokio::time::error::Elapsed),
+    Timeout(Arc<tokio::time::error::Elapsed>),
+    #[error("internal error")]
+    Internal(Arc<anyhow::Error>),
+}
+
+impl From<anyhow::Error> for Error {
+    fn from(value: anyhow::Error) -> Self {
+        Error::Internal(Arc::new(value))
+    }
+}
+
+impl From<tokio::time::error::Elapsed> for Error {
+    fn from(value: tokio::time::error::Elapsed) -> Self {
+        Error::Timeout(Arc::new(value))
+    }
+}
+
+impl From<reqwest::Error> for Error {
+    fn from(value: reqwest::Error) -> Self {
+        Error::ReqwestError(Arc::new(value))
+    }
 }
 
 impl From<reqwest_middleware::Error> for Error {
     fn from(value: reqwest_middleware::Error) -> Self {
         match value {
-            reqwest_middleware::Error::Middleware(e) => Error::MiddlewareError(e),
-            reqwest_middleware::Error::Reqwest(e) => Error::ReqwestError(e),
+            reqwest_middleware::Error::Middleware(e) => Error::MiddlewareError(Arc::new(e)),
+            reqwest_middleware::Error::Reqwest(e) => Error::ReqwestError(Arc::new(e)),
         }
     }
 }
