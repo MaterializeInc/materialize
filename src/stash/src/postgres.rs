fa9712d501284f4b537c3a7ba86ab42a7a9c80f1
Parker Timmerman <parker.timmerman@materialize.com>
2023-09-20 17:28:16 -0400

    start, stash changes
    stash migration
    
    add insta to the exceptions because it uses lazy static
    
    inline snapshot to avoid copyright header issue

--- src/stash/src/postgres.rs
+++ src/stash/src/postgres.rs
@@ -921,6 +921,7 @@ impl Stash {
                             35 => upgrade::v35_to_v36::upgrade(&mut tx).await?,
                             36 => upgrade::v36_to_v37::upgrade(),
                             37 => upgrade::v37_to_v38::upgrade(&mut tx).await?,
+                            38 => upgrade::v38_to_v39::upgrade(&mut tx).await?,
 
                             // Up-to-date, no migration needed!
                             STASH_VERSION => return Ok(STASH_VERSION),
