c300277e3e42ac5f31d9b8c3b81c31f9cecb53f1
Jan Teske <jteske@posteo.net>
2023-09-22 14:27:16 +0200

    prof: minor readability improvements
    This commit makes minor readability improvements to the mz-prof crate.
    Mostly removing unnecessary boilerplate and clippy `allow`s.

--- src/prof/src/time.rs
+++ src/prof/src/time.rs
@@ -32,7 +32,7 @@ pub async unsafe fn prof_time(
     time::sleep(total_time).await;
     let builder = pg.report();
     let report = builder.build_unresolved()?;
-    let mut profile = <StackProfile as Default>::default();
+    let mut profile = StackProfile::default();
     for (f, weight) in report.data {
         let thread_name;
         // No other known way to convert `*mut c_void` to `usize`.
