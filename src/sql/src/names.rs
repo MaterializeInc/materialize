aa4d9fd7a3ca032072f5f13ee4b1f42ac7914cfc
Matt Jibson <matt.jibson@gmail.com>
2023-09-26 05:36:29 +0000

    sql: correctly determine resolved ids for arrays
    Explicitly described maps, arrays, and lists referenced their inner
    types in resolved_ids. However, on restart some types would have a named
    variant that was not obviously an array. For example, `int[]` would
    convert to `pg_catalog._int`, triggering the "named" type path instead
    of "array" type path. In the named path, there was no awareness of
    possible inner types, so the outer type would be added to resolved_ids,
    resulting in catalog consistency violations.
    
    Teach the named type path to use referenced types when adding to
    resolved_ids so the code paths have the same results.
    
    Fixes #21958

--- src/sql/src/names.rs
+++ src/sql/src/names.rs
@@ -1285,7 +1285,7 @@ impl<'a> NameResolver<'a> {
                         (full_name, item)
                     }
                 };
-                self.ids.insert(item.id());
+                self.referenced_ids(item.id());
                 Ok(ResolvedDataType::Named {
                     id: item.id(),
                     qualifiers: item.name().qualifiers.clone(),
@@ -1294,6 +1294,20 @@ impl<'a> NameResolver<'a> {
                     print_id: true,
                 })
             }
+        }
+    }
+
+    /// Adds leaf types referenced by `id`.
+    fn referenced_ids(&mut self, id: GlobalId) {
+        let item = self.catalog.get_item(&id);
+        let details = item.type_details().expect("must be a Type");
+        let refs = details.typ.references();
+        if refs.is_empty() {
+            self.ids.insert(id);
+            return;
+        }
+        for r in refs {
+            self.referenced_ids(*r);
         }
     }
 
