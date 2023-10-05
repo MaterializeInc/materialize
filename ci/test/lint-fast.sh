ae45a601be3c63b840e714f70537064ea18fcf66
Rainer Niedermayr <rainer@materialize.com>
2023-10-05 16:11:48 +0200

    ci: buf: logic to generate configuration

--- ci/test/lint-fast.sh
+++ ci/test/lint-fast.sh
@@ -30,10 +30,14 @@ try cargo --locked about generate ci/deploy/licenses.hbs > /dev/null
 
 
 if [[ "${BUILDKITE_PULL_REQUEST:-true}" != "false" ]]; then
+  # see ./ci/test/lint-buf/README.md
+
   fetch_pr_target_branch
 
+  ci_collapsed_heading "Generate protobuf config"
+  try bin/pyactivate ./ci/test/lint-buf/generate-buf-config.py
+
   ci_collapsed_heading "Lint protobuf"
-  # exceptions can be defined in src/buf.yaml
   COMMON_ANCESTOR="$(get_common_ancestor_commit_of_pr_and_target)"
   try buf breaking src --against ".git#ref=$COMMON_ANCESTOR,subdir=src"
 fi
