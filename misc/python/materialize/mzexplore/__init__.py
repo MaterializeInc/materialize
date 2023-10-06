29e7cd9b273e1a0dccb08b004ee12d17b94a0138
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2023-10-06 20:08:16 +0300

    misc: add `mzexplore` command for catalog exploration

---
+++ misc/python/materialize/mzexplore/__init__.py
@@ -0,0 +1,16 @@
+# Copyright Materialize, Inc. and contributors. All rights reserved.
+#
+# Use of this software is governed by the Business Source License
+# included in the LICENSE file at the root of this repository.
+#
+# As of the Change Date specified in that file, in accordance with
+# the Business Source License, use of this software will be governed
+# by the Apache License, Version 2.0.
+
+# These are actually used in materialize.cli.mzexplore
+from materialize.mzexplore import extract  # noqa: F401
+from materialize.mzexplore.common import (
+    ExplaineeType,  # noqa: F401
+    ExplainFlag,  # noqa: F401
+    ExplainStage,  # noqa: F401
+)
