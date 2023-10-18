62784e3ca561d7b9785f6c5969a22b97581b34f9
Rainer Niedermayr <rainer@materialize.com>
2023-10-18 18:02:34 +0200

    scalability framework: fix and extract endpoint description logic

--- misc/python/materialize/scalability/endpoints.py
+++ misc/python/materialize/scalability/endpoints.py
@@ -9,10 +9,13 @@
 
 import pg8000
 
+from materialize import git
 from materialize.mzcompose.composition import Composition
 from materialize.mzcompose.services.materialized import Materialized
 from materialize.mzcompose.services.postgres import Postgres
 from materialize.scalability.endpoint import Endpoint
+
+POSTGRES_ENDPOINT_NAME = "postgres"
 
 
 class MaterializeRemote(Endpoint):
@@ -57,7 +60,7 @@ class PostgresContainer(Endpoint):
             self._port = self.composition.default_port("postgres")
 
     def name(self) -> str:
-        return "postgres"
+        return POSTGRES_ENDPOINT_NAME
 
     def __str__(self) -> str:
         return "PostgresContainer"
@@ -152,3 +155,14 @@ class MaterializeContainer(MaterializeNonRemote):
 
     def __str__(self) -> str:
         return f"MaterializeContainer ({self.image})"
+
+
+def endpoint_name_to_description(endpoint_name: str) -> str:
+    if endpoint_name == POSTGRES_ENDPOINT_NAME:
+        return endpoint_name
+
+    commit_sha = endpoint_name.split(" ")[1].strip("()")
+
+    # empty when mz_version() reports a Git SHA that is not available in the current repository
+    commit_message = git.get_commit_message(commit_sha)
+    return f"{endpoint_name} - {commit_message}"
--- test/scalability/lib.py
+++ test/scalability/lib.py
@@ -12,28 +12,26 @@ import os
 import pandas as pd
 from matplotlib import pyplot as plt  # type: ignore
 
-from materialize import MZ_ROOT, git
+from materialize import MZ_ROOT
+from materialize.scalability.endpoints import endpoint_name_to_description
 
 RESULTS_DIR = MZ_ROOT / "test" / "scalability" / "results"
 
 
 def plotit(csv_file_name: str) -> None:
-    targets = next(os.walk(RESULTS_DIR))[1]
+    endpoints = next(os.walk(RESULTS_DIR))[1]
     legend = []
     plt.rcParams["figure.figsize"] = (16, 10)
     fig, (summary_subplot, details_subplot) = plt.subplots(2, 1)
-    for i, target in enumerate(targets):
-        target_sha = target.split(" ")[1].strip("()")
+    for i, endpoint in enumerate(endpoints):
+        legend.append(endpoint_name_to_description(endpoint))
 
-        # empty when mz_version() reports a Git SHA that is not available in the current repository
-        target_comment = git.get_commit_message(target_sha)
-
-        legend.append(f"{target} - {target_comment}")
-
-        df = pd.read_csv(RESULTS_DIR / target / f"{csv_file_name}.csv")
+        df = pd.read_csv(RESULTS_DIR / endpoint / f"{csv_file_name}.csv")
         summary_subplot.scatter(df["concurrency"], df["tps"], label="tps")
 
-        df_details = pd.read_csv(RESULTS_DIR / target / f"{csv_file_name}_details.csv")
+        df_details = pd.read_csv(
+            RESULTS_DIR / endpoint / f"{csv_file_name}_details.csv"
+        )
         details_subplot.scatter(
             df_details["concurrency"] + i, df_details["wallclock"], alpha=0.25
         )
