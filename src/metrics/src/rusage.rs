5e4f231d8778b25d77fd27884512dbb485f4675f
Moritz Hoffmann <mh@materialize.com>
2023-10-20 13:29:33 -0400

    Expose rusage utime/stime
    Signed-off-by: Moritz Hoffmann <mh@materialize.com>

--- src/metrics/src/rusage.rs
+++ src/metrics/src/rusage.rs
@@ -5,23 +5,25 @@
 
 //! Report rusage metrics.
 
-use mz_ore::metrics::MetricsRegistry;
-use prometheus::IntGauge;
+use std::ops::Add;
 use std::time::Duration;
 
+use mz_ore::metrics::MetricsRegistry;
+use prometheus::{Gauge, IntGauge};
+
 macro_rules! metrics {
-    ($namespace:ident $(($name:ident, $desc:expr)),*) => {
-        metrics! { @define $namespace $(($name, $desc)),*}
+    ($namespace:ident $(($name:ident, $desc:expr, $suffix:expr, $type:ident)),*) => {
+        metrics! { @define $namespace $(($name, $desc, $suffix, $type)),*}
     };
-    (@define $namespace:ident $(($name:ident, $desc:expr)),*) => {
+    (@define $namespace:ident $(($name:ident, $desc:expr, $suffix:expr, $type:ident)),*) => {
         struct RuMetrics {
-            $($name: IntGauge,)*
+            $($name: <$type as Unit>::Gauge,)*
         }
         impl RuMetrics {
             fn new(registry: &MetricsRegistry) -> Self {
                 Self {
                     $($name: registry.register(mz_ore::metric!(
-                        name: concat!(stringify!($namespace), "_", stringify!($name)),
+                        name: concat!(stringify!($namespace), "_", stringify!($name), $suffix),
                         help: $desc,
                     )),)*
                 }
@@ -29,31 +31,73 @@ macro_rules! metrics {
             fn update(&self) {
                 let rusage = unsafe {
                     let mut rusage = std::mem::zeroed();
-                    libc::getrusage(libc::RUSAGE_SELF, &mut rusage);
+                    let ret = libc::getrusage(libc::RUSAGE_SELF, &mut rusage);
+                    if ret < 0 {
+                        return;
+                    }
                     rusage
                 };
-                $(self.$name.set(rusage.$name);)*
+                $(self.$name.set(<$type as Unit>::from(rusage.$name));)*
             }
         }
     };
 }
 
+/// Type for converting values from POSIX to Prometheus.
+trait Unit {
+    /// Prometheus gauge
+    type Gauge;
+    /// Libc type
+    type From;
+    /// Gauge type.
+    type To;
+    /// Convert an actual value.
+    fn from(value: Self::From) -> Self::To;
+}
+
+/// Unit for converting POSIX timeval.
+struct Timeval;
+impl Unit for Timeval {
+    type Gauge = Gauge;
+    type From = libc::timeval;
+    type To = f64;
+    fn from(Self::From { tv_sec, tv_usec }: Self::From) -> Self::To {
+        // timeval can capture negative values; it'd be surprising to see a negative values here.
+        Duration::from_secs(tv_sec.abs_diff(0))
+            .add(Duration::from_micros(tv_usec.abs_diff(0)))
+            .as_secs_f64()
+    }
+}
+
+/// Unit for direct conversion to i64.
+struct Unitless;
+impl Unit for Unitless {
+    type Gauge = IntGauge;
+    type From = libc::c_long;
+    type To = i64;
+    fn from(value: Self::From) -> Self::To {
+        value
+    }
+}
+
 metrics! {
     mz_metrics_libc
-    (ru_maxrss, "maximum resident set size"),
-    (ru_ixrss, "integral shared memory size"),
-    (ru_idrss, "integral unshared data size"),
-    (ru_isrss, "integral unshared stack size"),
-    (ru_minflt, "page reclaims (soft page faults)"),
-    (ru_majflt, "page faults (hard page faults)"),
-    (ru_nswap, "swaps"),
-    (ru_inblock, "block input operations"),
-    (ru_oublock, "block output operations"),
-    (ru_msgsnd, "IPC messages sent"),
-    (ru_msgrcv, "IPC messages received"),
-    (ru_nsignals, "signals received"),
-    (ru_nvcsw, "voluntary context switches"),
-    (ru_nivcsw, "involuntary context switches")
+    (ru_utime, "user CPU time used", "_s", Timeval),
+    (ru_stime, "system CPU time used", "_s", Timeval),
+    (ru_maxrss, "maximum resident set size", "", Unitless),
+    (ru_ixrss, "integral shared memory size", "", Unitless),
+    (ru_idrss, "integral unshared data size", "", Unitless),
+    (ru_isrss, "integral unshared stack size", "", Unitless),
+    (ru_minflt, "page reclaims (soft page faults)", "", Unitless),
+    (ru_majflt, "page faults (hard page faults)", "", Unitless),
+    (ru_nswap, "swaps", "", Unitless),
+    (ru_inblock, "block input operations", "", Unitless),
+    (ru_oublock, "block output operations", "", Unitless),
+    (ru_msgsnd, "IPC messages sent", "", Unitless),
+    (ru_msgrcv, "IPC messages received", "", Unitless),
+    (ru_nsignals, "signals received", "", Unitless),
+    (ru_nvcsw, "voluntary context switches", "", Unitless),
+    (ru_nivcsw, "involuntary context switches", "", Unitless)
 }
 
 /// Register a task to read rusage stats.
