9bf8878360932b333d1982d4e02fc502b3f1a77a
nrainer-materialize <129728240+nrainer-materialize@users.noreply.github.com>
2023-10-25 11:48:16 +0200

    scalability framework: extract classes and methods (#22611)

---
+++ misc/python/materialize/scalability/workload_executor.py
@@ -0,0 +1,263 @@
+# Copyright Materialize, Inc. and contributors. All rights reserved.
+#
+# Use of this software is governed by the Business Source License
+# included in the LICENSE file at the root of this repository.
+#
+# As of the Change Date specified in that file, in accordance with
+# the Business Source License, use of this software will be governed
+# by the Apache License, Version 2.0.
+
+import pathlib
+import threading
+import time
+from concurrent import futures
+from typing import Any
+
+import pandas as pd
+from psycopg import Cursor
+
+from materialize.scalability.benchmark_config import BenchmarkConfiguration
+from materialize.scalability.df import df_details_cols, df_totals_cols, paths
+from materialize.scalability.endpoint import Endpoint
+from materialize.scalability.operation import Operation
+from materialize.scalability.regression import RegressionOutcome
+from materialize.scalability.result_analyzer import ResultAnalyzer
+from materialize.scalability.schema import Schema
+from materialize.scalability.workload import Workload
+from materialize.scalability.workload_result import WorkloadResult
+from materialize.scalability.workloads import *  # noqa: F401 F403
+from materialize.scalability.workloads_test import *  # noqa: F401 F403
+
+
+class WorkloadExecutor:
+    def __init__(
+        self,
+        config: BenchmarkConfiguration,
+        schema: Schema,
+        baseline_endpoint: Endpoint | None,
+        other_endpoints: list[Endpoint],
+        result_analyzer: ResultAnalyzer,
+    ):
+        self.config = config
+        self.schema = schema
+        self.baseline_endpoint = baseline_endpoint
+        self.other_endpoints = other_endpoints
+        self.result_analyzer = result_analyzer
+
+    def run_workloads(
+        self,
+    ) -> RegressionOutcome:
+        overall_regression_outcome = RegressionOutcome()
+
+        for workload_cls in self.config.workload_classes:
+            assert issubclass(
+                workload_cls, Workload
+            ), f"{workload_cls} is not a Workload"
+            self.run_workload_for_all_endpoints(
+                workload_cls,
+                overall_regression_outcome,
+            )
+
+        return overall_regression_outcome
+
+    def run_workload_for_all_endpoints(
+        self,
+        workload_cls: type[Workload],
+        overall_regression_outcome: RegressionOutcome,
+    ):
+        if self.baseline_endpoint is not None:
+            baseline_result = self.run_workload_for_endpoint(
+                self.baseline_endpoint, workload_cls()
+            )
+        else:
+            baseline_result = None
+
+        for other_endpoint in self.other_endpoints:
+            other_endpoint_result = self.run_workload_for_endpoint(
+                other_endpoint, workload_cls()
+            )
+
+            if self.baseline_endpoint is not None and baseline_result is not None:
+                regression_outcome = (
+                    self.result_analyzer.determine_regression_in_workload(
+                        workload_cls.__name__,
+                        self.baseline_endpoint,
+                        other_endpoint,
+                        baseline_result,
+                        other_endpoint_result,
+                    )
+                )
+                overall_regression_outcome.merge(regression_outcome)
+
+    def run_workload_for_endpoint(
+        self,
+        endpoint: Endpoint,
+        workload: Workload,
+    ) -> WorkloadResult:
+        df_totals = pd.DataFrame()
+        df_details = pd.DataFrame()
+
+        concurrencies = self._get_concurrencies()
+        print(f"Concurrencies: {concurrencies}")
+
+        for concurrency in concurrencies:
+            df_total, df_detail = self.run_workload_for_endpoint_with_concurrency(
+                endpoint,
+                workload,
+                concurrency,
+                self.config.get_count_for_concurrency(concurrency),
+            )
+            df_totals = pd.concat([df_totals, df_total], ignore_index=True)
+            df_details = pd.concat([df_details, df_detail], ignore_index=True)
+
+            endpoint_name = endpoint.name()
+            pathlib.Path(paths.endpoint_dir(endpoint_name)).mkdir(
+                parents=True, exist_ok=True
+            )
+
+            df_totals.to_csv(paths.df_totals_csv(endpoint_name, workload))
+            df_details.to_csv(paths.df_details_csv(endpoint_name, workload))
+
+        return WorkloadResult(workload, df_totals, df_details)
+
+    def run_workload_for_endpoint_with_concurrency(
+        self,
+        endpoint: Endpoint,
+        workload: Workload,
+        concurrency: int,
+        count: int,
+    ) -> tuple[pd.DataFrame, pd.DataFrame]:
+        print(
+            f"Preparing benchmark for workload '{workload.name()}' at concurrency {concurrency} ..."
+        )
+        endpoint.up()
+
+        init_sqls = self.schema.init_sqls()
+
+        init_conn = endpoint.sql_connection()
+        init_conn.autocommit = True
+        init_cursor = init_conn.cursor()
+        for init_sql in init_sqls:
+            print(init_sql)
+            init_cursor.execute(init_sql.encode("utf8"))
+
+        print(
+            f"Creating a cursor pool with {concurrency} entries against endpoint: {endpoint.url()}"
+        )
+        cursor_pool = self._create_cursor_pool(concurrency, endpoint)
+
+        print(
+            f"Benchmarking workload '{workload.name()}' at concurrency {concurrency} ..."
+        )
+        operations = workload.operations()
+
+        global next_worker_id
+        next_worker_id = 0
+        local = threading.local()
+        lock = threading.Lock()
+
+        start = time.time()
+        with futures.ThreadPoolExecutor(
+            concurrency, initializer=self.initialize_worker, initargs=(local, lock)
+        ) as executor:
+            measurements = executor.map(
+                self.execute_operation,
+                [
+                    (
+                        workload,
+                        concurrency,
+                        local,
+                        cursor_pool,
+                        operations[i % len(operations)],
+                    )
+                    for i in range(count)
+                ],
+            )
+        wallclock_total = time.time() - start
+
+        df_detail = pd.DataFrame(measurements)
+        print("Best and worst individual measurements:")
+        print(df_detail.sort_values(by=[df_details_cols.WALLCLOCK]))
+
+        print(
+            f"concurrency: {concurrency}; wallclock_total: {wallclock_total}; tps = {count/wallclock_total}"
+        )
+
+        df_total = pd.DataFrame(
+            [
+                {
+                    df_totals_cols.CONCURRENCY: concurrency,
+                    df_totals_cols.WALLCLOCK: wallclock_total,
+                    df_totals_cols.WORKLOAD: type(workload).__name__,
+                    df_totals_cols.COUNT: count,
+                    df_totals_cols.TPS: count / wallclock_total,
+                    df_totals_cols.MEAN_TX_DURATION: df_detail[
+                        df_totals_cols.WALLCLOCK
+                    ].mean(),
+                    df_totals_cols.MEDIAN_TX_DURATION: df_detail[
+                        df_totals_cols.WALLCLOCK
+                    ].median(),
+                    df_totals_cols.MIN_TX_DURATION: df_detail[
+                        df_totals_cols.WALLCLOCK
+                    ].min(),
+                    df_totals_cols.MAX_TX_DURATION: df_detail[
+                        df_totals_cols.WALLCLOCK
+                    ].max(),
+                }
+            ]
+        )
+
+        return df_total, df_detail
+
+    def execute_operation(
+        self, args: tuple[Workload, int, threading.local, list[Cursor], Operation]
+    ) -> dict[str, Any]:
+        workload, concurrency, local, cursor_pool, operation = args
+        assert (
+            len(cursor_pool) >= local.worker_id + 1
+        ), f"len(cursor_pool) is {len(cursor_pool)} but local.worker_id is {local.worker_id}"
+        cursor = cursor_pool[local.worker_id]
+
+        start = time.time()
+        operation.execute(cursor)
+        wallclock = time.time() - start
+
+        return {
+            df_details_cols.CONCURRENCY: concurrency,
+            df_details_cols.WALLCLOCK: wallclock,
+            df_details_cols.OPERATION: type(operation).__name__,
+            df_details_cols.WORKLOAD: type(workload).__name__,
+        }
+
+    def initialize_worker(self, local: threading.local, lock: threading.Lock):
+        """Give each other worker thread a unique ID"""
+        lock.acquire()
+        global next_worker_id
+        local.worker_id = next_worker_id
+        next_worker_id = next_worker_id + 1
+        lock.release()
+
+    def _get_concurrencies(self) -> list[int]:
+        concurrencies: list[int] = [
+            round(self.config.exponent_base**c) for c in range(0, 1024)
+        ]
+        concurrencies = sorted(set(concurrencies))
+        return [
+            c
+            for c in concurrencies
+            if self.config.min_concurrency <= c <= self.config.max_concurrency
+        ]
+
+    def _create_cursor_pool(self, concurrency: int, endpoint: Endpoint) -> list[Cursor]:
+        connect_sqls = self.schema.connect_sqls()
+
+        cursor_pool = []
+        for i in range(concurrency):
+            conn = endpoint.sql_connection()
+            conn.autocommit = True
+            cursor = conn.cursor()
+            for connect_sql in connect_sqls:
+                cursor.execute(connect_sql.encode("utf8"))
+            cursor_pool.append(cursor)
+
+        return cursor_pool
--- test/scalability/mzcompose.py
+++ test/scalability/mzcompose.py
@@ -8,23 +8,17 @@
 # by the Apache License, Version 2.0.
 
 import argparse
-import pathlib
 import sys
-import threading
-import time
-from concurrent import futures
-from math import floor, sqrt
-from typing import Any
 
 import pandas as pd
 from jupyter_core.command import main as jupyter_core_command_main
-from psycopg import Cursor
 
 from materialize import benchmark_utils, buildkite, spawn
 from materialize.mzcompose.composition import Composition, WorkflowArgumentParser
 from materialize.mzcompose.services.materialized import Materialized
 from materialize.mzcompose.services.postgres import Postgres
-from materialize.scalability.df import df_details_cols, df_totals_cols, paths
+from materialize.scalability.benchmark_config import BenchmarkConfiguration
+from materialize.scalability.df import paths
 from materialize.scalability.endpoint import Endpoint
 from materialize.scalability.endpoints import (
     MaterializeContainer,
@@ -33,13 +27,12 @@ from materialize.scalability.endpoints import (
     PostgresContainer,
     endpoint_name_to_description,
 )
-from materialize.scalability.operation import Operation
 from materialize.scalability.regression import RegressionOutcome
 from materialize.scalability.result_analyzer import ResultAnalyzer
 from materialize.scalability.result_analyzers import DefaultResultAnalyzer
 from materialize.scalability.schema import Schema, TransactionIsolation
 from materialize.scalability.workload import Workload, WorkloadSelfTest
-from materialize.scalability.workload_result import WorkloadResult
+from materialize.scalability.workload_executor import WorkloadExecutor
 from materialize.scalability.workloads import *  # noqa: F401 F403
 from materialize.scalability.workloads_test import *  # noqa: F401 F403
 from materialize.util import all_subclasses
@@ -48,168 +41,6 @@ SERVICES = [
     Materialized(image="materialize/materialized:latest", sanity_restart=False),
     Postgres(),
 ]
-
-
-def initialize_worker(local: threading.local, lock: threading.Lock):
-    """Give each other worker thread a unique ID"""
-    lock.acquire()
-    global next_worker_id
-    local.worker_id = next_worker_id
-    next_worker_id = next_worker_id + 1
-    lock.release()
-
-
-def execute_operation(
-    args: tuple[Workload, int, threading.local, list[Cursor], Operation]
-) -> dict[str, Any]:
-    workload, concurrency, local, cursor_pool, operation = args
-    assert (
-        len(cursor_pool) >= local.worker_id + 1
-    ), f"len(cursor_pool) is {len(cursor_pool)} but local.worker_id is {local.worker_id}"
-    cursor = cursor_pool[local.worker_id]
-
-    start = time.time()
-    operation.execute(cursor)
-    wallclock = time.time() - start
-
-    return {
-        df_details_cols.CONCURRENCY: concurrency,
-        df_details_cols.WALLCLOCK: wallclock,
-        df_details_cols.OPERATION: type(operation).__name__,
-        df_details_cols.WORKLOAD: type(workload).__name__,
-    }
-
-
-def run_with_concurrency(
-    c: Composition,
-    endpoint: Endpoint,
-    schema: Schema,
-    workload: Workload,
-    concurrency: int,
-    count: int,
-) -> tuple[pd.DataFrame, pd.DataFrame]:
-    print(
-        f"Preparing benchmark for workload '{workload.name()}' at concurrency {concurrency} ..."
-    )
-    endpoint.up()
-
-    init_sqls = schema.init_sqls()
-
-    init_conn = endpoint.sql_connection()
-    init_conn.autocommit = True
-    init_cursor = init_conn.cursor()
-    for init_sql in init_sqls:
-        print(init_sql)
-        init_cursor.execute(init_sql.encode("utf8"))
-
-    connect_sqls = schema.connect_sqls()
-
-    print(
-        f"Creating a cursor pool with {concurrency} entries against endpoint: {endpoint.url()}"
-    )
-    cursor_pool = []
-    for i in range(concurrency):
-        conn = endpoint.sql_connection()
-        conn.autocommit = True
-        cursor = conn.cursor()
-        for connect_sql in connect_sqls:
-            cursor.execute(connect_sql.encode("utf8"))
-        cursor_pool.append(cursor)
-
-    print(f"Benchmarking workload '{workload.name()}' at concurrency {concurrency} ...")
-    operations = workload.operations()
-
-    global next_worker_id
-    next_worker_id = 0
-    local = threading.local()
-    lock = threading.Lock()
-
-    start = time.time()
-    with futures.ThreadPoolExecutor(
-        concurrency, initializer=initialize_worker, initargs=(local, lock)
-    ) as executor:
-        measurements = executor.map(
-            execute_operation,
-            [
-                (
-                    workload,
-                    concurrency,
-                    local,
-                    cursor_pool,
-                    operations[i % len(operations)],
-                )
-                for i in range(count)
-            ],
-        )
-    wallclock_total = time.time() - start
-
-    df_detail = pd.DataFrame(measurements)
-    print("Best and worst individual measurements:")
-    print(df_detail.sort_values(by=[df_details_cols.WALLCLOCK]))
-
-    print(
-        f"concurrency: {concurrency}; wallclock_total: {wallclock_total}; tps = {count/wallclock_total}"
-    )
-
-    df_total = pd.DataFrame(
-        [
-            {
-                df_totals_cols.CONCURRENCY: concurrency,
-                df_totals_cols.WALLCLOCK: wallclock_total,
-                df_totals_cols.WORKLOAD: type(workload).__name__,
-                df_totals_cols.COUNT: count,
-                df_totals_cols.TPS: count / wallclock_total,
-                "mean_t_dur": df_detail["wallclock"].mean(),
-                "median_t_dur": df_detail["wallclock"].median(),
-                "min_t_dur": df_detail["wallclock"].min(),
-                "max_t_dur": df_detail["wallclock"].max(),
-            }
-        ]
-    )
-
-    return (df_total, df_detail)
-
-
-def run_workload(
-    c: Composition,
-    args: argparse.Namespace,
-    endpoint: Endpoint,
-    schema: Schema,
-    workload: Workload,
-) -> WorkloadResult:
-    df_totals = pd.DataFrame()
-    df_details = pd.DataFrame()
-
-    concurrencies: list[int] = [round(args.exponent_base**c) for c in range(0, 1024)]
-    concurrencies = sorted(set(concurrencies))
-    concurrencies = [
-        c
-        for c in concurrencies
-        if c >= args.min_concurrency and c <= args.max_concurrency
-    ]
-    print(f"Concurrencies: {concurrencies}")
-
-    for concurrency in concurrencies:
-        df_total, df_detail = run_with_concurrency(
-            c,
-            endpoint,
-            schema,
-            workload,
-            concurrency,
-            floor(args.count * sqrt(concurrency)),
-        )
-        df_totals = pd.concat([df_totals, df_total], ignore_index=True)
-        df_details = pd.concat([df_details, df_detail], ignore_index=True)
-
-        endpoint_name = endpoint.name()
-        pathlib.Path(paths.endpoint_dir(endpoint_name)).mkdir(
-            parents=True, exist_ok=True
-        )
-
-        df_totals.to_csv(paths.df_totals_csv(endpoint_name, workload))
-        df_details.to_csv(paths.df_details_csv(endpoint_name, workload))
-
-    return WorkloadResult(workload, df_totals, df_details)
 
 
 def workflow_default(c: Composition, parser: WorkflowArgumentParser) -> None:
@@ -293,14 +124,55 @@ def workflow_default(c: Composition, parser: WorkflowArgumentParser) -> None:
     parser.add_argument("--cluster-name", type=str, help="Cluster to SET CLUSTER to")
 
     args = parser.parse_args()
+    regression_against_target = args.regression_against
 
+    validate_and_adjust_targets(args, regression_against_target)
+
+    print(f"Targets: {args.target}")
+    print(f"Checking regression against: {regression_against_target}")
+
+    baseline_endpoint, other_endpoints = get_baseline_and_other_endpoints(
+        c, args, regression_against_target
+    )
+    workload_classes = get_workload_classes(args)
+
+    schema = Schema(
+        create_index=args.create_index,
+        transaction_isolation=args.transaction_isolation,
+        cluster_name=args.cluster_name,
+        object_count=args.object_count,
+    )
+
+    result_analyzer = create_result_analyzer(args)
+
+    workload_names = [workload_cls.__name__ for workload_cls in workload_classes]
+    df_workloads = pd.DataFrame(data={"workload": workload_names})
+    df_workloads.to_csv(paths.workloads_csv())
+
+    config = BenchmarkConfiguration(
+        workload_classes=workload_classes,
+        exponent_base=args.exponent_base,
+        min_concurrency=args.min_concurrency,
+        max_concurrency=args.max_concurrency,
+        count=args.count,
+    )
+
+    overall_regression_outcome = WorkloadExecutor(
+        config, schema, baseline_endpoint, other_endpoints, result_analyzer
+    ).run_workloads()
+
+    report_regression_result(baseline_endpoint, overall_regression_outcome)
+
+
+def validate_and_adjust_targets(
+    args: argparse.Namespace, regression_against_target: str
+) -> None:
     if args.materialize_url is not None and "remote" not in args.target:
         assert False, "--materialize_url requires --target=remote"
 
     if len(args.target) == 0:
         args.target = ["HEAD"]
 
-    regression_against_target = args.regression_against
     if (
         regression_against_target is not None
         and regression_against_target not in args.target
@@ -308,9 +180,10 @@ def workflow_default(c: Composition, parser: WorkflowArgumentParser) -> None:
         print(f"Adding {regression_against_target} as target")
         args.target.append(regression_against_target)
 
-    print(f"Targets: {args.target}")
-    print(f"Checking regression against: {regression_against_target}")
 
+def get_baseline_and_other_endpoints(
+    c: Composition, args: argparse.Namespace, regression_against_target: str
+) -> tuple[Endpoint | None, list[Endpoint]]:
     baseline_endpoint: Endpoint | None = None
     other_endpoints: list[Endpoint] = []
     for i, target in enumerate(args.target):
@@ -340,7 +213,11 @@ def workflow_default(c: Composition, parser: WorkflowArgumentParser) -> None:
         else:
             other_endpoints.append(endpoint)
 
-    workloads = (
+    return baseline_endpoint, other_endpoints
+
+
+def get_workload_classes(args: argparse.Namespace) -> list[type[Workload]]:
+    return (
         [globals()[workload] for workload in args.workload]
         if args.workload
         else [
@@ -349,49 +226,6 @@ def workflow_default(c: Composition, parser: WorkflowArgumentParser) -> None:
             if not issubclass(workload_cls, WorkloadSelfTest)
         ]
     )
-
-    schema = Schema(
-        create_index=args.create_index,
-        transaction_isolation=args.transaction_isolation,
-        cluster_name=args.cluster_name,
-        object_count=args.object_count,
-    )
-
-    result_analyzer = create_result_analyzer(args)
-
-    workload_names = [workload.__name__ for workload in workloads]
-    df_workloads = pd.DataFrame(data={"workload": workload_names})
-    df_workloads.to_csv(paths.workloads_csv())
-
-    overall_regression_outcome = RegressionOutcome()
-
-    for workload in workloads:
-        assert issubclass(workload, Workload), f"{workload} is not a Workload"
-        workload_name = workload.__name__
-
-        if baseline_endpoint is not None:
-            baseline_result = run_workload(
-                c, args, baseline_endpoint, schema, workload()
-            )
-        else:
-            baseline_result = None
-
-        for other_endpoint in other_endpoints:
-            other_endpoint_result = run_workload(
-                c, args, other_endpoint, schema, workload()
-            )
-
-            if baseline_endpoint is not None and baseline_result is not None:
-                regression_outcome = result_analyzer.determine_regression_in_workload(
-                    workload_name,
-                    baseline_endpoint,
-                    other_endpoint,
-                    baseline_result,
-                    other_endpoint_result,
-                )
-                overall_regression_outcome.merge(regression_outcome)
-
-    report_regression_result(baseline_endpoint, overall_regression_outcome)
 
 
 def report_regression_result(
