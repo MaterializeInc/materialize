34159c08d2d25e73a07ef45b3fe222131a049679
Moritz Hoffmann <mh@materialize.com>
2023-11-09 15:16:43 -0500

    Enable lgalloc in CI
    Signed-off-by: Moritz Hoffmann <mh@materialize.com>

--- misc/python/materialize/mzcompose/__init__.py
+++ misc/python/materialize/mzcompose/__init__.py
@@ -50,6 +50,7 @@ DEFAULT_SYSTEM_PARAMETERS = {
     "persist_pubsub_client_enabled": "true",
     "persist_stats_audit_percent": "100",
     "persist_batch_delete_enabled": "true",
+    "enable_columnation_lgalloc": "true",
     "enable_rbac_checks": "true",
     "enable_try_parse_monotonic_iso8601_timestamp": "true",
     "enable_dangerous_functions": "true",
