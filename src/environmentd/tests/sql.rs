03d2e4677d3a7b70851318a8e5db887118540b68
Parker Timmerman <parker.timmerman@materialize.com>
2023-11-13 15:59:42 -0500

    start, add retry around flaky portion of test

--- src/environmentd/tests/sql.rs
+++ src/environmentd/tests/sql.rs
@@ -2905,11 +2905,15 @@ fn test_auto_run_on_introspection_feature_enabled() {
         .unwrap();
     assert_introspection_notice(true);
 
-    let _row = client
-        .query_one(
-            "SELECT * FROM mz_internal.mz_cluster_replica_heartbeats LIMIT 1",
-            &[],
-        )
+    // We add a retry here since it might take a moment to get a replica heartbeat.
+    let _row = Retry::default()
+        .max_tries(5)
+        .retry(|_state| {
+            client.query_one(
+                "SELECT * FROM mz_internal.mz_cluster_replica_heartbeats LIMIT 1",
+                &[],
+            )
+        })
         .unwrap();
     assert_introspection_notice(true);
 
