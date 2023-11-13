b02c1b0fdd03e648a30bd88b051b76bbd2b76117
Rainer Niedermayr <rainer@materialize.com>
2023-11-13 13:59:17 +0100

    ci: rework logic to determine comparison target

--- misc/python/materialize/docker.py
+++ misc/python/materialize/docker.py
@@ -14,11 +14,51 @@ from materialize import buildkite, git
 
 
 def resolve_ancestor_image_tag_for_comparison() -> str:
-    if buildkite.is_in_buildkite() and not buildkite.is_in_pull_request():
-        latest_version = git.get_latest_version()
-        print(f"Using latest version ({latest_version}) for other comparison")
-        return f"v{latest_version}"
+    image_tag, context = _resolve_ancestor_image_tag_for_comparison()
+    print(f"Using {image_tag} as image tag for comparison (context: {context})")
+    return image_tag
+
+
+def _resolve_ancestor_image_tag_for_comparison() -> tuple[str, str]:
+    if buildkite.is_in_buildkite():
+        if buildkite.is_in_pull_request():
+            # return the merge base
+            common_ancestor_commit = buildkite.get_merge_base()
+            return f"devel-{common_ancestor_commit}", "merge base of pull request"
+        elif git.is_on_release_version():
+            # return the previous release
+            tagged_release_version = git.get_tagged_release_version()
+            assert tagged_release_version is not None
+            previous_release_version = git.get_previous_version(tagged_release_version)
+            return (
+                f"v{previous_release_version}",
+                f"previous release because on release branch {tagged_release_version}",
+            )
+        else:
+            # return the latest release
+            latest_version = git.get_latest_version()
+            return (
+                f"v{latest_version}",
+                "latest release because not in a pull request and not on a release branch",
+            )
     else:
-        common_ancestor_commit = buildkite.get_merge_base()
-        print(f"Using merge base ({common_ancestor_commit}) for other comparison")
-        return f"devel-{common_ancestor_commit}"
+        if git.is_on_release_version():
+            # return the previous release
+            tagged_release_version = git.get_tagged_release_version()
+            assert tagged_release_version is not None
+            previous_release_version = git.get_previous_version(tagged_release_version)
+            return (
+                f"v{previous_release_version}",
+                f"previous release because on local release branch {tagged_release_version}",
+            )
+        elif git.is_on_main_branch():
+            # return the latest release
+            latest_version = git.get_latest_version()
+            return f"v{latest_version}", "latest release because on local main branch"
+        else:
+            # return the merge base
+            common_ancestor_commit = buildkite.get_merge_base()
+            return (
+                f"devel-{common_ancestor_commit}",
+                "merge base of local non-main branch",
+            )
