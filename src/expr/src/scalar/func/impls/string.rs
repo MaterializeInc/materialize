14cd2a41726f72ef7ca62cdf53a55fb297c243d6
Parker Timmerman <parker.timmerman@materialize.com>
2023-11-16 10:46:26 -0500

    start, return error message from quote_ident if ident is too long

--- src/expr/src/scalar/func/impls/string.rs
+++ src/expr/src/scalar/func/impls/string.rs
@@ -1043,8 +1043,10 @@ impl LazyUnaryFunc for QuoteIdent {
             return Ok(Datum::Null);
         }
         let v = d.unwrap_str();
-        // Note: using unchecked here is okay because we go right back to a string.
-        let i = mz_sql_parser::ast::Ident::new_unchecked(v);
+        let i = mz_sql_parser::ast::Ident::new(v).map_err(|err| EvalError::InvalidIdentifier {
+            ident: v.to_string(),
+            detail: Some(err.to_string()),
+        })?;
         let r = temp_storage.push_string(i.to_string());
 
         Ok(Datum::String(r))
