efe55e0646f1a11616e61db699b7f38aa81a1b72
Nikhil Benesch <nikhil.benesch@gmail.com>
2023-11-24 17:22:11 -0500

    sql: mark ALTER CONNECTION TODO as resolved
    `ALTER CONNECTION` correctly disallows changing the progress topic of a
    Kafka connection, so mark the TODO as resolved.

--- src/sql/src/plan/statement/ddl.rs
+++ src/sql/src/plan/statement/ddl.rs
@@ -2497,9 +2497,11 @@ fn kafka_sink_builder(
     sink_from: GlobalId,
 ) -> Result<StorageSinkConnection<ReferencedConnection>, PlanError> {
     let item = scx.get_item_by_resolved_name(&connection)?;
-    // Get Kafka connection + progress topic
+    // Get Kafka connection + progress topic.
     //
-    // TODO: In ALTER CONNECTION, the progress topic must be immutable
+    // We don't need to worry about the progress topic changing after sink
+    // creation as `ALTER CONNECTION` is not permitted to change a sink's
+    // progress topic.
     let (connection, progress_topic) = match item.connection()? {
         Connection::Kafka(connection) => {
             let id = item.id();
