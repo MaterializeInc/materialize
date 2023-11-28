849bd6cd7f826ffb624265dd2d54eff4249c236e
Joseph Koshakow <koshy44@gmail.com>
2023-11-28 11:14:45 -0500

    catalog: Fixup persist catalog metrics
    Resolves #23465

--- src/catalog/src/durable/impls/persist/metrics.rs
+++ src/catalog/src/durable/impls/persist/metrics.rs
@@ -11,14 +11,14 @@
 
 use mz_ore::metric;
 use mz_ore::metrics::{IntCounter, MetricsRegistry};
-use prometheus::{Counter, IntCounterVec};
+use prometheus::Counter;
 
 #[derive(Debug, Clone)]
 pub struct Metrics {
     pub transactions: IntCounter,
-    pub transaction_commit_errors: IntCounterVec,
+    pub transaction_commit_errors: IntCounter,
     pub transaction_commit_latency_duration_seconds: Counter,
-    pub transactions_committed: IntCounter,
+    pub transaction_commits_initiated: IntCounter,
     pub snapshot_latency_duration_seconds: Counter,
     pub snapshots_taken: IntCounter,
     pub sync_latency_duration_seconds: Counter,
@@ -30,36 +30,36 @@ impl Metrics {
     pub fn new(registry: &MetricsRegistry) -> Self {
         Self {
             transactions: registry.register(metric!(
-                name: "catalog_transactions",
+                name: "mz_catalog_transactions",
                 help: "Total number of started transactions.",
             )),
             transaction_commit_errors: registry.register(metric!(
-                name: "catalog_transaction_errors",
+                name: "mz_catalog_transaction_errors",
                 help: "Total number of transaction errors.",
                 var_labels: ["cause"],
             )),
             transaction_commit_latency_duration_seconds: registry.register(metric!(
-                name: "catalog_transaction_latency_seconds",
+                name: "mz_catalog_transaction_latency_seconds",
                 help: "Total latency for durable catalog transactions.",
             )),
-            transactions_committed: registry.register(metric!(
-                name: "catalog_transactions_committed",
-                help: "Count of transactions committed.",
+            transaction_commits_initiated: registry.register(metric!(
+                name: "mz_catalog_transaction_commits_initiated",
+                help: "Count of transaction commits that were initiated, this includes ones that ended in successes or failures.",
             )),
             snapshot_latency_duration_seconds: registry.register(metric!(
-                name: "catalog_snapshot_latency_seconds",
+                name: "mz_catalog_snapshot_latency_seconds",
                 help: "Total latency for fetching a snapshot of the durable catalog.",
             )),
             snapshots_taken: registry.register(metric!(
-                name: "catalog_snapshots_taken",
+                name: "mz_catalog_snapshots_taken",
                 help: "Count of snapshots taken.",
             )),
             sync_latency_duration_seconds: registry.register(metric!(
-                name: "catalog_sync_latency_seconds",
+                name: "mz_catalog_sync_latency_seconds",
                 help: "Total latency for syncing the in-memory state of the durable catalog with the persisted contents.",
             )),
             syncs: registry.register(metric!(
-                name: "catalog_syncs",
+                name: "mz_catalog_syncs",
                 help: "Count of catalog syncs.",
             )),
         }
