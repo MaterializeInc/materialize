90e675ffd22b939904eec67452eaaca229c58a86
Sean Loiselle <sean@materialize.com>
2023-12-09 07:49:28 -0700

    adapter: add ast_rewrite_postgres_source_timeline_id_0_80_0

--- src/adapter/src/catalog/migrate.rs
+++ src/adapter/src/catalog/migrate.rs
@@ -16,6 +16,7 @@ use mz_ore::now::{EpochMillis, NowFn};
 use mz_sql::ast::display::AstDisplay;
 use mz_sql::ast::Raw;
 use mz_storage_types::connections::ConnectionContext;
+use mz_storage_types::sources::GenericSourceConnection;
 use semver::Version;
 use tracing::info;
 
@@ -53,7 +54,7 @@ pub(crate) async fn migrate(
     state: &CatalogState,
     tx: &mut Transaction<'_>,
     _now: NowFn,
-    _connection_context: &ConnectionContext,
+    connection_context: &ConnectionContext,
 ) -> Result<(), anyhow::Error> {
     let catalog_version = tx.get_catalog_content_version();
     let catalog_version = match catalog_version {
@@ -95,6 +96,9 @@ pub(crate) async fn migrate(
     // Each migration should be a function that takes `tx` and `conn_cat` as
     // input and stages arbitrary transformations to the catalog on `tx`.
 
+    // This function blocks for at most 1 second per PG source.
+    ast_rewrite_postgres_source_timeline_id_0_80_0(tx, &conn_cat, connection_context).await?;
+
     info!(
         "migration from catalog version {:?} complete",
         catalog_version
@@ -111,6 +115,120 @@ pub(crate) async fn migrate(
 //
 // Please include the adapter team on any code reviews that add or edit
 // migrations.
+
+/// Attempt to connect to the upstream PostgreSQL server to get the
+/// publication's timeline ID.
+async fn ast_rewrite_postgres_source_timeline_id_0_80_0(
+    txn: &mut Transaction<'_>,
+    conn_catalog: &ConnCatalog<'_>,
+    connection_context: &ConnectionContext,
+) -> Result<(), anyhow::Error> {
+    use mz_postgres_util::PostgresError;
+    use mz_proto::RustType;
+    use mz_sql::catalog::SessionCatalog;
+    use mz_sql_parser::ast::{PgConfigOption, PgConfigOptionName, Value, WithOptionValue};
+    use mz_storage_types::connections::inline::IntoInlineConnection;
+    use mz_storage_types::connections::PostgresConnection;
+    use mz_storage_types::sources::{
+        PostgresSourcePublicationDetails, ProtoPostgresSourcePublicationDetails,
+    };
+    use prost::Message;
+
+    let mut updated_items = BTreeMap::new();
+    for mut item in txn.loaded_items() {
+        let catalog_item = conn_catalog.get_item(&item.id);
+        if let Ok(Some(source)) = catalog_item.source_desc() {
+            let pg_conn = match &source.connection {
+                GenericSourceConnection::Postgres(pg_conn) => {
+                    pg_conn.clone().into_inline_connection(&conn_catalog)
+                }
+                _ => continue,
+            };
+
+            let mut pg_source_create_stmt =
+                match mz_sql::parse::parse(&item.create_sql)?.into_element().ast {
+                    mz_sql::ast::Statement::CreateSource(c) => c,
+                    _ => unreachable!("PG sources are created w/ CreateSource"),
+                };
+
+            let source_options = match &mut pg_source_create_stmt.connection {
+                mz_sql::ast::CreateSourceConnection::Postgres {
+                    connection: _,
+                    options,
+                } => options,
+                _ => unreachable!("pg source must be pg conn"),
+            };
+
+            let details_pos = source_options
+                .iter()
+                .position(|o| o.name == PgConfigOptionName::Details)
+                .expect("pg source must have options");
+
+            let details = source_options.swap_remove(details_pos);
+            let details = match details.value.expect("details contains value") {
+                WithOptionValue::Value(Value::String(details)) => details,
+                _ => unreachable!("details encoded as string value"),
+            };
+
+            let details =
+                hex::decode(details).expect("PostgresSourcePublicationDetails must be decodable");
+            let mut details: ProtoPostgresSourcePublicationDetails =
+                ProtoPostgresSourcePublicationDetails::decode(&*details)
+                    .expect("corrupted PostgresSourcePublicationDetails");
+
+            if details.timeline_id.is_some() {
+                continue;
+            }
+
+            async fn get_timeline(
+                connection_context: &ConnectionContext,
+                connection: PostgresConnection,
+            ) -> Result<u64, PostgresError> {
+                let config = connection
+                    .config(&*connection_context.secrets_reader)
+                    .await?;
+
+                let replication_client = config
+                    .connect_replication(&connection_context.ssh_tunnel_manager)
+                    .await?;
+
+                mz_postgres_util::get_timeline_id(&replication_client).await
+            }
+
+            let result: Result<Result<u64, PostgresError>, tokio::time::error::Elapsed> =
+                tokio::time::timeout(
+                    std::time::Duration::from_secs(1),
+                    get_timeline(connection_context, pg_conn.connection),
+                )
+                .await;
+
+            let timeline_id = match result {
+                Ok(Ok(timeline_id)) => timeline_id,
+                _ => {
+                    tracing::info!("could not determine timeline ID for PG source {}", item.id);
+                    continue;
+                }
+            };
+
+            details.timeline_id = Some(timeline_id);
+
+            let publication_details = PostgresSourcePublicationDetails::from_proto(details)
+                .expect("proto to rust conversion succeeds for PostgresSourcePublicationDetails");
+
+            source_options.push(PgConfigOption {
+                name: PgConfigOptionName::Details,
+                value: Some(WithOptionValue::Value(Value::String(hex::encode(
+                    publication_details.into_proto().encode_to_vec(),
+                )))),
+            });
+
+            item.create_sql = pg_source_create_stmt.to_ast_string_stable();
+            updated_items.insert(item.id, item);
+        }
+    }
+    txn.update_items(updated_items)?;
+    Ok(())
+}
 
 fn _add_to_audit_log(
     tx: &mut Transaction,
