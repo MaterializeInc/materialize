0d2d15e9065347bf79b14cd897897a6cd74b52fe
Rainer Niedermayr <rainer@materialize.com>
2023-12-11 11:47:55 +0100

    lint: split lint file into multiple files

---
+++ ci/test/lint-main/after/check-no-diff.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Copyright Materialize, Inc. and contributors. All rights reserved.
+#
+# Use of this software is governed by the Business Source License
+# included in the LICENSE file at the root of this repository.
+#
+# As of the Change Date specified in that file, in accordance with
+# the Business Source License, use of this software will be governed
+# by the Apache License, Version 2.0.
+
+set -euo pipefail
+
+cd "$(dirname "$0")/../../../.."
+
+. misc/shlib/shlib.bash
+
+try git diff --compact-summary --exit-code
+
+try_status_report
