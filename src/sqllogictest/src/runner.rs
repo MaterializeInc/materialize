MaterializeInc/materialize

commit c502b5db60a601c87d84facd6c3d80a83edce39f

sqllogictest: rewrite whitespace as '␠' in strings
Change the `rewrite_file` command to insert '␠' which is the inverse of
what happens in `parse_query` when `mode cockroach` is selected.

src/sqllogictest/src/runner.rs
@@ -40,6 +40,7 @@ use bytes::BytesMut;
 use chrono::{DateTime, NaiveDateTime, NaiveTime, Utc};
 use fallible_iterator::FallibleIterator;
 use futures::sink::SinkExt;
+use itertools::Itertools;
 use md5::{Digest, Md5};
 use mz_controller::ControllerConfig;
 use mz_environmentd::CatalogConfig;
@@ -1933,7 +1934,7 @@ pub async fn rewrite_file(runner: &mut Runner<'_>, filename: &Path) -> Result<()
                     if i != 0 {
                         buf.append("\n");
                     }
-                    buf.append(&row.join("  "));
+                    buf.append(&row.iter().map(|col| col.replace(' ', "␠")).join("  "));
                 }
                 // In standard mode, output each value on its own line,
                 // and ignore row boundaries.

commit e512be97307f28adb4c2f3e4fae7f67c98c9bcfd

sqllogictest: rewrite whitespace as '␠' in strings (fixup)
Don't replace ' ' with '␠' in rows with only one column as suggesed by
Gábor. I should have double-checked this before merging.

Fixes a regression introduced by #23644.

src/sqllogictest/src/runner.rs
@@ -1933,7 +1933,12 @@ pub async fn rewrite_file(runner: &mut Runner<'_>, filename: &Path) -> Result<()
                     if i != 0 {
                         buf.append("\n");
                     }
-                    buf.append(&row.iter().map(|col| col.replace(' ', "␠")).join("  "));
+
+                    if row.len() <= 1 {
+                        buf.append(&row.iter().join("  "));
+                    } else {
+                        buf.append(&row.iter().map(|col| col.replace(' ', "␠")).join("  "));
+                    }
                 }
                 // In standard mode, output each value on its own line,
                 // and ignore row boundaries.