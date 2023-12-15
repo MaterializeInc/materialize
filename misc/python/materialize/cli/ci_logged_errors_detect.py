d9b443e70c2c761b3123fb7af1aa9522a28c5d2b
Philip Stoev <pstoev@materialize.com>
2023-12-15 12:09:47 +0100

    cloudtest: Enable soft assertions
    This brings cloudtest in line with mzcompose
    
    Relates to #23921

--- misc/python/materialize/cli/ci_logged_errors_detect.py
+++ misc/python/materialize/cli/ci_logged_errors_detect.py
@@ -96,6 +96,7 @@ IGNORE_RE = re.compile(
     | cannot\ load\ unknown\ system\ parameter\ from\ catalog\ storage(\ to\ set\ (default|configured)\ parameter)?
     | internal\ error:\ no\ AWS\ external\ ID\ prefix\ configured
     | failed\ writing\ row\ to\ mz_aws_connections.*no\ AWS\ external\ ID\ prefix\ configured
+    | failed\ writing\ row\ to\ mz_aws_connections.*no\ AWS\ connection\ role\ configured
     )
     """,
     re.VERBOSE | re.MULTILINE,
