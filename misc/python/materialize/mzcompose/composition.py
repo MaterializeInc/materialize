2c258d86d7a06318776b6fe33deba93a102c4a9d
Philip Stoev <pstoev@materialize.com>
2023-12-18 12:56:46 +0100

    rqg: Initial integration of RQG in Buildkite
    - Build a dedicated RQG container
    - Allow workloads to be specified as Python objects
    - Load either the simple dataset of the dbt2 dataset as needed
    - Put two workloads in the CI

--- misc/python/materialize/mzcompose/composition.py
+++ misc/python/materialize/mzcompose/composition.py
@@ -609,6 +609,7 @@ class Composition:
         stdin: str | None = None,
         check: bool = True,
         workdir: str | None = None,
+        env_extra: dict[str, str] = {},
     ) -> subprocess.CompletedProcess:
         """Execute a one-off command in a service's running container
 
@@ -626,6 +627,7 @@ class Composition:
             "exec",
             *(["--detach"] if detach else []),
             *(["--workdir", workdir] if workdir else []),
+            *(f"-e{k}={v}" for k, v in env_extra.items()),
             "-T",
             service,
             *(
