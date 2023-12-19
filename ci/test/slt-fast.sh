1c538baeea62c9723155803f7dfc859a16cfa57c
Daniel Harrison <52528+danhhz@users.noreply.github.com>
2023-12-18 16:11:50 -0800

    [DNM] no -auto-index-selects

--- ci/test/slt-fast.sh
+++ ci/test/slt-fast.sh
@@ -369,5 +369,5 @@ for f in "${tests[@]}"; do
 done
 tests=("${temp[@]}")
 
-sqllogictest -v --auto-index-selects "${tests[@]}" "$@"
+sqllogictest -v "${tests[@]}" "$@"
 sqllogictest -v "${tests_without_views[@]}" "$@"
