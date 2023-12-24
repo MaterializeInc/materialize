61dfede95ca27eef89f1f01ed2839aa9e4b75b5a
Sean Loiselle <sean@materialize.io>
2023-12-24 17:37:25 -0500

    sql+adapter: ensure references to all clusters are not linked

--- src/catalog/src/memory/objects.rs
+++ src/catalog/src/memory/objects.rs
@@ -1928,6 +1928,12 @@ impl mz_sql::catalog::CatalogCluster<'_> for Cluster {
     }
 
     fn linked_object_id(&self) -> Option<GlobalId> {
+        assert!(
+            self.linked_object_id.is_none(),
+            "cluster {} still linked to {:?}",
+            self.id,
+            self.linked_object_id
+        );
         self.linked_object_id
     }
 
