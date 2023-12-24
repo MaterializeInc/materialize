5805f86c1bd5db015f1f8a28ccfcbd7cf3b03f41
Nikhil Benesch <nikhil.benesch@gmail.com>
2023-12-24 11:45:53 -0500

    test/aws: rename to aws-localstack
    To make room for AWS checks that run against the real AWS.

--- test/aws-localstack/mzcompose.py
+++ test/aws-localstack/mzcompose.py
@@ -7,6 +7,8 @@
 # the Business Source License, use of this software will be governed
 # by the Apache License, Version 2.0.
 
+"""Tests of AWS functionality that run against localstack."""
+
 from typing import Any, cast
 
 import boto3
