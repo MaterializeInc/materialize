76b8957ebe9d0218d22090f336163a72422ab405
Joseph Koshakow <koshy44@gmail.com>
2024-01-09 14:38:10 -0500

    Add epoch migration test

--- test/persist-catalog-migration/mzcompose.py
+++ test/persist-catalog-migration/mzcompose.py
@@ -195,3 +195,70 @@ def check_objects(c: Composition, i: int):
     """
         )
     )
+
+
+def workflow_test_epoch_migration(c: Composition) -> None:
+    pass
+    # c.down(destroy_volumes=True)
+    # c.up("minio", "cockroach")
+    #
+    # # Switch to emergency stash catalog so only the stash epoch is incremented.
+    # with c.override(
+    #         Testdrive(no_reset=True),
+    # ):
+    #     c.up("testdrive", persistent=True)
+    #     c.up("materialized")
+    #     c.testdrive(
+    #         input=dedent(
+    #             """
+    #     $ postgres-execute connection=postgres://mz_system@materialized:6877/materialize
+    #     ALTER SYSTEM SET catalog_kind TO 'emergency-stash'
+    # """
+    #         )
+    #     )
+    #     c.down("materialized")
+    #
+    # # Start and stop Materialize with stash multiple times to increment the epoch.
+    # for _ in range(0, 2):
+    #     c.up("materialized")
+    #     c.down("materialized")
+    #
+    # # Switch to persist catalog.
+    # with c.override(
+    #         Testdrive(no_reset=True),
+    # ):
+    #     c.up("testdrive", persistent=True)
+    #     c.up("materialized")
+    #     c.testdrive(
+    #         input=dedent(
+    #             """
+    #     $ postgres-execute connection=postgres://mz_system@materialized:6877/materialize
+    #     ALTER SYSTEM SET catalog_kind TO 'persist'
+    # """
+    #         )
+    #     )
+    #     c.down("materialized")
+    #
+    # # Start and stop Materialize with persist multiple times to increment the epoch.
+    # # for _ in range(0, 1):
+    # c.up("materialized")
+    # # c.down("materialized")
+    # #
+    # # # Switch to stash catalog.
+    # # with c.override(
+    # #         Testdrive(no_reset=True),
+    # # ):
+    # #     c.up("testdrive", persistent=True)
+    # #     c.up("materialized")
+    # #     c.testdrive(
+    # #         input=dedent(
+    # #             """
+    # #     $ postgres-execute connection=postgres://mz_system@materialized:6877/materialize
+    # #     ALTER SYSTEM SET catalog_kind TO 'stash'
+    # # """
+    # #         )
+    # #     )
+    # #     c.down("materialized")
+    # #
+    # # c.up("materialized")
+    # # c.down("materialized")
