e81e339f9575d10e3508ac6208c69f7baa1cac94
Sean Loiselle <sean@materialize.io>
2024-01-09 14:49:13 -0500

    sql: structure error when modifying replicas has too many for storage objects

--- src/sql/src/plan/error.rs
+++ src/sql/src/plan/error.rs
@@ -230,6 +230,15 @@ pub enum PlanError {
     MissingName(CatalogItemType),
     InvalidRefreshAt,
     InvalidRefreshEveryAlignedTo,
+    CreateReplicaFailStorageObjects {
+        /// The current number of replicas on the cluster
+        current_replica_count: usize,
+        /// THe number of internal replicas on the cluster
+        internal_replica_count: usize,
+        /// The number of replicas that executing this command would have
+        /// created
+        hypothetical_replica_count: usize,
+    },
     // TODO(benesch): eventually all errors should be structured.
     Unstructured(String),
 }
@@ -282,6 +291,19 @@ impl PlanError {
             Self::LoadGeneratorSourcePurification(e) => e.detail(),
             Self::CsrPurification(e) => e.detail(),
             Self::KafkaSinkPurification(e) => e.detail(),
+            Self::CreateReplicaFailStorageObjects { current_replica_count: current, internal_replica_count: internal, hypothetical_replica_count: target } => {
+                Some(format!(
+                    "Currently have {} replica{}{}; command would result in {}",
+                    current,
+                    if *current != 1 { "s" } else { "" },
+                    if *internal > 0 {
+                        format!(" ({} internal)", internal)
+                    } else {
+                        "".to_string()
+                    },
+                    target
+                ))
+            }
             _ => None,
         }
     }
@@ -612,6 +634,9 @@ impl fmt::Display for PlanError {
             Self::InvalidRefreshEveryAlignedTo => {
                 write!(f, "REFRESH EVERY ... ALIGNED TO argument must be an expression that can be simplified \
                            and/or cast to a constant whose type is mz_timestamp")
+            }
+            Self::CreateReplicaFailStorageObjects {..} => {
+                write!(f, "cannot create more than one replica of a cluster containing sources or sinks")
             }
         }
     }
