3c73ffb5b7a7965071d48d65d6f298f366bf22dc
Philip Stoev <pstoev@materialize.com>
2024-01-15 13:53:15 +0100

    rqg: Add a workload with window functions

--- test/rqg/mzcompose.py
+++ test/rqg/mzcompose.py
@@ -96,6 +96,13 @@ WORKLOADS = [
         validator="ResultsetComparatorSimplify",
     ),
     Workload(
+        name="window-functions",
+        dataset=Dataset.SIMPLE,
+        grammar="conf/mz/window-functions.yy",
+        reference_implementation=ReferenceImplementation.POSTGRES,
+        validator="ResultsetComparatorSimplify",
+    ),
+    Workload(
         # A workload that performs DML that preserve the dataset's invariants
         # and also checks that those invariants are not violated
         name="banking",
