0dd6de1689daa80549b66a4049d3cc3f9ac4c3c3
Rainer Niedermayr <rainer@materialize.com>
2024-01-15 12:40:45 +0100

    tests: add platform check for Kafka source with retain history

--- misc/python/materialize/checks/all_checks/retain_history.py
+++ misc/python/materialize/checks/all_checks/retain_history.py
@@ -10,11 +10,16 @@ from textwrap import dedent
 
 from materialize.checks.actions import Testdrive
 from materialize.checks.checks import Check
+from materialize.checks.common import KAFKA_SCHEMA_WITH_SINGLE_STRING_FIELD
 from materialize.checks.executors import Executor
 from materialize.mz_version import MzVersion
 
 
-class RetainHistory(Check):
+def schemas() -> str:
+    return dedent(KAFKA_SCHEMA_WITH_SINGLE_STRING_FIELD)
+
+
+class RetainHistoryOnMv(Check):
     def _can_run(self, e: Executor) -> bool:
         return e.current_mz_version >= MzVersion.parse_mz("v0.81.0")
 
@@ -184,6 +189,145 @@ class RetainHistory(Check):
                 {definition_validations}
 
                 {other_validations}
+                """
+            )
+        )
+
+
+class RetainHistoryOnKafkaSource(Check):
+    def _can_run(self, e: Executor) -> bool:
+        return e.current_mz_version >= MzVersion.parse_mz("v0.81.0")
+
+    def initialize(self) -> Testdrive:
+        return Testdrive(
+            schemas()
+            + dedent(
+                """
+                > CREATE TABLE time (time_index INT, t TIMESTAMP);
+                > INSERT INTO time VALUES (0, now());
+
+                # We want to have the topic created at a slightly later timestamp
+                $ sleep-is-probably-flaky-i-have-justified-my-need-with-a-comment duration="2s"
+
+                $ kafka-create-topic topic=retain-history
+
+                $ kafka-ingest format=avro key-format=avro topic=retain-history key-schema=${keyschema} schema=${schema} repeat=5
+                {"key1": "K${kafka-ingest.iteration}"} {"f1": "A${kafka-ingest.iteration}"}
+
+                # Give the source some time
+                $ sleep-is-probably-flaky-i-have-justified-my-need-with-a-comment duration="2s"
+
+                > INSERT INTO time VALUES (1, now());
+
+                > CREATE SOURCE retain_history_source
+                  FROM KAFKA CONNECTION kafka_conn (TOPIC 'testdrive-retain-history-${testdrive.seed}')
+                  FORMAT AVRO USING CONFLUENT SCHEMA REGISTRY CONNECTION csr_conn
+                  ENVELOPE UPSERT
+                  WITH (RETAIN HISTORY FOR '30s')
+
+                # Give the source some time
+                $ sleep-is-probably-flaky-i-have-justified-my-need-with-a-comment duration="2s"
+
+                > INSERT INTO time VALUES (2, now());
+            """
+            )
+        )
+
+    def manipulate(self) -> list[Testdrive]:
+        return [
+            Testdrive(schemas() + dedent(s))
+            for s in [
+                """
+                $ kafka-ingest format=avro key-format=avro topic=retain-history key-schema=${keyschema} schema=${schema} repeat=2
+                {"key1": "K${kafka-ingest.iteration}"} {"f1": "B${kafka-ingest.iteration}"}
+
+                # Give the source some time
+                $ sleep-is-probably-flaky-i-have-justified-my-need-with-a-comment duration="2s"
+
+                > INSERT INTO time VALUES (3, now());
+                """,
+                """
+                $ kafka-ingest format=avro key-format=avro topic=retain-history key-schema=${keyschema} schema=${schema} repeat=6
+                {"key1": "K${kafka-ingest.iteration}"} {"f1": "C${kafka-ingest.iteration}"}
+
+                # Give the source some time
+                $ sleep-is-probably-flaky-i-have-justified-my-need-with-a-comment duration="2s"
+
+                > INSERT INTO time VALUES (4, now());
+
+                $ kafka-ingest format=avro key-format=avro topic=retain-history key-schema=${keyschema} schema=${schema} repeat=1
+                {"key1": "K${kafka-ingest.iteration}"} {"f1": "D${kafka-ingest.iteration}"}
+
+                # Give the source some time
+                $ sleep-is-probably-flaky-i-have-justified-my-need-with-a-comment duration="2s"
+
+                > INSERT INTO time VALUES (5, now());
+                """,
+            ]
+        ]
+
+    def validate(self) -> Testdrive:
+        return Testdrive(
+            dedent(
+                """
+                $ set-from-sql var=time0
+                SELECT t::STRING FROM time WHERE time_index = 0
+                $ set-from-sql var=time1
+                SELECT t::STRING FROM time WHERE time_index = 1
+                $ set-from-sql var=time2
+                SELECT t::STRING FROM time WHERE time_index = 2
+                $ set-from-sql var=time3
+                SELECT t::STRING FROM time WHERE time_index = 3
+                $ set-from-sql var=time4
+                SELECT t::STRING FROM time WHERE time_index = 4
+                $ set-from-sql var=time5
+                SELECT t::STRING FROM time WHERE time_index = 5
+
+                # TODO: check / compare with MV and file issue
+                # ! SELECT * FROM retain_history_source AS OF '${time0}'::TIMESTAMP; -- time0
+                # contains: is not valid for all inputs
+
+                # TODO: check / compare with MV and file issue
+                # ! SELECT * FROM retain_history_source AS OF '${time1}'::TIMESTAMP; -- time1
+                # contains: is not valid for all inputs
+
+                > SELECT * FROM retain_history_source AS OF '${time2}'::TIMESTAMP; -- time2
+                K0 A0
+                K1 A1
+                K2 A2
+                K3 A3
+                K4 A4
+
+                > SELECT * FROM retain_history_source AS OF '${time3}'::TIMESTAMP; -- time3
+                K0 B0
+                K1 B1
+                K2 A2
+                K3 A3
+                K4 A4
+
+                > SELECT * FROM retain_history_source AS OF '${time4}'::TIMESTAMP; -- time4
+                K0 C0
+                K1 C1
+                K2 C2
+                K3 C3
+                K4 C4
+                K5 C5
+
+                > SELECT * FROM retain_history_source AS OF '${time5}'::TIMESTAMP; -- time5
+                K0 D0
+                K1 C1
+                K2 C2
+                K3 C3
+                K4 C4
+                K5 C5
+
+                > SELECT * FROM retain_history_source;
+                K0 D0
+                K1 C1
+                K2 C2
+                K3 C3
+                K4 C4
+                K5 C5
                 """
             )
         )
