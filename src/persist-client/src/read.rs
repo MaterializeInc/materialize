ddd3f045159308980a160969effa4d0ad9949616
Ben Kirwin <bkirwi@materialize.com>
2024-01-22 18:35:46 -0500

    Error-log if we try and take a snapshot before the since

--- src/persist-client/src/read.rs
+++ src/persist-client/src/read.rs
@@ -21,9 +21,9 @@ use differential_dataflow::lattice::Lattice;
 use differential_dataflow::trace::Description;
 use futures::Stream;
 use mz_dyncfg::Config;
-use mz_ore::instrument;
 use mz_ore::now::EpochMillis;
 use mz_ore::task::{AbortOnDropHandle, JoinHandle, RuntimeExt};
+use mz_ore::{instrument, soft_assert_or_log};
 use mz_persist::location::{Blob, SeqNo};
 use mz_persist_types::{Codec, Codec64};
 use proptest_derive::Arbitrary;
@@ -751,6 +751,14 @@ where
         as_of: Antichain<T>,
     ) -> Result<Vec<LeasedBatchPart<T>>, Since<T>> {
         let batches = self.machine.snapshot(&as_of).await?;
+
+        soft_assert_or_log!(
+            PartialOrder::less_equal(self.since(), &as_of),
+            "ReadHandle::snapshot called with as_of {:?} not past the handle's since {:?}, \
+            though the read has succeeded. This implies the since is not being held back correctly.",
+            &as_of,
+            self.since()
+        );
 
         let filter = FetchBatchFilter::Snapshot { as_of };
         let mut leased_parts = Vec::new();
