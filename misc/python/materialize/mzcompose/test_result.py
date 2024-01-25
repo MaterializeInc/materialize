32a3aa167803e57e33b97b2607f5d0fa77f6ed9e
Rainer Niedermayr <rainer@materialize.com>
2024-01-25 18:50:34 +0100

    ci: test report: fix handling of None value

--- misc/python/materialize/mzcompose/test_result.py
+++ misc/python/materialize/mzcompose/test_result.py
@@ -85,12 +85,15 @@ def try_determine_errors_from_cmd_execution(
                 file_path = parts[0]
                 line_number = int(parts[1])
 
-        message = f"Executing {file_path} failed!"
+        message = (
+            f"Executing {file_path if file_path is not None else 'command'} failed!"
+        )
+
         collected_errors.append(
             TestFailureDetails(
                 message,
                 details=chunk,
-                location=f"{file_path}",
+                location=file_path,
                 line_number=line_number,
             )
         )
