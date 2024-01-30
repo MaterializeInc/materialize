91638580e4df2ef19c56189ffe302b60f4d43318
Rainer Niedermayr <rainer@materialize.com>
2024-01-30 19:09:21 +0100

    tests: minio: patch node selector

--- misc/python/materialize/cloudtest/k8s/minio.py
+++ misc/python/materialize/cloudtest/k8s/minio.py
@@ -10,6 +10,10 @@
 from materialize.cloudtest import DEFAULT_K8S_NAMESPACE
 from materialize.cloudtest.k8s.api.k8s_resource import K8sResource
 
+MINIO_YAML_DIRECTORY_URL = (
+    "https://raw.githubusercontent.com/kubernetes/examples/master/staging/storage/minio"
+)
+
 
 class Minio(K8sResource):
     def __init__(
@@ -27,16 +31,33 @@ class Minio(K8sResource):
             "true",
         )
 
-        for yaml in [
-            "minio-standalone-pvc",
+        # the PVC will be created afterwards
+        for yaml_file in [
             "minio-standalone-deployment",
             "minio-standalone-service",
         ]:
             self.kubectl(
                 "create",
                 "-f",
-                f"https://raw.githubusercontent.com/kubernetes/examples/master/staging/storage/minio/{yaml}.yaml",
+                f"{MINIO_YAML_DIRECTORY_URL}/{yaml_file}.yaml",
             )
+
+        self.kubectl(
+            "patch",
+            "deployment",
+            "minio-deployment",
+            "--type",
+            "json",
+            "-p",
+            '[{"op": "add", "path": "/spec/template/spec/nodeSelector", "value": {"supporting-services": "true"} }]',
+        )
+
+        # the PVC needs to be created after patching the deployment
+        self.kubectl(
+            "create",
+            "-f",
+            f"{MINIO_YAML_DIRECTORY_URL}/minio-standalone-pvc.yaml",
+        )
 
         self.wait(
             resource="deployment.apps/minio-deployment",
