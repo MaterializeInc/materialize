9491d6de09610ad875c030b4635d883a69cdfc8b
Parker Timmerman <parker.timmerman@materialize.com>
2024-01-31 11:28:56 -0500

    fivetran-destination: Use the provided Docker image (#24835)
    This PR is centered around testing the Fivetran Destination, it does a
    few individual things:
    
    1. Uses the Fivetran provided Docker image of the "destination tester"
    instead of building our own from our fork of the fivetran sdk.
    2. Breaks up the existing "test_writes" into separate tests, since the
    testing behavior we were trying to do isn't yet supported (see
    [Slack](https://materializeinc.slack.com/archives/C060KAR4802/p1706548430680759)).
    3. Updates our connector so we no longer splice the "_fivetran_synced"
    and "_fivetran_deleted" columns into various requests, since the tester
    now provides these.
    4. Runs our Fivetran destination tests in CI.
    
    There are a couple outstanding issues related to testing the Fivetran
    Destination that I have pinged them about and are currently in progress:
    
    1. aarch64 based Docker image. Currently if you run these tests locally
    on a Mac it will run the x86 image in emulation mode.
    [Slack](https://materializeinc.slack.com/archives/C060KAR4802/p1706548430680759)
    2. The "truncate" option in their testing framework is currently broken.
    [Slack](https://materializeinc.slack.com/archives/C060KAR4802/p1706651239233319).
    
    Even with these issues I wanted to get this change up in case I switch
    to other work soon.
    
    ### Motivation
    
    Fixes https://github.com/MaterializeInc/materialize/issues/24446
    
    ### Checklist
    
    - [ ] This PR has adequate test coverage / QA involvement has been duly
    considered.
    - [ ] This PR has an associated up-to-date [design
    doc](https://github.com/MaterializeInc/materialize/blob/main/doc/developer/design/README.md),
    is a design doc
    ([template](https://github.com/MaterializeInc/materialize/blob/main/doc/developer/design/00000000_template.md)),
    or is sufficiently small to not require a design.
      <!-- Reference the design in the description. -->
    - [ ] If this PR evolves [an existing `$T ⇔ Proto$T`
    mapping](https://github.com/MaterializeInc/materialize/blob/main/doc/developer/command-and-response-binary-encoding.md)
    (possibly in a backwards-incompatible way), then it is tagged with a
    `T-proto` label.
    - [ ] If this PR will require changes to cloud orchestration or tests,
    there is a companion cloud PR to account for those changes that is
    tagged with the release-blocker label
    ([example](https://github.com/MaterializeInc/cloud/pull/5021)).
    <!-- Ask in #team-cloud on Slack if you need help preparing the cloud
    PR. -->
    - [x] This PR includes the following [user-facing behavior
    changes](https://github.com/MaterializeInc/materialize/blob/main/doc/developer/guide-changes.md#what-changes-require-a-release-note):
      - N/a

--- test/fivetran-destination/mzcompose.py
+++ test/fivetran-destination/mzcompose.py
@@ -78,6 +78,11 @@ SERVICES = [
     ),
 ]
 
+# Tests that are currently broken because the Fivetran Tester seems to do the wrong thing.
+#
+# 'test-truncate': https://materializeinc.slack.com/archives/C060KAR4802/p1706651239233319
+BROKEN_TESTS = ["test-truncate"]
+
 
 def workflow_default(c: Composition, parser: WorkflowArgumentParser) -> None:
     parser.add_argument("filter", nargs="?")
@@ -89,6 +94,9 @@ def workflow_default(c: Composition, parser: WorkflowArgumentParser) -> None:
         if path.name.startswith("test-"):
             if args.filter and args.filter not in path.name:
                 print(f"Test case {path.name!r} does not match filter; skipping...")
+                continue
+            if path.name in BROKEN_TESTS:
+                print(f"Test case {path.name!r} is currently broken; skipping...")
                 continue
             with c.test_case(path.name):
                 _run_test_case(c, path)
@@ -107,27 +115,53 @@ def _run_test_case(c: Composition, path: Path):
             assert False, f"unexpected test file: {test_file}"
 
 
+# Run the Fivetran Destination Tester with a single file.
 def _run_destination_tester(c: Composition, test_file: Path):
-    data_dir = ROOT / "data"
-    for data_file in data_dir.iterdir():
-        if data_file.name in ("configuration.json", ".gitignore"):
-            continue
-        data_file.unlink()
-    shutil.copy(test_file, data_dir)
+    # The Fivetran Destination tester operates on an entire directory at a time. We run
+    # individual test cases by copying everything into a single "data" directory which
+    # automatically gets cleaned up at the start and end of every run.
+    with DataDirGuard(ROOT / "data") as data_dir:
+        test_file = ROOT / test_file
+        shutil.copy(test_file, data_dir.path())
 
-    expected_failure = None
-    last_line = test_file.read_text().splitlines()[-1]
-    if last_line.startswith("// FAIL: "):
-        expected_failure = last_line.removeprefix("// FAIL: ")
-    if expected_failure:
-        ret = c.run("fivetran-destination-tester", check=False, capture=True)
-        print("stdout:")
-        print(ret.stdout)
-        assert (
-            ret.returncode != 0
-        ), f"destination tester did not fail with expected message {expected_failure!r}"
-        assert (
-            expected_failure in ret.stdout
-        ), f"destination tester did not fail with expected message {expected_failure!r}"
-    else:
-        c.run("fivetran-destination-tester")
+        last_line = test_file.read_text().splitlines()[-1]
+        if last_line.startswith("// FAIL: "):
+            expected_failure = last_line.removeprefix("// FAIL: ")
+        else:
+            expected_failure = None
+
+        if expected_failure:
+            ret = c.run("fivetran-destination-tester", check=False, capture=True)
+            print("stdout:")
+            print(ret.stdout)
+            assert (
+                ret.returncode != 0
+            ), f"destination tester did not fail with expected message {expected_failure!r}"
+            assert (
+                expected_failure in ret.stdout
+            ), f"destination tester did not fail with expected message {expected_failure!r}"
+        else:
+            c.run("fivetran-destination-tester")
+
+
+# Type that implements the Context Protocol that makes it easy to automatically clean up our data
+# directory before and after every test run.
+class DataDirGuard:
+    def __init__(self, dir: Path):
+        self._dir = dir
+
+    def clean(self):
+        for file in self._dir.iterdir():
+            if file.name in ("configuration.json", ".gitignore"):
+                continue
+            file.unlink()
+
+    def path(self) -> Path:
+        return self._dir
+
+    def __enter__(self):
+        self.clean()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.clean()
