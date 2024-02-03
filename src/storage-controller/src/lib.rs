54dafa9e3ace7703f5a0b5af76bf6767cc57936a
Jan Teske <jteske@posteo.net>
2024-02-03 12:24:46 +0100

    catalog,storage: add compute operator hydration relations
    This commit adds two new relations for exposing compute operator
    hydration status:
     * `mz_compute_operator_hydration_statuses_per_worker` and
     * `mz_compute_operator_hydration_statuses`
    
    The latter is a view over the former. The former is a storage-managed
    collection associated with a new
    `IntrospectionType::ComputeOperatorHydrationStatus`.
    
    The new relations are left empty for now. Filling them is the subject of
    a subsequent commit.

--- src/storage-controller/src/lib.rs
+++ src/storage-controller/src/lib.rs
@@ -805,7 +805,8 @@ where
                         // Truncate compute-maintained collections.
                         IntrospectionType::ComputeDependencies
                         | IntrospectionType::ComputeReplicaHeartbeats
-                        | IntrospectionType::ComputeHydrationStatus => {
+                        | IntrospectionType::ComputeHydrationStatus
+                        | IntrospectionType::ComputeOperatorHydrationStatus => {
                             self.reconcile_managed_collection(id, vec![]).await;
                         }
 
