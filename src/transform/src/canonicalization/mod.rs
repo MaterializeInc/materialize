8b75adee9277ffcd65d137454066c7b3ff2a314d
Frank McSherry <fmcsherry@me.com>
2024-02-09 12:30:40 -0500

    Order reduce grouping keys

--- src/transform/src/canonicalization/mod.rs
+++ src/transform/src/canonicalization/mod.rs
@@ -21,3 +21,56 @@ mod topk_elision;
 pub use flatmap_to_map::FlatMapToMap;
 pub use projection_extraction::ProjectionExtraction;
 pub use topk_elision::TopKElision;
+
+use crate::TransformCtx;
+use crate::TransformError;
+use mz_expr::MirRelationExpr;
+
+/// Orders the keys in a `Reduce`.
+///
+/// This ordering is best thought of as a "logical" transformation, in that
+/// it aims to canonicalize the representation without (yet) worrying about
+/// physical properties like the order of arrangement keys (which can help
+/// to avoid re-arranging data).
+#[derive(Debug)]
+pub struct ReduceKeyOrder;
+
+impl crate::Transform for ReduceKeyOrder {
+    #[tracing::instrument(
+        target = "optimizer",
+        level = "debug",
+        skip_all,
+        fields(path.segment = "reduce_key_order")
+    )]
+    fn transform(
+        &self,
+        relation: &mut MirRelationExpr,
+        _: &mut TransformCtx,
+    ) -> Result<(), TransformError> {
+        // Visit looking for `Reduce` expressions with keys not in canonical order.
+        relation.visit_pre_mut(|expr| {
+            if let MirRelationExpr::Reduce {
+                group_key: keys,
+                aggregates,
+                ..
+            } = expr
+            {
+                let old_keys = keys.clone();
+                keys.sort();
+                if keys != &old_keys {
+                    // Need to find each of `old_keys` in `keys`, and install a projection.
+                    let mut projection = Vec::with_capacity(keys.len() + aggregates.len());
+                    projection.extend(
+                        old_keys
+                            .iter()
+                            .map(|ok| keys.iter().position(|k| ok == k).unwrap()),
+                    );
+                    projection.extend(keys.len()..(keys.len() + aggregates.len()));
+                    *expr = expr.take_dangerous().project(projection);
+                }
+            }
+        });
+        mz_repr::explain::trace_plan(&*relation);
+        Ok(())
+    }
+}
