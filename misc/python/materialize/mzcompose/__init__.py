4265c8a8d41887c81c1453a4b4e88397a739a300
Rainer Niedermayr <rainer@materialize.com>
2024-02-12 16:23:31 +0100

    ci: set enable_mysql_source to true

--- misc/python/materialize/mzcompose/__init__.py
+++ misc/python/materialize/mzcompose/__init__.py
@@ -69,6 +69,7 @@ DEFAULT_SYSTEM_PARAMETERS = {
     "enable_jemalloc_profiling": "true",
     "enable_logical_compaction_window": "true",
     "enable_multi_worker_storage_persist_sink": "true",
+    "enable_mysql_source": "true",
     "enable_rbac_checks": "true",
     "enable_reduce_mfp_fusion": "true",
     "enable_refresh_every_mvs": "true",
