668e3e35bad38192b55c23a6e508b447785ee79f
Roshan Jobanputra <roshan@materialize.com>
2024-02-14 14:39:12 -0500

    Test to try and reproduce consistency issue with many inserts

--- test/mysql-cdc/mzcompose.py
+++ test/mysql-cdc/mzcompose.py
@@ -7,6 +7,8 @@
 # the Business Source License, use of this software will be governed
 # by the Apache License, Version 2.0.
 
+from textwrap import dedent
+
 from materialize.mzcompose.composition import Composition, WorkflowArgumentParser
 from materialize.mzcompose.services.materialized import Materialized
 from materialize.mzcompose.services.mysql import MySql
@@ -119,3 +121,74 @@ def workflow_schema_change_restart(c: Composition) -> None:
             f"--var=mysql-root-password={MySql.DEFAULT_ROOT_PASSWORD}",
             "schema-restart/after-restart.td",
         )
+
+
+def workflow_many_inserts(c: Composition, parser: WorkflowArgumentParser) -> None:
+    """
+    Tests a scenario that caused a consistency issue in the past. We insert a large
+    number of rows into a table, and then create a source for that table. We then
+    insert more rows into the table while the source is snapshotting the table, and
+    verify that the correct count of rows is captured by the source.
+
+    In earlier incarnations of the MySQL source, the source would snapshot inside
+    a transaction using REPEATABLE READ 'WITH CONSISTENT SNAPSHOT' semantics, which
+    we assumed would produce a consistent read from the point that the transaction started at.
+    However this test would fail because the snapshot would capture some of the
+    rows inserted after the transaction started and they would not be correctly rewound.
+
+    We replaced the 'WITH CONSISTENT SNAPSHOT' with a SELECT count(*) FROM {table}
+    to trigger a consistent read inside the snapshot transaction to fix the issue.
+    """
+    c.up("materialized", "mysql")
+    c.up("testdrive", persistent=True)
+
+    n = 100000
+
+    insertions = "\n".join(
+        [
+            f"""
+            SET @i:=0;
+            INSERT INTO t1 (f2) SELECT @i:=@i+1 FROM mysql.time_zone t1, mysql.time_zone t2 LIMIT {round(n/1000)};
+            """
+            for i in range(0, 1000)
+        ]
+    )
+
+    c.testdrive(
+        dedent(
+            f"""
+            $ postgres-execute connection=postgres://mz_system:materialize@${{testdrive.materialize-internal-sql-addr}}
+            ALTER SYSTEM SET enable_mysql_source = true
+
+            $ mysql-connect name=mysql url=mysql://root@mysql password={MySql.DEFAULT_ROOT_PASSWORD}
+
+            > CREATE SECRET IF NOT EXISTS mysqlpass AS '{MySql.DEFAULT_ROOT_PASSWORD}'
+            > CREATE CONNECTION IF NOT EXISTS mysql_conn TO MYSQL (HOST mysql, USER root, PASSWORD SECRET mysqlpass)
+
+            $ mysql-execute name=mysql
+            DROP DATABASE IF EXISTS public;
+            CREATE DATABASE public;
+            USE public;
+            DROP TABLE IF EXISTS t1;
+            CREATE TABLE t1 (pk SERIAL PRIMARY KEY, f2 BIGINT);
+
+            {insertions}
+            {insertions}
+
+            > DROP SOURCE IF EXISTS s1;
+
+            $ mysql-connect name=mysql url=mysql://root@mysql password={MySql.DEFAULT_ROOT_PASSWORD}
+
+            > CREATE SOURCE s1
+                FROM MYSQL CONNECTION mysql_conn
+                FOR TABLES (public.t1);
+
+            $ mysql-execute name=mysql
+            USE public;
+            {insertions}
+
+            > SELECT count(*) FROM t1
+            {n*3}
+            """
+        )
+    )
