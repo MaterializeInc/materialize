MaterializeInc/materialize

commit 4dda3335e26fb0c8f3de4afe7c597ecb45ee3c75

Refactor activity log sources/views/indexes (#25276)
This PR does the following:

* Gets rid of `mz_activity_log` and `mz_activity_log_redacted`. These
are basically pure footguns that nobody should be using. Instead, folks
should use `mz_recent_activity_log` and
`mz_recent_activity_log_redacted`.
* Moves the SQL / redacted SQL text of queries out to a source called
`mz_sql_text`. This allows identical SQL to be deduplicated.
* Removes the SQL text and redacted SQL text from
`mz_prepared_statement_history`.
* Incorporates the session history into `mz_recent_activity_log` and
`mz_recent_activity_log_redacted`.
* Instead of indexing `mz_recent_activity_log`, index its two inputs:
the SQL text and everything but the SQL text. These are in the
(undocumented) views `mz_recent_activity_log_thinned` and
`mz_recent_sql_text`. That way, the amount of data we have to index is
much smaller (due to deduplication in `mz_sql_text`.

After this rolls out, before it goes to prod, we need to do the
following:

* Update the console to change its queries to not join in
`mz_session_history`, which is no longer necessary.
* For users who were using the old `mz_activity_log` or
`mz_activity_log_redacted`, have them switch to the newer `recent`
versions if possible
* For users who were using the underlying
`mz_prepared_statement_history` source, make sure they know they now
need to also join against `mz_sql_text` (on `sql_hash`) to get the full
SQL text.

misc/python/materialize/checks/all_checks/statement_logging.py
@@ -60,7 +60,7 @@ class StatementLogging(Check):
                     """
                     $ postgres-execute connection=postgres://mz_system@${testdrive.materialize-internal-sql-addr}
                     ALTER SYSTEM SET enable_rbac_checks TO false
-                    > SELECT sql, finished_status FROM mz_internal.mz_statement_execution_history mseh, mz_internal.mz_prepared_statement_history mpsh WHERE mseh.prepared_statement_id = mpsh.id AND sql LIKE '%/* Btv was here */' ORDER BY mseh.began_at;
+                    > SELECT sql, finished_status FROM mz_internal.mz_statement_execution_history mseh, mz_internal.mz_prepared_statement_history mpsh, (SELECT DISTINCT sql, sql_hash, redacted_sql FROM mz_internal.mz_sql_text) mst WHERE mseh.prepared_statement_id = mpsh.id AND mst.sql_hash = mpsh.sql_hash AND sql LIKE '%/* Btv was here */' ORDER BY mseh.began_at;
                     "SELECT 'hello' /* Btv was here */" success
                     "SELECT 'goodbye' /* Btv was here */" success
                     $ postgres-execute connection=postgres://mz_system@${testdrive.materialize-internal-sql-addr}
@@ -75,7 +75,7 @@ class StatementLogging(Check):
                     """
                     $ postgres-execute connection=postgres://mz_system@${testdrive.materialize-internal-sql-addr}
                     ALTER SYSTEM SET enable_rbac_checks TO false
-                    > SELECT count(*) <= 2 FROM mz_internal.mz_statement_execution_history mseh, mz_internal.mz_prepared_statement_history mpsh WHERE mseh.prepared_statement_id = mpsh.id AND sql LIKE '%/* Btv was here */'
+                    > SELECT count(*) <= 2 FROM mz_internal.mz_statement_execution_history mseh, mz_internal.mz_prepared_statement_history mpsh, (SELECT DISTINCT sql, sql_hash, redacted_sql FROM mz_internal.mz_sql_text) mst WHERE mseh.prepared_statement_id = mpsh.id AND mpsh.sql_hash = mst.sql_hash AND sql LIKE '%/* Btv was here */'
                     true
                     $ postgres-execute connection=postgres://mz_system@${testdrive.materialize-internal-sql-addr}
                     ALTER SYSTEM SET enable_rbac_checks TO true

src/adapter/Cargo.toml
@@ -31,6 +31,7 @@ launchdarkly-server-sdk = { version = "1.0.0", default_features = false, feature
   "hypertls",
 ] }
 maplit = "1.0.2"
+md-5 = "0.10.5"
 mz-adapter-types = { path = "../adapter-types" }
 mz-audit-log = { path = "../audit-log" }
 mz-build-info = { path = "../build-info" }

src/adapter/src/coord/statement_logging.rs
@@ -11,11 +11,13 @@ use std::collections::BTreeMap;
 use std::sync::Arc;
 
 use bytes::BytesMut;
+use md5::{Digest, Md5};
 use mz_controller_types::ClusterId;
 use mz_ore::now::{to_datetime, NowFn};
 use mz_ore::task::spawn;
 use mz_ore::{cast::CastFrom, cast::CastInto, now::EpochMillis};
 use mz_repr::adt::array::ArrayDimension;
+use mz_repr::adt::timestamp::TimestampLike;
 use mz_repr::{Datum, Diff, GlobalId, Row, RowPacker, Timestamp};
 use mz_sql::plan::Params;
 use mz_sql_parser::ast::{statement_kind_label_value, StatementKind};
@@ -68,6 +70,12 @@ pub enum PreparedStatementLoggingInfo {
 pub struct StatementLoggingId(Uuid);
 
 #[derive(Debug)]
+pub(crate) struct PreparedStatementEvent {
+    prepared_statement: Row,
+    sql_text: Row,
+}
+
+#[derive(Debug)]
 pub(crate) struct StatementLogging {
     /// Information about statement executions that have been logged
     /// but not finished.
@@ -88,7 +96,7 @@ pub(crate) struct StatementLogging {
     reproducible_rng: rand_chacha::ChaCha8Rng,
 
     pending_statement_execution_events: Vec<(Row, Diff)>,
-    pending_prepared_statement_events: Vec<Row>,
+    pending_prepared_statement_events: Vec<PreparedStatementEvent>,
     pending_session_events: Vec<Row>,
     pending_statement_lifecycle_events: Vec<Row>,
 
@@ -180,11 +188,16 @@ impl Coordinator {
             .into_iter()
             .map(|update| (update, 1))
             .collect();
-        let prepared_statement_updates =
+        let (prepared_statement_updates, sql_text_updates) =
             std::mem::take(&mut self.statement_logging.pending_prepared_statement_events)
                 .into_iter()
-                .map(|update| (update, 1))
-                .collect();
+                .map(
+                    |PreparedStatementEvent {
+                         prepared_statement,
+                         sql_text,
+                     }| ((prepared_statement, 1), (sql_text, 1)),
+                )
+                .unzip::<_, _, Vec<_>, Vec<_>>();
         let statement_execution_updates =
             std::mem::take(&mut self.statement_logging.pending_statement_execution_events);
         let statement_lifecycle_updates =
@@ -199,6 +212,7 @@ impl Coordinator {
             (PreparedStatementHistory, prepared_statement_updates),
             (StatementExecutionHistory, statement_execution_updates),
             (StatementLifecycleHistory, statement_lifecycle_updates),
+            (SqlText, sql_text_updates),
         ] {
             self.controller
                 .storage
@@ -241,7 +255,10 @@ impl Coordinator {
         &mut self,
         session: &mut Session,
         logging: &Arc<QCell<PreparedStatementLoggingInfo>>,
-    ) -> Option<(Option<(StatementPreparedRecord, Row)>, Uuid)> {
+    ) -> Option<(
+        Option<(StatementPreparedRecord, PreparedStatementEvent)>,
+        Uuid,
+    )> {
         let logging = session.qcell_rw(&*logging);
         let mut out = None;
 
@@ -261,22 +278,42 @@ impl Coordinator {
                     "accounting for logging should be done in `begin_statement_execution`"
                 );
                 let uuid = Uuid::new_v4();
+                let sql = std::mem::take(sql);
+                let redacted_sql = std::mem::take(redacted_sql);
+                let sql_hash: [u8; 16] = Md5::digest(sql.as_bytes()).into();
                 let record = StatementPreparedRecord {
                     id: uuid,
-                    sql: std::mem::take(sql),
-                    redacted_sql: std::mem::take(redacted_sql),
+                    sql_hash,
                     name: std::mem::take(name),
                     session_id: *session_id,
                     prepared_at: *prepared_at,
                     kind: *kind,
                 };
-                let mut row = Row::default();
-                let mut packer = row.packer();
-                Self::pack_statement_prepared_update(&record, &mut packer);
-                let cost = packer.byte_len();
+                let mut mpsh_row = Row::default();
+                let mut mpsh_packer = mpsh_row.packer();
+                Self::pack_statement_prepared_update(&record, &mut mpsh_packer);
+                let sql_row = Row::pack([
+                    Datum::TimestampTz(
+                        to_datetime(*prepared_at)
+                            .truncate_day()
+                            .try_into()
+                            .expect("must fit"),
+                    ),
+                    Datum::Bytes(sql_hash.as_slice()),
+                    Datum::String(sql.as_str()),
+                    Datum::String(redacted_sql.as_str()),
+                ]);
+
+                let cost = mpsh_packer.byte_len() + sql_row.byte_len();
                 let throttled_count = self.statement_logging_throttling_check(cost)?;
-                packer.push(Datum::UInt64(throttled_count.try_into().expect("must fit")));
-                out = Some((record, row));
+                mpsh_packer.push(Datum::UInt64(throttled_count.try_into().expect("must fit")));
+                out = Some((
+                    record,
+                    PreparedStatementEvent {
+                        prepared_statement: mpsh_row,
+                        sql_text: sql_row,
+                    },
+                ));
 
                 *logging = PreparedStatementLoggingInfo::AlreadyLogged { uuid };
                 uuid
@@ -417,8 +454,7 @@ impl Coordinator {
             id,
             session_id,
             name,
-            sql,
-            redacted_sql,
+            sql_hash,
             prepared_at,
             kind,
         } = record;
@@ -426,8 +462,7 @@ impl Coordinator {
             Datum::Uuid(*id),
             Datum::Uuid(*session_id),
             Datum::String(name.as_str()),
-            Datum::String(sql.as_str()),
-            Datum::String(redacted_sql.as_str()),
+            Datum::Bytes(sql_hash.as_slice()),
             Datum::TimestampTz(to_datetime(*prepared_at).try_into().expect("must fit")),
             kind.map(statement_kind_label_value).into(),
         ]);

src/adapter/src/statement_logging.rs
@@ -106,8 +106,7 @@ pub struct StatementEndedExecutionRecord {
 #[derive(Clone, Debug)]
 pub struct StatementPreparedRecord {
     pub id: Uuid,
-    pub sql: String,
-    pub redacted_sql: String,
+    pub sql_hash: [u8; 16],
     pub name: String,
     pub session_id: Uuid,
     pub prepared_at: EpochMillis,

src/catalog/src/builtin.rs
@@ -49,7 +49,7 @@ use mz_storage_client::controller::IntrospectionType;
 use mz_storage_client::healthcheck::{
     MZ_AWS_PRIVATELINK_CONNECTION_STATUS_HISTORY_DESC, MZ_PREPARED_STATEMENT_HISTORY_DESC,
     MZ_SESSION_HISTORY_DESC, MZ_SINK_STATUS_HISTORY_DESC, MZ_SOURCE_STATUS_HISTORY_DESC,
-    MZ_STATEMENT_EXECUTION_HISTORY_DESC,
+    MZ_SQL_TEXT_DESC, MZ_STATEMENT_EXECUTION_HISTORY_DESC,
 };
 use mz_storage_client::statistics::{MZ_SINK_STATISTICS_RAW_DESC, MZ_SOURCE_STATISTICS_RAW_DESC};
 use once_cell::sync::Lazy;
@@ -2548,15 +2548,50 @@ pub static MZ_PREPARED_STATEMENT_HISTORY: Lazy<BuiltinSource> = Lazy::new(|| Bui
     access: vec![MONITOR_SELECT],
 });
 
-pub static MZ_PREPARED_STATEMENT_HISTORY_REDACTED: Lazy<BuiltinView> = Lazy::new(|| BuiltinView {
-    name: "mz_prepared_statement_history_redacted",
+pub static MZ_SQL_TEXT: Lazy<BuiltinSource> = Lazy::new(|| BuiltinSource {
+    name: "mz_sql_text",
+    schema: MZ_INTERNAL_SCHEMA,
+    desc: MZ_SQL_TEXT_DESC.clone(),
+    data_source: IntrospectionType::SqlText,
+    is_retained_metrics_object: false,
+    access: vec![MONITOR_SELECT],
+});
+
+pub static MZ_SQL_TEXT_REDACTED: Lazy<BuiltinView> = Lazy::new(|| BuiltinView {
+    name: "mz_sql_text_redacted",
     schema: MZ_INTERNAL_SCHEMA,
     column_defs: None,
-    // everything but "sql"
-    sql: "
-SELECT id, session_id, name, redacted_sql, prepared_at, statement_type, throttled_count
-FROM mz_internal.mz_prepared_statement_history",
-    access: vec![SUPPORT_SELECT, MONITOR_REDACTED_SELECT, MONITOR_SELECT],
+    sql: "SELECT sql_hash, redacted_sql FROM mz_internal.mz_sql_text",
+    access: vec![MONITOR_SELECT, MONITOR_REDACTED_SELECT, SUPPORT_SELECT],
+});
+
+pub static MZ_RECENT_SQL_TEXT: Lazy<BuiltinView> = Lazy::new(|| {
+    BuiltinView {
+    name: "mz_recent_sql_text",
+    schema: MZ_INTERNAL_SCHEMA,
+    column_defs: None,
+    // This should always be 1 day more than the interval in
+    // `MZ_RECENT_THINNED_ACTIVITY_LOG` , because `prepared_day`
+    // is rounded down to the nearest day.  Thus something that actually happened three days ago
+    // could have a `prepared day` anywhere from 3 to 4 days back.
+    sql: "SELECT DISTINCT sql_hash, sql, redacted_sql FROM mz_internal.mz_sql_text WHERE prepared_day + INTERVAL '4 days' >= mz_now()",
+    access: vec![MONITOR_SELECT],
+}
+});
+
+pub static MZ_RECENT_SQL_TEXT_REDACTED: Lazy<BuiltinView> = Lazy::new(|| BuiltinView {
+    name: "mz_recent_sql_text_redacted",
+    schema: MZ_INTERNAL_SCHEMA,
+    column_defs: None,
+    sql: "SELECT sql_hash, redacted_sql FROM mz_internal.mz_recent_sql_text",
+    access: vec![MONITOR_SELECT, MONITOR_REDACTED_SELECT, SUPPORT_SELECT],
+});
+
+pub static MZ_RECENT_SQL_TEXT_IND: Lazy<BuiltinIndex> = Lazy::new(|| BuiltinIndex {
+    name: "mz_recent_sql_text_ind",
+    schema: MZ_INTERNAL_SCHEMA,
+    sql: "IN CLUSTER mz_introspection ON mz_internal.mz_recent_sql_text (sql_hash)",
+    is_retained_metrics_object: false,
 });
 
 pub static MZ_SESSION_HISTORY: Lazy<BuiltinSource> = Lazy::new(|| BuiltinSource {
@@ -2568,60 +2603,59 @@ pub static MZ_SESSION_HISTORY: Lazy<BuiltinSource> = Lazy::new(|| BuiltinSource 
     access: vec![PUBLIC_SELECT],
 });
 
-pub static MZ_ACTIVITY_LOG: Lazy<BuiltinView> = Lazy::new(|| {
+pub static MZ_ACTIVITY_LOG_THINNED: Lazy<BuiltinView> = Lazy::new(|| {
     BuiltinView {
-    name: "mz_activity_log",
+    name: "mz_activity_log_thinned",
     schema: MZ_INTERNAL_SCHEMA,
     column_defs: None,
     sql: "
 SELECT mseh.id AS execution_id, sample_rate, cluster_id, application_name, cluster_name,
 transaction_isolation, execution_timestamp, transient_index_id, params, began_at, finished_at, finished_status,
 error_message, rows_returned, execution_strategy, transaction_id,
-mpsh.id AS prepared_statement_id, sql, mpsh.name AS prepared_statement_name,
-session_id, redacted_sql, prepared_at, statement_type, throttled_count
-FROM mz_internal.mz_statement_execution_history mseh, mz_internal.mz_prepared_statement_history mpsh
-WHERE mseh.prepared_statement_id = mpsh.id",
+mpsh.id AS prepared_statement_id, sql_hash, mpsh.name AS prepared_statement_name,
+session_id, prepared_at, statement_type, throttled_count,
+initial_application_name, authenticated_user
+FROM mz_internal.mz_statement_execution_history mseh,
+     mz_internal.mz_prepared_statement_history mpsh,
+     mz_internal.mz_session_history msh
+WHERE mseh.prepared_statement_id = mpsh.id
+AND mpsh.session_id = msh.id",
     access: vec![MONITOR_SELECT],
 }
 });
 
-pub static MZ_ACTIVITY_LOG_REDACTED: Lazy<BuiltinView> = Lazy::new(|| {
+pub static MZ_RECENT_ACTIVITY_LOG_THINNED: Lazy<BuiltinView> = Lazy::new(|| {
     BuiltinView {
-    name: "mz_activity_log_redacted",
+    name: "mz_recent_activity_log_thinned",
     schema: MZ_INTERNAL_SCHEMA,
     column_defs: None,
-    sql: "
-SELECT execution_id, sample_rate, cluster_id, application_name, cluster_name,
-transaction_isolation, execution_timestamp, transient_index_id, began_at, finished_at, finished_status,
-error_message, rows_returned, execution_strategy, transaction_id, prepared_statement_id,
-prepared_statement_name, session_id, redacted_sql, prepared_at, statement_type, throttled_count
-FROM mz_internal.mz_activity_log",
-    access: vec![SUPPORT_SELECT, MONITOR_REDACTED_SELECT, MONITOR_SELECT],
-    }
+    sql:
+        "SELECT * FROM mz_internal.mz_activity_log_thinned WHERE prepared_at + INTERVAL '3 days' > mz_now()
+AND began_at + INTERVAL '3 days' > mz_now()",
+    access: vec![MONITOR_SELECT],
+}
 });
 
 pub static MZ_RECENT_ACTIVITY_LOG: Lazy<BuiltinView> = Lazy::new(|| BuiltinView {
     name: "mz_recent_activity_log",
     schema: MZ_INTERNAL_SCHEMA,
     column_defs: None,
-    sql:
-        "SELECT * FROM mz_internal.mz_activity_log WHERE prepared_at + INTERVAL '3 days' > mz_now()
-AND began_at + INTERVAL '3 days' > mz_now()",
+    sql: "SELECT mralt.*, mrst.sql
+FROM mz_internal.mz_recent_activity_log_thinned mralt,
+     mz_internal.mz_recent_sql_text mrst
+WHERE mralt.sql_hash = mrst.sql_hash",
     access: vec![MONITOR_SELECT],
 });
 
-pub static MZ_RECENT_ACTIVITY_LOG_REDACTED: Lazy<BuiltinView> = Lazy::new(|| {
-    BuiltinView {
-        name: "mz_recent_activity_log_redacted",
-        schema: MZ_INTERNAL_SCHEMA,
-        column_defs: None,
-        sql: "SELECT execution_id, sample_rate, cluster_id, application_name, cluster_name,
-transaction_isolation, execution_timestamp, transient_index_id, began_at, finished_at, finished_status,
-error_message, rows_returned, execution_strategy, transaction_id, prepared_statement_id,
-prepared_statement_name, session_id, redacted_sql, prepared_at, statement_type
-FROM mz_internal.mz_recent_activity_log",
-        access: vec![SUPPORT_SELECT, MONITOR_REDACTED_SELECT, MONITOR_SELECT],
-    }
+pub static MZ_RECENT_ACTIVITY_LOG_REDACTED: Lazy<BuiltinView> = Lazy::new(|| BuiltinView {
+    name: "mz_recent_activity_log_redacted",
+    schema: MZ_INTERNAL_SCHEMA,
+    column_defs: None,
+    sql: "SELECT mralt.*, mrst.redacted_sql
+FROM mz_internal.mz_recent_activity_log_thinned mralt,
+     mz_internal.mz_recent_sql_text mrst
+WHERE mralt.sql_hash = mrst.sql_hash",
+    access: vec![MONITOR_SELECT, MONITOR_REDACTED_SELECT, SUPPORT_SELECT],
 });
 
 pub static MZ_STATEMENT_LIFECYCLE_HISTORY: Lazy<BuiltinSource> = Lazy::new(|| BuiltinSource {
@@ -6307,13 +6341,13 @@ ON mz_internal.mz_frontiers (object_id)",
     is_retained_metrics_object: false,
 };
 
-pub const MZ_RECENT_ACTIVITY_LOG_IND: BuiltinIndex = BuiltinIndex {
-    name: "mz_recent_activity_log_ind",
+pub const MZ_RECENT_ACTIVITY_LOG_THINNED_IND: BuiltinIndex = BuiltinIndex {
+    name: "mz_recent_activity_log_thinned_ind",
     schema: MZ_INTERNAL_SCHEMA,
     sql: "IN CLUSTER mz_introspection
--- session_id because we plan to join
--- this against mz_internal.mz_session_history
-ON mz_internal.mz_recent_activity_log (session_id)",
+-- sql_hash because we plan to join
+-- this against mz_internal.mz_sql_text
+ON mz_internal.mz_recent_activity_log_thinned (sql_hash)",
     is_retained_metrics_object: false,
 };
 
@@ -6704,13 +6738,17 @@ pub static BUILTINS_STATIC: Lazy<Vec<Builtin<NameReference>>> = Lazy::new(|| {
         Builtin::Source(&MZ_STATEMENT_EXECUTION_HISTORY),
         Builtin::View(&MZ_STATEMENT_EXECUTION_HISTORY_REDACTED),
         Builtin::Source(&MZ_PREPARED_STATEMENT_HISTORY),
-        Builtin::View(&MZ_PREPARED_STATEMENT_HISTORY_REDACTED),
         Builtin::Source(&MZ_SESSION_HISTORY),
-        Builtin::View(&MZ_ACTIVITY_LOG),
-        Builtin::View(&MZ_ACTIVITY_LOG_REDACTED),
+        Builtin::Source(&MZ_SQL_TEXT),
+        Builtin::View(&MZ_SQL_TEXT_REDACTED),
+        Builtin::View(&MZ_RECENT_SQL_TEXT),
+        Builtin::View(&MZ_RECENT_SQL_TEXT_REDACTED),
+        Builtin::Index(&MZ_RECENT_SQL_TEXT_IND),
+        Builtin::View(&MZ_ACTIVITY_LOG_THINNED),
+        Builtin::View(&MZ_RECENT_ACTIVITY_LOG_THINNED),
         Builtin::View(&MZ_RECENT_ACTIVITY_LOG),
         Builtin::View(&MZ_RECENT_ACTIVITY_LOG_REDACTED),
-        Builtin::Index(&MZ_RECENT_ACTIVITY_LOG_IND),
+        Builtin::Index(&MZ_RECENT_ACTIVITY_LOG_THINNED_IND),
         Builtin::View(&MZ_SOURCE_STATUSES),
         Builtin::Source(&MZ_STATEMENT_LIFECYCLE_HISTORY),
         Builtin::Source(&MZ_STORAGE_SHARDS),

src/environmentd/tests/server.rs
@@ -233,14 +233,17 @@ SELECT
     mseh.began_at,
     mseh.finished_at,
     mseh.finished_status,
-    mpsh.sql,
+    mst.sql,
     mpsh.prepared_at,
-    mpsh.redacted_sql
+    mst.redacted_sql
 FROM
     mz_internal.mz_statement_execution_history AS mseh
         LEFT JOIN
             mz_internal.mz_prepared_statement_history AS mpsh
             ON mseh.prepared_statement_id = mpsh.id
+        JOIN
+            (SELECT DISTINCT sql, sql_hash, redacted_sql FROM mz_internal.mz_sql_text) mst
+            ON mpsh.sql_hash = mst.sql_hash
 ORDER BY mseh.began_at;",
             &[],
         )
@@ -348,10 +351,13 @@ FROM
         LEFT JOIN
             mz_internal.mz_prepared_statement_history AS mpsh
             ON mseh.prepared_statement_id = mpsh.id
-WHERE mpsh.sql ~~ 'SELECT%'
-AND mpsh.sql !~~ '%unique string to prevent this query showing up in results after retries%'
-AND mpsh.sql !~~ '%pg_catalog.pg_type%' --this gets executed behind the scenes by tokio-postgres
-OR mpsh.sql ~~ 'CREATE TABLE%'
+        JOIN
+            (SELECT DISTINCT sql, sql_hash, redacted_sql FROM mz_internal.mz_sql_text) AS mst
+            ON mpsh.sql_hash = mst.sql_hash
+WHERE mst.sql ~~ 'SELECT%'
+AND mst.sql !~~ '%unique string to prevent this query showing up in results after retries%'
+AND mst.sql !~~ '%pg_catalog.pg_type%' --this gets executed behind the scenes by tokio-postgres
+OR mst.sql ~~ 'CREATE TABLE%'
 ORDER BY mseh.began_at",
                     &[],
                 )
@@ -467,7 +473,9 @@ fn test_statement_logging_throttling() {
                     "SELECT
     sql,
     throttled_count
-FROM mz_internal.mz_prepared_statement_history
+FROM mz_internal.mz_prepared_statement_history mpsh
+JOIN (SELECT DISTINCT sql, sql_hash, redacted_sql FROM mz_internal.mz_sql_text) mst
+ON mpsh.sql_hash = mst.sql_hash
 WHERE sql IN ('SELECT 1', 'SELECT 2')",
                     &[],
                 )
@@ -549,7 +557,10 @@ FROM
         LEFT JOIN
             mz_internal.mz_prepared_statement_history AS mpsh
             ON mseh.prepared_statement_id = mpsh.id
-WHERE mpsh.sql ~~ 'SUBSCRIBE%'
+        JOIN
+            mz_internal.mz_sql_text AS mst
+            ON mpsh.sql_hash = mst.sql_hash
+WHERE mst.sql ~~ 'SUBSCRIBE%'
 ORDER BY mseh.began_at",
             &[],
         )
@@ -594,13 +605,16 @@ fn test_statement_logging_sampling_inner(
     let mut internal_client = server.connect_internal(postgres::NoTls).unwrap();
     let sqls: Vec<String> = internal_client
         .query(
-            "SELECT mpsh.sql
+            "SELECT mst.sql
 FROM
     mz_internal.mz_statement_execution_history AS mseh
         JOIN
             mz_internal.mz_prepared_statement_history AS mpsh
             ON mseh.prepared_statement_id = mpsh.id
-WHERE mpsh.sql ~~ 'SELECT%'
+        JOIN
+            mz_internal.mz_sql_text AS mst
+            ON mpsh.sql_hash = mst.sql_hash
+WHERE mst.sql ~~ 'SELECT%'
 ORDER BY mseh.began_at ASC;",
             &[],
         )

src/storage-client/src/controller.rs
@@ -64,6 +64,7 @@ pub enum IntrospectionType {
     StatementExecutionHistory,
     SessionHistory,
     PreparedStatementHistory,
+    SqlText,
     // For statement lifecycle logging, which is closely related
     // to statement logging
     StatementLifecycleHistory,

src/storage-client/src/healthcheck.rs
@@ -10,25 +10,29 @@
 use mz_repr::{RelationDesc, ScalarType};
 use once_cell::sync::Lazy;
 
-// NOTE: Update the views `mz_prepared_statement_history_redacted`,
-// `mz_activity_log`, and `mz_activity_log_redacted` whenever this
-// is updated, to include the new columns where appropriate.
-//
-// The `redacted` views should contain only those columns that should
-// be queryable by support.
 pub static MZ_PREPARED_STATEMENT_HISTORY_DESC: Lazy<RelationDesc> = Lazy::new(|| {
     RelationDesc::empty()
         .with_column("id", ScalarType::Uuid.nullable(false))
         .with_column("session_id", ScalarType::Uuid.nullable(false))
         .with_column("name", ScalarType::String.nullable(false))
-        .with_column("sql", ScalarType::String.nullable(false))
-        .with_column("redacted_sql", ScalarType::String.nullable(false))
+        .with_column("sql_hash", ScalarType::Bytes.nullable(false))
         .with_column(
             "prepared_at",
             ScalarType::TimestampTz { precision: None }.nullable(false),
         )
         .with_column("statement_type", ScalarType::String.nullable(true))
         .with_column("throttled_count", ScalarType::UInt64.nullable(false))
+});
+
+pub static MZ_SQL_TEXT_DESC: Lazy<RelationDesc> = Lazy::new(|| {
+    RelationDesc::empty()
+        .with_column(
+            "prepared_day",
+            ScalarType::TimestampTz { precision: None }.nullable(false),
+        )
+        .with_column("sql_hash", ScalarType::Bytes.nullable(false))
+        .with_column("sql", ScalarType::String.nullable(false))
+        .with_column("redacted_sql", ScalarType::String.nullable(false))
 });
 
 pub static MZ_SESSION_HISTORY_DESC: Lazy<RelationDesc> = Lazy::new(|| {

commit 5979487ec40ac9cacb5cdf3d64054af827298247

platform-checks: Fix preflight and rollback scenario
Seen in https://buildkite.com/materialize/nightlies/builds/6561#018dccef-ef30-4f63-824c-ff8d982392d8

Broken by https://github.com/MaterializeInc/materialize/pull/25276/files

This scenario is slightly annoying because it is the only one that ends
up running the verify step in the old version. I'm not sure if this will
be too much effort in the future to maintain.

misc/python/materialize/checks/all_checks/statement_logging.py
@@ -60,7 +60,10 @@ class StatementLogging(Check):
                     """
                     $ postgres-execute connection=postgres://mz_system@${testdrive.materialize-internal-sql-addr}
                     ALTER SYSTEM SET enable_rbac_checks TO false
-                    > SELECT sql, finished_status FROM mz_internal.mz_statement_execution_history mseh, mz_internal.mz_prepared_statement_history mpsh, (SELECT DISTINCT sql, sql_hash, redacted_sql FROM mz_internal.mz_sql_text) mst WHERE mseh.prepared_statement_id = mpsh.id AND mst.sql_hash = mpsh.sql_hash AND sql LIKE '%/* Btv was here */' ORDER BY mseh.began_at;
+                    >[version>=8900] SELECT sql, finished_status FROM mz_internal.mz_statement_execution_history mseh, mz_internal.mz_prepared_statement_history mpsh, (SELECT DISTINCT sql, sql_hash, redacted_sql FROM mz_internal.mz_sql_text) mst WHERE mseh.prepared_statement_id = mpsh.id AND mst.sql_hash = mpsh.sql_hash AND sql LIKE '%/* Btv was here */' ORDER BY mseh.began_at;
+                    "SELECT 'hello' /* Btv was here */" success
+                    "SELECT 'goodbye' /* Btv was here */" success
+                    >[version<8900] SELECT sql, finished_status FROM mz_internal.mz_statement_execution_history mseh, mz_internal.mz_prepared_statement_history mpsh WHERE mseh.prepared_statement_id = mpsh.id AND sql LIKE '%/* Btv was here */' ORDER BY mseh.began_at;
                     "SELECT 'hello' /* Btv was here */" success
                     "SELECT 'goodbye' /* Btv was here */" success
                     $ postgres-execute connection=postgres://mz_system@${testdrive.materialize-internal-sql-addr}
@@ -75,7 +78,9 @@ class StatementLogging(Check):
                     """
                     $ postgres-execute connection=postgres://mz_system@${testdrive.materialize-internal-sql-addr}
                     ALTER SYSTEM SET enable_rbac_checks TO false
-                    > SELECT count(*) <= 2 FROM mz_internal.mz_statement_execution_history mseh, mz_internal.mz_prepared_statement_history mpsh, (SELECT DISTINCT sql, sql_hash, redacted_sql FROM mz_internal.mz_sql_text) mst WHERE mseh.prepared_statement_id = mpsh.id AND mpsh.sql_hash = mst.sql_hash AND sql LIKE '%/* Btv was here */'
+                    >[version>=8900] SELECT count(*) <= 2 FROM mz_internal.mz_statement_execution_history mseh, mz_internal.mz_prepared_statement_history mpsh, (SELECT DISTINCT sql, sql_hash, redacted_sql FROM mz_internal.mz_sql_text) mst WHERE mseh.prepared_statement_id = mpsh.id AND mpsh.sql_hash = mst.sql_hash AND sql LIKE '%/* Btv was here */'
+                    true
+                    >[version<8900] SELECT count(*) <= 2 FROM mz_internal.mz_statement_execution_history mseh, mz_internal.mz_prepared_statement_history mpsh WHERE mseh.prepared_statement_id = mpsh.id AND sql LIKE '%/* Btv was here */'
                     true
                     $ postgres-execute connection=postgres://mz_system@${testdrive.materialize-internal-sql-addr}
                     ALTER SYSTEM SET enable_rbac_checks TO true