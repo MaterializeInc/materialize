c03935d3ec51bf12f6962284d14dc8fcf86b0ee8
Dennis Felsing <dennis@felsing.org>
2024-02-22 08:05:49 +0000

    TSan

--- misc/python/materialize/rustc_flags.py
+++ misc/python/materialize/rustc_flags.py
@@ -25,19 +25,9 @@ coverage = [
 sanitizer = {
     "address": [
         "-Zsanitizer=address",
-        # "-Cllvm-args=-asan-use-after-scope",
-        # "-Cllvm-args=-asan-use-after-return=always",
-        # "-Cllvm-args=-asan-recover",
         "-Cllvm-args=-asan-stack=false",  # Remove when #25017 is fixed
-        # "-Cdebug-assertions=on",
         "-Clink-arg=-fuse-ld=lld",  # access beyond end of merged section
-        # "-Clink-arg=-fsanitize=address",
         "-Clinker=clang",  # doesn't work for all, still using ~/.local/bin/cc
-        # "-Clink-arg=-Wl,-rpath=/usr/lib/llvm-17/lib/clang/17/lib/linux",
-        # "-Clink-arg=-lasan",
-        # "-Clink-arg=-static-libsan",
-        # "-Clink-arg=-Wl,-rpath=/home/deen/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/x86_64-unknown-linux-gnu/lib",
-        # "-Clink-arg=-Wl,-rpath=/usr/lib/llvm-17/lib/clang/17/lib/linux",
     ],
     "hwaddress": [
         "-Zsanitizer=hwaddress",
@@ -49,24 +39,18 @@ sanitizer = {
     ],
     "cfi": [
         "-Zsanitizer=cfi",
-        "-Cdebug-assertions=off",
         "-Clink-arg=-fuse-ld=lld",  # access beyond end of merged section
-        "-Clink-arg=-fsanitize=cfi",
-        "-Clinker=cc",
+        "-Clinker=clang",  # doesn't work for all, still using ~/.local/bin/cc
     ],
     "thread": [
         "-Zsanitizer=thread",
-        "-Cdebug-assertions=off",
         "-Clink-arg=-fuse-ld=lld",  # access beyond end of merged section
-        "-Clink-arg=-fsanitize=thread",
-        "-Clinker=cc",
+        "-Clinker=clang",  # doesn't work for all, still using ~/.local/bin/cc
     ],
     "leak": [
         "-Zsanitizer=leak",
-        "-Cdebug-assertions=off",
         "-Clink-arg=-fuse-ld=lld",  # access beyond end of merged section
-        "-Clink-arg=-fsanitize=leak",
-        "-Clinker=cc",
+        "-Clinker=clang",  # doesn't work for all, still using ~/.local/bin/cc
     ],
     "undefined": [],
 }
@@ -77,13 +61,7 @@ sanitizer = {
 # export ASAN_OPTIONS=detect_leaks=0:verify_asan_link_order=0
 # Alternatively make cc detect when it's building the script, then without asan
 sanitizer_cflags = {
-    "address": [
-        "-fsanitize=address",
-        # "-static-libsan",
-        # "-Wl,-rpath=/home/deen/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/x86_64-unknown-linux-gnu/lib",
-        ##"-Wl,-rpath=/usr/lib/llvm-17/lib/clang/17/lib/linux",
-        # "-fsanitize-address-use-after-scope",
-    ],
+    "address": ["-fsanitize=address"],
     "hwaddress": ["-fsanitize=hwaddress"],
     "cfi": ["-fsanitize=cfi"],
     "thread": ["-fsanitize=thread"],
