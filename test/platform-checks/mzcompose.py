76e3b145616f7b750e9385e41b0ccc748e4cafbf
Rainer Niedermayr <rainer@materialize.com>
2024-02-23 17:59:36 +0100

    tests: MySQL platform checks: mzcompose

--- test/platform-checks/mzcompose.py
+++ test/platform-checks/mzcompose.py
@@ -27,6 +27,7 @@ from materialize.mzcompose.services.debezium import Debezium
 from materialize.mzcompose.services.kafka import Kafka
 from materialize.mzcompose.services.materialized import Materialized
 from materialize.mzcompose.services.minio import Mc, Minio
+from materialize.mzcompose.services.mysql import MySql
 from materialize.mzcompose.services.persistcli import Persistcli
 from materialize.mzcompose.services.postgres import Postgres
 from materialize.mzcompose.services.schema_registry import SchemaRegistry
@@ -48,6 +49,7 @@ SERVICES = [
     Minio(setup_materialize=True),
     Mc(),
     Postgres(),
+    MySql(),
     Zookeeper(),
     Kafka(
         auto_create_topics=True,
@@ -132,6 +134,7 @@ def setup(c: Composition) -> None:
         "kafka",
         "schema-registry",
         "postgres",
+        "mysql",
         "debezium",
         "minio",
         "ssh-bastion-host",
