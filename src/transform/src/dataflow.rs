f3ae15d1d78299b6a31b903cecfea714c1f9225c
Jan Teske <jteske@posteo.net>
2024-02-26 12:27:54 +0100

    transform: use checked recursion for ProjectionPushdown
    Not doing this can lead to stack overflows as observed with the query
    from #25545 in debug mode. Seems like an oversight.

--- src/transform/src/dataflow.rs
+++ src/transform/src/dataflow.rs
@@ -331,7 +331,7 @@ where
     // Collect the mutable references to views after pushing projection down
     // in order to run cleanup actions on them in a second loop.
     let mut view_refs = Vec::new();
-    let projection_pushdown = crate::movement::ProjectionPushdown;
+    let projection_pushdown = crate::movement::ProjectionPushdown::default();
     for (id, view) in view_sequence {
         if let Some(columns) = demand.get(&id) {
             let projection_pushed_down = columns.iter().map(|c| *c).collect();
