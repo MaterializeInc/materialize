5b7e9bcf29c6d90a370ce99bedd46b414809a30c
Rainer Niedermayr <rainer@materialize.com>
2024-02-29 10:29:56 +0100

    tests: mysql: extract additional args

--- misc/python/materialize/mzcompose/services/mysql.py
+++ misc/python/materialize/mzcompose/services/mysql.py
@@ -16,6 +16,16 @@ from materialize.mzcompose.service import (
 class MySql(Service):
     DEFAULT_ROOT_PASSWORD = "p@ssw0rd"
 
+    DEFAULT_ADDITIONAL_ARGS = [
+        "--log-bin=mysql-bin",
+        "--gtid_mode=ON",
+        "--enforce_gtid_consistency=ON",
+        "--binlog-format=row",
+        "--log-slave-updates",
+        "--binlog-row-image=full",
+        "--server-id=1",
+    ]
+
     def __init__(
         self,
         root_password: str = DEFAULT_ROOT_PASSWORD,
@@ -23,15 +33,7 @@ class MySql(Service):
         version: str = "8.0.35",
         port: int = 3306,
         volumes: list[str] = ["mydata:/var/lib/mysql-files"],
-        additional_args: list[str] = [
-            "--log-bin=mysql-bin",
-            "--gtid_mode=ON",
-            "--enforce_gtid_consistency=ON",
-            "--binlog-format=row",
-            "--log-slave-updates",
-            "--binlog-row-image=full",
-            "--server-id=1",
-        ],
+        additional_args: list[str] = DEFAULT_ADDITIONAL_ARGS,
     ) -> None:
         image = f"mysql:{version}"
 
