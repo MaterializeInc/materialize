a1753a15a867fe44242df0ea618fa3f407c31b35
Rainer Niedermayr <rainer@materialize.com>
2024-03-01 18:44:45 +0100

    Revert "test: Re-enable test-version-skips catalog test (#25682)"
    This reverts commit 37c9cc8c43aec63db40ad3b2a05a22fdddab8516.

--- test/persist-catalog-migration/mzcompose.py
+++ test/persist-catalog-migration/mzcompose.py
@@ -30,6 +30,9 @@ def workflow_default(c: Composition) -> None:
     for i, name in enumerate(c.workflows):
         if name == "default":
             continue
+        # TODO: Reenable when #25499 is fixed
+        if name == "test-version-skips":
+            continue
         with c.test_case(name):
             c.workflow(name)
 
