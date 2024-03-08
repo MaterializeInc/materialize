8f50c550bbfe0840acc7311f92b81cc84f9e5269
Rainer Niedermayr <rainer@materialize.com>
2024-03-08 20:57:54 +0100

    buildkite insights: introduce cacheable fetch-builds from all pipelines

--- misc/python/materialize/buildkite_insights/buildkite_api/builds_api.py
+++ misc/python/materialize/buildkite_insights/buildkite_api/builds_api.py
@@ -23,6 +23,42 @@ def get_builds(
     include_retries: bool = True,
 ) -> list[Any]:
     request_path = f"organizations/materialize/pipelines/{pipeline_slug}/builds"
+    params = _get_params(
+        branch=branch,
+        build_states=build_states,
+        items_per_page=items_per_page,
+        include_retries=include_retries,
+    )
+
+    return generic_api.get_multiple(request_path, params, max_fetches=max_fetches)
+
+
+def get_builds_of_all_pipelines(
+    max_fetches: int | None,
+    build_states: list[str] | None = None,
+    items_per_page: int = 100,
+    include_retries: bool = True,
+) -> list[Any]:
+    params = _get_params(
+        branch=None,
+        build_states=build_states,
+        items_per_page=items_per_page,
+        include_retries=include_retries,
+    )
+
+    return generic_api.get_multiple(
+        "organizations/materialize/builds",
+        params,
+        max_fetches=max_fetches,
+    )
+
+
+def _get_params(
+    branch: str | None,
+    build_states: list[str] | None,
+    items_per_page: int = 100,
+    include_retries: bool = True,
+) -> dict[str, Any]:
     params: dict[str, Any] = {
         "include_retried_jobs": str(include_retries).lower(),
         "per_page": str(items_per_page),
@@ -34,21 +70,4 @@ def get_builds(
     if build_states is not None and len(build_states) > 0:
         params["state[]"] = build_states
 
-    return generic_api.get_multiple(request_path, params, max_fetches=max_fetches)
-
-
-def get_builds_of_all_pipelines(
-    max_fetches: int | None,
-    items_per_page: int = 100,
-    include_retries: bool = True,
-) -> list[Any]:
-    params = {
-        "include_retried_jobs": str(include_retries).lower(),
-        "per_page": str(items_per_page),
-    }
-
-    return generic_api.get_multiple(
-        "organizations/materialize/builds",
-        params,
-        max_fetches=max_fetches,
-    )
+    return params
