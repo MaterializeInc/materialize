06a4bcade4d58dc61ff97ad19a79c407043c9c63
Rainer Niedermayr <rainer@materialize.com>
2024-03-11 14:21:05 +0100

    feature benchmark: scenario-specific ancestor override

--- misc/python/materialize/version_ancestor_overrides.py
+++ misc/python/materialize/version_ancestor_overrides.py
@@ -10,6 +10,8 @@
 
 from __future__ import annotations
 
+from typing import Any
+
 from materialize.mz_version import MzVersion
 
 """
@@ -17,27 +19,42 @@ Git revisions that are based on commits listed as keys require at least the vers
 Note that specified versions do not necessarily need to be already published.
 Commits must be ordered descending by their date.
 """
-_MIN_ANCESTOR_MZ_VERSION_PER_COMMIT_TO_ACCOUNT_FOR_PERFORMANCE_REGRESSIONS: dict[
-    str, MzVersion
-] = {
-    # insert newer commits at the top
-    # PR#24155 (equivalence propagation) significantly increased wallclock for OptbenchTPCH
-    "3cfaa8207faa7df087942cd44311a3e7b4534c25": MzVersion.parse_mz("v0.92.0"),
-    # PR#25502 (JoinFusion across MFPs) increased number of messages
-    "62ea182963be5b956e13115b8ad39f7835fc4351": MzVersion.parse_mz("v0.91.0"),
-    # PR#24906 (Compute operator hydration status logging) increased number of messages against v0.88.1
-    "067ae870eef724f7eb5851b5745b9ff52b881481": MzVersion.parse_mz("v0.89.0"),
-    # PR#24918 (persist-txn: switch to a new operator protocol for lazy) increased number of messages against v0.86.1 (but got reverted in 0.87.1)
-    "b648576b52b8ba9bb3a4732f7022ab5c06ebed32": MzVersion.parse_mz("v0.87.0"),
-    # PR#23659 (persist-txn: enable in CI with "eager uppers") introduces regressions against v0.79.0
-    "c4f520a57a3046e5074939d2ea345d1c72be7079": MzVersion.parse_mz("v0.80.0"),
-    # PR#23421 (coord: smorgasbord of improvements for the crdb-backed timestamp oracle) introduces regressions against 0.78.13
-    "5179ebd39aea4867622357a832aaddcde951b411": MzVersion.parse_mz("v0.79.0"),
-    # insert newer commits at the top
-}
+
+
+def get_ancestor_overrides_for_performance_regressions(
+    scenario_class: type[Any],
+) -> dict[str, MzVersion]:
+    # Git revisions that are based on commits listed as keys require at least the version specified in the value.
+    # Note that specified versions do not necessarily need to be already published.
+    # Commits must be ordered descending by their date.
+    min_ancestor_mz_version_per_commit = dict()
+
+    # add legacy entries
+    min_ancestor_mz_version_per_commit.update(
+        {
+            # insert newer commits at the top
+            # PR#24155 (equivalence propagation) significantly increased wallclock for OptbenchTPCH
+            "3cfaa8207faa7df087942cd44311a3e7b4534c25": MzVersion.parse_mz("v0.92.0"),
+            # PR#25502 (JoinFusion across MFPs) increased number of messages
+            "62ea182963be5b956e13115b8ad39f7835fc4351": MzVersion.parse_mz("v0.91.0"),
+            # PR#24906 (Compute operator hydration status logging) increased number of messages against v0.88.1
+            "067ae870eef724f7eb5851b5745b9ff52b881481": MzVersion.parse_mz("v0.89.0"),
+            # PR#24918 (persist-txn: switch to a new operator protocol for lazy) increased number of messages against v0.86.1 (but got reverted in 0.87.1)
+            "b648576b52b8ba9bb3a4732f7022ab5c06ebed32": MzVersion.parse_mz("v0.87.0"),
+            # PR#23659 (persist-txn: enable in CI with "eager uppers") introduces regressions against v0.79.0
+            "c4f520a57a3046e5074939d2ea345d1c72be7079": MzVersion.parse_mz("v0.80.0"),
+            # PR#23421 (coord: smorgasbord of improvements for the crdb-backed timestamp oracle) introduces regressions against 0.78.13
+            "5179ebd39aea4867622357a832aaddcde951b411": MzVersion.parse_mz("v0.79.0"),
+            # insert newer commits at the top
+        }
+    )
+
+    return min_ancestor_mz_version_per_commit
 
 """
-See: #_MIN_ANCESTOR_MZ_VERSION_PER_COMMIT_TO_ACCOUNT_FOR_PERFORMANCE_REGRESSIONS
+Git revisions that are based on commits listed as keys require at least the version specified in the value.
+Note that specified versions do not necessarily need to be already published.
+Commits must be ordered descending by their date.
 """
 _MIN_ANCESTOR_MZ_VERSION_PER_COMMIT_TO_ACCOUNT_FOR_SCALABILITY_REGRESSIONS: dict[
     str, MzVersion
@@ -61,9 +78,6 @@ _MIN_ANCESTOR_MZ_VERSION_PER_COMMIT_TO_ACCOUNT_FOR_CORRECTNESS_REGRESSIONS: dict
     "82a5130a8466525c5b3bdb3eff845c7c34585774": MzVersion.parse_mz("v0.85.0"),
 }
 
-ANCESTOR_OVERRIDES_FOR_PERFORMANCE_REGRESSIONS = (
-    _MIN_ANCESTOR_MZ_VERSION_PER_COMMIT_TO_ACCOUNT_FOR_PERFORMANCE_REGRESSIONS
-)
 ANCESTOR_OVERRIDES_FOR_SCALABILITY_REGRESSIONS = (
     _MIN_ANCESTOR_MZ_VERSION_PER_COMMIT_TO_ACCOUNT_FOR_SCALABILITY_REGRESSIONS
 )
