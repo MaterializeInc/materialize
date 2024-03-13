9911cc081c1263e265bdf1fab3bd395debc890ce
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2024-03-13 13:55:45 +0200

    ci: activate `enable_variadic_left_join_lowering`

--- misc/python/materialize/mzcompose/__init__.py
+++ misc/python/materialize/mzcompose/__init__.py
@@ -79,6 +79,7 @@ DEFAULT_SYSTEM_PARAMETERS = {
     "enable_statement_lifecycle_logging": "true",
     "enable_table_keys": "true",
     "enable_try_parse_monotonic_iso8601_timestamp": "true",
+    "enable_variadic_left_join_lowering": "true",
     "enable_worker_core_affinity": "true",
     "persist_batch_delete_enabled": "true",
     "persist_fast_path_limit": "1000",
