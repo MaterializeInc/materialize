91c74cf6b748f3573eea3b2311390ca3db2c561f
Joseph Koshakow <koshy44@gmail.com>
2024-03-15 09:07:58 -0400

    Add comment

--- src/storage-controller/src/lib.rs
+++ src/storage-controller/src/lib.rs
@@ -1775,6 +1775,7 @@ where
                 let drop_notif = match description.data_source {
                     DataSource::Other(DataSourceOther::TableWrites) if read_frontier.is_empty() => {
                         pending_collection_drops.push(id);
+                        // TODO(jkosh44) Batch all table drops into single call to `drop_handles`.
                         Some(self.persist_table_worker.drop_handles(vec![id]))
                     }
                     DataSource::Webhook | DataSource::Introspection(_)
