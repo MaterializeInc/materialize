f6dcdb97f1b0636ad86130daf68037e700e3e487
Bobby Iliev <bobby@bobbyiliev.com>
2024-03-21 00:16:35 +0200

    Re-enable test to run in coverage builds again

--- src/mz/tests/local.rs
+++ src/mz/tests/local.rs
@@ -102,7 +102,6 @@ mod tests {
 
     /// Tests local commands that do not requires interacting with any API.
     #[mz_ore::test]
-    #[cfg_attr(coverage, ignore)] // TODO: Reenable when #25976 is fixed
     #[cfg_attr(miri, ignore)] // unsupported operation: can't call foreign function `pipe2` on OS `linux`
     fn test_local() {
         let unique_id = Uuid::new_v4().to_string();
