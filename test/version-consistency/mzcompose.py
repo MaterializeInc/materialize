423c6e021ee04508fedc0c7509051ece90f8419e
Rainer Niedermayr <rainer@materialize.com>
2024-03-22 08:47:09 +0100

    version consistency: remove random evaluation strategy selection

--- test/version-consistency/mzcompose.py
+++ test/version-consistency/mzcompose.py
@@ -6,7 +6,6 @@
 # As of the Change Date specified in that file, in accordance with
 # the Business Source License, use of this software will be governed
 # by the Apache License, Version 2.0.
-from random import Random
 
 from materialize.mzcompose.composition import Composition, WorkflowArgumentParser
 from materialize.mzcompose.services.cockroach import Cockroach
@@ -17,6 +16,7 @@ from materialize.version_ancestor_overrides import (
     ANCESTOR_OVERRIDES_FOR_CORRECTNESS_REGRESSIONS,
 )
 from materialize.version_consistency.version_consistency_test import (
+    EVALUATION_STRATEGY_NAME_DFR,
     EVALUATION_STRATEGY_NAMES,
     VersionConsistencyTest,
 )
@@ -39,23 +39,14 @@ def workflow_default(c: Composition, parser: WorkflowArgumentParser) -> None:
 
     test = VersionConsistencyTest()
 
-    evaluation_strategy_name_random = "random"
-    extended_evaluation_strategy_names = EVALUATION_STRATEGY_NAMES + [
-        evaluation_strategy_name_random
-    ]
     parser.add_argument(
         "--evaluation-strategy",
-        default=evaluation_strategy_name_random,
+        default=EVALUATION_STRATEGY_NAME_DFR,
         type=str,
-        choices=extended_evaluation_strategy_names,
+        choices=EVALUATION_STRATEGY_NAMES,
     )
 
     args = test.parse_output_consistency_input_args(parser)
-
-    if args.evaluation_strategy == evaluation_strategy_name_random:
-        evaluation_strategy_name = Random(args.seed).choice(EVALUATION_STRATEGY_NAMES)
-    else:
-        evaluation_strategy_name = args.evaluation_strategy
 
     name_mz_this, name_mz_other = "mz_this", "mz_other"
     port_mz_internal, port_mz_this, port_mz_other = 6875, 6875, 16875
@@ -86,7 +77,7 @@ def workflow_default(c: Composition, parser: WorkflowArgumentParser) -> None:
         test.mz2_connection = c.sql_connection(
             service=name_mz_other, port=port_mz_internal
         )
-        test.evaluation_strategy_name = evaluation_strategy_name
+        test.evaluation_strategy_name = args.evaluation_strategy
 
         test_summary = test.run_output_consistency_tests(connection, args)
 
