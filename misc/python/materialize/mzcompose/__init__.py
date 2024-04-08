0167caaf7d11c2f34358a30809c923cd7da66442
Daniel Harrison <52528+danhhz@users.noreply.github.com>
2024-04-08 08:17:02 -0700

    DNM ci experiments

--- misc/python/materialize/mzcompose/__init__.py
+++ misc/python/materialize/mzcompose/__init__.py
@@ -88,7 +88,7 @@ DEFAULT_SYSTEM_PARAMETERS = {
     "persist_batch_delete_enabled": "true",
     "persist_fast_path_limit": "1000",
     "persist_inline_update_max_bytes": "1048576",
-    "persist_inline_update_threshold_bytes": "4096",
+    "persist_inline_update_threshold_bytes": "0",
     "persist_pubsub_client_enabled": "true",
     "persist_pubsub_push_diff_enabled": "true",
     "persist_sink_minimum_batch_updates": "128",
