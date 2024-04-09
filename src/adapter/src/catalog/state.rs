5dd3e9ca7c86dbd7984968c3b495c1fd607f9ce2
Joseph Koshakow <koshy44@gmail.com>
2024-04-09 12:23:41 -0400

    catalog: Extract apply update logic to module (#26508)
    This commit moves all the logic for applying catalog updates to a
    separate module. This commit is purely code movement/organization and
    makes no changes to the content of the code.

--- src/adapter/src/catalog/state.rs
+++ src/adapter/src/catalog/state.rs
@@ -38,7 +38,7 @@ use mz_catalog::memory::error::{Error, ErrorKind};
 use mz_catalog::memory::objects::{
     CatalogEntry, CatalogItem, Cluster, ClusterConfig, ClusterReplica, ClusterReplicaProcessStatus,
     CommentsMap, Connection, DataSourceDesc, Database, DefaultPrivileges, Index, MaterializedView,
-    Role, Schema, Secret, Sink, Source, StateUpdate, StateUpdateKind, Table, Type, View,
+    Role, Schema, Secret, Sink, Source, Table, Type, View,
 };
 use mz_catalog::SYSTEM_CONN_ID;
 use mz_controller::clusters::{
@@ -50,8 +50,8 @@ use mz_expr::MirScalarExpr;
 use mz_ore::cast::CastFrom;
 use mz_ore::collections::CollectionExt;
 use mz_ore::now::{to_datetime, EpochMillis, NOW_ZERO};
+use mz_ore::soft_assert_no_log;
 use mz_ore::str::StrExt;
-use mz_ore::{instrument, soft_assert_no_log};
 use mz_pgrepr::oid::INVALID_OID;
 use mz_repr::adt::mz_acl_item::PrivilegeMap;
 use mz_repr::namespaces::{
@@ -59,7 +59,7 @@ use mz_repr::namespaces::{
     PG_CATALOG_SCHEMA,
 };
 use mz_repr::role_id::RoleId;
-use mz_repr::{Diff, GlobalId, RelationDesc};
+use mz_repr::{GlobalId, RelationDesc};
 use mz_secrets::InMemorySecretsController;
 use mz_sql::catalog::{
     CatalogCluster, CatalogClusterReplica, CatalogConfig, CatalogDatabase,
@@ -79,7 +79,7 @@ use mz_sql::plan::{
 };
 use mz_sql::rbac;
 use mz_sql::session::user::MZ_SYSTEM_ROLE_ID;
-use mz_sql::session::vars::{SystemVars, Var, VarError, VarInput, DEFAULT_DATABASE_NAME};
+use mz_sql::session::vars::{SystemVars, Var, VarInput, DEFAULT_DATABASE_NAME};
 use mz_sql_parser::ast::QualifiedReplica;
 use mz_storage_client::controller::StorageMetadata;
 use mz_storage_types::connections::inline::{
@@ -2244,202 +2244,6 @@ impl CatalogState {
                 SystemObjectType::Object(self.get_object_type(object_id))
             }
             SystemObjectId::System => SystemObjectType::System,
-        }
-    }
-
-    /// Update in-memory catalog state from a list of updates made to the durable catalog state.
-    ///
-    /// This is meant specifically for bootstrapping because it does not produce builtin table
-    /// updates. The builtin tables need to be loaded before we can produce builtin table updates
-    /// which creates a bootstrapping problem.
-    // TODO(jkosh44) It is very IMPORTANT that per timestamp, the updates are sorted retractions
-    // then additions. Within the retractions the objects should be sorted in reverse dependency
-    // order (objects->schema->database, replica->cluster, etc.). Within the additions the objects
-    // should be sorted in dependency order (database->schema->objects, cluster->replica, etc.).
-    // Objects themselves also need to be sorted by dependency order, this will be tricky but we can
-    // look at the existing bootstrap code for ways of doing this. For now we rely on the caller
-    // providing objects in dependency order.
-    #[instrument]
-    pub(crate) fn apply_updates_for_bootstrap(&mut self, updates: Vec<StateUpdate>) {
-        for StateUpdate { kind, diff } in updates {
-            assert_eq!(
-                diff, 1,
-                "initial catalog updates should be consolidated: ({kind:?}, {diff:?})"
-            );
-            self.apply_update(kind, diff);
-        }
-    }
-
-    #[instrument(level = "debug")]
-    fn apply_update(&mut self, kind: StateUpdateKind, diff: Diff) {
-        fn apply<K, V>(map: &mut BTreeMap<K, V>, key: K, value: impl FnOnce() -> V, diff: Diff)
-        where
-            K: Ord + Debug,
-            V: PartialEq + Eq + Debug,
-        {
-            if diff == 1 {
-                let prev = map.insert(key, value());
-                assert_eq!(
-                    prev, None,
-                    "values must be explicitly retracted before inserting a new value"
-                );
-            } else if diff == -1 {
-                let prev = map.remove(&key);
-                // We can't assert the exact contents of the previous value, since we don't know
-                // what it should look like.
-                assert!(
-                    prev.is_some(),
-                    "retraction does not match existing value: {key:?}"
-                );
-            }
-        }
-
-        assert!(
-            diff == 1 || diff == -1,
-            "invalid update in catalog updates: ({kind:?}, {diff:?})"
-        );
-        match kind {
-            StateUpdateKind::Role(role) => {
-                apply(
-                    &mut self.roles_by_id,
-                    role.id,
-                    || Role {
-                        name: role.name.clone(),
-                        id: role.id,
-                        oid: role.oid,
-                        attributes: role.attributes,
-                        membership: role.membership,
-                        vars: role.vars,
-                    },
-                    diff,
-                );
-                apply(&mut self.roles_by_name, role.name, || role.id, diff);
-            }
-            StateUpdateKind::Database(database) => {
-                apply(
-                    &mut self.database_by_id,
-                    database.id.clone(),
-                    || Database {
-                        name: database.name.clone(),
-                        id: database.id.clone(),
-                        oid: database.oid,
-                        schemas_by_id: BTreeMap::new(),
-                        schemas_by_name: BTreeMap::new(),
-                        owner_id: database.owner_id,
-                        privileges: PrivilegeMap::from_mz_acl_items(database.privileges),
-                    },
-                    diff,
-                );
-                apply(
-                    &mut self.database_by_name,
-                    database.name,
-                    || database.id.clone(),
-                    diff,
-                );
-            }
-            StateUpdateKind::Schema(schema) => {
-                let (schemas_by_id, schemas_by_name, database_spec) = match &schema.database_id {
-                    Some(database_id) => {
-                        let db = self
-                            .database_by_id
-                            .get_mut(database_id)
-                            .expect("catalog out of sync");
-                        (
-                            &mut db.schemas_by_id,
-                            &mut db.schemas_by_name,
-                            ResolvedDatabaseSpecifier::Id(*database_id),
-                        )
-                    }
-                    None => (
-                        &mut self.ambient_schemas_by_id,
-                        &mut self.ambient_schemas_by_name,
-                        ResolvedDatabaseSpecifier::Ambient,
-                    ),
-                };
-                apply(
-                    schemas_by_id,
-                    schema.id.clone(),
-                    || Schema {
-                        name: QualifiedSchemaName {
-                            database: database_spec,
-                            schema: schema.name.clone(),
-                        },
-                        id: SchemaSpecifier::Id(schema.id.clone()),
-                        oid: schema.oid,
-                        items: BTreeMap::new(),
-                        functions: BTreeMap::new(),
-                        types: BTreeMap::new(),
-                        owner_id: schema.owner_id,
-                        privileges: PrivilegeMap::from_mz_acl_items(schema.privileges),
-                    },
-                    diff,
-                );
-                apply(schemas_by_name, schema.name.clone(), || schema.id, diff);
-            }
-            StateUpdateKind::DefaultPrivilege(default_privilege) => match diff {
-                1 => self
-                    .default_privileges
-                    .grant(default_privilege.object, default_privilege.acl_item),
-                -1 => self
-                    .default_privileges
-                    .revoke(&default_privilege.object, &default_privilege.acl_item),
-                _ => unreachable!("invalid diff: {diff}"),
-            },
-            StateUpdateKind::SystemPrivilege(system_privilege) => match diff {
-                1 => self.system_privileges.grant(system_privilege),
-                -1 => self.system_privileges.revoke(&system_privilege),
-                _ => unreachable!("invalid diff: {diff}"),
-            },
-            StateUpdateKind::SystemConfiguration(system_configuration) => {
-                let res = match diff {
-                    1 => self.insert_system_configuration(
-                        &system_configuration.name,
-                        VarInput::Flat(&system_configuration.value),
-                    ),
-                    -1 => self.remove_system_configuration(&system_configuration.name),
-                    _ => unreachable!("invalid diff: {diff}"),
-                };
-                match res {
-                    Ok(_) => (),
-                    // When system variables are deleted, nothing deletes them from the underlying
-                    // durable catalog, which isn't great. Still, we need to be able to ignore
-                    // unknown variables.
-                    Err(Error {
-                        kind: ErrorKind::VarError(VarError::UnknownParameter(name)),
-                    }) => {
-                        warn!(%name, "unknown system parameter from catalog storage");
-                    }
-                    Err(e) => panic!("unable to update system variable: {e:?}"),
-                }
-            }
-            StateUpdateKind::Comment(comment) => match diff {
-                1 => {
-                    let prev = self.comments.update_comment(
-                        comment.object_id,
-                        comment.sub_component,
-                        Some(comment.comment),
-                    );
-                    assert_eq!(
-                        prev, None,
-                        "values must be explicitly retracted before inserting a new value"
-                    );
-                }
-                -1 => {
-                    let prev = self.comments.update_comment(
-                        comment.object_id,
-                        comment.sub_component,
-                        None,
-                    );
-                    assert_eq!(
-                        prev,
-                        Some(comment.comment),
-                        "retraction does not match existing value: ({:?}, {:?})",
-                        comment.object_id,
-                        comment.sub_component,
-                    );
-                }
-                _ => unreachable!("invalid diff: {diff}"),
-            },
         }
     }
 
