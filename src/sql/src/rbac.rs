e84a67698b6e80e80382b065a47535b7410ba37b
Joseph Koshakow <koshy44@gmail.com>
2024-04-12 13:24:40 -0400

    RBAC: Fix concurrent role drop check (#26626)
    Previously, RBAC would check that a users role was still valid (i.e. not
    concurrently dropped) before doing anything else.
    4db9a3cf24b2dc7454a9eeafa9644ab2ca305441 moved this check so that it
    happened after collecting the role membership. As a result, it was
    possible to try and look up a role in the catalog before checking that
    the role exists. This would cause a panic instead of a user error.
    
    This commit fixes the described issue by moving the valid role check
    back to being the first thing that RBAC does.
    
    Fixes #26622

--- src/sql/src/rbac.rs
+++ src/sql/src/rbac.rs
@@ -38,11 +38,10 @@ use crate::session::user::{MZ_SUPPORT_ROLE_ID, MZ_SYSTEM_ROLE_ID, SUPPORT_USER, 
 use crate::session::vars::SystemVars;
 
 /// Common checks that need to be performed before we can start checking a role's privileges.
-fn rbac_preamble(
+fn rbac_check_preamble(
     catalog: &impl SessionCatalog,
     session_meta: &dyn SessionMetadata,
-    rbac_requirements: RbacRequirements,
-) -> Result<RbacRequirements, UnauthorizedError> {
+) -> Result<(), UnauthorizedError> {
     // PostgreSQL allows users that have their role dropped to perform some actions,
     // such as `SET ROLE` and certain `SELECT` queries. We haven't implemented
     // `SET ROLE` and feel it's safer to force to user to re-authenticate if their
@@ -72,6 +71,15 @@ fn rbac_preamble(
         ));
     };
 
+    Ok(())
+}
+
+/// Filters `RbacRequirements` based on the session role metadata and RBAC related feature flags.
+fn filter_requirements(
+    catalog: &impl SessionCatalog,
+    session_meta: &dyn SessionMetadata,
+    rbac_requirements: RbacRequirements,
+) -> RbacRequirements {
     // Skip RBAC non-mandatory checks if RBAC is disabled. However, we never skip RBAC checks for
     // system roles. This allows us to limit access of system users even when RBAC is off.
     let is_rbac_disabled = !is_rbac_enabled_for_session(catalog.system_vars(), session_meta)
@@ -80,10 +88,10 @@ fn rbac_preamble(
     // Skip RBAC checks on user items if the session is a superuser.
     let is_superuser = session_meta.is_superuser();
     if is_rbac_disabled || is_superuser {
-        return Ok(rbac_requirements.filter_to_mandatory_requirements());
+        return rbac_requirements.filter_to_mandatory_requirements();
     }
 
-    Ok(rbac_requirements)
+    rbac_requirements
 }
 
 // The default item types that most statements require USAGE privileges for.
@@ -284,6 +292,8 @@ pub fn check_usage(
     resolved_ids: &ResolvedIds,
     item_types: &BTreeSet<CatalogItemType>,
 ) -> Result<(), UnauthorizedError> {
+    rbac_check_preamble(catalog, session)?;
+
     // Obtain all roles that the current session is a member of.
     let role_membership = catalog.collect_role_membership(&session.role_metadata().current_role);
 
@@ -308,7 +318,7 @@ pub fn check_usage(
 
     let mut rbac_requirements = RbacRequirements::empty();
     rbac_requirements.privileges = required_privileges;
-    let rbac_requirements = rbac_preamble(catalog, session, rbac_requirements)?;
+    let rbac_requirements = filter_requirements(catalog, session, rbac_requirements);
     let required_privileges = rbac_requirements.privileges;
 
     check_object_privileges(
@@ -331,6 +341,8 @@ pub fn check_plan(
     target_cluster_id: Option<ClusterId>,
     resolved_ids: &ResolvedIds,
 ) -> Result<(), UnauthorizedError> {
+    rbac_check_preamble(catalog, session)?;
+
     let rbac_requirements = generate_rbac_requirements(
         catalog,
         plan,
@@ -338,7 +350,7 @@ pub fn check_plan(
         target_cluster_id,
         session.role_metadata().current_role,
     );
-    let rbac_requirements = rbac_preamble(catalog, session, rbac_requirements)?;
+    let rbac_requirements = filter_requirements(catalog, session, rbac_requirements);
     debug!(
         "rbac requirements {rbac_requirements:?} for plan {:?}",
         PlanKind::from(plan)
