b541583c79ec25846129990041b6427d002f9072
Matt Jibson <matt.jibson@gmail.com>
2024-04-18 15:18:33 +0000

    sql: don't allow silent errors on func impls
    Some typecasts are implemented with SQL. Previously we would silently fail to
    produce those if there was some programming error in the SQL (or the catalog was
    not properly instantiated). Instead loudly complain about the failure.

--- src/sql/src/plan/typeconv.rs
+++ src/sql/src/plan/typeconv.rs
@@ -30,7 +30,7 @@ use crate::plan::scope::Scope;
 fn sql_impl_cast(expr: &'static str) -> CastTemplate {
     let invoke = crate::func::sql_impl(expr);
     CastTemplate::new(move |ecx, _ccx, from_type, _to_type| {
-        let mut out = invoke(ecx.qcx, vec![from_type.clone()]).ok()?;
+        let mut out = invoke(ecx.qcx, vec![from_type.clone()]).expect("must succeed");
         Some(move |e| {
             out.splice_parameters(&[e], 0);
             out
