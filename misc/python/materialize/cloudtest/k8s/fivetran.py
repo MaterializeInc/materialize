88ff617d2195ab5438102e85a3069b78c8926b75
Rainer Niedermayr <rainer@materialize.com>
2024-04-29 13:56:17 -0400

    cloudtests: cleanup

--- misc/python/materialize/cloudtest/k8s/fivetran.py
+++ misc/python/materialize/cloudtest/k8s/fivetran.py
@@ -90,63 +90,10 @@ class FivetranDestinationDeployment(K8sDeployment):
         )
 
 
-# TODO: probably not needed
-# class FivetranDestinationTesterDeployment(K8sDeployment):
-#     def __init__(self, namespace: str, apply_node_selectors: bool) -> None:
-#         super().__init__(namespace)
-#
-#         volume_mounts = [
-#             V1VolumeMount(name="data", mount_path="/data"),
-#         ]
-#
-#         env = [
-#             V1EnvVar(name="GRPC_HOSTNAME", value="fivetran-destination"),
-#         ]
-#
-#         command = ["--port=6874"]
-#
-#         container = V1Container(
-#             name="fivetran-destination-tester",
-#             image=self.image(
-#                 "fivetran-destination-tester",
-#                 tag="mzbuild-VW54BM6M7R2WD6UXNFDMEJJU7SD4TUBI",
-#             ),
-#             env=env,
-#             command=command,
-#             volume_mounts=volume_mounts,
-#         )
-#
-#         node_selector = None
-#         if apply_node_selectors:
-#             node_selector = {"supporting-services": "true"}
-#
-#         template = V1PodTemplateSpec(
-#             metadata=V1ObjectMeta(
-#                 namespace=namespace, labels={"app": "fivetran-destination-tester"}
-#             ),
-#             spec=V1PodSpec(containers=[container], node_selector=node_selector),
-#         )
-#
-#         selector = V1LabelSelector(match_labels={"app": "fivetran-destination-tester"})
-#
-#         spec = V1DeploymentSpec(template=template, selector=selector)
-#
-#         self.deployment = V1Deployment(
-#             api_version="apps/v1",
-#             kind="Deployment",
-#             metadata=V1ObjectMeta(
-#                 name="fivetran-destination-tester", namespace=namespace
-#             ),
-#             spec=spec,
-#         )
-
-
 def fivetran_resources(
     namespace: str = DEFAULT_K8S_NAMESPACE, apply_node_selectors: bool = False
 ) -> list[K8sResource]:
     return [
         FivetranDestination(namespace),
         FivetranDestinationDeployment(namespace, apply_node_selectors),
-        # TODO: probably not needed for td tests
-        # FivetranDestinationTesterDeployment(namespace, apply_node_selectors),
     ]
