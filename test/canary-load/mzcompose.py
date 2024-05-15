7b92c384a2cd1343ba75ab02e45da90a5a71bffc
Rainer Niedermayr <rainer@materialize.com>
2024-05-15 10:27:01 +0200

    qa canary: logging

--- test/canary-load/mzcompose.py
+++ test/canary-load/mzcompose.py
@@ -120,6 +120,10 @@ def workflow_default(c: Composition, parser: WorkflowArgumentParser) -> None:
                     else:
                         raise
                 except FailedTestExecutionError as e:
+                    assert len(e.errors) == 1, "Unexpected error"
+                    print(
+                        f"Test failure occurred ('{e.errors[0].message}'), collecting it, and continuing."
+                    )
                     # collect, continue, and rethrow at the end
                     failures.append(e.errors[0])
 
