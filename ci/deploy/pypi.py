7627949c8bdf01bb1fa7dbd7f65ce7c152cbd998
Gus Wynn <gus@materialize.com>
2024-05-15 09:11:20 -0700

    ci: fix bin/lint on macos

--- ci/deploy/pypi.py
+++ ci/deploy/pypi.py
@@ -7,7 +7,7 @@
 # the Business Source License, use of this software will be governed
 # by the Apache License, Version 2.0.
 
-import distutils.core
+import distutils
 import os
 import sys
 from pathlib import Path
