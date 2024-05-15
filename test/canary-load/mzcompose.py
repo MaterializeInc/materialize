85cbaaab8ba1c57517ae02ff7880227ff17bd01b
nrainer-materialize <129728240+nrainer-materialize@users.noreply.github.com>
2024-05-15 11:20:06 +0200

    qa canary: improve error logging (#27097)

--- test/canary-load/mzcompose.py
+++ test/canary-load/mzcompose.py
@@ -120,8 +120,13 @@ def workflow_default(c: Composition, parser: WorkflowArgumentParser) -> None:
                     else:
                         raise
                 except FailedTestExecutionError as e:
+                    assert len(e.errors) > 0, "Exception contains not errors"
+                    for error in e.errors:
+                        print(
+                            f"Test failure occurred ('{error.message}'), collecting it, and continuing."
+                        )
                     # collect, continue, and rethrow at the end
-                    failures.append(e.errors[0])
+                    failures.extend(e.errors)
 
             if len(failures) > 0:
                 raise FailedTestExecutionError(
