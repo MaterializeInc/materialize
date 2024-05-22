eaaf73d6319b79eaa45648794684135e175141aa
Michael Greenberg <michael.greenberg@materialize.com>
2024-05-22 09:36:01 -0400

    lifetime fix #1
    Co-authored-by: Moritz Hoffmann <antiguru@gmail.com>

--- src/expr/src/scalar/func/impls/string.rs
+++ src/expr/src/scalar/func/impls/string.rs
@@ -668,7 +668,7 @@ impl<'a> EagerUnaryFunc<'a> for CastStringToVarChar {
     type Input = &'a str;
     type Output = Result<VarChar<&str>, EvalError>;
 
-    fn call(&self, a: &'a str) -> Result<VarChar<&str>, EvalError> {
+    fn call(&self, a: &'a str) -> Result<VarChar<&'a str>, EvalError> {
         let s =
             mz_repr::adt::varchar::format_str(a, self.length, self.fail_on_len).map_err(|_| {
                 assert!(self.fail_on_len);
