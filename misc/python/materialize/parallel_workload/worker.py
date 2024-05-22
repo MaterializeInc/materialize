10c1885d3191e3bf4db28ca2ec61fb174b6837dd
Rainer Niedermayr <rainer@materialize.com>
2024-05-22 14:58:43 +0200

    parallel workload: minor

--- misc/python/materialize/parallel_workload/worker.py
+++ misc/python/materialize/parallel_workload/worker.py
@@ -109,9 +109,9 @@ class Worker:
                 if action.run(self.exe):
                     self.num_queries[type(action)] += 1
             except QueryError as e:
-                for error in action.errors_to_ignore(self.exe):
-                    if error in e.msg:
-                        self.ignored_errors[error][type(action)] += 1
+                for error_to_ignore in action.errors_to_ignore(self.exe):
+                    if error_to_ignore in e.msg:
+                        self.ignored_errors[error_to_ignore][type(action)] += 1
                         if (
                             "Please disconnect and re-connect" in e.msg
                             or "network error" in e.msg
