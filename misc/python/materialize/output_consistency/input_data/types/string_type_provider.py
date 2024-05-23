080e6f63dd22510482d7ae846ed4fb1a04b6602b
Rainer Niedermayr <rainer@materialize.com>
2024-05-23 11:07:10 +0200

    output consistency: string type: add VARCHAR, CHAR, BPCHAR

--- misc/python/materialize/output_consistency/input_data/types/string_type_provider.py
+++ misc/python/materialize/output_consistency/input_data/types/string_type_provider.py
@@ -18,15 +18,21 @@ from materialize.output_consistency.input_data.return_specs.string_return_spec i
 from materialize.output_consistency.operation.return_type_spec import ReturnTypeSpec
 
 TEXT_TYPE_IDENTIFIER = "TEXT"
+VARCHAR_8_TYPE_IDENTIFIER = "VARCHAR_8"
+CHAR_8_TYPE_IDENTIFIER = "CHAR_8"
+BPCHAR_TYPE_IDENTIFIER = "BPCHAR"
 
 
 class StringDataType(DataType):
-    def __init__(self, internal_identifier: str, type_name: str):
+    def __init__(
+        self, internal_identifier: str, type_name: str, only_few_values: bool = False
+    ):
         super().__init__(
             internal_identifier,
             type_name,
             DataTypeCategory.STRING,
         )
+        self.only_few_values = only_few_values
 
     def resolve_return_type_spec(
         self, characteristics: set[ExpressionCharacteristics]
@@ -36,7 +42,18 @@ class StringDataType(DataType):
 
 
 TEXT_DATA_TYPE = StringDataType(TEXT_TYPE_IDENTIFIER, "TEXT")
+VARCHAR_8_DATA_TYPE = StringDataType(VARCHAR_8_TYPE_IDENTIFIER, "VARCHAR(8)")
+CHAR_8_DATA_TYPE = StringDataType(
+    CHAR_8_TYPE_IDENTIFIER, "CHAR(8)", only_few_values=True
+)
+BPCHAR_DATA_TYPE = StringDataType(
+    BPCHAR_TYPE_IDENTIFIER, "BPCHAR", only_few_values=True
+)
 
 
-STRING_DATA_TYPES: list[StringDataType] = []
-STRING_DATA_TYPES.append(TEXT_DATA_TYPE)
+STRING_DATA_TYPES: list[StringDataType] = [
+    TEXT_DATA_TYPE,
+    VARCHAR_8_DATA_TYPE,
+    CHAR_8_DATA_TYPE,
+    BPCHAR_DATA_TYPE,
+]
