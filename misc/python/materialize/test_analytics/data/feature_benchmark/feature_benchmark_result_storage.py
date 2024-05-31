e933fad94d9229444a3570e06b40ea2d4ceceec9
Rainer Niedermayr <rainer@materialize.com>
2024-05-31 17:22:55 +0200

    feature benchmark: store results

---
+++ misc/python/materialize/test_analytics/data/feature_benchmark/feature_benchmark_result_storage.py
@@ -0,0 +1,64 @@
+# Copyright Materialize, Inc. and contributors. All rights reserved.
+#
+# Use of this software is governed by the Business Source License
+# included in the LICENSE file at the root of this repository.
+#
+# As of the Change Date specified in that file, in accordance with
+# the Business Source License, use of this software will be governed
+# by the Apache License, Version 2.0.
+from dataclasses import dataclass
+
+from pg8000 import Cursor
+
+from materialize import buildkite
+from materialize.buildkite import BuildkiteEnvVar
+from materialize.test_analytics.connection.test_analytics_connection import (
+    execute_updates,
+)
+
+
+@dataclass
+class FeatureBenchmarkResultEntry:
+    scenario_name: str
+    scenario_version: str
+    wallclock: float
+    messages: int
+    memory: float
+
+
+def insert_result(
+    cursor: Cursor,
+    framework_version: str,
+    results: list[FeatureBenchmarkResultEntry],
+    verbose: bool = False,
+) -> None:
+    step_id = buildkite.get_var(BuildkiteEnvVar.BUILDKITE_STEP_ID)
+
+    sql_statements = []
+
+    for result_entry in results:
+        sql_statements.append(
+            f"""
+            INSERT INTO feature_benchmark_results
+            (
+                build_step_id,
+                framework_version,
+                scenario_name,
+                scenario_version,
+                wallclock,
+                messages,
+                memory
+            )
+            SELECT
+                '{step_id}',
+                '{framework_version}',
+                '{result_entry.scenario_name}',
+                '{result_entry.scenario_version}',
+                {result_entry.wallclock},
+                {result_entry.messages},
+                {result_entry.memory}
+            ;
+            """
+        )
+
+    execute_updates(sql_statements, cursor, verbose)
