16286718739c480abcf0f81dff37f9141463022e
Parker Timmerman <parker.timmerman@materialize.com>
2024-06-04 13:56:43 -0400

    tests: constrain proptest strategies to reduce flakiness (#27440)
    This PR constrains some of the `ScalarType` and `Datum` proptest
    strategies to reduce edge case strategies that could blow up to very
    large sizes and cause timeouts
    
    ### Motivation
    
    Fixes https://github.com/MaterializeInc/materialize/issues/27370
    Fixes https://github.com/MaterializeInc/materialize/issues/27430
    
    ### Checklist
    
    - [ ] This PR has adequate test coverage / QA involvement has been duly
    considered. ([trigger-ci for additional test/nightly
    runs](https://trigger-ci.dev.materialize.com/))
    - [ ] This PR has an associated up-to-date [design
    doc](https://github.com/MaterializeInc/materialize/blob/main/doc/developer/design/README.md),
    is a design doc
    ([template](https://github.com/MaterializeInc/materialize/blob/main/doc/developer/design/00000000_template.md)),
    or is sufficiently small to not require a design.
      <!-- Reference the design in the description. -->
    - [ ] If this PR evolves [an existing `$T ⇔ Proto$T`
    mapping](https://github.com/MaterializeInc/materialize/blob/main/doc/developer/command-and-response-binary-encoding.md)
    (possibly in a backwards-incompatible way), then it is tagged with a
    `T-proto` label.
    - [ ] If this PR will require changes to cloud orchestration or tests,
    there is a companion cloud PR to account for those changes that is
    tagged with the release-blocker label
    ([example](https://github.com/MaterializeInc/cloud/pull/5021)).
    <!-- Ask in #team-cloud on Slack if you need help preparing the cloud
    PR. -->
    - [x] This PR includes the following [user-facing behavior
    changes](https://github.com/MaterializeInc/materialize/blob/main/doc/developer/guide-changes.md#what-changes-require-a-release-note):
      - N/a

--- src/repr/src/adt/mz_acl_item.rs
+++ src/repr/src/adt/mz_acl_item.rs
@@ -24,7 +24,7 @@ use mz_ore::str::StrExt;
 use mz_persist_types::columnar::FixedSizeCodec;
 use mz_proto::{RustType, TryFromProtoError};
 use proptest::arbitrary::Arbitrary;
-use proptest::proptest;
+use proptest::prelude::*;
 use proptest::strategy::{BoxedStrategy, Strategy};
 use proptest_derive::Arbitrary;
 use serde::{Deserialize, Serialize};
@@ -1144,6 +1144,7 @@ fn proptest_packed_acl_item_roundtrips() {
 }
 
 #[mz_ore::test]
+#[cfg_attr(miri, ignore)] // slow
 fn proptest_packed_acl_item_sorts() {
     fn sort_acl_items(mut og: Vec<AclItem>) {
         let mut packed: Vec<_> = og.iter().copied().map(PackedAclItem::from_value).collect();
@@ -1155,7 +1156,7 @@ fn proptest_packed_acl_item_sorts() {
         assert_eq!(og, rnd);
     }
 
-    proptest!(|(acl_items in proptest::arbitrary::any::<Vec<AclItem>>())| {
+    proptest!(|(acl_items in proptest::collection::vec(any::<AclItem>(), 0..64))| {
         sort_acl_items(acl_items);
     });
 }
@@ -1174,7 +1175,7 @@ fn proptest_packed_mz_acl_item_roundtrips() {
 }
 
 #[mz_ore::test]
-#[ignore] // TODO: Reenable when #27370 is fixed
+#[cfg_attr(miri, ignore)] // slow
 fn proptest_packed_mz_acl_item_sorts() {
     fn sort_mz_acl_items(mut og: Vec<MzAclItem>) {
         let mut packed: Vec<_> = og
@@ -1193,7 +1194,7 @@ fn proptest_packed_mz_acl_item_sorts() {
         assert_eq!(og, rnd);
     }
 
-    proptest!(|(acl_items in proptest::arbitrary::any::<Vec<MzAclItem>>())| {
+    proptest!(|(acl_items in proptest::collection::vec(any::<MzAclItem>(), 0..64))| {
         sort_mz_acl_items(acl_items);
     });
 }
