fd59ee719966189721712169cf1c4ce99dc0c34c
Rainer Niedermayr <rainer@materialize.com>
2024-06-10 09:12:19 +0200

    test analytics: use PRODUCTION_ANALYTICS_APP_PASSWORD

--- misc/python/materialize/test_analytics/config/test_analytics_db_config.py
+++ misc/python/materialize/test_analytics/config/test_analytics_db_config.py
@@ -18,10 +18,10 @@ def create_test_analytics_config(c: Composition) -> MzDbConfig:
     username = os.getenv(
         "CANARY_LOADTEST_USERNAME", "infra+qacanaryload@materialize.io"
     )
-    app_password = os.environ["CANARY_LOADTEST_APP_PASSWORD"]
+    app_password = os.environ["PRODUCTION_ANALYTICS_APP_PASSWORD"]
     hostname = get_cloud_hostname(c, app_password=app_password)
-    database = "test_analytics"
-    search_path = "public"
+    database = "raw"
+    search_path = "test_analytics"
 
     return MzDbConfig(
         hostname=hostname,
@@ -35,10 +35,10 @@ def create_test_analytics_config(c: Composition) -> MzDbConfig:
 def create_local_dev_config() -> MzDbConfig:
     hostname = os.environ["MATERIALIZE_PROD_SANDBOX_HOSTNAME"]
     username = os.environ["MATERIALIZE_PROD_SANDBOX_USERNAME"]
-    app_password = os.environ["MATERIALIZE_PROD_SANDBOX_APP_PASSWORD"]
+    app_password = os.environ["PRODUCTION_ANALYTICS_APP_PASSWORD"]
 
-    database = "test_analytics"
-    search_path = "public"
+    database = "raw"
+    search_path = "test_analytics"
 
     return MzDbConfig(
         hostname=hostname,
