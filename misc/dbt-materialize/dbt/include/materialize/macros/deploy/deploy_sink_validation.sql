bc3ec8ddca3b6193f9cf04fc552789e54fb281d9
Bobby Iliev <bobby@bobbyiliev.com>
2024-06-11 19:14:13 +0300

    Change source reference to upstream relation

--- misc/dbt-materialize/dbt/include/materialize/macros/deploy/deploy_sink_validation.sql
+++ misc/dbt-materialize/dbt/include/materialize/macros/deploy/deploy_sink_validation.sql
@@ -16,7 +16,7 @@
 {% macro deploy_sink_validation() %}
 -- Deploy Sink Validation
 -- This macro queries all sinks and their dependent objects in the current database and specified schema.
--- It prints debug information including the list of sinks to be altered, their current dependent objects, 
+-- It prints debug information including the list of sinks to be altered, their current dependent objects,
 -- and the new dependent objects from the _dbt_deploy schema.
 
 {% set current_target_name = target.name %}
@@ -33,44 +33,44 @@
 
 -- Loop through each schema and print sink details
 {% for schema in schemas %}
-    {% set sinks_and_sources_query %}
+    {% set sinks_and_upstream_relations_query %}
     SELECT
         mz_object_dependencies.object_id,
         mz_object_dependencies.referenced_object_id,
         mz_objects.name AS sink_name,
         mz_schemas.name AS sink_schema_name,
         mz_databases.name AS database_name,
-        source_objects.name AS source_name,
-        source_schemas.name AS source_schema_name,
-        source_objects.type AS source_type
+        upstream_relations.name AS source_name,
+        upstream_relation_schemas.name AS source_schema_name,
+        upstream_relations.type AS source_type
     FROM mz_internal.mz_object_dependencies
     JOIN mz_objects ON mz_object_dependencies.object_id = mz_objects.id
     JOIN mz_schemas ON mz_objects.schema_id = mz_schemas.id
     JOIN mz_databases ON mz_schemas.database_id = mz_databases.id
-    JOIN mz_objects AS source_objects ON mz_object_dependencies.referenced_object_id = source_objects.id
-    JOIN mz_schemas AS source_schemas ON source_objects.schema_id = source_schemas.id
+    JOIN mz_objects AS upstream_relations ON mz_object_dependencies.referenced_object_id = upstream_relations.id
+    JOIN mz_schemas AS upstream_relation_schemas ON upstream_relations.schema_id = upstream_relation_schemas.id
     WHERE mz_objects.type = 'sink'
-      AND source_objects.type IN ('table', 'materialized-view', 'source')
+      AND upstream_relations.type IN ('table', 'materialized-view', 'source')
       AND mz_databases.name = current_database()
       AND mz_schemas.name = {{ dbt.string_literal(schema) }};
     {% endset %}
 
-    {% set sinks_and_sources = run_query(sinks_and_sources_query) %}
+    {% set sinks_and_upstream_relations = run_query(sinks_and_upstream_relations_query) %}
     {% if execute %}
         -- Print debug information
-        {{ log("Sinks and their sources in schema: " ~ schema, info=True) }}
-        {% for sink in sinks_and_sources.rows %}
-            {% set sink_name = adapter.quote(sink[2]) %}
-            {% set sink_schema_name = adapter.quote(sink[3]) %}
-            {% set source_name = adapter.quote(sink[5]) %}
-            {% set source_schema = sink[6] %}
-            {% set deploy_schema = adapter.quote(source_schema ~ "_dbt_deploy") %}
-            {% set new_source = deploy_schema ~ '.' ~ source_name %}
+        {{ log("Sinks and their upstream relations in schema: " ~ schema, info=True) }}
+        {% for sink in sinks_and_upstream_relations.rows %}
+            {% set sink_name = sink[2] %}
+            {% set sink_schema_name = sink[3] %}
+            {% set upstream_relation_name = sink[5] %}
+            {% set upstream_relation_schema = sink[6] %}
+            {% set deploy_schema = upstream_relation_schema ~ "_dbt_deploy" %}
+            {% set new_upstream_relation = deploy_schema ~ '.' ~ upstream_relation_name %}
             {{ log("  Sink: " ~ sink_name ~ " (Schema: " ~ schema ~ ")", info=True) }}
-            {{ log("    Current Source: " ~ source_schema ~ '.' ~ source_name, info=True) }}
-            {{ log("    New Source: " ~ new_source, info=True) }}
-            {{ log("    The sink will be altered to use the new source:", info=True) }}
-            {{ log("    ALTER SINK " ~ sink_schema_name ~ "." ~ sink_name ~ " SET FROM " ~ new_source, info=True) }}
+            {{ log("    Current upstream relation: " ~ upstream_relation_schema ~ '.' ~ upstream_relation_name, info=True) }}
+            {{ log("    New upstream relation: " ~ new_upstream_relation, info=True) }}
+            {{ log("    The sink will be altered to use the new upstream relation:", info=True) }}
+            {{ log("    ALTER SINK " ~ sink_schema_name ~ "." ~ sink_name ~ " SET FROM " ~ new_upstream_relation, info=True) }}
         {% endfor %}
     {% endif %}
 {% endfor %}
