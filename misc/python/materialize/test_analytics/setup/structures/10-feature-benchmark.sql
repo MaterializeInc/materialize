074105620d6d06fd2ef590a54104fd726ff76a1f
Rainer Niedermayr <rainer@materialize.com>
2024-06-13 14:13:13 +0200

    test analytics: feature benchmark: add view

--- misc/python/materialize/test_analytics/setup/structures/10-feature-benchmark.sql
+++ misc/python/materialize/test_analytics/setup/structures/10-feature-benchmark.sql
@@ -27,3 +27,30 @@ CREATE TABLE feature_benchmark_result (
    memory_mz DOUBLE,
    memory_clusterd DOUBLE
 );
+
+CREATE VIEW v_feature_benchmark_result_per_day AS
+    SELECT
+        branch,
+        scenario_name,
+        framework_version,
+        scenario_version,
+        scale,
+        date_trunc('day', date) AS day,
+        max(wallclock) AS max_wallclock,
+        max(messages) AS max_messages,
+        max(memory_mz) AS max_memory_mz,
+        max(memory_clusterd) AS max_memory_clusterd
+    FROM feature_benchmark_result r
+    INNER JOIN build b
+    ON b.build_id = r.build_id
+    INNER JOIN build_step bs
+    ON bs.build_step_id = r.build_step_id
+    WHERE bs.is_latest_retry = TRUE
+    GROUP BY
+        branch,
+        scenario_name,
+        framework_version,
+        scenario_version,
+        scale,
+        date_trunc('day', date)
+;
