9d6f060a7aafcd39a26ea2b52d1430835b8b5c25
nrainer-materialize <129728240+nrainer-materialize@users.noreply.github.com>
2024-06-18 07:33:37 +0200

    tests: kafka-sink-topic-config.td: disable status check when using Redpanda (#27704)

--- test/cloudtest/test_full_testdrive.py
+++ test/cloudtest/test_full_testdrive.py
@@ -30,4 +30,4 @@ def test_full_testdrive(mz: MaterializeApplication) -> None:
     # TODO: #26392 (test requires fivetran running in cloudtest)
     mz.testdrive.delete("/workdir/testdrive/fivetran-destination.td")
 
-    mz.testdrive.run(f"testdrive/{file_pattern}")
+    mz.testdrive.run("--var=uses-redpanda=True", f"testdrive/{file_pattern}")
