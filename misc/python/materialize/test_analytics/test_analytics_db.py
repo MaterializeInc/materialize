fab8da1f94d67842ce6a1dc04bd770a5f550e08e
Rainer Niedermayr <rainer@materialize.com>
2024-06-18 15:24:19 +0200

    test analytics: increase current and minimal supported data version

--- misc/python/materialize/test_analytics/test_analytics_db.py
+++ misc/python/materialize/test_analytics/test_analytics_db.py
@@ -23,7 +23,7 @@ from materialize.test_analytics.data.scalability_framework.scalability_framework
     ScalabilityFrameworkResultStorage,
 )
 
-TEST_ANALYTICS_DATA_VERSION: int = 4
+TEST_ANALYTICS_DATA_VERSION: int = 5
 
 
 class TestAnalyticsDb:
