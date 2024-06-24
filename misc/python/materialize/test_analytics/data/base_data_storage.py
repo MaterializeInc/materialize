484ac89b94c5300a81b570c1b15f8c50295fcc72
Rainer Niedermayr <rainer@materialize.com>
2024-06-24 15:13:14 +0200

    test analytics: add query data functionality

--- misc/python/materialize/test_analytics/data/base_data_storage.py
+++ misc/python/materialize/test_analytics/data/base_data_storage.py
@@ -6,7 +6,8 @@
 # As of the Change Date specified in that file, in accordance with
 # the Business Source License, use of this software will be governed
 # by the Apache License, Version 2.0.
-
+from collections.abc import Sequence
+from typing import Any
 
 from materialize.test_analytics.connector.test_analytics_connector import (
     DatabaseConnector,
@@ -17,3 +18,17 @@ class BaseDataStorage:
 
     def __init__(self, database_connector: DatabaseConnector):
         self.database_connector = database_connector
+
+    def query_data(self, query: str, verbose: bool = False) -> Sequence[Sequence[Any]]:
+        cursor = self.database_connector.create_cursor(allow_reusing_connection=True)
+
+        if verbose:
+            print(f"Executing query: {query}")
+
+        cursor.execute(query)
+        result = cursor.fetchall()
+
+        if verbose:
+            print(f"Query returned {len(result)} rows")
+
+        return result
