58492911eb965bc21fde2f123bdaf6059b28a071
Parker Timmerman <parker.timmerman@materialize.com>
2024-06-25 11:51:51 -0400

    start, add FixedSizeBytesStats

--- src/persist-types/src/stats/bytes.rs
+++ src/persist-types/src/stats/bytes.rs
@@ -10,16 +10,18 @@
 use std::fmt::Debug;
 
 use arrow::array::BinaryArray;
-use mz_proto::{ProtoType, RustType, TryFromProtoError};
-use proptest::strategy::{Strategy, Union};
+use mz_proto::{IntoRustIfSome, ProtoType, RustType, TryFromProtoError};
+use proptest::strategy::{Just, Strategy, Union};
+use serde::Serialize;
 
 use crate::columnar::Data;
 use crate::dyn_struct::ValidityRef;
 use crate::stats::json::{any_json_stats, JsonStats};
 use crate::stats::primitive::{any_primitive_vec_u8_stats, PrimitiveStats};
 use crate::stats::{
-    proto_bytes_stats, ColumnStatKinds, ColumnStats, ColumnarStats, DynStats, OptionStats,
-    ProtoAtomicBytesStats, ProtoBytesStats, StatsFrom, TrimStats,
+    proto_bytes_stats, proto_fixed_size_bytes_stats, ColumnStatKinds, ColumnStats, ColumnarStats,
+    DynStats, OptionStats, ProtoAtomicBytesStats, ProtoBytesStats, ProtoFixedSizeBytesStats,
+    StatsFrom, TrimStats,
 };
 
 /// `PrimitiveStats<Vec<u8>>` that cannot safely be trimmed.
@@ -56,12 +58,100 @@ impl RustType<ProtoAtomicBytesStats> for AtomicBytesStats {
     }
 }
 
+/// `PrimitiveStats<Vec<u8>>` for types that implement [`FixedSizeCodec`] and
+/// cannot safely be trimmed.
+///
+/// [`FixedSizeCodec`]: crate::columnar::FixedSizeCodec
+#[derive(Debug, Clone)]
+pub struct FixedSizeBytesStats {
+    /// See [PrimitiveStats::lower]
+    lower: Vec<u8>,
+    /// See [PrimitiveStats::upper]
+    upper: Vec<u8>,
+    /// The kind of data these stats represent.
+    kind: FixedSizeBytesStatsKind,
+}
+
+impl FixedSizeBytesStats {
+    fn debug_json(&self) -> serde_json::Value {
+        serde_json::json!({
+            "lower": hex::encode(&self.lower),
+            "upper": hex::encode(&self.upper),
+            "kind": self.kind,
+        })
+    }
+}
+
+impl RustType<ProtoFixedSizeBytesStats> for FixedSizeBytesStats {
+    fn into_proto(&self) -> ProtoFixedSizeBytesStats {
+        ProtoFixedSizeBytesStats {
+            lower: self.lower.into_proto(),
+            upper: self.upper.into_proto(),
+            kind: Some(self.kind.into_proto()),
+        }
+    }
+
+    fn from_proto(proto: ProtoFixedSizeBytesStats) -> Result<Self, TryFromProtoError> {
+        Ok(FixedSizeBytesStats {
+            lower: proto.lower.into_rust()?,
+            upper: proto.upper.into_rust()?,
+            kind: proto
+                .kind
+                .into_rust_if_some("missing field ProtoFixedSizeBytesStats::kind")?,
+        })
+    }
+}
+
+/// The type of data encoded in an [`PackedBytesStats`].
+#[derive(Debug, Clone, Serialize)]
+#[serde(rename_all = "kebab-case")]
+pub enum FixedSizeBytesStatsKind {
+    PackedTime,
+    PackedInterval,
+    PackedNumeric,
+    Uuid,
+}
+
+impl RustType<proto_fixed_size_bytes_stats::Kind> for FixedSizeBytesStatsKind {
+    fn into_proto(&self) -> proto_fixed_size_bytes_stats::Kind {
+        match self {
+            FixedSizeBytesStatsKind::PackedTime => {
+                proto_fixed_size_bytes_stats::Kind::PackedTime(())
+            }
+            FixedSizeBytesStatsKind::PackedInterval => {
+                proto_fixed_size_bytes_stats::Kind::PackedInterval(())
+            }
+            FixedSizeBytesStatsKind::PackedNumeric => {
+                proto_fixed_size_bytes_stats::Kind::PackedNumeric(())
+            }
+            FixedSizeBytesStatsKind::Uuid => proto_fixed_size_bytes_stats::Kind::Uuid(()),
+        }
+    }
+
+    fn from_proto(proto: proto_fixed_size_bytes_stats::Kind) -> Result<Self, TryFromProtoError> {
+        let kind = match proto {
+            proto_fixed_size_bytes_stats::Kind::PackedTime(_) => {
+                FixedSizeBytesStatsKind::PackedTime
+            }
+            proto_fixed_size_bytes_stats::Kind::PackedInterval(_) => {
+                FixedSizeBytesStatsKind::PackedInterval
+            }
+            proto_fixed_size_bytes_stats::Kind::PackedNumeric(_) => {
+                FixedSizeBytesStatsKind::PackedNumeric
+            }
+            proto_fixed_size_bytes_stats::Kind::Uuid(_) => FixedSizeBytesStatsKind::Uuid,
+        };
+        Ok(kind)
+    }
+}
+
 /// Statistics about a column of `Vec<u8>`.
 #[derive(Clone)]
 pub enum BytesStats {
     Primitive(PrimitiveStats<Vec<u8>>),
     Json(JsonStats),
     Atomic(AtomicBytesStats),
+    FixedSize(FixedSizeBytesStats),
 }
 
 impl Debug for BytesStats {
@@ -76,6 +166,7 @@ impl DynStats for BytesStats {
             BytesStats::Primitive(x) => x.debug_json(),
             BytesStats::Json(x) => x.debug_json(),
             BytesStats::Atomic(x) => x.debug_json(),
+            BytesStats::FixedSize(x) => x.debug_json(),
         }
     }
 
@@ -93,6 +184,7 @@ impl ColumnStats<Vec<u8>> for BytesStats {
             BytesStats::Primitive(x) => Some(x.lower.as_slice()),
             BytesStats::Json(_) => None,
             BytesStats::Atomic(x) => Some(&x.lower),
+            BytesStats::FixedSize(x) => Some(&x.lower),
         }
     }
     fn upper<'a>(&'a self) -> Option<<Vec<u8> as Data>::Ref<'a>> {
@@ -100,6 +192,7 @@ impl ColumnStats<Vec<u8>> for BytesStats {
             BytesStats::Primitive(x) => Some(x.upper.as_slice()),
             BytesStats::Json(_) => None,
             BytesStats::Atomic(x) => Some(&x.upper),
+            BytesStats::FixedSize(x) => Some(&x.upper),
         }
     }
     fn none_count(&self) -> usize {
@@ -163,6 +256,7 @@ impl RustType<ProtoBytesStats> for BytesStats {
             BytesStats::Primitive(x) => proto_bytes_stats::Kind::Primitive(RustType::into_proto(x)),
             BytesStats::Json(x) => proto_bytes_stats::Kind::Json(RustType::into_proto(x)),
             BytesStats::Atomic(x) => proto_bytes_stats::Kind::Atomic(RustType::into_proto(x)),
+            BytesStats::FixedSize(x) => proto_bytes_stats::Kind::FixedSize(RustType::into_proto(x)),
         };
         ProtoBytesStats { kind: Some(kind) }
     }
@@ -178,6 +272,9 @@ impl RustType<ProtoBytesStats> for BytesStats {
             Some(proto_bytes_stats::Kind::Atomic(x)) => {
                 Ok(BytesStats::Atomic(AtomicBytesStats::from_proto(x)?))
             }
+            Some(proto_bytes_stats::Kind::FixedSize(x)) => {
+                Ok(BytesStats::FixedSize(FixedSizeBytesStats::from_proto(x)?))
+            }
             None => Err(TryFromProtoError::missing_field("ProtoBytesStats::kind")),
         }
     }
@@ -191,6 +288,8 @@ impl TrimStats for ProtoBytesStats {
             Some(Kind::Json(stats)) => stats.trim(),
             // We explicitly don't trim atomic stats!
             Some(Kind::Atomic(_)) => {}
+            // We explicitly don't trim fixed size stats!
+            Some(Kind::FixedSize(_)) => {}
             None => {}
         }
     }
@@ -198,6 +297,13 @@ impl TrimStats for ProtoBytesStats {
 
 /// Returns a [`Strategy`] for generating arbitrary [`ByteStats`].
 pub(crate) fn any_bytes_stats() -> impl Strategy<Value = BytesStats> {
+    let kind_of_packed = Union::new(vec![
+        Just(FixedSizeBytesStatsKind::PackedTime),
+        Just(FixedSizeBytesStatsKind::PackedInterval),
+        Just(FixedSizeBytesStatsKind::PackedNumeric),
+        Just(FixedSizeBytesStatsKind::Uuid),
+    ]);
+
     Union::new(vec![
         any_primitive_vec_u8_stats()
             .prop_map(BytesStats::Primitive)
@@ -208,6 +314,15 @@ pub(crate) fn any_bytes_stats() -> impl Strategy<Value = BytesStats> {
                 BytesStats::Atomic(AtomicBytesStats {
                     lower: x.lower,
                     upper: x.upper,
+                })
+            })
+            .boxed(),
+        (any_primitive_vec_u8_stats(), kind_of_packed)
+            .prop_map(|(x, kind)| {
+                BytesStats::FixedSize(FixedSizeBytesStats {
+                    lower: x.lower,
+                    upper: x.upper,
+                    kind,
                 })
             })
             .boxed(),
