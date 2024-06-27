251f7930a8c740c2d0b12b87e92eff1aa62859af
Rainer Niedermayr <rainer@materialize.com>
2024-06-27 09:39:34 +0200

    test analytics: views: v_feature_benchmark_result_per_day and _per_week: also min and avg values

--- misc/python/materialize/test_analytics/setup/views/10-feature-benchmark.sql
+++ misc/python/materialize/test_analytics/setup/views/10-feature-benchmark.sql
@@ -21,9 +21,17 @@ CREATE OR REPLACE VIEW v_feature_benchmark_result_per_day AS
         res.scenario_version,
         res.scale,
         date_trunc('day', b.date) AS day,
+        min(res.wallclock) AS min_wallclock,
+        avg(res.wallclock) AS avg_wallclock,
         max(res.wallclock) AS max_wallclock,
+        min(res.messages) AS min_messages,
+        avg(res.messages) AS avg_messages,
         max(res.messages) AS max_messages,
+        min(res.memory_mz) AS min_memory_mz,
+        avg(res.memory_mz) AS avg_memory_mz,
         max(res.memory_mz) AS max_memory_mz,
+        min(res.memory_clusterd) AS min_memory_clusterd,
+        avg(res.memory_clusterd) AS avg_memory_clusterd,
         max(res.memory_clusterd) AS max_memory_clusterd
     FROM feature_benchmark_result res
     INNER JOIN build_job bj
@@ -48,9 +56,17 @@ CREATE OR REPLACE VIEW v_feature_benchmark_result_per_week AS
         res.scenario_version,
         res.scale,
         date_trunc('week', b.date) AS day,
+        min(res.wallclock) AS min_wallclock,
+        avg(res.wallclock) AS avg_wallclock,
         max(res.wallclock) AS max_wallclock,
+        min(res.messages) AS min_messages,
+        avg(res.messages) AS avg_messages,
         max(res.messages) AS max_messages,
+        min(res.memory_mz) AS min_memory_mz,
+        avg(res.memory_mz) AS avg_memory_mz,
         max(res.memory_mz) AS max_memory_mz,
+        min(res.memory_clusterd) AS min_memory_clusterd,
+        avg(res.memory_clusterd) AS avg_memory_clusterd,
         max(res.memory_clusterd) AS max_memory_clusterd
     FROM feature_benchmark_result res
     INNER JOIN build_job bj
