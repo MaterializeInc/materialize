857179fe59bb7d806b4e1fad972f19374ddfe4f5
Steffen Hausmann <steffen@hausmann.info>
2024-06-30 18:55:25 +0200

    Correct comment

--- src/sql/src/func.rs
+++ src/sql/src/func.rs
@@ -4706,7 +4706,7 @@ pub static OP_IMPLS: Lazy<BTreeMap<&'static str, Func>> = Lazy::new(|| {
             params!(ListElementAnyCompatible, ListAnyCompatible) => ElementListConcat => ListAnyCompatible, oid::OP_CONCAT_ELEMENY_LIST_OID;
         },
 
-        // JSON, MAP, RANGE
+        // JSON, MAP, RANGE, LIST
         "->" => Scalar {
             params!(Jsonb, Int64) => JsonbGetInt64 { stringify: false } => Jsonb, 3212;
             params!(Jsonb, String) => JsonbGetString { stringify: false } => Jsonb, 3211;
