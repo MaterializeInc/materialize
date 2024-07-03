c6808878099536681b5a2e861a34c6d3e2b399fc
Rainer Niedermayr <rainer@materialize.com>
2024-07-03 18:32:22 +0200

    test analytics: views: add v_scalability_framework_result_per_month

--- misc/python/materialize/test_analytics/setup/views/11-scalability-framework.sql
+++ misc/python/materialize/test_analytics/setup/views/11-scalability-framework.sql
@@ -74,3 +74,34 @@ CREATE OR REPLACE VIEW v_scalability_framework_result_per_week AS
         res.concurrency,
         date_trunc('week', b.date)
 ;
+
+CREATE OR REPLACE VIEW v_scalability_framework_result_per_month AS
+    SELECT
+        b.branch,
+        res.workload_name,
+        res.workload_group,
+        res.framework_version,
+        res.workload_version,
+        res.concurrency,
+        date_trunc('month', b.date) AS month,
+        min(res.count) AS min_count,
+        avg(res.count) AS avg_count,
+        max(res.count) AS max_count,
+        min(res.tps) AS min_tps,
+        avg(res.tps) AS avg_tps,
+        max(res.tps) AS max_tps
+    FROM scalability_framework_result res
+    INNER JOIN build_job bj
+    ON bj.build_job_id = res.build_job_id
+    INNER JOIN build b
+    ON b.build_id = bj.build_id
+    WHERE bj.is_latest_retry = TRUE
+    GROUP BY
+        b.branch,
+        res.workload_name,
+        res.workload_group,
+        res.framework_version,
+        res.workload_version,
+        res.concurrency,
+        date_trunc('month', b.date)
+;
