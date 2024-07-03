a626c2da4a43242adac529e13005a4b909256021
Rainer Niedermayr <rainer@materialize.com>
2024-07-03 13:06:31 +0200

    test analytics: views: add v_feature_benchmark_result_per_month

--- misc/python/materialize/test_analytics/setup/views/10-feature-benchmark.sql
+++ misc/python/materialize/test_analytics/setup/views/10-feature-benchmark.sql
@@ -82,3 +82,38 @@ CREATE OR REPLACE VIEW v_feature_benchmark_result_per_week AS
         res.scale,
         date_trunc('week', b.date)
 ;
+
+CREATE OR REPLACE VIEW v_feature_benchmark_result_per_month AS
+    SELECT
+        b.branch,
+        res.scenario_name,
+        res.framework_version,
+        res.scenario_version,
+        res.scale,
+        date_trunc('month', b.date) AS month,
+        min(res.wallclock) AS min_wallclock,
+        avg(res.wallclock) AS avg_wallclock,
+        max(res.wallclock) AS max_wallclock,
+        min(res.messages) AS min_messages,
+        avg(res.messages) AS avg_messages,
+        max(res.messages) AS max_messages,
+        min(res.memory_mz) AS min_memory_mz,
+        avg(res.memory_mz) AS avg_memory_mz,
+        max(res.memory_mz) AS max_memory_mz,
+        min(res.memory_clusterd) AS min_memory_clusterd,
+        avg(res.memory_clusterd) AS avg_memory_clusterd,
+        max(res.memory_clusterd) AS max_memory_clusterd
+    FROM feature_benchmark_result res
+    INNER JOIN build_job bj
+    ON bj.build_job_id = res.build_job_id
+    INNER JOIN build b
+    ON b.build_id = bj.build_id
+    WHERE bj.is_latest_retry = TRUE
+    GROUP BY
+        b.branch,
+        res.scenario_name,
+        res.framework_version,
+        res.scenario_version,
+        res.scale,
+        date_trunc('month', b.date)
+;
