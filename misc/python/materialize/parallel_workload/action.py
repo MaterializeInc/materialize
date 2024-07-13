d95db7ecfa8d3198095e2e35861277ba2a64cb23
Dennis Felsing <dennis@felsing.org>
2024-07-13 07:48:37 +0000

    Temporarily ignore 28219
    to get a chance to run into further errors

--- misc/python/materialize/parallel_workload/action.py
+++ misc/python/materialize/parallel_workload/action.py
@@ -153,6 +153,7 @@ class Action:
                 [
                     "cannot write in read-only mode",
                     "500: internal storage failure! ReadOnly",
+                    "was at a timestamp before its since",  # TODO: #28219
                 ]
             )
         if exe.db.scenario in (
