1dbc32f26b957dbaa78483530078a690a44caf36
Dennis Felsing <dennis@felsing.org>
2024-07-17 23:01:48 +0000

    mzcompose: Support CI_EXTRA_ARGS

--- test/feature-benchmark/mzcompose.py
+++ test/feature-benchmark/mzcompose.py
@@ -480,7 +480,11 @@ def workflow_default(c: Composition, parser: WorkflowArgumentParser) -> None:
         selected_scenarios, lambda scenario_cls: scenario_cls.__name__
     )
 
-    if len(scenarios_scheduled_to_run) == 0 and buildkite.is_in_buildkite():
+    if (
+        len(scenarios_scheduled_to_run) == 0
+        and buildkite.is_in_buildkite()
+        and not os.getenv("CI_EXTRA_ARGS")
+    ):
         raise FailedTestExecutionError(
             error_summary="No scenarios were selected", errors=[]
         )
