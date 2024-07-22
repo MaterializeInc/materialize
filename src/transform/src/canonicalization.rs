63331db4bb7ddf755a117fa640c93700297aebc3
Frank McSherry <fmcsherry@me.com>
2024-07-22 05:25:18 -0400

    Remove `expr` reduction from `FoldConstants`. (#28263)
    Our `FoldConstants` transform aims to replace expressions with constant
    inputs into the constants they represent. Confusingly (to me) it also
    calls `expr.reduce(&types)` on *most* expressions it sees in
    non-constant AST nodes. This .. has nothing to do with constant folding,
    I bet.
    
    ### Motivation
    
    <!--
    Which of the following best describes the motivation behind this PR?
    
      * This PR fixes a recognized bug.
    
        [Ensure issue is linked somewhere.]
    
      * This PR adds a known-desirable feature.
    
        [Ensure issue is linked somewhere.]
    
      * This PR fixes a previously unreported bug.
    
        [Describe the bug in detail, as if you were filing a bug report.]
    
      * This PR adds a feature that has not yet been specified.
    
    [Write a brief specification for the feature, including justification
    for its inclusion in Materialize, as if you were writing the original
         feature specification.]
    
       * This PR refactors existing code.
    
    [Describe what was wrong with the existing code, if it is not obvious.]
    -->
    
    ### Tips for reviewer
    
    <!--
    Leave some tips for your reviewer, like:
    
        * The diff is much smaller if viewed with whitespace hidden.
        * [Some function/module/file] deserves extra attention.
    * [Some function/module/file] is pure code movement and only needs a
    skim.
    
    Delete this section if no tips.
    -->
    
    ### Checklist
    
    - [ ] This PR has adequate test coverage / QA involvement has been duly
    considered. ([trigger-ci for additional test/nightly
    runs](https://trigger-ci.dev.materialize.com/))
    - [ ] This PR has an associated up-to-date [design
    doc](https://github.com/MaterializeInc/materialize/blob/main/doc/developer/design/README.md),
    is a design doc
    ([template](https://github.com/MaterializeInc/materialize/blob/main/doc/developer/design/00000000_template.md)),
    or is sufficiently small to not require a design.
      <!-- Reference the design in the description. -->
    - [ ] If this PR evolves [an existing `$T ⇔ Proto$T`
    mapping](https://github.com/MaterializeInc/materialize/blob/main/doc/developer/command-and-response-binary-encoding.md)
    (possibly in a backwards-incompatible way), then it is tagged with a
    `T-proto` label.
    - [ ] If this PR will require changes to cloud orchestration or tests,
    there is a companion cloud PR to account for those changes that is
    tagged with the release-blocker label
    ([example](https://github.com/MaterializeInc/cloud/pull/5021)).
    <!-- Ask in #team-cloud on Slack if you need help preparing the cloud
    PR. -->
    - [ ] This PR includes the following [user-facing behavior
    changes](https://github.com/MaterializeInc/materialize/blob/main/doc/developer/guide-changes.md#what-changes-require-a-release-note):
    - <!-- Add release notes here or explicitly state that there are no
    user-facing behavior changes. -->

--- src/transform/src/canonicalization.rs
+++ src/transform/src/canonicalization.rs
@@ -21,3 +21,134 @@ mod topk_elision;
 pub use flatmap_to_map::FlatMapToMap;
 pub use projection_extraction::ProjectionExtraction;
 pub use topk_elision::TopKElision;
+
+use mz_expr::MirRelationExpr;
+
+use crate::analysis::{DerivedBuilder, RelationType};
+use crate::TransformCtx;
+
+/// A transform that visits each AST node and reduces scalar expressions.
+#[derive(Debug)]
+pub struct ReduceScalars;
+
+impl crate::Transform for ReduceScalars {
+    #[mz_ore::instrument(
+        target = "optimizer",
+        level = "debug",
+        fields(path.segment = "reduce_scalars")
+    )]
+    fn transform(
+        &self,
+        relation: &mut MirRelationExpr,
+        ctx: &mut TransformCtx,
+    ) -> Result<(), crate::TransformError> {
+        let mut builder = DerivedBuilder::new(ctx.features);
+        builder.require(RelationType);
+        let derived = builder.visit(&*relation);
+
+        // Descend the AST, reducing scalar expressions.
+        let mut todo = vec![(&mut *relation, derived.as_view())];
+        while let Some((expr, view)) = todo.pop() {
+            match expr {
+                MirRelationExpr::Constant { .. }
+                | MirRelationExpr::Get { .. }
+                | MirRelationExpr::Let { .. }
+                | MirRelationExpr::LetRec { .. }
+                | MirRelationExpr::Project { .. }
+                | MirRelationExpr::Union { .. }
+                | MirRelationExpr::Threshold { .. }
+                | MirRelationExpr::Negate { .. } => {
+                    // No expressions to reduce
+                }
+                MirRelationExpr::ArrangeBy { .. } => {
+                    // Has expressions, but we aren't brave enough to reduce these yet.
+                }
+                MirRelationExpr::Filter { predicates, .. } => {
+                    let input_type = view
+                        .last_child()
+                        .value::<RelationType>()
+                        .expect("RelationType required")
+                        .as_ref()
+                        .unwrap();
+                    for predicate in predicates.iter_mut() {
+                        predicate.reduce(input_type);
+                    }
+                    predicates.retain(|p| !p.is_literal_true());
+                }
+                MirRelationExpr::FlatMap { exprs, .. } => {
+                    let input_type = view
+                        .last_child()
+                        .value::<RelationType>()
+                        .expect("RelationType required")
+                        .as_ref()
+                        .unwrap();
+                    for expr in exprs.iter_mut() {
+                        expr.reduce(input_type);
+                    }
+                }
+                MirRelationExpr::Map { scalars, .. } => {
+                    // Use the output type, to incorporate the types of `scalars` as they land.
+                    let output_type = view
+                        .value::<RelationType>()
+                        .expect("RelationType required")
+                        .as_ref()
+                        .unwrap();
+                    let input_arity = output_type.len() - scalars.len();
+                    for (index, scalar) in scalars.iter_mut().enumerate() {
+                        scalar.reduce(&output_type[..input_arity + index]);
+                    }
+                }
+                MirRelationExpr::Join { equivalences, .. } => {
+                    let output_type = view
+                        .value::<RelationType>()
+                        .expect("RelationType required")
+                        .as_ref()
+                        .unwrap();
+                    for class in equivalences.iter_mut() {
+                        for expr in class.iter_mut() {
+                            expr.reduce(output_type);
+                        }
+                        class.sort();
+                        class.dedup();
+                    }
+                    equivalences.retain(|e| e.len() > 1);
+                    equivalences.sort();
+                    equivalences.dedup();
+                }
+                MirRelationExpr::Reduce {
+                    group_key,
+                    aggregates,
+                    ..
+                } => {
+                    let input_type = view
+                        .last_child()
+                        .value::<RelationType>()
+                        .expect("RelationType required")
+                        .as_ref()
+                        .unwrap();
+                    for key in group_key.iter_mut() {
+                        key.reduce(input_type);
+                    }
+                    for aggregate in aggregates.iter_mut() {
+                        aggregate.expr.reduce(input_type);
+                    }
+                }
+                MirRelationExpr::TopK { limit, .. } => {
+                    let input_type = view
+                        .last_child()
+                        .value::<RelationType>()
+                        .expect("RelationType required")
+                        .as_ref()
+                        .unwrap();
+                    if let Some(limit) = limit {
+                        limit.reduce(input_type);
+                    }
+                }
+            }
+            todo.extend(expr.children_mut().rev().zip(view.children_rev()))
+        }
+
+        mz_repr::explain::trace_plan(&*relation);
+        Ok(())
+    }
+}
