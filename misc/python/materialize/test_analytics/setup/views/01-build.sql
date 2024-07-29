9a02feb504043a0fa1a8be15280d97de1c7d1303
nrainer-materialize <129728240+nrainer-materialize@users.noreply.github.com>
2024-07-29 12:38:33 +0200

    test analytics: views: fix issues (#28557)

--- misc/python/materialize/test_analytics/setup/views/01-build.sql
+++ misc/python/materialize/test_analytics/setup/views/01-build.sql
@@ -8,7 +8,6 @@
 -- by the Apache License, Version 2.0.
 
 CREATE OR REPLACE VIEW v_count_builds_per_week AS
-IN CLUSTER test_analytics AS
 SELECT
     EXTRACT(YEAR FROM date) AS year,
     EXTRACT(WEEK FROM date) AS week,
