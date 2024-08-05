1ab980f411006c99df669b1f6cdc80fc1849911b
Dennis Felsing <dennis@felsing.org>
2024-08-05 06:42:13 +0000

    Revert me later: Switch back some pipelines to AWS for now

--- ci/mkpipeline.py
+++ ci/mkpipeline.py
@@ -53,7 +53,7 @@ from .deploy.deploy_util import rust_version
 # trying to capture that.)
 CI_GLUE_GLOBS = ["bin", "ci"]
 
-DEFAULT_AGENT = "hetzner-aarch64-4cpu-8gb"
+DEFAULT_AGENT = "linux-aarch64-small"
 
 
 def steps(pipeline: Any) -> Iterator[dict[str, Any]]:
