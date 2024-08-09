4300186862fa1642263294edb7b49b730afe3f91
Ben Kirwin <bkirwi@materialize.com>
2024-08-09 16:19:58 -0400

    Enable the columnar stats in CI for recent versions

--- misc/python/materialize/mzcompose/__init__.py
+++ misc/python/materialize/mzcompose/__init__.py
@@ -96,6 +96,9 @@ def get_default_system_parameters(
         "enable_table_keys": "true",
         "enable_variadic_left_join_lowering": "true",
         "enable_worker_core_affinity": "true",
+        "persist_batch_columnar_format": (
+            "both_v2" if version >= MzVersion.parse_mz("v0.112.0-dev") else "row"
+        ),
         "persist_batch_delete_enabled": "true",
         "persist_batch_record_part_format": "true",
         "persist_fast_path_limit": "1000",
