0c53742bbb4c0c3e2bb4d0f945bab0257c1a2b5c
nrainer-materialize <129728240+nrainer-materialize@users.noreply.github.com>
2024-08-21 11:56:37 +0200

    feature flag consistency: add ignore (#29137)

--- misc/python/materialize/feature_flag_consistency/ignore_filter/feature_flag_consistency_ignore_filter.py
+++ misc/python/materialize/feature_flag_consistency/ignore_filter/feature_flag_consistency_ignore_filter.py
@@ -15,11 +15,17 @@ from materialize.output_consistency.execution.evaluation_strategy import (
     EvaluationStrategy,
     is_other_db_evaluation_strategy,
 )
+from materialize.output_consistency.ignore_filter.ignore_verdict import (
+    IgnoreVerdict,
+    YesIgnore,
+)
 from materialize.output_consistency.ignore_filter.inconsistency_ignore_filter import (
     GenericInconsistencyIgnoreFilter,
     PostExecutionInconsistencyIgnoreFilterBase,
     PreExecutionInconsistencyIgnoreFilterBase,
 )
+from materialize.output_consistency.query.query_template import QueryTemplate
+from materialize.output_consistency.validation.validation_message import ValidationError
 
 
 class FeatureFlagConsistencyIgnoreFilter(GenericInconsistencyIgnoreFilter):
@@ -42,6 +48,36 @@ class FeatureFlagPostExecutionInconsistencyIgnoreFilter(
 ):
     def __init__(self, configuration_pair: FeatureFlagSystemConfigurationPair):
         self.configuration_pair = configuration_pair
+
+    def _shall_ignore_success_mismatch(
+        self,
+        error: ValidationError,
+        query_template: QueryTemplate,
+        contains_aggregation: bool,
+    ) -> IgnoreVerdict:
+        if query_template.uses_join() and (
+            query_template.has_where_condition() or query_template.has_row_selection()
+        ):
+            return YesIgnore("#17189: evaluation order")
+
+        return super()._shall_ignore_success_mismatch(
+            error, query_template, contains_aggregation
+        )
+
+    def _shall_ignore_error_mismatch(
+        self,
+        error: ValidationError,
+        query_template: QueryTemplate,
+        contains_aggregation: bool,
+    ) -> IgnoreVerdict:
+        if query_template.uses_join() and (
+            query_template.has_where_condition() or query_template.has_row_selection()
+        ):
+            return YesIgnore("#17189: evaluation order")
+
+        return super()._shall_ignore_error_mismatch(
+            error, query_template, contains_aggregation
+        )
 
 
 def get_flag_configuration(
