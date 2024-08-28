9603ae6325602cebc78fbb4a44baec366f0e8846
Dennis Felsing <dennis@felsing.org>
2024-08-28 14:22:05 +0000

    ci: Disable frontegg-mock tests in miri
    Seen failing in https://buildkite.com/materialize/nightly/builds/9287#_

--- src/frontegg-mock/tests/local.rs
+++ src/frontegg-mock/tests/local.rs
@@ -7,6 +7,9 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
 
+// can't call foreign function `OPENSSL_init_ssl` on OS `linux`
+#![cfg(not(miri))]
+
 use std::collections::BTreeMap;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use std::time::Duration;
