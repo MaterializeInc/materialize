f9919983576ce46640f20b3bd5667e03381fb06b
Rainer Niedermayr <rainer@materialize.com>
2024-09-12 17:00:14 +0200

    ci: add error entry regarding source version

--- misc/python/materialize/cli/ci_annotate_errors.py
+++ misc/python/materialize/cli/ci_annotate_errors.py
@@ -88,6 +88,7 @@ ERROR_RE = re.compile(
     | SUMMARY:\ .*Sanitizer
     | Fixpoint\ .*\ detected\ a\ loop\ of\ length\ .*\ after\ .*\ iterations
     | Fixpoint\ .*\ failed\ to\ reach\ a\ fixed\ point,\ or\ cycle\ of\ length\ at\ most
+    | primary\ source\ \w+\ seemingly\ dropped\ before\ subsource
     # \s\S is any character including newlines, so this matches multiline strings
     # non-greedy using ? so that we don't match all the result comparison issues into one block
     | ----------\ RESULT\ COMPARISON\ ISSUE\ START\ ----------[\s\S]*?----------\ RESULT\ COMPARISON\ ISSUE\ END\ ------------
