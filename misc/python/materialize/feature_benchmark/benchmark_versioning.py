9bec5ad9e0f026da18cd52e7545558849e58e555
Cara Haas <cara@materialize.com>
2024-09-23 14:55:10 -0400

    update the benchmark_versioning.py scenario sha256 values

--- misc/python/materialize/feature_benchmark/benchmark_versioning.py
+++ misc/python/materialize/feature_benchmark/benchmark_versioning.py
@@ -23,11 +23,11 @@ SHA256_OF_FRAMEWORK: dict[str, str] = {
 
 # Consider increasing the scenario's class #version() if changes are expected to impact results!
 SHA256_BY_SCENARIO_FILE: dict[str, str] = {
-    "benchmark_main.py": "79c067f9a234a002f3264b38d983130fa93b15911dc559ce8690133e790416bc",
+    "benchmark_main.py": "1d61feccbf7178b6b430bbcbec00f70e008339d6f64f9cd9ea6208c13c1f5851",
     "concurrency.py": "2e9c149c136b83b3853abc923a1adbdaf55a998ab4557712f8424c8b16f2adb1",
     "customer.py": "d1e72837a342c3ebf1f4a32ec583b1b78a78644cdba495030a6df45ebbffe703",
     "optbench.py": "f1e63f31d3ec6bf55093a467046b8d2f12e4b8b419420bad53e14a4f23b72989",
     "scale.py": "c4c8749d166e4df34e0b0e92220434fdb508c5c2ac56eb80c07043be0048dded",
     "skew.py": "bf60802205fc51ebf94fb008bbdb6b2ccce3c9ed88a6188fa7f090f2c84b120f",
-    "subscribe.py": "951cf7f702b511f12a8f506c831c7bdeae5c5735d1b70cb478f12bde72b23197",
+    "subscribe.py": "510cf9037308936f1bb02a1c2e6345a90bcbda89f701653518d46d1fb56a2328",
 }
