bf12c83c02426afe39368d87e4b4be8f7a6834ab
Rainer Niedermayr <rainer@materialize.com>
2024-09-25 13:45:34 -0400

    tests: adapt test_egress_ips.py

--- test/cloudtest/test_egress_ips.py
+++ test/cloudtest/test_egress_ips.py
@@ -8,12 +8,17 @@
 # by the Apache License, Version 2.0.
 
 from materialize.cloudtest.app.materialize_application import MaterializeApplication
+from materialize.mz_version import MzVersion
 
 
 def test_egress_ips(mz: MaterializeApplication) -> None:
     egress_ips = mz.environmentd.sql_query("SELECT * FROM mz_catalog.mz_egress_ips")
-    assert egress_ips == (
-        ["1.2.3.4", 32, "1.2.3.4/32"],
-        ["2001:db8::", 60, "2001:db8::/60"],
-        ["88.77.66.0", 28, "88.77.66.0/28"],
-    )
+
+    if MzVersion.parse_cargo() >= MzVersion.parse_mz("v0.118.0-dev"):
+        assert egress_ips == (
+            ["1.2.3.4", 32, "1.2.3.4/32"],
+            ["2001:db8::", 60, "2001:db8::/60"],
+            ["88.77.66.0", 28, "88.77.66.0/28"],
+        )
+    else:
+        assert egress_ips == (["1.2.3.4"], ["88.77.66.55"])
