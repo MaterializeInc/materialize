7a674568ceacea66666f5c63862814d16f38c1d1
Parker Timmerman <parker.timmerman@materialize.com>
2024-09-30 10:48:05 -0400

    Revert "persist: remove schema reg dyncfgs"
    This reverts commit 9b25df1ee70ec73d68a2633f000694bc6bf904c4.

--- src/persist-proc/src/lib.rs
+++ src/persist-proc/src/lib.rs
@@ -103,6 +103,12 @@ fn test_impl(attr: TokenStream, item: TokenStream) -> TokenStream {
                 },
                 {
                     let mut x = ::mz_dyncfg::ConfigUpdates::default();
+                    x.add_dynamic("persist_schema_register", ::mz_dyncfg::ConfigVal::Bool(true));
+                    x.add_dynamic("persist_schema_require", ::mz_dyncfg::ConfigVal::Bool(true));
+                    x
+                },
+                {
+                    let mut x = ::mz_dyncfg::ConfigUpdates::default();
                     x.add_dynamic("persist_batch_columnar_format", ::mz_dyncfg::ConfigVal::String("both_v2".into()));
                     x.add_dynamic("persist_batch_columnar_format_percent", ::mz_dyncfg::ConfigVal::Usize(100));
                     x.add_dynamic("persist_part_decode_format", ::mz_dyncfg::ConfigVal::String("arrow".into()));
--- src/storage-client/src/storage_collections.rs
+++ src/storage-client/src/storage_collections.rs
@@ -440,6 +440,8 @@ where
                 txns_client.dyncfgs().clone(),
                 Arc::clone(&txns_metrics),
                 txns_id,
+                Arc::new(RelationDesc::empty()),
+                Arc::new(UnitSchema),
             )
             .await;
 
