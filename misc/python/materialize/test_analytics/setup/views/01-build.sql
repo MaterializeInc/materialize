69952692b2c83af05dc39fea7db88f54b708b173
Rainer Niedermayr <rainer@materialize.com>
2024-10-04 12:28:03 +0200

    test analytics: add view v_branch_type

--- misc/python/materialize/test_analytics/setup/views/01-build.sql
+++ misc/python/materialize/test_analytics/setup/views/01-build.sql
@@ -18,4 +18,26 @@ GROUP BY
     b.pipeline
 ;
 
+CREATE OR REPLACE VIEW v_branch_type AS
+WITH data AS
+(
+    SELECT
+        b.branch,
+        (b.branch = 'main') AS is_main_branch,
+        (b.branch LIKE 'v%.%.%') AS is_release_branch,
+        (b.branch <> 'main' AND b.branch NOT LIKE 'v%.%.%') AS is_feature_branch
+    FROM build b
+    GROUP BY
+        b.branch
+)
+SELECT
+    branch,
+    CASE WHEN is_main_branch THEN 'main' WHEN is_release_branch THEN 'release' WHEN is_feature_branch THEN 'feature' ELSE '?' END AS branch_type,
+    is_main_branch,
+    is_release_branch,
+    is_feature_branch
+FROM data
+;
+
 ALTER VIEW v_most_recent_build OWNER TO qa;
+ALTER VIEW v_branch_type OWNER TO qa;
