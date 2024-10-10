3f3476d93b3c374e962609cb93161a46115e901b
Rainer Niedermayr <rainer@materialize.com>
2024-10-10 11:27:34 +0200

    output consistency: introduce RowIndexParam and RowIndexExpression

---
+++ misc/python/materialize/output_consistency/expression/row_index_expression.py
@@ -0,0 +1,71 @@
+# Copyright Materialize, Inc. and contributors. All rights reserved.
+#
+# Use of this software is governed by the Business Source License
+# included in the LICENSE file at the root of this repository.
+#
+# As of the Change Date specified in that file, in accordance with
+# the Business Source License, use of this software will be governed
+# by the Apache License, Version 2.0.
+from __future__ import annotations
+
+from materialize.output_consistency.data_type.data_type_category import DataTypeCategory
+from materialize.output_consistency.execution.sql_dialect_adjuster import (
+    SqlDialectAdjuster,
+)
+from materialize.output_consistency.execution.value_storage_layout import (
+    ValueStorageLayout,
+)
+from materialize.output_consistency.expression.expression import (
+    Expression,
+    LeafExpression,
+)
+from materialize.output_consistency.input_data.types.number_types_provider import (
+    INT8_TYPE,
+)
+from materialize.output_consistency.operation.return_type_spec import ReturnTypeSpec
+from materialize.output_consistency.query.data_source import DataSource
+
+
+class RowIndexExpression(LeafExpression):
+
+    def __init__(self, expression_to_share_data_source: Expression):
+        super().__init__(
+            column_name="row_index",
+            data_type=INT8_TYPE,
+            characteristics=set(),
+            storage_layout=ValueStorageLayout.ANY,
+        )
+        self.expression_to_share_data_source = expression_to_share_data_source
+
+    def resolve_return_type_spec(self) -> ReturnTypeSpec:
+        return self.data_type.resolve_return_type_spec(self.own_characteristics)
+
+    def resolve_return_type_category(self) -> DataTypeCategory:
+        return self.data_type.category
+
+    def get_data_source(self) -> DataSource | None:
+        data_sources = self.expression_to_share_data_source.collect_data_sources()
+
+        if len(data_sources) == 0:
+            # this happens when the expression is a constant
+            return None
+
+        # this is a simplification; ideally, we would use all data sources
+        return data_sources[0]
+
+    def to_sql_as_column(
+        self, sql_adjuster: SqlDialectAdjuster, include_alias: bool
+    ) -> str:
+        if self.get_data_source() is None:
+            # We won't use row_index in this case but a constant instead to avoid a potentially ambiguous column
+            # reference
+            return "0"
+
+        return super().to_sql_as_column(sql_adjuster, include_alias)
+
+    def collect_vertical_table_indices(self) -> set[int]:
+        # not relevant because this is already handled by the column sharing the data source
+        return set()
+
+    def __str__(self) -> str:
+        return f"RowIndexExpression (expression_to_share_data_source={self.expression_to_share_data_source})"
