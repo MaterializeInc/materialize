b6ea2b898f29ebb039e88c8a90b73553bbec87cb
Dennis Felsing <dennis@felsing.org>
2024-10-10 09:24:19 +0000

    ci: Move slow lint to lint-slow.sh
    Seen in https://buildkite.com/materialize/test/builds/92033#019273fc-0709-4cb2-b4ff-f11da7bc9ef2

--- ci/test/lint-slow.sh
+++ ci/test/lint-slow.sh
@@ -24,4 +24,9 @@ try bin/xcompile cargo test --locked --doc
 
 try bin/ci-closed-issues-detect --changed-lines-only
 
+# Smoke out failures in generating the license metadata page, even though we
+# don't care about its output in the test pipeline, so that we don't only
+# discover the failures after a merge to main.
+try cargo --locked about generate ci/deploy/licenses.hbs > /dev/null
+
 try_status_report
