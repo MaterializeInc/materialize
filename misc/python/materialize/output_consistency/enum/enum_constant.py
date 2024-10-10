c04b537138e5e5624915d764581806613ae5a688
Rainer Niedermayr <rainer@materialize.com>
2024-10-10 11:13:43 +0200

    output consistency: refactor EnumConstant

--- misc/python/materialize/output_consistency/enum/enum_constant.py
+++ misc/python/materialize/output_consistency/enum/enum_constant.py
@@ -8,30 +8,19 @@
 # by the Apache License, Version 2.0.
 from __future__ import annotations
 
-from materialize.output_consistency.data_type.data_type import DataType
-from materialize.output_consistency.data_type.data_type_category import DataTypeCategory
 from materialize.output_consistency.enum.enum_data_type import EnumDataType
-from materialize.output_consistency.execution.sql_dialect_adjuster import (
-    SqlDialectAdjuster,
-)
-from materialize.output_consistency.execution.value_storage_layout import (
-    ValueStorageLayout,
-)
-from materialize.output_consistency.expression.expression import (
-    Expression,
-    LeafExpression,
+from materialize.output_consistency.expression.constant_expression import (
+    ConstantExpression,
 )
 from materialize.output_consistency.expression.expression_characteristics import (
     ExpressionCharacteristics,
 )
-from materialize.output_consistency.operation.return_type_spec import ReturnTypeSpec
-from materialize.output_consistency.selection.row_selection import DataRowSelection
 from materialize.util import stable_int_hash
 
-ENUM_RETURN_TYPE_SPEC = ReturnTypeSpec(DataTypeCategory.ENUM)
+ENUM_DATA_TYPE = EnumDataType()
 
 
-class EnumConstant(Expression):
+class EnumConstant(ConstantExpression):
     """A constant SQL value"""
 
     def __init__(
@@ -41,52 +30,16 @@ class EnumConstant(Expression):
         characteristics: set[ExpressionCharacteristics],
         tags: set[str] | None = None,
     ):
-        super().__init__(characteristics, ValueStorageLayout.ANY, False, False)
-        self.value = value
-        self.add_quotes = add_quotes
-        self.data_type = EnumDataType()
+        super().__init__(
+            value, ENUM_DATA_TYPE, add_quotes, characteristics, is_aggregate=False
+        )
         self.tags = tags
 
     def hash(self) -> int:
         return stable_int_hash(self.value)
 
-    def resolve_return_type_category(self) -> DataTypeCategory:
-        return DataTypeCategory.ENUM
-
-    def resolve_return_type_spec(self) -> ReturnTypeSpec:
-        return ENUM_RETURN_TYPE_SPEC
-
-    def try_resolve_exact_data_type(self) -> DataType | None:
-        return self.data_type
-
-    def is_leaf(self) -> bool:
-        return True
-
-    def contains_leaf_not_directly_consumed_by_aggregation(self) -> bool:
-        return False
-
-    def recursively_collect_involved_characteristics(
-        self, row_selection: DataRowSelection
-    ) -> set[ExpressionCharacteristics]:
-        return self.own_characteristics
-
     def __str__(self) -> str:
         return f"EnumConstant (value={self.value})"
-
-    def to_sql(
-        self, sql_adjuster: SqlDialectAdjuster, include_alias: bool, is_root_level: bool
-    ) -> str:
-        sql_value = self.data_type.value_to_sql(self.value, sql_adjuster)
-        if self.add_quotes:
-            return f"'{sql_value}'"
-
-        return sql_value
-
-    def collect_leaves(self) -> list[LeafExpression]:
-        return []
-
-    def collect_vertical_table_indices(self) -> set[int]:
-        return set()
 
     def is_tagged(self, tag: str) -> bool:
         if self.tags is None:
