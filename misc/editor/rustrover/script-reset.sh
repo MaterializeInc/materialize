a5e03e4bc42d790803eb4faf6aaf2a84a63a7e3d
Siddhartha Sahu <siddhartha.sahu@materialize.com>
2024-10-16 10:46:06 -0400

    Move to misc/rustrover

--- misc/editor/rustrover/script-reset.sh
+++ misc/editor/rustrover/script-reset.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
+# Copyright Materialize, Inc. and contributors. All rights reserved.
+#
+# Use of this software is governed by the Business Source License
+# included in the LICENSE file at the root of this repository.
+#
+# As of the Change Date specified in that file, in accordance with
+# the Business Source License, use of this software will be governed
+# by the Apache License, Version 2.0.
+
 set -euo pipefail; shopt -s nullglob
 
-if [[ ! -d "./.run" ]]; then
+if [[ ! -f "./Cargo.lock" ]]; then
   echo "Please run this script from the root of the repository."
   exit 1
 fi
