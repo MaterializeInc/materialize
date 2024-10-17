22ddb482eb58713882e26b80fe507e37ff8e44e7
Dennis Felsing <dennis@felsing.org>
2024-10-17 10:54:20 +0000

    lints: Retry bazel check on network errors
    Seen in https://buildkite.com/materialize/test/builds/92661#019299fe-14a8-41dd-a101-3e426d0c4495

--- misc/shlib/shlib.bash
+++ misc/shlib/shlib.bash
@@ -76,9 +76,11 @@ try() {
 
     # Try the command.
     if "$@"; then
+        result=$?
         try_last_failed=false
         ((++ci_try_passed))
     else
+        result=$?
         try_last_failed=true
         # The command failed. Tell Buildkite to uncollapse this log section, so
         # that the errors are immediately visible.
@@ -86,6 +88,7 @@ try() {
         echo "^^^ 🚨 Failed: $*"
     fi
     ((++ci_try_total))
+    return $result
 }
 try_last_failed=false
 
