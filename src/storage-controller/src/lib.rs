1ec2a97fdf9fb7a681a2d7a4e106d3cf22253c80
Petros Angelatos <petrosagg@gmail.com>
2024-10-17 13:00:54 +0300

    allow multiple DroppedIds responses to arrive

--- src/storage-controller/src/lib.rs
+++ src/storage-controller/src/lib.rs
@@ -1850,11 +1850,6 @@ where
                             .read_hold
                             .try_downgrade(Antichain::new())
                             .expect("must be possible");
-                    } else {
-                        soft_panic_or_log!(
-                            "DroppedIds for ID {id} but we have neither ingestion nor export \
-                             under that ID"
-                        );
                     }
                 }
             }
