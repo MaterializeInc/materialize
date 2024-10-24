b9dda657c6287282f229638d3ecda5f0f114e690
Parker Timmerman <parker.timmerman@materialize.com>
2024-10-24 15:59:19 -0400

    peek and subscribe plans
    * update planning and sequencing for Peeks and Subscribes
    * update timestamp and timedomain logic

--- src/adapter/src/coord/sequencer/inner.rs
+++ src/adapter/src/coord/sequencer/inner.rs
@@ -2291,7 +2291,7 @@ impl Coordinator {
     pub(super) async fn determine_real_time_recent_timestamp(
         &self,
         session: &Session,
-        source_ids: impl Iterator<Item = GlobalId>,
+        source_ids: impl Iterator<Item = CatalogItemId>,
     ) -> Result<Option<BoxFuture<'static, Result<Timestamp, StorageError<Timestamp>>>>, AdapterError>
     {
         let vars = session.vars();
@@ -2309,7 +2309,7 @@ impl Coordinator {
             // storage objects that ingest data from external systems) remap
             // data. We "cheat" a little bit and filter out any IDs that aren't
             // user objects because we know they are not a RTR source.
-            let mut to_visit = VecDeque::from_iter(source_ids.filter(GlobalId::is_user));
+            let mut to_visit = VecDeque::from_iter(source_ids.filter(CatalogItemId::is_user));
             // If none of the sources are user objects, we don't need to provide
             // a RTR timestamp.
             if to_visit.is_empty() {
@@ -2328,6 +2328,10 @@ impl Coordinator {
                         .filter(|id| !timestamp_objects.contains(id) && id.is_user()),
                 );
             }
+            let timestamp_objects = timestamp_objects
+                .into_iter()
+                .flat_map(|item_id| self.catalog().get_entry(&item_id).global_ids())
+                .collect();
 
             let r = self
                 .controller
@@ -2643,7 +2647,12 @@ impl Coordinator {
         mut ctx: ExecuteContext,
         plan: plan::ReadThenWritePlan,
     ) {
-        let mut source_ids = plan.selection.depends_on();
+        let mut source_ids: BTreeSet<_> = plan
+            .selection
+            .depends_on()
+            .into_iter()
+            .map(|gid| self.catalog().resolve_item_id(&gid))
+            .collect();
         source_ids.insert(plan.id);
         guard_write_critical_section!(self, ctx, Plan::ReadThenWrite(plan), source_ids);
 
@@ -2698,7 +2707,7 @@ impl Coordinator {
         //   timestamp.
         fn validate_read_dependencies(
             catalog: &Catalog,
-            id: &GlobalId,
+            id: &CatalogItemId,
         ) -> Result<(), AdapterError> {
             use mz_catalog::memory::objects;
             use CatalogItemType::*;
@@ -2741,8 +2750,9 @@ impl Coordinator {
             Ok(())
         }
 
-        for id in selection.depends_on() {
-            if let Err(err) = validate_read_dependencies(self.catalog(), &id) {
+        for gid in selection.depends_on() {
+            let item_id = self.catalog().resolve_item_id(&gid);
+            if let Err(err) = validate_read_dependencies(self.catalog(), &item_id) {
                 ctx.retire(Err(err));
                 return;
             }
