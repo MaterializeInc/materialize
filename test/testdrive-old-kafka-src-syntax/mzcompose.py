65c3fc536ca64b07fa0e9992dba21b4b38433ca6
Rainer Niedermayr <rainer@materialize.com>
2024-10-28 14:10:55 +0100

    migration tests: testdrive-old-kafka-syntax

--- test/testdrive-old-kafka-src-syntax/mzcompose.py
+++ test/testdrive-old-kafka-src-syntax/mzcompose.py
@@ -12,10 +12,10 @@ Testdrive is the basic framework and language for defining product tests under
 the expected-result/actual-result (aka golden testing) paradigm. A query is
 retried until it produces the desired result.
 """
-
+import glob
 from pathlib import Path
 
-from materialize import ci_util
+from materialize import ci_util, spawn
 from materialize.mzcompose import get_default_system_parameters
 from materialize.mzcompose.composition import Composition, WorkflowArgumentParser
 from materialize.mzcompose.services.fivetran_destination import FivetranDestination
@@ -23,11 +23,20 @@ from materialize.mzcompose.services.kafka import Kafka
 from materialize.mzcompose.services.materialized import Materialized
 from materialize.mzcompose.services.minio import Minio
 from materialize.mzcompose.services.mysql import MySql
-from materialize.mzcompose.services.postgres import Postgres
+from materialize.mzcompose.services.postgres import (
+    METADATA_STORE,
+    CockroachOrPostgresMetadata,
+    Postgres,
+)
 from materialize.mzcompose.services.redpanda import Redpanda
 from materialize.mzcompose.services.schema_registry import SchemaRegistry
 from materialize.mzcompose.services.testdrive import Testdrive
 from materialize.mzcompose.services.zookeeper import Zookeeper
+from materialize.source_table_migration import (
+    get_new_image_for_source_table_migration_test,
+    get_old_image_for_source_table_migration_test,
+    verify_sources_after_source_table_migration,
+)
 
 SERVICES = [
     Zookeeper(),
@@ -38,12 +47,25 @@ SERVICES = [
     MySql(),
     Minio(setup_materialize=True, additional_directories=["copytos3"]),
     Materialized(external_minio=True),
+    CockroachOrPostgresMetadata(),
     FivetranDestination(volumes_extra=["tmp:/share/tmp"]),
     Testdrive(external_minio=True),
 ]
 
 
 def workflow_default(c: Composition, parser: WorkflowArgumentParser) -> None:
+    for name in c.workflows:
+        if name == "default":
+            continue
+
+        if name == "migration":
+            continue
+
+        with c.test_case(name):
+            c.workflow(name)
+
+
+def workflow_kafka(c: Composition, parser: WorkflowArgumentParser) -> None:
     """Run testdrive."""
     parser.add_argument(
         "--redpanda",
@@ -230,3 +252,202 @@ def workflow_default(c: Composition, parser: WorkflowArgumentParser) -> None:
             ci_util.upload_junit_report(
                 "testdrive", Path(__file__).parent / junit_report
             )
+
+
+def workflow_migration(c: Composition, parser: WorkflowArgumentParser) -> None:
+    """Run testdrive."""
+    parser.add_argument(
+        "--redpanda",
+        action="store_true",
+        help="run against Redpanda instead of the Confluent Platform",
+    )
+    parser.add_argument(
+        "--aws-region",
+        help="run against the specified AWS region instead of localstack",
+    )
+    parser.add_argument(
+        "--kafka-default-partitions",
+        type=int,
+        metavar="N",
+        help="set the default number of kafka partitions per topic",
+    )
+    parser.add_argument(
+        "--system-param",
+        type=str,
+        action="append",
+        nargs="*",
+        help="System parameters to set in Materialize, i.e. what you would set with `ALTER SYSTEM SET`",
+    )
+
+    parser.add_argument("--replicas", type=int, default=1, help="use multiple replicas")
+
+    parser.add_argument(
+        "--default-timeout",
+        type=str,
+        help="set the default timeout for Testdrive",
+    )
+    parser.add_argument(
+        "files",
+        nargs="*",
+        default=["*.td"],
+        help="run against the specified files",
+    )
+
+    (args, _) = parser.parse_known_args()
+
+    matching_files = []
+    for filter in args.files:
+        matching_files.extend(
+            glob.glob(filter, root_dir="test/testdrive-old-kafka-src-syntax")
+        )
+    matching_files = [file for file in matching_files if file != "session.td"]
+
+    dependencies = [
+        "fivetran-destination",
+        "minio",
+        "materialized",
+        "postgres",
+        "mysql",
+    ]
+
+    if args.redpanda:
+        kafka_deps = ["redpanda"]
+    else:
+        kafka_deps = ["zookeeper", "kafka", "schema-registry"]
+
+    dependencies += kafka_deps
+
+    testdrive = Testdrive(
+        forward_buildkite_shard=True,
+        kafka_default_partitions=args.kafka_default_partitions,
+        aws_region=args.aws_region,
+        # validate_catalog_store=True,
+        default_timeout=args.default_timeout,
+        volumes_extra=["mzdata:/mzdata"],
+        external_minio=True,
+        fivetran_destination=True,
+        fivetran_destination_files_path="/share/tmp",
+        entrypoint_extra=[f"--var=uses-redpanda={args.redpanda}"],
+    )
+
+    sysparams = args.system_param
+    if not args.system_param:
+        sysparams = []
+
+    additional_system_parameter_defaults = {}
+    for val in sysparams:
+        x = val[0].split("=", maxsplit=1)
+        assert len(x) == 2, f"--system-param '{val}' should be the format <key>=<val>"
+        key = x[0]
+        val = x[1]
+
+        additional_system_parameter_defaults[key] = val
+
+    mz_old = Materialized(
+        default_size=Materialized.Size.DEFAULT_SIZE,
+        image=get_old_image_for_source_table_migration_test(),
+        external_metadata_store=True,
+        external_minio=True,
+        additional_system_parameter_defaults=additional_system_parameter_defaults,
+    )
+    mz_new = Materialized(
+        default_size=Materialized.Size.DEFAULT_SIZE,
+        image=get_new_image_for_source_table_migration_test(),
+        external_metadata_store=True,
+        external_minio=True,
+        additional_system_parameter_defaults=additional_system_parameter_defaults,
+    )
+
+    for file in matching_files:
+        with c.override(testdrive, mz_old):
+            c.up(*dependencies)
+
+            c.sql(
+                "ALTER SYSTEM SET max_clusters = 50;",
+                port=6877,
+                user="mz_system",
+            )
+
+            non_default_testdrive_vars = []
+
+            if args.replicas > 1:
+                c.sql("DROP CLUSTER quickstart CASCADE", user="mz_system", port=6877)
+                # Make sure a replica named 'r1' always exists
+                replica_names = [
+                    "r1" if replica_id == 0 else f"replica{replica_id}"
+                    for replica_id in range(0, args.replicas)
+                ]
+                replica_string = ",".join(
+                    f"{replica_name} (SIZE '{mz_old.default_replica_size}')"
+                    for replica_name in replica_names
+                )
+                c.sql(
+                    f"CREATE CLUSTER quickstart REPLICAS ({replica_string})",
+                    user="mz_system",
+                    port=6877,
+                )
+
+                # Note that any command that outputs SHOW CLUSTERS will have output
+                # that depends on the number of replicas testdrive has. This means
+                # it might be easier to skip certain tests if the number of replicas
+                # is > 1.
+                c.sql(
+                    f"""
+                    CREATE CLUSTER testdrive_single_replica_cluster SIZE = '{mz_old.default_replica_size}';
+                    GRANT ALL PRIVILEGES ON CLUSTER testdrive_single_replica_cluster TO materialize;
+                    """,
+                    user="mz_system",
+                    port=6877,
+                )
+
+                non_default_testdrive_vars.append(f"--var=replicas={args.replicas}")
+                non_default_testdrive_vars.append(
+                    "--var=single-replica-cluster=testdrive_single_replica_cluster"
+                )
+
+            non_default_testdrive_vars.append(
+                f"--var=default-replica-size={mz_old.default_replica_size}"
+            )
+            non_default_testdrive_vars.append(
+                f"--var=default-storage-size={mz_old.default_storage_size}"
+            )
+
+            print(f"Running {file} with mz_old")
+
+            c.run_testdrive_files(
+                *non_default_testdrive_vars,
+                "--no-reset",
+                file,
+            )
+
+            c.kill("materialized", wait=True)
+
+            with c.override(mz_new):
+                c.up("materialized")
+
+                print("Running mz_new")
+                verify_sources_after_source_table_migration(c, file)
+
+                c.kill("materialized", wait=True)
+                c.kill("postgres", wait=True)
+                c.kill("mysql", wait=True)
+                c.kill(METADATA_STORE, wait=True)
+
+                for dep in kafka_deps:
+                    c.kill(dep, wait=True)
+
+                for dep in kafka_deps:
+                    c.rm(dep)
+
+                c.rm("materialized")
+                c.rm(METADATA_STORE)
+                c.rm("postgres")
+                c.rm("mysql")
+
+                # remove the testdrive container which uses the mzdata volume
+                testdrive_container_id = spawn.capture(
+                    ["docker", "ps", "-a", "--filter", f"volume={c.name}_mzdata", "-q"]
+                ).strip()
+                spawn.runv(["docker", "rm", testdrive_container_id])
+
+                c.rm_volumes("mzdata", force=True)
