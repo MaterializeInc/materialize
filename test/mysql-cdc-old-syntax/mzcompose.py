0c22e2bb145b3235ddde420ea3a700753714ed8a
Rainer Niedermayr <rainer@materialize.com>
2024-10-28 13:47:06 +0100

    migration tests: mysql-cdc-old-syntax

--- test/mysql-cdc-old-syntax/mzcompose.py
+++ test/mysql-cdc-old-syntax/mzcompose.py
@@ -22,9 +22,19 @@ from materialize.mysql_util import (
 )
 from materialize.mzcompose.composition import Composition, WorkflowArgumentParser
 from materialize.mzcompose.services.materialized import Materialized
+from materialize.mzcompose.services.minio import Minio
 from materialize.mzcompose.services.mysql import MySql
+from materialize.mzcompose.services.postgres import (
+    METADATA_STORE,
+    CockroachOrPostgresMetadata,
+)
 from materialize.mzcompose.services.test_certs import TestCerts
 from materialize.mzcompose.services.testdrive import Testdrive
+from materialize.source_table_migration import (
+    get_new_image_for_source_table_migration_test,
+    get_old_image_for_source_table_migration_test,
+    verify_sources_after_source_table_migration,
+)
 
 
 def create_mysql(mysql_version: str) -> MySql:
@@ -46,6 +56,7 @@ def create_mysql_replica(mysql_version: str) -> MySql:
 
 SERVICES = [
     Materialized(
+        external_minio=True,
         additional_system_parameter_defaults={
             "log_filter": "mz_storage::source::mysql=trace,info"
         },
@@ -53,6 +64,8 @@ SERVICES = [
     create_mysql(MySql.DEFAULT_VERSION),
     create_mysql_replica(MySql.DEFAULT_VERSION),
     TestCerts(),
+    CockroachOrPostgresMetadata(),
+    Minio(setup_materialize=True),
     Testdrive(default_timeout="60s"),
 ]
 
@@ -81,7 +94,11 @@ def workflow_default(c: Composition, parser: WorkflowArgumentParser) -> None:
         workflows_with_internal_sharding = ["cdc"]
         # Otherwise we are running all workflows
         sharded_workflows = workflows_with_internal_sharding + buildkite.shard_list(
-            [w for w in c.workflows if w not in workflows_with_internal_sharding],
+            [
+                w
+                for w in c.workflows
+                if w not in workflows_with_internal_sharding and w != "migration"
+            ],
             lambda w: w,
         )
         print(
@@ -281,3 +298,90 @@ def workflow_many_inserts(c: Composition, parser: WorkflowArgumentParser) -> Non
             """
         ),
     )
+
+
+def workflow_migration(c: Composition, parser: WorkflowArgumentParser) -> None:
+    parser.add_argument(
+        "filter",
+        nargs="*",
+        default=["*.td"],
+        help="limit to only the files matching filter",
+    )
+    args = parser.parse_args()
+
+    matching_files = []
+    for filter in args.filter:
+        matching_files.extend(glob.glob(filter, root_dir="test/mysql-cdc-old-syntax"))
+
+    # TODO: database-issues#8706
+    matching_files = [file for file in matching_files if file != "alter-source.td"]
+
+    sharded_files: list[str] = sorted(
+        buildkite.shard_list(matching_files, lambda file: file)
+    )
+    print(f"Files: {sharded_files}")
+
+    mysql_version = get_targeted_mysql_version(parser)
+
+    for file in sharded_files:
+
+        mz_old = Materialized(
+            name="materialized",
+            image=get_old_image_for_source_table_migration_test(),
+            external_metadata_store=True,
+            external_minio=True,
+            additional_system_parameter_defaults={
+                "log_filter": "mz_storage::source::mysql=trace,info"
+            },
+        )
+
+        mz_new = Materialized(
+            name="materialized",
+            image=get_new_image_for_source_table_migration_test(),
+            external_metadata_store=True,
+            external_minio=True,
+            additional_system_parameter_defaults={
+                "log_filter": "mz_storage::source::mysql=trace,info"
+            },
+        )
+
+        with c.override(mz_old, create_mysql(mysql_version)):
+            c.up("materialized", "mysql")
+
+            print(f"Running {file} with mz_old")
+
+            valid_ssl_context = retrieve_ssl_context_for_mysql(c)
+            wrong_ssl_context = retrieve_invalid_ssl_context_for_mysql(c)
+
+            c.sources_and_sinks_ignored_from_validation.add("drop_table")
+
+            c.run_testdrive_files(
+                f"--var=ssl-ca={valid_ssl_context.ca}",
+                f"--var=ssl-client-cert={valid_ssl_context.client_cert}",
+                f"--var=ssl-client-key={valid_ssl_context.client_key}",
+                f"--var=ssl-wrong-ca={wrong_ssl_context.ca}",
+                f"--var=ssl-wrong-client-cert={wrong_ssl_context.client_cert}",
+                f"--var=ssl-wrong-client-key={wrong_ssl_context.client_key}",
+                f"--var=mysql-root-password={MySql.DEFAULT_ROOT_PASSWORD}",
+                "--var=mysql-user-password=us3rp4ssw0rd",
+                f"--var=default-replica-size={Materialized.Size.DEFAULT_SIZE}-{Materialized.Size.DEFAULT_SIZE}",
+                f"--var=default-storage-size={Materialized.Size.DEFAULT_SIZE}-1",
+                "--no-reset",
+                file,
+            )
+
+            c.kill("materialized", wait=True)
+
+            with c.override(mz_new):
+                c.up("materialized")
+
+                print("Running mz_new")
+                verify_sources_after_source_table_migration(c, file)
+
+                c.kill("materialized", wait=True)
+                c.kill("mysql", wait=True)
+                c.kill(METADATA_STORE, wait=True)
+                c.rm("materialized")
+                c.rm(METADATA_STORE)
+                c.rm("mysql")
+                c.rm_volumes("mzdata")
