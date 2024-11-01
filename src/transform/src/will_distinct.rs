39ca1df177d2069ba93f34e58d5ac8d9ef8f1f7f
Frank McSherry <fmcsherry@me.com>
2024-11-01 15:28:52 -0400

    Relax acceptance condition

--- src/transform/src/will_distinct.rs
+++ src/transform/src/will_distinct.rs
@@ -9,7 +9,7 @@
 
 //! Transform that pushes down the information that a collection will be subjected to a `Distinct` on specific columns.
 
-use mz_expr::{MirRelationExpr, MirScalarExpr};
+use mz_expr::MirRelationExpr;
 
 use crate::analysis::{DerivedView, NonNegative};
 
@@ -54,7 +54,7 @@ impl crate::Transform for WillDistinct {
 impl WillDistinct {
     fn apply(&self, expr: &mut MirRelationExpr, derived: DerivedView) {
         // Record a list of expressions paired with optional "will distinct by" columns.
-        let mut todo = vec![(expr, derived, None::<&[MirScalarExpr]>)];
+        let mut todo = vec![(expr, derived, false)];
         while let Some((expr, derived, distinct_by)) = todo.pop() {
             // If we find a `Distinct` expression in the shadow of another `Distinct` that will apply to its key columns,
             // we can remove this `Distinct` operator as the distinctness will be enforced by the other expression.
@@ -65,51 +65,42 @@ impl WillDistinct {
                     aggregates,
                     ..
                 },
-                Some(columns),
-            ) = (&*expr, distinct_by)
+                true,
+            ) = (&mut *expr, distinct_by)
             {
-                if aggregates.is_empty()
-                    && columns
-                        .iter()
-                        .enumerate()
-                        .all(|(c, e)| e == &MirScalarExpr::Column(c))
-                    && columns.len() == group_key.len()
-                {
+                if aggregates.is_empty() {
                     let arity = input.arity();
                     *expr = input
-                        .clone()
+                        .take_dangerous()
                         .map(group_key.clone())
                         .project((arity..arity + group_key.len()).collect::<Vec<_>>());
+
+                    // Bail out of traversal at this point, on account of we've lost track of the shape of the expression.
+                } else {
+                    todo.extend(
+                        expr.children_mut()
+                            .rev()
+                            .zip(derived.children_rev())
+                            .map(|(x, y)| (x, y, false)),
+                    );
                 }
             } else {
                 match (expr, distinct_by) {
-                    // A distinct expression whose keys are only column references can be communicated onward.
                     (
                         MirRelationExpr::Reduce {
-                            input,
-                            group_key,
-                            aggregates,
-                            ..
+                            input, aggregates, ..
                         },
                         _,
                     ) => {
-                        if aggregates.is_empty()
-                            && group_key.iter().all(|e| {
-                                if let MirScalarExpr::Column(_) = e {
-                                    true
-                                } else {
-                                    false
-                                }
-                            })
-                        {
-                            todo.push((input, derived.last_child(), Some(&*group_key)));
+                        if aggregates.is_empty() {
+                            todo.push((input, derived.last_child(), true));
                         } else {
-                            todo.push((input, derived.last_child(), None))
+                            todo.push((input, derived.last_child(), false))
                         }
                     }
                     // If all inputs to the union are non-negative, any distinct enforced above the expression can be
                     // communicated on to each input.
-                    (MirRelationExpr::Union { base, inputs }, exprs) => {
+                    (MirRelationExpr::Union { base, inputs }, will_distinct) => {
                         if derived
                             .children_rev()
                             .all(|v| *v.value::<NonNegative>().unwrap())
@@ -118,14 +109,14 @@ impl WillDistinct {
                             todo.extend(
                                 children_rev
                                     .zip(derived.children_rev())
-                                    .map(|(x, y)| (x, y, exprs.clone())),
+                                    .map(|(x, y)| (x, y, will_distinct)),
                             );
                         } else {
                             let children_rev = inputs.iter_mut().rev().chain(Some(&mut **base));
                             todo.extend(
                                 children_rev
                                     .zip(derived.children_rev())
-                                    .map(|(x, y)| (x, y, None)),
+                                    .map(|(x, y)| (x, y, false)),
                             );
                         }
                     }
@@ -134,7 +125,7 @@ impl WillDistinct {
                             x.children_mut()
                                 .rev()
                                 .zip(derived.children_rev())
-                                .map(|(x, y)| (x, y, None)),
+                                .map(|(x, y)| (x, y, false)),
                         );
                     }
                 }
