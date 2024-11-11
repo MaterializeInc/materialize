7cbc2cb55493e058fd1f2aa82cb66653fd845ca8
Aljoscha Krettek <aljoscha.krettek@gmail.com>
2024-11-11 19:23:41 +0100

    test: in upsert tests, update auto-spill threshold
    Introducing provisional values in UPSERT state increased the state size
    by 8 bytes (for a mz_repr::Timestamp), this test stores two value, so we
    increase the threshold by 2 * 8.

--- test/upsert/mzcompose.py
+++ test/upsert/mzcompose.py
@@ -480,7 +480,7 @@ def workflow_autospill(c: Composition) -> None:
                 additional_system_parameter_defaults={
                     "disk_cluster_replicas_default": "true",
                     "upsert_rocksdb_auto_spill_to_disk": "true",
-                    "upsert_rocksdb_auto_spill_threshold_bytes": "250",
+                    "upsert_rocksdb_auto_spill_threshold_bytes": "266",
                     "enable_unorchestrated_cluster_replicas": "true",
                     "storage_dataflow_delay_sources_past_rehydration": "true",
                 },
@@ -495,7 +495,7 @@ def workflow_autospill(c: Composition) -> None:
                 additional_system_parameter_defaults={
                     "disk_cluster_replicas_default": "true",
                     "upsert_rocksdb_auto_spill_to_disk": "true",
-                    "upsert_rocksdb_auto_spill_threshold_bytes": "250",
+                    "upsert_rocksdb_auto_spill_threshold_bytes": "266",
                     "enable_unorchestrated_cluster_replicas": "true",
                     "storage_dataflow_delay_sources_past_rehydration": "true",
                     # Enable the RocksDB merge operator
