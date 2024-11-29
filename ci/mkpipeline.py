3c6e105d7e3ec53c447362a28e44cb57068f4238
Parker Timmerman <parker.timmerman@materialize.com>
2024-11-29 09:42:04 -0500

    Revert "Temporarily disable bazel (revert me after fix)"
    This reverts commit 8b713d54d1c26cac9707461dab87251e4a3f9b9c.

--- ci/mkpipeline.py
+++ ci/mkpipeline.py
@@ -697,18 +697,14 @@ def trim_builds(
                 step["concurrency"] = 1
                 step["concurrency_group"] = f"rust-build-aarch64/{hash(deps)}"
         elif step.get("id") == "build-x86_64":
-            (deps, check) = get_deps(
-                Arch.X86_64, bazel=ui.env_is_truthy("CI_BAZEL_BUILD", "1")
-            )
+            (deps, check) = get_deps(Arch.X86_64, bazel=True)
             if check:
                 step["skip"] = True
             else:
                 step["concurrency"] = 1
                 step["concurrency_group"] = f"build-x86_64/{hash(deps)}"
         elif step.get("id") == "build-aarch64":
-            (deps, check) = get_deps(
-                Arch.AARCH64, bazel=ui.env_is_truthy("CI_BAZEL_BUILD", "1")
-            )
+            (deps, check) = get_deps(Arch.AARCH64, bazel=True)
             if check:
                 step["skip"] = True
             else:
