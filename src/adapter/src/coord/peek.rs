a5e58a60b622f008e0403f655a808f80ffb50e61
Michael Greenberg <michael.greenberg@materialize.com>
2024-12-19 12:09:52 -0500

    does recalculating types matter?

--- src/adapter/src/coord/peek.rs
+++ src/adapter/src/coord/peek.rs
@@ -315,13 +315,15 @@ pub fn create_fast_path_plan<T: Timestamp>(
     if dataflow_plan.objects_to_build.len() >= 1 && dataflow_plan.objects_to_build[0].id == view_id
     {
         let mut mir = &*dataflow_plan.objects_to_build[0].plan.as_inner_mut();
-        if let Some((rows, ..)) = mir.as_const() {
+        if let Some((rows, found_typ)) = mir.as_const() {
             // In the case of a constant, we can return the result now.
+            let recalc_typ = mir.typ();
+            assert_eq!(found_typ, &recalc_typ);
             return Ok(Some(FastPathPlan::Constant(
                 rows.clone()
                     .map(|rows| rows.into_iter().map(|(row, diff)| (row, diff)).collect()),
                 // For best accuracy, we need to recalculate typ.
-                mir.typ(),
+                recalc_typ,
             )));
         } else {
             // If there is a TopK that would be completely covered by the finishing, then jump
