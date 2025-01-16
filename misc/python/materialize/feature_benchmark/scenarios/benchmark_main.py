c13b70dac5e1786d31a2ebeae72a14d8d43d5430
Dennis Felsing <dennis@felsing.org>
2025-01-16 06:21:08 +0000

    feature-benchmark: Old plan for old versions
    Follow-up to https://github.com/MaterializeInc/materialize/pull/31045,
    follow-up to https://github.com/MaterializeInc/materialize/pull/30983

--- misc/python/materialize/feature_benchmark/scenarios/benchmark_main.py
+++ misc/python/materialize/feature_benchmark/scenarios/benchmark_main.py
@@ -709,7 +709,7 @@ class AccumulateReductions(Dataflow):
 
 > SET CLUSTER = idx_cluster;
 
-? EXPLAIN SELECT count(*) FROM accumulable;
+?[version>=13000] EXPLAIN SELECT count(*) FROM accumulable;
 Explained Query:
   With
     cte l0 =
@@ -726,6 +726,29 @@ Explained Query:
               Get l0 // { arity: 1 }
           Constant // { arity: 0 }
             - ()
+
+Used Indexes:
+  - materialize.public.i_accumulable (*** full scan ***)
+
+Target cluster: idx_cluster
+
+?[version<13000] EXPLAIN SELECT count(*) FROM accumulable;
+Explained Query:
+  Return // { arity: 1 }
+    Union // { arity: 1 }
+      Get l0 // { arity: 1 }
+      Map (0) // { arity: 1 }
+        Union // { arity: 0 }
+          Negate // { arity: 0 }
+            Project () // { arity: 0 }
+              Get l0 // { arity: 1 }
+          Constant // { arity: 0 }
+            - ()
+  With
+    cte l0 =
+      Reduce aggregates=[count(*)] // { arity: 1 }
+        Project () // { arity: 0 }
+          ReadIndex on=accumulable i_accumulable=[*** full scan ***] // { arity: 5 }
 
 Used Indexes:
   - materialize.public.i_accumulable (*** full scan ***)
@@ -2099,7 +2122,7 @@ class HydrateIndex(Scenario):
 1
 > ALTER CLUSTER idx_cluster SET (REPLICATION FACTOR 1)
 > SET CLUSTER = idx_cluster
-? EXPLAIN SELECT COUNT(*) FROM t1
+?[version>=13000] EXPLAIN SELECT COUNT(*) FROM t1
 Explained Query:
   With
     cte l0 =
@@ -2116,6 +2139,29 @@ Explained Query:
               Get l0 // {{ arity: 1 }}
           Constant // {{ arity: 0 }}
             - ()
+
+Used Indexes:
+  - materialize.public.i1 (*** full scan ***)
+
+Target cluster: idx_cluster
+
+?[version<13000] EXPLAIN SELECT COUNT(*) FROM t1
+Explained Query:
+  Return // {{ arity: 1 }}
+    Union // {{ arity: 1 }}
+      Get l0 // {{ arity: 1 }}
+      Map (0) // {{ arity: 1 }}
+        Union // {{ arity: 0 }}
+          Negate // {{ arity: 0 }}
+            Project () // {{ arity: 0 }}
+              Get l0 // {{ arity: 1 }}
+          Constant // {{ arity: 0 }}
+            - ()
+  With
+    cte l0 =
+      Reduce aggregates=[count(*)] // {{ arity: 1 }}
+        Project () // {{ arity: 0 }}
+          ReadIndex on=t1 i1=[*** full scan ***] // {{ arity: 2 }}
 
 Used Indexes:
   - materialize.public.i1 (*** full scan ***)
