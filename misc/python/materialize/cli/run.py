MaterializeInc/materialize

commit 5a6a838b84045c35d909309fde9025bdebac8045

persist: support for Azure blob storage (#30817)

misc/python/materialize/cli/run.py
@@ -50,6 +50,7 @@ SANITIZER_TARGET = (
     else f"{Arch.host()}-apple-darwin"
 )
 DEFAULT_POSTGRES = "postgres://root@localhost:26257/materialize"
+DEFAULT_BLOB = "file://mzdata/persist/blob"
 
 # sets entitlements on the built binary, e.g. environmentd, so you can inspect it with Instruments
 MACOS_ENTITLEMENTS_DATA = """
@@ -89,6 +90,11 @@ def main() -> int:
         "--postgres",
         help="Postgres/CockroachDB connection string",
         default=os.getenv("MZDEV_POSTGRES", DEFAULT_POSTGRES),
+    )
+    parser.add_argument(
+        "--blob",
+        help="Blob storage connection string",
+        default=os.getenv("MZDEV_BLOB", DEFAULT_BLOB),
     )
     parser.add_argument(
         "--release",
@@ -278,7 +284,7 @@ def main() -> int:
                 f"--orchestrator-process-scratch-directory={scratch}",
                 "--secrets-controller=local-file",
                 f"--persist-consensus-url={args.postgres}?options=--search_path=consensus",
-                f"--persist-blob-url=file://{mzdata}/persist/blob",
+                f"--persist-blob-url={args.blob}",
                 f"--timestamp-oracle-url={args.postgres}?options=--search_path=tsoracle",
                 f"--environment-id={environment_id}",
                 "--bootstrap-role=materialize",

ci/nightly/pipeline.template.yml
@@ -226,18 +226,6 @@ steps:
               composition: testdrive
               args: [--redpanda]
 
-      - id: redpanda-testdrive-aarch64
-        label: ":panda_face: :racing_car: testdrive aarch64"
-        depends_on: build-aarch64
-        timeout_in_minutes: 180
-        agents:
-          queue: hetzner-aarch64-8cpu-16gb
-        plugins:
-          - ./ci/plugins/mzcompose:
-              composition: testdrive
-              args: [--redpanda]
-        skip: "Disabled due to taking too long for the value provided"
-
       # TODO(def-) Remove this when old upsert implementation is removed
       - id: testdrive-old-upsert
         label: ":racing_car: testdrive with old Upsert"
@@ -309,6 +297,51 @@ steps:
                 --system-param=persist_encoding_enable_dictionary=true,
               ]
 
+      - id: persistence-testdrive
+        label: ":racing_car: testdrive with --persistent-user-tables"
+        depends_on: build-aarch64
+        timeout_in_minutes: 30
+        agents:
+          queue: hetzner-aarch64-4cpu-8gb
+        plugins:
+          - ./ci/plugins/mzcompose:
+              composition: testdrive
+              args: [--persistent-user-tables]
+        skip: "Persistence tests disabled"
+
+      - id: azurite-testdrive
+        label: "testdrive with :azure: blob store"
+        depends_on: build-aarch64
+        timeout_in_minutes: 180
+        agents:
+          queue: hetzner-aarch64-8cpu-16gb
+        plugins:
+          - ./ci/plugins/mzcompose:
+              composition: testdrive
+              args: [--azurite]
+
+      - id: azurite-testdrive-replicas-4
+        label: ":racing_car: testdrive 4 replicas with :azure: blob store"
+        depends_on: build-aarch64
+        timeout_in_minutes: 180
+        agents:
+          queue: hetzner-aarch64-16cpu-32gb
+        plugins:
+          - ./ci/plugins/mzcompose:
+              composition: testdrive
+              args: [--replicas=4, --azurite]
+
+      - id: azurite-testdrive-size-8
+        label: ":racing_car: testdrive with SIZE 8 and :azure: blob store"
+        depends_on: build-aarch64
+        timeout_in_minutes: 180
+        agents:
+          queue: hetzner-aarch64-16cpu-32gb
+        plugins:
+          - ./ci/plugins/mzcompose:
+              composition: testdrive
+              args: [--default-size=8, --azurite]
+
       - id: testdrive-in-cloudtest
         label: "Full Testdrive in Cloudtest (K8s)"
         depends_on: build-aarch64
@@ -326,17 +359,6 @@ steps:
               args: [-m=long, test/cloudtest/test_full_testdrive.py, --no-test-parallelism]
         sanitizer: skip
 
-      - id: persistence-testdrive
-        label: ":racing_car: testdrive with --persistent-user-tables"
-        depends_on: build-aarch64
-        timeout_in_minutes: 30
-        agents:
-          queue: hetzner-aarch64-4cpu-8gb
-        plugins:
-          - ./ci/plugins/mzcompose:
-              composition: testdrive
-              args: [--persistent-user-tables]
-        skip: "Persistence tests disabled"
 
   - group: Limits
     key: limits-group
@@ -456,6 +478,17 @@ steps:
               composition: zippy
               args: [--scenario=KafkaSources, --actions=10000, --max-execution-time=30m]
 
+      - id: zippy-kafka-sources-azurite
+        label: "Zippy Kafka Sources with :azure: blob store"
+        depends_on: build-aarch64
+        timeout_in_minutes: 120
+        agents:
+          queue: hetzner-aarch64-8cpu-16gb
+        plugins:
+          - ./ci/plugins/mzcompose:
+              composition: zippy
+              args: [--scenario=KafkaSources, --actions=10000, --max-execution-time=30m, --azurite]
+
       # TODO(def-) Remove this when old upsert implementation is removed
       - id: zippy-kafka-sources-old-upsert
         label: "Zippy Kafka Sources with old Upsert"
@@ -468,6 +501,18 @@ steps:
               composition: zippy
               args: [--scenario=KafkaSources, --actions=10000, --max-execution-time=30m, --system-param=storage_use_continual_feedback_upsert=false]
 
+      # TODO(def-) Remove this when old upsert implementation is removed
+      - id: zippy-kafka-sources-old-upsert-azurite
+        label: "Zippy Kafka Sources with old Upsert and :azure: blob store"
+        depends_on: build-aarch64
+        timeout_in_minutes: 120
+        agents:
+          queue: hetzner-aarch64-8cpu-16gb
+        plugins:
+          - ./ci/plugins/mzcompose:
+              composition: zippy
+              args: [--scenario=KafkaSources, --actions=10000, --max-execution-time=30m, --system-param=storage_use_continual_feedback_upsert=false, --azurite]
+
       - id: zippy-kafka-parallel-insert
         label: "Zippy Kafka Parallel Insert"
         depends_on: build-aarch64
@@ -479,6 +524,17 @@ steps:
               composition: zippy
               args: [--scenario=KafkaParallelInsert, --transaction-isolation=serializable, --actions=10000, --max-execution-time=30m]
 
+      - id: zippy-kafka-parallel-insert-azurite
+        label: "Zippy Kafka Parallel Insert with :azurite: blob store"
+        depends_on: build-aarch64
+        timeout_in_minutes: 120
+        agents:
+          queue: hetzner-aarch64-8cpu-16gb
+        plugins:
+          - ./ci/plugins/mzcompose:
+              composition: zippy
+              args: [--scenario=KafkaParallelInsert, --transaction-isolation=serializable, --actions=10000, --max-execution-time=30m, --azurite]
+
       - id: zippy-user-tables
         label: "Zippy User Tables"
         depends_on: build-aarch64
@@ -488,7 +544,7 @@ steps:
         plugins:
           - ./ci/plugins/mzcompose:
               composition: zippy
-              args: [--scenario=UserTables, --actions=10000, --max-execution-time=30m]
+              args: [--scenario=UserTables, --actions=10000, --max-execution-time=30m, --azurite]
 
       - id: zippy-postgres-cdc
         label: "Zippy Postgres CDC"
@@ -499,7 +555,7 @@ steps:
         plugins:
           - ./ci/plugins/mzcompose:
               composition: zippy
-              args: [--scenario=PostgresCdc, --actions=10000, --max-execution-time=30m]
+              args: [--scenario=PostgresCdc, --actions=10000, --max-execution-time=30m, --azurite]
 
       - id: zippy-mysql-cdc
         label: "Zippy MySQL CDC"
@@ -510,7 +566,7 @@ steps:
         plugins:
           - ./ci/plugins/mzcompose:
               composition: zippy
-              args: [--scenario=MySqlCdc, --actions=10000, --max-execution-time=30m]
+              args: [--scenario=MySqlCdc, --actions=10000, --max-execution-time=30m, --azurite]
 
       - id: zippy-debezium-postgres
         label: "Zippy Debezium Postgres"
@@ -629,6 +685,7 @@ steps:
         plugins:
           - ./ci/plugins/mzcompose:
               composition: testdrive-old-kafka-src-syntax
+              args: [--azurite]
         agents:
           queue: hetzner-aarch64-8cpu-16gb
 
@@ -675,6 +732,19 @@ steps:
   - group: "Platform checks"
     key: platform-checks
     steps:
+      - id: checks-no-restart-no-upgrade-azurite
+        label: "Checks without restart or upgrade with :azure: blob store %N"
+        depends_on: build-aarch64
+        inputs: [misc/python/materialize/checks]
+        timeout_in_minutes: 45
+        parallelism: 2
+        agents:
+          queue: hetzner-aarch64-16cpu-32gb
+        plugins:
+          - ./ci/plugins/mzcompose:
+              composition: platform-checks
+              args: [--scenario=NoRestartNoUpgrade, "--seed=$BUILDKITE_JOB_ID", --azurite]
+
       - id: checks-restart-entire-mz
         label: "Checks + restart of the entire Mz"
         depends_on: build-aarch64
@@ -687,7 +757,25 @@ steps:
         plugins:
           - ./ci/plugins/mzcompose:
               composition: platform-checks
-              args: [--scenario=RestartEntireMz, "--seed=$BUILDKITE_JOB_ID"]
+              args: [--scenario=RestartEntireMz, "--seed=$BUILDKITE_JOB_ID", --azurite]
+
+      - id: checks-restart-environmentd-clusterd-storage-azurite
+        label: "Checks + restart of environmentd & storage clusterd with :azure: blob store %N"
+        depends_on: build-aarch64
+        inputs: [misc/python/materialize/checks]
+        timeout_in_minutes: 45
+        parallelism: 2
+        agents:
+          queue: hetzner-aarch64-16cpu-32gb
+        plugins:
+          - ./ci/plugins/mzcompose:
+              composition: platform-checks
+              args:
+                [
+                  --scenario=RestartEnvironmentdClusterdStorage,
+                  "--seed=$BUILDKITE_JOB_ID",
+                  --azurite,
+                ]
 
       # TODO(def-) Remove this when old upsert implementation is removed
       - id: checks-restart-entire-mz-old-upsert
@@ -751,6 +839,18 @@ steps:
           - ./ci/plugins/mzcompose:
               composition: platform-checks
               args: [--scenario=RestartEntireMz, --execution-mode=parallel, "--seed=$BUILDKITE_JOB_ID"]
+
+      - id: checks-parallel-restart-entire-mz-azurite
+        label: "Checks parallel + restart of the entire Mz with :azure: blob store %N"
+        depends_on: build-aarch64
+        timeout_in_minutes: 180
+        parallelism: 2
+        agents:
+          queue: hetzner-aarch64-16cpu-32gb
+        plugins:
+          - ./ci/plugins/mzcompose:
+              composition: platform-checks
+              args: [--scenario=RestartEntireMz, --execution-mode=parallel, "--seed=$BUILDKITE_JOB_ID", --azurite]
 
       - id: checks-parallel-restart-environmentd-clusterd-storage
         label: "Checks parallel + restart of environmentd & storage clusterd"
@@ -824,8 +924,8 @@ steps:
               composition: platform-checks
               args: [--scenario=UpgradeEntireMzFourVersions, "--seed=$BUILDKITE_JOB_ID"]
 
-      - id: checks-0dt-restart-entire-mz-forced-migrations
-        label: "Checks 0dt restart of the entire Mz with forced migrations"
+      - id: checks-0dt-restart-entire-mz-forced-migrations-azurite
+        label: "Checks 0dt restart of the entire Mz with forced migrations with :azure: blob store"
         depends_on: build-aarch64
         timeout_in_minutes: 120
         parallelism: 2
@@ -834,7 +934,7 @@ steps:
         plugins:
           - ./ci/plugins/mzcompose:
               composition: platform-checks
-              args: [--scenario=ZeroDowntimeRestartEntireMzForcedMigrations, "--seed=$BUILDKITE_JOB_ID"]
+              args: [--scenario=ZeroDowntimeRestartEntireMzForcedMigrations, "--seed=$BUILDKITE_JOB_ID", --azurite]
 
       - id: checks-0dt-upgrade-entire-mz
         label: "Checks 0dt upgrade, whole-Mz restart"
@@ -1414,6 +1514,7 @@ steps:
     plugins:
       - ./ci/plugins/mzcompose:
           composition: data-ingest
+          args: [--azurite]
 
   - group: "Parallel Workload"
     key: parallel-workload
@@ -1431,7 +1532,7 @@ steps:
               args: [--runtime=1500, --complexity=dml, --threads=16]
 
       - id: parallel-workload-ddl
-        label: "Parallel Workload (DDL)"
+        label: "Parallel Workload (DDL) with :azure: blob store"
         depends_on: build-aarch64
         artifact_paths: [parallel-workload-queries.log.zst]
         timeout_in_minutes: 90
@@ -1440,7 +1541,7 @@ steps:
         plugins:
           - ./ci/plugins/mzcompose:
               composition: parallel-workload
-              args: [--runtime=1500, --threads=8]
+              args: [--runtime=1500, --threads=8, --azurite]
 
       - id: parallel-workload-ddl-only
         label: "Parallel Workload (DDL Only)"
@@ -1481,7 +1582,7 @@ steps:
               args: [--runtime=1500, --scenario=rename, --naughty-identifiers, --threads=16]
 
       - id: parallel-workload-rename
-        label: "Parallel Workload (rename)"
+        label: "Parallel Workload (rename) with :azure: blob store"
         depends_on: build-aarch64
         artifact_paths: [parallel-workload-queries.log.zst]
         timeout_in_minutes: 90
@@ -1490,7 +1591,7 @@ steps:
         plugins:
           - ./ci/plugins/mzcompose:
               composition: parallel-workload
-              args: [--runtime=1500, --scenario=rename, --threads=16]
+              args: [--runtime=1500, --scenario=rename, --threads=16, --azurite]
 
       - id: parallel-workload-cancel
         label: "Parallel Workload (cancel)"
@@ -1643,6 +1744,7 @@ steps:
     plugins:
       - ./ci/plugins/mzcompose:
           composition: txn-wal-fencing
+          args: [--azurite]
     agents:
       queue: hetzner-aarch64-8cpu-16gb
 
@@ -1735,6 +1837,18 @@ steps:
       - ./ci/plugins/mzcompose:
           composition: sqllogictest
           run: slow-tests
+
+  - id: cluster-tests-azurite
+    label: "Cluster tests with :azure: blob store"
+    depends_on: build-aarch64
+    timeout_in_minutes: 120
+    inputs: [test/cluster]
+    plugins:
+      - ./ci/plugins/mzcompose:
+          composition: cluster
+          args: ["--azurite"]
+    agents:
+      queue: hetzner-aarch64-8cpu-16gb
 
   - group: "Language tests"
     key: language-tests

ci/test/cargo-test/mzcompose.py
@@ -19,6 +19,7 @@ import subprocess
 from materialize import MZ_ROOT, buildkite, rustc_flags, spawn, ui
 from materialize.cli.run import SANITIZER_TARGET
 from materialize.mzcompose.composition import Composition, WorkflowArgumentParser
+from materialize.mzcompose.services.azure import Azurite
 from materialize.mzcompose.services.kafka import Kafka
 from materialize.mzcompose.services.minio import Minio
 from materialize.mzcompose.services.postgres import (
@@ -52,6 +53,10 @@ SERVICES = [
         allow_host_ports=True,
         additional_directories=["copytos3"],
     ),
+    Azurite(
+        ports=["40111:10000"],
+        allow_host_ports=True,
+    ),
 ]
 
 
@@ -65,7 +70,13 @@ def workflow_default(c: Composition, parser: WorkflowArgumentParser) -> None:
     parser.add_argument("args", nargs="*")
     args = parser.parse_args()
     c.up(
-        "zookeeper", "kafka", "schema-registry", "postgres", c.metadata_store(), "minio"
+        "zookeeper",
+        "kafka",
+        "schema-registry",
+        "postgres",
+        c.metadata_store(),
+        "minio",
+        "azurite",
     )
     # Heads up: this intentionally runs on the host rather than in a Docker
     # image. See database-issues#3739.
@@ -84,6 +95,7 @@ def workflow_default(c: Composition, parser: WorkflowArgumentParser) -> None:
         MZ_SOFT_ASSERTIONS="1",
         MZ_PERSIST_EXTERNAL_STORAGE_TEST_S3_BUCKET="mz-test-persist-1d-lifecycle-delete",
         MZ_S3_UPLOADER_TEST_S3_BUCKET="mz-test-1d-lifecycle-delete",
+        MZ_PERSIST_EXTERNAL_STORAGE_TEST_AZURE_CONTAINER="mz-test-azure",
         MZ_PERSIST_EXTERNAL_STORAGE_TEST_POSTGRES_URL=cockroach_url,
     )
 

deny.toml
@@ -48,10 +48,8 @@ skip = [
     { name = "windows-sys", version = "0.52.0" },
     # Newer versions of crates like `tempfile` are held back by crates like `atty`.
     # This is very Unfortunate as we don't actually use these platforms.
-    { name = "hermit-abi", version = "0.1.6" },
     { name = "hermit-abi", version = "0.2.6" },
     { name = "redox_syscall", version = "0.2.10" },
-    { name = "linux-raw-sys", version = "0.3.4" },
     { name = "rustix", version = "0.38.21" },
 
     # Will require updating many crates
@@ -118,6 +116,19 @@ skip = [
     { name = "sync_wrapper", version = "0.1.2" },
 
     { name = "memmap2", version = "0.5.4" },
+
+    { name = "wasi", version = "0.9.0+wasi-snapshot-preview1" },
+    { name = "async-channel", version = "1.9.0" },
+    { name = "event-listener", version = "2.5.3" },
+    { name = "fastrand", version = "1.9.0" },
+    { name = "futures-lite", version = "1.13.0" },
+    { name = "getrandom", version = "0.1.16" },
+    { name = "rand", version = "0.7.3" },
+    { name = "rand_chacha", version = "0.2.2" },
+    { name = "rand_core", version = "0.5.1" },
+    { name = "reqwest", version = "0.11.24" },
+    { name = "rustls-pemfile", version = "1.0.4" },
+    { name = "winreg", version = "0.50.0" },
 ]
 
 # Use `tracing` instead.
@@ -148,6 +159,7 @@ name = "strum-macros"
 [[bans.deny]]
 name = "log"
 wrappers = [
+    "azure_svc_blobstorage",
     "deadpool-postgres",
     "eventsource-client",
     "fail",
@@ -187,7 +199,6 @@ wrappers = [
   "bindgen",
   "bstr",
   "console",
-  "criterion",
   "dynfmt",
   "findshlibs",
   "insta",
@@ -227,14 +238,10 @@ version = 2
 ignore = [
     # Consider switching `yaml-rust` to the actively maintained `yaml-rust2` fork of the original project
     "RUSTSEC-2024-0320",
-    # Consider `std::io::IsTerminal` or `is-terminal` instead of `atty` (unmaintained)
-    "RUSTSEC-2021-0145",
     # Consider `encoding_rs` instead of `encoding` (unmaintained)
     "RUSTSEC-2021-0153",
     # proc-macro-error is unmaintained, possible alternative: proc-macro-error2
     "RUSTSEC-2024-0370",
-    # Use standard library's IsTerminal trait instead of `atty` (unmaintained)
-    "RUSTSEC-2024-0375",
     # `derivative` is unmaintained; consider using an alternative (unmaintained)
     "RUSTSEC-2024-0388",
     # `instant` is unmaintained, and the author recommends using the maintained [`web-time`] crate instead.

misc/python/materialize/checks/mzcompose_actions.py
@@ -61,6 +61,7 @@ class StartMz(MzcomposeAction):
         self.restart = restart
         self.force_migrations = force_migrations
         self.publish = publish
+        self.scenario = scenario
 
     def execute(self, e: Executor) -> None:
         c = e.mzcompose_composition()
@@ -72,7 +73,8 @@ class StartMz(MzcomposeAction):
             name=self.mz_service,
             image=image,
             external_metadata_store=True,
-            external_minio=True,
+            external_blob_store=True,
+            blob_store_is_azure=self.scenario.azurite,
             environment_extra=self.environment_extra,
             system_parameter_defaults=self.system_parameter_defaults,
             additional_system_parameter_defaults=self.additional_system_parameter_defaults,

misc/python/materialize/checks/scenarios.py
@@ -45,10 +45,15 @@ from materialize.mz_version import MzVersion
 
 class Scenario:
     def __init__(
-        self, checks: list[type[Check]], executor: Executor, seed: str | None = None
+        self,
+        checks: list[type[Check]],
+        executor: Executor,
+        azurite: bool,
+        seed: str | None = None,
     ) -> None:
         self._checks = checks
         self.executor = executor
+        self.azurite = azurite
         self.rng = None if seed is None else Random(seed)
         self._base_version = MzVersion.parse_cargo()
 
@@ -269,10 +274,11 @@ class SystemVarChange(Scenario):
         self,
         checks: list[type[Check]],
         executor: Executor,
+        azurite: bool,
         seed: str | None,
         change_entries: list[SystemVarChangeEntry],
     ):
-        super().__init__(checks, executor, seed)
+        super().__init__(checks, executor, azurite, seed)
         self.change_entries = change_entries
 
     def actions(self) -> list[Action]:

misc/python/materialize/checks/scenarios_upgrade.py
@@ -162,10 +162,14 @@ class UpgradeEntireMzFourVersions(Scenario):
     """Test upgrade X-4 -> X-3 -> X-2 -> X-1 -> X"""
 
     def __init__(
-        self, checks: list[type[Check]], executor: Executor, seed: str | None = None
+        self,
+        checks: list[type[Check]],
+        executor: Executor,
+        azurite: bool,
+        seed: str | None = None,
     ):
         self.minor_versions = get_minor_versions()
-        super().__init__(checks, executor, seed)
+        super().__init__(checks, executor, azurite, seed)
 
     def base_version(self) -> MzVersion:
         return self.minor_versions[3]

misc/python/materialize/checks/scenarios_zero_downtime.py
@@ -257,10 +257,14 @@ class ZeroDowntimeUpgradeEntireMzFourVersions(Scenario):
     """Test 0dt upgrade from X-4 -> X-3 -> X-2 -> X-1 -> X"""
 
     def __init__(
-        self, checks: list[type[Check]], executor: Executor, seed: str | None = None
+        self,
+        checks: list[type[Check]],
+        executor: Executor,
+        azurite: bool,
+        seed: str | None = None,
     ):
         self.minor_versions = get_minor_versions()
-        super().__init__(checks, executor, seed)
+        super().__init__(checks, executor, azurite, seed)
 
     def base_version(self) -> MzVersion:
         return self.minor_versions[3]

commit 8b1ded421296fa3169fd0eef638dda59b43ab14a

bin/environmentd: Fix default blob path
Broken in https://github.com/MaterializeInc/materialize/pull/30817

Noticed by Gabor in https://materializeinc.slack.com/archives/CM7ATT65S/p1737202942223419

misc/python/materialize/cli/run.py
@@ -50,7 +50,8 @@ SANITIZER_TARGET = (
     else f"{Arch.host()}-apple-darwin"
 )
 DEFAULT_POSTGRES = "postgres://root@localhost:26257/materialize"
-DEFAULT_BLOB = "file://mzdata/persist/blob"
+MZDATA = MZ_ROOT / "mzdata"
+DEFAULT_BLOB = f"file://{MZDATA}/persist/blob"
 
 # sets entitlements on the built binary, e.g. environmentd, so you can inspect it with Instruments
 MACOS_ENTITLEMENTS_DATA = """
@@ -234,7 +235,6 @@ def main() -> int:
             command += ["--tokio-console-listen-addr=127.0.0.1:6669"]
         if args.program == "environmentd":
             _handle_lingering_services(kill=args.reset)
-            mzdata = MZ_ROOT / "mzdata"
             scratch = MZ_ROOT / "scratch"
             urlparse(args.postgres).path.removeprefix("/")
             dbconn = _connect_sql(args.postgres)
@@ -249,10 +249,10 @@ def main() -> int:
             if args.reset:
                 # Remove everything in the `mzdata`` directory *except* for
                 # the `prometheus` directory and all contents of `tempo`.
-                paths = list(mzdata.glob("prometheus/*"))
+                paths = list(MZDATA.glob("prometheus/*"))
                 paths.extend(
                     p
-                    for p in mzdata.glob("*")
+                    for p in MZDATA.glob("*")
                     if p.name != "prometheus" and p.name != "tempo"
                 )
                 paths.extend(p for p in scratch.glob("*"))
@@ -263,24 +263,25 @@ def main() -> int:
                     else:
                         path.unlink()
 
-            mzdata.mkdir(exist_ok=True)
+            MZDATA.mkdir(exist_ok=True)
             scratch.mkdir(exist_ok=True)
-            environment_file = mzdata / "environment-id"
+            environment_file = MZDATA / "environment-id"
             try:
                 environment_id = environment_file.read_text().rstrip()
             except FileNotFoundError:
                 environment_id = f"local-az1-{uuid.uuid4()}-0"
                 environment_file.write_text(environment_id)
 
+            print(f"persist-blob-url: {args.blob}")
             command += [
                 # Setting the listen addresses below to 0.0.0.0 is required
                 # to allow Prometheus running in Docker (misc/prometheus)
                 # access these services to scrape metrics.
                 "--internal-http-listen-addr=0.0.0.0:6878",
                 "--orchestrator=process",
-                f"--orchestrator-process-secrets-directory={mzdata}/secrets",
+                f"--orchestrator-process-secrets-directory={MZDATA}/secrets",
                 "--orchestrator-process-tcp-proxy-listen-addr=0.0.0.0",
-                f"--orchestrator-process-prometheus-service-discovery-directory={mzdata}/prometheus",
+                f"--orchestrator-process-prometheus-service-discovery-directory={MZDATA}/prometheus",
                 f"--orchestrator-process-scratch-directory={scratch}",
                 "--secrets-controller=local-file",
                 f"--persist-consensus-url={args.postgres}?options=--search_path=consensus",