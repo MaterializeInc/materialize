8280cd10dab151f0e0cb49aa5f7e7f0df0b2eb48
Gabor Gevay <gabor@materialize.com>
2025-03-09 16:52:15 +0100

    Add feature flag enable_projection_pushdown_after_relation_cse

--- src/sql/src/plan/statement/dml.rs
+++ src/sql/src/plan/statement/dml.rs
@@ -376,7 +376,12 @@ generate_extracted_config!(
     (EnableEagerDeltaJoins, Option<bool>, Default(None)),
     (EnableVariadicLeftJoinLowering, Option<bool>, Default(None)),
     (EnableLetrecFixpointAnalysis, Option<bool>, Default(None)),
-    (EnableJoinPrioritizeArranged, Option<bool>, Default(None))
+    (EnableJoinPrioritizeArranged, Option<bool>, Default(None)),
+    (
+        EnableProjectionPushdownAfterRelationCse,
+        Option<bool>,
+        Default(None)
+    )
 );
 
 impl TryFrom<ExplainPlanOptionExtracted> for ExplainConfig {
@@ -427,6 +432,8 @@ impl TryFrom<ExplainPlanOptionExtracted> for ExplainConfig {
                 reoptimize_imported_views: v.reoptimize_imported_views,
                 enable_reduce_reduction: Default::default(),
                 enable_join_prioritize_arranged: v.enable_join_prioritize_arranged,
+                enable_projection_pushdown_after_relation_cse: v
+                    .enable_projection_pushdown_after_relation_cse,
             },
         })
     }
