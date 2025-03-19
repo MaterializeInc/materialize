4884b2306b72e1b7fe908f9a1235d971481c3c30
Dov Alperin <dov.alperin@materialize.com>
2025-03-19 17:08:56 -0400

    Fix the test_retain_history test
    This was probably always racy based on timestamp selection and compaction. This removes the racy parts and will be followed up with a PR to actually plan queries correctly with an index over a history retained view.
    
    Closes https://github.com/MaterializeInc/database-issues/issues/7450

--- src/environmentd/tests/sql.rs
+++ src/environmentd/tests/sql.rs
@@ -3271,7 +3271,6 @@ async fn test_explain_as_of() {
 
 // Test that RETAIN HISTORY results in the since and upper being separated by the specified amount.
 #[mz_ore::test(tokio::test(flavor = "multi_thread", worker_threads = 1))]
-#[ignore] // TODO: Reenable when database-issues#7450 is fixed
 async fn test_retain_history() {
     let server = test_util::TestHarness::default().start().await;
     let client = server.connect().await.unwrap();
@@ -3350,12 +3349,12 @@ async fn test_retain_history() {
                 let upper = source.write_frontier.into_element();
                 let since = source.read_frontier.into_element();
                 if upper.saturating_sub(since) < Timestamp::from(2000u64) {
-                    anyhow::bail!("{upper} - {since} should be atleast 2s apart")
+                    anyhow::bail!("{upper} - {since} should be at least 2s apart")
                 }
                 client
                     .query(
                         &format!(
-                            "SELECT 1 FROM {name} LIMIT 1 AS OF {}-2000",
+                            "SELECT 1 FROM {name} LIMIT 1 AS OF {}-1000",
                             ts.determination.timestamp_context.timestamp_or_default()
                         ),
                         &[],
@@ -3366,34 +3365,29 @@ async fn test_retain_history() {
             .await
             .unwrap();
 
-        // With an index the AS OF query should fail because we haven't taught the planner about retain
-        // history yet.
-        client
-            .batch_execute(&format!("CREATE DEFAULT INDEX ON {name}"))
+        // We should be able to query the views now and in the past.
+        let ts = get_explain_timestamp(name, &client).await;
+        let now_vals = client
+            .query(&format!("SELECT * FROM {name} AS OF {ts}"), &[])
             .await
-            .unwrap();
+            .expect("this query should succeed")
+            .into_iter()
+            .count();
 
-        let ts = get_explain_timestamp(name, &client).await;
-        assert_contains!(
-            client
-                .query(&format!("SELECT * FROM {name} AS OF {ts}-2000"), &[])
-                .await
-                .unwrap_err()
-                .to_string(),
-            "not valid for all inputs"
-        );
+        let earlier_values = client
+            .query(&format!("SELECT * FROM {name} AS OF {ts}-1000"), &[])
+            .await
+            .expect("this query should succeed")
+            .into_iter()
+            .count();
 
-        // Make sure we didn't fail just because the index didn't have enough time after creation.
-        tokio::time::sleep(Duration::from_secs(3)).await;
-        let ts = get_explain_timestamp(name, &client).await;
-        assert_contains!(
-            client
-                .query(&format!("SELECT * FROM {name} AS OF {ts}-2000"), &[])
-                .await
-                .unwrap_err()
-                .to_string(),
-            "not valid for all inputs"
-        );
+        // We sanity check that we have _some_
+        assert!(now_vals > 0);
+        assert!(earlier_values > 0);
+        // The `s` view is of the counter source, so it should have more values now than earlier.
+        if name == "s" {
+            assert!(now_vals > earlier_values);
+        }
     }
 }
 
