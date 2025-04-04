5761699ca8438d2fda1623448fc011e33ad922a4
Bobby Iliev <bobby@bobbyiliev.com>
2025-04-04 20:39:49 +0300

    self-hosted: bump all terraform modules to latest versions

--- test/terraform/gcp-temporary/main.tf
+++ test/terraform/gcp-temporary/main.tf
@@ -48,7 +48,7 @@ provider "helm" {
 }
 
 module "materialize" {
-  source = "github.com/MaterializeInc/terraform-google-materialize?ref=v0.3.0"
+  source = "github.com/MaterializeInc/terraform-google-materialize?ref=v0.3.2"
 
   project_id = var.project_id
   region     = var.region
