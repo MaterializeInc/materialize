8d8ec192da7e7e6a8ccefbadabbb3a2942ec2117
Frank McSherry <fmcsherry@me.com>
2025-04-04 12:55:36 -0400

    Dictionary compressed row container

--- misc/python/materialize/parallel_workload/action.py
+++ misc/python/materialize/parallel_workload/action.py
@@ -1571,6 +1571,12 @@ class FlipFlagsAction(Action):
             BOOLEAN_FLAG_VALUES
         )
         self.flags_with_values["enable_alter_table_add_column"] = BOOLEAN_FLAG_VALUES
+        self.flags_with_values["enable_arrangement_dictionary_compression"] = (
+            BOOLEAN_FLAG_VALUES
+        )
+        self.flags_with_values["enable_compute_active_dataflow_cancelation"] = (
+            BOOLEAN_FLAG_VALUES
+        )
         self.flags_with_values["enable_compute_peek_response_stash"] = (
             BOOLEAN_FLAG_VALUES
         )
