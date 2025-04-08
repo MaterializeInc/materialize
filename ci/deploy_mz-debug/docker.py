bbdc91eb452608a2881a81e4421cd540308d5099
Sang Jun Bak <sangjun.bak99@gmail.com>
2025-04-08 13:23:29 -0400

    Add deployment scripts and utilities for the Materialize debug tool
    - I refactored mz_lsp_server's deploy_utils into a tarball_uploader. I plan to move the other binaries to use it too.
    - Tested macos and linux separately
    - Unclear if deploy_utils is necessary or if I should rename it. Not confident on where each variable lives as well.
    - Created a binary to run mz-debug

---
+++ ci/deploy_mz-debug/docker.py
@@ -0,0 +1,53 @@
+# Copyright Materialize, Inc. and contributors. All rights reserved.
+#
+# Use of this software is governed by the Business Source License
+# included in the LICENSE file at the root of this repository.
+#
+# As of the Change Date specified in that file, in accordance with
+# the Business Source License, use of this software will be governed
+# by the Apache License, Version 2.0.
+
+import os
+from pathlib import Path
+
+from ci import tarball_uploader
+from materialize import mzbuild, ui
+from materialize.rustc_flags import Sanitizer
+from materialize.xcompile import Arch
+
+from .deploy_util import MZ_DEBUG_VERSION
+
+
+def main() -> None:
+    bazel = ui.env_is_truthy("CI_BAZEL_BUILD")
+    bazel_remote_cache = os.getenv("CI_BAZEL_REMOTE_CACHE")
+
+    repos = [
+        mzbuild.Repository(
+            Path("."),
+            Arch.X86_64,
+            coverage=False,
+            sanitizer=Sanitizer.none,
+            bazel=bazel,
+            bazel_remote_cache=bazel_remote_cache,
+        ),
+        mzbuild.Repository(
+            Path("."),
+            Arch.AARCH64,
+            coverage=False,
+            sanitizer=Sanitizer.none,
+            bazel=bazel,
+            bazel_remote_cache=bazel_remote_cache,
+        ),
+    ]
+
+    print("--- Tagging Docker images")
+    deps = [[repo.resolve_dependencies([repo.images["mz"]])["mz"]] for repo in repos]
+
+    mzbuild.publish_multiarch_images(f"v{MZ_DEBUG_VERSION.str_without_prefix()}", deps)
+    if tarball_uploader.is_latest_version(MZ_DEBUG_VERSION):
+        mzbuild.publish_multiarch_images("latest", deps)
+
+
+if __name__ == "__main__":
+    main()
