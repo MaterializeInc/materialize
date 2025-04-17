e02c5c70cc4ef788ec9c7828e7dd482e2cdea943
Alex Hunt <alex.hunt@materialize.com>
2025-04-17 15:18:53 +0100

    commit the authenticator

---
+++ src/authenticator/src/lib.rs
@@ -0,0 +1,28 @@
+// Copyright Materialize, Inc. and contributors. All rights reserved.
+//
+// Use of this software is governed by the Business Source License
+// included in the LICENSE file.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0.
+
+#[cfg(feature = "clap")]
+use clap::ValueEnum;
+use mz_adapter::Client as AdapterClient;
+use mz_frontegg_auth::Authenticator as FronteggAuthenticator;
+
+#[derive(Debug, Clone)]
+pub enum Authenticator {
+    Frontegg(FronteggAuthenticator),
+    Password(AdapterClient),
+    None,
+}
+
+#[cfg_attr(feature = "clap", derive(ValueEnum))]
+#[derive(Debug, Clone, Copy)]
+pub enum AuthenticatorKind {
+    Frontegg,
+    Password,
+    None,
+}
