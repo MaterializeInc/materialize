4c0122e36926248e78e418bd81fc42d47f9c955a
Peter Travers <traverspw@gmail.com>
2025-04-24 14:54:58 -0400

    refactor panic on mismatch

--- src/compute/src/render/reduce.rs
+++ src/compute/src/render/reduce.rs
@@ -2358,8 +2358,8 @@ mod monoids {
                 // FIXME(ptravers): not sure what we want to do here we can't overwrite
                 // without having owned access to the Row in dest. I don't see a way to
                 // achieve that without a clone. Is it better to panic or clone? I would lean
-                // clone but for safety but I don't know how likely it is to hit the sad case
-                // in practice.
+                // clone for safety but I don't know how likely it is to hit the sad case
+                // in practice?
                 (dest, src) => soft_panic_or_log!(
                     "Mismatched monoid variants in clone_from! destination: {dest:?} source: {src:?}",
                 ),
