1fbdd370d28b89831558c7051e724947cc1b7f88
Dennis Felsing <dennis@felsing.org>
2025-05-08 05:36:58 +0000

    tests: Bump versions for MySQL, Kafka, Redpanda, Postgres

--- misc/python/materialize/mzcompose/services/mysql.py
+++ misc/python/materialize/mzcompose/services/mysql.py
@@ -32,7 +32,7 @@ def create_mysql_server_args(server_id: str, is_master: bool) -> list[str]:
 
 class MySql(Service):
     DEFAULT_ROOT_PASSWORD = "p@ssw0rd"
-    DEFAULT_VERSION = "8.0.35"
+    DEFAULT_VERSION = "8.0.42"
 
     DEFAULT_ADDITIONAL_ARGS = create_mysql_server_args(server_id="1", is_master=True)
 
