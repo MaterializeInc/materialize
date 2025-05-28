1ff35cf70ec4fd59069a47e16260cb99ebb3aa7d
Peter Travers <traverspw@gmail.com>
2025-05-28 16:22:57 -0400

    fix ignore uppers for empty batch sets

--- src/persist-client/src/lib.rs
+++ src/persist-client/src/lib.rs
@@ -1322,6 +1322,7 @@ mod tests {
                     append_upper: Antichain::from_elem(5),
                 },
             );
+
             let batch = write0
                 .batch(&ts3, Antichain::from_elem(3), Antichain::from_elem(4))
                 .await
@@ -1338,6 +1339,7 @@ mod tests {
                     append_upper: Antichain::from_elem(3),
                 },
             );
+
             let batch = write0
                 .batch(&ts3, Antichain::from_elem(3), Antichain::from_elem(4))
                 .await
