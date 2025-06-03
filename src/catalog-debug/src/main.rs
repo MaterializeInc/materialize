7ccc9e2e75bde7664914da6638432c88ac4671c3
Alex Hunt <alex.hunt@materialize.com>
2025-06-03 13:38:40 +0100

    remove unused TODO

--- src/catalog-debug/src/main.rs
+++ src/catalog-debug/src/main.rs
@@ -655,7 +655,6 @@ async fn upgrade_check(
             enable_expression_cache_override: None,
             enable_0dt_deployment: true,
             helm_chart_version: None,
-            // TODO pass this in here?
             external_login_password_mz_system: None,
         },
         &mut storage,
