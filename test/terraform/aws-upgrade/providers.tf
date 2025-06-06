9cc2da05467dd631d7ae5aed402165af6fc96985
Bobby Iliev <bobby@bobbyiliev.com>
2025-06-06 15:47:07 +0300

    self-hosted: update aws upgrade test to use the new structure

---
+++ test/terraform/aws-upgrade/providers.tf
@@ -0,0 +1,23 @@
+# Copyright Materialize, Inc. and contributors. All rights reserved.
+#
+# Use of this software is governed by the Business Source License
+# included in the LICENSE file at the root of this repository.
+#
+# As of the Change Date specified in that file, in accordance with
+# the Business Source License, use of this software will be governed
+# by the Apache License, Version 2.0.
+
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.76.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.0"
+    }
+  }
+}
