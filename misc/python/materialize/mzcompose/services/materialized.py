043cd3763c09a5955e2f055912da2f2b994489a4
Dennis Felsing <dennis@felsing.org>
2025-07-04 16:39:07 +0000

    mzcompose: Disable sanity restarts in PRs

--- misc/python/materialize/mzcompose/services/materialized.py
+++ misc/python/materialize/mzcompose/services/materialized.py
@@ -292,7 +292,11 @@ class Materialized(Service):
                 limits["cpus"] = cpu
             config["deploy"] = {"resources": {"limits": limits}}
 
-        if sanity_restart:
+        # Sanity restarts are rather slow and rarely find a bug, running them
+        # on main and releases is enough to prevent regressions.
+        if sanity_restart and (
+            os.getenv("BUILDKITE_BRANCH") == "main" or os.getenv("BUILDKITE_TAG")
+        ):
             # Workaround for https://github.com/docker/compose/issues/11133
             config["labels"] = {"sanity_restart": True}
 
