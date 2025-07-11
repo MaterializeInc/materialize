97802cf2cc379c8f5115d80b27516be4650b1039
Frank McSherry <fmcsherry@me.com>
2025-07-11 10:08:10 -0400

    feature flags

--- src/sql/src/session/vars/definitions.rs
+++ src/sql/src/session/vars/definitions.rs
@@ -1794,6 +1794,12 @@ feature_flags!(
     },
     // Actual feature flags
     {
+        name: enable_guard_subquery_tablefunc,
+        desc: "Whether HIR -> MIR lowering should use a new tablefunc to guard subquery sizes",
+        default: true,
+        enable_for_item_parsing: true,
+    },
+    {
         name: enable_binary_date_bin,
         desc: "the binary version of date_bin function",
         default: false,
@@ -2214,6 +2220,7 @@ feature_flags!(
 impl From<&super::SystemVars> for OptimizerFeatures {
     fn from(vars: &super::SystemVars) -> Self {
         Self {
+            enable_guard_subquery_tablefunc: vars.enable_guard_subquery_tablefunc(),
             enable_consolidate_after_union_negate: vars.enable_consolidate_after_union_negate(),
             enable_eager_delta_joins: vars.enable_eager_delta_joins(),
             enable_new_outer_join_lowering: vars.enable_new_outer_join_lowering(),
