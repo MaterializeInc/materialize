3a3bdb161f4c56f4b3842f79b0af56763ce3eff7
Moritz Hoffmann <mh@materialize.com>
2025-07-25 16:25:46 +0200

    Include global_id in explain analyze's join (#33144)
    We previously aggregated data across equal LIR IDs, missing that they're
    scoped per global ID. This change fixes it by grouping by global ID in
    addition to the LIR ID.
    
    Fixes https://github.com/MaterializeInc/database-issues/issues/9130
    
    ---------
    
    Signed-off-by: Moritz Hoffmann <antiguru@gmail.com>

--- src/sql/src/plan/statement/dml.rs
+++ src/sql/src/plan/statement/dml.rs
@@ -845,7 +845,8 @@ pub fn plan_explain_analyze(
                         ctes.push((
                             "summary_memory",
                             r#"
-  SELECT mlm.lir_id AS lir_id,
+  SELECT mlm.global_id AS global_id,
+         mlm.lir_id AS lir_id,
          SUM(mas.size) AS total_memory,
          SUM(mas.records) AS total_records,
          CASE WHEN COUNT(DISTINCT mas.worker_id) <> 0 THEN SUM(mas.size) / COUNT(DISTINCT mas.worker_id) ELSE NULL END AS avg_memory,
@@ -853,24 +854,25 @@ pub fn plan_explain_analyze(
     FROM      mz_introspection.mz_lir_mapping mlm
          JOIN mz_introspection.mz_arrangement_sizes_per_worker mas
            ON (mlm.operator_id_start <= mas.operator_id AND mas.operator_id < mlm.operator_id_end)
-GROUP BY mlm.lir_id"#,
+GROUP BY mlm.global_id, mlm.lir_id"#,
                         ));
-                        from.push("LEFT JOIN summary_memory sm USING (lir_id)");
+                        from.push("LEFT JOIN summary_memory sm USING (global_id, lir_id)");
 
                         if skew {
                             ctes.push((
                                 "per_worker_memory",
                                 r#"
-  SELECT mlm.lir_id AS lir_id,
+  SELECT mlm.global_id AS global_id,
+         mlm.lir_id AS lir_id,
          mas.worker_id AS worker_id,
          SUM(mas.size) AS worker_memory,
          SUM(mas.records) AS worker_records
     FROM      mz_introspection.mz_lir_mapping mlm
          JOIN mz_introspection.mz_arrangement_sizes_per_worker mas
            ON (mlm.operator_id_start <= mas.operator_id AND mas.operator_id < mlm.operator_id_end)
-GROUP BY mlm.lir_id, mas.worker_id"#,
+GROUP BY mlm.global_id, mlm.lir_id, mas.worker_id"#,
                             ));
-                            from.push("LEFT JOIN per_worker_memory pwm USING (lir_id)");
+                            from.push("LEFT JOIN per_worker_memory pwm USING (global_id, lir_id)");
 
                             if let Some(worker_id) = worker_id {
                                 predicates.push(format!("pwm.worker_id = {worker_id}"));
@@ -901,29 +903,31 @@ GROUP BY mlm.lir_id, mas.worker_id"#,
                         ctes.push((
                             "summary_cpu",
                             r#"
-  SELECT mlm.lir_id AS lir_id,
+  SELECT mlm.global_id AS global_id,
+         mlm.lir_id AS lir_id,
          SUM(mse.elapsed_ns) AS total_ns,
          CASE WHEN COUNT(DISTINCT mse.worker_id) <> 0 THEN SUM(mse.elapsed_ns) / COUNT(DISTINCT mse.worker_id) ELSE NULL END AS avg_ns
     FROM      mz_introspection.mz_lir_mapping mlm
          JOIN mz_introspection.mz_scheduling_elapsed_per_worker mse
            ON (mlm.operator_id_start <= mse.id AND mse.id < mlm.operator_id_end)
-GROUP BY mlm.lir_id"#,
+GROUP BY mlm.global_id, mlm.lir_id"#,
                         ));
-                        from.push("LEFT JOIN summary_cpu sc USING (lir_id)");
+                        from.push("LEFT JOIN summary_cpu sc USING (global_id, lir_id)");
 
                         if skew {
                             ctes.push((
                                 "per_worker_cpu",
                                 r#"
-  SELECT mlm.lir_id AS lir_id,
+  SELECT mlm.global_id AS global_id,
+         mlm.lir_id AS lir_id,
          mse.worker_id AS worker_id,
          SUM(mse.elapsed_ns) AS worker_ns
     FROM      mz_introspection.mz_lir_mapping mlm
          JOIN mz_introspection.mz_scheduling_elapsed_per_worker mse
            ON (mlm.operator_id_start <= mse.id AND mse.id < mlm.operator_id_end)
-GROUP BY mlm.lir_id, mse.worker_id"#,
+GROUP BY mlm.global_id, mlm.lir_id, mse.worker_id"#,
                             ));
-                            from.push("LEFT JOIN per_worker_cpu pwc USING (lir_id)");
+                            from.push("LEFT JOIN per_worker_cpu pwc USING (global_id, lir_id)");
 
                             if let Some(worker_id) = worker_id {
                                 predicates.push(format!("pwc.worker_id = {worker_id}"));
