54497117bbec6c7d618c1e563953bf28adb08a9a
Dennis Felsing <dennis@felsing.org>
2025-07-25 16:56:32 +0000

    mzbuild: Mirror ssh-bastion-host from quay.io to Dockerhub
    https://materializeinc.slack.com/archives/C01LKF361MZ/p1753461350615199

--- misc/python/materialize/mzcompose/services/ssh_bastion_host.py
+++ misc/python/materialize/mzcompose/services/ssh_bastion_host.py
@@ -37,7 +37,7 @@ class SshBastionHost(Service):
         super().__init__(
             name=name,
             config={
-                "image": "quay.io/panubo/sshd:1.7.1",
+                "mzbuild": "ssh-bastion-host",
                 "init": True,
                 "ports": ["22"],
                 "environment": [
