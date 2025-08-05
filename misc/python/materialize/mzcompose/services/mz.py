bc420c15e2f3da7c386d75fe27473e80972c49bb
Dennis Felsing <dennis@felsing.org>
2025-08-05 17:52:59 +0000

    ci+tests: Fixes for coverage
    All noticed while trying to run https://github.com/MaterializeInc/materialize/pull/33245

--- misc/python/materialize/mzcompose/services/mz.py
+++ misc/python/materialize/mzcompose/services/mz.py
@@ -8,6 +8,7 @@
 # by the Apache License, Version 2.0.
 
 import hashlib
+import os
 
 import toml
 
@@ -57,10 +58,13 @@ class Mz(Service):
 
         with open(config_name, "w") as f:
             f.write(config_str)
+        os.chmod(config_name, 0o777)
         super().__init__(
             name=name,
             config={
                 "mzbuild": "mz",
-                "volumes": [f"{config_name}:/root/.config/materialize/mz.toml"],
+                "volumes": [
+                    f"{config_name}:/home/materialize/.config/materialize/mz.toml"
+                ],
             },
         )
