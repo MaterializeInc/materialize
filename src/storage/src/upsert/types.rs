3ed314edd55c571a597f41cd3ab03a53212ecd80
Petros Angelatos <petrosagg@gmail.com>
2025-08-11 19:26:53 +0300

    adjust expected memory sizes
    The in-memory hashmap backend is no longer used so having a slightly
    bigger struct in exchange for fewer allocations is worth it.
    
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>

--- src/storage/src/upsert/types.rs
+++ src/storage/src/upsert/types.rs
@@ -1419,7 +1419,7 @@ mod tests {
     fn test_memory_size() {
         let finalized_value: StateValue<(), ()> = StateValue::finalized_value(Ok(Row::default()));
         assert!(
-            finalized_value.memory_size() <= 88,
+            finalized_value.memory_size() <= 144,
             "memory size is {}",
             finalized_value.memory_size(),
         );
@@ -1427,7 +1427,7 @@ mod tests {
         let provisional_value_with_finalized_value: StateValue<(), ()> =
             finalized_value.into_provisional_value(Ok(Row::default()), (), ());
         assert!(
-            provisional_value_with_finalized_value.memory_size() <= 112,
+            provisional_value_with_finalized_value.memory_size() <= 168,
             "memory size is {}",
             provisional_value_with_finalized_value.memory_size(),
         );
@@ -1435,7 +1435,7 @@ mod tests {
         let provisional_value_without_finalized_value: StateValue<(), ()> =
             StateValue::new_provisional_value(Ok(Row::default()), (), ());
         assert!(
-            provisional_value_without_finalized_value.memory_size() <= 88,
+            provisional_value_without_finalized_value.memory_size() <= 144,
             "memory size is {}",
             provisional_value_without_finalized_value.memory_size(),
         );
@@ -1448,7 +1448,7 @@ mod tests {
             &mut Vec::new(),
         );
         assert!(
-            consolidating_value.memory_size() <= 90,
+            consolidating_value.memory_size() <= 146,
             "memory size is {}",
             consolidating_value.memory_size(),
         );
