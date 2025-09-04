bbcd9ca1bd3072f6ab3d4f18670b5d19c523bf3f
Petros Angelatos <petrosagg@gmail.com>
2025-09-04 15:13:06 +0300

    add error log for invalid PARTITION_BY statements
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>

--- src/sql/src/plan/statement/ddl.rs
+++ src/sql/src/plan/statement/ddl.rs
@@ -182,9 +182,19 @@ static MANAGED_REPLICA_PATTERN: std::sync::LazyLock<regex::Regex> =
 /// Given a relation desc and a column list, checks that:
 /// - the column list is a prefix of the desc;
 /// - all the listed columns are types that have meaningful Persist-level ordering.
-fn check_partition_by(desc: &RelationDesc, partition_by: Vec<Ident>) -> Result<(), PlanError> {
+fn check_partition_by(desc: &RelationDesc, mut partition_by: Vec<Ident>) -> Result<(), PlanError> {
+    if partition_by.len() > desc.len() {
+        tracing::error!(
+            "PARTITION BY contains more columns than the relation. (expected at most {}, got {})",
+            desc.len(),
+            partition_by.len()
+        );
+        partition_by.truncate(desc.len());
+    }
+
+    let desc_prefix = desc.iter().take(partition_by.len());
     for (idx, ((desc_name, desc_type), partition_name)) in
-        desc.iter().zip(partition_by.into_iter()).enumerate()
+        desc_prefix.zip_eq(partition_by).enumerate()
     {
         let partition_name = normalize::column_name(partition_name);
         if *desc_name != partition_name {
