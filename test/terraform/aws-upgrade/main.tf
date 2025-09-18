955eaa4b05ca0f87fb502b376bf96be16c0c6274
Materialize <infra+bot@materialize.com>
2025-09-18 19:21:53 +0000

    terraform tests: Bump to AWS v0.5.1, GCP v0.5.1, Azure v0.5.1

--- test/terraform/aws-upgrade/main.tf
+++ test/terraform/aws-upgrade/main.tf
@@ -51,7 +51,7 @@ variable "orchestratord_version" {
 }
 
 module "materialize_infrastructure" {
-  source = "git::https://github.com/MaterializeInc/terraform-aws-materialize.git?ref=v0.4.12"
+  source = "git::https://github.com/MaterializeInc/terraform-aws-materialize.git?ref=v0.5.1"
 
   providers = {
     aws        = aws
