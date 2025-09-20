e3954561cc1426df937d481d187076ee3dd4bc94
Sang Jun Bak <sangjun.bak99@gmail.com>
2025-09-20 00:35:02 -0400

    Don't default to initial role attributes
    Because the initial role attribute has login set to false, we were deactivating it on migration.

--- src/catalog/src/durable/upgrade/v74_mz_system.rs
+++ src/catalog/src/durable/upgrade/v74_mz_system.rs
@@ -37,9 +37,7 @@ pub fn upgrade(
 
 impl UpgradeFrom<v74::state_update_kind::Role> for v74::state_update_kind::Role {
     fn upgrade_from(value: v74::state_update_kind::Role) -> Self {
-        let new_key = value.key.map(|key| v74::RoleKey {
-            id: key.id.map(v74::RoleId::upgrade_from),
-        });
+        let new_key = value.key;
 
         let is_mz_system = value
             .value
@@ -49,9 +47,9 @@ impl UpgradeFrom<v74::state_update_kind::Role> for v74::state_update_kind::Role 
         let mut new_value = value.value.map(|value| v74::RoleValue {
             name: value.name,
             oid: value.oid,
-            attributes: value.attributes.map(v74::RoleAttributes::upgrade_from),
-            membership: value.membership.map(v74::RoleMembership::upgrade_from),
-            vars: value.vars.map(v74::RoleVars::upgrade_from),
+            attributes: value.attributes,
+            membership: value.membership,
+            vars: value.vars,
         });
 
         if is_mz_system {
@@ -65,99 +63,6 @@ impl UpgradeFrom<v74::state_update_kind::Role> for v74::state_update_kind::Role 
         v74::state_update_kind::Role {
             key: new_key,
             value: new_value,
-        }
-    }
-}
-
-impl UpgradeFrom<v74::RoleVars> for v74::RoleVars {
-    fn upgrade_from(value: v74::RoleVars) -> Self {
-        v74::RoleVars {
-            entries: value
-                .entries
-                .iter()
-                .map(|val| v74::role_vars::Entry::upgrade_from(val.clone()))
-                .collect(),
-        }
-    }
-}
-
-impl UpgradeFrom<v74::RoleMembership> for v74::RoleMembership {
-    fn upgrade_from(value: v74::RoleMembership) -> Self {
-        v74::RoleMembership {
-            map: value
-                .map
-                .iter()
-                .map(|val| v74::role_membership::Entry::upgrade_from(*val))
-                .collect(),
-        }
-    }
-}
-
-impl UpgradeFrom<v74::role_membership::Entry> for v74::role_membership::Entry {
-    fn upgrade_from(value: v74::role_membership::Entry) -> Self {
-        v74::role_membership::Entry {
-            key: value.key.map(v74::RoleId::upgrade_from),
-            value: value.value.map(v74::RoleId::upgrade_from),
-        }
-    }
-}
-
-impl UpgradeFrom<v74::role_vars::Entry> for v74::role_vars::Entry {
-    fn upgrade_from(value: v74::role_vars::Entry) -> Self {
-        v74::role_vars::Entry {
-            key: value.key,
-            val: value.val.map(v74::role_vars::entry::Val::upgrade_from),
-        }
-    }
-}
-
-impl UpgradeFrom<v74::role_vars::entry::Val> for v74::role_vars::entry::Val {
-    fn upgrade_from(value: v74::role_vars::entry::Val) -> Self {
-        match value {
-            v74::role_vars::entry::Val::Flat(x) => v74::role_vars::entry::Val::Flat(x),
-            v74::role_vars::entry::Val::SqlSet(x) => {
-                v74::role_vars::entry::Val::SqlSet(v74::role_vars::SqlSet::upgrade_from(x))
-            }
-        }
-    }
-}
-
-impl UpgradeFrom<v74::role_vars::SqlSet> for v74::role_vars::SqlSet {
-    fn upgrade_from(value: v74::role_vars::SqlSet) -> Self {
-        v74::role_vars::SqlSet {
-            entries: value.entries,
-        }
-    }
-}
-
-impl UpgradeFrom<v74::RoleAttributes> for v74::RoleAttributes {
-    fn upgrade_from(value: v74::RoleAttributes) -> Self {
-        v74::RoleAttributes {
-            inherit: value.inherit,
-            ..Default::default()
-        }
-    }
-}
-
-impl UpgradeFrom<v74::RoleId> for v74::RoleId {
-    fn upgrade_from(value: v74::RoleId) -> Self {
-        let value = match value.value {
-            Some(v74::role_id::Value::System(x)) => Some(v74::role_id::Value::System(x)),
-            Some(v74::role_id::Value::User(x)) => Some(v74::role_id::Value::User(x)),
-            Some(v74::role_id::Value::Public(_)) => {
-                Some(v74::role_id::Value::Public(v74::Empty {}))
-            }
-            Some(v74::role_id::Value::Predefined(x)) => Some(v74::role_id::Value::Predefined(x)),
-            None => None,
-        };
-        v74::RoleId { value }
-    }
-}
-
-impl UpgradeFrom<v74::RoleKey> for v74::RoleKey {
-    fn upgrade_from(value: v74::RoleKey) -> Self {
-        Self {
-            id: value.id.map(v74::RoleId::upgrade_from),
         }
     }
 }
