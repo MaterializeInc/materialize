a3b3415cf2aa4129e16448509e64bf20379d7403
Jan Teske <jteske@posteo.net>
2025-09-22 19:14:03 +0200

    clusterd: expose heap usage and limit
    This commit extends the usage metrics provided by clusterd's
    `/api/usage-metrics` endpoint with the memory usage, as well as the
    effective heap (memory+swap) limit.
    
    As for the existing usage collection, we mainly focus on Linux support,
    for macOS we only provide the required stubs to make sure the code
    compiles and run.

--- src/clusterd/src/usage_metrics.rs
+++ src/clusterd/src/usage_metrics.rs
@@ -15,7 +15,7 @@
 use std::path::PathBuf;
 
 use serde::Serialize;
-use tracing::error;
+use tracing::{debug, error};
 
 /// A system usage metrics collector.
 pub(crate) struct Collector {
@@ -25,9 +25,15 @@ pub(crate) struct Collector {
 impl Collector {
     /// Collect current system usage metrics.
     pub fn collect(&self) -> Usage {
+        let disk_bytes = self.collect_disk_usage();
+        let (memory_bytes, swap_bytes) = collect_heap_usage();
+        let heap_limit = collect_heap_limit();
+
         Usage {
-            disk_bytes: self.collect_disk_usage(),
-            swap_bytes: self.collect_swap_usage(),
+            disk_bytes,
+            memory_bytes,
+            swap_bytes,
+            heap_limit,
         }
     }
 
@@ -49,29 +55,9 @@ impl Collector {
         let used_blocks = u64::from(stat.blocks() - stat.blocks_available());
         let used_bytes = used_blocks * stat.fragment_size();
 
+        debug!("disk usage: {used_bytes}");
+
         Some(used_bytes)
-    }
-
-    #[cfg(target_os = "linux")]
-    fn collect_swap_usage(&self) -> Option<u64> {
-        use mz_compute::memory_limiter::ProcStatus;
-        use mz_ore::cast::CastInto;
-
-        match ProcStatus::from_proc() {
-            Ok(status) => {
-                let bytes = status.vm_swap.cast_into();
-                Some(bytes)
-            }
-            Err(err) => {
-                error!("error reading /proc/self/status: {err}");
-                None
-            }
-        }
-    }
-
-    #[cfg(not(target_os = "linux"))]
-    fn collect_swap_usage(&self) -> Option<u64> {
-        None
     }
 }
 
@@ -79,5 +65,154 @@ impl Collector {
 #[derive(Serialize)]
 pub(crate) struct Usage {
     disk_bytes: Option<u64>,
+    memory_bytes: Option<u64>,
     swap_bytes: Option<u64>,
+    heap_limit: Option<u64>,
 }
+
+#[cfg(target_os = "linux")]
+mod linux {
+    use std::fs;
+    use std::path::Path;
+
+    use mz_compute::memory_limiter;
+    use mz_ore::cast::CastInto;
+    use tracing::{debug, error};
+
+    /// Collect memory and swap usage.
+    pub fn collect_heap_usage() -> (Option<u64>, Option<u64>) {
+        use mz_ore::cast::CastInto;
+
+        match memory_limiter::ProcStatus::from_proc() {
+            Ok(status) => {
+                let memory_bytes = status.vm_rss.cast_into();
+                let swap_bytes = status.vm_swap.cast_into();
+
+                debug!("memory usage: {memory_bytes}");
+                debug!("swap usage: {swap_bytes}");
+
+                (Some(memory_bytes), Some(swap_bytes))
+            }
+            Err(err) => {
+                error!("error reading /proc/self/status: {err}");
+                (None, None)
+            }
+        }
+    }
+
+    /// Collect the heap limit, i.e. memory + swap limit.
+    pub fn collect_heap_limit() -> Option<u64> {
+        // If we don't know the physical limits, we can't know the heap limit.
+        let (phys_mem_limit, phys_swap_limit) = get_physical_limits()?;
+
+        // Limits might be reduced by the cgroup.
+        let (cgroup_mem_limit, cgroup_swap_limit) = get_cgroup_limits();
+        let mem_limit = cgroup_mem_limit.unwrap_or(u64::MAX).min(phys_mem_limit);
+        let swap_limit = cgroup_swap_limit.unwrap_or(u64::MAX).min(phys_swap_limit);
+
+        let heap_limit = mem_limit + swap_limit;
+
+        // Heap limit might be reduced by the memory limiter.
+        let limiter_limit = memory_limiter::get_memory_limit().map(CastInto::cast_into);
+        let heap_limit = limiter_limit.unwrap_or(u64::MAX).min(heap_limit);
+
+        debug!("memory limit: {mem_limit} (phys={phys_mem_limit}, cgroup={cgroup_mem_limit:?})");
+        debug!("swap limit: {swap_limit} (phys={phys_swap_limit}, cgroup={cgroup_swap_limit:?})");
+        debug!("heap limit: {heap_limit} (limiter={limiter_limit:?})");
+
+        Some(heap_limit)
+    }
+
+    /// Collect the physical memory and swap limits.
+    fn get_physical_limits() -> Option<(u64, u64)> {
+        let meminfo = match fs::read_to_string("/proc/meminfo") {
+            Ok(meminfo) => meminfo,
+            Err(error) => {
+                error!("reading `/proc/meminfo`: {error}");
+                return None;
+            }
+        };
+
+        fn parse_kib_line(line: &str) -> Option<u64> {
+            let result = line
+                .split_whitespace()
+                .nth(1)
+                .and_then(|x| x.parse::<u64>().ok())
+                .map(|kib| kib * 1024);
+            if result.is_none() {
+                error!("invalid `/proc/meminfo` line: {line}");
+            }
+            result
+        }
+
+        let mut memory = None;
+        let mut swap = None;
+        for line in meminfo.lines() {
+            if line.starts_with("MemTotal:") {
+                memory = parse_kib_line(line);
+            } else if line.starts_with("SwapTotal:") {
+                swap = parse_kib_line(line);
+            }
+        }
+
+        let Some(memory) = memory else {
+            error!("failed to collect physical memory limit");
+            return None;
+        };
+        let Some(swap) = swap else {
+            error!("failed to collect physical swap limit");
+            return None;
+        };
+
+        Some((memory, swap))
+    }
+
+    /// Collect the memory and swap limits enforced by the current cgroup.
+    ///
+    /// We make the following simplifying assumptions that hold for a standard Kubernetes
+    /// environment:
+    //  * The current process is a member of exactly one cgroups v2 hierarchy.
+    //  * The cgroups hierarchy is mounted at `/sys/fs/cgroup`.
+    //  * The limits are applied to the current cgroup directly (and not one of its ancestors).
+    fn get_cgroup_limits() -> (Option<u64>, Option<u64>) {
+        let Ok(proc_cgroup) = fs::read_to_string("/proc/self/cgroup") else {
+            return (None, None);
+        };
+        let Some(cgroup_path) = proc_cgroup.split(':').nth(2) else {
+            error!("invalid `/proc/self/cgroup` format: {proc_cgroup}");
+            return (None, None);
+        };
+
+        let root = Path::new("/sys/fs/cgroup").join(cgroup_path);
+        let memory_file = root.join("memory.max");
+        let swap_file = root.join("memory.swap.max");
+
+        let memory = fs::read_to_string(memory_file)
+            .ok()
+            .and_then(|s| s.parse().ok());
+        let swap = fs::read_to_string(swap_file)
+            .ok()
+            .and_then(|s| s.parse().ok());
+
+        (memory, swap)
+    }
+}
+
+#[cfg(not(target_os = "linux"))]
+mod macos {
+    use mz_compute::memory_limiter;
+    use mz_ore::cast::CastInto;
+
+    pub fn collect_heap_usage() -> (Option<u64>, Option<u64>) {
+        (None, None)
+    }
+
+    pub fn collect_heap_limit() -> Option<u64> {
+        memory_limiter::get_memory_limit().map(CastInto::cast_into)
+    }
+}
+
+#[cfg(target_os = "linux")]
+use linux::*;
+#[cfg(not(target_os = "linux"))]
+use macos::*;
