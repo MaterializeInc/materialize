26987dabf60f3933153796d0fb87415c03d1b6a7
Materialize <infra+bot@materialize.com>
2025-09-23 20:57:04 +0000

    terraform tests: Bump to AWS v0.5.2, GCP v0.5.2, Azure v0.5.2

--- test/terraform/aws-persistent/main.tf
+++ test/terraform/aws-persistent/main.tf
@@ -27,7 +27,7 @@ variable "orchestratord_version" {
 }
 
 module "materialize_infrastructure" {
-  source = "git::https://github.com/MaterializeInc/terraform-aws-materialize.git?ref=v0.5.1"
+  source = "git::https://github.com/MaterializeInc/terraform-aws-materialize.git?ref=v0.5.2"
 
   # Basic settings
   namespace    = "aws-persistent"
