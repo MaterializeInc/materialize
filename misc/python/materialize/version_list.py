ee1bad6b69a036769bd60aebd7068b79ea3695d5
Sang Jun Bak <sangjun.bak99@gmail.com>
2025-10-30 15:22:54 -0400

    Deduplicate self managed version fetching code
    Caused by overlapping work between Dennis and i.

--- misc/python/materialize/version_list.py
+++ misc/python/materialize/version_list.py
@@ -38,16 +38,24 @@ class SelfManagedVersion:
 
 
 def fetch_self_managed_versions() -> list[SelfManagedVersion]:
-    return [
-        SelfManagedVersion(
+    result: list[SelfManagedVersion] = []
+    for entry in yaml.safe_load(
+        requests.get("https://materializeinc.github.io/materialize/index.yaml").text
+    )["entries"]["materialize-operator"]:
+        self_managed_version = SelfManagedVersion(
             MzVersion.parse_mz(entry["version"]),
             MzVersion.parse_mz(entry["appVersion"]),
         )
-        for entry in yaml.safe_load(
-            requests.get("https://materializeinc.github.io/materialize/index.yaml").text
-        )["entries"]["materialize-operator"]
-        if MzVersion.parse_mz(entry["appVersion"]) not in INVALID_VERSIONS
-    ]
+        if (
+            not self_managed_version.version.prerelease
+            and self_managed_version.version not in BAD_SELF_MANAGED_VERSIONS
+        ):
+            result.append(self_managed_version)
+    return result
+
+
+def get_all_self_managed_versions() -> list[MzVersion]:
+    return sorted([version.version for version in fetch_self_managed_versions()])
 
 
 def get_self_managed_versions() -> list[MzVersion]:
@@ -71,11 +79,11 @@ def get_supported_self_managed_versions() -> tuple[list[MzVersion], list[MzVersi
     self_managed_versions = fetch_self_managed_versions()
     current_version = MzVersion.parse_cargo()
     filtered_versions = sorted(
-        [
+        {
             v.version
             for v in self_managed_versions
             if v.helm_version.major == 25 or v.helm_version.major == 26
-        ]
+        }
     )
     previous_versions = [v for v in filtered_versions if v < current_version]
     future_versions = [v for v in filtered_versions if v > current_version]
@@ -88,19 +96,14 @@ BAD_SELF_MANAGED_VERSIONS = {
     MzVersion.parse_mz("v0.130.2"),
     MzVersion.parse_mz("v0.130.3"),
     MzVersion.parse_mz("v0.130.4"),
+    MzVersion.parse_mz(
+        "v0.147.7"
+    ),  # Incompatible for upgrades because it clears login attribute for roles due to catalog migration
+    MzVersion.parse_mz(
+        "v0.147.14"
+    ),  # Incompatible for upgrades because it clears login attribute for roles due to catalog migration
+    MzVersion.parse_mz("v0.157.0"),
 }
-
-
-def get_all_self_managed_versions() -> list[MzVersion]:
-    result = set()
-    for entry in yaml.safe_load(
-        requests.get("https://materializeinc.github.io/materialize/index.yaml").text
-    )["entries"]["materialize-operator"]:
-        version = MzVersion.parse_mz(entry["appVersion"])
-        if not version.prerelease and version not in BAD_SELF_MANAGED_VERSIONS:
-            result.add(version)
-    return sorted(result)
-
 
 # not released on Docker
 INVALID_VERSIONS = {
@@ -124,13 +127,6 @@ INVALID_VERSIONS = {
     MzVersion.parse_mz("v0.93.0"),  # accidental release
     MzVersion.parse_mz("v0.99.1"),  # incompatible for upgrades
     MzVersion.parse_mz("v0.113.1"),  # incompatible for upgrades
-    MzVersion.parse_mz(
-        "v0.147.7"
-    ),  # Incompatible for upgrades because it clears login attribute for roles due to catalog migration
-    MzVersion.parse_mz(
-        "v0.147.14"
-    ),  # Incompatible for upgrades because it clears login attribute for roles due to catalog migration
-    MzVersion.parse_mz("v0.157.0"),
 }
 
 _SKIP_IMAGE_CHECK_BELOW_THIS_VERSION = MzVersion.parse_mz("v0.77.0")
