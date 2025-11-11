f38e6783ae1ed5ce8e309bfaa279fe87d6fe99d3
Materialize <infra+bot@materialize.com>
2025-11-11 23:32:37 +0000

    terraform tests: Bump to AWS v0.5.10, GCP v0.5.10, Azure v0.5.10

--- test/terraform/aws-temporary/main.tf
+++ test/terraform/aws-temporary/main.tf
@@ -51,7 +51,7 @@ variable "orchestratord_version" {
 }
 
 module "materialize_infrastructure" {
-  source = "git::https://github.com/MaterializeInc/terraform-aws-materialize.git?ref=v0.5.8"
+  source = "git::https://github.com/MaterializeInc/terraform-aws-materialize.git?ref=v0.5.10"
 
   providers = {
     aws        = aws
