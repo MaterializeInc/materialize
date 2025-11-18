223c7792019c47aa9b773bc79dfdda0951e1a303
Dennis Felsing <dennis@felsing.org>
2025-11-18 17:05:04 +0000

    terraform tests: Bump to AWS v0.6.4, GCP v0.6.4, Azure v0.6.4

--- test/terraform/azure-temporary/main.tf
+++ test/terraform/azure-temporary/main.tf
@@ -60,7 +60,7 @@ resource "azurerm_resource_group" "materialize" {
 }
 
 module "materialize" {
-  source              = "git::https://github.com/MaterializeInc/terraform-azurerm-materialize.git?ref=v0.6.3"
+  source              = "git::https://github.com/MaterializeInc/terraform-azurerm-materialize.git?ref=v0.6.4"
   resource_group_name = azurerm_resource_group.materialize.name
   location            = "eastus2"
   prefix              = "mz-tf-test"
