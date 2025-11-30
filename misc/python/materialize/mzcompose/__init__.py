7f4c709ff6ebf1bb31892e52f3d3f2c879f36f9b
Dennis Felsing <dennis@felsing.org>
2025-11-30 19:20:39 +0000

    tests: Switch us to 100% statement logging by default
    Still 0% for benchmarks to keep them more consistent
    
    See https://materializeinc.slack.com/archives/C01LKF361MZ/p1764511979959739 for discussion

--- misc/python/materialize/mzcompose/__init__.py
+++ misc/python/materialize/mzcompose/__init__.py
@@ -370,10 +370,14 @@ def get_variable_system_parameters(
         ),
         VariableSystemParameter("pg_offset_known_interval", "1s", ["100ms", "1s"]),
         VariableSystemParameter(
-            "statement_logging_default_sample_rate", "0.01", ["0", "0.01"]
+            "statement_logging_default_sample_rate",
+            "1.0",
+            ["0", "0.01", "0.5", "0.99", "1.0"],
         ),
         VariableSystemParameter(
-            "statement_logging_max_sample_rate", "0.01", ["0", "0.01"]
+            "statement_logging_max_sample_rate",
+            "1.0",
+            ["0", "0.01", "0.5", "0.99", "1.0"],
         ),
         VariableSystemParameter("storage_reclock_to_latest", "true", ["true", "false"]),
         VariableSystemParameter(
