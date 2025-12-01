515958a551032ff69e7b17c48c4506a9cb75221a
bobbyiliev <bobby@bobbyiliev.com>
2025-12-01 20:11:14 +0200

    terraform: migrate aws-temporary to monorepo modules

---
+++ test/terraform/aws-temporary/variables.tf
@@ -0,0 +1,66 @@
+# Copyright Materialize, Inc. and contributors. All rights reserved.
+#
+# Use of this software is governed by the Business Source License
+# included in the LICENSE file at the root of this repository.
+#
+# As of the Change Date specified in that file, in accordance with
+# the Business Source License, use of this software will be governed
+# by the Apache License, Version 2.0.
+
+variable "name_prefix" {
+  description = "Prefix for resource names"
+  type        = string
+  default     = "aws-test-dev"
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "operator_version" {
+  description = "Version of the Materialize operator"
+  type        = string
+  default     = "v26.0.0-beta.1"
+}
+
+variable "orchestratord_version" {
+  description = "Version of orchestratord (optional override)"
+  type        = string
+  default     = null
+}
+
+variable "tags" {
+  description = "Tags to apply to all resources"
+  type        = map(string)
+  default = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
+
+variable "force_rollout" {
+  description = "UUID to force a rollout"
+  type        = string
+  default     = "00000000-0000-0000-0000-000000000001"
+}
+
+variable "request_rollout" {
+  description = "UUID to request a rollout"
+  type        = string
+  default     = "00000000-0000-0000-0000-000000000001"
+}
+
+variable "license_key" {
+  description = "Materialize license key"
+  type        = string
+  default     = ""
+  sensitive   = true
+}
+
+variable "environmentd_version" {
+  description = "Materialize environmentd version tag"
+  type        = string
+  default     = "v26.0.0-beta.1"
+}
