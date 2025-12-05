8a96e35b0e38a5210870901571db6fb20ab03c57
Dennis Felsing <dennis@felsing.org>
2025-12-05 22:45:10 +0000

    release process: Set console version whenever we bump
    Not just in manual release cut
    
    Noticed in
    https://github.com/MaterializeInc/materialize/commit/790c7071668d94813d7a3716196f2abcb04a104e#diff-d8516b256903c0a467dadf225f84ffb3023c18412afafdd5db11087cdbbf84e7
    that we still have an old console on main and also our rc.1.
    
    This is not a huge issue because with the current process we do manually
    cut a new .0 version, but still should be fixed!

--- misc/python/materialize/release/cut_release.py
+++ misc/python/materialize/release/cut_release.py
@@ -10,13 +10,8 @@
 """Cut a new release and push the tag to the upstream Materialize repository."""
 
 import argparse
-import os
-import pathlib
 import re
-import shutil
-import subprocess
 import sys
-import time
 
 from semver.version import Version
 
@@ -59,44 +54,6 @@ def main():
     try:
         print(f"Checking out SHA {args.sha}")
         checkout(args.sha)
-        print("Cloning console repo")
-        console_dir = MZ_ROOT / "console"
-        if os.path.exists(console_dir):
-            shutil.rmtree(console_dir)
-        try:
-            spawn.runv(
-                [
-                    "git",
-                    "clone",
-                    "https://github.com/MaterializeInc/console",
-                    console_dir,
-                ],
-                env={**os.environ, "GIT_TERMINAL_PROMPT": "0"},
-            )
-        except subprocess.CalledProcessError:
-            spawn.runv(
-                ["git", "clone", "git@github.com:MaterializeInc/console", console_dir],
-                env={**os.environ, "GIT_TERMINAL_PROMPT": "0"},
-            )
-
-        print(f"Bumping console version to {version}")
-        spawn.runv(["git", "tag", "-a", version, "-m", version], cwd=console_dir)
-        spawn.runv(["git", "push", "origin", version], cwd=console_dir)
-
-        print("Waiting for console version to be released on DockerHub (~15 min)")
-        console_version = version[1:]
-        console_image = f"materialize/console:{console_version}"
-        time.sleep(15 * 60)
-        while True:
-            try:
-                spawn.runv(["docker", "manifest", "inspect", console_image])
-            except subprocess.CalledProcessError:
-                print(f"{console_image} not yet on DockerHub, sleeping 1 min")
-                time.sleep(60)
-                continue
-            break
-        print(f"{console_image} found on DockerHub")
-
         print(f"Bumping version to {version}")
         spawn.runv(
             [
@@ -108,24 +65,6 @@ def main():
                 "--no-commit",
                 "--sbom",
             ]
-        )
-        dockerfile = pathlib.Path("misc/images/materialized-base/Dockerfile")
-        dockerfile_text = dockerfile.read_text()
-        dockerfile.write_text(
-            re.sub(
-                r"FROM materialize/console:.*? AS console",
-                f"FROM {console_image} AS console",
-                dockerfile_text,
-            )
-        )
-        deployment = pathlib.Path("misc/helm-charts/operator/templates/deployment.yaml")
-        deployment_text = deployment.read_text()
-        deployment.write_text(
-            re.sub(
-                r'"--console-image-tag-default=[^"]*"',
-                f'"--console-image-tag-default={console_version}"',
-                deployment_text,
-            )
         )
         # Commit here instead of in bump-version so we have access to the correct git author
         spawn.runv(["git", "commit", "-am", f"release: bump to version {version}"])
