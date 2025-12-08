c5c9b19fec3ddac49911ddaec91cfab4c081c573
Moritz Hoffmann <antiguru@gmail.com>
2025-12-08 11:53:44 +0800

    Introduce ExcludeNull type (#34379)
    The ExcludeNull type wraps another datum type but filters `NULL` values.
    This allows us to wrap `Datum` to only handle non-null values in
    functions that otherwise need to implement the propagates-nulls logic
    internally. In turn, it us to clean up some code!
    
    ---------
    
    Signed-off-by: Moritz Hoffmann <antiguru@gmail.com>

--- src/repr/src/lib.rs
+++ src/repr/src/lib.rs
@@ -65,8 +65,8 @@ pub use crate::row::{
     datum_list_size, datum_size, datums_size, read_datum, row_size,
 };
 pub use crate::scalar::{
-    ArrayRustType, AsColumnType, Datum, DatumType, PropArray, PropDatum, PropDict, PropList,
-    ProtoScalarType, ReprScalarBaseType, ReprScalarType, SqlScalarBaseType, SqlScalarType,
-    arb_datum, arb_datum_for_column, arb_datum_for_scalar, arb_range_type,
+    ArrayRustType, AsColumnType, Datum, DatumType, ExcludeNull, PropArray, PropDatum, PropDict,
+    PropList, ProtoScalarType, ReprScalarBaseType, ReprScalarType, SqlScalarBaseType,
+    SqlScalarType, arb_datum, arb_datum_for_column, arb_datum_for_scalar, arb_range_type,
 };
 pub use crate::timestamp::{Timestamp, TimestampManipulation};
