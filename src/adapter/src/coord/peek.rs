a099da22b9749e22b266659005a4cd404c191cc3
Moritz Hoffmann <antiguru@gmail.com>
2025-12-09 11:37:33 +0800

    Avoid a clone in fast-path peeks
    Avoids an unnecessary clone of constant rows in the fast-path peek. No
    behavior changes expected. We're receiving rows as the correct type, so
    we don't need to map/collect the vector again.
    
    Signed-off-by: Moritz Hoffmann <antiguru@gmail.com>

--- src/adapter/src/coord/peek.rs
+++ src/adapter/src/coord/peek.rs
@@ -446,11 +446,8 @@ pub fn create_fast_path_plan<T: Timestamp>(
         let mut mir = &*dataflow_plan.objects_to_build[0].plan.as_inner_mut();
         if let Some((rows, found_typ)) = mir.as_const() {
             // In the case of a constant, we can return the result now.
-            return Ok(Some(FastPathPlan::Constant(
-                rows.clone()
-                    .map(|rows| rows.into_iter().map(|(row, diff)| (row, diff)).collect()),
-                found_typ.clone(),
-            )));
+            let plan = FastPathPlan::Constant(rows.clone(), found_typ.clone());
+            return Ok(Some(plan));
         } else {
             // If there is a TopK that would be completely covered by the finishing, then jump
             // through the TopK.
