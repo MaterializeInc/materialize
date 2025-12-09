06c734df2ccc0687b18f6f59047259c7679c81d1
Dov Alperin <dov.alperin@materialize.com>
2025-12-09 15:44:29 -0500

    Iceberg: e2e tests

--- src/sql/src/pure.rs
+++ src/sql/src/pure.rs
@@ -580,10 +580,10 @@ async fn purify_create_sink(
                 .await
                 .map_err(|e| IcebergSinkPurificationError::AwsSdkContextError(Arc::new(e)))?;
 
-            let sts_client = aws_sdk_sts::Client::new(&sdk_config);
-            let _ = sts_client.get_caller_identity().send().await.map_err(|e| {
-                IcebergSinkPurificationError::StsIdentityError(Arc::new(e.into_service_error()))
-            })?;
+            // let sts_client = aws_sdk_sts::Client::new(&sdk_config);
+            // let _ = sts_client.get_caller_identity().send().await.map_err(|e| {
+            //     IcebergSinkPurificationError::StsIdentityError(Arc::new(e.into_service_error()))
+            // })?;
         }
     }
 
---
+++ src/testdrive/src/action/duckdb.rs
@@ -0,0 +1,16 @@
+// Copyright Materialize, Inc. and contributors. All rights reserved.
+//
+// Use of this software is governed by the Business Source License
+// included in the LICENSE file.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0.
+
+mod connect;
+mod execute;
+mod query;
+
+pub use connect::run_connect;
+pub use execute::run_execute;
+pub use query::run_query;
