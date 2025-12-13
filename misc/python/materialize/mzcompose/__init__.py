502d23293ea005ef0b48b789aed3d831af5e32c6
Gabor Gevay <gabor@materialize.com>
2025-12-13 13:52:26 +0100

    DNM  Temporarily turn off statement logging in CI

--- misc/python/materialize/mzcompose/__init__.py
+++ misc/python/materialize/mzcompose/__init__.py
@@ -359,7 +359,7 @@ def get_variable_system_parameters(
         VariableSystemParameter("pg_offset_known_interval", "1s", ["100ms", "1s"]),
         VariableSystemParameter(
             "statement_logging_default_sample_rate",
-            "1.0",
+            "0",
             ["0", "0.01", "0.5", "0.99", "1.0"],
         ),
         VariableSystemParameter(
