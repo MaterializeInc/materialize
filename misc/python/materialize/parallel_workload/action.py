b2f81bf4f9ef5accd708d061d52b4021cb3da187
Michael Greenberg <michael.greenberg@materialize.com>
2025-12-17 16:53:30 -0500

    satisfy linter, rewrite slts

--- misc/python/materialize/parallel_workload/action.py
+++ misc/python/materialize/parallel_workload/action.py
@@ -1422,9 +1422,7 @@ class FlipFlagsAction(Action):
             "true",
             "false",
         ]
-        self.flags_with_values["enable_cast_elimination"] = (
-            BOOLEAN_FLAG_VALUES
-        )
+        self.flags_with_values["enable_cast_elimination"] = BOOLEAN_FLAG_VALUES
 
         # If you are adding a new config flag in Materialize, consider using it
         # here instead of just marking it as uninteresting to silence the
