48e6cb050a555affafe7756d7807d7eb47e5e3a8
Gabor Gevay <gabor@materialize.com>
2025-12-17 13:51:45 +0100

    tests: Remove the disabling of frontend peeks in statement logging tests

--- misc/python/materialize/checks/all_checks/statement_logging.py
+++ misc/python/materialize/checks/all_checks/statement_logging.py
@@ -17,13 +17,9 @@ class StatementLogging(Check):
     def initialize(self) -> Testdrive:
         return Testdrive(
             dedent(
-                # TODO(peek-seq): enable_frontend_peek_sequencing when it supports statement logging.
                 """
                 $ postgres-execute connection=postgres://mz_system@${testdrive.materialize-internal-sql-addr}
                 ALTER SYSTEM SET statement_logging_max_sample_rate TO 1.0
-
-                $[version>=2600200] postgres-execute connection=postgres://mz_system@${testdrive.materialize-internal-sql-addr}
-                ALTER SYSTEM SET enable_frontend_peek_sequencing = false;
                 """
             )
         )
