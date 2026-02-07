999ffe85dd94653755528dd8f08e0b2ecf2791e8
Marty Kulma <18468315+martykulma@users.noreply.github.com>
2026-02-06 19:44:53 -0500

    PG: retrieve version num using SELECT current_setting() instead of SHOW

--- src/postgres-util/src/schemas.rs
+++ src/postgres-util/src/schemas.rs
@@ -34,10 +34,12 @@ pub async fn get_schemas(client: &Client) -> Result<Vec<PostgresSchemaDesc>, Pos
 /// Get the major version of the PostgreSQL server.
 pub async fn get_pg_major_version(client: &Client) -> Result<u32, PostgresError> {
     // server_version_num is an integer like 140005 for version 14.5
-    let query = "SHOW server_version_num";
+    // NOTE: We use the statement SELECT instead of SHOW because older Aurora
+    // versions don't support SHOW via a replication channel.
+    let query = "SELECT current_setting('server_version_num')";
     let row = simple_query_opt(client, query).await?;
     let version_num: u32 = row
-        .and_then(|r| r.get("server_version_num").map(|s| s.parse().ok()))
+        .and_then(|r| r.get("current_setting").map(|s| s.parse().ok()))
         .flatten()
         .ok_or_else(|| {
             PostgresError::Generic(anyhow::anyhow!("failed to get PostgreSQL version"))
