72e1690dc250d9bee51b4ee11cf09d4b16614a0f
Ben Kirwin <bkirwi@materialize.com>
2026-02-24 18:46:44 -0500

    Take advantage of the new ordering in the subscribe formatting
    This code previously resorted and reallocated data in ~all cases. We now
    do more or less the minimum for the given subscribe format.

--- src/compute/src/sink/subscribe.rs
+++ src/compute/src/sink/subscribe.rs
@@ -220,7 +220,7 @@ impl SubscribeProtocol {
         if order.is_empty() {
             rows.sort_unstable_by(|(t0, r0, _), (t1, r1, _)| {
                 // NB: sort in reverse, so it's cheaper to peek off the tail.
-                t0.cmp(t1).then(r0.cmp(r1)).reverse()
+                t0.cmp(t1).then_with(|| r0.cmp(r1)).reverse()
             });
         } else {
             let mut left_datum_vec = mz_repr::DatumVec::new();
@@ -228,7 +228,7 @@ impl SubscribeProtocol {
             rows.sort_unstable_by(|(t0, r0, _), (t1, r1, _)| {
                 // NB: sort in reverse, so it's cheaper to peek off the tail.
                 t0.cmp(t1)
-                    .then({
+                    .then_with(|| {
                         let dv0 = left_datum_vec.borrow_with(r0.as_row_ref());
                         let dv1 = right_datum_vec.borrow_with(r1.as_row_ref());
                         compare_columns(order, &dv0, &dv1, || r0.cmp(r1))
