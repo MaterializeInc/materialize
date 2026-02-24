c21fb90a6cf698e0944bba20bae471fc2f1c3466
Dennis Felsing <dennis@felsing.org>
2026-02-24 07:19:11 +0000

    console tests: Fix timeout properly
    Apparently https://github.com/MaterializeInc/materialize/pull/35171
    doesn't work. Seen in https://buildkite.com/materialize/test/builds/116611#019c8b50-fae5-4a58-8af3-38b38eb165fe

--- console/src/api/materialize/query-history/queryHistoryList.test.sql.ts
+++ console/src/api/materialize/query-history/queryHistoryList.test.sql.ts
@@ -110,7 +110,7 @@ function insertActivityLog(
 }
 
 describe("buildQueryHistoryListQuery", () => {
-  it("with isRedacted: false", async () => {
+  it("with isRedacted: false", { timeout: 30_000 }, async () => {
     const strictQueryHistoryListSchema = queryHistoryListSchema.strict();
     await testdrive(`
         > CREATE TABLE recent_activity_log (
@@ -508,7 +508,7 @@ describe("buildQueryHistoryListQuery", () => {
       });
       expect(result.rows.map((r) => r.sql)).toEqual(["SELECT 11", "SELECT 10"]);
     }
-  }, 30_000);
+  });
 
   it("with isRedacted: true", async () => {
     const strictQueryHistoryListSchema = queryHistoryListSchema.strict();
