004b36df23a5b25457df89fc7588c6af0e62a77e
Jan Teske <jteske@posteo.net>
2026-02-25 14:50:39 +0100

    catalog: stop checking the upgrade shard
    The upgrade shard was introduced because opening the catalog for reading
    would automatically bump the version of the catalog shard, possibly
    fencing out existing readers unintentionally. This doesn't happen
    anymore, persist shard versions now must be bumped explicitly. As a
    result, we don't need the upgrade shard anymore and can stop checking
    it.
    
    This still leaves around an initial version check during catalog open
    (using the catalog shard version). This isn't required for correctness
    but ensures that we get a nice error rather than panicking in persist.
    Makes tests easier to write, though it is something we could remove if
    it ends up being inconvenient.

--- src/catalog/src/durable/persist/tests.rs
+++ src/catalog/src/durable/persist/tests.rs
@@ -12,13 +12,13 @@ use mz_persist_client::PersistLocation;
 use mz_persist_client::cache::PersistClientCache;
 use uuid::Uuid;
 
-use crate::durable::persist::{UPGRADE_SEED, fetch_catalog_upgrade_shard_version, shard_id};
+use crate::durable::persist::{CATALOG_SEED, fetch_catalog_shard_version, shard_id};
 use crate::durable::{DurableCatalogError, TestCatalogStateBuilder, test_bootstrap_args};
 
-/// Test that the catalog forces users to upgrade one version at a time.
+/// Test that the catalog forces users to upgrade one major version at a time.
 #[mz_ore::test(tokio::test)]
 #[cfg_attr(miri, ignore)] //  unsupported operation: can't call foreign function `TLS_client_method` on OS `linux`
-async fn test_upgrade_shard() {
+async fn test_version_step() {
     let first_version = semver::Version::parse("0.147.0").expect("failed to parse version");
     let second_version = semver::Version::parse("26.0.0").expect("failed to parse version");
     let second_dev_version =
@@ -27,7 +27,7 @@ async fn test_upgrade_shard() {
     let organization_id = Uuid::new_v4();
     let deploy_generation = 0;
     let mut persist_cache = PersistClientCache::new_no_metrics();
-    let upgrade_shard_id = shard_id(organization_id, UPGRADE_SEED);
+    let catalog_shard_id = shard_id(organization_id, CATALOG_SEED);
 
     persist_cache.cfg.build_version = first_version.clone();
     let persist_client = persist_cache
@@ -37,7 +37,7 @@ async fn test_upgrade_shard() {
 
     assert_eq!(
         None,
-        fetch_catalog_upgrade_shard_version(&persist_client, upgrade_shard_id).await
+        fetch_catalog_shard_version(&persist_client, catalog_shard_id).await
     );
 
     let persist_openable_state = TestCatalogStateBuilder::new(persist_client.clone())
@@ -46,16 +46,17 @@ async fn test_upgrade_shard() {
         .with_version(first_version.clone())
         .expect_build("failed to create persist catalog")
         .await;
-    let _persist_state = persist_openable_state
+    let mut persist_state = persist_openable_state
         .open(SYSTEM_TIME().into(), &test_bootstrap_args())
         .await
         .expect("failed to open persist catalog")
         .0;
+    persist_state.mark_bootstrap_complete().await;
 
     assert_eq!(
         Some(first_version.clone()),
-        fetch_catalog_upgrade_shard_version(&persist_client, upgrade_shard_id).await,
-        "opening the catalog should increment the upgrade version"
+        fetch_catalog_shard_version(&persist_client, catalog_shard_id).await,
+        "writable open + bootstrap should set the catalog shard version"
     );
 
     persist_cache.cfg.build_version = third_version.clone();
@@ -115,8 +116,8 @@ async fn test_upgrade_shard() {
 
     assert_eq!(
         Some(first_version.clone()),
-        fetch_catalog_upgrade_shard_version(&persist_client, upgrade_shard_id).await,
-        "opening a savepoint catalog should not increment the upgrade version"
+        fetch_catalog_shard_version(&persist_client, catalog_shard_id).await,
+        "opening a savepoint catalog should not increment the catalog shard version"
     );
 
     let persist_openable_state = state_builder
@@ -130,84 +131,23 @@ async fn test_upgrade_shard() {
 
     assert_eq!(
         Some(first_version),
-        fetch_catalog_upgrade_shard_version(&persist_client, upgrade_shard_id).await,
-        "opening a readonly catalog should not increment the upgrade version"
+        fetch_catalog_shard_version(&persist_client, catalog_shard_id).await,
+        "opening a readonly catalog should not increment the catalog shard version"
     );
 
     let persist_openable_state = state_builder
         .expect_build("failed to create persist catalog")
         .await;
-    let _persist_state = persist_openable_state
-        .open(SYSTEM_TIME().into(), &test_bootstrap_args())
-        .await
-        .expect("failed to open readonly persist catalog")
-        .0;
-
-    assert_eq!(
-        Some(second_version),
-        fetch_catalog_upgrade_shard_version(&persist_client, upgrade_shard_id).await,
-        "opening the catalog should increment the upgrade version"
-    );
-}
-
-/// Test that the catalog cannot be opened with old versions after an upgrade.
-#[mz_ore::test(tokio::test)]
-#[cfg_attr(miri, ignore)] //  unsupported operation: can't call foreign function `TLS_client_method` on OS `linux`
-async fn test_version_regression() {
-    let first_version = semver::Version::parse("0.147.0").expect("failed to parse version");
-    let second_version = semver::Version::parse("26.1.0").expect("failed to parse version");
-    let organization_id = Uuid::new_v4();
-    let deploy_generation = 0;
-    let mut persist_cache = PersistClientCache::new_no_metrics();
-    let upgrade_shard_id = shard_id(organization_id, UPGRADE_SEED);
-
-    persist_cache.cfg.build_version = first_version.clone();
-    let persist_client = persist_cache
-        .open(PersistLocation::new_in_mem())
-        .await
-        .expect("in-mem location is valid");
-
-    assert_eq!(
-        None,
-        fetch_catalog_upgrade_shard_version(&persist_client, upgrade_shard_id).await
-    );
-
-    let persist_openable_state = TestCatalogStateBuilder::new(persist_client.clone())
-        .with_organization_id(organization_id)
-        .with_deploy_generation(deploy_generation)
-        .with_version(first_version.clone())
-        .expect_build("failed to create persist catalog")
-        .await;
-    let _persist_state = persist_openable_state
+    let mut persist_state = persist_openable_state
         .open(SYSTEM_TIME().into(), &test_bootstrap_args())
         .await
         .expect("failed to open persist catalog")
         .0;
+    persist_state.mark_bootstrap_complete().await;
 
     assert_eq!(
-        Some(first_version.clone()),
-        fetch_catalog_upgrade_shard_version(&persist_client, upgrade_shard_id).await
-    );
-
-    persist_cache.cfg.build_version = second_version.clone();
-    let persist_client = persist_cache
-        .open(PersistLocation::new_in_mem())
-        .await
-        .expect("in-mem location is valid");
-    let persist_openable_state = TestCatalogStateBuilder::new(persist_client.clone())
-        .with_organization_id(organization_id)
-        .with_deploy_generation(deploy_generation)
-        .with_version(second_version.clone())
-        .expect_build("failed to create persist catalog")
-        .await;
-    let _persist_state = persist_openable_state
-        .open(SYSTEM_TIME().into(), &test_bootstrap_args())
-        .await
-        .expect("failed to open readonly persist catalog")
-        .0;
-
-    assert_eq!(
-        Some(second_version.clone()),
-        fetch_catalog_upgrade_shard_version(&persist_client, upgrade_shard_id).await
+        Some(second_version),
+        fetch_catalog_shard_version(&persist_client, catalog_shard_id).await,
+        "writable open + bootstrap should increment the catalog shard version"
     );
 }
