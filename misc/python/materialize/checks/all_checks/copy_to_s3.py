417c033794935bb65fbbbcb5fedfff3a61def1f0
Patrick Butler <patrick.butler@materialize.com>
2026-03-03 09:53:44 -0500

    add roundtrip to copy to tests

--- misc/python/materialize/checks/all_checks/copy_to_s3.py
+++ misc/python/materialize/checks/all_checks/copy_to_s3.py
@@ -23,6 +23,8 @@ class CopyToS3(Check):
                 > CREATE SECRET minio AS '${arg.aws-secret-access-key}'
                 > CREATE CONNECTION aws_conn1 TO AWS (ENDPOINT '${arg.aws-endpoint}', REGION 'us-east-1', ACCESS KEY ID '${arg.aws-access-key-id}', SECRET ACCESS KEY SECRET minio)
                 > COPY (SELECT 1, 2, 3) TO 's3://copytos3/key1' WITH (AWS CONNECTION = aws_conn1, FORMAT = 'csv');
+                > CREATE TABLE t1 (a INT, b INT, c INT);
+                > COPY INTO t1 FROM 's3://copytos3/key1' (FORMAT CSV, AWS CONNECTION = aws_conn1);
                 """
             )
         )
