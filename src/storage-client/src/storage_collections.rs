cc0ef6f659271738045beda01d52f09fd642080e
Jan Teske <jteske@posteo.net>
2026-03-06 13:32:23 +0100

    storage_client: merge DowngradeSince requests
    In the `StorageCollections` background task, merge `DowngradeSince`
    requests. If multiple downgrade requestss for the same collection have
    queued up, we can apply a single downgrade to persist, saving network
    I/O and consensus capacity.

--- src/storage-client/src/storage_collections.rs
+++ src/storage-client/src/storage_collections.rs
@@ -12,6 +12,7 @@
 use std::cmp::Reverse;
 use std::collections::{BTreeMap, BTreeSet};
 use std::fmt::Debug;
+use std::iter;
 use std::num::NonZeroI64;
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
@@ -2760,68 +2761,87 @@ where
                     }
                 }
                 cmd = self.cmds_rx.recv() => {
-                    let cmd = if let Some(cmd) = cmd {
-                        cmd
-                    } else {
+                    let Some(cmd) = cmd else {
                         // We're done!
                         break;
                     };
 
-                    match cmd {
-                        BackgroundCmd::Register{ id, is_in_txns, write_handle, since_handle } => {
-                            debug!("registering handles for {}", id);
-                            let previous = self.shard_by_id.insert(id, write_handle.shard_id());
-                            if previous.is_some() {
-                                panic!("already registered a WriteHandle for collection {id}");
-                            }
+                    // Drain all commands so we can merge `DowngradeSince` requests. Without this
+                    // optimization, downgrading sinces could fall behind in the face of a large
+                    // amount of storage collections.
+                    let commands = iter::once(cmd).chain(
+                        iter::from_fn(|| self.cmds_rx.try_recv().ok())
+                    );
+                    let mut downgrades = BTreeMap::<_, Antichain<_>>::new();
+                    for cmd in commands {
+                        match cmd {
+                            BackgroundCmd::Register{
+                                id,
+                                is_in_txns,
+                                write_handle,
+                                since_handle
+                            } => {
+                                debug!("registering handles for {}", id);
+                                let previous = self.shard_by_id.insert(id, write_handle.shard_id());
+                                if previous.is_some() {
+                                    panic!("already registered a WriteHandle for collection {id}");
+                                }
 
-                            let previous = self.since_handles.insert(id, since_handle);
-                            if previous.is_some() {
-                                panic!("already registered a SinceHandle for collection {id}");
-                            }
+                                let previous = self.since_handles.insert(id, since_handle);
+                                if previous.is_some() {
+                                    panic!("already registered a SinceHandle for collection {id}");
+                                }
 
-                            if is_in_txns {
-                                self.txns_shards.insert(id);
-                            } else {
-                                let upper = write_handle.upper().clone();
-                                if !upper.is_empty() {
-                                    let fut = gen_upper_future(id, write_handle, upper);
-                                    upper_futures.push(fut.boxed());
+                                if is_in_txns {
+                                    self.txns_shards.insert(id);
+                                } else {
+                                    let upper = write_handle.upper().clone();
+                                    if !upper.is_empty() {
+                                        let fut = gen_upper_future(id, write_handle, upper);
+                                        upper_futures.push(fut.boxed());
+                                    }
                                 }
                             }
-
-                        }
-                        BackgroundCmd::DowngradeSince(cmds) => {
-                            self.downgrade_sinces(cmds).await;
-                        }
-                        BackgroundCmd::SnapshotStats(id, as_of, tx) => {
-                            // NB: The requested as_of could be arbitrarily far
-                            // in the future. So, in order to avoid blocking
-                            // this loop until it's available and the
-                            // `snapshot_stats` call resolves, instead return
-                            // the future to the caller and await it there.
-                            let res = match self.since_handles.get(&id) {
-                                Some(x) => {
-                                    let fut: BoxFuture<
-                                        'static,
-                                        Result<SnapshotStats, StorageError<T>>,
-                                    > = match as_of {
-                                        SnapshotStatsAsOf::Direct(as_of) => {
-                                            x.snapshot_stats(id, Some(as_of))
-                                        }
-                                        SnapshotStatsAsOf::Txns(data_snapshot) => {
-                                            x.snapshot_stats_from_txn(id, data_snapshot)
-                                        }
-                                    };
-                                    SnapshotStatsRes(fut)
+                            BackgroundCmd::DowngradeSince(cmds) => {
+                                for (id, new) in cmds {
+                                    downgrades.entry(id)
+                                        .and_modify(|since| since.join_assign(&new))
+                                        .or_insert(new);
                                 }
-                                None => SnapshotStatsRes(Box::pin(futures::future::ready(Err(
-                                    StorageError::IdentifierMissing(id),
-                                )))),
-                            };
-                            // It's fine if the listener hung up.
-                            let _ = tx.send(res);
+                            }
+                            BackgroundCmd::SnapshotStats(id, as_of, tx) => {
+                                // NB: The requested as_of could be arbitrarily far
+                                // in the future. So, in order to avoid blocking
+                                // this loop until it's available and the
+                                // `snapshot_stats` call resolves, instead return
+                                // the future to the caller and await it there.
+                                let res = match self.since_handles.get(&id) {
+                                    Some(x) => {
+                                        let fut: BoxFuture<
+                                            'static,
+                                            Result<SnapshotStats, StorageError<T>>,
+                                        > = match as_of {
+                                            SnapshotStatsAsOf::Direct(as_of) => {
+                                                x.snapshot_stats(id, Some(as_of))
+                                            }
+                                            SnapshotStatsAsOf::Txns(data_snapshot) => {
+                                                x.snapshot_stats_from_txn(id, data_snapshot)
+                                            }
+                                        };
+                                        SnapshotStatsRes(fut)
+                                    }
+                                    None => SnapshotStatsRes(Box::pin(futures::future::ready(Err(
+                                        StorageError::IdentifierMissing(id),
+                                    )))),
+                                };
+                                // It's fine if the listener hung up.
+                                let _ = tx.send(res);
+                            }
                         }
+                    }
+
+                    if !downgrades.is_empty() {
+                        self.downgrade_sinces(downgrades).await;
                     }
                 }
                 Some(holds_changes) = self.holds_rx.recv() => {
@@ -2916,7 +2936,7 @@ where
         }
     }
 
-    async fn downgrade_sinces(&mut self, cmds: Vec<(GlobalId, Antichain<T>)>) {
+    async fn downgrade_sinces(&mut self, cmds: BTreeMap<GlobalId, Antichain<T>>) {
         for (id, new_since) in cmds {
             let since_handle = if let Some(c) = self.since_handles.get_mut(&id) {
                 c
