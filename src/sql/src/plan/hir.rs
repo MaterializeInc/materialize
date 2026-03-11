eed00ff86af1d453b1541ea4e5fd0a60452005eb
Dennis Felsing <dennis@felsing.org>
2026-03-11 09:13:09 +0000

    sql: Fix autorouting for expensive functions
    Only top-level expensive functions were considered in the auto-routing
    
    Broken in https://github.com/MaterializeInc/materialize/pull/20994
    
    One line fix, the rest is adding tests that actually made me find this.

--- src/sql/src/plan/hir.rs
+++ src/sql/src/plan/hir.rs
@@ -2391,7 +2391,7 @@ impl HirRelationExpr {
             use HirRelationExpr::*;
             use HirScalarExpr::*;
 
-            self.visit_children(|scalar: &HirScalarExpr| {
+            e.visit_children(|scalar: &HirScalarExpr| {
                 if let Err(_) = scalar.visit_pre(&mut |scalar: &HirScalarExpr| {
                     result |= match scalar {
                         Column(..)
