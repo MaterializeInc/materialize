MaterializeInc/materialize

commit 09152d2db8c663d9e30bfd53015e86538a544990

[explain] new syntax (LIR-based, Postgres like) (#32262)
Introduces new default syntax for `EXPLAIN`, such that now (1) `EXPLAIN`
by default explains LIR plans, which have unambiguous interpretations
(unlike MIR plans), and (2) `EXPLAIN` shows information in a
Postgres-like syntax, and significantly less information than it used to
for `EXPLAIN PHYSICAL PLAN FOR` (i.e., for LIR).

You can still explain MIR plans with the old syntax using `EXPLAIN
OPTIMIZED PLAN FOR`.

You can still explain LIR plans with the old, very verbose syntax using
`EXPLAIN PHYSICAL PLAN AS VERBOSE TEXT FOR`.

Remaining TODOs:

- [x] Update docs for `mz_lir_mapping` to describe new operator names.
- [x] Write docs for new default syntax.
- [x] Write test for new default syntax.
- [x] Write changelog post.
https://github.com/MaterializeInc/www/pull/1457

Some remaining questions should be resolved by follow-up PRs, per conversation with @ggevay.
https://github.com/MaterializeInc/database-issues/issues/9375
https://github.com/MaterializeInc/database-issues/issues/9376
https://github.com/MaterializeInc/database-issues/issues/9377

### Motivation

  * This PR adds a known-desirable feature.
  #31643
  https://github.com/MaterializeInc/database-issues/issues/8889

src/compute-types/src/plan/render_plan.rs
@@ -18,13 +18,13 @@ use mz_expr::{
     CollectionPlan, EvalError, Id, LetRecLimit, LocalId, MapFilterProject, MirScalarExpr, TableFunc,
 };
 use mz_proto::{IntoRustIfSome, ProtoMapEntry, ProtoType, RustType, TryFromProtoError};
-use mz_repr::explain::ExprHumanizer;
+use mz_repr::explain::{CompactScalars, ExprHumanizer};
 use mz_repr::{Diff, GlobalId, Row};
 use proptest::prelude::*;
 use serde::{Deserialize, Serialize};
 
 use crate::plan::join::{DeltaJoinPlan, JoinPlan, LinearJoinPlan};
-use crate::plan::reduce::{BucketedPlan, HierarchicalPlan, KeyValPlan, ReducePlan};
+use crate::plan::reduce::{BucketedPlan, HierarchicalPlan, KeyValPlan, MonotonicPlan, ReducePlan};
 use crate::plan::threshold::ThresholdPlan;
 use crate::plan::top_k::{MonotonicTopKPlan, TopKPlan};
 use crate::plan::{AvailableCollections, GetPlan, LirId, Plan, PlanNode};
@@ -795,8 +795,10 @@ impl<T> Expr<T> {
 ///
 /// Invariant: the [`std::fmt::Display`] instance should produce a single line for a given expr.
 #[derive(Debug)]
-struct RenderPlanExprHumanizer<'a, T> {
+pub struct RenderPlanExprHumanizer<'a, T> {
+    /// The [`Expr`] to be rendered.
     expr: &'a Expr<T>,
+    /// Humanization information.
     humanizer: &'a dyn ExprHumanizer,
 }
 
@@ -810,6 +812,10 @@ impl<'a, T> RenderPlanExprHumanizer<'a, T> {
 }
 
 impl<'a, T> std::fmt::Display for RenderPlanExprHumanizer<'a, T> {
+    // NOTE: This code needs to be kept in sync with `Plan::fmt_default_text`.
+    //
+    // This code determines what you see in `mz_lir_mapping`; that other code
+    // determine what you see when you run `EXPLAIN`.
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         use Expr::*;
 
@@ -818,119 +824,129 @@ impl<'a, T> std::fmt::Display for RenderPlanExprHumanizer<'a, T> {
                 write!(f, "Constant ")?;
 
                 match rows {
-                    Ok(rows) => write!(f, "{} rows", rows.len()),
-                    Err(err) => write!(f, "error ({err})"),
+                    Ok(rows) => write!(f, "({} rows)", rows.len()),
+                    Err(err) => write!(f, "(error: {err})"),
                 }
             }
-            Get { id, keys: _, plan } => {
-                write!(f, "Get::")?;
-
-                match plan {
-                    GetPlan::PassArrangements => write!(f, "PassArrangements")?,
-                    GetPlan::Arrangement(_key, Some(val), _mfp) => write!(
-                        f,
-                        "Arrangement (val={})",
-                        HumanizedExplain::new(false).expr(val, None)
-                    )?,
-                    GetPlan::Arrangement(_key, None, _mfp) => write!(f, "Arrangement")?,
-                    GetPlan::Collection(_mfp) => write!(f, "Collection")?,
+            Get { id, keys, plan } => {
+                let id = match id {
+                    Id::Local(id) => id.to_string(),
+                    Id::Global(id) => self
+                        .humanizer
+                        .humanize_id(*id)
+                        .unwrap_or_else(|| id.to_string()),
                 };
 
-                write!(f, " ")?;
-
-                match id {
-                    Id::Local(id) => write!(f, "{id}"),
-                    Id::Global(id) => {
-                        if let Some(id) = self.humanizer.humanize_id(*id) {
-                            write!(f, "{id}")
+                match plan {
+                    GetPlan::PassArrangements => {
+                        if keys.raw && keys.arranged.is_empty() {
+                            write!(f, "Stream {id}")
                         } else {
-                            write!(f, "{id}")
+                            write!(f, "Arranged {id}")
                         }
                     }
+                    GetPlan::Arrangement(_key, Some(val), _mfp) => write!(
+                        f,
+                        "Index Lookup on {id} (val={})",
+                        HumanizedExplain::new(false).expr(val, None)
+                    ),
+                    GetPlan::Arrangement(_key, None, _mfp) => write!(f, "Arranged {id}"),
+                    GetPlan::Collection(_mfp) => write!(f, "Read {id}"),
                 }
             }
             Mfp {
-                input,
+                input: _,
                 mfp: _,
                 input_key_val: _,
             } => {
                 // TODO(mgree) show MFP detail
-                write!(f, "MapFilterProject {input}")
+                write!(f, "Map/Filter/Project")
             }
             FlatMap {
-                input,
+                input: _,
                 func,
-                exprs: _,
+                exprs,
                 mfp_after: _,
                 input_key: _,
             } => {
-                // TODO(mgree) show FlatMap detail
-                write!(f, "FlatMap {input} ({func})")
+                let mode = HumanizedExplain::new(false);
+                let exprs = CompactScalars(mode.seq(exprs, None));
+
+                write!(f, "Table Function {func}({exprs})")
             }
-            Join { inputs, plan } => match plan {
+            Join { inputs: _, plan } => match plan {
                 JoinPlan::Linear(LinearJoinPlan {
                     source_relation,
                     stage_plans,
                     ..
                 }) => {
-                    write!(f, "Join::Differential ")?;
+                    write!(f, "Differential Join ")?;
 
-                    write!(f, "{}", inputs[*source_relation])?;
+                    write!(f, "%{}", source_relation)?;
                     for dsp in stage_plans {
-                        write!(f, " » {}", inputs[dsp.lookup_relation])?;
+                        write!(f, " » %{}", dsp.lookup_relation)?;
                     }
 
                     Ok(())
                 }
                 JoinPlan::Delta(DeltaJoinPlan { path_plans }) => {
-                    write!(f, "Join::Delta")?;
+                    write!(f, "Delta Join ")?;
 
+                    let mut first = true;
                     for dpp in path_plans {
-                        write!(f, "[{}", inputs[dpp.source_relation])?;
+                        if !first {
+                            write!(f, " ")?;
+                            first = false;
+                        }
+                        write!(f, "[%{}", dpp.source_relation)?;
 
                         for dsp in &dpp.stage_plans {
-                            write!(f, " » {}", inputs[dsp.lookup_relation])?;
+                            write!(f, " » %{}", dsp.lookup_relation)?;
                         }
-                        write!(f, "] ")?;
+                        write!(f, "]")?;
                     }
 
                     Ok(())
                 }
             },
             Reduce {
-                input,
+                input: _,
                 key_val_plan: _key_val_plan,
                 plan,
                 input_key: _input_key,
                 mfp_after: _mfp_after,
-            } => {
-                write!(f, "Reduce::")?;
-
-                match plan {
-                    ReducePlan::Distinct => write!(f, "Distinct {input}"),
-                    ReducePlan::Accumulable(..) => write!(f, "Accumulable {input}"),
-                    ReducePlan::Hierarchical(HierarchicalPlan::Monotonic(..)) => {
-                        write!(f, "Hierarchical {input} (monotonic)")
+            } => match plan {
+                ReducePlan::Distinct => write!(f, "Distinct GroupAggregate"),
+                ReducePlan::Accumulable(..) => write!(f, "Accumulable GroupAggregate"),
+                ReducePlan::Hierarchical(HierarchicalPlan::Monotonic(MonotonicPlan {
+                    must_consolidate,
+                    ..
+                })) => {
+                    if *must_consolidate {
+                        write!(f, "Consolidating ")?;
                     }
-                    ReducePlan::Hierarchical(HierarchicalPlan::Bucketed(BucketedPlan {
-                        buckets,
-                        ..
-                    })) => {
-                        write!(f, "Hierarchical {input} (buckets:")?;
-
-                        for bucket in buckets {
-                            write!(f, " {bucket}")?;
-                        }
-                        write!(f, ")")
-                    }
-                    ReducePlan::Basic(..) => write!(f, "Basic"),
-                    ReducePlan::Collation(..) => write!(f, "Collation"),
+                    write!(f, "Monotonic GroupAggregate")
                 }
-            }
-            TopK { input, top_k_plan } => {
-                write!(f, "TopK::")?;
+                ReducePlan::Hierarchical(HierarchicalPlan::Bucketed(BucketedPlan {
+                    buckets,
+                    ..
+                })) => {
+                    write!(f, "Bucketed Hierarchical GroupAggregate (buckets:")?;
+
+                    for bucket in buckets {
+                        write!(f, " {bucket}")?;
+                    }
+                    write!(f, ")")
+                }
+                ReducePlan::Basic(..) => write!(f, "Non-incremental GroupAggregate`"),
+                ReducePlan::Collation(..) => write!(f, "Collated Multi-GroupAggregate"),
+            },
+            TopK {
+                input: _,
+                top_k_plan,
+            } => {
                 match top_k_plan {
-                    TopKPlan::MonotonicTop1(..) => write!(f, "MonotonicTop1")?,
+                    TopKPlan::MonotonicTop1(..) => write!(f, "Monotonic Top1")?,
                     TopKPlan::MonotonicTopK(MonotonicTopKPlan {
                         limit: Some(limit), ..
                     }) => write!(
@@ -939,39 +955,42 @@ impl<'a, T> std::fmt::Display for RenderPlanExprHumanizer<'a, T> {
                         HumanizedExplain::new(false).expr(limit, None)
                     )?,
                     TopKPlan::MonotonicTopK(MonotonicTopKPlan { .. }) => {
-                        write!(f, "MonotonicTopK")?
+                        write!(f, "Monotonic TopK")?
                     }
-                    TopKPlan::Basic(..) => write!(f, "Basic")?,
+                    TopKPlan::Basic(..) => write!(f, "Non-monotonic TopK")?,
                 };
-                write!(f, " {input}")
+
+                Ok(())
             }
-            Negate { input } => write!(f, "Negate {input}"),
+            Negate { input: _ } => write!(f, "Negate Diffs"),
             Threshold {
-                input,
+                input: _,
                 threshold_plan: _,
-            } => write!(f, "Threshold {input}"),
+            } => write!(f, "Threshold Diffs"),
             Union {
-                inputs,
+                inputs: _,
                 consolidate_output,
             } => {
-                write!(f, "Union")?;
-
-                for input in inputs {
-                    write!(f, " {input}")?;
-                }
-
                 if *consolidate_output {
-                    write!(f, " (consolidates output)")?;
+                    write!(f, "Consolidating ")?;
                 }
+
+                write!(f, "Union")?;
 
                 Ok(())
             }
             ArrangeBy {
-                input,
-                forms: _,
+                input: _,
+                forms,
                 input_key: _,
                 input_mfp: _,
-            } => write!(f, "Arrange {input}"),
+            } => {
+                if forms.raw && forms.arranged.is_empty() {
+                    write!(f, "Unarranged Raw Stream")
+                } else {
+                    write!(f, "Arrange")
+                }
+            }
         }
     }
 }

doc/user/data/explain_plan_operators.yml
@@ -21,11 +21,7 @@ operators:
     uses_memory: False
     memory_details: ""
     expansive: False
-
-    example: |
-      ```mzsql
-      Constant 2 rows
-      ```
+    example: "`→Constant (2 rows)`"
 
   - operator: Get
     plan_types: "optimized,raw"
@@ -38,28 +34,30 @@ operators:
 
     example: "`Get materialize.public.ordered`"
 
-  - operator: Get::~
+  - operator: Stream, Arranged, Index Lookup, Read
     plan_types: "LIR"
     description: |
       Produces rows from either an existing relation (source/view/materialized view/table) or from a previous
       CTE in the same plan.
-      There may be a `MapFilterProject` included in the lookup.
+      A parent `Fused Map/Filter/Project` operator can combine with this operator.
 
-      There are three types of `Get`.
+      There are four types of `Get`.
 
-      1. `Get::PassArrangements`, which means the plan will use an
-         existing [arrangement](/get-started/arrangements/#arrangements).
+      1. `Stream` indicates that the results are not [arranged](/get-started/arrangements/#arrangements) in memory
+         and will be streamed directly.
 
-      2. `Get::Arrangement`, which means that the results will be
-         _looked up_ in an existing arrangement.
+      2. `Arranged` indicates that the results are [arranged](/get-started/arrangements/#arrangements) in memory.
 
-      3. `Get::Collection`, which means that the results are
-         unarranged, and will be processed as they arrive.
+      2. `Index Lookup` indicates the results will be
+         _looked up_ in an existing [arrangement]((/get-started/arrangements/#arrangements).
+
+      3. `Read` indicates that the results are unarranged,
+          and will be processed as they arrive.
 
     uses_memory: False
     memory_details: ""
     expansive: False
-    example: "`Get::PassArrangements materialize.public.ordered`"
+    example: "`Arranged materialize.public.t`"
 
   - operator: Project
     plan_types: "optimized,raw"
@@ -84,12 +82,13 @@ operators:
       Each row has more data (i.e., longer rows but same number of rows).
     example: "`Map (((#1 * 10000000dec) / #2) * 1000dec)`"
 
-  - operator: MapFilterProject
+  - operator: Map/Filter/Project
     plan_types: "LIR"
     description: |
-      The number after the operator is the input operator's `lir_id`.
+      Computes new columns (maps), filters columns, and projects away columns. Works row-by-row.
+      Maps and filters will be printed, but projects will not.
 
-      Computes new columns, filters columns, and projects away columns. Works row-by-row.
+      These may be marked as **`Fused`** `Map/Filter/Project`, which means they will combine with the operator beneath them to run more efficiently.
     uses_memory: False
     memory_details: ""
     expansive: True
@@ -98,7 +97,12 @@ operators:
       Each row may also have less data, from the `Project`.
       There may be fewer rows, from the `Filter`.
 
-    example: "`MapFilterProject 5`"
+    example: |
+      ```mzsql
+      →Map/Filter/Project
+        Filter: (#0{a} < 7)
+        Map: (#0{a} + #1{b})
+      ```
 
   - operator: FlatMap
     plan_types: "optimized"
@@ -111,18 +115,25 @@ operators:
       Depends on the [table function](/sql/functions/#table-functions) used.
     example: "`FlatMap jsonb_foreach(#3)`"
 
-  - operator: FlatMap
+  - operator: Table Function
     plan_types: "LIR"
     description: |
-      The number after the operator is the input operator's `lir_id`.
-
       Appends the result of some (one-to-many) [table function](/sql/functions/#table-functions) to each row in the input.
+
+      A parent `Fused Table Function unnest_list` operator will fuse with its child `GroupAggregate` operator. Fusing these operator is part of how we efficiently compile window functions from SQL to dataflows.
+
+      A parent `Fused Map/Filter/Project` can combine with this operator.
+
     uses_memory: False
     memory_details: ""
     expansive: True
     expansive_details: |
       Depends on the [table function](/sql/functions/#table-functions) used.
-    example: "`FlatMap 3 (jsonb_foreach)`"
+    example: |
+      ```mzsql
+      →Table Function generate_series(#0{a}, #1{b}, 1)
+        Input key: (#0{a})
+      ```
 
   - operator: CallTable
     plan_types: "raw"
@@ -173,22 +184,28 @@ operators:
       Depends on the join order and facts about the joined collections.
     example: "`Join on=(#1 = #2) type=delta`"
 
-  - operator: Join::~
+  - operator: Differential Join, Delta Join
     plan_types: "LIR"
     description: |
-      The input operators are listed in the order performed by the join.
+      Both join operators indicate the join ordering selected.
 
       Returns combinations of rows from each input whenever some equality predicates are `true`.
 
-      There are two types of `Join`: `Join::Differential` (also called linear join) and `Join::Delta`, with [documented differences](/transform-data/optimization/#join).
+      Joins will indicate the join order of their children, starting from 0.
+      For example, `Differential Join %1 » %0` will join its second child into its first.
+
+      The [two joins differ in performance characteristics](/transform-data/optimization/#join).
     uses_memory: True
     memory_details: |
       Uses memory for 3-way or more differential joins.
     expansive: True
     expansive_details: |
       Depends on the join order and facts about the joined collections.
-    example: "`Join::Differential 6 » 7`"
-
+    example: |
+      ```mzsql
+      →Differential Join %1 » %0
+        Join stage %0: Lookup key #0{a} in %0
+      ```
 
   - operator: CrossJoin
     plan_types: "optimized"
@@ -216,34 +233,43 @@ operators:
     expansive: False
     example: "`Reduce group_by=[#0] aggregates=[max((#0 * #1))]`"
 
-  - operator: Reduce::~
+  - operator: GroupAggregate
     plan_types: "LIR"
     description: |
-      The number after the operator is the input operator's `lir_id`.
-
       Groups the input rows by some scalar expressions, reduces each group using some aggregate functions, and produces rows containing the group key and aggregate outputs.
 
-      There are five types of `Reduce`, ordered by increasing complexity:
+      There are five types of `GroupAggregate`, ordered by increasing complexity:
 
-      1. `Reduce::Distinct` corresponds to the SQL `DISTINCT` operator.
+      1. `Distinct GroupAggregate` corresponds to the SQL `DISTINCT` operator.
 
-      2. `Reduce::Accumulable` (e.g., `SUM`, `COUNT`) corresponds to several easy to implement aggregations that can be executed simultaneously and efficiently.
+      2. `Accumulable GroupAggregate` (e.g., `SUM`, `COUNT`) corresponds to several easy to implement aggregations that can be executed simultaneously and efficiently.
 
-      3. `Reduce::Hierarchical` (e.g., `MIN`, `MAX`) corresponds to an aggregation requiring a tower of arrangements. These can be either monotonic (more efficient) or bucketed. These may benefit from a hint; [see `mz_introspection.mz_expected_group_size_advice`](/sql/system-catalog/mz_introspection/#mz_expected_group_size_advice).
+      3. `Hierarchical GroupAggregate` (e.g., `MIN`, `MAX`) corresponds to an aggregation requiring a tower of arrangements. These can be either monotonic (more efficient) or bucketed. These may benefit from a hint; [see `mz_introspection.mz_expected_group_size_advice`](/sql/system-catalog/mz_introspection/#mz_expected_group_size_advice).
+         These may either be bucketed or monotonic (more efficient).
+         These may consolidate their output, which will increase memory usage.
 
-      4. `Reduce::Collation` corresponds to an arbitrary mix of reductions of different types, which will be performed separately and then joined together.
+      4. `Collated Multi-GroupAggregate` corresponds to an arbitrary mix of reductions of different types, which will be performed separately and then joined together.
 
-      5. `Reduce::Basic` (e.g., window functions, `list_agg`) corresponds to a single non-incremental aggregation.
+      5. `Non-incremental GroupAggregate` (e.g., window functions, `list_agg`) corresponds to a single non-incremental aggregation.
+         These are the most computationally intensive reductions.
+
+      A parent `Fused Map/Filter/Project` can combine with this operator.
 
     uses_memory: True
     memory_details: |
       `Distinct` and `Accumulable` aggregates use a moderate amount of memory (proportional to twice the output size).
       `MIN` and `MAX` aggregates can use significantly more memory. This can be improved by including group size hints in the query, see
       [`mz_introspection.mz_expected_group_size_advice`](/sql/system-catalog/mz_introspection/#mz_expected_group_size_advice).
-      `Basic` aggregates use memory proportional to the input + output size.
-      `Collation` aggregates use memory that is the sum of their constituents, plus some memory for the join at the end.
+      `Non-incremental` aggregates use memory proportional to the input + output size.
+      `Collated` aggregates use memory that is the sum of their constituents, plus some memory for the join at the end.
     expansive: False
-    example: "`Reduce::Accumulable 8`"
+    example: |
+      ```mzsql
+      →Accumulable GroupAggregate
+        Simple aggregates: count(*)
+        Post-process Map/Filter/Project
+          Filter: (#0 > 1)
+      ```
 
   - operator: Reduce
     plan_types: "raw"
@@ -293,26 +319,30 @@ operators:
     expansive: False
     example: "`TopK order_by=[#1 asc nulls_last, #0 desc nulls_first] limit=5`"
 
-  - operator: TopK::~
+  - operator: TopK
     plan_types: "LIR"
     description: |
-      The number after the operator is the input operator's `lir_id`.
-
       Groups the input rows, sorts them according to some ordering, and returns at most `K` rows at some offset from the top of the list, where `K` is some (possibly computed) limit.
 
       There are three types of `TopK`. Two are special cased for monotonic inputs (i.e., inputs which never retract data).
 
-      1. `TopK::MonotonicTop1`.
-      2. `TopK::MonotonicTopK`, which may give an expression indicating the limit.
-      3. `TopK::Basic`, a generic `TopK` plan.
+      1. `Monotonic Top1`.
+      2. `Monotonic TopK`, which may give an expression indicating the limit.
+      3. `Non-monotonic TopK`, a generic `TopK` plan.
+
+      Each version of the `TopK` operator may include grouping, ordering, and limit directives.
     uses_memory: True
     memory_details: |
-      `MonotonicTop1` or `MonotonicTopK` uses a moderate amount of memory. Other TopKs use significantly more memory as the operator can significantly overestimate
+      `Monotonic Top1` and `Monotonic TopK` use a moderate amount of memory. `Non-monotonic TopK` uses significantly more memory as the operator can significantly overestimate
       the group sizes. Consult
       [`mz_introspection.mz_expected_group_size_advice`](/sql/system-catalog/mz_introspection/#mz_expected_group_size_advice).
     expansive: False
-    example: "`TopK::Basic 10`"
-
+    example: |
+      ```mzsql
+      →Consolidating Monotonic TopK
+        Order By #1 asc nulls_last, #0 desc nulls_first
+        Limit 5
+      ```
 
   - operator: Negate
     plan_types: "optimized,raw"
@@ -323,14 +353,14 @@ operators:
     expansive: False
     example: "`Negate`"
 
-  - operator: Negate
+  - operator: Negate Diffs
     plan_types: "LIR"
     description: |
       Negates the row counts of the input. This is usually used in combination with union to remove rows from the other union input.
     uses_memory: False
     memory_details: ""
     expansive: False
-    example: "`Negate 17`"
+    example: "`→Negate Diffs`"
 
   - operator: Threshold
     plan_types: "optimized,raw"
@@ -342,7 +372,7 @@ operators:
     expansive: False
     example: "`Threshold`"
 
-  - operator: Threshold
+  - operator: Threshold Diffs
     plan_types: "LIR"
     description: |
       Removes any rows with negative counts.
@@ -350,7 +380,7 @@ operators:
     memory_details: |
       Uses memory proportional to the input and output size, twice.
     expansive: False
-    example: "`Threshold 47`"
+    example: "`→Threshold Diffs`"
 
   - operator: Union
     plan_types: "optimized,raw"
@@ -368,9 +398,9 @@ operators:
       Combines its inputs into a unified output, emitting one row for each row on any input. (Corresponds to `UNION ALL` rather than `UNION`/`UNION DISTINCT`.)
     uses_memory: True
     memory_details: |
-      If the union "consolidates output", it will make moderate use of memory, particularly at hydration time. If the union is not marked with "consolidates output", it will not consume memory.
+      A `Consolidating Union` will make moderate use of memory, particularly at hydration time. A `Union` that is not `Consolidating` will not consume memory.
     expansive: False
-    example: "`Union 7 10 11 14 (consolidates output)`"
+    example: "`→Consolidating Union`"
 
   - operator: ArrangeBy
     plan_types: "optimized"
@@ -390,10 +420,23 @@ operators:
     memory_details: |
       Uses memory proportional to the input size. Note that in the LIR / physical plan, `Arrange`/`ArrangeBy` almost always means that an arrangement will actually be created. (This is in contrast to the "optimized" plan, where an `ArrangeBy` being present in the plan often does not mean that an arrangement will actually be created.)
     expansive: False
-    example: "`Arrange 12`"
+    example: |
+      ```mzsql
+      →Arrange
+          Keys: 1 arrangement available, plus raw stream
+            Arrangement 0: #0
+      ```
+  - operator: Unarranged Raw Stream
+    plan_types: "LIR"
+    description: |
+      Indicates a point where data will be streamed (even if it is somehow already arranged).
+    uses_memory: False
+    expansive: False
+    example: "`→Unarranged Raw Stream`"
+
 
   - operator: With ... Return ...
-    plan_types: "optimized,raw"
+    plan_types: "optimized,raw,LIR"
     description: |
       Introduces CTEs, i.e., makes it possible for sub-plans to be consumed multiple times by downstream operators.
     uses_memory: False

commit bd9d3e1ab8a4dc6b72e14a195b691d2f8f0cfed1

explain: Fix displaying Delta joins (#35422)
Broken in https://github.com/MaterializeInc/materialize/pull/32262

src/compute-types/src/plan/render_plan.rs
@@ -888,8 +888,8 @@ impl<'a, T> std::fmt::Display for RenderPlanExprHumanizer<'a, T> {
                     for dpp in path_plans {
                         if !first {
                             write!(f, " ")?;
-                            first = false;
                         }
+                        first = false;
                         write!(f, "[%{}", dpp.source_relation)?;
 
                         for dsp in &dpp.stage_plans {

test/testdrive/explain-analyze.td
@@ -93,7 +93,7 @@ operator                     total_elapsed
 "              Read l2" <XXX>
 "  Arrange (#0, #3)" <XXX>
 "    Stream l2" <null>
-"l2 = Delta Join [%0 » %1 » %2 » %3 » %4 » %5][%1 » %0 » %2 » %3 » %4 » %5][%2 » %0 » %1 » %3 » %4 » %5][%3 » %0 » %1 » %2 » %4 » %5][%4 » %0 » %1 » %2 » %3 » %5][%5 » %0 » %1 » %2 » %3 » %4]" <XXX>
+"l2 = Delta Join [%0 » %1 » %2 » %3 » %4 » %5] [%1 » %0 » %2 » %3 » %4 » %5] [%2 » %0 » %1 » %3 » %4 » %5] [%3 » %0 » %1 » %2 » %4 » %5] [%4 » %0 » %1 » %2 » %3 » %5] [%5 » %0 » %1 » %2 » %3 » %4]" <XXX>
 "  Arrange (#0{replica_id})" <XXX>
 "    Arranged l0" <null>
 "  Arrange (#0{replica_id}, #3{bucket_start})" <XXX>
@@ -121,7 +121,7 @@ operator                     total_elapsed
 "    Map/Filter/Project" <XXX>
 "      Accumulable GroupAggregate" <XXX>
 "        Map/Filter/Project" <XXX>
-"          Delta Join [%0 » %2 » %1][%1 » %0 » %2][%2 » %0 » %1]" <XXX>
+"          Delta Join [%0 » %2 » %1] [%1 » %0 » %2] [%2 » %0 » %1]" <XXX>
 "            Arranged <XXX>" <null>
 "            Arranged <XXX>" <null>
 "            Arrange (#0{replica_id}) (#1{size})" <XXX>