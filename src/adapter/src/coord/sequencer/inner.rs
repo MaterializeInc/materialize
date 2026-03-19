a9331a66ed0abcc9069ee3b8f8da321619b77e2a
Dennis Felsing <dennis@felsing.org>
2026-03-19 15:34:09 +0100

    webhooks: Invalidate secret cache on secret changes (#35478)

--- src/adapter/src/coord/sequencer/inner.rs
+++ src/adapter/src/coord/sequencer/inner.rs
@@ -699,6 +699,7 @@ impl Coordinator {
                 if let Err(err) = self.secrets_controller.ensure(connection_id, &secret).await {
                     return ctx.retire(Err(err.into()));
                 }
+                self.caching_secrets_reader.invalidate(connection_id);
             }
             _ => (),
         };
