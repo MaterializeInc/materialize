MaterializeInc/materialize

commit 3b709958e4bce2215d6990b406bf01094f3fde3d

[blue/green] SQL parsing (#21979)
This PR implements SQL parsing for blue/green deployments, specifically
it adds support for:

```
ALTER <object_type> SWAP <name_a> <name_b>
```

In the parser we support `SWAP` for all `ObjectType`s, but in reality
we'll start with only supporting swap for clusters and schemas, also we
may never support swapping something like a Role. Even though we support
parsing it, we will return an error in Planning. I opted for this
approach because it led to less duplicated code in the planner. If folks
would rather have the unsupported swap features rejected in parsing
instead of planning I can make that change!

### Motivation

Progress towards: #12972 

### Checklist

- [ ] This PR has adequate test coverage / QA involvement has been duly
considered.
- [ ] This PR has an associated up-to-date [design
doc](https://github.com/MaterializeInc/materialize/blob/main/doc/developer/design/README.md),
is a design doc
([template](https://github.com/MaterializeInc/materialize/blob/main/doc/developer/design/00000000_template.md)),
or is sufficiently small to not require a design.
  <!-- Reference the design in the description. -->
- [ ] If this PR evolves [an existing `$T ⇔ Proto$T`
mapping](https://github.com/MaterializeInc/materialize/blob/main/doc/developer/command-and-response-binary-encoding.md)
(possibly in a backwards-incompatible way), then it is tagged with a
`T-proto` label.
- [ ] If this PR will require changes to cloud orchestration or tests,
there is a companion cloud PR to account for those changes that is
tagged with the release-blocker label
([example](https://github.com/MaterializeInc/cloud/pull/5021)).
<!-- Ask in #team-cloud on Slack if you need help preparing the cloud
PR. -->
- [x] This PR includes the following [user-facing behavior
changes](https://github.com/MaterializeInc/materialize/blob/main/doc/developer/guide-changes.md#what-changes-require-a-release-note):
  - N/a

src/sql-parser/src/ast/defs/statement.rs
@@ -65,6 +65,7 @@ pub enum Statement<T: AstInfo> {
     AlterCluster(AlterClusterStatement<T>),
     AlterOwner(AlterOwnerStatement<T>),
     AlterObjectRename(AlterObjectRenameStatement),
+    AlterObjectSwap(AlterObjectSwapStatement),
     AlterIndex(AlterIndexStatement<T>),
     AlterSecret(AlterSecretStatement<T>),
     AlterSetCluster(AlterSetClusterStatement<T>),
@@ -132,6 +133,7 @@ impl<T: AstInfo> AstDisplay for Statement<T> {
             Statement::AlterCluster(stmt) => f.write_node(stmt),
             Statement::AlterOwner(stmt) => f.write_node(stmt),
             Statement::AlterObjectRename(stmt) => f.write_node(stmt),
+            Statement::AlterObjectSwap(stmt) => f.write_node(stmt),
             Statement::AlterIndex(stmt) => f.write_node(stmt),
             Statement::AlterSetCluster(stmt) => f.write_node(stmt),
             Statement::AlterSecret(stmt) => f.write_node(stmt),
@@ -201,6 +203,7 @@ pub fn statement_kind_label_value(kind: StatementKind) -> &'static str {
         StatementKind::CreateSecret => "create_secret",
         StatementKind::AlterCluster => "alter_cluster",
         StatementKind::AlterObjectRename => "alter_object_rename",
+        StatementKind::AlterObjectSwap => "alter_object_swap",
         StatementKind::AlterIndex => "alter_index",
         StatementKind::AlterRole => "alter_role",
         StatementKind::AlterSecret => "alter_secret",
@@ -1923,6 +1926,27 @@ impl AstDisplay for AlterObjectRenameStatement {
     }
 }
 impl_display!(AlterObjectRenameStatement);
+
+/// `ALTER <OBJECT> SWAP ...`
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct AlterObjectSwapStatement {
+    pub object_type: ObjectType,
+    pub name_a: UnresolvedObjectName,
+    pub name_b: UnresolvedObjectName,
+}
+
+impl AstDisplay for AlterObjectSwapStatement {
+    fn fmt<W: fmt::Write>(&self, f: &mut AstFormatter<W>) {
+        f.write_str("ALTER ");
+        f.write_node(&self.object_type);
+        f.write_str(" SWAP ");
+
+        f.write_node(&self.name_a);
+        f.write_str(" ");
+        f.write_node(&self.name_b);
+    }
+}
+impl_display!(AlterObjectSwapStatement);
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub enum AlterIndexAction<T: AstInfo> {

src/sql-parser/src/parser.rs
@@ -4098,6 +4098,13 @@ impl<'a> Parser<'a> {
     fn parse_alter_object(&mut self) -> Result<Statement<Raw>, ParserStatementError> {
         let object_type = self.expect_object_type().map_no_statement_parser_err()?;
 
+        // ALTER ... SWAP works the same for all items, hence this separate branch.
+        if self.parse_keyword(SWAP) {
+            return self
+                .parse_alter_swap(object_type)
+                .map_parser_err(StatementKind::AlterObjectSwap);
+        }
+
         match object_type {
             ObjectType::Role => self
                 .parse_alter_role()
@@ -4868,6 +4875,85 @@ impl<'a> Parser<'a> {
                 }))
             }
             _ => unreachable!(),
+        }
+    }
+
+    fn parse_alter_swap(&mut self, object_type: ObjectType) -> Result<Statement<Raw>, ParserError> {
+        match object_type {
+            ObjectType::Table
+            | ObjectType::View
+            | ObjectType::MaterializedView
+            | ObjectType::Source
+            | ObjectType::Sink
+            | ObjectType::Index
+            | ObjectType::Type
+            | ObjectType::Secret
+            | ObjectType::Connection
+            | ObjectType::Func => {
+                let name_a = self.parse_item_name()?;
+                let name_b = self.parse_item_name()?;
+
+                Ok(Statement::AlterObjectSwap(AlterObjectSwapStatement {
+                    object_type,
+                    name_a: UnresolvedObjectName::Item(name_a),
+                    name_b: UnresolvedObjectName::Item(name_b),
+                }))
+            }
+            ObjectType::Cluster => {
+                let name_a = self.parse_identifier()?;
+                let name_b = self.parse_identifier()?;
+
+                Ok(Statement::AlterObjectSwap(AlterObjectSwapStatement {
+                    object_type,
+                    name_a: UnresolvedObjectName::Cluster(name_a),
+                    name_b: UnresolvedObjectName::Cluster(name_b),
+                }))
+            }
+            ObjectType::ClusterReplica => {
+                let name_a = self.parse_cluster_replica_name()?;
+                let name_b = self.parse_cluster_replica_name()?;
+
+                Ok(Statement::AlterObjectSwap(AlterObjectSwapStatement {
+                    object_type,
+                    name_a: UnresolvedObjectName::ClusterReplica(name_a),
+                    name_b: UnresolvedObjectName::ClusterReplica(name_b),
+                }))
+            }
+            ObjectType::Database => {
+                let name_a = self.parse_database_name()?;
+                let name_b = self.parse_database_name()?;
+
+                Ok(Statement::AlterObjectSwap(AlterObjectSwapStatement {
+                    object_type,
+                    name_a: UnresolvedObjectName::Database(name_a),
+                    name_b: UnresolvedObjectName::Database(name_b),
+                }))
+            }
+            ObjectType::Schema => {
+                let name_a = self.parse_schema_name()?;
+                let name_b = self.parse_schema_name()?;
+
+                Ok(Statement::AlterObjectSwap(AlterObjectSwapStatement {
+                    object_type,
+                    name_a: UnresolvedObjectName::Schema(name_a),
+                    name_b: UnresolvedObjectName::Schema(name_b),
+                }))
+            }
+            ObjectType::Role => {
+                let name_a = self.parse_identifier()?;
+                let name_b = self.parse_identifier()?;
+
+                Ok(Statement::AlterObjectSwap(AlterObjectSwapStatement {
+                    object_type,
+                    name_a: UnresolvedObjectName::Role(name_a),
+                    name_b: UnresolvedObjectName::Role(name_b),
+                }))
+            }
+            ObjectType::Subsource => parser_err!(
+                self,
+                self.peek_prev_pos(),
+                "Unexpected object type 'SUBSOURCE'"
+            ),
         }
     }
 

src/sql/src/plan/statement/ddl.rs
@@ -65,14 +65,15 @@ use prost::Message;
 use crate::ast::display::AstDisplay;
 use crate::ast::{
     AlterConnectionStatement, AlterIndexAction, AlterIndexStatement, AlterObjectRenameStatement,
-    AlterSecretStatement, AvroSchema, AvroSchemaOption, AvroSchemaOptionName, AwsConnectionOption,
-    AwsConnectionOptionName, AwsPrivatelinkConnectionOption, AwsPrivatelinkConnectionOptionName,
-    ClusterOption, ClusterOptionName, ColumnOption, CreateClusterReplicaStatement,
-    CreateClusterStatement, CreateConnection, CreateConnectionStatement, CreateDatabaseStatement,
-    CreateIndexStatement, CreateMaterializedViewStatement, CreateRoleStatement,
-    CreateSchemaStatement, CreateSecretStatement, CreateSinkConnection, CreateSinkOption,
-    CreateSinkOptionName, CreateSinkStatement, CreateSourceConnection, CreateSourceFormat,
-    CreateSourceOption, CreateSourceOptionName, CreateSourceStatement, CreateSubsourceOption,
+    AlterObjectSwapStatement, AlterSecretStatement, AvroSchema, AvroSchemaOption,
+    AvroSchemaOptionName, AwsConnectionOption, AwsConnectionOptionName,
+    AwsPrivatelinkConnectionOption, AwsPrivatelinkConnectionOptionName, ClusterOption,
+    ClusterOptionName, ColumnOption, CreateClusterReplicaStatement, CreateClusterStatement,
+    CreateConnection, CreateConnectionStatement, CreateDatabaseStatement, CreateIndexStatement,
+    CreateMaterializedViewStatement, CreateRoleStatement, CreateSchemaStatement,
+    CreateSecretStatement, CreateSinkConnection, CreateSinkOption, CreateSinkOptionName,
+    CreateSinkStatement, CreateSourceConnection, CreateSourceFormat, CreateSourceOption,
+    CreateSourceOptionName, CreateSourceStatement, CreateSubsourceOption,
     CreateSubsourceOptionName, CreateSubsourceStatement, CreateTableStatement, CreateTypeAs,
     CreateTypeStatement, CreateViewStatement, CreateWebhookSourceStatement, CsrConfigOption,
     CsrConfigOptionName, CsrConnection, CsrConnectionAvro, CsrConnectionOption,
@@ -4787,6 +4788,23 @@ pub fn plan_alter_cluster_replica_rename(
             Ok(Plan::AlterNoop(AlterNoopPlan { object_type }))
         }
     }
+}
+
+pub fn describe_alter_object_swap(
+    _: &StatementContext,
+    _: AlterObjectSwapStatement,
+) -> Result<StatementDesc, PlanError> {
+    Ok(StatementDesc::new(None))
+}
+
+pub fn plan_alter_object_swap(
+    _: &mut StatementContext,
+    _: AlterObjectSwapStatement,
+) -> Result<Plan, PlanError> {
+    Err(PlanError::Unsupported {
+        feature: "ALTER ... SWAP ...".to_string(),
+        issue_no: Some(12972),
+    })
 }
 
 pub fn describe_alter_secret_options(

src/sql-parser/tests/testdata/alter
@@ -68,3 +68,122 @@ ALTER ROLE parker RESET cluster
 ALTER ROLE parker RESET cluster
 =>
 AlterRole(AlterRoleStatement { name: Ident("parker"), option: Variable(Reset { name: Ident("cluster") }) })
+
+parse-statement
+ALTER TABLE SWAP cool_table cooler_table
+----
+ALTER TABLE SWAP cool_table cooler_table
+=>
+AlterObjectSwap(AlterObjectSwapStatement { object_type: Table, name_a: Item(UnresolvedItemName([Ident("cool_table")])), name_b: Item(UnresolvedItemName([Ident("cooler_table")])) })
+
+parse-statement
+ALTER VIEW SWAP good_data okay_data
+----
+ALTER VIEW SWAP good_data okay_data
+=>
+AlterObjectSwap(AlterObjectSwapStatement { object_type: View, name_a: Item(UnresolvedItemName([Ident("good_data")])), name_b: Item(UnresolvedItemName([Ident("okay_data")])) })
+
+parse-statement
+ALTER MATERIALIZED VIEW SWAP ice_cream.avg_price ice_cream.median_price
+----
+ALTER MATERIALIZED VIEW SWAP ice_cream.avg_price ice_cream.median_price
+=>
+AlterObjectSwap(AlterObjectSwapStatement { object_type: MaterializedView, name_a: Item(UnresolvedItemName([Ident("ice_cream"), Ident("avg_price")])), name_b: Item(UnresolvedItemName([Ident("ice_cream"), Ident("median_price")])) })
+
+parse-statement
+ALTER SOURCE SWAP blue_data green_data
+----
+ALTER SOURCE SWAP blue_data green_data
+=>
+AlterObjectSwap(AlterObjectSwapStatement { object_type: Source, name_a: Item(UnresolvedItemName([Ident("blue_data")])), name_b: Item(UnresolvedItemName([Ident("green_data")])) })
+
+parse-statement
+ALTER SINK SWAP sink_a sink_b
+----
+ALTER SINK SWAP sink_a sink_b
+=>
+AlterObjectSwap(AlterObjectSwapStatement { object_type: Sink, name_a: Item(UnresolvedItemName([Ident("sink_a")])), name_b: Item(UnresolvedItemName([Ident("sink_b")])) })
+
+parse-statement
+ALTER CONNECTION SWAP fast_connection slow_connection
+----
+ALTER CONNECTION SWAP fast_connection slow_connection
+=>
+AlterObjectSwap(AlterObjectSwapStatement { object_type: Connection, name_a: Item(UnresolvedItemName([Ident("fast_connection")])), name_b: Item(UnresolvedItemName([Ident("slow_connection")])) })
+
+parse-statement
+ALTER FUNCTION SWAP function_blue func_green
+----
+ALTER FUNCTION SWAP function_blue func_green
+=>
+AlterObjectSwap(AlterObjectSwapStatement { object_type: Func, name_a: Item(UnresolvedItemName([Ident("function_blue")])), name_b: Item(UnresolvedItemName([Ident("func_green")])) })
+
+parse-statement
+ALTER TABLE SWAP a.b.c.d.e.f.g h.i.j.k.l.m.n
+----
+ALTER TABLE SWAP a.b.c.d.e.f.g h.i.j.k.l.m.n
+=>
+AlterObjectSwap(AlterObjectSwapStatement { object_type: Table, name_a: Item(UnresolvedItemName([Ident("a"), Ident("b"), Ident("c"), Ident("d"), Ident("e"), Ident("f"), Ident("g")])), name_b: Item(UnresolvedItemName([Ident("h"), Ident("i"), Ident("j"), Ident("k"), Ident("l"), Ident("m"), Ident("n")])) })
+
+parse-statement
+ALTER CLUSTER SWAP foo bar
+----
+ALTER CLUSTER SWAP foo bar
+=>
+AlterObjectSwap(AlterObjectSwapStatement { object_type: Cluster, name_a: Cluster(Ident("foo")), name_b: Cluster(Ident("bar")) })
+
+parse-statement
+ALTER CLUSTER SWAP cluster.too_many cluster.this_wont_work
+----
+error: Expected identifier, found dot
+ALTER CLUSTER SWAP cluster.too_many cluster.this_wont_work
+                          ^
+
+parse-statement
+ALTER CLUSTER REPLICA SWAP blue.r1 green.r1
+----
+ALTER CLUSTER REPLICA SWAP blue.r1 green.r1
+=>
+AlterObjectSwap(AlterObjectSwapStatement { object_type: ClusterReplica, name_a: ClusterReplica(QualifiedReplica { cluster: Ident("blue"), replica: Ident("r1") }), name_b: ClusterReplica(QualifiedReplica { cluster: Ident("green"), replica: Ident("r1") }) })
+
+parse-statement
+ALTER CLUSTER REPLICA SWAP cluster.replica.too_many cluster.replica.wont_work
+----
+error: Expected identifier, found dot
+ALTER CLUSTER REPLICA SWAP cluster.replica.too_many cluster.replica.wont_work
+                                          ^
+
+parse-statement
+ALTER DATABASE SWAP good_db other_db
+----
+ALTER DATABASE SWAP good_db other_db
+=>
+AlterObjectSwap(AlterObjectSwapStatement { object_type: Database, name_a: Database(UnresolvedDatabaseName(Ident("good_db"))), name_b: Database(UnresolvedDatabaseName(Ident("other_db"))) })
+
+parse-statement
+ALTER DATABASE SWAP too_many.components this.wont_work
+----
+error: Expected identifier, found dot
+ALTER DATABASE SWAP too_many.components this.wont_work
+                            ^
+
+parse-statement
+ALTER SCHEMA SWAP a.b.c d.e.f
+----
+ALTER SCHEMA SWAP a.b.c d.e.f
+=>
+AlterObjectSwap(AlterObjectSwapStatement { object_type: Schema, name_a: Schema(UnresolvedSchemaName([Ident("a"), Ident("b"), Ident("c")])), name_b: Schema(UnresolvedSchemaName([Ident("d"), Ident("e"), Ident("f")])) })
+
+parse-statement
+ALTER ROLE SWAP blue green
+----
+ALTER ROLE SWAP blue green
+=>
+AlterObjectSwap(AlterObjectSwapStatement { object_type: Role, name_a: Role(Ident("blue")), name_b: Role(Ident("green")) })
+
+parse-statement
+ALTER SUBSOURCE SWAP portion_of_source other_portion_of_source
+----
+error: Expected one of TABLE or VIEW or MATERIALIZED or SOURCE or SINK or INDEX or TYPE or ROLE or USER or CLUSTER or SECRET or CONNECTION or DATABASE or SCHEMA or FUNCTION, found SUBSOURCE
+ALTER SUBSOURCE SWAP portion_of_source other_portion_of_source
+      ^

test/sqllogictest/swap.slt
@@ -1,0 +1,21 @@
+# Copyright Materialize, Inc. and contributors. All rights reserved.
+#
+# Use of this software is governed by the Business Source License
+# included in the LICENSE file at the root of this repository.
+#
+# As of the Change Date specified in that file, in accordance with
+# the Business Source License, use of this software will be governed
+# by the Apache License, Version 2.0.
+
+mode cockroach
+
+reset-server
+
+statement ok
+CREATE SCHEMA blue;
+
+statement ok
+CREATE SCHEMA green;
+
+statement error ERROR: ALTER ... SWAP ... not yet supported, see https://github.com/MaterializeInc/materialize/issues/12972 for more details
+ALTER SCHEMA SWAP blue green;

src/sql/src/plan/statement.rs
@@ -120,6 +120,7 @@ pub fn describe(
         Statement::AlterConnection(stmt) => ddl::describe_alter_connection(&scx, stmt)?,
         Statement::AlterIndex(stmt) => ddl::describe_alter_index_options(&scx, stmt)?,
         Statement::AlterObjectRename(stmt) => ddl::describe_alter_object_rename(&scx, stmt)?,
+        Statement::AlterObjectSwap(stmt) => ddl::describe_alter_object_swap(&scx, stmt)?,
         Statement::AlterRole(stmt) => ddl::describe_alter_role(&scx, stmt)?,
         Statement::AlterSecret(stmt) => ddl::describe_alter_secret_options(&scx, stmt)?,
         Statement::AlterSetCluster(stmt) => ddl::describe_alter_set_cluster(&scx, stmt)?,
@@ -294,6 +295,7 @@ pub fn plan(
         Statement::AlterConnection(stmt) => ddl::plan_alter_connection(scx, stmt),
         Statement::AlterIndex(stmt) => ddl::plan_alter_index_options(scx, stmt),
         Statement::AlterObjectRename(stmt) => ddl::plan_alter_object_rename(scx, stmt),
+        Statement::AlterObjectSwap(stmt) => ddl::plan_alter_object_swap(scx, stmt),
         Statement::AlterRole(stmt) => ddl::plan_alter_role(scx, stmt),
         Statement::AlterSecret(stmt) => ddl::plan_alter_secret(scx, stmt),
         Statement::AlterSetCluster(stmt) => ddl::plan_alter_item_set_cluster(scx, stmt),

src/adapter/src/command.rs
@@ -787,10 +787,12 @@ impl ExecuteResponse {
         match plan {
             AbortTransaction => vec![TransactionRolledBack],
             AlterClusterRename
+            | AlterClusterSwap
             | AlterCluster
             | AlterClusterReplicaRename
             | AlterOwner
             | AlterItemRename
+            | AlterItemSwap
             | AlterNoop
             | AlterSecret
             | AlterSink

src/adapter/src/coord/command_handler.rs
@@ -531,6 +531,7 @@ impl Coordinator {
                     | Statement::AlterIndex(_)
                     | Statement::AlterSetCluster(_)
                     | Statement::AlterObjectRename(_)
+                    | Statement::AlterObjectSwap(_)
                     | Statement::AlterOwner(_)
                     | Statement::AlterRole(_)
                     | Statement::AlterSecret(_)

commit e28be51f38a54ab0d5e4992ad5b40c6050843375

sql: Fix `SWAP` with `IF EXISTS` (#36031)
Bug introduced in
https://github.com/MaterializeInc/materialize/pull/21979

We parsed `IF EXISTS` but then just ignored it.

src/sql-parser/src/ast/defs/statement.rs
@@ -2797,6 +2797,7 @@ impl_display_t!(AlterRetainHistoryStatement);
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct AlterObjectSwapStatement {
     pub object_type: ObjectType,
+    pub if_exists: bool,
     pub name_a: UnresolvedObjectName,
     pub name_b: Ident,
 }
@@ -2807,6 +2808,9 @@ impl AstDisplay for AlterObjectSwapStatement {
 
         f.write_node(&self.object_type);
         f.write_str(" ");
+        if self.if_exists {
+            f.write_str("IF EXISTS ");
+        }
         f.write_node(&self.name_a);
 
         f.write_str(" SWAP WITH ");

src/sql-parser/src/parser.rs
@@ -5872,6 +5872,7 @@ impl<'a> Parser<'a> {
 
                 Ok(Statement::AlterObjectSwap(AlterObjectSwapStatement {
                     object_type,
+                    if_exists,
                     name_a: UnresolvedObjectName::Cluster(name),
                     name_b,
                 }))
@@ -6700,6 +6701,7 @@ impl<'a> Parser<'a> {
 
                 Ok(Statement::AlterObjectSwap(AlterObjectSwapStatement {
                     object_type,
+                    if_exists,
                     name_a: name,
                     name_b,
                 }))

src/sql/src/plan/statement/ddl.rs
@@ -6768,6 +6768,7 @@ pub fn plan_alter_schema_swap<F>(
     scx: &mut StatementContext,
     name_a: UnresolvedSchemaName,
     name_b: Ident,
+    if_exists: bool,
     gen_temp_suffix: F,
 ) -> Result<Plan, PlanError>
 where
@@ -6787,7 +6788,19 @@ where
         sql_bail!("cannot swap schemas that are in the ambient database");
     }
 
-    let schema_a = scx.resolve_schema(name_a.clone())?;
+    let schema_a = match scx.resolve_schema(name_a.clone()) {
+        Ok(schema) => schema,
+        Err(_) if if_exists => {
+            scx.catalog.add_notice(PlanNotice::ObjectDoesNotExist {
+                name: name_a.to_ast_string_simple(),
+                object_type: ObjectType::Schema,
+            });
+            return Ok(Plan::AlterNoop(AlterNoopPlan {
+                object_type: ObjectType::Schema,
+            }));
+        }
+        Err(e) => return Err(e),
+    };
 
     let db_spec = schema_a.database().clone();
     if matches!(db_spec, ResolvedDatabaseSpecifier::Ambient) {
@@ -6918,12 +6931,25 @@ pub fn plan_alter_cluster_swap<F>(
     scx: &mut StatementContext,
     name_a: Ident,
     name_b: Ident,
+    if_exists: bool,
     gen_temp_suffix: F,
 ) -> Result<Plan, PlanError>
 where
     F: Fn(&dyn Fn(&str) -> bool) -> Result<String, PlanError>,
 {
-    let cluster_a = scx.resolve_cluster(Some(&name_a))?;
+    let cluster_a = match scx.resolve_cluster(Some(&name_a)) {
+        Ok(cluster) => cluster,
+        Err(_) if if_exists => {
+            scx.catalog.add_notice(PlanNotice::ObjectDoesNotExist {
+                name: name_a.to_ast_string_simple(),
+                object_type: ObjectType::Cluster,
+            });
+            return Ok(Plan::AlterNoop(AlterNoopPlan {
+                object_type: ObjectType::Cluster,
+            }));
+        }
+        Err(e) => return Err(e),
+    };
     let cluster_b = scx.resolve_cluster(Some(&name_b))?;
 
     let check = |temp_suffix: &str| {
@@ -6996,6 +7022,7 @@ pub fn plan_alter_object_swap(
 
     let AlterObjectSwapStatement {
         object_type,
+        if_exists,
         name_a,
         name_b,
     } = stmt;
@@ -7023,10 +7050,10 @@ pub fn plan_alter_object_swap(
 
     match (object_type, name_a, name_b) {
         (ObjectType::Schema, UnresolvedObjectName::Schema(name_a), name_b) => {
-            plan_alter_schema_swap(scx, name_a, name_b, gen_temp_suffix)
+            plan_alter_schema_swap(scx, name_a, name_b, if_exists, gen_temp_suffix)
         }
         (ObjectType::Cluster, UnresolvedObjectName::Cluster(name_a), name_b) => {
-            plan_alter_cluster_swap(scx, name_a, name_b, gen_temp_suffix)
+            plan_alter_cluster_swap(scx, name_a, name_b, if_exists, gen_temp_suffix)
         }
         (ObjectType::Schema | ObjectType::Cluster, _, _) => {
             unreachable!("parser ensures name type matches object type")

src/sql-parser/tests/testdata/alter
@@ -74,14 +74,21 @@ ALTER SCHEMA blue SWAP WITH green
 ----
 ALTER SCHEMA blue SWAP WITH green
 =>
-AlterObjectSwap(AlterObjectSwapStatement { object_type: Schema, name_a: Schema(UnresolvedSchemaName([Ident("blue")])), name_b: Ident("green") })
+AlterObjectSwap(AlterObjectSwapStatement { object_type: Schema, if_exists: false, name_a: Schema(UnresolvedSchemaName([Ident("blue")])), name_b: Ident("green") })
+
+parse-statement
+ALTER SCHEMA IF EXISTS blue SWAP WITH green
+----
+ALTER SCHEMA IF EXISTS blue SWAP WITH green
+=>
+AlterObjectSwap(AlterObjectSwapStatement { object_type: Schema, if_exists: true, name_a: Schema(UnresolvedSchemaName([Ident("blue")])), name_b: Ident("green") })
 
 parse-statement
 ALTER SCHEMA materialize.blue SWAP WITH green
 ----
 ALTER SCHEMA materialize.blue SWAP WITH green
 =>
-AlterObjectSwap(AlterObjectSwapStatement { object_type: Schema, name_a: Schema(UnresolvedSchemaName([Ident("materialize"), Ident("blue")])), name_b: Ident("green") })
+AlterObjectSwap(AlterObjectSwapStatement { object_type: Schema, if_exists: false, name_a: Schema(UnresolvedSchemaName([Ident("materialize"), Ident("blue")])), name_b: Ident("green") })
 
 parse-statement
 ALTER SCHEMA materialize.blue SWAP WITH other.green
@@ -95,7 +102,14 @@ ALTER CLUSTER foo SWAP WITH bar
 ----
 ALTER CLUSTER foo SWAP WITH bar
 =>
-AlterObjectSwap(AlterObjectSwapStatement { object_type: Cluster, name_a: Cluster(Ident("foo")), name_b: Ident("bar") })
+AlterObjectSwap(AlterObjectSwapStatement { object_type: Cluster, if_exists: false, name_a: Cluster(Ident("foo")), name_b: Ident("bar") })
+
+parse-statement
+ALTER CLUSTER IF EXISTS foo SWAP WITH bar
+----
+ALTER CLUSTER IF EXISTS foo SWAP WITH bar
+=>
+AlterObjectSwap(AlterObjectSwapStatement { object_type: Cluster, if_exists: true, name_a: Cluster(Ident("foo")), name_b: Ident("bar") })
 
 parse-statement
 ALTER CLUSTER cluster.too_many SWAP WITH cluster.this_wont_work

test/sqllogictest/swap.slt
@@ -176,6 +176,55 @@ statement ok
 ALTER SCHEMA blue SWAP WITH green;
 
 
+# Test IF EXISTS with SWAP.
+# IF EXISTS should be a no-op when the named object doesn't exist.
+
+# Cluster: IF EXISTS with non-existent cluster should succeed as a no-op.
+statement ok
+ALTER CLUSTER IF EXISTS nonexistent SWAP WITH bar;
+
+# Verify bar still exists and wasn't affected.
+query T
+SELECT name FROM mz_clusters WHERE name = 'bar';
+----
+bar
+
+# Cluster: IF EXISTS with existing cluster should still work normally.
+statement ok
+CREATE CLUSTER swap_test_a SIZE = 'scale=1,workers=1';
+
+statement ok
+CREATE CLUSTER swap_test_b SIZE = 'scale=1,workers=1';
+
+statement ok
+ALTER CLUSTER IF EXISTS swap_test_a SWAP WITH swap_test_b;
+
+statement ok
+DROP CLUSTER swap_test_a CASCADE;
+
+statement ok
+DROP CLUSTER swap_test_b CASCADE;
+
+# Schema: IF EXISTS with non-existent schema should succeed as a no-op.
+statement ok
+ALTER SCHEMA IF EXISTS nonexistent SWAP WITH green;
+
+# Schema: IF EXISTS with existing schema should still work normally.
+statement ok
+CREATE SCHEMA swap_s1;
+
+statement ok
+CREATE SCHEMA swap_s2;
+
+statement ok
+ALTER SCHEMA IF EXISTS swap_s1 SWAP WITH swap_s2;
+
+statement ok
+DROP SCHEMA swap_s1 CASCADE;
+
+statement ok
+DROP SCHEMA swap_s2 CASCADE;
+
 # Disable the feature.
 simple conn=mz_system,user=mz_system
 ALTER SYSTEM SET enable_alter_swap TO false;