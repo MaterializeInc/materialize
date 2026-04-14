MaterializeInc/materialize

commit 0ac4a9a3f1f5ea4dec5ced810976875bac66955b

Update to Rust 1.80 (#28674)
Update to Rust 1.80

### Checklist

- [ ] This PR has adequate test coverage / QA involvement has been duly
considered. ([trigger-ci for additional test/nightly
runs](https://trigger-ci.dev.materialize.com/))
- [ ] This PR has an associated up-to-date [design
doc](https://github.com/MaterializeInc/materialize/blob/main/doc/developer/design/README.md),
is a design doc
([template](https://github.com/MaterializeInc/materialize/blob/main/doc/developer/design/00000000_template.md)),
or is sufficiently small to not require a design.
  <!-- Reference the design in the description. -->
- [ ] If this PR evolves [an existing `$T ⇔ Proto$T`
mapping](https://github.com/MaterializeInc/materialize/blob/main/doc/developer/command-and-response-binary-encoding.md)
(possibly in a backwards-incompatible way), then it is tagged with a
`T-proto` label.
- [ ] If this PR will require changes to cloud orchestration or tests,
there is a companion cloud PR to account for those changes that is
tagged with the release-blocker label
([example](https://github.com/MaterializeInc/cloud/pull/5021)).
<!-- Ask in #team-cloud on Slack if you need help preparing the cloud
PR. -->
- [ ] This PR includes the following [user-facing behavior
changes](https://github.com/MaterializeInc/materialize/blob/main/doc/developer/guide-changes.md#what-changes-require-a-release-note):
- <!-- Add release notes here or explicitly state that there are no
user-facing behavior changes. -->

---------

Signed-off-by: Moritz Hoffmann <mh@materialize.com>

src/sql/src/session/vars/value.rs
@@ -306,8 +306,8 @@ impl Value for Duration {
             "d" => (Duration::from_secs, SEC_TO_DAY),
             o => {
                 return Err(VarParseError::InvalidParameterValue {
-                    invalid_values: vec![s.to_string()],
-                    reason: format!("expected us, ms, s, min, h, or d but got {o:?}").into(),
+                    invalid_values: vec![o.to_string()],
+                    reason: "expected us, ms, s, min, h, or d but got {o:?}".to_string(),
                 })
             }
         };
@@ -465,7 +465,7 @@ impl Value for Vec<Ident> {
             .collect::<Result<_, _>>()
             .map_err(|e| VarParseError::InvalidParameterValue {
                 invalid_values: values.to_vec(),
-                reason: e.to_string().into(),
+                reason: e.to_string(),
             })?;
         Ok(values)
     }
@@ -499,7 +499,7 @@ impl Value for Vec<SerializableDirective> {
             .collect();
         dirs.map_err(|e| VarParseError::InvalidParameterValue {
             invalid_values: values.to_vec(),
-            reason: e.to_string().into(),
+            reason: e.to_string(),
         })
     }
 
@@ -549,7 +549,7 @@ impl Value for Failpoints {
                 })?;
             fail::cfg(failpoint, action).map_err(|e| VarParseError::InvalidParameterValue {
                 invalid_values: input.to_vec(),
-                reason: e.to_string().into(),
+                reason: e.to_string(),
             })?;
         }
 
@@ -964,7 +964,7 @@ impl Value for CloneableEnvFilter {
         let s = extract_single_value(input)?;
         CloneableEnvFilter::from_str(s).map_err(|e| VarParseError::InvalidParameterValue {
             invalid_values: vec![s.to_string()],
-            reason: e.to_string().into(),
+            reason: e.to_string(),
         })
     }
 

commit e8432049910e114de5760a2b1682ce415a043ef5

sql: Fix error reason formatting
Broken in #28674

src/sql/src/session/vars/value.rs
@@ -314,7 +314,7 @@ impl Value for Duration {
             o => {
                 return Err(VarParseError::InvalidParameterValue {
                     invalid_values: vec![o.to_string()],
-                    reason: "expected us, ms, s, min, h, or d but got {o:?}".to_string(),
+                    reason: format!("expected us, ms, s, min, h, or d but got {o:?}"),
                 });
             }
         };