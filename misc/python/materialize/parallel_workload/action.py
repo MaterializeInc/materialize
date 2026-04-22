4f4538b61afbb150e5f75ef70bb0c4f43328fe13
Michael Greenberg <michael.greenberg@materialize.com>
2026-04-22 15:59:21 -0400

    optimizer: Push `COALESCE into `CASE WHEN` (#35826)
    ### Motivation
    
    
    https://materializeinc.slack.com/archives/C0246GEHL8N/p1774867717130349?thread_ts=1774866513.092989&cid=C0246GEHL8N
    
    ### Description
    
    Implements the optimization: 
    ```
    COALESCE((CASE WHEN e1 THEN e2 ELSE e3), ...)
    =>
    CASE WHEN e1 THEN COALESCE(e2, ...) ELSE COALESCE(e3, ...)
    ```
    In order to not pile onto `MSE::reduce`, I implemented this as an
    optimization that runs in the fixpoint before join implementation.
    
    (Before join implementation is important:
    https://github.com/MaterializeInc/database-issues/issues/11300 it's not
    clear what updates are possible once we've settled on arrangements, and
    others get this wrong [and should be fixed].)
    
    ### Verification
    
    SLT updates, spec tests.

--- misc/python/materialize/parallel_workload/action.py
+++ misc/python/materialize/parallel_workload/action.py
@@ -1582,6 +1582,7 @@ class FlipFlagsAction(Action):
         self.flags_with_values["enable_case_literal_transform"] = BOOLEAN_FLAG_VALUES
         self.flags_with_values["enable_cast_elimination"] = BOOLEAN_FLAG_VALUES
         self.flags_with_values["enable_upsert_v2"] = BOOLEAN_FLAG_VALUES
+        self.flags_with_values["enable_coalesce_case_transform"] = BOOLEAN_FLAG_VALUES
 
         # If you are adding a new config flag in Materialize, consider using it
         # here instead of just marking it as uninteresting to silence the
