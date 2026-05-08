MaterializeInc/materialize

commit 0acedcb19a8039ff7919b22393354ff92661a1ec

catalog: Improve error message in read-only write (#28666)
This commit improves the error message that is returned when a read only
catalog attempts to commit a write by including the contents of the
write in the error.

src/catalog/src/durable/persist.rs
@@ -1397,9 +1397,9 @@ impl DurableCatalogState for PersistCatalogState {
             // If the transaction is empty then we don't error, even in read-only mode. This matches the
             // semantics that the stash uses.
             if !txn_batch.is_empty() && catalog.is_read_only() {
-                return Err(DurableCatalogError::NotWritable(
-                    "cannot commit a transaction in a read-only catalog".to_string(),
-                )
+                return Err(DurableCatalogError::NotWritable(format!(
+                    "cannot commit a transaction in a read-only catalog: {txn_batch:?}"
+                ))
                 .into());
             }
 

commit cdeb677f66eae34e91aa1fcfdbe65a4c84558d68

catalog: Less detailled error message for read-only catalog (#36445)
Caused by #28666

src/catalog/src/durable/persist.rs
@@ -1805,8 +1805,14 @@ impl DurableCatalogState for PersistCatalogState {
             if catalog.mode == Mode::Readonly {
                 let updates: Vec<_> = StateUpdate::from_txn_batch(txn_batch).collect();
                 if !updates.is_empty() {
+                    let collection_types: Vec<_> = updates
+                        .iter()
+                        .filter_map(|u| u.0.collection_type())
+                        .collect();
                     return Err(DurableCatalogError::NotWritable(format!(
-                        "cannot commit a transaction in a read-only catalog: {updates:#?}"
+                        "cannot commit a transaction in a read-only catalog: \
+                         {} updates across collections: {collection_types:?}",
+                        updates.len(),
                     ))
                     .into());
                 }