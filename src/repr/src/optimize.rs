c12f359fc693723208a9c06b070adc2e653d8015
Frank McSherry <mcsherry@materialize.com>
2026-05-20 11:31:01 -0400

    Remove enable_guard_subquery_tablefunc feature flag

--- src/repr/src/optimize.rs
+++ src/repr/src/optimize.rs
@@ -95,7 +95,6 @@ optimizer_feature_flags!({
     // Use `EquivalenceClassesWithholdingErrors` instead of raw
     // `EquivalenceClasses` during eq prop for joins.
     enable_eq_classes_withholding_errors: bool,
-    enable_guard_subquery_tablefunc: bool,
     // Enable consolidation of unions that happen immediately after negate.
     //
     // The refinement happens in the LIR ⇒ LIR phase.
