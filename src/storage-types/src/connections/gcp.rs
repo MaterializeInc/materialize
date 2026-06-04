bdcdec3078e51a6a0200370c49a1b3e97499798b
Kynan Rilee <kynan.rilee@materialize.com>
2026-06-04 16:14:18 -0400

    Google says token_uri is always https://oauth2.googleapis.com/token

--- src/storage-types/src/connections/gcp.rs
+++ src/storage-types/src/connections/gcp.rs
@@ -24,6 +24,9 @@ use crate::controller::AlterError;
 /// endpoint without requiring any specific IAM grants on the service account.
 const VALIDATION_SCOPE: &str = "https://www.googleapis.com/auth/cloud-platform";
 
+/// Modern GCP Service Account keys always use the same token URI.
+const OAUTH_TOKEN_URI: &str = "https://oauth2.googleapis.com/token";
+
 /// GCP connection configuration.
 #[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize, Hash)]
 pub struct GcpConnection {
@@ -35,6 +38,28 @@ pub struct GcpConnection {
 impl AlterCompatible for GcpConnection {
     fn alter_compatible(&self, _id: GlobalId, _other: &Self) -> Result<(), AlterError> {
         // Every element of the GCP connection is configurable.
+        Ok(())
+    }
+}
+
+/// `gcp_auth` parses the Service Account Key JSON into a private type,
+/// so we have to write our own deserializer to check whether it's safe.
+#[derive(Deserialize)]
+pub struct GcpServiceAccountKeyTokenUri {
+    token_uri: String,
+}
+
+impl GcpServiceAccountKeyTokenUri {
+    pub fn validate_json(json: &str) -> Result<(), anyhow::Error> {
+        let k: GcpServiceAccountKeyTokenUri =
+            serde_json::from_str(json).map_err(anyhow::Error::from)?;
+
+        if k.token_uri != OAUTH_TOKEN_URI {
+            return Err(anyhow::Error::msg(format!(
+                "token_uri must be {OAUTH_TOKEN_URI}."
+            )));
+        }
+
         Ok(())
     }
 }
@@ -54,8 +79,10 @@ impl GcpConnection {
             .read_string(self.credentials_json)
             .await
             .map_err(GcpConnectionValidationError::SecretRead)?;
-        let service_account = CustomServiceAccount::from_json(&json)
+        GcpServiceAccountKeyTokenUri::validate_json(&json)
             .map_err(GcpConnectionValidationError::ParseKey)?;
+        let service_account = CustomServiceAccount::from_json(&json)
+            .map_err(|e| GcpConnectionValidationError::ParseKey(anyhow::Error::from(e)))?;
         service_account
             .token(&[VALIDATION_SCOPE])
             .await
@@ -74,7 +101,7 @@ pub enum GcpConnectionValidationError {
     #[error("failed to read service-account key from secret store: {}", .0.display_with_causes())]
     SecretRead(#[source] anyhow::Error),
     #[error("failed to parse service-account key JSON: {}", .0.display_with_causes())]
-    ParseKey(#[source] gcp_auth::Error),
+    ParseKey(#[source] anyhow::Error),
     #[error("failed to obtain access token from Google: {}", .0.display_with_causes())]
     FetchToken(#[source] gcp_auth::Error),
 }
