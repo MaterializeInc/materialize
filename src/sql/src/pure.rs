08052446722d0b7e2eed2c955f67cee83ac63ba7
Peter Larsen <plarsen@umich.edu>
2026-06-05 12:40:13 -0400

    Revert "PR feedback"
    This reverts commit dda02cacc3c80fd39ceadbb243640596adba46b9.

--- src/sql/src/pure.rs
+++ src/sql/src/pure.rs
@@ -2051,7 +2051,9 @@ async fn purify_create_table_from_source(
                 .iter_mut()
                 .find(|option| option.name == TableFromSourceOptionName::TextColumns)
             {
-                text_cols_option.value = gen_text_columns.map(WithOptionValue::Sequence);
+                if let Some(gen_text_columns) = gen_text_columns {
+                    text_cols_option.value = Some(WithOptionValue::Sequence(gen_text_columns));
+                }
             }
             if let Some(exclude_cols_option) = with_options
                 .iter_mut()
@@ -2104,7 +2106,9 @@ async fn purify_create_table_from_source(
                 .iter_mut()
                 .find(|option| option.name == TableFromSourceOptionName::TextColumns)
             {
-                text_cols_option.value = gen_text_columns.map(WithOptionValue::Sequence);
+                if let Some(gen_text_columns) = gen_text_columns {
+                    text_cols_option.value = Some(WithOptionValue::Sequence(gen_text_columns));
+                }
             }
             if let Some(exclude_cols_option) = with_options
                 .iter_mut()
@@ -2157,7 +2161,9 @@ async fn purify_create_table_from_source(
                 .iter_mut()
                 .find(|opt| opt.name == TableFromSourceOptionName::TextColumns)
             {
-                text_cols_option.value = gen_text_columns.map(WithOptionValue::Sequence);
+                if let Some(gen_text_columns) = gen_text_columns {
+                    text_cols_option.value = Some(WithOptionValue::Sequence(gen_text_columns));
+                }
             }
             if let Some(exclude_cols_option) = with_options
                 .iter_mut()
