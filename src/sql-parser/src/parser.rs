4c3569002614964224cf2d9c25f83f8ce0aca4fb
Kynan Rilee <kynan.rilee@gmail.com>
2026-06-10 11:29:07 -0400

    [SS-69] Make it obvious: Iceberg sink's AWS connection isn't used. (#36693)
    For Iceberg sinks to S3 Tables, we've been reusing the Iceberg Catalog
    connection's AWS creds.
    Users separately specify AWS creds for the Iceberg sink itself, but we
    (validate and then) ignore them.
    
    We _could_ remove the AWS creds option from Iceberg sinks, but:
    - Users have already defined Iceberg sinks with superfluous AWS creds
    (because it's required).
    - We'll likely need creds on Iceberg sinks when we support Polaris +
    \<insert storage service here>.
    
    **In this PR:**
    - Add comments about the sink creds being ignored.
    - Make sink creds optional. Iceberg sinks to Google BigLake will just
    reuse the catalog connection's GCP creds.
    
    _stacked on #36692_

--- src/sql-parser/src/parser.rs
+++ src/sql-parser/src/parser.rs
@@ -3861,7 +3861,7 @@ impl<'a> Parser<'a> {
         &mut self,
     ) -> Result<CreateSinkConnection<Raw>, ParserError> {
         self.expect_keyword(CONNECTION)?;
-        let connection = self.parse_raw_name()?;
+        let catalog_connection = self.parse_raw_name()?;
 
         let options = if self.consume_token(&Token::LParen) {
             let options = self.parse_comma_separated(Parser::parse_iceberg_sink_config_option)?;
@@ -3871,8 +3871,11 @@ impl<'a> Parser<'a> {
             vec![]
         };
 
-        self.expect_keywords(&[USING, AWS, CONNECTION])?;
-        let aws_connection = self.parse_raw_name()?;
+        let aws_connection = if self.parse_keywords(&[USING, AWS, CONNECTION]) {
+            Some(self.parse_raw_name()?)
+        } else {
+            None
+        };
 
         let key = if self.parse_keyword(KEY) {
             let key_columns = self.parse_parenthesized_column_list(Mandatory)?;
@@ -3887,7 +3890,7 @@ impl<'a> Parser<'a> {
         };
 
         Ok(CreateSinkConnection::Iceberg {
-            connection,
+            catalog_connection,
             aws_connection,
             key,
             options,
