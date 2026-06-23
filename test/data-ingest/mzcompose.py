836b897e538331fbe449e2abc84fbfd751630f6d
Dennis Felsing <dennis@felsing.org>
2026-06-23 15:09:59 +0200

    Revert "tests: Reenable two tests disabled because of RTR bug (#37244)" (#37250)
    This reverts commit 827a5a07dbf442d8eb7ac6983de33923bc4ea833.
    
    Still occurs in CI: https://buildkite.com/materialize/test/builds/126451
    &
    https://buildkite.com/materialize/test/builds/126450#019ef458-3343-4958-8b3c-fc98af612871

--- test/data-ingest/mzcompose.py
+++ test/data-ingest/mzcompose.py
@@ -18,7 +18,6 @@ import time
 from materialize import buildkite
 from materialize.data_ingest.executor import (
     KafkaExecutor,
-    KafkaRoundtripExecutor,
     MySqlExecutor,
 )
 from materialize.data_ingest.workload import *  # noqa: F401 F403
@@ -116,8 +115,9 @@ def workflow_default(c: Composition, parser: WorkflowArgumentParser) -> None:
         "sql-server",
     )
 
+    # TODO: Reenable KafkaRoundtripExecutor when https://linear.app/materializeinc/issue/SS-303 is fixed
     # TODO: Reenable SqlServerExecutor when https://linear.app/materializeinc/issue/SS-291 is fixed
-    executor_classes = [MySqlExecutor, KafkaExecutor, KafkaRoundtripExecutor]
+    executor_classes = [MySqlExecutor, KafkaExecutor]
 
     with c.override(
         # Fixed port so that we keep the same port after restarting Mz in disruptions
