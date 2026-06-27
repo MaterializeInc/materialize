9a023fae65ec75d4b2cf7ec3051bef8a2af98c6e
Dennis Felsing <dennis@felsing.org>
2026-06-27 06:43:24 +0000

    canary environment: Reenable iceberg sinks

--- test/canary-environment/models/qa_canary_environment/public_loadgen_sources/customer_gcs_iceberg_sink.sql
+++ test/canary-environment/models/qa_canary_environment/public_loadgen_sources/customer_gcs_iceberg_sink.sql
@@ -7,11 +7,10 @@
 -- the Business Source License, use of this software will be governed
 -- by the Apache License, Version 2.0.
 
--- TODO: Reenable when SS-282 is fixed
-CREATE SINK pg_relationships_gcs_iceberg_sink
+CREATE SINK customer_gcs_iceberg_sink
     IN CLUSTER qa_canary_environment_sinks
-    FROM qa_canary_environment.public_pg_cdc_sources.pg_relationships
-    INTO ICEBERG CATALOG CONNECTION qa_canary_environment.public.qa_canary_gcs_iceberg_catalog (NAMESPACE = 'qa_canary_environment', TABLE = 'pg_relationships')
-    KEY (a, b) NOT ENFORCED
+    FROM qa_canary_environment.public_loadgen_sources.customer_tbl
+    INTO ICEBERG CATALOG CONNECTION qa_canary_environment.public.qa_canary_gcs_iceberg_catalog (NAMESPACE = 'qa_canary_environment', TABLE = 'customer')
+    KEY (key)
     MODE UPSERT
     WITH (COMMIT INTERVAL = '60s');
